07c1f0156c64440b771b821726c4c0c9a8dc1950
Renato Cron <renato.cron@gmail.com>
2022-09-01 08:48:24 -0300

    Removendo unique dos CRUD (para com os 500), seed nao cria mais ODS,TipoOrgao,Orgao

---
+++ backend/prisma/migrations/20220901114559_drop_unique/migration.sql
@@ -0,0 +1,11 @@
+-- DropIndex
+DROP INDEX "fonte_recurso_fonte_key";
+
+-- DropIndex
+DROP INDEX "ods_numero_key";
+
+-- DropIndex
+DROP INDEX "orgao_descricao_key";
+
+-- DropIndex
+DROP INDEX "tipo_documento_codigo_key";
