c7884c603740ea7b35b4dade841ac1f84bece953
Renato Cron <renato.cron@gmail.com>
2022-09-02 10:18:39 -0300

    Ajustando Swagger do upload

--- backend/src/upload/upload.controller.ts
+++ backend/src/upload/upload.controller.ts
@@ -3,7 +3,7 @@ import { UploadService } from './upload.service';
 import { CreateUploadDto } from './dto/create-upload.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { PessoaFromJwt } from 'src/auth/models/PessoaFromJwt';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
 import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
 import { IpAddress } from 'src/common/decorators/current-ip';
 import { Upload } from 'src/upload/entities/upload.entity';
@@ -14,13 +14,14 @@ export class UploadController {
     constructor(private readonly uploadService: UploadService) { }
 
     @Post()
+    @ApiConsumes('multipart/form-data')
     @ApiBearerAuth('access-token')
     @UseInterceptors(FileInterceptor('arquivo'))
     async create(
         @Body() createUploadDto: CreateUploadDto,
         @CurrentUser() user: PessoaFromJwt,
         @UploadedFile() file: Express.Multer.File,
-        @IpAddress() ipAddress: string) : Promise<Upload>{
+        @IpAddress() ipAddress: string): Promise<Upload> {
 
         const uploadFile = await this.uploadService.upload(createUploadDto, user, file, ipAddress);
 
