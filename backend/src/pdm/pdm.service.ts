36194594635e790aac1993f0478e7a6aa94ce43c
Renato CRON <renato.cron@gmail.com>
2022-09-02 18:45:04 -0300

    Associar/listar uploads com PDM

--- backend/src/pdm/pdm.service.ts
+++ backend/src/pdm/pdm.service.ts
@@ -1,13 +1,19 @@
-import { ForbiddenException, HttpException, Injectable, Logger } from '@nestjs/common';
-import { Pessoa, Prisma } from '@prisma/client';
+import { ForbiddenException, HttpException, Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
 import { PessoaFromJwt } from 'src/auth/models/PessoaFromJwt';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { UploadService } from 'src/upload/upload.service';
+import { CreatePdmDocumentDto } from './dto/create-pdm-document.dto';
 import { CreatePdmDto } from './dto/create-pdm.dto';
 import { UpdatePdmDto } from './dto/update-pdm.dto';
+import { PdmDocument } from './entities/pdm-document.entity';
 
 @Injectable()
 export class PdmService {
-    constructor(private readonly prisma: PrismaService) { }
+    constructor(
+        private readonly prisma: PrismaService,
+        private readonly uploadService: UploadService,
+    ) { }
 
     async create(createPdmDto: CreatePdmDto, user: PessoaFromJwt) {
         const created = await this.prisma.pdm.create({
@@ -124,5 +130,51 @@ export class PdmService {
         return { id: id };
     }
 
+    async append_document(pdm_id: number, createPdmDocDto: CreatePdmDocumentDto, user: PessoaFromJwt) {
+        let pdm = await this.prisma.pdm.count({ where: { id: pdm_id } });
+        if (!pdm) throw new HttpException('PDM não encontrado', 404);
+
+        const arquivoId = this.uploadService.checkUploadToken(createPdmDocDto.upload_token);
+
+        const arquivo = await this.prisma.arquivoDocumento.create({
+            data: {
+                criado_em: new Date(Date.now()),
+                criado_por: user.id,
+                arquivo_id: arquivoId,
+                pdm_id: pdm_id
+            },
+            select: {
+                id: true
+            }
+        });
+
+        return { id: arquivo.id }
+    }
+
+    async list_document(pdm_id: number, user: PessoaFromJwt) {
+        let pdm = await this.prisma.pdm.count({ where: { id: pdm_id } });
+        if (!pdm) throw new HttpException('PDM não encontrado', 404);
+
+        const arquivos: PdmDocument[] = await this.prisma.arquivoDocumento.findMany({
+            where: { pdm_id: pdm_id, removido_em: null },
+            select: {
+                id: true,
+                arquivo: {
+                    select: {
+                        id: true,
+                        tamanho_bytes: true,
+                        TipoDocumento: true,
+                        descricao: true,
+                        nome_original: true
+                    }
+                }
+            }
+        });
+        for (const item of arquivos) {
+            item.arquivo.download_token = this.uploadService.getDownloadToken(item.arquivo.id, '1 month').download_token;
+        }
+
+        return arquivos
+    }
 
 }
