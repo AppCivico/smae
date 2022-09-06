f7faed80803f37da911a1d4619cc901927ba40ae
lucasansei <lucas.ansei@eokoe.com>
2022-09-06 16:38:41 -0300

    Removing try catch

--- backend/src/minha-conta/minha-conta.controller.ts
+++ backend/src/minha-conta/minha-conta.controller.ts
@@ -1,27 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
-import { ApiBearerAuth, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { ApiBearerAuth, ApiNoContentResponse, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
 import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
 import { PessoaFromJwt } from 'src/auth/models/PessoaFromJwt';
+import { RecordWithId } from 'src/common/dto/record-with-id.dto';
 import { MinhaContaDto } from 'src/minha-conta/models/minha-conta.dto';
+import { CreatePessoaDto } from 'src/pessoa/dto/create-pessoa.dto';
+import { PessoaService } from 'src/pessoa/pessoa.service';
+import { NovaSenhaDto } from './models/nova-senha.dto';
 
 @ApiTags('Minha Conta')
 @Controller('minha-conta')
 export class MinhaContaController {
+    constructor(private readonly pessoaService: PessoaService) { }
 
     @Get()
     @ApiBearerAuth('access-token')
     @ApiUnauthorizedResponse()
     getMe(@CurrentUser() user: PessoaFromJwt): MinhaContaDto {
-        try {
-            console.log('x')
-            if (global.gc) {
-                 global.gc();
-                 }
-        } catch (e) {
-            console.log("`node --expose-gc index.js`");
-            process.exit();
-        }
-
         return { 'sessao': user };
+    }
+
+    @Post()
+    @ApiBearerAuth('access-token')
+    @ApiUnauthorizedResponse()
+    @ApiNoContentResponse()
+    @HttpCode(HttpStatus.ACCEPTED)
+    async updatePassword(@Body() novaSenhaDto: NovaSenhaDto, @CurrentUser() user: PessoaFromJwt) {
+        await this.pessoaService.novaSenha(novaSenhaDto, user);
+        return '';
     }
 }
