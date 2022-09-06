6197d249ca31f46879defbd3a460260439f84b25
lucasansei <lucas.ansei@eokoe.com>
2022-09-06 16:46:53 -0300

    Adicionando rota para troca de senha

--- backend/src/minha-conta/models/nova-senha.dto.ts
+++ backend/src/minha-conta/models/nova-senha.dto.ts
@@ -2,11 +2,11 @@ import { ApiProperty } from "@nestjs/swagger";
 import { IsString, Matches, MaxLength, MinLength } from "class-validator";
 
 export class NovaSenhaDto {
-    @ApiProperty({ description: 'Senha atual' })
+    @ApiProperty({ description: 'Senha atual', example: 'Foo*bar1' })
     @IsString({ message: '$property| Senha: Precisa ser alfanumérico' })
     senha_corrente: string
 
-    @ApiProperty({ description: 'Senha nova' })
+    @ApiProperty({ description: 'Senha nova', example: 'Foo*bar1' })
     @IsString({ message: '$property| Senha: Precisa ser alfanumérico' })
     @MinLength(8, { message: '$property| Senha: Mínimo de 8 caracteres' })
     @MaxLength(1000, { message: '$property| Senha: Máximo de 1000 caracteres' })
