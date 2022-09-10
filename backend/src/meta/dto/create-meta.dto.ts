15b3f4d0d2be9f06f1ce9acf56e0faecdd05b84e
Renato CRON <renato.cron@gmail.com>
2022-09-10 20:39:39 -0300

    Meta com metaOrgao preenchido

--- backend/src/meta/dto/create-meta.dto.ts
+++ backend/src/meta/dto/create-meta.dto.ts
@@ -1,5 +1,40 @@
 import { Type } from "class-transformer"
 import { IsOptional, IsPositive, IsString, MaxLength, ValidateIf } from "class-validator"
+import { IsTrueFalseString } from "src/common/decorators/IsTrueFalseStr"
+
+export class MetaParticipanteOuResp {
+    /**
+   * ID da pessoa_id
+   * @example "1"
+    */
+    @IsPositive({ message: '$property| pessoa_id' })
+    @Type(() => Number)
+    pessoa_id: number;
+}
+
+export class MetaOrgaoParticipante {
+    /**
+    * orgão participante é responsável?
+    * @example "false"
+    */
+    @IsTrueFalseString()
+    responsavel: string
+
+
+    /**
+    * órgão
+    * @example "1"
+    */
+    @IsPositive({ message: '$property| orgao_id' })
+    @Type(() => Number)
+    orgao_id: number;
+
+    /**
+    * participante é responsável?
+    */
+    participantes: MetaParticipanteOuResp[]
+
+}
 
 export class CreateMetaDto {
 
@@ -69,4 +104,14 @@ export class CreateMetaDto {
     @Type(() => Number)
     pdm_id: number
 
+
+    /**
+    * orgaos_participantes
+    */
+    orgaos_participantes: MetaOrgaoParticipante[]
+
+    /**
+    * meta_participantes
+    */
+    coordenadores_cp: MetaParticipanteOuResp[]
 }
--- backend/src/meta/meta.service.ts
+++ backend/src/meta/meta.service.ts
@@ -1,7 +1,9 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
 import { PessoaFromJwt } from 'src/auth/models/PessoaFromJwt';
+import { RecordWithId } from 'src/common/dto/record-with-id.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { CreateMetaDto } from './dto/create-meta.dto';
+import { CreateMetaDto, MetaOrgaoParticipante, MetaParticipanteOuResp } from './dto/create-meta.dto';
 import { UpdateMetaDto } from './dto/update-meta.dto';
 
 @Injectable()
@@ -10,18 +12,75 @@ export class MetaService {
 
     async create(createMetaDto: CreateMetaDto, user: PessoaFromJwt) {
 
-        const created = await this.prisma.meta.create({
-            data: {
-                criado_por: user.id,
-                criado_em: new Date(Date.now()),
-                status: '',
-                ativo: true,
-                ...createMetaDto,
-            },
-            select: { id: true }
+        const created = await this.prisma.$transaction(async (prisma: Prisma.TransactionClient): Promise<RecordWithId> => {
+            const meta = await prisma.meta.create({
+                data: {
+                    criado_por: user.id,
+                    criado_em: new Date(Date.now()),
+                    status: '',
+                    ativo: true,
+                    ...createMetaDto,
+                },
+                select: { id: true }
+            });
+
+            await prisma.metaOrgao.createMany({
+                data: await this.buildOrgaosParticipantes(meta.id, createMetaDto.orgaos_participantes, createMetaDto.coordenadores_cp),
+            });
+
+            return meta;
         });
 
         return created;
+    }
+
+    async buildOrgaosParticipantes(metaId: number, orgaos_participantes: MetaOrgaoParticipante[], coordenadores_cp: MetaParticipanteOuResp[]): Promise<Prisma.MetaOrgaoCreateManyInput[]> {
+        const arr: Prisma.MetaOrgaoCreateManyInput[] = [];
+
+        let orgaoVisto: Record<number, boolean> = {};
+        // ordena por responsáveis primeiro
+        orgaos_participantes.sort((a, b) => {
+            return a.responsavel === 'true' && b.responsavel === 'false' ? -1 :
+                a.responsavel === 'true' && b.responsavel === 'true' ? 0 : 1;
+        });
+
+        for (const orgao of orgaos_participantes) {
+            if (!orgaoVisto[orgao.orgao_id]) {
+                orgaoVisto[orgao.orgao_id] = true;
+
+                arr.push({
+                    orgao_id: orgao.orgao_id,
+                    responsavel: orgao.responsavel === 'true',
+                    meta_id: metaId
+                });
+            }
+        }
+
+        // buscar pelo orgão "implicito" via membros dos coordenadores_cp
+        for (const coordenador of coordenadores_cp) {
+            const pessoaFisicaOrgao = await this.prisma.pessoa.findFirst({
+                where: {
+                    id: coordenador.pessoa_id
+                },
+                select: {
+                    pessoa_fisica: { select: { orgao_id: true } }
+                }
+            });
+
+            const orgaoId = pessoaFisicaOrgao?.pessoa_fisica?.orgao_id;
+            if (orgaoId) {
+                if (!orgaoVisto[orgaoId]) {
+                    orgaoVisto[orgaoId] = true;
+                    arr.push({
+                        orgao_id: orgaoId,
+                        responsavel: false,// o coordenador nunca é o responśavel da meta
+                        meta_id: metaId
+                    });
+                }
+            }
+        }
+
+        return arr;
     }
 
     async findAll() {
