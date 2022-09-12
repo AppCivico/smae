a16d43e850e62d50212aa46f125262a632b8c3b8
Renato CRON <renato.cron@gmail.com>
2022-09-12 08:17:51 -0300

    Listando metas

--- backend/src/meta/entities/meta.entity.ts
+++ backend/src/meta/entities/meta.entity.ts
@@ -2,6 +2,23 @@ export class IdDesc {
     id: number
     descricao: string
 }
+
+export class IdNomeExibicao {
+    id: number
+    nome_exibicao: string
+}
+
+export class MetaOrgao {
+    orgao: IdDesc
+    responsavel: boolean
+}
+
+export class MetaResp {
+    orgao: IdDesc
+    pessoa: IdNomeExibicao
+    coorderandor_responsavel_cp: boolean
+}
+
 export class Meta {
     id: number
     status: string
@@ -14,4 +31,6 @@ export class Meta {
     tema: IdDesc | null
     sub_tema: IdDesc | null
     ativo: boolean
+    meta_orgao: MetaOrgao[]
+    meta_responsavel: MetaResp[]
 }
