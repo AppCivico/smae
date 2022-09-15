fff5149b9cc21cbda7511e119f08a1f5045dbe16
Renato Cron <renato.cron@gmail.com>
2022-09-15 17:09:47 -0300

    Mudanças de tipagem

--- backend/src/common/decorators/find-params.ts
+++ backend/src/common/decorators/find-params.ts
@@ -1,18 +1,22 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNumberString, IsOptional, IsString } from 'class-validator';
+import { Transform } from 'class-transformer';
+import { IsNumberString } from 'class-validator';
 
 export class FindOneParams {
     @ApiProperty()
     @IsNumberString(undefined, { message: ':id precisa ser um número' })
+    @Transform(({ value }) => +value)
     id: number;
 }
 
 export class FindTwoParams {
     @ApiProperty()
     @IsNumberString(undefined, { message: ':id precisa ser um número' })
+    @Transform(({ value }) => +value)
     id: number;
 
     @ApiProperty()
     @IsNumberString(undefined, { message: ':id2 precisa ser um número' })
+    @Transform(({ value }) => +value)
     id2: number;
 }
