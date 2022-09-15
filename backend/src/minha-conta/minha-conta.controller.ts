135993236f2327d7fb0ebe5f941355928315856d
Renato Cron <renato.cron@gmail.com>
2022-09-15 18:18:30 -0300

    Remove unecessary deps

--- backend/src/minha-conta/minha-conta.controller.ts
+++ backend/src/minha-conta/minha-conta.controller.ts
@@ -2,9 +2,7 @@ import { Body, Controller, Get, HttpCode, HttpStatus, Post } from '@nestjs/commo
 import { ApiBearerAuth, ApiNoContentResponse, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
 import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
 import { PessoaFromJwt } from 'src/auth/models/PessoaFromJwt';
-import { RecordWithId } from 'src/common/dto/record-with-id.dto';
 import { MinhaContaDto } from 'src/minha-conta/models/minha-conta.dto';
-import { CreatePessoaDto } from 'src/pessoa/dto/create-pessoa.dto';
 import { PessoaService } from 'src/pessoa/pessoa.service';
 import { NovaSenhaDto } from './models/nova-senha.dto';
 
