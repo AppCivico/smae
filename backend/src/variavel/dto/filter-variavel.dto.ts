1e52374b5398c3e286de75ef82552501e95618b7
Renato Cron <renato.cron@gmail.com>
2022-09-16 17:01:33 +0000

    WIP variaveis associadas com indicador

---
+++ backend/src/variavel/dto/filter-variavel.dto.ts
@@ -0,0 +1,32 @@
+import { Transform, Type } from "class-transformer";
+import { IsBoolean, IsOptional, IsPositive } from "class-validator";
+
+export class FilterVariavelDto {
+    /**
+   * Filtrar por meta_id?
+   * @example "1"
+    */
+    @IsOptional()
+    @IsPositive({ message: '$property| meta_id' })
+    @Type(() => Number)
+    meta_id?: number;
+
+    /**
+  * Filtrar por indicador_id?
+  * @example "1"
+   */
+    @IsOptional()
+    @IsPositive({ message: '$property| indicador_id' })
+    @Type(() => Number)
+    indicador_id?: number;
+
+
+    /**
+    * Não retornar as variáveis desativadas? envie true
+   * @example "1"
+   */
+    @IsOptional()
+    @IsBoolean()
+    @Transform(({ value }: any) => value === 'true')
+    remover_desativados?: boolean;
+}
