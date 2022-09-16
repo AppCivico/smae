b6c92c315228e6d85e1510a227e887b7d1b71069
Renato Cron <renato.cron@gmail.com>
2022-09-16 19:21:42 +0000

    Fix porPeriodo

--- backend/src/common/date2ymd.ts
+++ backend/src/common/date2ymd.ts
@@ -1,6 +1,6 @@
 export class Date2YMD {
-    static fromUTC(d: Date) {
-        const str = d.toUTCString();
-        return str.substring(0, 12)
+    static toString(d: Date) {
+        const str = d.toISOString();
+        return str.substring(0, 10)
     }
 }
