4e10d61ba961a50615525b77b2dfa471643065a4
Renato Cron <renato.cron@gmail.com>
2022-09-16 16:29:41 +0000

    Add nivel_regionalizacao (com WIP variaveis)

--- backend/src/variavel/variavel.service.ts
+++ backend/src/variavel/variavel.service.ts
@@ -1,13 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Prisma } from '@prisma/client';
 import { PessoaFromJwt } from 'src/auth/models/PessoaFromJwt';
+import { RecordWithId } from 'src/common/dto/record-with-id.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateVariavelDto } from './dto/create-variavel.dto';
 import { UpdateVariavelDto } from './dto/update-variavel.dto';
 
 @Injectable()
 export class VariavelService {
-    create(createVariavelDto: CreateVariavelDto, user: PessoaFromJwt) {
+    constructor(
+        private readonly jwtService: JwtService,
+        private readonly prisma: PrismaService,
+    ) { }
 
-        return { id: 0 };
+    async create(createVariavelDto: CreateVariavelDto, user: PessoaFromJwt) {
+        // TODO: verificar se todos os membros de createVariavelDto.responsaveis estão ativos e sao realmente do orgão createVariavelDto.orgao_id
+        // TODO: verificar se o createVariavelDto.periodicidade é a mesma do indicador (por enquanto)
+
+        const created = await this.prisma.$transaction(async (prisma: Prisma.TransactionClient): Promise<RecordWithId> => {
+            let r = createVariavelDto.responsaveis!;
+            delete createVariavelDto.responsaveis;
+
+            const variavel = await prisma.variavel.create({
+                data: {
+                    ...createVariavelDto,
+                },
+                select: { id: true }
+            });
+
+
+            return variavel;
+        });
+
+
+        return { id: created.id };
     }
 
 }
