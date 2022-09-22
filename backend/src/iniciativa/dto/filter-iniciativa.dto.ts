43ffdae584e892016cc0cd8e2ee0f059b8eda1ff
lucasansei <lucas.ansei@eokoe.com>
2022-09-22 10:22:26 -0300

    Updating text for swagger

--- backend/src/iniciativa/dto/filter-iniciativa.dto.ts
+++ backend/src/iniciativa/dto/filter-iniciativa.dto.ts
@@ -3,11 +3,11 @@ import { IsOptional, IsPositive } from "class-validator";
 
 export class FilterIniciativaDto {
     /**
-   * Filtrar por pdm_id?
+   * Filtrar por meta_id?
    * @example "1"
     */
     @IsOptional()
-    @IsPositive({ message: '$property| pdm_id' })
+    @IsPositive({ message: '$property| meta_id' })
     @Type(() => Number)
     meta_id: number;
 }
