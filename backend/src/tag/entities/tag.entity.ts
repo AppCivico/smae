b19bb464583d582a6330b4ca886fe49d3806aa5b
lucasansei <lucas.ansei@eokoe.com>
2022-09-23 09:44:00 -0300

    Add tag for Meta and Iniciativa

--- backend/src/tag/entities/tag.entity.ts
+++ backend/src/tag/entities/tag.entity.ts
@@ -1,6 +1,8 @@
 export class Tag {
     descricao: string;
     icone: string | null;
-    pdm_id: number;
+    pdm_id: number | null;
+    meta_id: number | null;
+    iniciativa_id: number | null;
     ods_id: number | null;
 }
