11709a8800ce35c53beb203d1b26eeac50359484
lucasansei <lucas.ansei@eokoe.com>
2022-09-29 10:37:30 -0300

    Add bool on iniciativa GET

--- backend/src/iniciativa/entities/iniciativa.entity.ts
+++ backend/src/iniciativa/entities/iniciativa.entity.ts
@@ -24,4 +24,5 @@ export class Iniciativa {
     complemento: string | null
     orgaos_participantes: IniciativaOrgao[]
     coordenadores_cp: IdNomeExibicao[]
+    compoe_indicador_meta: boolean
 }
