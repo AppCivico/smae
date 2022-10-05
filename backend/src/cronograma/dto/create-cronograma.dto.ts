9c146ee28ad5bbd248ab1c7017ea2fc67f7d593a
lucasansei <lucas.ansei@eokoe.com>
2022-10-05 09:39:26 -0300

    Fix: error messages

--- backend/src/cronograma/dto/create-cronograma.dto.ts
+++ backend/src/cronograma/dto/create-cronograma.dto.ts
@@ -42,7 +42,7 @@ export class CreateCronogramaDto {
     /**
     * iniciativa_id
     */
-    @IsPositive({ message: '$property| meta precisa ser um número ou null' })
+    @IsPositive({ message: '$property| iniciativa precisa ser um número ou null' })
     @Type(() => Number)
     @ValidateIf((object, value) => value !== null)
     @IsOptional()
@@ -51,7 +51,7 @@ export class CreateCronogramaDto {
     /**
     * atividade_id
     */
-    @IsPositive({ message: '$property| meta precisa ser um número ou null' })
+    @IsPositive({ message: '$property| atividade precisa ser um número ou null' })
     @Type(() => Number)
     @ValidateIf((object, value) => value !== null)
     @IsOptional()
@@ -60,21 +60,21 @@ export class CreateCronogramaDto {
     /**
     * descricao
     */
-    @IsString({ message: '$property| contexto: Precisa ser alfanumérico' })
+    @IsString({ message: '$property| descricao: Precisa ser alfanumérico' })
     @IsOptional()
     descricao?: string
 
     /**
     * observacao
     */
-    @IsString({ message: '$property| contexto: Precisa ser alfanumérico' })
+    @IsString({ message: '$property| observacao: Precisa ser alfanumérico' })
     @IsOptional()
     observacao?: string
 
-    @IsBoolean({ message: '$property| precisa ser um número' })
+    @IsBoolean({ message: '$property| precisa ser um boolean' })
     por_regiao: boolean
 
-    @IsString({ message: '$property| contexto: Precisa ser alfanumérico' })
+    @IsString({ message: '$property| tipo_regiao: Precisa ser alfanumérico' })
     tipo_regiao?: string
 
 
