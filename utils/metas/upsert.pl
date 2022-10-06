2b83161d3096f9a39bd2a50ceb10e81860d5db74
Renato CRON <renato.cron@gmail.com>
2022-10-05 22:42:18 -0300

    insert de metas

--- utils/metas/upsert.pl
+++ utils/metas/upsert.pl
@@ -7,23 +7,192 @@ use Text::CSV qw( csv );
 use JSON;
 use open qw/:std :utf8/;
 
-my $enpdoint = $ENV{URL}    || die 'missing URL';
-my $apikey   = $ENV{APIKEY} || die 'missing APIKEY';
-my $pdm_id   = $ENV{PDM_ID} || die 'missing PDM_ID';
+my $enpdoint  = $ENV{URL}       || die 'missing URL';
+my $apikey    = $ENV{APIKEY}    || die 'missing APIKEY';
+my $pdm_id    = $ENV{PDM_ID}    || die 'missing PDM_ID';
+my $perfil_cp = $ENV{PERFIL_CP} || die 'missing PERFIL_CP';
 
 my $ua = Mojo::UserAgent->new;
 
 do {
-    my ($temas_ids)      = &upsert_temas('tema.utf8.csv', 'tema');
     my ($ods_ids)        = &upsert_ods('ods.utf8.csv');
+    my ($temas_ids)      = &upsert_temas('tema.utf8.csv',       'tema');
     my ($macrotemas_ids) = &upsert_temas('macro_tema.utf8.csv', 'macrotema');
     my ($tags_ids)       = &upsert_tags('tag.csv', $ods_ids);
+    my ($orgao_ids)      = &lookup_orgao('../orgaos/orgao.tsv');
+    &upsert_metas(
+        'Meta.csv',
+        'Meta_tag.csv',
+        'Meta_Orgao.csv',
+        $ods_ids,
+        $temas_ids,
+        $macrotemas_ids,
+        $tags_ids,
+        $orgao_ids,
+    );
+
 };
 
 exit;
 
+sub lookup_orgao {
+    my ($tsv) = @_;
+
+    my $aoh = csv(
+        in      => $tsv,
+        headers => "auto",
+        binary  => 1,
+        sep     => "\t",
+    );
+
+    my $current_orgao
+      = [$ua->get($enpdoint . '/orgao' => {authorization => "Bearer $apikey"})->result->json->{linhas}->@*];
+
+    my $by_desc_orgao = {};
+    $by_desc_orgao->{$_->{descricao}} = $_ for $current_orgao->@*;
+
+    my $backref = {};
+    foreach my $orgao (@{$aoh}) {
+        $orgao->{descricao} = trim($orgao->{descricao});
+
+        if (!$by_desc_orgao->{$orgao->{descricao}}) {
+            die 'missing orgao ' . $orgao->{descricao};
+        }
+        else {
+            $backref->{$orgao->{id}} = $by_desc_orgao->{$orgao->{descricao}}{id};
+        }
+    }
+
+    return $backref;
+}
+
+sub upsert_metas {
+    my ($metasin, $tagsin, $orgaoin, $ods_ids, $temas_ids, $macrotemas_ids, $tags_ids, $orgao_ids) = @_;
+
+
+    my $current_pessoas = [
+        $ua->get(
+            $enpdoint . "/pessoa?coorderandor_responsavel_cp=true" => {authorization => "Bearer $apikey"},
+        )->result->json->{linhas}->@*
+    ];
+
+    my $pessoa_by_orgao = {};
+    foreach my $pessoa (@{$current_pessoas}) {
+        push @{$pessoa_by_orgao->{$pessoa->{orgao_id}}}, $pessoa->{id};
+    }
+
+
+    my $orgao_aoh = csv(
+        in      => $orgaoin,
+        headers => "auto",
+        binary  => 1,
+        sep     => ';',
+    );
+
+    # monta lista de todos os orgãos que serão necessarios ter membro responsável
+    my $orgao_necessarios = {};
+    $orgao_necessarios->{$orgao_ids->{$_->{orgao_id}}} = 1 for @{$orgao_aoh};
+
+    foreach my $orgao_id (keys %$orgao_necessarios) {
+        next if $pessoa_by_orgao->{$orgao_id};
+
+        my $res = $ua->post(
+            $enpdoint . "/pessoa" => {authorization => "Bearer $apikey"},
+            json                  => {
+                email             => 'tmp.responsavel.' . $orgao_id . '@appcivico.com',
+                nome_exibicao     => 'tmp responsavel ' . $orgao_id,
+                nome_completo     => 'tmp responsavel ' . $orgao_id,
+                lotacao           => 'null',
+                cargo             => 'null',
+                orgao_id          => $orgao_id,
+                perfil_acesso_ids => [$perfil_cp * 1],
+            }
+        )->result->json;
+        my $id = $res->{id} || die 'missing id';
+        push @{$pessoa_by_orgao->{$orgao_id}}, $id;
+    }
+
+    # ler todas as metas
+    my $tags_aoh = csv(
+        in      => $tagsin,
+        headers => "auto",
+        binary  => 1,
+        sep     => ';',
+    );
+    my $tags_por_meta = {};
+
+    # filtra apenas as definidas
+    $tags_ids->{$_->{tag_id}} && push @{$tags_por_meta->{$_->{meta_id}}}, $tags_ids->{$_->{tag_id}} for @$tags_aoh;
+
+    my $orgao_por_meta = {};
+    my $resp_por_meta  = {};
+    for my $orgao_csv (@$orgao_aoh) {
+        push @{$orgao_por_meta->{$orgao_csv->{meta_id}}}, {
+            responsavel   => $orgao_csv->{orgao_responsavel} =~ /false/i ? \0 : \1,
+            orgao_id      => $orgao_ids->{$orgao_csv->{orgao_id}},
+            participantes => $pessoa_by_orgao->{$orgao_ids->{$orgao_csv->{orgao_id}}}
+        };
+
+        $resp_por_meta->{$orgao_csv->{meta_id}} = $pessoa_by_orgao->{$orgao_ids->{$orgao_csv->{orgao_id}}};
+    }
+
+    my $metas_aoh = csv(
+        in      => $metasin,
+        headers => "auto",
+        binary  => 1,
+        sep     => ';',
+    );
+
+    my $current_metas = [
+        $ua->get(
+            $enpdoint . "/meta?pdm_id=$pdm_id" => {authorization => "Bearer $apikey"},
+        )->result->json->{linhas}->@*
+    ];
+
+    my $meta_por_codigo = {};
+    $meta_por_codigo->{$_->{codigo}} = $_ for @$current_metas;
+    use DDP;
+    p $tags_por_meta;
+    foreach my $meta (@{$metas_aoh}) {
+        for (keys %$meta) {
+            $meta->{$_} = trim($meta->{$_});
+            delete $meta->{$_} if ($meta->{$_} eq 'NULL');
+        }
+        next if $meta_por_codigo->{$meta->{codigo}};
+
+        my $json = {
+            pdm_id           => $pdm_id,
+            codigo           => $meta->{codigo},
+            complemento      => $meta->{complemento} || '',
+            contexto         => $meta->{contexto}    || '',
+            macro_tema_id    => $meta->{macrotema_id} ? $macrotemas_ids->{$meta->{macrotema_id}} : undef,
+            tema_id          => $meta->{tema_id}      ? $temas_ids->{$meta->{tema_id}}           : undef,
+            titulo           => $meta->{titulo},
+            coordenadores_cp => $resp_por_meta->{$meta->{id}},
+            (
+                (@{$tags_por_meta->{$meta->{id}} || []} > 0)
+                ? (
+                    tags => $tags_por_meta->{$meta->{id}},
+                  )
+                : ()
+            ),
+            orgaos_participantes => $orgao_por_meta->{$meta->{id}},
+        };
+        use DDP;
+        p $json;
+        my $res = $ua->post(
+            $enpdoint . "/meta" => {authorization => "Bearer $apikey"},
+            json                => $json
+        )->result->json;
+        use DDP;
+        p $res;
+        my $id = $res->{id} || die 'missing id';
+    }
+
+}
+
 sub upsert_tags {
-    my $fn = shift;
+    my $fn      = shift;
     my $ods_ids = shift;
 
     my $current_tags = [
@@ -34,7 +203,7 @@ sub upsert_tags {
 
     my $exists_by_name = {};
     $exists_by_name->{$_->{descricao}} = $_ for @$current_tags;
-use DDP; p $exists_by_name;
+
     my $aoh = csv(
         in      => $fn,
         headers => "auto",
@@ -45,7 +214,7 @@ use DDP; p $exists_by_name;
     my $backref = {};
     foreach my $csv (@$aoh) {
         $csv->{descricao} = trim($csv->{descricao});
-        $csv->{ods_id} = trim($csv->{ods_id});
+        $csv->{ods_id}    = trim($csv->{ods_id});
 
         $csv->{descricao} ||= 'vazio ' . $csv->{id};
 
@@ -68,7 +237,8 @@ use DDP; p $exists_by_name;
             $backref->{$csv->{id}} = $exists_by_name->{$csv->{descricao}}{id} = $id;
         }
     }
-use DDP; p $backref;
+    use DDP;
+    p $backref;
 
     return ($backref);
 }
@@ -85,8 +255,6 @@ sub upsert_ods {
 
     my $exists_by_number = {};
     $exists_by_number->{$_->{numero}} = $_ for @$current_ods;
-    use DDP;
-    p $exists_by_number;
 
     my $aoh = csv(
         in      => $fn,
@@ -96,8 +264,9 @@ sub upsert_ods {
     );
     my $backref = {};
     foreach my $csv (@$aoh) {
-        use DDP;
-        p $csv;
+
+#        use DDP;
+        #       p $csv;
         $csv->{titulo}    = trim($csv->{titulo});
         $csv->{descricao} = trim($csv->{descricao});
         $csv->{numero}    = trim($csv->{numero});
