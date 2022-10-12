d871b310d9673263a35192199a1355f22e28c578
Renato Cron <renato.cron@gmail.com>
2022-10-11 22:38:09 -0300

    Verificar se variaveis enviadas fazem parte do indicador

--- backend/src/indicador/dto/create-indicador.dto.ts
+++ backend/src/indicador/dto/create-indicador.dto.ts
@@ -65,35 +65,6 @@ export class CreateIndicadorDto {
     periodicidade: Periodicidade
 
     /**
-    * Expressão para montar calcular as series do indicador
-    *
-    * Funções: CEIL, FLOOR, POWER, LOG
-    *
-    * Operações: + - / *
-    *
-    * Referencias das variáveis: $REFERENCIA
-    *
-    * @example "CEIL($A + $B) / 100.4 * power( $B + LOG( $A ) ) * FLOOR( 1 - 1 / 2 )"
-    */
-    @IsOptional()
-    @IsString({ message: '$property| Precisa ser um texto de formula válido' })
-    @ValidateIf((object, value) => value !== null)
-    formula?: string | null
-
-    /**
-    * Variáveis para usar na expressão
-    */
-    @IsOptional()
-    formula_variaveis?: FormulaVariaveis[]
-
-    /**
-    * calcular_acumulado - se é para usar o acumulado tbm nas series da formula
-    */
-    @IsBoolean({ message: '$property| precisa ser um boolean' })
-    @ValidateIf((object, value) => value !== null)
-    calcular_acumulado?: boolean | null
-
-    /**
     * regionalizavel
     */
     @IsBoolean({ message: '$property| precisa ser um boolean' })
