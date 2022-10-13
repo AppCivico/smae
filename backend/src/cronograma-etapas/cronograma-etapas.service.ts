2e1bed63d5ddf2f1c2d2d924db306665e55b72ee
lucasansei <lucas.ansei@eokoe.com>
2022-10-13 15:15:42 -0300

    Adding logs

--- backend/src/cronograma-etapas/cronograma-etapas.service.ts
+++ backend/src/cronograma-etapas/cronograma-etapas.service.ts
@@ -146,8 +146,17 @@ export class CronogramaEtapaService {
         for (const cronogramaEtapa of cronogramaEtapas) {
 
             if (cronogramaEtapa.etapa.etapa_pai_id) {
-                const parentIndex = cronogramaEtapas.map(e => e.etapa_id).indexOf(cronogramaEtapa.etapa.etapa_pai_id);
-                if (parentIndex) continue;
+                const firstLevelParentIndex = cronogramaEtapas.map(e => e.etapa_id).indexOf(cronogramaEtapa.etapa.etapa_pai_id);
+                console.log(firstLevelParentIndex);
+                if (firstLevelParentIndex) continue;
+
+                // let secondLevelParentIndex;
+                // const foo = cronogramaEtapas.filter(e => {
+                //     return e.etapa.etapa_filha.length
+                // })
+                // if (cronogramaEtapa.etapa.etapa_filha.length > 0) {
+                    
+                // }
             }
         
             let ordem;
