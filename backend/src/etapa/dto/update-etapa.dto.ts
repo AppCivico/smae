44c9e08ce434c25f159ba8903fe8199d75d58920
lucasansei <lucas.ansei@eokoe.com>
2022-10-17 10:15:48 -0300

    Allow update of regiao_id for Etapa

--- backend/src/etapa/dto/update-etapa.dto.ts
+++ backend/src/etapa/dto/update-etapa.dto.ts
@@ -3,7 +3,8 @@ import { CreateEtapaDto } from './create-etapa.dto';
 
 export class UpdateEtapaDto extends PartialType(OmitType(CreateEtapaDto, [
     'etapa_pai_id',
-    'regiao_id',
     'cronograma_id',
     'ordem'
-])) { }
+])) {
+    
+}
