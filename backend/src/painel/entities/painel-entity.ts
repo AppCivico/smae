0754fd3fb70037e8fa630db769b9568c505e6f74
lucasansei <lucas.ansei@eokoe.com>
2022-10-20 15:05:48 -0300

    Update Painel POST

--- backend/src/painel/entities/painel-entity.ts
+++ backend/src/painel/entities/painel-entity.ts
@@ -1,5 +1,10 @@
+import { Periodicidade } from "@prisma/client"
+
 export class Painel {
     id: number
     nome: string
-    periodo: string
+    periodicidade: Periodicidade
+    mostrar_planejado_por_padrao: boolean
+    mostrar_acumulado_por_padrao: boolean
+    mostrar_indicador_por_padrao: boolean
 }
