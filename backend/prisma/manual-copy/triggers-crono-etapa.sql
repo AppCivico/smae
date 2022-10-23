78f913b6acb0eb9eaf14ab1c8d1d229b189e2f34
Renato Santos <renato.cron@gmail.com>
2022-10-23 13:33:35 -0300

    Update triggers-crono-etapa.sql
    v2, ainda não revisada, ta mais facil sempre recalcular tudo e só conferir se não ta criando um loop atualizando pro mesmo valor, pq ao remover eu acho q tbm precisa marcar recalcular pelo menos o cronograma de acordo com as linhas atuais

--- backend/prisma/manual-copy/triggers-crono-etapa.sql
+++ backend/prisma/manual-copy/triggers-crono-etapa.sql
@@ -1,39 +1,82 @@
 CREATE OR REPLACE FUNCTION f_trg_estapa_esticar_datas_do_pai() RETURNS trigger AS $emp_stamp$
     BEGIN
+    
+        -- etapas
+        IF NEW.etapa_pai_id IS NOT NULL THEN
+        
+            if (NEW.inicio_previsto IS NOT NULL) then
+                UPDATE etapa e
+                SET inicio_previsto = NEW.inicio_previsto
+                WHERE e.id = NEW.etapa_pai_id
+                AND (e.inicio_previsto IS NULL OR e.inicio_previsto > NEW.inicio_previsto); -- apenas se tiver maior
+            END IF;
+            
+            IF  NEW.inicio_real IS NOT NULL THEN
+                UPDATE etapa e
+                SET inicio_real = NEW.inicio_real
+                WHERE e.id = NEW.etapa_pai_id
+                AND (e.inicio_real IS  NULL OR e.inicio_real > NEW.inicio_real) ;
+            END IF;
 
-        IF NEW.etapa_pai_id IS NOT NULL AND NEW.inicio_previsto IS NOT NULL THEN
-            UPDATE etapa e
-            SET inicio_previsto = NEW.inicio_previsto
-            WHERE e.id = NEW.etapa_pai_id
-            AND e.inicio_previsto IS NOT NULL
-            AND e.inicio_previsto > NEW.inicio_previsto; -- apenas se tiver maior
-
+            IF  NEW.termino_previsto IS NOT NULL THEN
+                UPDATE etapa e
+                SET termino_previsto = (
+                     select max(termino_previsto) from etapa ef where ef.etapa_pai_id = NEW.etapa_pai_id and removido_em is null
+                )
+                WHERE e.id = NEW.etapa_pai_id
+                AND (
+                    select count(1) from etapa ef where ef.etapa_pai_id = NEW.etapa_pai_id
+                    where ef.termino_real IS NULL  and removido_em is null
+                ) = 0
+                AND (e.termino_previsto IS NULL OR e.termino_previsto != (
+                     select max(termino_previsto) from etapa ef where ef.etapa_pai_id = NEW.etapa_pai_id  and removido_em is null
+                ));
+            END IF;
+            
+            
+            IF  NEW.termino_real IS NOT NULL THEN
+                UPDATE etapa e
+                SET termino_real = (
+                     select max(termino_previsto) from etapa ef where ef.etapa_pai_id = NEW.etapa_pai_id  and removido_em is null
+                )
+                WHERE e.id = NEW.etapa_pai_id
+                AND (
+                    select count(1) from etapa ef where ef.etapa_pai_id = NEW.etapa_pai_id
+                    where ef.termino_real IS NULL  and removido_em is null
+                ) = 0
+                AND (termino_real is null or termino_real != (
+                     select max(termino_previsto) from etapa ef where ef.etapa_pai_id = NEW.etapa_pai_id  and removido_em is null
+                )); 
+            END IF;
+            
         END IF;
 
-        IF NEW.cronograma_id IS NOT NULL AND NEW.inicio_previsto IS NOT NULL THEN
+        -- cronogramas
+        IF NEW.cronograma_id IS NOT NULL THEN
+            UPDATE cronograma c
+            SET inicio_previsto = (
+                select min(inicio_previsto) from etapa ef where ef.cronograma_id = NEW.cronograma_id  and removido_em is null
+            )
+            WHERE c.id = NEW.cronograma_id
+            AND (c.inicio_previsto IS NULL OR inicio_previsto != (
+                select min(inicio_previsto) from etapa ef where ef.cronograma_id = NEW.cronograma_id  and removido_em is null
+            ));
+
+            UPDATE cronograma c
+            SET inicio_real = (
+                select min(inicio_real) from etapa ef where ef.cronograma_id = NEW.cronograma_id  and removido_em is null
+            )
+            WHERE c.id = NEW.cronograma_id
+            AND (c.inicio_real IS NULL OR inicio_real != (
+                select min(inicio_real) from etapa ef where ef.cronograma_id = NEW.cronograma_id  and removido_em is null
+            ));
 
             UPDATE cronograma e
-            SET inicio_previsto = NEW.inicio_previsto
+            SET termino_previsto = (select max(termino_previsto) from etapa where ef.cronograma_id = NEW.cronograma_id and removido_em is null)
             WHERE e.id = NEW.cronograma_id
-            AND e.inicio_previsto IS NOT NULL
-            AND e.inicio_previsto > NEW.inicio_previsto; -- apenas se tiver maior
-        END IF;
-
-        IF NEW.etapa_pai_id IS NOT NULL AND NEW.termino_previsto IS NOT NULL THEN
-            UPDATE etapa e
-            SET termino_previsto = NEW.termino_previsto
-            WHERE e.id = NEW.etapa_pai_id
-            AND e.termino_previsto IS NOT NULL
-            AND e.termino_previsto < NEW.termino_previsto; -- apenas se tiver menor
-        END IF;
-
-        IF NEW.cronograma_id IS NOT NULL AND NEW.termino_previsto IS NOT NULL THEN
-
-            UPDATE cronograma e
-            SET termino_previsto = NEW.termino_previsto
-            WHERE e.id = NEW.cronograma_id
-            AND e.termino_previsto IS NOT NULL
-            AND e.termino_previsto < NEW.termino_previsto; -- apenas se tiver menor
+            AND (e.termino_previsto is null or termino_previsto != (select max(termino_previsto) from etapa where ef.cronograma_id = NEW.cronograma_id  and removido_em is null))
+            AND (select count(1) from etapa where ef.cronograma_id = NEW.cronograma_id and termino_previsto is null and removido_em is null) = 0;
+             
         END IF;
 
         RETURN NEW;
@@ -51,5 +94,9 @@ CREATE TRIGGER trg_estapa_esticar_datas_do_pai_update AFTER  UPDATE ON etapa
         (OLD.inicio_previsto IS DISTINCT FROM NEW.inicio_previsto)
         OR
         (OLD.termino_previsto IS DISTINCT FROM NEW.termino_previsto)
+        OR
+        (OLD.inicio_real IS DISTINCT FROM NEW.inicio_real)
+        OR
+        (OLD.termino_real IS DISTINCT FROM NEW.termino_real)
     )
     EXECUTE FUNCTION f_trg_estapa_esticar_datas_do_pai();
