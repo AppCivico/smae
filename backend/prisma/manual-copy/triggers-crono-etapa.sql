8d0465bed4b7cd55799c5760dcaf842edae5f99a
Renato Cron <renato.cron@gmail.com>
2022-10-24 19:21:23 -0300

    Mudar para versão onde o termino_real e termino_previsto podem virar null novamente na fase/subfase

--- backend/prisma/manual-copy/triggers-crono-etapa.sql
+++ backend/prisma/manual-copy/triggers-crono-etapa.sql
@@ -2,11 +2,10 @@ CREATE OR REPLACE FUNCTION atualiza_inicio_fim_cronograma (pCronoId int)
     RETURNS varchar
     AS $$
 DECLARE
-
-v_inicio_previsto date;
-v_inicio_real date;
-v_termino_previsto date;
-v_termino_real date;
+    v_inicio_previsto date;
+    v_inicio_real date;
+    v_termino_previsto date;
+    v_termino_real date;
 BEGIN
 
     SELECT
@@ -86,72 +85,83 @@ LANGUAGE plpgsql;
 
 
 CREATE OR REPLACE FUNCTION f_trg_estapa_esticar_datas_do_pai() RETURNS trigger AS $emp_stamp$
-    BEGIN
+DECLARE
+    v_termino_previsto date;
+    v_termino_real date;
+BEGIN
 
-        -- apenas em modificações de etapas (e nao fases e subfases)
-        -- buscar quais cronogramas a etapa está associada, e então recalcular um por um
-        IF NEW.etapa_pai_id IS NULL THEN
-            PERFORM  atualiza_inicio_fim_cronograma(sub.cronograma_id)
-                FROM (
-                    select ce.cronograma_id
-                    FROM cronograma_etapa ce
-                    WHERE ce.etapa_id = NEW.id
-                    GROUP BY 1
-                ) sub;
+    -- apenas em modificações de etapas (e nao fases e subfases)
+    -- buscar quais cronogramas a etapa está associada, e então recalcular um por um
+    IF NEW.etapa_pai_id IS NULL THEN
+        PERFORM  atualiza_inicio_fim_cronograma(sub.cronograma_id)
+            FROM (
+                select ce.cronograma_id
+                FROM cronograma_etapa ce
+                WHERE ce.etapa_id = NEW.id
+                GROUP BY 1
+            ) sub;
+    END IF;
+
+    -- fase e subfases
+    IF NEW.etapa_pai_id IS NOT NULL THEN
+
+        if (NEW.inicio_previsto IS NOT NULL) then
+            UPDATE etapa e
+            SET inicio_previsto = NEW.inicio_previsto
+            WHERE e.id = NEW.etapa_pai_id
+            AND (e.inicio_previsto IS NULL OR e.inicio_previsto > NEW.inicio_previsto); -- apenas se tiver maior
         END IF;
 
-        -- fase e subfases
-        IF NEW.etapa_pai_id IS NOT NULL THEN
-
-            if (NEW.inicio_previsto IS NOT NULL) then
-                UPDATE etapa e
-                SET inicio_previsto = NEW.inicio_previsto
-                WHERE e.id = NEW.etapa_pai_id
-                AND (e.inicio_previsto IS NULL OR e.inicio_previsto > NEW.inicio_previsto); -- apenas se tiver maior
-            END IF;
-
-            IF  NEW.inicio_real IS NOT NULL THEN
-                UPDATE etapa e
-                SET inicio_real = NEW.inicio_real
-                WHERE e.id = NEW.etapa_pai_id
-                AND (e.inicio_real IS  NULL OR e.inicio_real > NEW.inicio_real) ;
-            END IF;
-
-
-            -- sempre recalcula o termino_previsto de acordo com a situacao atual
+        IF  NEW.inicio_real IS NOT NULL THEN
             UPDATE etapa e
-            SET termino_previsto = (
-                 select max(termino_previsto) from etapa ef where ef.etapa_pai_id = NEW.etapa_pai_id and removido_em is null
-            )
+            SET inicio_real = NEW.inicio_real
             WHERE e.id = NEW.etapa_pai_id
-            AND (
-                select count(1) from etapa ef where ef.etapa_pai_id = NEW.etapa_pai_id
-                AND ef.termino_previsto IS NULL  and removido_em is null
-            ) = 0
-            AND (e.termino_previsto IS NULL OR e.termino_previsto != (
-                 select max(termino_previsto) from etapa ef where ef.etapa_pai_id = NEW.etapa_pai_id and removido_em is null
-            ));
-
-
-            -- sempre recalcula o termino_real de acordo com a situacao atual
-            UPDATE etapa e
-            SET termino_real = (
-                 select max(termino_real) from etapa ef where ef.etapa_pai_id = NEW.etapa_pai_id  and removido_em is null
-            )
-            WHERE e.id = NEW.etapa_pai_id
-            AND (
-                select count(1) from etapa ef where ef.etapa_pai_id = NEW.etapa_pai_id
-                AND ef.termino_real IS NULL  and removido_em is null
-            ) = 0
-            AND (termino_real is null or termino_real != (
-                 select max(termino_real) from etapa ef where ef.etapa_pai_id = NEW.etapa_pai_id  and removido_em is null
-            ));
-
-
+            AND (e.inicio_real IS  NULL OR e.inicio_real > NEW.inicio_real) ;
         END IF;
 
-        RETURN NEW;
-    END;
+
+        -- sempre recalcula o termino_previsto de acordo com a situacao atual
+        SELECT (
+             select max(termino_previsto) from etapa ef where ef.etapa_pai_id = NEW.etapa_pai_id and removido_em is null
+        ) into v_termino_previsto
+        FROM etapa e
+        WHERE e.id = NEW.etapa_pai_id
+        AND (
+            select count(1) from etapa ef where ef.etapa_pai_id = NEW.etapa_pai_id
+            AND ef.termino_previsto IS NULL  and removido_em is null
+        ) = 0
+        AND (e.termino_previsto IS NULL OR e.termino_previsto != (
+             select max(termino_previsto) from etapa ef where ef.etapa_pai_id = NEW.etapa_pai_id and removido_em is null
+        ));
+
+
+        -- sempre recalcula o termino_real de acordo com a situacao atual
+        SELECT (
+             select max(termino_real) from etapa ef where ef.etapa_pai_id = NEW.etapa_pai_id  and removido_em is null
+        ) into v_termino_real
+        FROM etapa e
+        WHERE e.id = NEW.etapa_pai_id
+        AND (
+            select count(1) from etapa ef where ef.etapa_pai_id = NEW.etapa_pai_id
+            AND ef.termino_real IS NULL  and removido_em is null
+        ) = 0
+        AND (termino_real is null or termino_real != (
+             select max(termino_real) from etapa ef where ef.etapa_pai_id = NEW.etapa_pai_id  and removido_em is null
+        ));
+
+        UPDATE etapa e
+        SET termino_previsto = v_termino_previsto,
+            termino_real = v_termino_real
+        WHERE e.id = NEW.etapa_pai_id
+        AND (
+            (termino_previsto IS DISTINCT FROM v_termino_previsto) OR
+            (termino_real IS DISTINCT FROM v_termino_real)
+        );
+
+    END IF;
+
+    RETURN NEW;
+END;
 $emp_stamp$ LANGUAGE plpgsql;
 
 
