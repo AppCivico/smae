c657e75b5744c899c55b3ee2ddb3115f467f77f1
Renato Cron <renato.cron@gmail.com>
2022-10-26 18:00:13 -0300

    Endpoint /pdm?ativo=true agora volta os dados do ciclo

--- backend/src/pdm/dto/list-pdm.dto.ts
+++ backend/src/pdm/dto/list-pdm.dto.ts
@@ -1,5 +1,21 @@
+import { DateYMD } from "src/common/date2ymd";
 import { ListPdm } from "src/pdm/entities/list-pdm.entity";
+
+export class CicloFisicoFase {
+    id: number
+    data_inicio: DateYMD
+    data_fim: DateYMD
+    ciclo_fase: string
+    fase_corrente: boolean
+}
+
+export class CicloFisicoAtivo {
+    id: number
+    data_ciclo: DateYMD
+    fases: CicloFisicoFase[]
+}
 
 export class ListPdmDto {
     linhas: ListPdm[]
+    ciclo_fisico_ativo?: CicloFisicoAtivo | null
 }
