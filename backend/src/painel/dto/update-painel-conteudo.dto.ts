ff21224f5cdac8f3448bcd0adbde00f458e00cde
lucasansei <lucas.ansei@eokoe.com>
2022-10-26 09:55:48 -0300

    Add update for detalhes

--- backend/src/painel/dto/update-painel-conteudo.dto.ts
+++ backend/src/painel/dto/update-painel-conteudo.dto.ts
@@ -1,12 +1,23 @@
 import { OmitType, PartialType } from '@nestjs/swagger';
+import { IsArray, IsBoolean, IsNumber } from 'class-validator';
+import { RecordWithId } from 'src/common/dto/record-with-id.dto';
 import { CreatePainelConteudoDto } from './create-painel-conteudo.dto';
 
-export class UpdatePainelConteudoDto extends PartialType(OmitType(CreatePainelConteudoDto, [
+export class UpdatePainelConteudoVisualizacaoDto extends PartialType(OmitType(CreatePainelConteudoDto, [
     'meta_id',
     'mostrar_indicador',
     'painel_id'
 ])) { }
 
+export class UpdatePainelConteudoDetalheDto {
+    @IsBoolean()
+    mostrar_indicador?: boolean | null
+
+    @IsNumber()
+    id: number
+
+    filhos?:  UpdatePainelConteudoDetalheDto[] | null
+}
 export class PainelConteudoIdAndMeta {
     id: number
     meta_id: number
@@ -15,4 +26,7 @@ export class PainelConteudoIdAndMeta {
 export class PainelConteudoUpsertRet {
     created: PainelConteudoIdAndMeta[]
     deleted: PainelConteudoIdAndMeta[]
+}
+export class PainelConteudoDetalheUpdateRet {
+    updated: RecordWithId[]
 }
