c35357ef62c20a13f01638e37d0aac79efe3ecdd
Renato Cron <renato.cron@gmail.com>
2022-10-31 15:53:54 -0300

    Trigger de permissão do acesso

--- backend/prisma/migrations/20221031184119_variavel_responsavel/migration.sql
+++ backend/prisma/migrations/20221031184119_variavel_responsavel/migration.sql
@@ -1,3 +1,6 @@
+alter table "VariavelResponsavel" rename to variavel_responsavel;
+
+
 CREATE OR REPLACE FUNCTION pessoa_acesso_pdm (pPessoa_id int)
     RETURNS varchar
     AS $$
@@ -256,4 +259,37 @@ BEGIN
 END
 $$
 LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION f_recalc_acesso_pessoas() RETURNS trigger AS $emp_stamp$
+BEGIN
+    PERFORM  select pessoa_acesso_pdm(id) from pessoa where ativa;
+    RETURN NEW;
+END;
+$emp_stamp$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_ciclo_fisico_recalc_pessoa AFTER INSERT OR DELETE OR UPDATE ON ciclo_fisico
+    FOR EACH STATEMENT
+    EXECUTE FUNCTION f_recalc_acesso_pessoas();
+
+CREATE TRIGGER trg_meta_responsavel_recalc_pessoa AFTER INSERT OR DELETE OR UPDATE ON meta
+    FOR EACH STATEMENT
+    EXECUTE FUNCTION f_recalc_acesso_pessoas();
+
+CREATE TRIGGER trg_iniciativa_responsavel_recalc_pessoa AFTER INSERT OR DELETE OR UPDATE ON iniciativa
+    FOR EACH STATEMENT
+    EXECUTE FUNCTION f_recalc_acesso_pessoas();
+
+CREATE TRIGGER trg_atividade_responsavel_recalc_pessoa AFTER INSERT OR DELETE OR UPDATE ON atividade
+    FOR EACH STATEMENT
+    EXECUTE FUNCTION f_recalc_acesso_pessoas();
+
+CREATE TRIGGER trg_cronograma_etapa_recalc_pessoa AFTER INSERT OR DELETE OR UPDATE ON cronograma_etapa
+    FOR EACH STATEMENT
+    EXECUTE FUNCTION f_recalc_acesso_pessoas();
+
+CREATE TRIGGER trg_variavel_responsavel_recalc_pessoa AFTER INSERT OR DELETE OR UPDATE ON variavel_responsavel
+    FOR EACH STATEMENT
+    EXECUTE FUNCTION f_recalc_acesso_pessoas();
+
 
