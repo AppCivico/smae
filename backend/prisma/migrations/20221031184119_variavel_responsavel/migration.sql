d27ed84be35e47c273ed87895358a87988803a43
Renato CRON <renato.cron@gmail.com>
2022-10-31 17:36:06 -0300

    Update trigger func  f_recalc_acesso_pessoas

--- backend/prisma/migrations/20221031184119_variavel_responsavel/migration.sql
+++ backend/prisma/migrations/20221031184119_variavel_responsavel/migration.sql
@@ -263,7 +263,7 @@ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION f_recalc_acesso_pessoas() RETURNS trigger AS $emp_stamp$
 BEGIN
-    PERFORM pessoa_acesso_pdm(id) from pessoa where ativa;
+    PERFORM pessoa_acesso_pdm(id) from pessoa where desativado=false;
     RETURN NEW;
 END;
 $emp_stamp$ LANGUAGE plpgsql;
