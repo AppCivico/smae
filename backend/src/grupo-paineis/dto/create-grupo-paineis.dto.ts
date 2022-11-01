f8d977d31396570f465a479ec3caa3d766cf10ae
lucasansei <lucas.ansei@eokoe.com>
2022-11-01 15:50:29 -0300

    Add grupo paineis CRUD

---
+++ backend/src/grupo-paineis/dto/create-grupo-paineis.dto.ts
@@ -0,0 +1,16 @@
+import { IsBoolean, IsString, MaxLength } from "class-validator";
+
+export class CreateGrupoPaineisDto {
+    /**
+    * nome
+    */
+    @IsString({ message: '$property| Precisa ser alfanumérico' })
+    @MaxLength(30, { message: '$property| código 30 caracteres' })
+    nome: string
+
+    /**
+    * ativo
+    */
+    @IsBoolean()
+    ativo: boolean
+}
