99f48cfa750a8f48d0ed602eafd8d121d6a40480
lucasansei <lucas.ansei@eokoe.com>
2022-11-10 15:01:30 -0300

    Fix: empty series

--- backend/src/painel/dto/detalhe-painel.dto.ts
+++ backend/src/painel/dto/detalhe-painel.dto.ts
@@ -1,4 +1,5 @@
 import { Periodicidade, Periodo } from '@prisma/client';
+import { RowWithIdTitle } from '../entities/painel-conteudo.entity';
 
 export class DetailPainelVisualizacaoDto {
     id: number
@@ -18,6 +19,26 @@ export class PainelConteudoSerieDto {
 
 }
 
+export class PainelConteudoSerie {
+    meta: PainelConteudoMeta
+    // detalhes: 
+}
+
+export class PainelConteudoMeta {
+    id: number
+    titulo: string
+    codigo: string
+    indicador: RowWithIdTitle | null
+}
+
+export class RowWithSeries {
+    id: number
+    codigo?: string
+    titulo: string
+    series: SeriesTemplate
+
+    // iniciativa?: 
+}
 export class SeriesTemplate {
     titulo: string
     periodo_inicio: Date
