98d14c293b5ea05fa515428dc8488bb989af43f0
Renato Cron <renato.cron@gmail.com>
2022-11-16 12:20:28 -0300

    Atualizando endpoints do cronograma que o filter seja igual o que o serviço espera

--- backend/src/mf/metas/metas-cronogramas.controller.ts
+++ backend/src/mf/metas/metas-cronogramas.controller.ts
@@ -3,6 +3,9 @@ import { ApiBearerAuth, ApiExtraModels, ApiOkResponse, ApiTags, refs } from '@ne
 import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
 import { Roles } from 'src/auth/decorators/roles.decorator';
 import { PessoaFromJwt } from 'src/auth/models/PessoaFromJwt';
+import { CronogramaEtapaService } from 'src/cronograma-etapas/cronograma-etapas.service';
+import { FilterCronogramaEtapaDto } from 'src/cronograma-etapas/dto/filter-cronograma-etapa.dto';
+import { ListCronogramaEtapaDto } from 'src/cronograma-etapas/dto/list-cronograma-etapa.dto';
 import { CronogramaService } from 'src/cronograma/cronograma.service';
 import { FilterCronogramaDto } from 'src/cronograma/dto/fillter-cronograma.dto';
 import { ListCronogramaDto } from 'src/cronograma/dto/list-cronograma.dto';
@@ -15,11 +18,12 @@ import { RequestInfoDto } from './dto/mf-meta.dto';
 export class MetasCronogramaController {
     constructor(
         private readonly cronogramaService: CronogramaService,
+        private readonly cronogramaEtapaService: CronogramaEtapaService,
         private readonly mfService: MfService
     ) { }
 
 
-    @Get('cronogramas')
+    @Get('cronograma')
     @ApiBearerAuth('access-token')
     @Roles('PDM.admin_cp', 'PDM.tecnico_cp', 'PDM.ponto_focal')
     @ApiExtraModels(ListCronogramaDto, RequestInfoDto)
@@ -37,6 +41,28 @@ export class MetasCronogramaController {
 
         return {
             'linhas': await this.cronogramaService.findAll(filters),
+            requestInfo: { queryTook: Date.now() - start },
+        };
+    }
+
+    @Get('cronograma-etapa')
+    @ApiBearerAuth('access-token')
+    @Roles('PDM.admin_cp', 'PDM.tecnico_cp', 'PDM.ponto_focal')
+    @ApiExtraModels(ListCronogramaEtapaDto, RequestInfoDto)
+    @ApiOkResponse({
+        schema: { allOf: refs(ListCronogramaEtapaDto, RequestInfoDto) },
+    })
+    async cronogramas_etapas(
+        @Query() filters: FilterCronogramaEtapaDto,
+        @CurrentUser() user: PessoaFromJwt
+    ): Promise<ListCronogramaEtapaDto & RequestInfoDto> {
+        const start = Date.now();
+        const config = await this.mfService.pessoaAcessoPdm(user);
+
+        filters.cronograma_etapa_ids = config.cronogramas_etapas;
+
+        return {
+            'linhas': await this.cronogramaEtapaService.findAll(filters),
             requestInfo: { queryTook: Date.now() - start },
         };
     }
