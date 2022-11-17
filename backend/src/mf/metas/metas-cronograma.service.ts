dfde4d95f4d83693a9cc23f7633d3cdf07c05cfc
Renato Cron <renato.cron@gmail.com>
2022-11-17 14:21:56 -0300

    Adicionando /mf/metas/{id}/iniciativas-e-atividades

--- backend/src/mf/metas/metas-cronograma.service.ts
+++ backend/src/mf/metas/metas-cronograma.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { RetornoMetaCronogramaDto } from 'src/mf/metas/dto/mf-crono.dto';
+import { IniciativasCronoRetorno, RetornoMetaCronogramaDto } from 'src/mf/metas/dto/mf-crono.dto';
+import { MfService } from 'src/mf/mf.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 
@@ -8,9 +9,10 @@ export class MetasCronogramaService {
 
     constructor(
         private readonly prisma: PrismaService,
+        private readonly mfService: MfService,
     ) { }
 
-    async metaIniciativaAtividadesComCrono(metaId: number): Promise<RetornoMetaCronogramaDto> {
+    async metaIniciativaAtividadesComCrono(meta_id: number): Promise<RetornoMetaCronogramaDto> {
         const dadosMetas: {
             meta_id: number
             iniciativa_id: number | null
@@ -24,7 +26,7 @@ export class MetasCronogramaService {
             im.id as cronograma_id
         from meta m
         join cronograma im on im.meta_id = m.id and im.removido_em is null
-        where m.id = ${metaId}
+        where m.id = ${meta_id}
         and m.ativo = TRUE
         and m.removido_em is null
             UNION ALL
@@ -36,7 +38,7 @@ export class MetasCronogramaService {
         from meta m
         join iniciativa i on i.meta_id = m.id and i.removido_em is null
         join cronograma ii on ii.iniciativa_id = i.id and ii.removido_em is null
-        where m.id = ${metaId}
+        where m.id = ${meta_id}
         and m.ativo = TRUE
         and m.removido_em is null
             UNION ALL
@@ -49,25 +51,32 @@ export class MetasCronogramaService {
         join iniciativa i on i.meta_id = m.id and i.removido_em is null
         join atividade a on a.iniciativa_id = i.id and a.removido_em is null
         join cronograma ia on ia.atividade_id = a.id and ia.removido_em is null
-        where m.id = ${metaId}
+        where m.id = ${meta_id}
         and m.ativo = TRUE
         and m.removido_em is null
         `;
 
-        const metas = await this.prisma.meta.findMany({
+        const cronoMeta = await this.prisma.meta.findFirstOrThrow({
             where: {
                 id: { in: dadosMetas.map(r => r.meta_id) },
             },
             select: {
-                id: true,
-                titulo: true,
                 codigo: true,
-                meta_orgao: {
-                    where: {
-                        responsavel: true,
-                    },
+                titulo: true,
+                id: true,
+                meta_responsavel: {
                     select: {
-                        orgao: { select: { sigla: true, descricao: true } }
+                        pessoa: {
+                            select: {
+                                nome_exibicao: true
+                            },
+                        },
+                        orgao: {
+                            select: {
+                                sigla: true
+                            }
+                        },
+                        coordenador_responsavel_cp: true
                     }
                 }
             },
@@ -107,7 +116,7 @@ export class MetasCronogramaService {
             }
         });
 
-        const atividades = await this.prisma.iniciativa.findMany({
+        const atividades = await this.prisma.atividade.findMany({
             where: {
                 id: { in: dadosMetas.filter(n => n.atividade_id !== null).map(n => n.atividade_id!) },
                 removido_em: null,
@@ -118,9 +127,12 @@ export class MetasCronogramaService {
                     where: {
                         removido_em: null,
                     },
-                    select: { id: true, titulo: true, codigo: true }
+                    select: {
+                        id: true, titulo: true, codigo: true,
+                    },
                 },
-                iniciativa_responsavel: {
+                iniciativa_id: true,
+                atividade_responsavel: {
                     select: {
                         pessoa: {
                             select: {
@@ -137,20 +149,46 @@ export class MetasCronogramaService {
                 }
             }
         });
-        console.log(dadosMetas, metas, atividades);
 
-        return {
+
+        const retorno: RetornoMetaCronogramaDto = {
             meta: {
-                codigo: 'x',
-                cronogramas: [],
-                id: 1,
                 iniciativas: [],
-                titulo: '',
-                orgaos_participantes: [],
-                orgaos_responsaveis: [],
-                responsaveis_na_cp: []
+                ...this.mfService.extraiResponsaveis(cronoMeta.meta_responsavel),
+                id: meta_id,
+                titulo: cronoMeta.titulo,
+                codigo: cronoMeta.codigo,
+                cronogramas: dadosMetas.filter(n => n.atividade_id == null && n.iniciativa_id == null).map(n => n.cronograma_id),
+            },
+        };
+
+        for (const iniciativa of iniciativas) {
+            const retornoIniciativa: IniciativasCronoRetorno = {
+                atividades: [],
+                iniciativa: {
+                    id: iniciativa.id, codigo: iniciativa.codigo, titulo: iniciativa.titulo,
+                    ...this.mfService.extraiResponsaveis(iniciativa.iniciativa_responsavel)
+                },
+                cronogramas: dadosMetas.filter(n => n.atividade_id == null && n.iniciativa_id == iniciativa.id).map(n => n.cronograma_id),
+            };
+
+            for (const atividade of atividades) {
+                if (+atividade.iniciativa_id != +iniciativa.id) continue;
+
+                retornoIniciativa.atividades.push({
+                    atividade: {
+                        id: atividade.id, codigo: atividade.codigo, titulo: atividade.titulo,
+                        ...this.mfService.extraiResponsaveis(atividade.atividade_responsavel)
+                    },
+                    cronogramas: dadosMetas.filter(n => n.atividade_id == atividade.id).map(n => n.cronograma_id)
+                });
             }
+
+            retorno.meta.iniciativas.push(retornoIniciativa)
         }
+
+        return retorno;
+
     }
 
 
