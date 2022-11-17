42f7a34194b052a9f68c2addaec570558d06376c
Renato CRON <renato.cron@gmail.com>
2022-11-17 09:04:45 -0300

    colocando num migration

--- backend/prisma/migrations/20221115202813_status_meta_ciclo_fisico/migration.sql
+++ backend/prisma/migrations/20221115202813_status_meta_ciclo_fisico/migration.sql
@@ -122,3 +122,33 @@ END
 $$
 LANGUAGE plpgsql;
 
+CREATE OR REPLACE FUNCTION busca_periodos_variavel (pVariavelId int)
+    RETURNS TABLE (
+        periodicidade interval,
+        min date,
+        max date
+    )
+    AS $$
+BEGIN
+    RETURN QUERY
+    SELECT
+        periodicidade_intervalo (v.periodicidade),
+        coalesce(v.inicio_medicao, i.inicio_medicao),
+        coalesce(v.fim_medicao, i.fim_medicao)
+    FROM
+        variavel v
+        JOIN indicador_variavel iv ON IV.variavel_id = v.id and iv.desativado_em is null and iv.indicador_origem_id is null
+        JOIN indicador i ON Iv.indicador_id = i.id
+    WHERE
+        v.id = pVariavelId;
+END;
+$$
+LANGUAGE plpgsql STABLE;
+
+
+
+CREATE TRIGGER trg_indicador_recalc_pessoa AFTER INSERT OR DELETE OR UPDATE ON indicador
+    FOR EACH STATEMENT
+    EXECUTE FUNCTION f_recalc_acesso_pessoas();
+
+
