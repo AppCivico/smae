8db4ced1a44591403155e49962ceccf022d15cbf
Erico <erico@tapronto.com.br>
2022-11-18 13:12:47 -0300

    Evolucao

--- frontend/src/views/monitoramento/index.js
+++ frontend/src/views/monitoramento/index.js
@@ -1,5 +1,6 @@
 export { default as ListCiclos } from './ListCiclos.vue';
-export { default as ListMonitoramentoMetas } from './ListMonitoramentoMetas.vue';
-export { default as ListMonitoramentoMetasStatus } from './ListMonitoramentoMetasStatus.vue';
-export { default as ListMonitoramentoMetasCronograma } from './ListMonitoramentoMetasCronograma.vue';
-export { default as MonitoramentoMetas } from './MonitoramentoMetas.vue';
+export { default as ListMonitoramentoMetas } from './ListStatus.vue';
+export { default as ListMonitoramentoMetasEvolucao } from './ListEvolucao.vue';
+export { default as ListMonitoramentoMetasCronograma } from './ListCronograma.vue';
+export { default as MonitoramentoMetas } from './MetasEvolucao.vue';
+export { default as MonitoramentoMetasCronograma } from './MetasCronograma.vue';
