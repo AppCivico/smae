6825e72f9845a474453f5167a48d1d04be8723e8
Erico <erico@tapronto.com.br>
2022-11-18 12:04:20 -0300

    Perms sem PDM

--- frontend/src/components/monitoramento/sidebarRealizado.vue
+++ frontend/src/components/monitoramento/sidebarRealizado.vue
@@ -143,10 +143,10 @@
 		
 		
 		<div class="flex center">
-			<a class="btn mr1" @click="conferir" v-if="!perm.PDM.tecnico_cp">Conferir</a>
+			<a class="btn mr1" @click="conferir" v-if="!perm.PDM?.tecnico_cp">Conferir</a>
 			<a class="btn mr1 outline bgnone tcprimary" 
 				@click="solicitarComplementacao"
-				v-if="perm.PDM.admin_cp||perm.PDM.tecnico_cp"
+				v-if="perm.PDM?.admin_cp||perm.PDM?.tecnico_cp"
 			>Solicitar{{SingleAnalise.ultimoPedidoComplementacao?' nova':''}} complementação</a>
 			<a class="btn " @click="complementar" v-if="SingleAnalise.ultimoPedidoComplementacao&&!SingleAnalise.ultimoPedidoComplementacao.atendido">Complementar</a>
 		</div>
