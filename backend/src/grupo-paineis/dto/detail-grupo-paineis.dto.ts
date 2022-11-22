57cfafe706baccdf61488c9d4b26971422922bd7
lucasansei <lucas.ansei@eokoe.com>
2022-11-22 16:54:26 -0300

    Add detail for Grupo de Paineis

---
+++ backend/src/grupo-paineis/dto/detail-grupo-paineis.dto.ts
@@ -0,0 +1,20 @@
+export class DetailGrupoPaineisDto {
+    id: number
+    nome: string
+    ativo: boolean
+    painel_count: number | 0
+    pessoa_count: number | 0
+    paineis: Paineis[] | null
+    pessoas: Pessoas[] | null
+}
+
+export class Paineis {
+    id: number
+    nome: string
+    ativo: boolean
+}
+
+export class Pessoas {
+    id: number
+    nome_exibicao: string
+}
