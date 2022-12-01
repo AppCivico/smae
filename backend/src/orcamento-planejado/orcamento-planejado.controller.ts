a6e125bd4298907bc05b335cb4aa7a48a40fd252
Renato Cron <renato.cron@gmail.com>
2022-12-01 10:11:26 -0300

    tidy

--- backend/src/orcamento-planejado/orcamento-planejado.controller.ts
+++ backend/src/orcamento-planejado/orcamento-planejado.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Query } from '@nestjs/common';
-import { ApiBearerAuth, ApiNoContentResponse, ApiUnauthorizedResponse } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiNoContentResponse, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
 import { CurrentUser } from '../auth/decorators/current-user.decorator';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { PessoaFromJwt } from '../auth/models/PessoaFromJwt';
@@ -8,35 +8,36 @@ import { RecordWithId } from '../common/dto/record-with-id.dto';
 import { CreateOrcamentoPlanejadoDto, FilterOrcamentoPlanejadoDto, ListOrcamentoPlanejadoDto } from './dto/orcamento-planejado.dto';
 import { OrcamentoPlanejadoService } from './orcamento-planejado.service';
 
+@ApiTags('Orçamento')
 @Controller('orcamento-planejado')
 export class OrcamentoPlanejadoController {
-  constructor(private readonly orcamentoPlanejadoService: OrcamentoPlanejadoService) {}
+    constructor(private readonly orcamentoPlanejadoService: OrcamentoPlanejadoService) { }
 
-  @Patch()
-  @ApiBearerAuth('access-token')
-  @ApiUnauthorizedResponse()
-  @Roles('CadastroMeta.orcamento')
-  async upsert(@Body() createMetaDto: CreateOrcamentoPlanejadoDto, @CurrentUser() user: PessoaFromJwt): Promise<RecordWithId> {
-      return await this.orcamentoPlanejadoService.create(createMetaDto, user);
-  }
+    @Patch()
+    @ApiBearerAuth('access-token')
+    @ApiUnauthorizedResponse()
+    @Roles('CadastroMeta.orcamento')
+    async upsert(@Body() createMetaDto: CreateOrcamentoPlanejadoDto, @CurrentUser() user: PessoaFromJwt): Promise<RecordWithId> {
+        return await this.orcamentoPlanejadoService.create(createMetaDto, user);
+    }
 
-  @ApiBearerAuth('access-token')
-  @Get()
-  @Roles('CadastroMeta.orcamento')
-  async findAll(@Query() filters: FilterOrcamentoPlanejadoDto): Promise<ListOrcamentoPlanejadoDto> {
+    @ApiBearerAuth('access-token')
+    @Get()
+    @Roles('CadastroMeta.orcamento')
+    async findAll(@Query() filters: FilterOrcamentoPlanejadoDto): Promise<ListOrcamentoPlanejadoDto> {
 
-      return { 'linhas': await this.orcamentoPlanejadoService.findAll(filters) };
-  }
+        return { 'linhas': await this.orcamentoPlanejadoService.findAll(filters) };
+    }
 
-  @Delete(':id')
-  @ApiBearerAuth('access-token')
-  @ApiUnauthorizedResponse()
-  @Roles('CadastroMeta.orcamento')
-  @ApiNoContentResponse()
-  @HttpCode(HttpStatus.ACCEPTED)
-  async remove(@Param() params: FindOneParams, @CurrentUser() user: PessoaFromJwt) {
-      await this.orcamentoPlanejadoService.remove(+params.id, user);
-      return '';
-  }
+    @Delete(':id')
+    @ApiBearerAuth('access-token')
+    @ApiUnauthorizedResponse()
+    @Roles('CadastroMeta.orcamento')
+    @ApiNoContentResponse()
+    @HttpCode(HttpStatus.ACCEPTED)
+    async remove(@Param() params: FindOneParams, @CurrentUser() user: PessoaFromJwt) {
+        await this.orcamentoPlanejadoService.remove(+params.id, user);
+        return '';
+    }
 
 }
