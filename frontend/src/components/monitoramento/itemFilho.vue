8934ea52c6282e75b80a5c8aeccc2a8ddd6c5859
Erico <erico@tapronto.com.br>
2022-12-02 13:46:44 -0300

    Cronos

---
+++ frontend/src/components/monitoramento/itemFilho.vue
@@ -0,0 +1,42 @@
+<script setup>
+    import { ref } from 'vue';
+    import { storeToRefs } from 'pinia';
+    import { usePdMStore } from '@/stores';
+    const props = defineProps(['group','chave','link']);
+    const PdMStore = usePdMStore();
+    const { activePdm } = storeToRefs(PdMStore);
+</script>
+<template>
+    <div class="board_variavel mb2" v-for="v in group" :key="v[chave]?.id">
+        <router-link :to="`${link}${v[chave]?.id}`" class="block p1">
+            <div class="flex center g2 mb1">
+                <div class="f0" style="flex-basis: 2rem;">
+                    <svg width="28" height="33" viewBox="0 0 32 38" color="#8EC122" xmlns="http://www.w3.org/2000/svg"><use :xlink:href="`#i_${chave}`"></use></svg>
+                </div>
+                <div class="f1 mt1">
+                    <h2 class="mb1">{{v[chave]?.codigo}} - {{v[chave]?.titulo}}</h2>
+                </div>
+            </div>
+            <div class="f1 ml2">
+                <div class="flex g2 ml2">
+                    <div class="mr1 f0">
+                        <div class="t12 uc w700 mb05 tc300">ID</div>
+                        <div class="t13">{{v[chave]?.codigo}}</div>
+                    </div>
+                    <div class="mr1 f1">
+                        <div class="t12 uc w700 mb05 tc300">Órgão(s) responsável(eis)</div>
+                        <div class="t13">{{v[chave]?.orgaos_responsaveis?.join(', ')}}</div>
+                    </div>
+                    <div class="mr1 f1">
+                        <div class="t12 uc w700 mb05 tc300">Órgão(s) participante(s)</div>
+                        <div class="t13">{{v[chave]?.orgaos_participantes?.join(', ')}}</div>
+                    </div>
+                    <div class="f1">
+                        <div class="t12 uc w700 mb05 tc300">Responsável(eis) na Coordenadoria</div>
+                        <div class="t13">{{v[chave]?.responsaveis_na_cp?.join(', ')}}</div>
+                    </div>
+                </div>
+            </div>
+        </router-link>
+    </div>
+</template>
--- frontend/src/views/monitoramento/MetasCronograma.vue
+++ frontend/src/views/monitoramento/MetasCronograma.vue
@@ -1,33 +1,52 @@
 <script setup>
-    import { ref, reactive, onMounted, onUpdated } from 'vue';
+    import { ref, watch, onMounted, onUpdated } from 'vue';
     import { storeToRefs } from 'pinia';
     import { Dashboard} from '@/components';
     import { useEditModalStore, useAlertStore, useAuthStore, usePdMStore, useCiclosStore } from '@/stores';
     import { useRoute } from 'vue-router';
     import { router } from '@/router';
     import { default as AddEditEtapa } from '@/views/monitoramento/AddEditEtapa.vue';
+    import { default as itemFilho } from '@/components/monitoramento/itemFilho.vue';
     
     const baseUrl = `${import.meta.env.VITE_API_URL}`;
     
-    const route = useRoute();
-    const meta_id = route.params.meta_id;
-    const cron_id = route.params.cron_id;
-    const etapa_id = route.params.etapa_id;
-
     const editModalStore = useEditModalStore();
     const alertStore = useAlertStore();
-
     const authStore = useAuthStore();
     const { permissions } = storeToRefs(authStore);
     const perm = permissions.value;
+
+    const route = useRoute();
+    const meta_id = route.params.meta_id;
+    const iniciativa_id = route.params.iniciativa_id;
+    const atividade_id = route.params.atividade_id;
+    const cron_id = route.params.cron_id;
+    const etapa_id = route.params.etapa_id;
 
     const PdMStore = usePdMStore();
     const { activePdm } = storeToRefs(PdMStore);
 
     const CiclosStore = useCiclosStore();
     const { SingleMeta, SingleCronograma, SingleCronogramaEtapas } = storeToRefs(CiclosStore);
-    CiclosStore.getMetaById(meta_id);
-    CiclosStore.getCronogramasActiveByParent(meta_id,'meta_id');
+    
+    let parentlink = `${meta_id?meta_id:''}${iniciativa_id?'/'+iniciativa_id:''}${atividade_id?'/'+atividade_id:''}`;
+    let parentVar = atividade_id??iniciativa_id??meta_id??false;
+    let parentField = atividade_id?'atividade':iniciativa_id?'iniciativa':meta_id?'meta':false;
+    let parentFieldId = atividade_id?'atividade_id':iniciativa_id?'iniciativa_id':meta_id?'meta_id':false;
+    let parentLabel = ref(atividade_id?activePdm.value?.rotulo_atividade:iniciativa_id?activePdm.value?.rotulo_iniciativa:meta_id?'Meta':false);
+    watch(activePdm, async(v, vold) => {
+        if(atividade_id) parentLabel.value = v.rotulo_atividade;
+        else if(iniciativa_id) parentLabel.value = v.rotulo_iniciativa;
+    })
+
+    let currentParent = ref({});
+    CiclosStore.getCronogramasActiveByParent(parentVar,parentFieldId);
+    (async ()=>{
+        await CiclosStore.getMetaById(meta_id);
+        currentParent.value = SingleMeta.value;
+        if(iniciativa_id) currentParent.value = currentParent.value?.meta?.iniciativas.find(x=>x.iniciativa.id==iniciativa_id);
+        if(atividade_id) currentParent.value = currentParent.value?.atividades.find(x=>x.atividade.id==atividade_id);
+    })();
     
     function start(){
         if(cron_id&&etapa_id) editModalStore.modal(AddEditEtapa);
@@ -45,10 +64,10 @@
 </script>
 <template>
     <Dashboard>
-        <div class="label tamarelo">Cronograma da Meta</div>
+        <div class="label tamarelo">Cronograma de {{parentLabel}}</div>
         <div class="mb2">
             <div class="flex spacebetween center">
-                <h1>Meta {{SingleMeta.codigo}} - {{SingleMeta.titulo}}</h1>
+                <h1>{{parentLabel}} {{currentParent[parentField]?.codigo}} - {{currentParent[parentField]?.titulo}}</h1>
                 <hr class="ml2 f1" />
             </div>
         </div>
@@ -79,19 +98,23 @@
                         <div class="ml1 f1">{{r.etapa.atraso??'-'}}</div>
                         <div class="ml1 f0 flex center" style="flex-basis:20px; height: calc(20px + 1rem);">
                             <router-link 
-                                :to="`/monitoramento/cronograma/${meta_id}/${r.cronograma_id}/${r.etapa.id}`"><svg width="20" height="20"><use xlink:href="#i_edit"></use></svg></router-link>
+                                :to="`/monitoramento/cronograma/${parentlink}/editar/${r.cronograma_id}/${r.etapa.id}`"><svg width="20" height="20"><use xlink:href="#i_edit"></use></svg></router-link>
                         </div>
                     </div>
                     <hr class="mb05" />
                     <div class="pl3 flex center t11 w700 tc600" v-if="r.cronograma_origem_etapa&&r.cronograma_origem_etapa?.id!=SingleCronograma?.id">
-                        <template v-if="r.cronograma_origem_etapa.atividade" >
+                        <router-link
+                            :to="`/monitoramento/cronograma/${meta_id}/${r.cronograma_origem_etapa.atividade.iniciativa.id}/${r.cronograma_origem_etapa.atividade.id}`"
+                            v-if="r.cronograma_origem_etapa.atividade" >
                             <svg class="mr1" width="12" height="14" viewBox="0 0 12 14" xmlns="http://www.w3.org/2000/svg"><use xlink:href="#i_atividade"></use></svg>
-                            <span>Etapa via atividade {{r.cronograma_origem_etapa.atividade.codigo}} {{r.cronograma_origem_etapa.atividade.titulo}}</span>
-                        </template>
-                        <template v-else-if="r.cronograma_origem_etapa.iniciativa" >
+                            <span>Etapa via {{activePdm.rotulo_atividade}} {{r.cronograma_origem_etapa.atividade.codigo}} {{r.cronograma_origem_etapa.atividade.titulo}}</span>
+                        </router-link>
+                        <router-link
+                            :to="`/monitoramento/cronograma/${meta_id}/${r.cronograma_origem_etapa.iniciativa.id}`"
+                            v-else-if="r.cronograma_origem_etapa.iniciativa" >
                             <svg class="mr1" width="12" height="14" viewBox="0 0 12 14" xmlns="http://www.w3.org/2000/svg"><use xlink:href="#i_iniciativa"></use></svg>
-                            <span>Etapa via iniciativa {{r.cronograma_origem_etapa.iniciativa.codigo}} {{r.cronograma_origem_etapa.iniciativa.titulo}}</span>
-                        </template>
+                            <span>Etapa via {{activePdm.rotulo_iniciativa}} {{r.cronograma_origem_etapa.iniciativa.codigo}} {{r.cronograma_origem_etapa.iniciativa.titulo}}</span>
+                        </router-link>
                     </div>
 
                     <div class="etapa sub" v-for="(rr, rrindex) in r.etapa.etapa_filha" :key="rr.id">
@@ -116,7 +139,7 @@
                             <div class="ml1 f1">{{rr.atraso??'-'}}</div>
                             <div class="ml1 f0 flex center" style="flex-basis:20px; height: calc(20px + 1rem);">
                                 <router-link 
-                                    :to="`/monitoramento/cronograma/${meta_id}/${r.cronograma_id}/${rr.id}`"><svg width="20" height="20"><use xlink:href="#i_edit"></use></svg></router-link>
+                                    :to="`/monitoramento/cronograma/${parentlink}/editar/${rr.CronogramaEtapa[0].cronograma_id}/${rr.id}`"><svg width="20" height="20"><use xlink:href="#i_edit"></use></svg></router-link>
                             </div>
                         </div>
                         <hr class="mb3" />
@@ -143,9 +166,9 @@
                                     <div class="ml1 f1">{{rrr.inicio_real}}</div>
                                     <div class="ml1 f1">{{rrr.termino_real}}</div>
                                     <div class="ml1 f1">{{rrr.atraso??'-'}}</div>
-                                    <div class="ml1 f0 flex center mr05" style="flex-basis:20px; height: calc(20px + 1rem);">
+                                    <div class="ml1 f0 flex center" style="flex-basis:20px; height: calc(20px + 1rem);">
                                         <router-link 
-                                            :to="`/monitoramento/cronograma/${meta_id}/${r.cronograma_id}/${rrr.id}`"><svg width="20" height="20"><use xlink:href="#i_edit"></use></svg></router-link>
+                                            :to="`/monitoramento/cronograma/${parentlink}/editar/${rrr.CronogramaEtapa[0].cronograma_id}/${rrr.id}`"><svg width="20" height="20"><use xlink:href="#i_edit"></use></svg></router-link>
                                     </div>
                                 </div>
                                 <hr/>
@@ -166,5 +189,19 @@
             <div class="p1"><span>Carregando</span> <svg class="ml1 ib" width="20" height="20"><use xlink:href="#i_spin"></use></svg></div>
         </template>
 
+        <template v-if="currentParent.meta?.iniciativas?.length">
+            <div class="flex spacebetween center mt4 mb2">
+                <h2>{{activePdm.rotulo_iniciativa}}(s) com cronograma</h2>
+                <hr class="ml2 f1" />
+            </div>
+            <itemFilho :group="currentParent.meta.iniciativas" chave="iniciativa" :link="`/monitoramento/cronograma/${meta_id}/`"/>
+        </template>
+        <template v-if="currentParent.atividades?.length">
+            <div class="flex spacebetween center mt4 mb2">
+                <h2>{{activePdm.rotulo_atividade}}(s) com cronograma</h2>
+                <hr class="ml2 f1" />
+            </div>
+            <itemFilho :group="currentParent.atividades" chave="atividade" :link="`/monitoramento/cronograma/${meta_id}/${iniciativa_id}/`"/>
+        </template>
     </Dashboard>
 </template>
