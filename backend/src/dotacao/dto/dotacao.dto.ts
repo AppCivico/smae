395cda2a929a7c136238c828ca2a99a31a941987
Renato Cron <renato.cron@gmail.com>
2022-12-03 01:06:26 -0300

    Tratando erro 404 na busca da dotacao

--- backend/src/dotacao/dto/dotacao.dto.ts
+++ backend/src/dotacao/dto/dotacao.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, Matches, MaxLength } from "class-validator";
+import { IsBoolean, IsPositive, IsString, Matches, MaxLength } from "class-validator";
 
 export class ValidateDotacaoDto {
     /**
@@ -9,4 +9,18 @@ export class ValidateDotacaoDto {
     @MaxLength(40)
     @Matches(/^\d{2}\.\d{2}\.\d{2}\.\d{3}\.\d{4}\.\d\.\d{3}\.\d{8}\.\d{2}$/, { message: 'Dotação não está no formato esperado: 00.00.00.000.0000.0.000.00000000.00' })
     dotacao: string;
+
+    /**
+    * planejado: false
+    * @example "true"
+    */
+    @IsBoolean()
+    planejado: boolean
+
+    /**
+    * ano: ano para pesquisa
+    * @example "2022"
+    */
+    @IsPositive()
+    ano: number
 }
