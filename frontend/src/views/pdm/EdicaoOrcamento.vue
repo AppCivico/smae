3d136edf52ba5b15436c959993b9ed45ed78cff5
Erico <erico@tapronto.com.br>
2022-12-05 14:29:52 -0300

    Pdm edit e ciclos passados

--- frontend/src/views/pdm/EdicaoOrcamento.vue
+++ frontend/src/views/pdm/EdicaoOrcamento.vue
@@ -46,18 +46,28 @@
 	    <button @click="props.checkClose" class="btn round ml2"><svg width="12" height="12"><use xlink:href="#i_x"></use></svg></button>
 	</div>
 	<hr class="mt2 mb2">
-	<Form @submit="onSubmit" v-slot="{ errors, isSubmitting }">
-		<div v-for="y in anosOrcamento" :key="y.id" class="mb2">
-			<h4>{{y.ano_referencia}}</h4>
-			<label class="block mb05"><input v-model="y.previsao_custo_disponivel" class="inputcheckbox" type="checkbox" value=true /><span>Previsão de custo</span></label>
-			<label class="block mb05"><input v-model="y.planejado_disponivel" class="inputcheckbox" type="checkbox" value=true /><span>Orçamento planejado</span></label>
-			<label class="block mb05"><input v-model="y.execucao_disponivel" class="inputcheckbox" type="checkbox" value=true /><span>Execução orçamentária</span></label>
-		</div>
-		<div class="flex spacebetween center mb2">
-		    <hr class="mr2 f1"/>
-		    <button class="btn outline tcprimary bgnone mr1" type="button" @click="props.checkClose">Cancelar</button>
-		    <button class="btn" :disabled="isSubmitting">Salvar</button>
-		    <hr class="ml2 f1"/>
-		</div>
-	</Form>
+	<template v-if="!(singlePdm?.loading || singlePdm?.error)">
+		<Form @submit="onSubmit" v-slot="{ errors, isSubmitting }">
+			<div v-for="y in anosOrcamento" :key="y.id" class="mb2">
+				<h4>{{y.ano_referencia}}</h4>
+				<label class="block mb05"><input v-model="y.previsao_custo_disponivel" class="inputcheckbox" type="checkbox" value=true /><span>Previsão de custo</span></label>
+				<label class="block mb05"><input v-model="y.planejado_disponivel" class="inputcheckbox" type="checkbox" value=true /><span>Orçamento planejado</span></label>
+				<label class="block mb05"><input v-model="y.execucao_disponivel" class="inputcheckbox" type="checkbox" value=true /><span>Execução orçamentária</span></label>
+			</div>
+			<div class="flex spacebetween center mb2">
+			    <hr class="mr2 f1"/>
+			    <button class="btn outline tcprimary bgnone mr1" type="button" @click="props.checkClose">Cancelar</button>
+			    <button class="btn" :disabled="isSubmitting">Salvar</button>
+			    <hr class="ml2 f1"/>
+			</div>
+		</Form>
+	</template>
+    <template v-if="singlePdm?.loading">
+        <span class="spinner">Carregando</span>
+    </template>
+    <template v-if="singlePdm?.error||error">
+        <div class="error p1">
+            <div class="error-msg">{{singlePdm.error??error}}</div>
+        </div>
+    </template>
 </template>
