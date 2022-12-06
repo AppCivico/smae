783737ad7871a9f475558cc9502ec8fc396af00e
Renato Cron <renato.cron@gmail.com>
2022-12-06 15:15:19 -0300

    Sincronizando backend-orcamento/

--- backend-orcamento/core/dao/api_client/sof_client.py
+++ backend-orcamento/core/dao/api_client/sof_client.py
@@ -1,6 +1,5 @@
 from functools import partial
 from .abstract_client import Client
-from core.utils.utils_dotacao import ParserDotacao
 
 
 class SofClient:
@@ -15,8 +14,6 @@ class SofClient:
         self.auth_token = auth_token
 
         self.get = self.__authorize_get()
-
-        self.parse_dotacao = ParserDotacao()
 
     def __build_base_url(self):
 
@@ -34,68 +31,6 @@ class SofClient:
         get = partial(client.get, headers=headers)
 
         return get
-
-    def empenhos_processo(self, ano:int, mes:int, proc:int,
-                    num_pag:int=1)->dict:
-
-        endpoint = 'empenhos'
-        params = {
-            'anoEmpenho' : ano,
-            'mesEmpenho' : mes,
-            'numProcesso' : proc,
-            'numPagina' : num_pag}
-
-        return self.get(endpoint, **params)
-
-
-    def empenhos_nota_empenho(self, ano:int, mes:int, cod_nota:int,
-                    num_pag:int=1)->dict:
-
-        endpoint = 'empenhos'
-        params = {
-            'anoEmpenho' : ano,
-            'mesEmpenho' : mes,
-            'codEmpenho' : cod_nota,
-            'numPagina' : num_pag}
-
-        return self.get(endpoint, **params)
-
-
-    def empenhos_dotacao(self, ano:int, mes:int, dotacao:str,
-                    num_pag:int=1)->dict:
-
-        endpoint = 'empenhos'
-
-        params = self.parse_dotacao(dotacao)
-        params['anoEmpenho'] = ano
-        params['mesEmpenho'] = mes
-        params['numPagina'] = num_pag
-
-
-        return self.get(endpoint, **params)
-
-    def __check_params(self, dotacao:str=None, processo:str=None,
-                nota_empenho:str=None)->None:
-
-        params = (dotacao, processo, nota_empenho)
-        checksum= sum(1 for p in params if not p is None)
-
-        if checksum==0:
-            raise ValueError('Either dotacao, processo or nota_empenho must be defined.')
-        if checksum>1:
-            raise ValueError('Only one of dotacao, processo or nota_empenho must be defined.')
-
-    def __call__(self, ano:int, mes:int, *_, dotacao:str=None,
-        processo:int=None, nota_empenho: int=None, num_pag:int=1)->dict:
-
-        self.__check_params(dotacao, processo, nota_empenho)
-
-        if dotacao:
-            return self.empenhos_dotacao(ano, mes, dotacao, num_pag)
-        if processo:
-            return self.empenhos_processo(ano, mes, processo, num_pag)
-        if nota_empenho:
-            return self.empenhos_nota_empenho(ano, mes, nota_empenho, num_pag)
 
 
 
--- backend-orcamento/v1/__init__.py
+++ backend-orcamento/v1/__init__.py
@@ -1,1 +1,2 @@
 from .endpoints.empenhos import app as empenhos_routes
+from .endpoints.itens_dotacao import app as itens_dotacao_routes
