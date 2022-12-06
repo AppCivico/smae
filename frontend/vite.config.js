64174c5f93f4d8e40cc645a0ba52eef4a2daaea3
Renato Cron <renato.cron@gmail.com>
2022-12-06 14:48:41 -0300

    Adicionar __FORMULA_JS_SCRIPT__

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,13 +2,24 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+const htmlPlugin = () => {
+    return {
+        name: 'html-transform',
+        transformIndexHtml(html) {
+            return html.replace(
+                /__FORMULA_JS_SCRIPT__/,
+                `<script type="text/javascript" src="${process.env.FORMULA_JS_URL || '/api/public/js/formula_parser.js'}"></script>`
+            )
+        }
+    }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [vue(), htmlPlugin()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
     }
-  }
 })
