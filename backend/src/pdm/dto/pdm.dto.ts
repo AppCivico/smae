1489472ed5d70d95aa30e5eefce05f36178e5b9e
Renato Cron <renato.cron@gmail.com>
2022-12-06 21:38:50 -0300

    Formatando data do PDM

--- backend/src/pdm/dto/pdm.dto.ts
+++ backend/src/pdm/dto/pdm.dto.ts
@@ -4,11 +4,11 @@ export class Pdm {
     descricao: string | null
     prefeito: string
     equipe_tecnica: string | null
-    data_inicio: Date | null
-    data_fim: Date | null
-    data_publicacao: Date | null
-    periodo_do_ciclo_participativo_inicio: Date | null
-    periodo_do_ciclo_participativo_fim: Date | null
+    data_inicio: string | null
+    data_fim: string | null
+    data_publicacao: string | null
+    periodo_do_ciclo_participativo_inicio: string | null
+    periodo_do_ciclo_participativo_fim: string | null
     rotulo_macro_tema: string | undefined
     rotulo_tema: string | undefined
     rotulo_sub_tema: string | undefined
@@ -20,4 +20,10 @@ export class Pdm {
     possui_contexto_meta: boolean
     possui_complementacao_meta: boolean
     logo: string | null
+    ativo: boolean
+    rotulo_iniciativa: string
+    rotulo_atividade: string
+    possui_iniciativa: boolean
+    possui_atividade: boolean
+    id: number
 }
