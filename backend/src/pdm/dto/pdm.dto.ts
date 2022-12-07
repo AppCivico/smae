e29ecb510876315cb9588e1cc769a6a8475dcc78
Renato Cron <renato.cron@gmail.com>
2022-12-06 21:41:46 -0300

    fix types

--- backend/src/pdm/dto/pdm.dto.ts
+++ backend/src/pdm/dto/pdm.dto.ts
@@ -9,11 +9,11 @@ export class Pdm {
     data_publicacao: string | null
     periodo_do_ciclo_participativo_inicio: string | null
     periodo_do_ciclo_participativo_fim: string | null
-    rotulo_macro_tema: string | undefined
-    rotulo_tema: string | undefined
-    rotulo_sub_tema: string | undefined
-    rotulo_contexto_meta: string | undefined
-    rotulo_complementacao_meta: string | undefined
+    rotulo_macro_tema: string
+    rotulo_tema: string
+    rotulo_sub_tema: string
+    rotulo_contexto_meta: string
+    rotulo_complementacao_meta: string
     possui_macro_tema: boolean
     possui_tema: boolean
     possui_sub_tema: boolean
