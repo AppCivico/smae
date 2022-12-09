78016f841ae262a7e297e413ba0ec4ffa18ec349
Renato Cron <renato.cron@gmail.com>
2022-12-09 16:04:12 -0300

    Garantir que minha-conta => escrever senha e o reset da senha sempre tenham o mesmo validador

--- backend/src/minha-conta/models/nova-senha.dto.ts
+++ backend/src/minha-conta/models/nova-senha.dto.ts
@@ -1,5 +1,6 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { IsString, Matches, MaxLength, MinLength } from "class-validator";
+import { MESSAGE, REGEXP } from "../../auth/models/EscreverNovaSenhaRequestBody.dto";
 
 export class NovaSenhaDto {
     @ApiProperty({ description: 'Senha atual', example: 'Foo*bar1' })
@@ -10,8 +11,8 @@ export class NovaSenhaDto {
     @IsString({ message: '$property| Senha: Precisa ser alfanumérico' })
     @MinLength(8, { message: '$property| Senha: Mínimo de 8 caracteres' })
     @MaxLength(1000, { message: '$property| Senha: Máximo de 1000 caracteres' })
-    @Matches(/((?=.*\d)|(?=.*\W+)|(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]))(?![.\n])(?=.*[A-Z])(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?])(?=.*[a-z]).*$/, {
-        message: '$property| Senha: Precisa ter pelo menos 1 número e um caractere em caixa alta e um dos seguintes caracteres especiais: !@#$%^&*()_+-=\\[]{};\':"\\|,.<>/?',
+    @Matches(REGEXP, {
+        message: MESSAGE,
     })
     senha_nova: string
 }
