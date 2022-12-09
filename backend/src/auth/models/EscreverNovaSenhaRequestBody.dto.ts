b611d07acc60266f15b91601d883ac2fc8634552
Renato Cron <renato.cron@gmail.com>
2022-12-09 16:04:12 -0300

    Garantir que minha-conta => escrever senha e o reset da senha sempre tenham o mesmo validador

--- backend/src/auth/models/EscreverNovaSenhaRequestBody.dto.ts
+++ backend/src/auth/models/EscreverNovaSenhaRequestBody.dto.ts
@@ -1,5 +1,7 @@
 import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
 
+export const MESSAGE = '$property| Senha: Precisa ter pelo menos 6 caracteres, 1 número e um caractere em CAIXA ALTA e um dos seguintes caracteres especiais: !, @, #, $, %, ^, &, *, (, ), _, +, -, =, [, ], {, }, ;, \', :, ", \, |, ,, ., <, >, /, ?';
+export const REGEXP = /^((?=.*\d)|(?=.*\W+)|(?=.*[\!\@\#\$\%\^\&\*\(\)\_\+\-\=\[\]\{\}\;\'\:\"\\\|\,\.\<\>\/\?]))(?![.\n])(?=.*[A-Z])(?=.*[\!\@\#\$\%\^\&\*\(\)\_\+\-\=\[\]\{\}\;\'\:\"\\\|\,\.\<\>\/\?])(?=.*[a-z]).*$/;
 export class EscreverNovaSenhaRequestBody {
     /**
     * Access Token
@@ -15,8 +17,8 @@ export class EscreverNovaSenhaRequestBody {
     @IsString({ message: '$property| Senha: Precisa ser alfanumérico' })
     @MinLength(8, { message: '$property| Senha: Mínimo de 8 caracteres' })
     @MaxLength(1000, { message: '$property| Senha: Máximo de 1000 caracteres' })
-    @Matches(/((?=.*\d)|(?=.*\W+)|(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]))(?![.\n])(?=.*[A-Z])(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?])(?=.*[a-z]).*$/, {
-        message: '$property| Senha: Precisa ter pelo menos 1 número e um caractere em caixa alta e um dos seguintes caracteres especiais: !@#$%^&*()_+-=\\[]{};\':"\\|,.<>/?',
+    @Matches(REGEXP, {
+        message: MESSAGE,
     })
     senha: string;
 }
