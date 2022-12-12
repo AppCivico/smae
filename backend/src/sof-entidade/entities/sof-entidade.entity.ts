889cbec04682f043888f619a2270cffd00daa6da
Renato Cron <renato.cron@gmail.com>
2022-12-12 16:46:49 -0300

    Documentando o /sof-entidade/{ano}

--- backend/src/sof-entidade/entities/sof-entidade.entity.ts
+++ backend/src/sof-entidade/entities/sof-entidade.entity.ts
@@ -1,1 +1,30 @@
-export class SofEntidade {}
+
+class Entidade {
+    codigo: string
+    descricao: string
+}
+
+class EntidadeUnidade {
+    codigo: string
+    descricao: string
+    cod_orgao: string
+}
+
+class SuccessEntidadesResponse {
+    orgaos: Entidade[]
+    unidades: EntidadeUnidade[]
+    funcoes: Entidade[]
+    subfuncoes: Entidade[]
+    programas: Entidade[]
+    projetos_atividades: Entidade[]
+    categorias: Entidade[]
+    grupos: Entidade[]
+    modalidades: Entidade[]
+    elementos: Entidade[]
+    fonte_recursos: Entidade[]
+};
+
+export class SofEntidadeDto {
+    atualizado_em: string
+    dados: SuccessEntidadesResponse
+}
--- backend/src/sof-entidade/sof-entidade.controller.ts
+++ backend/src/sof-entidade/sof-entidade.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Header, Param } from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation, ApiUnauthorizedResponse } from '@nestjs/swagger';
 import { FindAnoParams } from '../common/decorators/find-params';
+import { SofEntidadeDto } from './entities/sof-entidade.entity';
 import { SofEntidadeService } from './sof-entidade.service';
 
 @Controller('sof-entidade')
@@ -12,8 +13,8 @@ export class SofEntidadeController {
     @ApiUnauthorizedResponse()
     @Header('Cache-Control', 'max-age=3600')
     @ApiOperation({ summary: 'Dados do ano corrente são atualizados diariamente, resposta pode ser salva em storage local por ate 24h' })
-    async findByYear(@Param() params: FindAnoParams,) {
-        return await this.sofEntidadeService.findByYear(+params.ano);
+    async findByYear(@Param() params: FindAnoParams): Promise<SofEntidadeDto> {
+        return (await this.sofEntidadeService.findByYear(+params.ano)) as any as SofEntidadeDto;
     }
 
 }
