5ee689879a05afff9da04c95e115060c694d3bd5
Renato Cron <renato.cron@gmail.com>
2022-12-12 18:13:31 -0300

    Adicionando investimento_previsto e custeio_previsto como string

--- backend/src/meta-orcamento/entities/meta-orcamento.entity.ts
+++ backend/src/meta-orcamento/entities/meta-orcamento.entity.ts
@@ -1,13 +1,19 @@
+export class MetaOrcamentoItemOutDto {
+    custeio_previsto: string;
+    investimento_previsto: string;
+    parte_dotacao: string;
+}
+
 export class MetaOrcamento {
     meta_id: number
     ano_referencia: number
     soma_custeio_previsto: number
     soma_investimento_previsto: number
-    //parte_dotacao: string
     ultima_revisao: boolean
     criado_em: Date
     criador: {
         nome_exibicao: string
     }
+    itens: MetaOrcamentoItemOutDto[]
     id: number
 }
