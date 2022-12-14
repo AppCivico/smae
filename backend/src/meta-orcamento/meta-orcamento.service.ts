640175fc39fcf7987e0fdc650b7190709213f4e7
Renato Cron <renato.cron@gmail.com>
2022-12-14 10:21:34 -0300

    Atualizando Meta Orçamento

--- backend/src/meta-orcamento/meta-orcamento.service.ts
+++ backend/src/meta-orcamento/meta-orcamento.service.ts
@@ -2,15 +2,20 @@ import { HttpException, Injectable } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import { PessoaFromJwt } from '../auth/models/PessoaFromJwt';
 import { RecordWithId } from '../common/dto/record-with-id.dto';
+import { OrcamentoPlanejadoService } from '../orcamento-planejado/orcamento-planejado.service';
 import { PrismaService } from '../prisma/prisma.service';
-import { CreateMetaOrcamentoDto, FilterMetaOrcamentoDto } from './dto/meta-orcamento.dto';
+import { CreateMetaOrcamentoDto, FilterMetaOrcamentoDto, UpdateMetaOrcamentoDto } from './dto/meta-orcamento.dto';
 import { MetaOrcamento } from './entities/meta-orcamento.entity';
 
 @Injectable()
 export class MetaOrcamentoService {
-    constructor(private readonly prisma: PrismaService) { }
+    constructor(
+        private readonly prisma: PrismaService,
+        private readonly orcamentoPlanejado: OrcamentoPlanejadoService,
+    ) { }
 
-    async upsert(dto: CreateMetaOrcamentoDto, user: PessoaFromJwt): Promise<RecordWithId> {
+    async create(dto: CreateMetaOrcamentoDto, user: PessoaFromJwt): Promise<RecordWithId> {
+        const { meta_id, iniciativa_id, atividade_id } = await this.orcamentoPlanejado.validaMetaIniAtv(dto);
 
         const meta = await this.prisma.meta.findFirst({
             where: { id: dto.meta_id, removido_em: null },
@@ -27,34 +32,21 @@ export class MetaOrcamentoService {
 
             const now = new Date(Date.now());
 
-            await prisma.metaOrcamento.updateMany({
-                where: { meta_id: meta.id, ano_referencia: dto.ano_referencia, ultima_revisao: true },
-                data: { ultima_revisao: false },
-            });
-
-            const soma_custeio_previsto = dto.itens.reduce((acc, item) => acc + item.custeio_previsto, 0);
-            const soma_investimento_previsto = dto.itens.reduce((acc, item) => acc + item.investimento_previsto, 0);
 
             const metaOrcamento = await prisma.metaOrcamento.create({
                 data: {
                     criado_por: user.id,
                     criado_em: now,
-                    ultima_revisao: true,
-                    meta_id: meta.id,
+
+                    meta_id: meta_id!,
+                    iniciativa_id,
+                    atividade_id,
                     ano_referencia: dto.ano_referencia,
-                    soma_custeio_previsto,
-                    soma_investimento_previsto,
-                    itens: {
-                        createMany: {
-                            data: dto.itens.map((item) => {
-                                return {
-                                    custeio_previsto: item.custeio_previsto,
-                                    investimento_previsto: item.investimento_previsto,
-                                    parte_dotacao: item.parte_dotacao,
-                                }
-                            })
-                        }
-                    }
+
+                    custeio_previsto: dto.custeio_previsto,
+                    investimento_previsto: dto.investimento_previsto,
+                    parte_dotacao: dto.parte_dotacao,
+
                 },
                 select: { id: true }
             });
@@ -74,21 +66,19 @@ export class MetaOrcamentoService {
             where: {
                 meta_id: filters?.meta_id,
                 ano_referencia: filters?.ano_referencia,
-                ultima_revisao: (filters?.apenas_ultima_revisao == undefined ? true : filters?.apenas_ultima_revisao) ? true : undefined,
                 removido_em: null,
             },
             select: {
                 id: true,
-                meta_id: true,
+                criador: { select: { nome_exibicao: true } },
+                meta: { select: { id: true, codigo: true, titulo: true } },
+                atividade: { select: { id: true, codigo: true, titulo: true } },
+                iniciativa: { select: { id: true, codigo: true, titulo: true } },
                 criado_em: true,
-                criador: {
-                    select: { nome_exibicao: true }
-                },
                 ano_referencia: true,
-                soma_custeio_previsto: true,
-                ultima_revisao: true,
-                soma_investimento_previsto: true,
-                itens: true
+                custeio_previsto: true,
+                investimento_previsto: true,
+                parte_dotacao: true,
             },
             orderBy: [
                 { meta_id: 'asc' },
@@ -97,18 +87,59 @@ export class MetaOrcamentoService {
         });
 
         return metaOrcamentos.map((r) => {
-
             return {
                 ...r,
-                itens: r.itens.map((item) => {
-                    return {
-                        ...item,
-                        custeio_previsto: item.custeio_previsto.toFixed(2),
-                        investimento_previsto: item.investimento_previsto.toFixed(2),
-                    }
-                })
+                custeio_previsto: r.custeio_previsto.toFixed(2),
+                investimento_previsto: r.investimento_previsto.toFixed(2),
             }
+        })
+    }
+
+    async update(id: number, dto: UpdateMetaOrcamentoDto, user: PessoaFromJwt): Promise<RecordWithId> {
+        const { meta_id, iniciativa_id, atividade_id } = await this.orcamentoPlanejado.validaMetaIniAtv(dto);
+
+        const meta = await this.prisma.meta.findFirst({
+            where: { id: dto.meta_id, removido_em: null },
+            select: { pdm_id: true, id: true }
         });
+        if (!meta) throw new HttpException('meta não encontrada', 400);
+
+        const metaOrcamento = await this.prisma.metaOrcamento.findFirst({
+            where: { id: +id, removido_em: null }
+        });
+        if (!metaOrcamento) throw new HttpException('meta orçamento não encontrada', 400);
+
+        const anoCount = await this.prisma.pdmOrcamentoConfig.count({
+            where: { pdm_id: meta.pdm_id, ano_referencia: metaOrcamento.ano_referencia, previsao_custo_disponivel: true }
+        });
+        if (!anoCount) throw new HttpException('Ano de referencia não encontrado, verifique se está ativo no PDM', 400);
+
+
+        await this.prisma.$transaction(async (prisma: Prisma.TransactionClient) => {
+
+            const now = new Date(Date.now());
+
+            const metaOrcamento = await prisma.metaOrcamento.updateMany({
+                where: {
+                    id: +id
+                },
+                data: {
+                    meta_id: meta_id!,
+                    iniciativa_id,
+                    atividade_id,
+
+                    custeio_previsto: dto.custeio_previsto,
+                    investimento_previsto: dto.investimento_previsto,
+                    parte_dotacao: dto.parte_dotacao,
+
+                }
+            });
+        }, {
+            maxWait: 5000,
+            timeout: 100000
+        });
+
+        return { id: id };
     }
 
     async remove(id: number, user: PessoaFromJwt) {
