b806a771a6b24a6d779847e999130054c109f7f1
Renato Cron <renato.cron@gmail.com>
2022-12-15 04:55:19 -0300

    Add projeto_atividade no OrcamentoRealizado

--- backend/src/orcamento-realizado/entities/orcamento-realizado.entity.ts
+++ backend/src/orcamento-realizado/entities/orcamento-realizado.entity.ts
@@ -12,6 +12,7 @@ export class OrcamentoRealizado {
     iniciativa: IdCodTituloDto | null
     ano_referencia: number
     dotacao: string
+    projeto_atividade: string
     processo: string | null
     nota_empenho: string | null
 
