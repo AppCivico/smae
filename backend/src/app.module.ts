f65a2d13cf892319a662347bbb9030f3fc281a0b
Renato Cron <renato.cron@gmail.com>
2022-12-15 18:47:46 -0300

    Tratando exceptions PrismaClientKnownRequestError e virando 404 automaticamene

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,54 +1,53 @@
 import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { PrismaModule } from './prisma/prisma.module';
-import { PessoaModule } from './pessoa/pessoa.module';
-import { AuthModule } from './auth/auth.module';
-import { APP_GUARD, APP_INTERCEPTOR, RouterModule } from '@nestjs/core';
-import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
-import { MinhaContaController } from './minha-conta/minha-conta.controller';
-import { MinhaContaModule } from './minha-conta/minha-conta.module';
-import { LoggerMiddleware } from './common/middleware/logger.middleware';
-
+import { ConfigModule } from '@nestjs/config';
+import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR, RouterModule } from '@nestjs/core';
+import { ScheduleModule } from '@nestjs/schedule';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
-import { RolesGuard } from './auth/guards/roles.guard';
-import { OrgaoModule } from './orgao/orgao.module';
-import { TipoOrgaoModule } from './tipo-orgao/tipo-orgao.module';
-import { OdsModule } from './ods/ods.module';
-import { EixoModule } from './eixo/eixo.module';
-import { PdmModule } from './pdm/pdm.module';
-import { FonteRecursoModule } from './fonte-recurso/fonte-recurso.module';
-import { TipoDocumentoModule } from './tipo-documento/tipo-documento.module';
-import { TagModule } from './tag/tag.module';
-import { ObjetivoEstrategicoModule } from './objetivo-estrategico/objetivo-estrategico.module';
-import { RegiaoModule } from './regiao/regiao.module';
-import { UploadModule } from './upload/upload.module';
-import { ConfigModule } from '@nestjs/config';
-import { SubTemaModule } from './subtema/subtema.module';
-import { MetaModule } from './meta/meta.module';
-import { IndicadorModule } from './indicador/indicador.module';
-import { UnidadeMedidaModule } from './unidade-medida/unidade-medida.module';
-import { VariavelModule } from './variavel/variavel.module';
-import { IniciativaModule } from './iniciativa/iniciativa.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { AtividadeModule } from './atividade/atividade.module';
-import { CronogramaModule } from './cronograma/cronograma.module';
-import { EtapaModule } from './etapa/etapa.module';
-import { CronogramaEtapaModule } from './cronograma-etapas/cronograma-etapas.module';
-import { ScheduleModule } from '@nestjs/schedule';
-
-import { PainelModule } from './painel/painel.module';
-import { MfModule } from './mf/mf.module';
-import { MetasModule as MfMetasModule } from './mf/metas/metas.module';
-import { GrupoPaineisModule } from './grupo-paineis/grupo-paineis.module';
-import { PdmCicloModule } from './pdm-ciclo/pdm-ciclo.module';
-import { MetaOrcamentoModule } from './meta-orcamento/meta-orcamento.module';
-import { OrcamentoPlanejadoModule } from './orcamento-planejado/orcamento-planejado.module';
-import { DotacaoModule } from './dotacao/dotacao.module';
-import { SofApiModule } from './sof-api/sof-api.module';
-import { OrcamentoRealizadoModule } from './orcamento-realizado/orcamento-realizado.module';
-import { SofEntidadeModule } from './sof-entidade/sof-entidade.module';
+import { AuthModule } from './auth/auth.module';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { RolesGuard } from './auth/guards/roles.guard';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
 import { ContentInterceptor } from './content.interceptor';
+import { CronogramaEtapaModule } from './cronograma-etapas/cronograma-etapas.module';
+import { CronogramaModule } from './cronograma/cronograma.module';
+import { DotacaoModule } from './dotacao/dotacao.module';
+import { EixoModule } from './eixo/eixo.module';
+import { ErrorFilter } from './error.filter';
+import { EtapaModule } from './etapa/etapa.module';
+import { FonteRecursoModule } from './fonte-recurso/fonte-recurso.module';
+import { GrupoPaineisModule } from './grupo-paineis/grupo-paineis.module';
+import { IndicadorModule } from './indicador/indicador.module';
+import { IniciativaModule } from './iniciativa/iniciativa.module';
+import { MetaOrcamentoModule } from './meta-orcamento/meta-orcamento.module';
+import { MetaModule } from './meta/meta.module';
+import { MetasModule as MfMetasModule } from './mf/metas/metas.module';
+import { MfModule } from './mf/mf.module';
+import { MinhaContaController } from './minha-conta/minha-conta.controller';
+import { MinhaContaModule } from './minha-conta/minha-conta.module';
+import { ObjetivoEstrategicoModule } from './objetivo-estrategico/objetivo-estrategico.module';
+import { OdsModule } from './ods/ods.module';
+import { OrcamentoPlanejadoModule } from './orcamento-planejado/orcamento-planejado.module';
+import { OrcamentoRealizadoModule } from './orcamento-realizado/orcamento-realizado.module';
+import { OrgaoModule } from './orgao/orgao.module';
+import { PainelModule } from './painel/painel.module';
+import { PdmCicloModule } from './pdm-ciclo/pdm-ciclo.module';
+import { PdmModule } from './pdm/pdm.module';
+import { PessoaModule } from './pessoa/pessoa.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { RegiaoModule } from './regiao/regiao.module';
+import { SofApiModule } from './sof-api/sof-api.module';
+import { SofEntidadeModule } from './sof-entidade/sof-entidade.module';
+import { SubTemaModule } from './subtema/subtema.module';
+import { TagModule } from './tag/tag.module';
+import { TipoDocumentoModule } from './tipo-documento/tipo-documento.module';
+import { TipoOrgaoModule } from './tipo-orgao/tipo-orgao.module';
+import { UnidadeMedidaModule } from './unidade-medida/unidade-medida.module';
+import { UploadModule } from './upload/upload.module';
+import { VariavelModule } from './variavel/variavel.module';
 
 
 @Module({
@@ -114,6 +113,10 @@ import { ContentInterceptor } from './content.interceptor';
         {
             provide: APP_GUARD,
             useClass: RolesGuard,
+        },
+        {
+            provide: APP_FILTER,
+            useClass: ErrorFilter
         },
         {
             provide: APP_INTERCEPTOR,
