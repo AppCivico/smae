c7f47b4d5fecbe13f2e3ddeaa4920767dfdc5c6c
Renato Cron <renato.cron@gmail.com>
2022-12-15 15:38:07 -0300

    Nova versao do planejado

---
+++ backend/prisma/migrations/20221215180912_orcamento_versao_10k/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `empenho_liquido` on the `dotacao_planejado` table. All the data in the column will be lost.
+  - You are about to drop the column `valor_liquidado` on the `dotacao_planejado` table. All the data in the column will be lost.
+  - Added the required column `saldo_disponivel` to the `dotacao_planejado` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `val_orcado_atualizado` to the `dotacao_planejado` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `val_orcado_inicial` to the `dotacao_planejado` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "dotacao_planejado" DROP COLUMN "empenho_liquido",
+DROP COLUMN "valor_liquidado",
+ADD COLUMN     "saldo_disponivel" DOUBLE PRECISION NOT NULL,
+ADD COLUMN     "val_orcado_atualizado" DOUBLE PRECISION NOT NULL,
+ADD COLUMN     "val_orcado_inicial" DOUBLE PRECISION NOT NULL;
--- backend/src/dotacao/dotacao.service.ts
+++ backend/src/dotacao/dotacao.service.ts
@@ -14,6 +14,21 @@ export class DotacaoService {
         private readonly sof: SofApiService,
     ) { }
 
+    private getDotacaoOrgaoUnidadeProjFonte(dotacao: string): {
+        orgao: string
+        unidade: string
+        proj_atividade: string
+        fonte: string
+    } {
+        const partes = dotacao.split('.');
+        return {
+            orgao: partes[0],
+            unidade: partes[1],
+            proj_atividade: partes.slice(5, 7).join(''),
+            fonte: partes[8]
+        }
+    }
+
     async getOneProjetoAtividade(ano: number, dotacao: string): Promise<string> {
         // código é feito juntando o 6º com o 7º campo da dotação
         // const x = '16.10.12.122.3024.2.100.33903500.00';
@@ -100,7 +115,10 @@ export class DotacaoService {
 
         if (dotacaoExistente && dotacaoExistente.informacao_valida && dotacaoExistente.mes_utilizado == mesMaisAtual) {
             return {
-                empenho_liquido: dotacaoExistente.empenho_liquido.toFixed(2),
+                val_orcado_atualizado: dotacaoExistente.val_orcado_atualizado.toFixed(2),
+                val_orcado_inicial: dotacaoExistente.val_orcado_inicial.toFixed(2),
+                saldo_disponivel: dotacaoExistente.saldo_disponivel.toFixed(2),
+
                 id: dotacaoExistente.id,
                 informacao_valida: dotacaoExistente.informacao_valida,
                 smae_soma_valor_planejado: dotacaoExistente.smae_soma_valor_planejado.toFixed(2),
@@ -123,7 +141,9 @@ export class DotacaoService {
 
         return {
             id: dotacaoPlanejado.id,
-            empenho_liquido: dotacaoPlanejado.empenho_liquido.toFixed(2),
+            val_orcado_atualizado: dotacaoPlanejado.val_orcado_atualizado.toFixed(2),
+            val_orcado_inicial: dotacaoPlanejado.val_orcado_inicial.toFixed(2),
+            saldo_disponivel: dotacaoPlanejado.saldo_disponivel.toFixed(2),
             informacao_valida: dotacaoPlanejado.informacao_valida,
             smae_soma_valor_planejado: dotacaoPlanejado.smae_soma_valor_planejado.toFixed(2),
             mes_utilizado: dotacaoPlanejado.mes_utilizado,
@@ -264,10 +284,11 @@ export class DotacaoService {
     private async sincronizarDotacaoPlanejado(dto: AnoDotacaoDto, mes: number) {
         const now = new Date(Date.now());
         try {
-            const r = await this.sof.empenhoDotacao({
-                dotacao: dto.dotacao,
+
+            const r = await this.sof.orcadoProjeto({
                 ano: dto.ano,
-                mes: mes
+                mes: mes,
+                ...this.getDotacaoOrgaoUnidadeProjFonte(dto.dotacao)
             });
 
             for (const dotacao of r.data) {
@@ -284,7 +305,8 @@ export class DotacaoService {
                     // se ja existe, atualiza caso estiver com dados inválidos, ou se o valor for diferente no empenho_liquido
                     if (jaExiste && (
                         jaExiste.informacao_valida == false
-                        || (jaExiste.empenho_liquido.toFixed(2) != dotacao.empenho_liquido.toFixed(2))
+                        || (jaExiste.val_orcado_atualizado.toFixed(2) != dotacao.val_orcado_atualizado.toFixed(2))
+                        || (jaExiste.val_orcado_inicial.toFixed(2) != dotacao.val_orcado_inicial.toFixed(2))
                     )) {
                         await prisma.dotacaoPlanejado.update({
                             where: {
@@ -293,8 +315,10 @@ export class DotacaoService {
                             data: {
                                 informacao_valida: true,
                                 sincronizado_em: now,
-                                empenho_liquido: dotacao.empenho_liquido,
-                                pressao_orcamentaria: Math.round(jaExiste.smae_soma_valor_planejado * 100) > Math.round(dotacao.empenho_liquido * 100),
+                                val_orcado_atualizado: dotacao.val_orcado_atualizado,
+                                val_orcado_inicial: dotacao.val_orcado_inicial,
+                                saldo_disponivel: dotacao.saldo_disponivel,
+                                pressao_orcamentaria: Math.round(jaExiste.smae_soma_valor_planejado * 100) > Math.round(dotacao.val_orcado_atualizado * 100),
                             }
                         });
                     }
@@ -304,8 +328,9 @@ export class DotacaoService {
                             data: {
                                 informacao_valida: true,
                                 sincronizado_em: now,
-                                empenho_liquido: Number(dotacao.empenho_liquido),
-                                valor_liquidado: Number(dotacao.val_liquidado),
+                                val_orcado_atualizado: dotacao.val_orcado_atualizado,
+                                val_orcado_inicial: dotacao.val_orcado_inicial,
+                                saldo_disponivel: dotacao.saldo_disponivel,
                                 mes_utilizado: mes,
                                 ano_referencia: dto.ano,
                                 dotacao: dto.dotacao,
@@ -346,8 +371,9 @@ export class DotacaoService {
                             data: {
                                 informacao_valida: false,
                                 sincronizado_em: null,
-                                empenho_liquido: 0,
-                                valor_liquidado: 0,
+                                val_orcado_atualizado: 0,
+                                val_orcado_inicial: 0,
+                                saldo_disponivel: 0,
                                 mes_utilizado: mes,
                                 ano_referencia: dto.ano,
                                 dotacao: dto.dotacao,
