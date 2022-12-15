c69459e289de9e3683dccff15c4408a27c4e9a1b
Renato Cron <renato.cron@gmail.com>
2022-12-15 02:07:08 -0300

    Atualização backend-orcamento: projeto_atividade + /v1/orcado

---
+++ backend-orcamento/v1/endpoints/orcado.py
@@ -0,0 +1,49 @@
+from fastapi import APIRouter, Depends, HTTPException
+
+from requests.exceptions import HTTPError
+from core.exceptions import EmptyData, UnexpectedResponse
+
+from core.dao import DaoOrcadoProjeto
+from core.schemas.orcado_projeto import OrcadoProjeto as schm_orcado
+from core.schemas.orcado_projeto import RetornoOrcado as schm_retorno
+
+from core.schemas import MetaDados
+from config import SOF_API_TOKEN
+
+app = APIRouter()
+
+
+def get_dao():
+
+    dao = DaoOrcadoProjeto(auth_token=SOF_API_TOKEN)
+    
+    return dao
+
+def build_response(dao, endpoint_name, **params):
+
+
+    dao_func = getattr(dao, endpoint_name)
+    try:
+        result = dao_func(**params)
+    except EmptyData as e:
+        raise HTTPException(404, detail=str(e))
+    except UnexpectedResponse as e:
+        raise HTTPException(422, detail=str(e))
+    except HTTPError as e:
+        raise HTTPException(503, str(e))
+
+    if result:
+        resp_data = [schm_retorno(**item) for item in result]
+        resp = schm_orcado(data=resp_data,
+                    metadados=MetaDados(sucess=True))
+
+        return resp
+    
+    raise HTTPException(404, detail=f"Nenhum item encontrado")
+
+@app.get("/orcado_projeto", response_model=schm_orcado, tags=['Orcado'])
+def orcado_projeto(ano:int, mes:int, orgao:int, unidade: int, proj_atividade: int,
+                fonte: str, dao: DaoOrcadoProjeto = Depends(get_dao)):
+
+   return build_response(dao, 'orcado_projeto', ano=ano, mes=mes, orgao=orgao,
+                    unidade=unidade, proj_atividade=proj_atividade, fonte=fonte)
