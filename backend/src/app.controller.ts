47106549281080a49f8c0f29225d5733f7b74670
Renato CRON <renato.cron@gmail.com>
2022-12-16 16:15:47 -0300

    Modulo texto

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -4,26 +4,12 @@ import { PrismaService } from './prisma/prisma.service';
 
 @Controller()
 export class AppController {
-    constructor(
-        private readonly prisma: PrismaService,
-    ) { }
+    constructor() { }
 
     @IsPublic()
     @Get('/ping')
     async ping() {
         return 'pong';
     }
-
-    @IsPublic()
-    @Get('/texto-tos')
-    async textos() {
-        const textoConfig = await this.prisma.textoConfig.findFirstOrThrow({ where: { id: 1 } });
-        return {
-            bemvindo_email: textoConfig.bemvindo_email,
-            tos: textoConfig.tos,
-        };
-    }
-
-
 
 }
