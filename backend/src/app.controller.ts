c7169bff30777e93b7befa7ea96d5ea0053c6ee8
Renato CRON <renato.cron@gmail.com>
2022-12-16 16:09:55 -0300

    Add endpoint pra baixar os textos

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,13 +1,29 @@
 import { Controller, Get } from '@nestjs/common';
 import { IsPublic } from './auth/decorators/is-public.decorator';
+import { PrismaService } from './prisma/prisma.service';
 
 @Controller()
 export class AppController {
-    constructor() { }
+    constructor(
+        private readonly prisma: PrismaService,
+    ) { }
 
     @IsPublic()
     @Get('/ping')
     async ping() {
         return 'pong';
     }
+
+    @IsPublic()
+    @Get('/texto-tos')
+    async textos() {
+        const textoConfig = await this.prisma.textoConfig.findFirstOrThrow({ where: { id: 1 } });
+        return {
+            bemvindo_email: textoConfig.bemvindo_email,
+            tos: textoConfig.tos,
+        };
+    }
+
+
+
 }
