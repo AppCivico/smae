a401437b792c1dcbb51f400f10652b5df38fc73f
Renato Cron <renato.cron@gmail.com>
2022-12-19 03:05:51 -0300

    WIP fix

--- backend/src/reports/orcamento-executado/orcamento-executado.controller.ts
+++ backend/src/reports/orcamento-executado/orcamento-executado.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
 import { Roles } from '../../auth/decorators/roles.decorator';
-import { OrcamentoExecutadoParams } from './dto/create-orcamento-executado.dto';
+import { CreateOrcamentoExecutadoDto } from './dto/create-orcamento-executado.dto';
 import { ListOrcamentoExecutadoDto } from './entities/orcamento-executado.entity';
 import { OrcamentoExecutadoService } from './orcamento-executado.service';
 
@@ -14,7 +14,7 @@ export class OrcamentoExecutadoController {
     @ApiBearerAuth('access-token')
     @ApiUnauthorizedResponse()
     @Roles('Reports.executar')
-    async create(@Body() createOrcamentoExecutadoDto: OrcamentoExecutadoParams): Promise<ListOrcamentoExecutadoDto> {
+    async create(@Body() createOrcamentoExecutadoDto: CreateOrcamentoExecutadoDto): Promise<ListOrcamentoExecutadoDto> {
         return this.orcamentoExecutadoService.create(createOrcamentoExecutadoDto);
     }
 
