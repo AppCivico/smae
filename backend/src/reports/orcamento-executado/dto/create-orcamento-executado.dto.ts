6a5dca3361e4f857f8da4f2665da9f52e584225e
Renato Cron <renato.cron@gmail.com>
2022-12-19 02:58:02 -0300

    WIP

--- backend/src/reports/orcamento-executado/dto/create-orcamento-executado.dto.ts
+++ backend/src/reports/orcamento-executado/dto/create-orcamento-executado.dto.ts
@@ -1,1 +1,21 @@
-export class CreateOrcamentoExecutadoDto {}
+import { IntersectionType } from "@nestjs/swagger"
+import { TipoRelatorio } from "@prisma/client"
+import { IsOnlyDate } from "../../../common/decorators/IsDateOnly"
+import { FiltroMetasIniAtividadeDto } from "../../reports/dto/filtros.dto"
+
+export class OrcamentoExecutadoParams {
+    pdm_id: number
+
+    tipo: TipoRelatorio
+
+    @IsOnlyDate()
+    inicio: Date
+
+    @IsOnlyDate()
+    fim: Date
+}
+
+export class CreateOrcamentoExecutadoDto extends IntersectionType(
+    FiltroMetasIniAtividadeDto,
+    OrcamentoExecutadoParams
+) { }
