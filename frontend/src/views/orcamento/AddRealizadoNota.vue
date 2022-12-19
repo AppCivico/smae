b5fba109e1f9038d412403b81749e56921bb77d8
sobral <robson@oficiodesign.com>
2022-12-19 15:15:50 -0300

    Remover código de debug

--- frontend/src/views/orcamento/AddRealizadoNota.vue
+++ frontend/src/views/orcamento/AddRealizadoNota.vue
@@ -82,8 +82,9 @@
 	}
 
 async function checkClose() {
-    alertStore.confirm('Deseja sair sem sal++var as alterações?', `${parentlink}/orcamento/realizado`);
-  }
+  alertStore.confirm('Deseja sair sem salvar as alterações?', `${parentlink}/orcamento/realizado`);
+}
+
 	async function checkDelete(id) {
 	    alertStore.confirmAction('Deseja mesmo remover esse item?',async()=>{if(await OrcamentosStore.deleteOrcamentoRealizado(id)) router.push(`${parentlink}/orcamento`)},'Remover');
 	}
