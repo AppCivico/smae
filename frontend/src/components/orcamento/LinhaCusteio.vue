ebf2a979c10a16160712c3792c886aeeafb6ed55
sobral <robson@oficiodesign.com>
2022-12-19 13:27:54 -0300

    Exibir datas de atualização em "Previsão de custo"

--- frontend/src/components/orcamento/LinhaCusteio.vue
+++ frontend/src/components/orcamento/LinhaCusteio.vue
@@ -1,21 +1,20 @@
 <script setup>
-    const props = defineProps(['group','permissao','parentlink']);
-    function formataValor(d){
-        return Number(d).toLocaleString('pt-BR', {
-          style: 'currency',
-          currency: 'BRL',
-        }) ?? d;
-    }
+import dateToField from '@/helpers/dateToField';
+import formataValor from "@/helpers/formataValor";
+
+const props = defineProps(['group', 'permissao', 'parentlink']);
 </script>
+
 <template>
     <tr v-for="item in group.items" :key="item.id">
         <td style="word-break: break-all;">{{item?.parte_dotacao}}</td>
         <td>{{formataValor(item?.investimento_previsto)}}</td>
         <td>{{formataValor(item?.custeio_previsto)}}</td>
+        <td>{{dateToField(item?.atualizado_em)}}</td>
         <td style="white-space: nowrap; text-align: right">
-            <router-link 
+            <router-link
                 v-if="permissao"
-                :to="`${parentlink}/orcamento/custeio/${item.ano_referencia}/${item.id}`" 
+                :to="`${parentlink}/orcamento/custeio/${item.ano_referencia}/${item.id}`"
                 class="tprimary"><svg width="20" height="20"><use xlink:href="#i_edit"></use></svg></router-link>
         </td>
     </tr>
