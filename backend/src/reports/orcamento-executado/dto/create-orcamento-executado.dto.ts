13b248c701dafefaa1363f3c7c86debc147539ff
Renato Cron <renato.cron@gmail.com>
2022-12-19 03:05:51 -0300

    WIP fix

--- backend/src/reports/orcamento-executado/dto/create-orcamento-executado.dto.ts
+++ backend/src/reports/orcamento-executado/dto/create-orcamento-executado.dto.ts
@@ -1,17 +1,26 @@
-import { IntersectionType } from "@nestjs/swagger"
+import { ApiProperty, IntersectionType } from "@nestjs/swagger"
 import { TipoRelatorio } from "@prisma/client"
+import { Type } from "class-transformer"
+import { IsEnum } from "class-validator"
 import { IsOnlyDate } from "../../../common/decorators/IsDateOnly"
 import { FiltroMetasIniAtividadeDto } from "../../reports/dto/filtros.dto"
 
 export class OrcamentoExecutadoParams {
     pdm_id: number
 
+
+    @ApiProperty({ enum: TipoRelatorio, enumName: 'TipoRelatorio' })
+    @IsEnum(TipoRelatorio, {
+        message: '$property| Precisa ser um dos seguintes valores: ' + Object.values(TipoRelatorio).join(', ')
+    })
     tipo: TipoRelatorio
 
     @IsOnlyDate()
+    @Type(() => Date)
     inicio: Date
 
     @IsOnlyDate()
+    @Type(() => Date)
     fim: Date
 }
 
