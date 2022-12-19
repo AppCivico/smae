9ce4e4279a02fb09afb14a94a6f33ec5a87a48da
Renato Cron <renato.cron@gmail.com>
2022-12-19 20:50:49 -0300

    Adicionando linhas_planejado

--- backend/src/reports/orcamento-executado/orcamento-executado.service.ts
+++ backend/src/reports/orcamento-executado/orcamento-executado.service.ts
@@ -4,9 +4,9 @@ import { DotacaoService } from '../../dotacao/dotacao.service';
 import { PrismaService } from '../../prisma/prisma.service';
 import { UtilsService } from '../utils/utils.service';
 import { CreateOrcamentoExecutadoDto } from './dto/create-orcamento-executado.dto';
-import { OrcamentoExecutadoSaidaDto } from './entities/orcamento-executado.entity';
+import { ListOrcamentoExecutadoDto, OrcamentoExecutadoSaidaDto, OrcamentoPlanejadoSaidaDto } from './entities/orcamento-executado.entity';
 
-class RetornoDb {
+class RetornoRealizadoDb {
     plan_dotacao_ano_utilizado: string | null
     plan_dotacao_mes_utilizado: string | null
     plan_sof_val_orcado_atualizado: string | null
@@ -37,6 +37,31 @@ class RetornoDb {
     orcamento_realizado_item_id?: number
 }
 
+class RetornoPlanejadoDb {
+    plan_dotacao_ano_utilizado: string
+    plan_dotacao_mes_utilizado: string
+    plan_sof_val_orcado_atualizado: string
+    plan_valor_planejado: string
+    plan_dotacao_sincronizado_em: Date
+    dotacao: string
+
+    meta_id: string
+    meta_codigo: string
+    meta_titulo: string
+
+    iniciativa_id: string
+    iniciativa_codigo: string
+    iniciativa_titulo: string
+
+    atividade_id: string
+    atividade_codigo: string
+    atividade_titulo: string
+    ano: string
+
+    total_registros?: number
+    orcamento_planejado_id?: number
+}
+
 @Injectable()
 export class OrcamentoExecutadoService {
     constructor(
@@ -45,7 +70,10 @@ export class OrcamentoExecutadoService {
         private readonly dotacaoService: DotacaoService
     ) { }
 
-    async create(dto: CreateOrcamentoExecutadoDto): Promise<OrcamentoExecutadoSaidaDto[]> {
+    async create(dto: CreateOrcamentoExecutadoDto): Promise<ListOrcamentoExecutadoDto> {
+
+        const anoIni = Date2YMD.toString(dto.inicio).substring(0, 4);
+        const anoFim = Date2YMD.toString(dto.fim).substring(0, 4);
 
         const { metas } = await this.utils.applyFilter(dto, { iniciativas: false, atividades: false });
 
@@ -75,81 +103,153 @@ export class OrcamentoExecutadoService {
             select: { id: true }
         });
 
-        console.log(search, orgaoMatch);
-
-        const ret: OrcamentoExecutadoSaidaDto[] = [];
-
+        const retExecutado: OrcamentoExecutadoSaidaDto[] = [];
+        const retPlanejado: OrcamentoPlanejadoSaidaDto[] = [];
+        const dotacoesPlanejadasVistas: Record<string, boolean> = {};
         if ((dto.tipo == 'Analitico') && search.length > 0) {
-            const resultados: RetornoDb[] = await this.prisma.$queryRaw`
-            with custos as (
-                select
-                    o.dotacao,
-                    o.processo,
-                    o.nota_empenho,
-                    to_char(i.valor_empenho, 'FM999999999.00') as smae_valor_empenhado,
-                    to_char(i.valor_liquidado, 'FM999999999.00') as smae_valor_liquidado,
-                    i.mes,
-                    o.ano_referencia as ano,
+            const resultadosRealizado: RetornoRealizadoDb[] = await this.queryAnaliticoExecutado(search);
+            for (const r of resultadosRealizado) {
+                retExecutado.push(this.convertRealizadoRow(r))
 
-                    o.meta_id,
+                if (r.plan_valor_planejado !== null) {
+                    dotacoesPlanejadasVistas[r.dotacao] = true;
+                }
+
+            }
+
+            const resultadosPlanejados = await this.queryAnaliticoPlanejado(anoIni, anoFim, Object.keys(dotacoesPlanejadasVistas));
+            for (const r of resultadosPlanejados) {
+                retPlanejado.push(this.convertPlanejadoRow(r))
+            }
+
+        } else if ((dto.tipo == 'Consolidado') && search.length > 0) {
+            const resultados: RetornoRealizadoDb[] = await this.queryConsolidadoExecutado(search);
+
+            for (const r of resultados) {
+                retExecutado.push(this.convertRealizadoRow(r))
+                if (r.plan_valor_planejado !== null) {
+                    dotacoesPlanejadasVistas[r.dotacao] = true;
+                }
+            }
+
+            const resultadosPlanejados = await this.queryConsolidadoPlanejado(anoIni, anoFim, Object.keys(dotacoesPlanejadasVistas));
+            for (const r of resultadosPlanejados) {
+                retPlanejado.push(this.convertPlanejadoRow(r))
+            }
+        }
+
+        return {
+            linhas: retExecutado,
+            linhas_planejado: retPlanejado
+        };
+
+    }
+
+
+    private async queryConsolidadoPlanejado(ano_ini: string, ano_fim: string, pularDotacoes: string[]): Promise<RetornoPlanejadoDb[]> {
+        return await this.prisma.$queryRaw`
+            with previsoes as (
+                select
+                    op.dotacao,
+                    op.valor_planejado as plan_valor_planejado,
+                    op.ano_referencia as ano,
+
+                    op.meta_id,
                     m.codigo as meta_codigo,
                     m.titulo as meta_titulo,
 
-                    o.iniciativa_id,
+                    op.iniciativa_id,
                     mi.codigo as iniciativa_codigo,
                     mi.titulo as iniciativa_titulo,
 
-                    o.atividade_id,
+                    op.atividade_id,
                     ma.codigo as atividade_codigo,
-                    ma.titulo as atividade_titulo,
+                    ma.titulo as atividade_titulo
 
-                    i.id as orcamento_realizado_item_id
-                from orcamento_realizado_item i
-                join orcamento_realizado o on o.id = i.orcamento_realizado_id
+                from orcamento_planejado op
                 left join meta m on m.id = meta_id
                 left join iniciativa mi on mi.id = iniciativa_id
                 left join atividade ma on ma.id = atividade_id
 
-                where i.id = ANY(${search.map(r => r.id)}::int[])
+                where NOT (op.dotacao = ANY(${pularDotacoes}::varchar[]))
+                and op.ano_referencia >= ${ano_ini}::int
+                and op.ano_referencia <= ${ano_fim}::int
+                and op.removido_em is null
             )
             select
                 dp.ano_referencia as plan_dotacao_ano_utilizado,
                 dp.mes_utilizado as plan_dotacao_mes_utilizado,
                 to_char(dp.val_orcado_atualizado, 'FM999999999.00') as plan_sof_val_orcado_atualizado,
-                to_char(op.valor_planejado, 'FM999999999.00') as plan_valor_planejado,
                 dp.sincronizado_em as plan_dotacao_sincronizado_em,
 
-                custos.*,
+                previsoes.dotacao,
+                previsoes.ano,
+                previsoes.meta_id,
+                previsoes.meta_codigo,
+                previsoes.meta_titulo,
+                previsoes.iniciativa_id,
+                previsoes.iniciativa_codigo,
+                previsoes.iniciativa_titulo,
+                previsoes.atividade_id,
+                previsoes.atividade_codigo,
+                previsoes.atividade_titulo,
 
-                coalesce(dn.empenho_liquido, drp.empenho_liquido, dr.empenho_liquido) as dotacao_valor_empenhado,
-                coalesce(dn.valor_liquidado, drp.valor_liquidado, dr.valor_liquidado) as dotacao_valor_liquidado,
-                coalesce(dn.mes_utilizado, drp.mes_utilizado, dr.mes_utilizado) as dotacao_mes_utilizado,
-                coalesce(dn.ano_referencia, drp.ano_referencia, dr.ano_referencia) as dotacao_ano_utilizado,
-                coalesce(dn.sincronizado_em, drp.sincronizado_em, dr.sincronizado_em) as dotacao_sincronizado_em
-
-            from custos
-            left join orcamento_planejado op ON custos.processo IS NULL
-                AND ( custos.dotacao = op.dotacao AND custos.ano = op.ano_referencia)
-            left join dotacao_planejado dp ON custos.processo IS NULL
-                AND ( custos.dotacao = dp.dotacao AND custos.ano = dp.ano_referencia)
-
-                left join dotacao_realizado dr ON custos.processo IS NULL
-                AND ( custos.dotacao = dr.dotacao AND custos.ano = dr.ano_referencia)
-
-                left join dotacao_processo drp ON custos.processo IS NOT NULL AND custos.nota_empenho IS NULL
-                AND ( custos.dotacao = drp.dotacao AND custos.ano = drp.ano_referencia AND custos.processo = drp.dotacao_processo)
-
-                left join dotacao_processo_nota dn ON custos.nota_empenho IS NOT NULL
-                AND ( custos.dotacao = dn.dotacao AND custos.ano = dn.ano_referencia AND custos.processo = dn.dotacao_processo  AND custos.nota_empenho = dn.dotacao_processo_nota)
+                to_char(sum(previsoes.plan_valor_planejado), 'FM999999999.00') as plan_valor_planejado,
+                count(1) as total_registros
+            from previsoes
+            left join dotacao_planejado dp ON previsoes.dotacao = dp.dotacao AND previsoes.ano = dp.ano_referencia
+            GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
+            order by previsoes.dotacao, 1, 2
             `;
+    }
 
-            for (const r of resultados) {
-                ret.push(this.convertRow(r))
-            }
+    private async queryAnaliticoPlanejado(ano_ini: string, ano_fim: string, pularDotacoes: string[]): Promise<RetornoPlanejadoDb[]> {
+        return await this.prisma.$queryRaw`
+            with previsoes as (
+                select
+                    op.dotacao,
+                    to_char(op.valor_planejado, 'FM999999999.00') as plan_valor_planejado,
+                    op.ano_referencia as ano,
 
-            console.log(resultados);
-        } else if ((dto.tipo == 'Consolidado') && search.length > 0) {
-            const resultados: RetornoDb[] = await this.prisma.$queryRaw`
+                    op.meta_id,
+                    m.codigo as meta_codigo,
+                    m.titulo as meta_titulo,
+
+                    op.iniciativa_id,
+                    mi.codigo as iniciativa_codigo,
+                    mi.titulo as iniciativa_titulo,
+
+                    op.atividade_id,
+                    ma.codigo as atividade_codigo,
+                    ma.titulo as atividade_titulo,
+
+                    op.id as orcamento_planejado_id
+                from orcamento_planejado op
+                left join meta m on m.id = meta_id
+                left join iniciativa mi on mi.id = iniciativa_id
+                left join atividade ma on ma.id = atividade_id
+
+                where NOT (op.dotacao = ANY(${pularDotacoes}::varchar[]))
+                and op.ano_referencia >= ${ano_ini}::int
+                and op.ano_referencia <= ${ano_fim}::int
+                and op.removido_em is null
+            )
+            select
+                dp.ano_referencia as plan_dotacao_ano_utilizado,
+                dp.mes_utilizado as plan_dotacao_mes_utilizado,
+                to_char(dp.val_orcado_atualizado, 'FM999999999.00') as plan_sof_val_orcado_atualizado,
+                dp.sincronizado_em as plan_dotacao_sincronizado_em,
+
+                previsoes.*
+
+            from previsoes
+            left join dotacao_planejado dp ON previsoes.dotacao = dp.dotacao AND previsoes.ano = dp.ano_referencia
+            order by op.dotacao, 1, 2
+            `;
+    }
+
+    private async queryConsolidadoExecutado(search: { id: number; }[]): Promise<RetornoRealizadoDb[]> {
+        return await this.prisma.$queryRaw`
             with custos as (
                 select
                 o.dotacao,
@@ -241,18 +341,114 @@ export class OrcamentoExecutadoService {
                 count(1) as total_registros
             FROM analitico
             GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24
+            order by analitico.dotacao, analitico.processo, analitico.nota_empenho, 1, 2
             `;
+    }
 
-            for (const r of resultados) {
-                ret.push(this.convertRow(r))
-            }
+    private async queryAnaliticoExecutado(search: { id: number; }[]): Promise<RetornoRealizadoDb[]> {
+        return await this.prisma.$queryRaw`
+            with custos as (
+                select
+                    o.dotacao,
+                    o.processo,
+                    o.nota_empenho,
+                    to_char(i.valor_empenho, 'FM999999999.00') as smae_valor_empenhado,
+                    to_char(i.valor_liquidado, 'FM999999999.00') as smae_valor_liquidado,
+                    i.mes,
+                    o.ano_referencia as ano,
+
+                    o.meta_id,
+                    m.codigo as meta_codigo,
+                    m.titulo as meta_titulo,
+
+                    o.iniciativa_id,
+                    mi.codigo as iniciativa_codigo,
+                    mi.titulo as iniciativa_titulo,
+
+                    o.atividade_id,
+                    ma.codigo as atividade_codigo,
+                    ma.titulo as atividade_titulo,
+
+                    i.id as orcamento_realizado_item_id
+                from orcamento_realizado_item i
+                join orcamento_realizado o on o.id = i.orcamento_realizado_id
+                left join meta m on m.id = meta_id
+                left join iniciativa mi on mi.id = iniciativa_id
+                left join atividade ma on ma.id = atividade_id
+
+                where i.id = ANY(${search.map(r => r.id)}::int[])
+            )
+            select
+                dp.ano_referencia as plan_dotacao_ano_utilizado,
+                dp.mes_utilizado as plan_dotacao_mes_utilizado,
+                to_char(dp.val_orcado_atualizado, 'FM999999999.00') as plan_sof_val_orcado_atualizado,
+                to_char(op.valor_planejado, 'FM999999999.00') as plan_valor_planejado,
+                dp.sincronizado_em as plan_dotacao_sincronizado_em,
+
+                custos.*,
+
+                coalesce(dn.empenho_liquido, drp.empenho_liquido, dr.empenho_liquido) as dotacao_valor_empenhado,
+                coalesce(dn.valor_liquidado, drp.valor_liquidado, dr.valor_liquidado) as dotacao_valor_liquidado,
+                coalesce(dn.mes_utilizado, drp.mes_utilizado, dr.mes_utilizado) as dotacao_mes_utilizado,
+                coalesce(dn.ano_referencia, drp.ano_referencia, dr.ano_referencia) as dotacao_ano_utilizado,
+                coalesce(dn.sincronizado_em, drp.sincronizado_em, dr.sincronizado_em) as dotacao_sincronizado_em
+
+            from custos
+            left join orcamento_planejado op ON custos.processo IS NULL
+                AND ( custos.dotacao = op.dotacao AND custos.ano = op.ano_referencia)
+            left join dotacao_planejado dp ON custos.processo IS NULL
+                AND ( custos.dotacao = dp.dotacao AND custos.ano = dp.ano_referencia)
+
+                left join dotacao_realizado dr ON custos.processo IS NULL
+                AND ( custos.dotacao = dr.dotacao AND custos.ano = dr.ano_referencia)
+
+                left join dotacao_processo drp ON custos.processo IS NOT NULL AND custos.nota_empenho IS NULL
+                AND ( custos.dotacao = drp.dotacao AND custos.ano = drp.ano_referencia AND custos.processo = drp.dotacao_processo)
+
+                left join dotacao_processo_nota dn ON custos.nota_empenho IS NOT NULL
+                AND ( custos.dotacao = dn.dotacao AND custos.ano = dn.ano_referencia AND custos.processo = dn.dotacao_processo  AND custos.nota_empenho = dn.dotacao_processo_nota)
+                order by custos.dotacao, custos.processo, custos.nota_empenho, 1, 2
+            `;
+    }
+
+
+
+    private convertPlanejadoRow(db: RetornoPlanejadoDb): OrcamentoPlanejadoSaidaDto {
+        const logs: string[] = [];
+
+        if ("total_registros" in db && db.total_registros) {
+            logs.push(`Total de Itens Consolidados ${db.total_registros}`)
+        }
+        if ("orcamento_planejado_id" in db && db.orcamento_planejado_id) {
+            logs.push(`orcamento_planejado_id = ${db.orcamento_planejado_id}`)
         }
 
-        return ret;
+        return {
+            meta: { codigo: db.meta_codigo, titulo: db.meta_titulo, id: +db.meta_id },
+            iniciativa: db.iniciativa_id ? { codigo: db.iniciativa_codigo, titulo: db.iniciativa_titulo, id: +db.iniciativa_id } : null,
+            atividade: db.atividade_id ? { codigo: db.atividade_codigo, titulo: db.atividade_titulo, id: +db.atividade_id } : null,
+
+            dotacao: db.dotacao,
+
+            orgao: this.dotacaoService.orgaoFromDotacao(db.dotacao),
+            unidade: this.dotacaoService.unidadeFromDotacao(db.dotacao),
+            fonte: this.dotacaoService.fonteFromDotacao(db.dotacao),
+
+            plan_dotacao_sincronizado_em: db.plan_dotacao_sincronizado_em.toISOString(),
+            plan_sof_val_orcado_atualizado: db.plan_sof_val_orcado_atualizado,
+            plan_valor_planejado: db.plan_valor_planejado,
+            plan_dotacao_ano_utilizado: db.plan_dotacao_ano_utilizado.toString(),
+            plan_dotacao_mes_utilizado: db.plan_dotacao_mes_utilizado.toString(),
+
+            ano: db.ano,
+
+            logs: logs
+        }
 
     }
 
-    private convertRow(db: RetornoDb): OrcamentoExecutadoSaidaDto {
+
+    private convertRealizadoRow(db: RetornoRealizadoDb): OrcamentoExecutadoSaidaDto {
         const logs: string[] = [];
 
         if ("total_registros" in db && db.total_registros) {
