67f28857cc2ff480f6181782140cb9b579e9b3fa
Renato Cron <renato.cron@gmail.com>
2022-12-19 03:05:51 -0300

    WIP fix

--- backend/src/reports/reports/dto/filtros.dto.ts
+++ backend/src/reports/reports/dto/filtros.dto.ts
@@ -1,12 +1,20 @@
-import { IsOptional } from "class-validator"
+import { Type } from "class-transformer"
+import { IsNumber, IsOptional } from "class-validator"
 
 export class FiltroMetasIniAtividadeDto {
     @IsOptional()
+    @IsNumber()
+    @Type(() => Number)
     meta_id?: number
 
     @IsOptional()
+    @IsNumber()
+    @Type(() => Number)
+
     iniciativa_id?: number
 
     @IsOptional()
+    @IsNumber()
+    @Type(() => Number)
     atividade_id?: number
 }
