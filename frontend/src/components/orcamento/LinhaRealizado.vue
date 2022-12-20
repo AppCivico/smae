0db591d4ef74f153f007b20ea9d4623023d2f283
sobral <robson@oficiodesign.com>
2022-12-19 21:04:50 -0300

    Indicar excesso em relação a valores do SOF

--- frontend/src/components/orcamento/LinhaRealizado.vue
+++ frontend/src/components/orcamento/LinhaRealizado.vue
@@ -1,22 +1,26 @@
 <script setup>
-    const props = defineProps(['group','permissao','parentlink']);
-    function formataValor(d){
-        return Number(d).toLocaleString('pt-BR', {
-          style: 'currency',
-          currency: 'BRL',
-        }) ?? d;
-    }
+import formataValor from '@/helpers/formataValor';
+
+defineProps(['group', 'permissao', 'parentlink']);
+
+const alemDoEmpenhado = (x) => x.smae_soma_valor_empenho > x.empenho_liquido;
+const alemDoLiquidado = (x) => x.smae_soma_valor_liquidado > x.valor_liquidado;
 </script>
 <template>
     <tr v-for="item in group.items" :key="item.id">
-        <td style="word-break: break-all;">{{item?.dotacao}} {{item?.processo? ' / '+item?.processo : ''}} {{item?.nota_empenho? ' / '+item?.nota_empenho : ''}}</td>
-        <td>{{formataValor(item?.soma_valor_empenho)}}</td>
-        <td>{{formataValor(item?.soma_valor_liquidado)}}</td>
+        <td style="word-break: break-all;">
+          {{ item?.dotacao }}
+          {{ item?.processo ? '/ '+item?.processo : '' }}
+          {{ item?.nota_empenho ? ' / '+item?.nota_empenho : '' }}
+        </td>
+
+        <td :class="{'tvermelho': alemDoEmpenhado(item)}">{{formataValor(item?.soma_valor_empenho)}}</td>
+        <td :class="{'tvermelho': alemDoLiquidado(item)}">{{formataValor(item?.soma_valor_liquidado)}}</td>
         <td>{{ item?.itens.length }}</td>
         <td style="white-space: nowrap; text-align: right">
-            <router-link 
+            <router-link
                 v-if="permissao"
-                :to="`${parentlink}/orcamento/realizado/${item.ano_referencia}/${item.id}`" 
+                :to="`${parentlink}/orcamento/realizado/${item.ano_referencia}/${item.id}`"
                 class="tprimary"><svg width="20" height="20"><use xlink:href="#i_edit"></use></svg></router-link>
         </td>
     </tr>
