2beb588dba7360cfa5276f349a03824a17f103d2
Renato Cron <renato.cron@gmail.com>
2022-12-19 21:15:22 -0300

    update MV

--- backend/prisma/migrations/20221215041334_msview/migration.sql
+++ backend/prisma/migrations/20221215041334_msview/migration.sql
@@ -9,9 +9,9 @@ WITH dict AS (
             json_object_keys(dados) AS json_keys,
             ano
         FROM
-            sof_entidade ) a
+            sof_entidade) a
     WHERE
-        json_keys in ('projetos_atividades')
+        json_keys IN ('projetos_atividades', 'unidades', 'fonte_recursos', 'orgaos')
         -- json_keys not in ('metadados')
     GROUP BY
         ano
@@ -31,19 +31,36 @@ lists AS (
     FROM
         exploded e
         JOIN sof_entidade sof ON sof.ano = e.ano
+),
+lines AS (
+    SELECT
+        ano,
+        col,
+        trim((json_array_elements(arr)) ->> 'codigo') AS codigo,
+        (json_array_elements(arr)) ->> 'cod_orgao' AS cod_orgao,
+        trim((json_array_elements(arr)) ->> 'descricao') AS descricao
+    FROM
+        lists
 )
 SELECT
     ano,
     col,
-    trim((json_array_elements(arr)) ->> 'codigo') AS codigo,
-    trim((json_array_elements(arr)) ->> 'descricao') AS descricao,
-    (json_array_elements(arr)) ->> 'cod_orgao' AS cod_orgao
+    coalesce(codigo, '') AS codigo,
+    coalesce(cod_orgao, '') AS cod_orgao,
+    string_agg(descricao, ' / ' ORDER BY descricao) AS descricao
 FROM
-    lists;
+    lines
+GROUP BY
+    1,
+    2,
+    3,
+    4;
 
-CREATE INDEX ix_sof_entidades_linhas_generico
-  ON sof_entidades_linhas (ano, col, codigo);
+CREATE INDEX ix_sof_entidades_linhas_generico ON sof_entidades_linhas (ano, col, codigo)
+WHERE
+    col != 'projetos_atividades';
 
--- na teoria da pra usar qualquer coluna, mas os códigos dos outros items estão dando resultados duplicados
-CREATE UNIQUE INDEX ix_uniq_sof_entidades_projetos_atividades
-  ON sof_entidades_linhas (ano, codigo) where col = 'projetos_atividades';
+CREATE UNIQUE INDEX ix_uniq_sof_entidades_projetos_atividades ON sof_entidades_linhas (ano, codigo)
+WHERE
+    col = 'projetos_atividades';
+
