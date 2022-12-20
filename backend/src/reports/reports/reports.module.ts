ff3be3b9c935438a1d70c77f480ca2158392a86c
Renato Cron <renato.cron@gmail.com>
2022-12-20 11:11:07 -0300

    V1 da validacao do reports

--- backend/src/reports/reports/reports.module.ts
+++ backend/src/reports/reports/reports.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ReportsService } from './reports.service';
 import { ReportsController } from './reports.controller';
+import { PrismaModule } from '../../prisma/prisma.module';
+import { OrcamentoModule } from '../orcamento/orcamento.module';
 
 @Module({
+    imports: [PrismaModule, OrcamentoModule],
     controllers: [ReportsController],
     providers: [ReportsService]
 })
