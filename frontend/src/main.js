912be700f0a9b9700b18ded445fcffd7433357d2
sobral <robson@oficiodesign.com>
2022-12-21 00:51:40 -0300

    Disponibilizar rotas dentro do gerenciador de estados

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,16 @@
-import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-
+import { createApp } from 'vue';
 import App from './App.vue';
 import { router } from './router';
+import { useRoute } from 'vue-router';
 
 const app = createApp(App);
+const pinia = createPinia();
 
-app.use(createPinia());
+pinia.use(({ store }) => {
+  store.route = useRoute();
+})
+
+app.use(pinia);
 app.use(router);
-
 app.mount('#app');
