c660592ee1f434eda91ea8b965586e21b4adc058
sobral <robson@oficiodesign.com>
2022-12-21 22:00:51 -0300

    Adicionar telas de listagens de relatórios...
    ...ainda que só parcialmente funcionais

---
+++ frontend/src/views/relatorios/RelatoriosSemestraisOuAnuais.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { useAuthStore, useRelatoriosStore } from '@/stores';
+import { storeToRefs } from 'pinia';
+import { reactive, ref } from 'vue';
+
+const authStore = useAuthStore();
+const { permissions } = storeToRefs(authStore);
+const perm = permissions.value;
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+const relatoriosStore = useRelatoriosStore();
+const { tempRelatoriosTypes } = storeToRefs(relatoriosStore);
+relatoriosStore.clear();
+relatoriosStore.filterRelatorios();
+
+const filters = reactive({
+  textualSearch: ""
+});
+let itemsFiltered = ref(tempRelatoriosTypes);
+
+function filterItems() {
+  relatoriosStore.filterRelatorios(filters);
+}
+</script>
+<template>
+  <div class="flex spacebetween center mb2">
+      <h1>{{ route.meta.título }}</h1>
+      <hr class="ml2 f1"/>
+      <router-link to="./novo" class="btn big ml2" v-if="perm?.CadastroTipoRelatorioso?.inserir">Novo relatório</router-link>
+  </div>
+
+</template>
---
+++ frontend/src/views/relatorios/index.js
@@ -0,0 +1,4 @@
+export { default as RelatoriosMensais } from './RelatoriosMensais.vue';
+export { default as RelatoriosOrcamentarios } from './RelatoriosOrcamentarios.vue';
+export { default as RelatoriosRaiz } from './RelatoriosRaiz.vue';
+export { default as RelatoriosSemestraisOuAnuais } from './RelatoriosSemestraisOuAnuais.vue';
