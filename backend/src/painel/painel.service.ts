7532f1242de318989efe08585874de9b8cc515f1
lucasansei <lucas.ansei@eokoe.com>
2022-12-22 19:40:29 -0300

    Fix: check if indicador exists

--- backend/src/painel/painel.service.ts
+++ backend/src/painel/painel.service.ts
@@ -1303,7 +1303,7 @@ export class PainelService {
                 titulo: series.meta.titulo,
                 codigo: series.meta.codigo,
 
-                indicador: series.mostrar_indicador ? {
+                indicador: series.mostrar_indicador && series.meta.indicador ? {
                     id: series.meta.indicador[0].id,
                     codigo: series.meta.indicador[0].codigo,
                     titulo: series.meta.indicador[0].titulo,
