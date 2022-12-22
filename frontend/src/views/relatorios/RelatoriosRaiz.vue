00b14ffc52db0c3350da79705dca7c08a2c04ff4
sobral <robson@oficiodesign.com>
2022-12-22 14:22:52 -0300

    Carregar relatórios na específica

--- frontend/src/views/relatorios/RelatoriosRaiz.vue
+++ frontend/src/views/relatorios/RelatoriosRaiz.vue
@@ -1,13 +1,5 @@
 <script setup>
 import { Dashboard } from '@/components';
-import { useRelatoriosStore } from '@/stores';
-import { onMounted } from 'vue';
-
-const relatóriosStore = useRelatoriosStore();
-onMounted(() => {
-  relatóriosStore.clear();
-  relatóriosStore.getAll();
-});
 </script>
 <template>
   <Dashboard>
