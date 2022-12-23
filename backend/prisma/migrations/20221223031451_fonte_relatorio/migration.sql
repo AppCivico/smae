83550ac866001b9ef9ba4c374dddb0cf4c84a699
Renato CRON <renato.cron@gmail.com>
2022-12-23 14:05:48 -0300

    Add valor_indicador_em/valor_variavel_em em algum migration

--- backend/prisma/migrations/20221223031451_fonte_relatorio/migration.sql
+++ backend/prisma/migrations/20221223031451_fonte_relatorio/migration.sql
@@ -6,6 +6,50 @@
 */
 -- AlterEnum
 BEGIN;
+CREATE OR REPLACE FUNCTION valor_indicador_em(pIndicador_id int, pSerie "Serie", pPeriodo date, janela int)
+    RETURNS numeric(95, 60)
+    AS $$
+DECLARE
+    _valor numeric(95, 60);
+BEGIN
+    SELECT
+        si.valor_nominal into _valor
+    FROM
+        serie_indicador si
+    where si.indicador_id = pIndicador_id
+    and si.serie = pSerie
+    and si.data_valor <= pPeriodo
+    and si.data_valor > (pPeriodo - (janela || ' months')::interval)::date
+    and ha_conferencia_pendente = false
+    order by si.data_valor desc
+    LIMIT 1;
+    return _valor;
+END
+$$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION valor_variavel_em(pVariavelId int, pSerie "Serie", pPeriodo date, janela int)
+    RETURNS numeric(95, 60)
+    AS $$
+DECLARE
+    _valor numeric(95, 60);
+BEGIN
+    SELECT
+        si.valor_nominal into _valor
+    FROM
+        serie_variavel si
+    where si.variavel_id = pVariavelId
+    and si.serie = pSerie
+    and si.data_valor <= pPeriodo
+    and si.data_valor > (pPeriodo - (janela || ' months')::interval)::date
+    and conferida = true
+    order by si.data_valor desc
+    LIMIT 1;
+    return _valor;
+END
+$$
+LANGUAGE plpgsql;
+
 CREATE TYPE "FonteRelatorio_new" AS ENUM ('Orcamento', 'Indicadores', 'MonitoramentoMensal');
 ALTER TABLE "relatorio" ALTER COLUMN "fonte" TYPE "FonteRelatorio_new" USING ("fonte"::text::"FonteRelatorio_new");
 ALTER TYPE "FonteRelatorio" RENAME TO "FonteRelatorio_old";
