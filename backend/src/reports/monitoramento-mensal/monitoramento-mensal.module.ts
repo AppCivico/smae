14860a7dbcddc7f7b22b201eac82a2eb3264fdd2
Renato CRON <renato.cron@gmail.com>
2022-12-23 11:08:04 -0300

    WIP MF

--- backend/src/reports/monitoramento-mensal/monitoramento-mensal.module.ts
+++ backend/src/reports/monitoramento-mensal/monitoramento-mensal.module.ts
@@ -4,11 +4,16 @@ import { MonitoramentoMensalController } from './monitoramento-mensal.controller
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { UtilsService } from '../utils/utils.service';
 import { PainelModule } from 'src/painel/painel.module';
+import { MonitoramentoMensalMfService } from './monitoramento-mensal-mf.service';
 
 @Module({
     imports: [PrismaModule, PainelModule],
     controllers: [MonitoramentoMensalController],
-    providers: [MonitoramentoMensalService, UtilsService],
+    providers: [
+        MonitoramentoMensalService,
+        MonitoramentoMensalMfService,
+        UtilsService,
+    ],
     exports: [MonitoramentoMensalService],
 })
 export class MonitoramentoMensalModule { }
