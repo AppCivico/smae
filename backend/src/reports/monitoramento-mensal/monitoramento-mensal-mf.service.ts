2a2a3fb82da5ab5103e28057c521e0155e78cddd
Renato CRON <renato.cron@gmail.com>
2022-12-23 12:30:09 -0300

    Analise quali CSV

--- backend/src/reports/monitoramento-mensal/monitoramento-mensal-mf.service.ts
+++ backend/src/reports/monitoramento-mensal/monitoramento-mensal-mf.service.ts
@@ -4,11 +4,23 @@ import { MetasAnaliseQualiService } from 'src/mf/metas/metas-analise-quali.servi
 import { MetasFechamentoService } from 'src/mf/metas/metas-fechamento.service';
 import { MetasRiscoService } from 'src/mf/metas/metas-risco.service';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { FileOutput } from '../utils/utils.service';
+import { DefaultCsvOptions, FileOutput } from '../utils/utils.service';
 import { CreateRelMonitoramentoMensalDto } from './dto/create-monitoramento-mensal.dto';
 import { RelMfMetas, RetMonitoramentoFisico, RetMonitoramentoMensal } from './entities/monitoramento-mensal.entity';
 
+const { Parser, transforms: { flatten } } = require('json2csv');
+const defaultTransform = [flatten({ paths: [] })];
 
+class QualiCsv {
+    informacoes_complementares: string
+    referencia_data: string
+    criado_em: string
+    criador_nome_exibicao: string
+    meta_id: string
+    meta_titulo: string
+    meta_codigo: string
+    id: string
+}
 
 @Injectable()
 export class MonitoramentoMensalMfService {
@@ -76,8 +88,46 @@ export class MonitoramentoMensalMfService {
 
 
     async getFiles(myInput: RetMonitoramentoMensal, pdm: Pdm): Promise<FileOutput[]> {
+        const out: FileOutput[] = [];
 
-        return []
+        console.log(myInput);
+
+        if (!myInput.monitoramento_fisico || myInput.monitoramento_fisico.metas.length == 0) return [];
+
+        const qualiRows: QualiCsv[] = [];
+        for (const meta of myInput.monitoramento_fisico.metas) {
+
+            if (meta.analiseQuali) {
+                qualiRows.push({
+                    id: meta.analiseQuali.id.toString(),
+                    criador_nome_exibicao: meta.analiseQuali.criador.nome_exibicao,
+                    criado_em: meta.analiseQuali.criado_em.toString(),
+                    informacoes_complementares: meta.analiseQuali.informacoes_complementares,
+                    meta_codigo: meta.meta.codigo,
+                    meta_titulo: meta.meta.titulo,
+                    meta_id: meta.meta.id.toString(),
+                    referencia_data: meta.analiseQuali.referencia_data.toString()
+                })
+            }
+
+        }
+        console.log(qualiRows);
+
+        if (qualiRows.length) {
+            const json2csvParser = new Parser({
+                ...DefaultCsvOptions,
+                transforms: defaultTransform,
+                fields: undefined
+            });
+
+            const linhas = json2csvParser.parse(qualiRows);
+            out.push({
+                name: 'analises-qualitativas.csv',
+                buffer: Buffer.from(linhas, "utf8")
+            });
+        }
+
+        return out;
     }
 
 }
