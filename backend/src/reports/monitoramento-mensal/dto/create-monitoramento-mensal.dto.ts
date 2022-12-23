b12436ae254b35e40616e156c211f5b782082685
Renato CRON <renato.cron@gmail.com>
2022-12-23 10:12:59 -0300

    Tbm precisa do painel

--- backend/src/reports/monitoramento-mensal/dto/create-monitoramento-mensal.dto.ts
+++ backend/src/reports/monitoramento-mensal/dto/create-monitoramento-mensal.dto.ts
@@ -1,6 +1,6 @@
 import { IntersectionType, OmitType } from "@nestjs/swagger"
 import { Type } from "class-transformer"
-import { IsNumber, IsOptional } from "class-validator"
+import { ArrayMaxSize, ArrayMinSize, IsArray, IsInt, IsNumber, IsOptional } from "class-validator"
 import { FiltroMetasIniAtividadeDto } from "../../relatorios/dto/filtros.dto"
 
 export class RelMonitoramentoMensalParams {
@@ -9,14 +9,24 @@ export class RelMonitoramentoMensalParams {
     */
     @IsOptional()
     @Type(() => Number)
-    ano?: number
+    ano: number
 
     /** mes do ciclo
     * @example ""
    */
     @IsNumber()
     @Type(() => Number)
-    mes?: number
+    mes: number
+
+    /**
+     * quais paineis puxar os relatorios
+     * @example "[]"
+    */
+    @IsArray({ message: '$property| tag(s): precisa ser uma array.' })
+    @ArrayMinSize(1, { message: '$property| tag(s): precisa ter pelo menos um item' })
+    @ArrayMaxSize(100, { message: '$property| tag(s): precisa ter no máximo 100 items' })
+    @IsInt({ each: true, message: '$property| Cada item precisa ser um número inteiro' })
+    paineis: number[]
 }
 
 // excluindo o atividade/iniciativa pq sempre busca pela meta aqui
