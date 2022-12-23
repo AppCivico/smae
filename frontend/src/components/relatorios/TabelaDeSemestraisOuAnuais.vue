5e140dabb7670b7f3d4ed3bcd4b10f945ec62fc0
sobral <robson@oficiodesign.com>
2022-12-23 10:17:16 -0300

    Listar relatórios semestrais

---
+++ frontend/src/components/relatorios/TabelaDeSemestraisOuAnuais.vue
@@ -0,0 +1,87 @@
+<script setup>
+import dateToDate from '@/helpers/dateToDate';
+import { useAlertStore, useAuthStore, useRelatoriosStore } from '@/stores';
+import { storeToRefs } from 'pinia';
+
+const { temPermissãoPara } = storeToRefs(useAuthStore());
+
+const alertStore = useAlertStore();
+const relatoriosStore = useRelatoriosStore();
+
+const baseUrl = `${import.meta.env.VITE_API_URL}`;
+
+const localizeDate = (d) => dateToDate(d, { timeStyle: 'short' });
+
+function excluirRelatório(id) {
+  alertStore.confirmAction('Deseja remover o relatório?', () => {
+    relatoriosStore.delete(id);
+  }, 'Remover');
+}
+
+</script>
+<template>
+  <table class="tablemain">
+      <colgroup>
+        <col />
+        <col />
+        <col />
+        <!--col /-->
+        <col class="col--dataHora" />
+
+        <col v-if="temPermissãoPara(['Reports.remover'])" class="col--botão-de-ação" />
+        <!--col v-if="temPermissãoPara('Reports.executar')" class="col--botão-de-ação" /-->
+        <col class="col--botão-de-ação" />
+      </colgroup>
+      <thead>
+          <tr>
+              <th>ano</th>
+              <th>semestre</th>
+              <th>tipo</th>
+              <!--th>tags</th-->
+              <th>gerado em</th>
+              <th v-if="temPermissãoPara(['Reports.remover'])"></th>
+              <!--th v-if="temPermissãoPara('Reports.executar')"></th-->
+              <th></th>
+          </tr>
+      </thead>
+      <tbody>
+          <template v-if="relatoriosStore.relatorios.length">
+              <tr v-for="item in relatoriosStore.relatorios" :key="item.id">
+                  <td>{{ item.parametros.ano }}</td>
+                  <td>{{ item.parametros.semestre }}</td>
+                  <td>{{ item.parametros.tipo }}</td>
+                  <!--td>{{ item.parametros.tags }}</td-->
+                  <td>{{ localizeDate(item.criado_em) }}</td>
+                  <td v-if="temPermissãoPara(['Reports.remover'])">
+                    <button @click="excluirRelatório(item.id)" class="like-a__text" arial-label="excluir" title="excluir"><img
+                    src="../../assets/icons/excluir.svg" /></button>
+                  </td>
+                  <!--td v-if="temPermissãoPara('Reports.executar')">
+                    <button class="like-a__text" arial-label="duplicar" title="duplicar"><img
+                    src="../../assets/icons/duplicar.svg" /></button>
+                  </td-->
+                  <td>
+                    <a :href="`${baseUrl}/download/${item.arquivo}`"
+                    download title="baixar"><img
+                    src="../../assets/icons/baixar.svg" /></a>
+                  </td>
+              </tr>
+          </template>
+          <tr v-else-if="relatoriosStore.loading">
+              <td colspan="7" aria-busy="true">
+                  Carregando
+              </td>
+          </tr>
+          <tr v-else-if="relatoriosStore.error">
+              <td colspan="7">
+                  erro: {{ relatoriosStore.error }}
+              </td>
+          </tr>
+          <tr v-else>
+              <td colspan="7">
+                  Nenhum resultado encontrado.
+              </td>
+          </tr>
+      </tbody>
+  </table>
+</template>
--- frontend/src/views/relatorios/RelatoriosSemestraisOuAnuais.vue
+++ frontend/src/views/relatorios/RelatoriosSemestraisOuAnuais.vue
@@ -1,34 +1,32 @@
 <script setup>
-import { useAuthStore, useRelatoriosStore } from '@/stores';
-import { storeToRefs } from 'pinia';
-import { reactive, ref } from 'vue';
-
-const authStore = useAuthStore();
-const { permissions } = storeToRefs(authStore);
-const perm = permissions.value;
+import TabelaDeSemestraisOuAnuais from '@/components/relatorios/TabelaDeSemestraisOuAnuais.vue';
+import { useRelatoriosStore } from '@/stores';
+import { onMounted } from 'vue';
 import { useRoute } from 'vue-router';
 
+// const { temPermissãoPara } = storeToRefs(useAuthStore());
 const route = useRoute();
 
-const relatoriosStore = useRelatoriosStore();
-const { tempRelatoriosTypes } = storeToRefs(relatoriosStore);
-relatoriosStore.clear();
-relatoriosStore.filterRelatorios();
+const relatóriosStore = useRelatoriosStore();
 
-const filters = reactive({
-  textualSearch: ""
+onMounted(() => {
+  relatóriosStore.clear();
+  relatóriosStore.getAll({ fonte: 'Indicadores' });
 });
-let itemsFiltered = ref(tempRelatoriosTypes);
-
-function filterItems() {
-  relatoriosStore.filterRelatorios(filters);
-}
 </script>
 <template>
   <div class="flex spacebetween center mb2">
       <h1>{{ route.meta.título }}</h1>
       <hr class="ml2 f1"/>
-      <router-link to="./novo" class="btn big ml2" v-if="perm?.CadastroTipoRelatorioso?.inserir">Novo relatório</router-link>
+      <!--router-link :to="{ name: 'novoRelatórioOrçamentário' }" class="btn big ml2" v-if="temPermissãoPara('Reports.executar')">
+        Novo relatório
+      </router-link-->
   </div>
+  <!--div class="flex center mb2">
+      <div class="f2 search">
+          <input v-model="filters.textualSearch" @input="filterItems" placeholder="Buscar" type="text" class="inputtext" />
+      </div>
+  </div-->
 
+  <TabelaDeSemestraisOuAnuais />
 </template>
