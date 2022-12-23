6d22fd2e8f079a8663abe094a6635836396fc5d4
Renato CRON <renato.cron@gmail.com>
2022-12-23 10:52:29 -0300

    WIP do MM

--- backend/src/reports/monitoramento-mensal/monitoramento-mensal.controller.ts
+++ backend/src/reports/monitoramento-mensal/monitoramento-mensal.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
 import { Roles } from 'src/auth/decorators/roles.decorator';
-import { CreateRelMonitoramentsoMensalDto } from './dto/create-monitoramento-mensal.dto';
+import { CreateRelMonitoramentoMensalDto } from './dto/create-monitoramento-mensal.dto';
 import { RetMonitoramentoMensal } from './entities/monitoramento-mensal.entity';
 import { MonitoramentoMensalService } from './monitoramento-mensal.service';
 
@@ -15,7 +15,7 @@ export class MonitoramentoMensalController {
     @ApiBearerAuth('access-token')
     @ApiUnauthorizedResponse()
     @Roles('Reports.executar')
-    async create(@Body() createOrcamentoExecutadoDto: CreateRelMonitoramentsoMensalDto): Promise<RetMonitoramentoMensal> {
+    async create(@Body() createOrcamentoExecutadoDto: CreateRelMonitoramentoMensalDto): Promise<RetMonitoramentoMensal> {
         return await this.monitoramentoMensalService.create(createOrcamentoExecutadoDto);
     }
 }
