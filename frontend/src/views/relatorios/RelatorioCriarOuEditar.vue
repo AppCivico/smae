3804ca0b80476c843b1e941a1c17d313647e7936
sobral <robson@oficiodesign.com>
2022-12-24 15:32:56 -0300

    Converter `checkClose` em um componente e...
    ... corrigir atribuição automatática de rota mãe

--- frontend/src/views/relatorios/RelatorioCriarOuEditar.vue
+++ frontend/src/views/relatorios/RelatorioCriarOuEditar.vue
@@ -1,8 +1,9 @@
 <script setup>
-import { useAlertStore, useRelatoriosStore } from '@/stores';
+import { useRelatoriosStore } from '@/stores';
 import { storeToRefs } from 'pinia';
 import { onMounted } from 'vue';
 import { useRoute } from 'vue-router';
+import CheckClose from '../../components/CheckClose.vue';
 import NovoMensal from '../../components/relatorios/NovoMensal.vue';
 import NovoOrcamentario from '../../components/relatorios/NovoOrcamentario.vue';
 import NovoSemestralOuAnual from '../../components/relatorios/NovoSemestralOuAnual.vue';
@@ -18,21 +19,14 @@ const formulários = {
 onMounted(() => {
   current = {};
 });
-
-const alertStore = useAlertStore();
-
 const route = useRoute();
-
-async function checkClose() {
-  alertStore.confirm('Deseja sair sem salvar as alterações?', route.meta?.rotaDeEscape || route.matched?.[0].path);
-}
 </script>
 
 <template>
     <div class="flex spacebetween center mb2">
         <h1>{{ route.meta.título || route.name }}</h1>
         <hr class="ml2 f1"/>
-        <button @click="checkClose" class="btn round ml2"><svg width="12" height="12"><use xlink:href="#i_x"></use></svg></button>
+        <CheckClose />
     </div>
 
     <component :is="formulários[route.name]" />
