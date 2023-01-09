9a36ba2123c2606d8ca509a337930eda6d0548e9
sobral <robson@oficiodesign.com>
2023-01-09 14:31:50 -0300

    Set AirBNB ESLint

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,25 +1,24 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-const htmlPlugin = () => {
-    return {
-        name: 'html-transform',
-        transformIndexHtml(html) {
-            return html.replace(
-                /__FORMULA_JS_SCRIPT__/,
-                `<script type="text/javascript" src="${process.env.FORMULA_JS_URL || '/api/public/js/formula_parser.js'}"></script>`
-            )
-        }
-    }
-}
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
+
+const htmlPlugin = () => ({
+  name: 'html-transform',
+  transformIndexHtml(html) {
+    return html.replace(
+      /__FORMULA_JS_SCRIPT__/,
+      `<script type="text/javascript" src="${process.env.FORMULA_JS_URL || '/api/public/js/formula_parser.js'}"></script>`,
+    );
+  },
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [htmlPlugin(), vue()],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
-    }
-})
+  plugins: [htmlPlugin(), vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
