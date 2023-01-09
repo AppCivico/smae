00310b767e3e65a65abfafdbcf62d5e71a8fc33f
Sobral <robson@oficiodesign.com>
2023-01-09 16:17:36 -0300

    Formatar código

--- frontend/src/components/MenuSecundario.vue
+++ frontend/src/components/MenuSecundario.vue
@@ -9,21 +9,34 @@ const { temPermissãoPara } = storeToRefs(authStore);
 const route = useRoute();
 
 const subMenu = route.matched?.[0]?.children
-  .filter((x) => x.meta?.presenteNoMenu)
-  .filter((x) => x.meta?.restringirÀsPermissões === undefined || temPermissãoPara(x.meta?.restringirÀsPermissões)) || [];
+  .filter((x) => {
+    const { presenteNoMenu, restringirÀsPermissões } = x.meta || {};
+
+    return presenteNoMenu
+      && (restringirÀsPermissões === undefined || temPermissãoPara(restringirÀsPermissões));
+  }) || [];
 const raizDoSubmenu = route.matched?.[0]?.path;
 </script>
 <template>
-    <div id="submenu">
-        <div v-if="route.meta.usarMigalhasDePão" class="breadcrumbmenu"><!--
-        TODO --></div>
-
-        <div class="subpadding">
-            <div class="links-container mb2">
-                <router-link v-for="item, k in subMenu" :to="`${raizDoSubmenu}/${item.path}`" :key="k">
-                  {{ item.meta?.títuloParaMenu || item.meta?.título || item.name }}
-                </router-link>
-            </div>
-        </div>
+  <div id="submenu">
+    <div
+      v-if="route.meta.usarMigalhasDePão"
+      class="breadcrumbmenu"
+    >
+      <!--
+        TODO -->
     </div>
+
+    <div class="subpadding">
+      <div class="links-container mb2">
+        <router-link
+          v-for="item, k in subMenu"
+          :key="k"
+          :to="`${raizDoSubmenu}/${item.path}`"
+        >
+          {{ item.meta?.títuloParaMenu || item.meta?.título || item.name }}
+        </router-link>
+      </div>
+    </div>
+  </div>
 </template>
