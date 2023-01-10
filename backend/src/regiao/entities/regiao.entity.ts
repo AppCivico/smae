b48491ee615dc107050367d6afe6d0a3cf9d027f
lucasansei <lucas.ansei@eokoe.com>
2023-01-10 16:50:43 -0300

    Add report for region

--- backend/src/regiao/entities/regiao.entity.ts
+++ backend/src/regiao/entities/regiao.entity.ts
@@ -1,8 +1,12 @@
+import { OmitType, PartialType } from '@nestjs/swagger';
+
 export class Regiao {
     id: number
     codigo: number | null
     descricao: string
     nivel: number
     parente_id: number | null
+    parente?: RegiaoPai
 }
 
+export class RegiaoPai extends PartialType(OmitType(Regiao, ['parente_id', 'parente'])) { }
