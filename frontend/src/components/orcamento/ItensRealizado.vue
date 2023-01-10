51da0b968f1b2ad8df2d528305f685c8deda2f19
Sobral <robson@oficiodesign.com>
2023-01-09 21:05:06 -0300

    Format code

--- frontend/src/components/orcamento/ItensRealizado.vue
+++ frontend/src/components/orcamento/ItensRealizado.vue
@@ -1,22 +1,23 @@
 <script setup>
 import toFloat from '@/helpers/toFloat';
 import { onMounted, onUpdated, ref } from 'vue';
+
 const props = defineProps(['controlador', 'respostasof']);
-let itens = ref(props.controlador)
+const itens = ref(props.controlador);
 
 function start() {
   itens.value = props.controlador;
 }
 start();
-onMounted(() => { start() });
-onUpdated(() => { start() });
+onMounted(() => { start(); });
+onUpdated(() => { start(); });
 
 function maskFloat(el) {
   el.target.value = dinheiro(Number(el.target.value.replace(/[\D]/g, '')) / 100);
   if (el.target?._vei?.onChange) el.target?._vei?.onChange(el);
 }
 function dinheiro(v) {
-  return new Intl.NumberFormat('pt-BR', { minimumFractionDigits: 2 }).format(Number(v))
+  return new Intl.NumberFormat('pt-BR', { minimumFractionDigits: 2 }).format(Number(v));
 }
 function removeItem(g, i) {
   g = g.splice(i, 1);
@@ -30,57 +31,99 @@ function attVar(g, i, n) {
 
 </script>
 <template>
-    <hr class="mt2 mb2">
-    <div class="flex g2">
-      <div class="f1">
-            <label class="label">Mês Ref. <span class="tvermelho">*</span></label>
-        </div>
-        <div class="f1">
-            <label class="label">Valor empenho <span class="tvermelho">*</span></label>
-        </div>
-        <div class="f1">
-            <label class="label">Valor liquidação <span class="tvermelho">*</span></label>
-        </div>
-        <div style="flex-basis: 30px;">
-        </div>
+  <hr class="mt2 mb2">
+  <div class="flex g2">
+    <div class="f1">
+      <label class="label">Mês Ref. <span class="tvermelho">*</span></label>
     </div>
-    <div class="flex center g2 mb1" v-for="(item, i) in itens" :key="i">
-        <div class="f1">
-            <input v-model="item.mes" type="number" min="1" max="12" step="1" class="inputtext light"/>
-        </div>
-        <div class="f1">
-            <input v-model="item.valor_empenho" @keyup="maskFloat" @change="attVar(itens, i, 'valor_empenho')" type="text" class="inputtext light"/>
-        </div>
-        <div class="f1">
-            <input v-model="item.valor_liquidado" @keyup="maskFloat" @change="attVar(itens, i, 'valor_liquidado')" type="text" class="inputtext light"/>
-        </div>
-        <div style="flex-basis: 30px;">
-            <a @click="removeItem(itens, i)" class="addlink"><svg width="20" height="20"><use xlink:href="#i_remove"></use></svg></a>
-        </div>
+    <div class="f1">
+      <label class="label">Valor empenho <span class="tvermelho">*</span></label>
     </div>
-    <div class="tc mb2">
-      <a @click="addItem(itens)" class="btn outline bgnone tcprimary">Adicionar novo empenho/liquidação</a>
+    <div class="f1">
+      <label class="label">Valor liquidação <span class="tvermelho">*</span></label>
     </div>
-    <div class="flex g2 mb2">
-      <div class="f1">
+    <div style="flex-basis: 30px;" />
+  </div>
+  <div
+    v-for="(item, i) in itens"
+    :key="i"
+    class="flex center g2 mb1"
+  >
+    <div class="f1">
+      <input
+        v-model="item.mes"
+        type="number"
+        min="1"
+        max="12"
+        step="1"
+        class="inputtext light"
+      >
+    </div>
+    <div class="f1">
+      <input
+        v-model="item.valor_empenho"
+        type="text"
+        class="inputtext light"
+        @keyup="maskFloat"
+        @change="attVar(itens, i, 'valor_empenho')"
+      >
+    </div>
+    <div class="f1">
+      <input
+        v-model="item.valor_liquidado"
+        type="text"
+        class="inputtext light"
+        @keyup="maskFloat"
+        @change="attVar(itens, i, 'valor_liquidado')"
+      >
+    </div>
+    <div style="flex-basis: 30px;">
+      <a
+        class="addlink"
+        @click="removeItem(itens, i)"
+      ><svg
+        width="20"
+        height="20"
+      ><use xlink:href="#i_remove" /></svg></a>
+    </div>
+  </div>
+  <div class="tc mb2">
+    <a
+      class="btn outline bgnone tcprimary"
+      @click="addItem(itens)"
+    >Adicionar novo empenho/liquidação</a>
+  </div>
+  <div class="flex g2 mb2">
+    <div class="f1" />
+    <div class="f1">
+      <div
+        v-if="respostasof.smae_soma_valor_empenho != undefined"
+        class="flex center flexwrap"
+      >
+        <span class="label mb0 tc300 mr1">Total Empenho SMAE</span>
+        {{ (somaitens = itens.reduce((r, x) => r + toFloat(x.valor_empenho), 0)) ? '' : '' }}
+        {{ (somatotal = toFloat(respostasof.smae_soma_valor_empenho) + somaitens) ? '' : '' }}
+        <span class="t14">R$ {{ dinheiro(somatotal) }}</span>
+        <span
+          v-if="somatotal > toFloat(respostasof.empenho_liquido)"
+          class="tvermelho w700 block"
+        >(valor supera empenho SOF)</span>
       </div>
-      <div class="f1">
-        <div class="flex center flexwrap" v-if="respostasof.smae_soma_valor_empenho != undefined">
-          <span class="label mb0 tc300 mr1">Total Empenho SMAE</span>
-          {{ (somaitens = itens.reduce((r, x) => r + toFloat(x.valor_empenho), 0)) ? '' : '' }}
-          {{ (somatotal = toFloat(respostasof.smae_soma_valor_empenho) + somaitens) ? '' : '' }}
-          <span class="t14">R$ {{ dinheiro(somatotal) }}</span>
-          <span v-if="somatotal > toFloat(respostasof.empenho_liquido)" class="tvermelho w700 block">(valor supera empenho SOF)</span>
-        </div>
-      </div>
-      <div class="f1">
-        <div class="flex center flexwrap" v-if="respostasof.smae_soma_valor_liquidado != undefined">
-          <span class="label mb0 tc300 mr1">Total liquidação SMAE</span>
-          {{ (somaitens = itens.reduce((r, x) => r + toFloat(x.valor_liquidado), 0)) ? '' : '' }}
-          {{ (somatotal = toFloat(respostasof.smae_soma_valor_liquidado) + somaitens) ? '' : '' }}
-          <span class="t14">R$ {{ dinheiro(somatotal) }}</span>
-          <span v-if="somatotal > toFloat(respostasof.valor_liquidado)" class="tvermelho w700 block">(valor supera liquidação SOF)</span>
-        </div>
+    </div>
+    <div class="f1">
+      <div
+        v-if="respostasof.smae_soma_valor_liquidado != undefined"
+        class="flex center flexwrap"
+      >
+        <span class="label mb0 tc300 mr1">Total liquidação SMAE</span>
+        {{ (somaitens = itens.reduce((r, x) => r + toFloat(x.valor_liquidado), 0)) ? '' : '' }}
+        {{ (somatotal = toFloat(respostasof.smae_soma_valor_liquidado) + somaitens) ? '' : '' }}
+        <span class="t14">R$ {{ dinheiro(somatotal) }}</span>
+        <span
+          v-if="somatotal > toFloat(respostasof.valor_liquidado)"
+          class="tvermelho w700 block"
+        >(valor supera liquidação SOF)</span>
       </div>
     </div>
+  </div>
 </template>
--- frontend/src/stores/relatorios.store.js
+++ frontend/src/stores/relatorios.store.js
@@ -3,42 +3,42 @@ import { defineStore } from 'pinia';
 
 const baseUrl = `${import.meta.env.VITE_API_URL}`;
 
-export const useRelatoriosStore = defineStore('relatorios', {
-    state: () => ({
-        relatorios: [],
-        current: {
-        },
-        loading: true,
-        error: false,
-    }),
-    actions: {
-        clear (){
-            this.relatorios = {};
-        },
-        async getAll(params = {}) {
-            this.loading = true;
-            try {
-              let r = await requestS.get(`${baseUrl}/relatorios`, params);
-                this.relatorios = r.linhas;
-            } catch (error) {
-                this.error = error;
-            }
-        },
-        async getById(id) {
-        },
-        async insert(params) {
-            if(await requestS.post(`${baseUrl}/relatorios`, params)) return true;
-            return false;
-        },
+export default defineStore('relatorios', {
+  state: () => ({
+    relatorios: [],
+    current: {
+    },
+    loading: true,
+    error: false,
+  }),
+  actions: {
+    clear() {
+      this.relatorios = {};
+    },
+    async getAll(params = {}) {
+      this.loading = true;
+      try {
+        const r = await requestS.get(`${baseUrl}/relatorios`, params);
+        this.relatorios = r.linhas;
+      } catch (error) {
+        this.error = error;
+      }
+    },
+    async getById(id) {
+    },
+    async insert(params) {
+      if (await requestS.post(`${baseUrl}/relatorios`, params)) return true;
+      return false;
+    },
 
-        async update(id, params) {
-        },
-        async delete(id) {
-            if(await requestS.delete(`${baseUrl}/relatorios/${id}`)) {
-              this.relatorios = this.relatorios.filter(x => x.id != id);
-              return true;
-            }
-            return false;
-        },
-    }
+    async update(id, params) {
+    },
+    async delete(id) {
+      if (await requestS.delete(`${baseUrl}/relatorios/${id}`)) {
+        this.relatorios = this.relatorios.filter((x) => x.id != id);
+        return true;
+      }
+      return false;
+    },
+  },
 });
