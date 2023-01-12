7f48ee882d55ba4641b7497f50a23908daf29b51
Sobral <robson@oficiodesign.com>
2023-01-12 11:01:33 -0300

    Formatar código

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,15 @@
 <script setup>
 import { Alert, EditModal, SideBar } from '@/components';
 import { useRoute } from 'vue-router';
+
 const route = useRoute();
 </script>
 
 <template>
-    <router-view :key="route.fullPath" />
-    <SideBar />
-    <EditModal />
-    <Alert />
+  <router-view :key="route.fullPath" />
+  <SideBar />
+  <EditModal />
+  <Alert />
 </template>
 
 <style lang="less">
--- frontend/src/stores/ciclos.store.js
+++ frontend/src/stores/ciclos.store.js
@@ -35,7 +35,9 @@ export const useCiclosStore = defineStore({
     fieldToDate(d) {
       if (d) {
         const x = d.split('/');
-        return (x.length == 3) ? new Date(Date.UTC(x[2], x[1] - 1, x[0])).toISOString().substring(0, 10) : null;
+        return (x.length == 3)
+          ? new Date(Date.UTC(x[2], x[1] - 1, x[0])).toISOString().substring(0, 10)
+          : null;
       }
       return null;
     },
