048282462f063cab6bc5d26ea38d0311a32b398b
Sobral <robson@oficiodesign.com>
2023-01-12 11:01:33 -0300

    Formatar código

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,15 +1,16 @@
 import { createPinia } from 'pinia';
 import { createApp } from 'vue';
+import { useRoute } from 'vue-router';
 import App from './App.vue';
 import { router } from './router';
-import { useRoute } from 'vue-router';
 
 const app = createApp(App);
 const pinia = createPinia();
 
 pinia.use(({ store }) => {
+  // eslint-disable-next-line no-param-reassign
   store.route = useRoute();
-})
+});
 
 app.use(pinia);
 app.use(router);
