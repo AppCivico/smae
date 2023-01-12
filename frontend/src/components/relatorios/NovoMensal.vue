ba3aedefbe9a853afcc8f12710358bdd0076b075
Sobral <robson@oficiodesign.com>
2023-01-12 14:20:57 -0300

    Formatar código

--- frontend/src/components/relatorios/NovoMensal.vue
+++ frontend/src/components/relatorios/NovoMensal.vue
@@ -1,14 +1,15 @@
 <script setup>
 // import AutocompleteField from '@/components/AutocompleteField.vue';
-import { relatórioMensal as schema } from "@/consts/formSchemas";
+import { relatórioMensal as schema } from '@/consts/formSchemas';
+import months from '@/consts/months';
 import monthAndYearToDate from '@/helpers/monthAndYearToDate';
 import { router } from '@/router';
-import { useAlertStore, usePaineisStore, usePdMStore, useRelatoriosStore, useTagsStore } from '@/stores';
+import {
+  useAlertStore, usePaineisStore, usePdMStore, useRelatoriosStore, useTagsStore
+} from '@/stores';
 import { storeToRefs } from 'pinia';
 import { Field, Form } from 'vee-validate';
 import { onMounted } from 'vue';
-import months from '../../consts/months';
-// import { ref } from 'vue';
 import { useRoute } from 'vue-router';
 
 const TagsStore = useTagsStore();
@@ -21,7 +22,7 @@ const relatoriosStore = useRelatoriosStore();
 const route = useRoute();
 const { current } = storeToRefs(relatoriosStore);
 
-let { loading } = storeToRefs(relatoriosStore);
+const { loading } = storeToRefs(relatoriosStore);
 
 current.value = {
   fonte: 'MonitoramentoMensal',
@@ -33,15 +34,12 @@ current.value = {
     paineis: [],
   },
   salvar_arquivo: false,
-}
-
-// const tags = ref({ participantes: [], busca: '' });
-// const paineis = ref({ participantes: [], busca: '' });
+};
 
 async function onSubmit(values) {
   try {
-    var msg;
-    var r;
+    let msg;
+    let r;
 
     values.parametros.inicio = monthAndYearToDate(values.parametros.inicio);
     values.parametros.fim = monthAndYearToDate(values.parametros.fim);
@@ -80,120 +78,227 @@ onMounted(() => {
 </script>
 
 <template>
-  <Form @submit="onSubmit" :validation-schema="schema" :initial-values="current" v-slot="{ errors, isSubmitting, values }">
-      <div class="flex g2 mb2">
-        <div class="f1">
-            <label class="label"><abbr title="Programa de metas">PdM</abbr> <span class="tvermelho">*</span></label>
-            <Field name="parametros.pdm_id" as="select" class="inputtext light
-            mb1" :class="{ 'error': errors['parametros.pdm_id'] }" :disabled="loading" v-model="current.parametros.pdm_id">
-                <option value="">Selecionar</option>
-                <option v-for="item in PdMStore.PdM" :value="item.id"
-                :key="item.id">{{ item.nome }}</option>
-            </Field>
-            <div class="error-msg">{{ errors['parametros.pdm_id'] }}</div>
-        </div>
-        <div class="f1">
-            <label for="mes" class="label">mes <span class="tvermelho">*</span></label>
-            <Field name="parametros.mes" as="select" class="inputtext light
-            mb1" :class="{ 'error': errors['parametros.mes'] }" :disabled="loading" v-model="current.parametros.mes">
-                <option value="">Selecionar</option>
-                <option v-for="item, k in months" :value="k + 1"
-                :key="k">{{ item }}</option>
-            </Field>
-            <div class="error-msg">{{ errors['parametros.mes'] }}</div>
-        </div>
-        <div class="f1">
-            <label for="ano" class="label">ano <span class="tvermelho">*</span></label>
-            <Field placeholder="2003" name="parametros.ano" id="ano" type="number" class="inputtext light mb1"
-            :class="{ 'error': errors['parametro.ano'] }" min="2003" />
-            <div class="error-msg">{{ errors['parametros.ano'] }}</div>
+  <Form
+    v-slot="{ errors, isSubmitting, values }"
+    :validation-schema="schema"
+    :initial-values="current"
+    @submit="onSubmit"
+  >
+    <div class="flex g2 mb2">
+      <div class="f1">
+        <label class="label">
+          <abbr title="Programa de metas">PdM</abbr>
+          <span class="tvermelho">*</span>
+        </label>
+        <Field
+          v-model="current.parametros.pdm_id"
+          name="parametros.pdm_id"
+          as="select"
+          class="inputtext light mb1"
+          :class="{ 'error': errors['parametros.pdm_id'] }"
+          :disabled="loading"
+        >
+          <option value="">
+            Selecionar
+          </option>
+          <option
+            v-for="item in PdMStore.PdM"
+            :key="item.id"
+            :value="item.id"
+          >
+            {{ item.nome }}
+          </option>
+        </Field>
+        <div class="error-msg">
+          {{ errors['parametros.pdm_id'] }}
         </div>
       </div>
-
-      <div class="mb2">
-          <div class="pl2">
-            <label class="block mb1">
-              <Field name="parametros.tipo" type="radio" value="Consolidado"
-              class="inputcheckbox" :class="{ 'error': errors['parametros.tipo'] }" />
-              <span>Consolidado</span>
-            </label>
-            <label class="block mb1">
-              <Field name="parametros.tipo" type="radio" value="Analitico"
-              class="inputcheckbox" :class="{ 'error': errors['parametros.tipo'] }" />
-              <span>Analítico</span>
-            </label>
-          </div>
-          <div class="error-msg">{{ errors['parametros.tipo'] }}</div>
+      <div class="f1">
+        <label
+          for="mes"
+          class="label"
+        >
+          mês <span class="tvermelho">*</span>
+        </label>
+        <Field
+          v-model="current.parametros.mes"
+          name="parametros.mes"
+          as="select"
+          class="inputtext light mb1"
+          :class="{ 'error': errors['parametros.mes'] }"
+          :disabled="loading"
+        >
+          <option value="">
+            Selecionar
+          </option>
+          <option
+            v-for="item, k in months"
+            :key="k"
+            :value="k + 1"
+          >
+            {{ item }}
+          </option>
+        </Field>
+        <div class="error-msg">
+          {{ errors['parametros.mes'] }}
+        </div>
       </div>
+      <div class="f1">
+        <label
+          for="ano"
+          class="label"
+        >
+          ano <span class="tvermelho">*</span>
+        </label>
+        <Field
+          id="ano"
+          placeholder="2003"
+          name="parametros.ano"
+          type="number"
+          class="inputtext light mb1"
+          :class="{ 'error': errors['parametro.ano'] }"
+          min="2003"
+        />
+        <div class="error-msg">
+          {{ errors['parametros.ano'] }}
+        </div>
+      </div>
+    </div>
 
-      <!--div class="mb2" v-if="filtradasPorPdM(values.parametros.pdm_id).length">
+    <div class="mb2">
+      <div class="pl2">
+        <label class="block mb1">
+          <Field
+            name="parametros.tipo"
+            type="radio"
+            value="Consolidado"
+            class="inputcheckbox"
+            :class="{ 'error': errors['parametros.tipo'] }"
+          />
+          <span>Consolidado</span>
+        </label>
+        <label class="block mb1">
+          <Field
+            name="parametros.tipo"
+            type="radio"
+            value="Analitico"
+            class="inputcheckbox"
+            :class="{ 'error': errors['parametros.tipo'] }"
+          />
+          <span>Analítico</span>
+        </label>
+      </div>
+      <div class="error-msg">
+        {{ errors['parametros.tipo'] }}
+      </div>
+    </div>
+
+    <!--div class="mb2" v-if="filtradasPorPdM(values.parametros.pdm_id).length">
           <div class="pl2">
             <label class="label">Tags</label>
             <AutocompleteField :controlador="tags" :grupo="filtradasPorPdM(values.parametros.pdm_id)" label="descricao" />
           </div>
       </div-->
 
-      <div class="mb2" v-if="filtradasPorPdM(values.parametros.pdm_id)?.length">
-          <div class="label">Tags</div>
-          <template v-if="filtradasPorPdM(values.parametros.pdm_id)?.loading">
-              <span class="spinner">Carregando</span>
-          </template>
-          <template v-if="filtradasPorPdM(values.parametros.pdm_id).length">
-              <label v-for="item in filtradasPorPdM(values.parametros.pdm_id)" :key="item.id" class="block mb1">
-                  <Field name="parametros.tags" class="inputcheckbox" type="checkbox"
-                      :class="{ 'error': errors['parametros.tags'] }"
-                      :value="item.id"
-                      :checked="values.parametros.tags && values.parametros.tags.includes(item.id)"
-                  /><span>{{ item.descricao }}</span>
-              </label>
-              <div class="error-msg">{{ errors['parametros.tags'] }}</div>
-          </template>
+    <div
+      v-if="filtradasPorPdM(values.parametros.pdm_id)?.length"
+      class="mb2"
+    >
+      <div class="label">
+        Tags
       </div>
+      <template v-if="filtradasPorPdM(values.parametros.pdm_id)?.loading">
+        <span class="spinner">Carregando</span>
+      </template>
+      <template v-if="filtradasPorPdM(values.parametros.pdm_id).length">
+        <label
+          v-for="item in filtradasPorPdM(values.parametros.pdm_id)"
+          :key="item.id"
+          class="block mb1"
+        >
+          <Field
+            name="parametros.tags"
+            class="inputcheckbox"
+            type="checkbox"
+            :class="{ 'error': errors['parametros.tags'] }"
+            :value="item.id"
+            :checked="values.parametros.tags && values.parametros.tags.includes(item.id)"
+          /><span>{{ item.descricao }}</span>
+        </label>
+        <div class="error-msg">
+          {{ errors['parametros.tags'] }}
+        </div>
+      </template>
+    </div>
 
-      <!--div class="mb2" v-if="PainéisStore.Paineis?.length">
+    <!--div class="mb2" v-if="PainéisStore.Paineis?.length">
           <div class="pl2">
             <label class="label">Painéis</label>
             <AutocompleteField :controlador="paineis" :grupo="PainéisStore.Paineis" label="descricao" />
           </div>
       </div-->
 
-      <div class="mb2" v-if="PainéisStore.Paineis?.length">
-          <div class="label">Painéis</div>
-          <template v-if="PainéisStore.Paineis?.loading">
-              <span class="spinner">Carregando</span>
-          </template>
-          <template v-if="PainéisStore.Paineis.length">
-              <label v-for="item in PainéisStore.Paineis" :key="item.id" class="block mb1">
-                  <Field name="parametros.paineis" class="inputcheckbox" type="checkbox"
-                      :class="{ 'error': errors['parametros.paineis'] }"
-                      :value="item.id"
-                      :checked="values.parametros.paineis && values.parametros.paineis.includes(item.id)"
-                  /><span>{{ item.nome }}</span>
-              </label>
-              <div class="error-msg">{{ errors['parametros.paineis'] }}</div>
-          </template>
+    <div
+      v-if="PainéisStore.Paineis?.length"
+      class="mb2"
+    >
+      <div class="label">
+        Painéis
       </div>
+      <template v-if="PainéisStore.Paineis?.loading">
+        <span class="spinner">Carregando</span>
+      </template>
+      <template v-if="PainéisStore.Paineis.length">
+        <label
+          v-for="item in PainéisStore.Paineis"
+          :key="item.id"
+          class="block mb1"
+        >
+          <Field
+            name="parametros.paineis"
+            class="inputcheckbox"
+            type="checkbox"
+            :class="{ 'error': errors['parametros.paineis'] }"
+            :value="item.id"
+            :checked="values.parametros.paineis && values.parametros.paineis.includes(item.id)"
+          /><span>{{ item.nome }}</span>
+        </label>
+        <div class="error-msg">
+          {{ errors['parametros.paineis'] }}
+        </div>
+      </template>
+    </div>
 
-      <hr />
+    <hr>
 
-      <div class="mb2 mt2">
-          <div class="pl2">
-            <label class="block">
-                <Field name="salvar_arquivo" type="checkbox"
-                :value="true" class="inputcheckbox" />
-                <span :class="{ 'error': errors.salvar_arquivo }">Salvar relatório no sistema</span>
-            </label>
-          </div>
-        <div class="error-msg">{{ errors.salvar_arquivo }}</div>
+    <div class="mb2 mt2">
+      <div class="pl2">
+        <label class="block">
+          <Field
+            name="salvar_arquivo"
+            type="checkbox"
+            :value="true"
+            class="inputcheckbox"
+          />
+          <span :class="{ 'error': errors.salvar_arquivo }">Salvar relatório no sistema</span>
+        </label>
       </div>
-
-      <div class="flex spacebetween center mb2">
-          <hr class="mr2 f1"/>
-          <button type="submit" class="btn big" :disabled="loading ||
-          isSubmitting">
-            {{ values.salvar_arquivo ? "baixar e salvar" : "apenas baixar" }}
-          </button>
-          <hr class="ml2 f1"/>
+      <div class="error-msg">
+        {{ errors.salvar_arquivo }}
       </div>
+    </div>
+
+    <div class="flex spacebetween center mb2">
+      <hr class="mr2 f1">
+      <button
+        type="submit"
+        class="btn big"
+        :disabled="loading ||
+          isSubmitting"
+      >
+        {{ values.salvar_arquivo ? "baixar e salvar" : "apenas baixar" }}
+      </button>
+      <hr class="ml2 f1">
+    </div>
   </Form>
 </template>
