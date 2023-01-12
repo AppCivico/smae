cc7070d261ac7504765c4760142acd6f134392e7
Renato Cron <renato.cron@gmail.com>
2023-01-12 10:47:00 -0300

    Add sql

--- backend/prisma/migrations/20230112132423_mes_corrente/migration.sql
+++ backend/prisma/migrations/20230112132423_mes_corrente/migration.sql
@@ -1,2 +1,3 @@
 -- AlterTable
 ALTER TABLE "orcamento_realizado_item" ADD COLUMN     "mes_corrente" BOOLEAN NOT NULL DEFAULT false;
+update orcamento_realizado_item set mes_corrente=true where (orcamento_realizado_id, mes) in ( select orcamento_realizado_id, max(mes)  from orcamento_realizado_item a join orcamento_realizado b on orcamento_realizado_id = b.id where  sobrescrito_em is null and removido_em is null group by 1);
