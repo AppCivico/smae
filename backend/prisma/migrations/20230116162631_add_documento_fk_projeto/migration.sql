d3a24e0f9c400b0d9ce42efb40c66cc8274d4749
Renato Cron <renato.cron@gmail.com>
2023-01-17 09:56:01 -0300

    Removendo migrations (too early)

--- backend/prisma/migrations/20230116162631_add_documento_fk_projeto/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "projeto" ADD COLUMN     "status" TEXT;
-
--- AddForeignKey
-ALTER TABLE "projeto_documento" ADD CONSTRAINT "projeto_documento_documento_id_fkey" FOREIGN KEY ("documento_id") REFERENCES "arquivo"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
