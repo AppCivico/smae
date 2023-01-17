624521006966836a823b38bf3f1ebd5c532c065d
Sobral <robson@oficiodesign.com>
2023-01-17 13:26:21 -0300

    Extrair configurações e auxiliares

--- frontend/src/helpers/fieldToDate.js
+++ frontend/src/helpers/fieldToDate.js
@@ -1,6 +1,18 @@
-export default function fieldToDate(d = '') {
-  const x = d.split('/');
-  return (x.length === 3)
-    ? new Date(Date.UTC(x[2], x[1] - 1, x[0])).toISOString().substring(0, 10)
-    : null;
-}
+export default (d = '') => {
+  if (d) {
+    let d1 = d;
+
+    if (d.length === 6) {
+      d1 = `01/0${d}`;
+    } else if (d.length === 7) {
+      d1 = `01/${d}`;
+    }
+
+    const x = d1.split('/');
+
+    return (x.length === 3)
+      ? new Date(Date.UTC(x[2], x[1] - 1, x[0])).toISOString().substring(0, 10)
+      : null;
+  }
+  return null;
+};
