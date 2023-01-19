079c8731dd112de355f83b35b0ed7c135ad81e61
Sobral <robson@oficiodesign.com>
2023-01-19 13:27:06 -0300

    Listar valores mensais para preenchimento em ordem crescente anual

--- frontend/src/components/metas/GruposDeSerie.vue
+++ frontend/src/components/metas/GruposDeSerie.vue
@@ -6,7 +6,7 @@ function nestLinhas(l) {
     if (!a[x.agrupador]) a[x.agrupador] = [];
     a[x.agrupador].push(x);
   });
-  return Object.entries(a).reverse();
+  return Object.entries(a)
 }
 function toggleAccordeon(t) {
   t.target.closest('.tzaccordeon').classList.toggle('active');
