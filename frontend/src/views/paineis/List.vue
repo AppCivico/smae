ef0f14e6b97a37b1a0e4618d5ac6a6faa8e1deca
Sobral <robson@oficiodesign.com>
2023-01-19 14:03:06 -0300

    Formatar código

--- frontend/src/views/paineis/List.vue
+++ frontend/src/views/paineis/List.vue
@@ -1,82 +1,114 @@
 <script setup>
-    import { ref, reactive } from 'vue';
-    import { storeToRefs } from 'pinia';
-    import { Dashboard} from '@/components';
-    import { useAuthStore, usePaineisStore, useMetasStore } from '@/stores';
+import { Dashboard } from '@/components';
+import { useAuthStore, useMetasStore, usePaineisStore } from '@/stores';
+import { storeToRefs } from 'pinia';
+import { ref } from 'vue';
 
-    const authStore = useAuthStore();
-    const { permissions } = storeToRefs(authStore);
-    const perm = permissions.value;
+const authStore = useAuthStore();
+const { permissions } = storeToRefs(authStore);
+const perm = permissions.value;
 
-    const PaineisStore = usePaineisStore();
-    const { tempPaineis } = storeToRefs(PaineisStore);
-    PaineisStore.clear();
-    PaineisStore.filterPaineis();
+const PaineisStore = usePaineisStore();
+const { tempPaineis } = storeToRefs(PaineisStore);
+PaineisStore.clear();
+PaineisStore.filterPaineis();
 
-    const MetasStore = useMetasStore();
-    const { Metas } = storeToRefs(MetasStore);
-    MetasStore.getAll();
+const MetasStore = useMetasStore();
+const { Metas } = storeToRefs(MetasStore);
+MetasStore.getAll();
 
-    const filters = ref({
-        textualSearch: ""
-    });
-    let itemsFiltered = ref(tempPaineis);
+const filters = ref({
+  textualSearch: '',
+});
+const itemsFiltered = ref(tempPaineis);
 
-    function filterItems(){
-        PaineisStore.filterPaineis(filters.value);
-    }
+function filterItems() {
+  PaineisStore.filterPaineis(filters.value);
+}
 </script>
 <template>
-    <Dashboard>
-        <div class="flex spacebetween center mb2">
-            <h1>Painel Indicador</h1>
-            <hr class="ml2 f1" />
-            <router-link v-if="perm?.CadastroPainel?.inserir" to="/paineis/novo" class="btn big ml2">Novo Modelo</router-link>
-        </div>
-        <div class="flex center mb2">
-            <div class="f2 search">
-                <input v-model="filters.textualSearch" @input="filterItems" placeholder="Buscar" type="text" class="inputtext" />
-            </div>
-        </div>
+  <Dashboard>
+    <div class="flex spacebetween center mb2">
+      <h1>Painel Indicador</h1>
+      <hr class="ml2 f1">
+      <router-link
+        v-if="perm?.CadastroPainel?.inserir"
+        to="/paineis/novo"
+        class="btn big ml2"
+      >
+        Novo Modelo
+      </router-link>
+    </div>
+    <div class="flex center mb2">
+      <div class="f2 search">
+        <input
+          v-model="filters.textualSearch"
+          placeholder="Buscar"
+          type="text"
+          class="inputtext"
+          @input="filterItems"
+        >
+      </div>
+    </div>
 
-        <table class="tablemain">
-            <thead>
-                <tr>
-                    <th style="width: 45%">Nome</th>
-                    <th style="width: 15%">Status</th>
-                    <th style="width: 15%">Periodicidade</th>
-                    <th style="width: 15%">Metas monitoradas</th>
-                    <th style="width: 10%"></th>
-                </tr>
-            </thead>
-            <tbody>
-                <template v-if="itemsFiltered.length">
-                    <tr v-for="p in itemsFiltered" :key="p.id">
-                        <td>{{p.nome}}</td>
-                        <td>{{p.ativo?"Ativo":"Inativo"}}</td>
-                        <td>{{p.periodicidade}}</td>
-                        <td>{{p.painel_conteudo.length}} de {{Metas.length}}</td>
-                        <td style="white-space: nowrap; text-align: right">
-                            <router-link v-if="perm?.CadastroPainel?.editar" :to="`/paineis/${p.id}`" class="tprimary"><svg width="20" height="20"><use xlink:href="#i_edit"></use></svg></router-link>
-                        </td>
-                    </tr>
-                </template>
-                <tr v-else-if="itemsFiltered.loading">
-                    <td colspan="54">
-                        Carregando
-                    </td>
-                </tr>
-                <tr v-else-if="itemsFiltered.error">
-                    <td colspan="54">
-                        Error: {{itemsFiltered.error}}
-                    </td>
-                </tr>
-                <tr v-else>
-                    <td colspan="54">
-                        Nenhum resultado encontrado.
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    </Dashboard>
+    <table class="tablemain">
+      <thead>
+        <tr>
+          <th style="width: 45%">
+            Nome
+          </th>
+          <th style="width: 15%">
+            Status
+          </th>
+          <th style="width: 15%">
+            Periodicidade
+          </th>
+          <th style="width: 15%">
+            Metas monitoradas
+          </th>
+          <th style="width: 10%" />
+        </tr>
+      </thead>
+      <tbody>
+        <template v-if="itemsFiltered.length">
+          <tr
+            v-for="p in itemsFiltered"
+            :key="p.id"
+          >
+            <td>{{ p.nome }}</td>
+            <td>{{ p.ativo?"Ativo":"Inativo" }}</td>
+            <td>{{ p.periodicidade }}</td>
+            <td>{{ p.painel_conteudo.length }} de {{ Metas.length }}</td>
+            <td style="white-space: nowrap; text-align: right">
+              <router-link
+                v-if="perm?.CadastroPainel?.editar"
+                :to="`/paineis/${p.id}`"
+                class="tprimary"
+              >
+                <svg
+                  width="20"
+                  height="20"
+                ><use xlink:href="#i_edit" /></svg>
+              </router-link>
+            </td>
+          </tr>
+        </template>
+        <tr v-else-if="itemsFiltered.loading">
+          <td colspan="54">
+            Carregando
+          </td>
+        </tr>
+        <tr v-else-if="itemsFiltered.error">
+          <td colspan="54">
+            Error: {{ itemsFiltered.error }}
+          </td>
+        </tr>
+        <tr v-else>
+          <td colspan="54">
+            Nenhum resultado encontrado.
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </Dashboard>
 </template>
