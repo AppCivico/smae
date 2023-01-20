71178aa83d66db378d0c59e44bb1b4c2bd0aaa2e
Renato Cron <renato.cron@gmail.com>
2023-01-20 11:07:19 -0300

    [wip] liberar metas para o perfil tecnico/admin cp [meta ok]

--- backend/src/auth/models/PessoaFromJwt.ts
+++ backend/src/auth/models/PessoaFromJwt.ts
@@ -1,3 +1,5 @@
+import { HttpException } from "@nestjs/common";
+import { Prisma } from "@prisma/client";
 import { PessoaFromJwtBase } from "./PessoaFromJwtBase";
 
 
@@ -13,5 +15,23 @@ export class PessoaFromJwt extends PessoaFromJwtBase {
     public hasSomeRoles(anyRequiredRole: string[]) {
         if (!this.privilegios) return false;
         return anyRequiredRole.some((role) => this.privilegios.includes(role));
+    }
+
+    public async assertHasMetaPdmAccess(meta_id: number, pessoaAcessoPdm: Prisma.PessoaAcessoPdmDelegate<Prisma.RejectOnNotFound | Prisma.RejectPerOperation | false | undefined>) {
+        if (!this.privilegios) return false;
+
+        if (!this.hasSomeRoles(['PDM.tecnico_cp', 'PDM.admin_cp']))
+            throw new HttpException('Você precisa ser tecnico_cp ou admin_cp para acessar este recurso no momento', 400);
+
+        const perfilAcesso = await pessoaAcessoPdm.findFirst({ where: { pessoa_id: this.id } });
+        if (!perfilAcesso)
+            throw new HttpException('Não foi encontrado um perfil calculado para o seu usuário. Verificar se há PDM ativo com ciclos no momento', 400);
+
+        const varOuCrono = [...perfilAcesso.metas_variaveis, perfilAcesso.metas_cronograma];
+        if (varOuCrono.includes(+meta_id) == false) {
+            throw new HttpException(`Seu perfil no momento não pode acessar a meta ${meta_id}`, 400);
+        }
+
+        return;
     }
 }
