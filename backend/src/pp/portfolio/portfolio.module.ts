505faea6e6a99466c043b1d65125f6e5e562523f
lucasansei <lucas.ansei@eokoe.com>
2023-01-24 08:33:36 -0300

    Adicionando import do prisma module

--- backend/src/pp/portfolio/portfolio.module.ts
+++ backend/src/pp/portfolio/portfolio.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PortfolioService } from './portfolio.service';
 import { PortfolioController } from './portfolio.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+    imports: [PrismaModule],
   controllers: [PortfolioController],
   providers: [PortfolioService]
 })
