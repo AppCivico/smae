db4735cd02166b42758b6ba2068db3775a24af29
Sobral <robson@oficiodesign.com>
2023-01-25 17:25:22 -0300

    Formatar código.

--- frontend/src/components/SubmenuConfig.vue
+++ frontend/src/components/SubmenuConfig.vue
@@ -1,39 +1,86 @@
 <script setup>
-    import { storeToRefs } from 'pinia';
-    import { useAuthStore } from '@/stores';
-    const authStore = useAuthStore();
-    const { permissions } = storeToRefs(authStore);
-    const perm = permissions.value;
+import { useAuthStore } from '@/stores';
+import { storeToRefs } from 'pinia';
+
+const authStore = useAuthStore();
+const { permissions } = storeToRefs(authStore);
+const perm = permissions.value;
 </script>
 <template>
-    <div id="submenu">
-        <div class="subpadding">
-            <template v-if="perm?.CadastroPessoa||perm?.CadastroRegiao||perm?.CadastroPdm">
-                <h2>Entrada de dados</h2>
-                <div class="links-container mb2">
-                    <router-link v-if="perm?.CadastroPessoa" to="/usuarios">Gerenciar usuários</router-link>
-                    <router-link v-if="perm?.CadastroRegiao" to="/regioes">Regiões, Subprefeituras e Distritos</router-link>
-                    <router-link v-if="perm?.CadastroPdm" to="/pdm" :class="{active: parentPage=='pdm'}">Programa de Metas</router-link>
-                </div>
-            </template>
-
-            <template v-if="perm?.CadastroOrgao||perm?.CadastroFonteRecurso||perm?.CadastroTipoDocumento||perm?.CadastroOds">
-                <h2>Formulários básicos</h2>
-                <div class="links-container mb2">
-                    <router-link v-if="perm?.CadastroOrgao" to="/orgaos">Orgãos</router-link>
-                    <router-link v-if="perm?.CadastroFonteRecurso" to="/fonte-recurso">Fontes de Recurso</router-link>
-                    <router-link v-if="perm?.CadastroTipoDocumento" to="/tipo-documento">Tipos de Documento</router-link>
-                    <router-link v-if="perm?.CadastroOds" to="/ods">ODS</router-link>
-                </div>
-            </template>
-
-            <template v-if="perm?.CadastroPainel">
-                <h2>Paineis de metas</h2>
-                <div class="links-container mb2">
-                    <router-link v-if="perm?.CadastroPainel" to="/paineis">Paineis de metas</router-link>
-                    <router-link v-if="perm?.CadastroPainel" to="/paineis-grupos">Grupos de paineis</router-link>
-                </div>
-            </template>
+  <div id="submenu">
+    <div class="subpadding">
+      <template v-if="perm?.CadastroPessoa||perm?.CadastroRegiao||perm?.CadastroPdm">
+        <h2>Entrada de dados</h2>
+        <div class="links-container mb2">
+          <router-link
+            v-if="perm?.CadastroPessoa"
+            to="/usuarios"
+          >
+            Gerenciar usuários
+          </router-link>
+          <router-link
+            v-if="perm?.CadastroRegiao"
+            to="/regioes"
+          >
+            Regiões, Subprefeituras e Distritos
+          </router-link>
+          <router-link
+            v-if="perm?.CadastroPdm"
+            to="/pdm"
+            :class="{active: parentPage=='pdm'}"
+          >
+            Programa de Metas
+          </router-link>
         </div>
+      </template>
+
+      <template v-if="perm?.CadastroOrgao||perm?.CadastroFonteRecurso||perm?.CadastroTipoDocumento||perm?.CadastroOds">
+        <h2>Formulários básicos</h2>
+        <div class="links-container mb2">
+          <router-link
+            v-if="perm?.CadastroOrgao"
+            to="/orgaos"
+          >
+            Orgãos
+          </router-link>
+          <router-link
+            v-if="perm?.CadastroFonteRecurso"
+            to="/fonte-recurso"
+          >
+            Fontes de Recurso
+          </router-link>
+          <router-link
+            v-if="perm?.CadastroTipoDocumento"
+            to="/tipo-documento"
+          >
+            Tipos de Documento
+          </router-link>
+          <router-link
+            v-if="perm?.CadastroOds"
+            to="/ods"
+          >
+            ODS
+          </router-link>
+        </div>
+      </template>
+
+      <template v-if="perm?.CadastroPainel">
+        <h2>Paineis de metas</h2>
+        <div class="links-container mb2">
+          <router-link
+            v-if="perm?.CadastroPainel"
+            to="/paineis"
+          >
+            Paineis de metas
+          </router-link>
+          <router-link
+            v-if="perm?.CadastroPainel"
+            to="/paineis-grupos"
+          >
+            Grupos de paineis
+          </router-link>
+        </div>
+      </template>
     </div>
+  </div>
 </template>
