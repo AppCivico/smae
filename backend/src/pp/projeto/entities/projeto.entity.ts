2e561809ec7a85d4e77aad60f421ed819c1ffc96
lucasansei <lucas.ansei@eokoe.com>
2023-01-27 17:04:42 -0300

    Adicionando GET de detalhes de row de Projeto

--- backend/src/pp/projeto/entities/projeto.entity.ts
+++ backend/src/pp/projeto/entities/projeto.entity.ts
@@ -1,17 +1,14 @@
+import { ProjetoStatus } from "@prisma/client"
 import { IdCodTituloDto } from "src/common/dto/IdCodTitulo.dto"
+import { IdNomeExibicao } from "src/common/dto/IdNomeExibicao.dto"
+import { IdSiglaDescricao } from "src/common/dto/IdSigla.dto"
 
 export class ProjetoDto {
     id: number
     nome: string
-    status: string
-    orgao_responsavel: ProjetoOrgaoResponsavel | null
+    status: ProjetoStatus
+    orgao_responsavel: IdSiglaDescricao | null
     meta: IdCodTituloDto | null
-}
-
-class ProjetoOrgaoResponsavel {
-    id: number
-    sigla: string
-    descricao: string
 }
 
 export class ListProjetoDto {
@@ -19,9 +16,55 @@ export class ListProjetoDto {
 }
 
 export class ProjetoDetailDto {
-//     id
-// nome
-// status
-// orgao_responsavel
-// meta
+    id: number
+    meta_id: number | null
+    iniciativa_id: number | null
+    atividade_id: number | null
+    nome: string
+    status: ProjetoStatus
+    resumo: string
+    codigo: string | null
+    descricao: string
+    objeto: string
+    objetivo: string
+    publico_alvo: string | null
+    previsao_inicio: Date | null
+    previsao_custo: number | null
+    previsao_duracao: number | null
+    previsao_termino: Date | null
+    inicio_real: Date | null
+    custo_real: number | null
+    realizado_inicio: Date | null
+    realizado_termino: Date | null
+    realizado_custo: number | null
+    escopo: string | null
+    nao_escopo: string | null
+    orgaos_participantes: IdSiglaDescricao[]
+    principais_etapas: string
+    orgao_gestor: IdSiglaDescricao
+    orgao_responsavel: IdSiglaDescricao | null
+    responsavel: IdNomeExibicao | null
+    premissas: ProjetoPremissa[] | null
+    restricoes: ProjetoRestricoes[] | null
+    recursos: ProjetoRecursos[] | null
+
+    // responsaveis_no_orgao_gestor:
+}
+
+export class ProjetoPremissa {
+    id: number
+    premissa: string
+}
+
+export class ProjetoRestricoes {
+    id: number
+    restricao: string
+}
+
+export class ProjetoRecursos {
+    id: number
+    fonte_recurso_cod_sof: string
+    fonte_recurso_ano: number
+    valor_percentual: number | null
+    valor_nominal: number | null
 }
