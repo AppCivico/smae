3757b759ac19f7f75817b94e0777757ee276cf97
Renato CRON <renato.cron@gmail.com>
2023-01-27 14:35:30 -0300

    Fix ciclo_fase_atual_id

--- backend/prisma/migrations/20230127151257_uniq/migration.sql
+++ backend/prisma/migrations/20230127151257_uniq/migration.sql
@@ -14,6 +14,7 @@ set acordar_ciclo_em = now()
 where pdm_id = (select id from pdm where ativo)
 AND data_ciclo = date_trunc('month', now() at time zone 'America/Sao_Paulo');
 
+update ciclo_fisico set ciclo_fase_atual_id=null, acordar_ciclo_em=null where ativo=false and acordar_ciclo_em is not null;
 
 create unique index ix_ciclo_fisico_uniq_ativo on ciclo_fisico(ativo) where ativo=true;
 create unique index ix_pdm_uniq_ativo on pdm(ativo) where ativo=true;
