78c35dd9056b91cb12e0031243fbf6d7c184575c
Renato Cron <renato.cron@gmail.com>
2023-01-30 20:18:45 -0300

    Upload de documento no projeto

--- backend/src/pp/projeto/projeto.module.ts
+++ backend/src/pp/projeto/projeto.module.ts
@@ -3,9 +3,10 @@ import { ProjetoService } from './projeto.service';
 import { ProjetoController } from './projeto.controller';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { PortfolioModule } from '../portfolio/portfolio.module';
+import { UploadModule } from '../../upload/upload.module';
 
 @Module({
-    imports: [PrismaModule, PortfolioModule],
+    imports: [PrismaModule, PortfolioModule, UploadModule],
     controllers: [ProjetoController],
     providers: [ProjetoService],
 })
