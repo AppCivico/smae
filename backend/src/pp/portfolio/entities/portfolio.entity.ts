806925eca47880ecb6bc7d3b1e67bf2bd110fbcc
Renato Cron <renato.cron@gmail.com>
2023-01-31 06:42:20 -0300

    Lint on PP

--- backend/src/pp/portfolio/entities/portfolio.entity.ts
+++ backend/src/pp/portfolio/entities/portfolio.entity.ts
@@ -1,11 +1,11 @@
-import { IdSiglaDescricao } from "../../../common/dto/IdSigla.dto"
+import { IdSiglaDescricao } from '../../../common/dto/IdSigla.dto';
 
 export class PortfolioDto {
-    id: number
-    titulo:string
-    orgaos: IdSiglaDescricao[]
+    id: number;
+    titulo: string;
+    orgaos: IdSiglaDescricao[];
 }
 
 export class ListPortfolioDto {
-    linhas: PortfolioDto[]
+    linhas: PortfolioDto[];
 }
