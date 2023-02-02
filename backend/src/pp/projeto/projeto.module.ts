0346cfd976f90389a8831d5b1190fff159787581
Renato Cron <renato.cron@gmail.com>
2023-02-02 13:44:18 -0300

    New endpoint for ProjetoProxyPdmMetasController

--- backend/src/pp/projeto/projeto.module.ts
+++ backend/src/pp/projeto/projeto.module.ts
@@ -4,11 +4,13 @@ import { ProjetoController } from './projeto.controller';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { PortfolioModule } from '../portfolio/portfolio.module';
 import { UploadModule } from '../../upload/upload.module';
+import { ProjetoProxyPdmMetasService } from './projeto.proxy-pdm-metas.service';
+import { ProjetoProxyPdmMetasController } from './projeto.proxy-pdm-metas.controller';
 
 @Module({
     imports: [PrismaModule, PortfolioModule, UploadModule],
-    controllers: [ProjetoController],
-    providers: [ProjetoService],
+    controllers: [ProjetoController, ProjetoProxyPdmMetasController],
+    providers: [ProjetoService, ProjetoProxyPdmMetasService],
     exports: [ProjetoService]
 })
 export class ProjetoModule { }
