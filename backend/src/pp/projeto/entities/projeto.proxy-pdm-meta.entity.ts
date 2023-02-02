440ba31fb7952130c55641ed256dc0ac870d1454
Renato Cron <renato.cron@gmail.com>
2023-02-02 13:44:18 -0300

    New endpoint for ProjetoProxyPdmMetasController

---
+++ backend/src/pp/projeto/entities/projeto.proxy-pdm-meta.entity.ts
@@ -0,0 +1,9 @@
+import { IdCodTituloDto } from 'src/common/dto/IdCodTitulo.dto';
+
+export class ProjetoProxyPdmMetaDto {
+    id: number;
+    nome: string;
+    metas: IdCodTituloDto[];
+}
+
+
---
+++ backend/src/pp/projeto/projeto.proxy-pdm-metas.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../prisma/prisma.service';
+import { ProjetoProxyPdmMetaDto } from './entities/projeto.proxy-pdm-meta.entity';
+
+@Injectable()
+export class ProjetoProxyPdmMetasService {
+    constructor(private readonly prisma: PrismaService,) { }
+    async findAll(): Promise<ProjetoProxyPdmMetaDto[]> {
+
+        const rows = await this.prisma.pdm.findMany({
+            orderBy: [{ ativo: 'desc' }, { atualizado_em: 'desc' }],
+            select: {
+                id: true,
+                nome: true,
+                ativo: true,
+                Meta: {
+                    where: {
+                        removido_em: null
+                    },
+                    select: {
+                        id: true,
+                        codigo: true,
+                        titulo: true
+                    }
+                }
+            },
+        });
+
+        return rows.map((r) => {
+            return {
+                id: r.id,
+                nome: r.nome,
+                ativo: r.ativo,
+                metas: r.Meta
+            }
+        });
+    }
+
+}
