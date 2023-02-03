56b233d3775dbb987e2fafe042dab3531bc9d658
Sobral <robson@oficiodesign.com>
2023-02-03 15:46:00 -0300

    Adicionar suporte ao Vee-Validate no componente Autocomplete2

---
+++ frontend/src/components/AutocompleteField2.vue
@@ -0,0 +1,121 @@
+<script setup>
+import { useField } from 'vee-validate';
+import {
+  onMounted, onUpdated, ref, toRef, watch
+} from 'vue';
+
+const props = defineProps({
+  //  {
+  //    busca: '', // termo de busca
+  //    participantes: [], // lista de valores já preenchidos
+  //  }
+  controlador: {
+    type: Object,
+    required: true,
+    validator(value) {
+      return typeof value.busca === 'string' && Array.isArray(value.participantes);
+    },
+  },
+  // lista de opções
+  grupo: {
+    type: Array,
+    default() {
+      return [];
+    },
+    validator(value) {
+      return typeof value === 'undefined' || Array.isArray(value);
+    },
+  },
+  // nome da propriedade de cada item de `grupo` que deve ser exibido
+  label: {
+    type: String,
+    required: true,
+  },
+  // necessária para que o vee-validate não se perca
+  name: {
+    type: String,
+    required: true,
+  },
+});
+
+const control = ref(props.controlador);
+const name = toRef(props, 'name');
+
+const { handleChange } = useField(name, undefined, {
+  initialValue: props.controlador.participantes,
+});
+
+watch(control.value.participantes, (newValue) => {
+  handleChange(newValue);
+});
+
+function start() {
+  control.value = props.controlador;
+}
+
+start();
+onMounted(() => { start(); });
+onUpdated(() => { start(); });
+
+function removeParticipante(item, p) {
+  item.participantes.splice(item.participantes.indexOf(p), 1);
+}
+function pushId(e, id) {
+  e.push(id);
+  e = [...new Set(e)];
+}
+function busca(e, item, g, label) {
+  e.preventDefault();
+  e.stopPropagation();
+  if (e.keyCode === 13) {
+    const i = g.find((x) => !item.participantes.includes(x.id)
+      && x[label].toLowerCase().includes(item.busca.toLowerCase()));
+    if (i) {
+      pushId(item.participantes, i.id);
+    }
+    item.busca = '';
+  }
+}
+</script>
+<template>
+  <template v-if="grupo?.length">
+    <div class="suggestion search">
+      <input
+        v-model="control.busca"
+        type="text"
+        class="inputtext light mb05"
+        @keyup.enter.stop.prevent="busca($event,control,grupo,label)"
+      >
+      <ul>
+        <li
+          v-for="r in grupo.filter((x) => !control.participantes.includes(x.id)
+            && x[label]?.toLowerCase().includes(control.busca.toLowerCase()))"
+          :key="r.id"
+        >
+          <a
+            tabindex="1"
+            @click="pushId(control.participantes,r.id)"
+          >{{ r[label] }}</a>
+        </li>
+      </ul>
+    </div>
+    <span
+      v-for="p in grupo.filter((x) => control.participantes.includes(x.id))"
+      :key="p.id"
+      class="tagsmall"
+      @click="removeParticipante(control, p.id)"
+    >{{ p[label] }}<svg
+      width="12"
+      height="12"
+    ><use xlink:href="#i_x" /></svg></span>
+  </template>
+  <template v-else>
+    <div class="search">
+      <input
+        type="text"
+        disabled
+        class="inputtext light mb05"
+      >
+    </div>
+  </template>
+</template>
