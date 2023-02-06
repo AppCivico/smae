fcab82ad8e089a9a092293c1c993d5d199cff5a6
Sobral <robson@oficiodesign.com>
2023-02-06 14:47:31 -0300

    Aprimimorar componente `AutocompleteField2`

--- frontend/src/components/AutocompleteField2.vue
+++ frontend/src/components/AutocompleteField2.vue
@@ -34,20 +34,24 @@ const props = defineProps({
   // necessária para que o vee-validate não se perca
   name: {
     type: String,
-    required: true,
+    default: '',
   },
 });
 
 const control = ref(props.controlador);
-const name = toRef(props, 'name');
 
-const { handleChange } = useField(name, undefined, {
-  initialValue: props.controlador.participantes,
-});
+// se tivermos o nome do campo, podemos habilitar o vee-validate.
+// É aqui que deixamos o componente retrocompatível
+if (props.name) {
+  const name = toRef(props, 'name');
+  const { handleChange } = useField(name, undefined, {
+    initialValue: props.controlador.participantes,
+  });
 
-watch(control.value.participantes, (newValue) => {
-  handleChange(newValue);
-});
+  watch(control.value.participantes, (newValue) => {
+    handleChange(newValue);
+  });
+}
 
 function start() {
   control.value = props.controlador;
@@ -92,10 +96,15 @@ function busca(e, item, g, label) {
             && x[label]?.toLowerCase().includes(control.busca.toLowerCase()))"
           :key="r.id"
         >
-          <a
+          <button
+            type="button"
+            class="like-a__text"
             tabindex="1"
+            :title="r.nome || r.titulo || r.descricao || r.nome_completo || null"
             @click="pushId(control.participantes,r.id)"
-          >{{ r[label] }}</a>
+          >
+            {{ r[label] }}
+          </button>
         </li>
       </ul>
     </div>
@@ -103,6 +112,7 @@ function busca(e, item, g, label) {
       v-for="p in grupo.filter((x) => control.participantes.includes(x.id))"
       :key="p.id"
       class="tagsmall"
+      :title="p.nome || p.titulo || p.descricao ||p.nome_completo || null"
       @click="removeParticipante(control, p.id)"
     >{{ p[label] }}<svg
       width="12"
