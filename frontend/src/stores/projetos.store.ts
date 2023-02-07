369149b39a239892793da289914e1805bd6ac6d3
Sobral <robson@oficiodesign.com>
2023-02-07 16:05:38 -0300

    Corrigir campos de origens

--- frontend/src/stores/projetos.store.ts
+++ frontend/src/stores/projetos.store.ts
@@ -1,22 +1,30 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { requestS } from '@/helpers';
 import { defineStore } from 'pinia';
 
-// eslint-disable-next-line import/no-extraneous-dependencies
+import { ListDadosMetaIniciativaAtividadesDto } from '@/../../backend/src/meta/dto/create-meta.dto';
 import { ListProjetoDto, ProjetoDetailDto } from '@/../../backend/src/pp/projeto/entities/projeto.entity';
+import { ListProjetoProxyPdmMetaDto } from '@/../../backend/src/pp/projeto/entities/projeto.proxy-pdm-meta.entity';
 
 const baseUrl = `${import.meta.env.VITE_API_URL}`;
 
 type Lista = ListProjetoDto['linhas'];
+type PdmsSimplificados = ListProjetoProxyPdmMetaDto['linhas'];
+type MetaSimplificada = ListDadosMetaIniciativaAtividadesDto['linhas'];
 
 interface ChamadasPendentes {
   lista: boolean,
   emFoco: boolean,
+  pdmsSimplificados: boolean,
+  metaSimplificada: boolean,
 }
 
 interface Estado {
   lista: Lista;
   emFoco: ProjetoDetailDto | null;
   chamadasPendentes: ChamadasPendentes;
+  pdmsSimplificados: PdmsSimplificados;
+  metaSimplificada: MetaSimplificada;
   erro: null | unknown,
 }
 
@@ -37,12 +45,37 @@ async function buscarItem(this: Estado, id = 0, params = {}): Promise<void> {
   this.chamadasPendentes.emFoco = true;
   try {
     const resposta = await requestS.get(`${baseUrl}/projeto/${id}`, params);
-    console.debug('resposta', resposta);
     this.emFoco = resposta;
   } catch (erro: unknown) {
     this.erro = erro;
   }
   this.chamadasPendentes.emFoco = false;
+}
+
+async function buscarPdms(this: Estado, params = {}): Promise<void> {
+  this.chamadasPendentes.pdmsSimplificados = true;
+  this.pdmsSimplificados = [];
+
+  try {
+    const { linhas } = await requestS.get(`${baseUrl}/projeto/proxy/pdm-e-metas`, params);
+    this.pdmsSimplificados = linhas;
+  } catch (erro: unknown) {
+    this.erro = erro;
+  }
+  this.chamadasPendentes.pdmsSimplificados = false;
+}
+
+async function buscarMetaSimplificada(this: Estado, params = {}): Promise<void> {
+  this.chamadasPendentes.metaSimplificada = true;
+  this.metaSimplificada = [];
+
+  try {
+    const { linhas } = await requestS.get(`${baseUrl}/projeto/proxy/iniciativas-atividades`, params);
+    [this.metaSimplificada] = linhas;
+  } catch (erro: unknown) {
+    this.erro = erro;
+  }
+  this.chamadasPendentes.metaSimplificada = false;
 }
 
 async function salvarItem(this: Estado, params = {}, id = 0): Promise<boolean> {
@@ -84,6 +117,9 @@ async function excluirItem(this: Estado, id: Number): Promise<boolean> {
 const projetosPorId = ({ lista }: Estado) => lista
   .reduce((acc, cur) => ({ ...acc, [cur.id]: cur }), {});
 
+const pdmsPorId = ({ pdmsSimplificados }: Estado) => pdmsSimplificados
+  .reduce((acc, cur) => ({ ...acc, [cur.id]: cur }), {});
+
 export const useProjetosStore = defineStore('projetos', {
   state: (): Estado => ({
     lista: [],
@@ -92,16 +128,24 @@ export const useProjetosStore = defineStore('projetos', {
     chamadasPendentes: {
       lista: true,
       emFoco: true,
+      pdmsSimplificados: true,
+      metaSimplificada: true,
     },
+
+    pdmsSimplificados: [],
+    metaSimplificada: [],
     erro: null,
   }),
   actions: {
     buscarItem,
+    buscarPdms,
+    buscarMetaSimplificada,
     buscarTudo,
     excluirItem,
     salvarItem,
   },
   getters: {
+    pdmsPorId,
     projetosPorId,
   },
 });
