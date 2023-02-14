aa0467de3608391697aaf95d5f8b1ef3ec0c6f9d
Sobral <robson@oficiodesign.com>
2023-02-14 10:43:50 -0300

    Adicionar componente para filtragem local

---
+++ frontend/src/components/LocalFilter.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { debounce } from 'lodash';
+import { computed } from 'vue';
+
+const props = defineProps({
+  modelValue: {
+    type: String,
+    default: '',
+  },
+});
+
+const emit = defineEmits(['update:modelValue']);
+
+const value = computed({
+  get() {
+    return props.modelValue;
+  },
+  set: debounce((newValue) => {
+    emit('update:modelValue', newValue);
+  }, 400),
+});
+</script>
+
+<template>
+  <div class="f1 mr1">
+    <label class="label tc300">Busca livre</label>
+    <input
+      :value="value"
+      class="inputtext"
+      @input="$emit('update:modelValue', $event.target.value)"
+    >
+    <small class="tc200 t13">ocorre nos dados já baixados</small>
+  </div>
+</template>
