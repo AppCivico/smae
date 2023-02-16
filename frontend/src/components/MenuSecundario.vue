855c2077919f783f33d871b63dec9e6b8a1edb86
Sobral <robson@oficiodesign.com>
2023-02-16 17:32:08 -0300

    Preparar menu secundário para aceitar migalhas de pão

--- frontend/src/components/MenuSecundario.vue
+++ frontend/src/components/MenuSecundario.vue
@@ -9,57 +9,64 @@ const { temPermissãoPara } = storeToRefs(authStore);
 const route = useRoute();
 const router = useRouter();
 
-let listaDeRotas = [];
+const limparRotas = (listaDeRotas) => (Array.isArray(listaDeRotas)
+  ? listaDeRotas
+  : [listaDeRotas])
+  .map((x) => {
+    let rotaParaResolver = x;
 
-switch (true) {
-  case Array.isArray(route.meta?.rotasParaMenuSecundário):
-    listaDeRotas = route.meta?.rotasParaMenuSecundário;
-    break;
-
-  case !!(route.meta?.rotasParaMenuSecundário?.name || route.meta?.rotasParaMenuSecundário?.path):
-    listaDeRotas = [route.meta?.rotasParaMenuSecundário];
-    break;
-
-  default:
-    break;
-}
-
-const rotasParaMenu = listaDeRotas.map((x) => {
-  let rotaParaResolver = x;
-
-  if (typeof x === 'string') {
-    if (x.indexOf('/') > -1) {
-      rotaParaResolver = { path: x };
-    } else {
-      rotaParaResolver = { name: x };
+    if (typeof x === 'string') {
+      if (x.indexOf('/') > -1) {
+        rotaParaResolver = { path: x };
+      } else {
+        rotaParaResolver = { name: x };
+      }
     }
-  }
 
-  return router.resolve({ ...rotaParaResolver, params: route.params });
-})
-// eslint-disable-next-line max-len
-  /* .filter((x) => (!x.meta?.restringirÀsPermissões || temPermissãoPara(x.meta.restringirÀsPermissões)))
-    || [] */;
+    return router.resolve({ ...rotaParaResolver, params: route.params }, '/projetos/8/resumo')
+    // eslint-disable-next-line max-len
+      .filter((y) => (!y.meta?.restringirÀsPermissões || temPermissãoPara(y.meta.restringirÀsPermissões)))
+        || [];
+  });
+
+const rotasParaMenu = route.meta?.rotasParaMenuSecundário
+  ? limparRotas(route.meta?.rotasParaMenuSecundário)
+  : [];
+
+const rotasParaMigalhasDePão = route.meta?.rotasParaMigalhasDePão
+  ? limparRotas(route.meta?.rotasParaMigalhasDePão)
+  : [];
+
 </script>
 <template>
   <div
-    v-if="rotasParaMenu.length"
+    v-if="rotasParaMenu.length || rotasParaMigalhasDePão.length"
     id="submenu"
   >
     <div
-      v-if="route.meta.usarMigalhasDePão"
+      v-if="rotasParaMigalhasDePão.length"
       class="breadcrumbmenu"
     >
-      <!--
-        TODO -->
+      <router-link
+        v-for="item, k in rotasParaMigalhasDePão"
+        :key="k"
+        :to="item.href"
+      >
+        <span>
+          {{ item.meta?.título || item.name }}
+        </span>
+      </router-link>
     </div>
 
-    <div class="subpadding">
+    <div
+      v-if="rotasParaMenu.length"
+      class="subpadding"
+    >
       <div class="links-container mb2">
         <router-link
           v-for="item, k in rotasParaMenu"
           :key="k"
-          :to="item"
+          :to="item.href"
         >
           {{ item.meta?.títuloParaMenu || item.meta?.título || item.name }}
         </router-link>
