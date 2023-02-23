ffc9ad08c9fce7ccccc03210167ec21cc82b507a
Sobral <robson@oficiodesign.com>
2023-02-23 20:46:04 -0300

    Listar tarefas no cronograma de projetos

---
+++ frontend/src/components/projetos/LinhaDeCronograma.vue
@@ -0,0 +1,128 @@
+<script>
+import dateToField from '@/helpers/dateToField';
+import { useAlertStore } from '@/stores/alert.store';
+import { useTarefasStore } from '@/stores/tarefas.store.ts';
+
+export default {
+  name: 'LinhaDeCronograma',
+  props: {
+    class: {
+      type: [Object, String],
+      default: '',
+    },
+    genealogia: {
+      type: String,
+      default: '',
+    },
+    índice: {
+      type: Number,
+      required: true,
+    },
+    linha: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    dateToField,
+    excluirTarefa(id) {
+      useAlertStore().confirmAction('Deseja mesmo remover esse item?', async () => {
+        if (await useTarefasStore().excluirItem(id)) {
+          useTarefasStore().$reset();
+          useTarefasStore().buscarTudo();
+          useAlertStore().success('Portfolio removido.');
+        }
+      }, 'Remover');
+    },
+  },
+};
+</script>
+
+<template>
+  <tr
+    class="t13"
+    :class="class"
+  >
+    <td
+      class="genealogia"
+      :class="{ 'genealogia--origem': linha.nivel === 1 }"
+    >
+      <span
+        class="valor"
+      >
+        <small
+          v-if="genealogia"
+          class="niveis-pais"
+        >{{ genealogia }}.</small>{{ índice + 1 }}
+      </span>
+    </td>
+    <th
+      class="pl1 left"
+    >
+      <router-link to="/">
+        {{ linha.tarefa }}
+      </router-link>
+    </th>
+
+    <td class="cell--number">
+      XYZ
+    </td>
+    <td class="cell--number">
+      {{ linha.duracao_planejado ?? '-' }}
+    </td>
+    <td class="cell--data">
+      {{ dateToField(linha.inicio_planejado) }}
+    </td>
+    <td class="cell--data">
+      {{ dateToField(linha.termino_planejado) }}
+    </td>
+    <td class="cell--data">
+      {{ dateToField(linha.inicio_real) }}
+    </td>
+    <td class="cell--data">
+      {{ dateToField(linha.termino_real) }}
+    </td>
+    <td class="cell--number">
+      {{ linha.atraso ?? '-' }}
+    </td>
+
+    <td
+      class="pl1 center mr05"
+      style="height: calc(20px + 1rem);"
+    >
+      <button
+        type="button"
+        class="like-a__text"
+        title="Excluir"
+        :disabled="linha.children?.length"
+        @click="excluirTarefa(linha.id)"
+      >
+        <svg
+          width="20"
+          height="20"
+        ><use xlink:href="#i_remove" /></svg>
+      </button>
+    </td>
+
+    <td
+      class="pl1 center mr05"
+      style="height: calc(20px + 1rem);"
+    >
+      <router-link to="/">
+        <svg
+          width="20"
+          height="20"
+        ><use xlink:href="#i_edit" /></svg>
+      </router-link>
+    </td>
+  </tr>
+
+  <LinhaDeCronograma
+    v-for="(linhaFilha, i) in linha.children"
+    :key="linhaFilha.id"
+    :genealogia="genealogia ? `${genealogia}.${índice + 1}` : `${índice + 1}`"
+    :índice="i"
+    :linha="linhaFilha"
+    class="tabela-de-etapas__item--sub"
+  />
+</template>
