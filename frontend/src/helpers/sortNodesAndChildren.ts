32f11e1031c9fa30875531bc31c849b18e863f5a
Sobral <robson@oficiodesign.com>
2023-02-26 16:52:25 -0300

    Anotar auxiliar

--- frontend/src/helpers/sortNodesAndChildren.ts
+++ frontend/src/helpers/sortNodesAndChildren.ts
@@ -1,5 +1,13 @@
 type Sorter = (a: any, b: any) => number;
 
+/**
+ * Sort tree structure
+ *
+ * @param {any[]} nodes
+ * @param {(Sorter | undefined)} comparador
+ * @return {*}
+ * @link https://stackoverflow.com/a/31598407/15425845
+ */
 function sortNodesAndChildren(nodes: any[], comparador: Sorter | undefined) {
   if (typeof comparador === 'function') {
     nodes.sort(comparador);
