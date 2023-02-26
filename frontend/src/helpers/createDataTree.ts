9242e5b2a3625e3198a68e7f9fdb043a57e7d29b
Sobral <robson@oficiodesign.com>
2023-02-26 20:51:30 -0300

    Aprimorar declaração de tipos no auxiliar que cria árvores

--- frontend/src/helpers/createDataTree.ts
+++ frontend/src/helpers/createDataTree.ts
@@ -6,14 +6,14 @@
  * @return {*}
  * @link https://stackoverflow.com/a/40732240/15425845
  */
-export default (dataset: any[], parentPropertyName = 'parentId', childrenPropertyName = 'children') => {
+export default (dataset: any[], parentPropertyName: string = 'parentId', childrenPropertyName: string = 'children') => {
   const hashTable = Object.create(null);
-  dataset.forEach((aData: { id: string | number; }) => {
+  dataset.forEach((aData: { id: string | number }) => {
     hashTable[aData.id] = { ...aData, [childrenPropertyName]: [] };
   });
 
   const dataTree: any[] = [];
-  dataset.forEach((aData: { [x: string]: string | number; id: string | number; }) => {
+  dataset.forEach((aData: { [x: string]: string | number; id: string | number }) => {
     if (aData[parentPropertyName]) {
       hashTable[aData[parentPropertyName]][childrenPropertyName].push(hashTable[aData.id]);
     } else {
