9f9bb230e9c06308588a1400ba6b8ed8a7b3135f
Sobral <robson@oficiodesign.com>
2023-02-28 08:56:04 -0300

    Ordenar tipos de relação entre dependências

--- frontend/src/consts/dependencyTypes.js
+++ frontend/src/consts/dependencyTypes.js
@@ -1,6 +1,6 @@
 export default {
-  termina_pro_inicio: 'deve terminar para que eu inicie em',
-  inicia_pro_inicio: 'deve iniciar para que eu inicie em',
-  inicia_pro_termino: 'deve iniciar para que eu termine em',
-  termina_pro_termino: 'deve terminar para que eu termine em',
+  inicia_pro_inicio: 'deve iniciar para que está tarefa inicie em',
+  inicia_pro_termino: 'deve iniciar para que está tarefa termine em',
+  termina_pro_inicio: 'deve terminar para que está tarefa inicie em',
+  termina_pro_termino: 'deve terminar para que está tarefa termine em',
 };
