2e4a2310ec664442155dfb9d4e40c503b9970ee2
Renato Cron <renato.cron@gmail.com>
2023-03-09 10:25:35 -0300

    Apagando arquivos "down.sql" (nao funciona bem), novas colunas em PlanoAcaoMonitoramento

--- backend/prisma/migrations/20230127151257_uniq/down.sql
+++
@@ -1,2 +0,0 @@
-drop index ix_ciclo_fisico_uniq_ativo on ciclo_fisico;
-drop index ix_pdm_uniq_ativo on ciclo_fisico;
--- backend/prisma/migrations/20230302181927_fixes_para_o_ordem_topologica_atualizando_triggers/down.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "tarefa" DROP COLUMN "ordem_topologica_inicio_planejado",
-ADD COLUMN     "ordem_topologica_inicio_planejado" SMALLINT NOT NULL DEFAULT 0,
-DROP COLUMN "ordem_topologica_termino_planejado",
-ADD COLUMN     "ordem_topologica_termino_planejado" SMALLINT NOT NULL DEFAULT 0;
-
