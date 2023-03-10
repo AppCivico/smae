7dc1d5240856f6b71beeb793e5a703eb0dc557f5
Sobral <robson@oficiodesign.com>
2023-03-10 08:23:15 -0300

    Redefinir tarefas na troca de projetos

--- frontend/src/views/projetos/ProjetosItem.vue
+++ frontend/src/views/projetos/ProjetosItem.vue
@@ -1,13 +1,18 @@
 <script setup>
 import { useProjetosStore } from '@/stores/projetos.store.ts';
+import { useTarefasStore } from '@/stores/tarefas.store.ts';
 
 const projetosStore = useProjetosStore();
+const tarefasStore = useTarefasStore();
 const props = defineProps({
   projetoId: {
     type: Number,
     default: 0,
   },
 });
+
+// Tarefas dependem do projeto, portanto precisam ser redefinidas na troca deste
+tarefasStore.$reset();
 
 if (projetosStore.emFoco?.id !== props.projetoId) {
   projetosStore.$reset();
