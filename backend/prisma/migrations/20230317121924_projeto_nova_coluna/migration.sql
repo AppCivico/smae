c5d3e01bf37b33b110ff4bca9ee7c12b6ed1214a
Renato Cron <renato.cron@gmail.com>
2023-03-17 09:52:52 -0300

    Update func atualiza_calendario_projeto pra calcular o v_realizado_duracao

--- backend/prisma/migrations/20230317121924_projeto_nova_coluna/migration.sql
+++ backend/prisma/migrations/20230317121924_projeto_nova_coluna/migration.sql
@@ -2,3 +2,106 @@
 ALTER TABLE "projeto" ADD COLUMN     "atraso" INTEGER,
 ADD COLUMN     "percentual_concluido" DOUBLE PRECISION NOT NULL DEFAULT 0,
 ADD COLUMN     "realizado_duracao" INTEGER;
+
+CREATE OR REPLACE FUNCTION atualiza_calendario_projeto(pProjetoId int)
+    RETURNS varchar
+    AS $$
+DECLARE
+
+v_previsao_inicio  date;
+v_realizado_inicio  date;
+v_previsao_termino date;
+v_realizado_termino date;
+v_previsao_custo  numeric;
+v_realizado_custo  numeric;
+v_previsao_duracao int;
+v_realizado_duracao int;
+
+BEGIN
+
+    SELECT
+        (
+         select min(inicio_planejado)
+         from tarefa t
+         where t.tarefa_pai_id IS NULL and t.projeto_id = pProjetoId and t.removido_em is null
+         and inicio_planejado is not null
+        ),
+        (
+         select min(inicio_real)
+         from tarefa t
+         where t.tarefa_pai_id IS NULL and t.projeto_id = pProjetoId and t.removido_em is null
+         and inicio_real is not null
+        ),
+        (
+         select max(termino_planejado)
+         from tarefa t
+         where t.tarefa_pai_id IS NULL and t.projeto_id = pProjetoId and t.removido_em is null
+         and termino_planejado is not null
+        ),
+        (
+         select max(termino_real)
+         from tarefa t
+         where t.tarefa_pai_id IS NULL and t.projeto_id = pProjetoId and t.removido_em is null
+         and termino_real is not null
+        ),
+        (
+         select sum(custo_estimado)
+         from tarefa t
+         where t.tarefa_pai_id IS NULL and t.projeto_id = pProjetoId and t.removido_em is null
+         and custo_estimado is not null
+        ),
+        (
+         select sum(custo_real)
+         from tarefa t
+         where t.tarefa_pai_id IS NULL and t.projeto_id = pProjetoId and t.removido_em is null
+         and custo_real is not null
+        )
+        into
+            v_previsao_inicio,
+            v_realizado_inicio,
+            v_previsao_termino,
+            v_realizado_termino,
+            v_previsao_custo,
+            v_realizado_custo;
+
+    v_previsao_duracao := case when v_previsao_inicio is not null and v_previsao_termino is not null
+        then
+            v_previsao_termino - v_previsao_inicio
+        else
+            null
+        end;
+
+    v_realizado_duracao := case when v_realizado_inicio is not null and v_realizado_termino is not null
+        then
+            v_realizado_termino - v_realizado_inicio
+        else
+            null
+        end;
+
+    UPDATE projeto p
+    SET
+        previsao_inicio = v_previsao_inicio,
+        realizado_inicio = v_realizado_inicio,
+        previsao_termino = v_previsao_termino,
+        realizado_termino = v_realizado_termino,
+        previsao_custo = v_previsao_custo,
+        realizado_custo = v_realizado_custo,
+        previsao_duracao = v_previsao_duracao,
+        realizado_duracao = v_realizado_duracao
+
+    WHERE p.id = pProjetoId
+    AND (
+        (v_previsao_inicio is DISTINCT from previsao_inicio) OR
+        (v_realizado_inicio is DISTINCT from realizado_inicio) OR
+        (v_previsao_termino is DISTINCT from previsao_termino) OR
+        (v_realizado_termino is DISTINCT from realizado_termino) OR
+        (v_previsao_custo is DISTINCT from previsao_custo) OR
+        (v_realizado_custo is DISTINCT from realizado_custo) OR
+        (v_previsao_duracao is DISTINCT from previsao_duracao) OR
+        (v_realizado_duracao is DISTINCT from realizado_duracao)
+    );
+
+    return '';
+END
+$$
+LANGUAGE plpgsql;
