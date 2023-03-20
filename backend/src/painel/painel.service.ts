e9b1135547e63eda7c5276c8b29cf4af37acf215
lucasansei <lucas.ansei@eokoe.com>
2023-03-20 16:33:41 -0300

    Adicionando retorno undef para caso item esteja já salvo

--- backend/src/painel/painel.service.ts
+++ backend/src/painel/painel.service.ts
@@ -817,7 +817,11 @@ export class PainelService {
         const deleted: PainelConteudoDetalheForSync[] = existent_painel_conteudo_detalhes.filter(curr => {
             const row_is_saved = saved_rows.find((e) => { e.id === curr.id });
 
-            if (!row_is_saved?.id) return curr
+            if (row_is_saved) {
+                return undefined;
+            } else {
+                return curr;
+            }
         }).map((curr) => { return curr });
 
         console.log("lgt existent_painel_conteudo_detalhes=" + existent_painel_conteudo_detalhes.length);
