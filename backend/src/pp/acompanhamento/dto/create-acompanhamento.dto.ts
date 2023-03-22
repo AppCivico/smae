c9d8cd2f7379b54049131bddcfd471259a073342
Renato Cron <renato.cron@gmail.com>
2023-03-22 13:45:31 -0300

    salvando o dto.risco

--- backend/src/pp/acompanhamento/dto/create-acompanhamento.dto.ts
+++ backend/src/pp/acompanhamento/dto/create-acompanhamento.dto.ts
@@ -1,5 +1,5 @@
 import { Type } from "class-transformer"
-import { IsOptional, IsString, ValidateIf } from "class-validator"
+import { ArrayMaxSize, ArrayMinSize, IsArray, IsInt, IsOptional, IsString, ValidateIf } from "class-validator"
 import { IsOnlyDate } from "src/common/decorators/IsDateOnly"
 
 export class CreateProjetoAcompanhamentoDto {
@@ -22,28 +22,45 @@ export class CreateProjetoAcompanhamentoDto {
 
     @IsString()
     participantes: string
-    
+
     @IsOptional()
     @IsString()
     detalhamento?: string
-    
+
     @IsOptional()
     @IsString()
     encaminhamento?: string
-    
+
     @IsOptional()
     @IsString()
     responsavel?: string
-    
+
     @IsOptional()
     @IsString()
     observacao?: string
-    
+
     @IsOptional()
     @IsString()
     detalhamento_status?: string
-    
+
     @IsOptional()
     @IsString()
     pontos_atencao?: string
+
+    //@IsOptional()
+    //@IsString()
+    //risco_tarefa_outros?: string
+
+    /**
+     * enviar array dos ids dos projeto-risco-id array vazia ou nulo para remover
+     * @example "[]"
+    */
+    @IsOptional()
+    @IsArray({ message: '$property| risco(s): precisa ser uma array ou.' })
+    @ArrayMinSize(0, { message: '$property| risco(s): precisa ter pelo menos um item' })
+    @ArrayMaxSize(100, { message: '$property| risco(s): precisa ter no máximo 100 items' })
+    @IsInt({ each: true, message: '$property| Cada item precisa ser um número inteiro' })
+    @ValidateIf((object, value) => value !== null)
+    risco?: number[] | null;
+
 }
