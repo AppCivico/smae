cbca875a3a56e27b2f6b7270110fa444492b5ab1
Sobral <robson@oficiodesign.com>
2023-03-27 16:34:01 -0300

    Adicionar primeira versão do gráfico de Gantt

---
+++ frontend/src/views/projetos/ProjetosGantt.vue
@@ -0,0 +1,78 @@
+<script setup>
+import Gantt from '@/components/projetos/gantt/Gantt.vue';
+import MenuDeMudançaDeStatusDeProjeto from '@/components/projetos/MenuDeMudançaDeStatusDeProjeto.vue';
+import { useProjetosStore } from '@/stores/projetos.store.ts';
+import { useTarefasStore } from '@/stores/tarefas.store.ts';
+import { storeToRefs } from 'pinia';
+
+const projetosStore = useProjetosStore();
+const tarefasStore = useTarefasStore();
+const {
+  chamadasPendentes, emFoco, erro,
+} = storeToRefs(projetosStore);
+const {
+  tarefasComHierarquia,
+} = storeToRefs(tarefasStore);
+
+defineProps({
+  projetoId: {
+    type: Number,
+    default: 0,
+  },
+});
+
+function iniciar() {
+  if (!tarefasStore.lista.length || !tarefasStore.lista[0]?.dependencias) {
+    tarefasStore.buscarTudo({ incluir_dependencias: true });
+  }
+}
+
+iniciar();
+</script>
+
+<template>
+  <div class="flex spacebetween center mb2">
+    <div>
+      <div class="t12 uc w700 tamarelo">
+        Projeto
+        <template v-if="emFoco?.eh_prioritario">
+          prioritário
+        </template>
+      </div>
+
+      <h1>{{ emFoco?.nome }}</h1>
+    </div>
+    <hr class="ml2 f1">
+    <MenuDeMudançaDeStatusDeProjeto />
+
+    <template v-if="emFoco?.id && !emFoco.arquivado">
+      <router-link
+        :to="{ name: 'projetosEditar', params: { projetoId: emFoco.id } }"
+        class="btn big ml2"
+      >
+        Editar
+      </router-link>
+    </template>
+  </div>
+
+  <Gantt
+    v-if="tarefasComHierarquia.length"
+    :data="tarefasComHierarquia"
+  />
+
+  <div
+    v-if="chamadasPendentes?.emFoco"
+    class="spinner"
+  >
+    Carregando
+  </div>
+
+  <div
+    v-if="erro"
+    class="error p1"
+  >
+    <div class="error-msg">
+      {{ erro }}
+    </div>
+  </div>
+</template>
