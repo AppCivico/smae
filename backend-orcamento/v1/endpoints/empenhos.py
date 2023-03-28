ea21fc39cc13883e7139dde4cea4aff6beb07a15
Renato Cron <renato.cron@gmail.com>
2023-03-28 12:38:25 -0300

    Sync bugfix do SOF (nota-empenho)

--- backend-orcamento/v1/endpoints/empenhos.py
+++ backend-orcamento/v1/endpoints/empenhos.py
@@ -37,6 +37,9 @@ def empenho_nota(nota_empenho:schm_empenho.NotaEmpenho, dao: DaoEmpenhos = Depen
 
     if result:
         resp_data = [schm_empenho.RetornoEmpenho(**item) for item in result]
+        #no caso das notas de empenho, soh pode haver um processo/dotação
+        if len(resp_data) > 1:
+            resp_data = resp_data[0]
         resp = schm_empenho.Empenhos(data=resp_data,
                     metadados=MetaDados(sucess=True))
 
