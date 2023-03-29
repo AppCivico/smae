a33e7414a21bf06331083cc70c859bd173a3727e
Sobral <robson@oficiodesign.com>
2023-03-29 11:20:36 -0300

    Corrigir validação de valor base de indicadores acumulados

--- frontend/src/stores/indicadores.store.js
+++ frontend/src/stores/indicadores.store.js
@@ -60,7 +60,9 @@ export const useIndicadoresStore = defineStore({
 
           x.inicio_medicao = this.dateToField(x.inicio_medicao);
           x.fim_medicao = this.dateToField(x.fim_medicao);
-          x.acumulado_valor_base = Number.parseFloat(x.acumulado_valor_base, 10) ?? null;
+          if (x.acumulado_valor_base !== null){
+            x.acumulado_valor_base = Number.parseFloat(x.acumulado_valor_base, 10);
+          }
           x.agregador_id = x.agregador ? x.agregador.id : null;
 
           this.singleIndicadores = x;
