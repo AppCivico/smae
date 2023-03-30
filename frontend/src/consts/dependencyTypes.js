a4949610bede1f478fa86a34441124d4349f9b0a
Sobral <robson@oficiodesign.com>
2023-03-30 08:13:18 -0300

    Atualizar nomes dos tipos de dependências

--- frontend/src/consts/dependencyTypes.js
+++ frontend/src/consts/dependencyTypes.js
@@ -1,6 +1,6 @@
 export default {
-  inicia_pro_inicio: 'deve iniciar para que esta tarefa inicie em',
-  inicia_pro_termino: 'deve iniciar para que esta tarefa termine em',
-  termina_pro_inicio: 'deve terminar para que esta tarefa inicie em',
-  termina_pro_termino: 'deve terminar para que esta tarefa termine em',
+  inicia_pro_inicio: 'deve iniciar para que esta tarefa inicie',
+  inicia_pro_termino: 'deve iniciar para que esta tarefa termine',
+  termina_pro_inicio: 'deve terminar para que esta tarefa inicie',
+  termina_pro_termino: 'deve terminar para que esta tarefa termine',
 };
