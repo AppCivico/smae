6798e94176c67201d31af49a8a71118f025146e9
Renato Cron <renato.cron@gmail.com>
2023-04-11 00:25:20 -0300

    Recalculando tarefas_proximo_recalculo

--- backend/src/pp/tarefa/tarefa.service.utils.ts
+++ backend/src/pp/tarefa/tarefa.service.utils.ts
@@ -1,6 +1,5 @@
 import { HttpException, Injectable } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
-
 import { PrismaService } from '../../prisma/prisma.service';
 
 @Injectable()
