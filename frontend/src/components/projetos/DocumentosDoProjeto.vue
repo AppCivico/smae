8065e3eb248ccbc12f78b8e5196b2814fe319a03
Sobral <robson@oficiodesign.com>
2023-04-13 15:25:20 -0300

    Mover documentos de projeto para um componente

---
+++ frontend/src/components/projetos/DocumentosDoProjeto.vue
@@ -0,0 +1,109 @@
+<script setup>
+import { useAlertStore } from '@/stores/alert.store';
+import { useProjetosStore } from '@/stores/projetos.store.ts';
+import { storeToRefs } from 'pinia';
+
+const baseUrl = `${import.meta.env.VITE_API_URL}`;
+
+const alertStore = useAlertStore();
+const projetosStore = useProjetosStore();
+const {
+  chamadasPendentes,
+  arquivos,
+  erro,
+} = storeToRefs(projetosStore);
+
+function excluirArquivo(id) {
+  alertStore.confirmAction('Deseja remover o arquivo?', () => {
+    projetosStore.excluirArquivo(id);
+  }, 'Remover');
+}
+
+function iniciar() {
+  projetosStore.buscarArquivos();
+}
+
+iniciar();
+</script>
+
+<template>
+  <table
+    v-if="arquivos.length"
+    class="tablemain mb1"
+  >
+    <caption>
+      Lista de documentos
+    </caption>
+    <col>
+    <col>
+    <col class="col--botão-de-ação">
+    <thead>
+      <tr>
+        <th>Arquivo</th>
+        <th>Descrição</th>
+        <th />
+      </tr>
+    </thead>
+    <tbody>
+      <template
+        v-for="item in arquivos"
+        :key="item.id"
+      >
+        <tr>
+          <td
+            class="cell--minimum"
+            :class="{ loading: chamadasPendentes.arquivos }"
+          >
+            <a
+              :href="baseUrl + '/download/' + item?.arquivo?.download_token"
+              download
+            >
+              {{ item?.arquivo?.nome_original ?? '-' }}
+            </a>
+          </td>
+          <td
+            class=""
+            :class="{ loading: chamadasPendentes.arquivos }"
+          >
+            <a
+              :href="baseUrl + '/download/' + item?.arquivo?.download_token"
+              download
+            >
+              {{ item?.arquivo?.descricao ?? '-' }}
+            </a>
+          </td>
+          <td>
+            <button
+              class="like-a__text"
+              type="button"
+              :disabled="chamadasPendentes.arquivos"
+              @click="excluirArquivo(item.id)"
+            >
+              <svg
+                width="20"
+                height="20"
+                arial-label="excluir"
+              >
+                <use xlink:href="#i_remove" />
+              </svg>
+            </button>
+          </td>
+        </tr>
+      </template>
+    </tbody>
+  </table>
+
+  <span
+    v-if="chamadasPendentes?.arquivos"
+    class="spinner"
+  >Carregando</span>
+
+  <div
+    v-if="erro"
+    class="error p1"
+  >
+    <div class="error-msg">
+      {{ erro }}
+    </div>
+  </div>
+</template>
--- frontend/src/views/projetos/ProjetosDocumentos.vue
+++ frontend/src/views/projetos/ProjetosDocumentos.vue
@@ -1,15 +1,44 @@
 <script setup>
+import DocumentosDoProjeto from '@/components/projetos/DocumentosDoProjeto.vue';
+import MenuDeMudançaDeStatusDeProjeto from '@/components/projetos/MenuDeMudançaDeStatusDeProjeto.vue';
+import { useProjetosStore } from '@/stores/projetos.store.ts';
+import { storeToRefs } from 'pinia';
 import { useRoute } from 'vue-router';
 
+const projetosStore = useProjetosStore();
+const {
+  emFoco,
+} = storeToRefs(projetosStore);
 const route = useRoute();
 </script>
 <template>
   <div class="flex spacebetween center mb2">
-    <h1>
-      {{ typeof route?.meta?.título === 'function'
-        ? route.meta.título()
-        : route?.meta?.título || 'Documentos' }}
-    </h1>
+    <div>
+      <div class="t12 uc w700 tamarelo">
+        Projeto
+        <template v-if="emFoco?.eh_prioritario">
+          prioritário
+        </template>
+      </div>
+
+      <h1>
+        {{ typeof route?.meta?.título === 'function'
+          ? route.meta.título()
+          : route?.meta?.título || 'Documentos' }}
+      </h1>
+    </div>
     <hr class="ml2 f1">
+    <MenuDeMudançaDeStatusDeProjeto />
+
+    <template v-if="emFoco?.id && !emFoco.arquivado">
+      <router-link
+        :to="{ name: 'projetosEditar', params: { projetoId: emFoco.id } }"
+        class="btn big ml2"
+      >
+        Editar
+      </router-link>
+    </template>
   </div>
+
+  <DocumentosDoProjeto />
 </template>
