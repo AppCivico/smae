d7f06328c7ac2757dada98be44bb2d89797a53f4
Sobral <robson@oficiodesign.com>
2023-04-14 15:38:24 -0300

    Listar novos relatórios

---
+++ frontend/src/views/relatorios/RelatoriosDeProjeto.vue
@@ -0,0 +1,23 @@
+<script setup>
+import TabelaBásica from '@/components/relatorios/TabelaBasica.vue';
+import { useAuthStore } from '@/stores/auth.store';
+import { useRelatoriosStore } from '@/stores/relatorios.store';
+import { storeToRefs } from 'pinia';
+import { onMounted } from 'vue';
+
+const relatóriosStore = useRelatoriosStore();
+const { temPermissãoPara } = storeToRefs(useAuthStore());
+
+onMounted(() => {
+  relatóriosStore.clear();
+  relatóriosStore.getAll({ fonte: 'Projeto' });
+});
+</script>
+<template>
+  <div class="flex spacebetween center mb2">
+    <h1>{{ $route.meta.título }}</h1>
+    <hr class="ml2 f1">
+  </div>
+
+  <TabelaBásica />
+</template>
