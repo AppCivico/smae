a621c7ebe9c4496bb1dac4911a3bcd1c1017b036
lucasansei <lucas.ansei@eokoe.com>
2023-04-16 17:59:38 -0300

    Ajustando colunas do report de projeto

--- backend/src/reports/pp-projeto/entities/previsao-custo.entity.ts
+++ backend/src/reports/pp-projeto/entities/previsao-custo.entity.ts
@@ -1,11 +1,27 @@
 import { PartialType, OmitType } from '@nestjs/mapped-types';
 import { ProjetoDetailDto } from '../../../pp/projeto/entities/projeto.entity';
 
-export class RelProjetoRelatorioDto extends PartialType(OmitType(ProjetoDetailDto, ['premissas', 'restricoes', 'fonte_recursos', 'orgaos_participantes'])) {
+export class RelProjetoRelatorioDto extends PartialType(OmitType(ProjetoDetailDto, [
+    'premissas', 'restricoes', 'fonte_recursos',
+    'orgaos_participantes', 'responsaveis_no_orgao_gestor',
+    'selecionado_em', 'em_planejamento_em', 'permissoes',
+    'responsavel', 'portfolio', 'orgao_gestor', 'orgao_responsavel'
+])) {
     fonte_recursos: string | null
     premissas: string | null
     restricoes: string | null
     orgaos_participantes: string | null
+    responsaveis_no_orgao_gestor: string | null
+    responsavel_id: number | null
+    responsavel_nome_exibicao: string | null
+    portfolio_titulo: string
+    portfolio_nivel_maximo_tarefa: number
+    orgao_gestor_id: number
+    orgao_gestor_sigla: string
+    orgao_gestor_descricao: string
+    orgao_responsavel_id: number | null
+    orgao_responsavel_sigla: string | null
+    orgao_responsavel_descricao: string | null
 }
 
 export class RelProjetoCronogramaDto {
