0312fc971adfefad614e503eb6727dcc3a389293
lucasansei <lucas.ansei@eokoe.com>
2023-04-16 12:50:29 -0300

    Adicionando filtros para relatório de projetos

--- backend/src/reports/pp-projetos/dto/create-projetos.dto.ts
+++ backend/src/reports/pp-projetos/dto/create-projetos.dto.ts
@@ -1,5 +1,10 @@
 import { OmitType, PartialType } from "@nestjs/swagger";
+import { IsOptional, IsString } from "class-validator";
 import { FilterProjetoDto } from "src/pp/projeto/dto/filter-projeto.dto";
 
 
-export class CreateRelProjetosDto extends OmitType(PartialType(FilterProjetoDto), ['eh_prioritario', 'arquivado']) {}
+export class CreateRelProjetosDto extends OmitType(PartialType(FilterProjetoDto), ['eh_prioritario', 'arquivado']) {
+    @IsOptional()
+    @IsString()
+    codigo?: string
+}
