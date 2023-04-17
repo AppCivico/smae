11e50bfbe7003a8f8d144745cd06f1ba98270340
Sobral <robson@oficiodesign.com>
2023-04-17 13:30:02 -0300

    Adicionar formulário de solicitação de relatórios de previsão de custo

--- frontend/src/views/relatorios/RelatoriosDePrevisaoDeCusto.vue
+++ frontend/src/views/relatorios/RelatoriosDePrevisaoDeCusto.vue
@@ -3,20 +3,24 @@ import TabelaBásica from '@/components/relatorios/TabelaBasica.vue';
 import { useAuthStore } from '@/stores/auth.store';
 import { useRelatoriosStore } from '@/stores/relatorios.store';
 import { storeToRefs } from 'pinia';
-import { onMounted } from 'vue';
 
 const relatóriosStore = useRelatoriosStore();
 const { temPermissãoPara } = storeToRefs(useAuthStore());
 
-onMounted(() => {
-  relatóriosStore.clear();
-  relatóriosStore.getAll({ fonte: 'PrevisaoCusto' });
-});
+relatóriosStore.clear();
+relatóriosStore.getAll({ fonte: 'PrevisaoCusto' });
 </script>
 <template>
   <div class="flex spacebetween center mb2">
     <h1>{{ $route.meta.título }}</h1>
     <hr class="ml2 f1">
+    <router-link
+      v-if="temPermissãoPara('Reports.executar')"
+      :to="{ name: 'novoRelatórioDePrevisãoECusto' }"
+      class="btn big ml2"
+    >
+      Novo relatório
+    </router-link>
   </div>
 
   <TabelaBásica />
