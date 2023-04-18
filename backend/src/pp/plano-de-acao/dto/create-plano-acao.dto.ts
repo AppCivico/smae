504fcae7715adbb2513408d530cb5d386cd775fc
Renato CRON <renato.cron@gmail.com>
2023-04-18 17:27:47 -0300

    Colocando limites em create-plano-acao.dto.ts

--- backend/src/pp/plano-de-acao/dto/create-plano-acao.dto.ts
+++ backend/src/pp/plano-de-acao/dto/create-plano-acao.dto.ts
@@ -1,19 +1,21 @@
 import { Type } from "class-transformer"
-import { IsNumber, IsOptional, IsString, MaxLength, ValidateIf } from "class-validator"
+import { IsIn, IsInt, IsNumber, IsOptional, IsString, Max, MaxLength, Min, ValidateIf } from "class-validator"
 import { IsOnlyDate } from "src/common/decorators/IsDateOnly"
 
 export class CreatePlanoAcaoDto {
-    @IsNumber()
+    @IsInt()
     projeto_risco_id: number
 
-    @IsNumber()
+    @IsInt()
     orgao_id: number
 
     @IsString()
     @IsOptional()
+    @MaxLength(2000)
     responsavel?: string
 
     @IsString()
+    @MaxLength(20000)
     contramedida: string
 
     @IsOnlyDate()
@@ -21,14 +23,17 @@ export class CreatePlanoAcaoDto {
     @ValidateIf((object, value) => value !== null)
     prazo_contramedida: Date | null
 
-    @IsNumber()
     @IsOptional()
     @ValidateIf((object, value) => value !== null)
+    @IsNumber({ maxDecimalPlaces: 2, allowInfinity: false, allowNaN: false }, { message: '$property| até duas casas decimais' })
+    @Min(0, { message: '$property| Valor Empenhado precisa ser positivo ou zero' })
     custo: number | null
 
-    @IsNumber()
+    @IsNumber({ maxDecimalPlaces: 2, allowInfinity: false, allowNaN: false }, { message: '$property| até duas casas decimais' })
     @IsOptional()
     @ValidateIf((object, value) => value !== null)
+    @Min(0, { message: '$property| Valor Empenhado precisa ser positivo ou zero' })
+    @Max(100, { message: '$property| Valor Empenhado precisa até 100' })
     custo_percentual: number | null
 
     @IsOptional()
