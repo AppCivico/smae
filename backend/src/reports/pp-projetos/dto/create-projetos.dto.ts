6d347a3f96dc91327e991afe618634efa78cdb3b
lucasansei <lucas.ansei@eokoe.com>
2023-04-18 17:51:20 -0300

    Aceitar apenas um valor de status

--- backend/src/reports/pp-projetos/dto/create-projetos.dto.ts
+++ backend/src/reports/pp-projetos/dto/create-projetos.dto.ts
@@ -1,10 +1,18 @@
-import { OmitType, PartialType } from "@nestjs/swagger";
-import { IsOptional, IsString } from "class-validator";
+import { ApiProperty, OmitType, PartialType } from "@nestjs/swagger";
+import { ProjetoStatus } from "@prisma/client";
+import { IsEnum, IsOptional, IsString } from "class-validator";
 import { FilterProjetoDto } from "src/pp/projeto/dto/filter-projeto.dto";
 
 
-export class CreateRelProjetosDto extends OmitType(PartialType(FilterProjetoDto), ['eh_prioritario', 'arquivado']) {
+export class CreateRelProjetosDto extends OmitType(PartialType(FilterProjetoDto), ['eh_prioritario', 'arquivado', 'status']) {
     @IsOptional()
     @IsString()
     codigo?: string
+
+    @IsOptional()
+    @ApiProperty({ enum: ProjetoStatus, enumName: 'ProjetoStatus' })
+    @IsEnum(ProjetoStatus, {
+        message: '$property| Precisa ser um dos seguintes valores: ' + Object.values(ProjetoStatus).join(', ')
+    })
+    status?: ProjetoStatus;
 }
