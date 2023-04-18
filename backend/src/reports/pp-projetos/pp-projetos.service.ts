139be0c0c5d640db1cbd2d31521eb0aa7b6ae895
lucasansei <lucas.ansei@eokoe.com>
2023-04-18 17:54:35 -0300

    Adicionando cast para param de string

--- backend/src/reports/pp-projetos/pp-projetos.service.ts
+++ backend/src/reports/pp-projetos/pp-projetos.service.ts
@@ -332,7 +332,7 @@ export class PPProjetosService implements ReportableService {
         }
 
         if (filters.status) {
-            whereConditions.push(`projeto.status = $${paramIndex}`);
+            whereConditions.push(`projeto.status = $${paramIndex}::ProjetoStatus`);
             queryParams.push(filters.status);
             paramIndex++;
         }
