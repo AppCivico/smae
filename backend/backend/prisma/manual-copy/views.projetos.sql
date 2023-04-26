0a4bcd8c0f0d8ca906ffca8260a5e8c26c6beb60
Renato CRON <renato.cron@gmail.com>
2023-04-26 15:53:59 -0300

    update view

--- backend/backend/prisma/manual-copy/views.projetos.sql
+++ backend/backend/prisma/manual-copy/views.projetos.sql
@@ -10,6 +10,27 @@ when p.status = 'Validado' then  'Validado'
 when p.status = 'EmAcompanhamento' then  'Em Acompanhamento'
 when p.status = 'Suspenso' then  'Suspenso'
 when p.status = 'Fechado' then  'Fechado'
-end as "Status"
+end as "Status",
 
-from projeto p;
+case when previsao_custo > 0 then
+    round((realizado_custo::numeric / previsao_custo::numeric) * 100.0)
+else null
+end as percentual_custo_realizado,
+
+case
+when p.status = 'Registrado' then  1
+when p.status = 'Selecionado' then  2
+when p.status = 'EmPlanejamento' then 3
+when p.status = 'Planejado' then  4
+when p.status = 'Validado' then  5
+when p.status = 'EmAcompanhamento' then  6
+when p.status = 'Suspenso' then   6
+when p.status = 'Fechado' then  7
+end as "numero_status"
+
+
+from projeto p
+where p.removido_em IS NULL
+order by p.status, p.codigo;
+-- isso vai garantir a ordem do enum (Registrado, Selecionado, EmPlanejamento, Planejado, Validado, EmAcompanhamento, Suspenso, Fechado)
+-- e depois o código
