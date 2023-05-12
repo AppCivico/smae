2f7382169cca728cb433167cc45fed08591d81e3
lucasansei <lucas.ansei@eokoe.com>
2023-05-12 11:23:22 -0300

    Adicionando obrigatoriedade de ods_id no create de tag

--- backend/src/tag/dto/create-tag.dto.ts
+++ backend/src/tag/dto/create-tag.dto.ts
@@ -28,9 +28,8 @@ export class CreateTagDto {
     /**
      * ID do ODS (opcional, enviar null para remover/não existir)
      */
-    @IsOptional()
     @IsInt({ message: '$property| ODS no PATCH pode não existir (fica o antigo), se enviado precisa ser nulo ou numérico' })
     @ValidateIf((object, value) => value !== null)
     @Type(() => Number)
-    ods_id?: number;
+    ods_id: number;
 }
