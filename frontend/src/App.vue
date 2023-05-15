a54e272d52f889fb34eaab0a6fa012ffb640a41c
Sobral <robson@oficiodesign.com>
2023-05-15 14:04:36 -0300

    Corrigir busca de permissões a cada montagem de menu

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,11 @@
 <script setup>
 import { Alert, EditModal, SideBar } from '@/components';
+import { useAuthStore } from '@/stores/auth.store';
 import { useRoute } from 'vue-router';
 
 const route = useRoute();
+const authStore = useAuthStore();
+authStore.getDados();
 </script>
 
 <template>
