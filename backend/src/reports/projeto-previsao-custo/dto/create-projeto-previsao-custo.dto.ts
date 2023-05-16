7f53fb15c331d22de4b47b5be05fc4443afd9d32
Renato CRON <renato.cron@gmail.com>
2023-05-15 22:11:53 -0300

    Fix filter required

--- backend/src/reports/projeto-previsao-custo/dto/create-projeto-previsao-custo.dto.ts
+++ backend/src/reports/projeto-previsao-custo/dto/create-projeto-previsao-custo.dto.ts
@@ -1,11 +1,14 @@
-import { OmitType } from "@nestjs/swagger";
+import { ApiProperty, OmitType } from "@nestjs/swagger";
 import { Transform } from "class-transformer";
-import { IsInt } from "class-validator";
+import { IsInt, IsOptional } from "class-validator";
 import { SuperCreateRelPrevisaoCustoDto } from "src/reports/previsao-custo/dto/create-previsao-custo.dto";
 
 export class CreateRelProjetoPrevisaoCustoDto extends OmitType(
     SuperCreateRelPrevisaoCustoDto,
-    ['atividade_id', 'iniciativa_id', 'meta_id', 'pdm_id', 'tags'] as const
+    [
+        'atividade_id', 'iniciativa_id', 'meta_id', 'pdm_id', 'tags',
+        'portfolio_id'// excluindo pra recriar com sem o decorator poluir
+    ] as const
 ) {
     /**
      * required nesse endpoint!
