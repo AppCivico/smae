a6aab68f01550a63e87b671216b19cf5b47149f6
Renato CRON <renato.cron@gmail.com>
2023-05-15 22:07:07 -0300

    Deixar o port como required

--- backend/src/reports/projeto-previsao-custo/dto/create-projeto-previsao-custo.dto.ts
+++ backend/src/reports/projeto-previsao-custo/dto/create-projeto-previsao-custo.dto.ts
@@ -1,7 +1,17 @@
 import { OmitType } from "@nestjs/swagger";
+import { Transform } from "class-transformer";
+import { IsInt } from "class-validator";
 import { SuperCreateRelPrevisaoCustoDto } from "src/reports/previsao-custo/dto/create-previsao-custo.dto";
 
 export class CreateRelProjetoPrevisaoCustoDto extends OmitType(
     SuperCreateRelPrevisaoCustoDto,
     ['atividade_id', 'iniciativa_id', 'meta_id', 'pdm_id', 'tags'] as const
-) { }
+) {
+    /**
+     * required nesse endpoint!
+     * @example "21"
+     */
+    @IsInt()
+    @Transform(({ value }: any) => +value)
+    portfolio_id: number;
+}
