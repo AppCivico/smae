26e3ae0e0d5729bf27d2ce2a40fb7de9c5ad9d86
Renato Cron <renato.cron@gmail.com>
2023-05-19 10:26:48 -0300

    Add campo valor_realizado_acumulado

--- backend/src/mf/auxiliar/dto/auxiliar.dto.ts
+++ backend/src/mf/auxiliar/dto/auxiliar.dto.ts
@@ -8,8 +8,15 @@ export class AutoPreencherValorDto {
         {},
         { message: 'Precisa ser um número com até 30 dígitos antes do ponto, e até 30 dígitos após, enviado em formato String ou vazio para remover' },
     )
-    @ValidateIf((object, value) => value !== '')
     valor_realizado: string;
+
+    @IsOptional()
+    @IsNumberString(
+        {},
+        { message: 'Precisa ser um número com até 30 dígitos antes do ponto, e até 30 dígitos após, enviado em formato String ou vazio para remover' },
+    )
+    @ValidateIf((object, value) => value !== '')
+    valor_realizado_acumulado: string;
 
     @IsOptional()
     @IsBoolean()
