8330080495dca5e9795ad52771d865ce50e0acc8
Sobral <robson@oficiodesign.com>
2023-05-19 10:30:43 -0300

    Formatar código

--- frontend/src/views/pdm/EdicaoOrcamento.vue
+++ frontend/src/views/pdm/EdicaoOrcamento.vue
@@ -1,72 +1,118 @@
 <script setup>
-	import { ref } from 'vue';
-	import { Form, Field } from 'vee-validate';
-	import { storeToRefs } from 'pinia';
-	import { useAlertStore, useEditModalStore, usePdMStore } from '@/stores';
-	const pr = defineProps(['props']);
-	const props = pr.props
-	const pdm_id = props.pdm_id;
+import { useAlertStore, useEditModalStore, usePdMStore } from '@/stores';
+import { storeToRefs } from 'pinia';
+import { Form } from 'vee-validate';
+import { ref } from 'vue';
 
-	const alertStore = useAlertStore();
-	const editModalStore = useEditModalStore();
+const pr = defineProps(['props']);
+const { props } = pr;
+const { pdm_id } = props;
 
-	const PdMStore = usePdMStore();
-	const { singlePdm } = storeToRefs(PdMStore);
+const alertStore = useAlertStore();
+const editModalStore = useEditModalStore();
 
-	let anosOrcamento = ref({});
-	(async()=>{
-		await PdMStore.getById(pdm_id);
-		anosOrcamento.value = singlePdm.value.orcamento_config;
-	})();
+const PdMStore = usePdMStore();
+const { singlePdm } = storeToRefs(PdMStore);
 
-	async function onSubmit(values) {
-	    try {
-	        var msg;
-	        var r;
+const anosOrcamento = ref({});
+(async () => {
+  await PdMStore.getById(pdm_id);
+  anosOrcamento.value = singlePdm.value.orcamento_config;
+})();
 
-	        values.orcamento_config = anosOrcamento.value;
+async function onSubmit(values) {
+  try {
+    let msg;
+    let r;
 
-            r = await PdMStore.updatePermissoesOrcamento(singlePdm.value.id, values);
-            msg = 'Dados salvos com sucesso!';
+    values.orcamento_config = anosOrcamento.value;
 
-	        if(r == true){
-	            editModalStore.clear();
-	            alertStore.success(msg);
-	        }
-	    } catch (error) {
-	        alertStore.error(error);
-	    }
-	}
+    r = await PdMStore.updatePermissoesOrcamento(singlePdm.value.id, values);
+    msg = 'Dados salvos com sucesso!';
+
+    if (r == true) {
+      editModalStore.clear();
+      alertStore.success(msg);
+    }
+  } catch (error) {
+    alertStore.error(error);
+  }
+}
 </script>
 <template>
-	<div class="flex spacebetween center mb2">
-	    <h2 class="mb0">Permissões para edição do orçamento</h2>
-	    <hr class="ml2 f1"/>
-	    <button @click="props.checkClose" class="btn round ml2"><svg width="12" height="12"><use xlink:href="#i_x"></use></svg></button>
-	</div>
-	<hr class="mt2 mb2">
-	<template v-if="!(singlePdm?.loading || singlePdm?.error)">
-		<Form @submit="onSubmit" v-slot="{ errors, isSubmitting }">
-			<div v-for="y in anosOrcamento" :key="y.id" class="mb2">
-				<h4>{{y.ano_referencia}}</h4>
-				<label class="block mb05"><input v-model="y.previsao_custo_disponivel" class="inputcheckbox" type="checkbox" value=true /><span>Previsão de custo</span></label>
-				<label class="block mb05"><input v-model="y.planejado_disponivel" class="inputcheckbox" type="checkbox" value=true /><span>Orçamento planejado</span></label>
-				<label class="block mb05"><input v-model="y.execucao_disponivel" class="inputcheckbox" type="checkbox" value=true /><span>Execução orçamentária</span></label>
-			</div>
-			<div class="flex spacebetween center mb2">
-			    <hr class="mr2 f1"/>
-			    <button class="btn outline tcprimary bgnone mr1" type="button" @click="props.checkClose">Cancelar</button>
-			    <button class="btn" :disabled="isSubmitting">Salvar</button>
-			    <hr class="ml2 f1"/>
-			</div>
-		</Form>
-	</template>
-    <template v-if="singlePdm?.loading">
-        <span class="spinner">Carregando</span>
-    </template>
-    <template v-if="singlePdm?.error||error">
-        <div class="error p1">
-            <div class="error-msg">{{singlePdm.error??error}}</div>
-        </div>
-    </template>
+  <div class="flex spacebetween center mb2">
+    <h2 class="mb0">
+      Permissões para edição do orçamento
+    </h2>
+    <hr class="ml2 f1">
+    <button
+      class="btn round ml2"
+      @click="props.checkClose"
+    >
+      <svg
+        width="12"
+        height="12"
+      ><use xlink:href="#i_x" /></svg>
+    </button>
+  </div>
+  <hr class="mt2 mb2">
+  <template v-if="!(singlePdm?.loading || singlePdm?.error)">
+    <Form
+      v-slot="{ errors, isSubmitting }"
+      @submit="onSubmit"
+    >
+      <div
+        v-for="y in anosOrcamento"
+        :key="y.id"
+        class="mb2"
+      >
+        <h4>{{ y.ano_referencia }}</h4>
+        <label class="block mb05"><input
+          v-model="y.previsao_custo_disponivel"
+          class="inputcheckbox"
+          type="checkbox"
+          value="true"
+        ><span>Previsão de custo</span></label>
+        <label class="block mb05"><input
+          v-model="y.planejado_disponivel"
+          class="inputcheckbox"
+          type="checkbox"
+          value="true"
+        ><span>Orçamento planejado</span></label>
+        <label class="block mb05"><input
+          v-model="y.execucao_disponivel"
+          class="inputcheckbox"
+          type="checkbox"
+          value="true"
+        ><span>Execução orçamentária</span></label>
+      </div>
+      <div class="flex spacebetween center mb2">
+        <hr class="mr2 f1">
+        <button
+          class="btn outline tcprimary bgnone mr1"
+          type="button"
+          @click="props.checkClose"
+        >
+          Cancelar
+        </button>
+        <button
+          class="btn"
+          :disabled="isSubmitting"
+        >
+          Salvar
+        </button>
+        <hr class="ml2 f1">
+      </div>
+    </Form>
+  </template>
+  <template v-if="singlePdm?.loading">
+    <span class="spinner">Carregando</span>
+  </template>
+  <template v-if="singlePdm?.error || error">
+    <div class="error p1">
+      <div class="error-msg">
+        {{ singlePdm.error ?? error }}
+      </div>
+    </div>
+  </template>
 </template>
