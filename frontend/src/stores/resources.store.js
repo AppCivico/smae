0a62cfc1788726dcf50b4e6d3e59156e20feef2f
Sobral <robson@oficiodesign.com>
2023-05-23 15:20:31 -0300

    Reduzir dependências cíclicas

--- frontend/src/stores/resources.store.js
+++ frontend/src/stores/resources.store.js
@@ -1,67 +1,65 @@
 import { defineStore } from 'pinia';
-import { requestS } from '@/helpers';
+
 const baseUrl = `${import.meta.env.VITE_API_URL}`;
 
 export const useResourcesStore = defineStore({
-    id: 'resources',
-    state: () => ({
-        resources: {},
-        tempResources: {},
-    }),
-    actions: {
-        clear (){
-            this.resources = {};
-            this.tempResources = {};
-        },
-        async getAll() {
-            this.resources = { loading: true };
-            try {
-                let r = await requestS.get(`${baseUrl}/unidade-medida`);
-                this.resources = r.linhas;
-            } catch (error) {
-                this.resources = { error };
-            }
-        },
-        async getById(id) {
-            this.tempResources = { loading: true };
-            try {
-                if(!this.resources.length){
-                    await this.getAll();
-                }
-                this.tempResources = this.resources.find((u)=>u.id == id);
-                if(!this.tempResources) throw 'Fonte de recurso não encontrada';
-            } catch (error) {
-                this.tempResources = { error };
-            }
-        },
-        async insertType(params) {
-            if(await requestS.post(`${baseUrl}/unidade-medida`, params)) return true;
-            return false;
-        },
-        async updateType(id, params) {
-            var m = {
-                sigla: params.sigla,
-                fonte: params.fonte,
-            };
-            if(await requestS.patch(`${baseUrl}/unidade-medida/${id}`, m)) return true;
-            return false;
-        },
-        async deleteType(id) {
-            if(await requestS.delete(`${baseUrl}/unidade-medida/${id}`)) return true;
-            return false;
-        },
-        async filterResources(f){
-            this.tempResources = { loading: true };
-            try {
-                if(!this.resources.length){
-                    await this.getAll();
-                }
-                this.tempResources = f ? this.resources.filter((u)=>{
-                    return f.textualSearch ? (u.fonte+u.sigla).toLowerCase().includes(f.textualSearch.toLowerCase()) : 1;
-                }) : this.resources;
-            } catch (error) {
-                this.tempResources = { error };
-            }
-        },
-    }
+  id: 'resources',
+  state: () => ({
+    resources: {},
+    tempResources: {},
+  }),
+  actions: {
+    clear() {
+      this.resources = {};
+      this.tempResources = {};
+    },
+    async getAll() {
+      this.resources = { loading: true };
+      try {
+        const r = await this.requestS.get(`${baseUrl}/unidade-medida`);
+        this.resources = r.linhas;
+      } catch (error) {
+        this.resources = { error };
+      }
+    },
+    async getById(id) {
+      this.tempResources = { loading: true };
+      try {
+        if (!this.resources.length) {
+          await this.getAll();
+        }
+        this.tempResources = this.resources.find((u) => u.id == id);
+        if (!this.tempResources) throw 'Fonte de recurso não encontrada';
+      } catch (error) {
+        this.tempResources = { error };
+      }
+    },
+    async insertType(params) {
+      if (await this.requestS.post(`${baseUrl}/unidade-medida`, params)) return true;
+      return false;
+    },
+    async updateType(id, params) {
+      const m = {
+        sigla: params.sigla,
+        fonte: params.fonte,
+      };
+      if (await this.requestS.patch(`${baseUrl}/unidade-medida/${id}`, m)) return true;
+      return false;
+    },
+    async deleteType(id) {
+      if (await this.requestS.delete(`${baseUrl}/unidade-medida/${id}`)) return true;
+      return false;
+    },
+    async filterResources(f) {
+      this.tempResources = { loading: true };
+      try {
+        if (!this.resources.length) {
+          await this.getAll();
+        }
+        this.tempResources = f ? this.resources.filter((u) => (f.textualSearch ? (u.fonte + u.sigla).toLowerCase().includes(f.textualSearch.toLowerCase()) : 1)) : this.resources;
+      } catch (error) {
+        this.tempResources = { error };
+      }
+    },
+  },
 });
