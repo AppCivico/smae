702929dcaea2a1ff394032939edc1280817669e2
Renato CRON <renato.cron@gmail.com>
2023-05-29 11:57:51 -0300

    Fix import migration

--- backend/src/importacao-orcamento/importacao-orcamento.service.ts
+++ backend/src/importacao-orcamento/importacao-orcamento.service.ts
@@ -1,14 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { RecordWithId } from 'src/common/dto/record-with-id.dto';
 import { CreateImportacaoOrcamentoDto } from './dto/create-importacao-orcamento.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UploadService } from 'src/upload/upload.service';
 import { Prisma } from '@prisma/client';
 import { PessoaFromJwt } from 'src/auth/models/PessoaFromJwt';
+import { JOB_IMPORTACAO_ORCAMENTO_LOCK } from 'src/common/dto/locks';
+import { Cron } from '@nestjs/schedule';
+import { DateTime } from 'luxon';
+import { SYSTEM_TIMEZONE } from 'src/common/date2ymd';
 
 
 @Injectable()
 export class ImportacaoOrcamentoService {
+    private readonly logger = new Logger(ImportacaoOrcamentoService.name);
     constructor(
         private readonly prisma: PrismaService,
         private readonly uploadService: UploadService,
@@ -35,6 +40,77 @@ export class ImportacaoOrcamentoService {
         );
 
         return { id: created.id }
+    }
+
+    @Cron('5 * * * *')
+    async handleCron() {
+        if (Boolean(process.env['DISABLE_IMPORTACAO_ORCAMENTO_CRONTAB'])) return;
+
+        await this.prisma.$transaction(
+            async (prisma: Prisma.TransactionClient) => {
+                this.logger.debug(`Adquirindo lock para verificação de relatórios`);
+                const locked: {
+                    locked: boolean;
+                }[] = await prisma.$queryRaw`SELECT
+                pg_try_advisory_xact_lock(${JOB_IMPORTACAO_ORCAMENTO_LOCK}) as locked
+            `;
+                if (!locked[0].locked) {
+                    this.logger.debug(`Já está em processamento...`);
+                    return;
+                }
+
+                await this.verificaImportacaoOrcamento();
+
+            },
+            {
+                maxWait: 30000,
+                timeout: 60 * 1000 * 5,
+                isolationLevel: 'Serializable',
+            },
+        );
+    }
+
+    async executaImportacaoOrcamento(filaId: number) {
+        try {
+            await this.verificaImportacaoOrcamento(filaId);
+        } catch (error) {
+            this.logger.error(`Falha ao executar verificaImportacaoOrcamento(${filaId}): ${error}`);
+        }
+    }
+
+    private async verificaImportacaoOrcamento(filtroId?: number | undefined) {
+
+        const pending = await this.prisma.importacaoOrcamento.findMany({
+            where: {
+
+                processado_em: null,
+                AND: [
+                    {
+                        OR: [
+                            { id: filtroId },
+                            { congelado_em: null },
+                            {
+                                congelado_em: {
+                                    lt: DateTime.now().minus({ hour: 1 }).toJSDate(),
+                                }
+                            },
+                        ]
+                    }
+                ]
+            }
+        });
+
+        for (const job of pending) {
+
+            await this.prisma.importacaoOrcamento.update({
+                where: { id: job.id },
+                data: {
+                    congelado_em: new Date(Date.now())
+                }
+            });
+
+
+        }
     }
 
 
