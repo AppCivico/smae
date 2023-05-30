21d93dbc8771f6a0a58d9dc365ca6ec3d0d7b86b
Renato CRON <renato.cron@gmail.com>
2023-05-30 15:56:06 -0300

    Endpoint findAll

--- backend/src/importacao-orcamento/importacao-orcamento.controller.ts
+++ backend/src/importacao-orcamento/importacao-orcamento.controller.ts
@@ -1,11 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { ApiBearerAuth, ApiUnauthorizedResponse } from '@nestjs/swagger';
 import { Roles } from 'src/auth/decorators/roles.decorator';
 import { RecordWithId } from 'src/common/dto/record-with-id.dto';
-import { CreateImportacaoOrcamentoDto } from './dto/create-importacao-orcamento.dto';
+import { CreateImportacaoOrcamentoDto, FilterImportacaoOrcamentoDto } from './dto/create-importacao-orcamento.dto';
 import { ImportacaoOrcamentoService } from './importacao-orcamento.service';
 import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
 import { PessoaFromJwt } from 'src/auth/models/PessoaFromJwt';
+import { ListImportacaoOrcamentoDto } from './entities/importacao-orcamento.entity';
 
 @Controller('importacao-orcamento')
 export class ImportacaoOrcamentoController {
@@ -15,8 +16,18 @@ export class ImportacaoOrcamentoController {
     @ApiBearerAuth('access-token')
     @ApiUnauthorizedResponse()
     @Roles('CadastroMeta.orcamento', 'Projeto.orcamento')
-    async create(@Body() dto: CreateImportacaoOrcamentoDto, @CurrentUser() user: PessoaFromJwt) : Promise<RecordWithId> {
+    async create(@Body() dto: CreateImportacaoOrcamentoDto, @CurrentUser() user: PessoaFromJwt): Promise<RecordWithId> {
         return this.importacaoOrcamentoService.create(dto, user);
+    }
+
+    @Get()
+    @ApiBearerAuth('access-token')
+    @ApiUnauthorizedResponse()
+    @Roles('CadastroMeta.orcamento', 'Projeto.orcamento')
+    async findAll(@Query() filters: FilterImportacaoOrcamentoDto, @CurrentUser() user: PessoaFromJwt): Promise<ListImportacaoOrcamentoDto> {
+        return {
+            linhas: await this.importacaoOrcamentoService.findAll(filters, user)
+        };
     }
 
 
