86d5115875778797900959ce627b7f9f563d26d7
Renato CRON <renato.cron@gmail.com>
2023-05-30 19:03:01 -0300

    sorry fix build

--- backend/src/importacao-orcamento/importacao-orcamento.module.ts
+++ backend/src/importacao-orcamento/importacao-orcamento.module.ts
@@ -7,12 +7,16 @@ import { OrcamentoRealizadoModule as PdmOrcamentoRealizadoModule } from 'src/orc
 import { OrcamentoRealizadoModule as ProjetoOrcamentoRealizadoModule } from 'src/pp/orcamento-realizado/orcamento-realizado.module';
 import { DotacaoModule } from 'src/dotacao/dotacao.module';
 import { UploadModule } from 'src/upload/upload.module';
+import { ProjetoModule } from 'src/pp/projeto/projeto.module';
+import { MetaModule } from 'src/meta/meta.module';
 
 @Module({
     imports: [
         PrismaModule,
         DotacaoModule,
         UploadModule,
+        forwardRef(() => ProjetoModule),
+        forwardRef(() => MetaModule),
         forwardRef(() => ProjetoOrcamentoRealizadoModule),
         forwardRef(() => PdmOrcamentoRealizadoModule),
     ],
