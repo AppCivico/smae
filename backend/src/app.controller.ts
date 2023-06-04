0959990f4b9ed20c3cf94b227789db8f54578330
Renato Cron <renato.cron@gmail.com>
2023-06-04 13:26:24 -0300

    Removendo rota no / do app que tava dando 500

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -16,38 +16,39 @@ export class AppController {
         return 'pong';
     }
 
-    @Get()
-    @IsPublic()
-    async getUsers(@Res() res: Response) {
-        const data = {
-            projectName: 'Example Project',
-            projectStatus: 'Em Elaboração',
-            projectDescription: 'A sample project for demonstration',
-            orgaoResponsavel: ['Org 1', 'Org 2'],
-            projectResponsible: 'John Doe',
-            startDate: '2023-04-20',
-            endDate: '2023-08-30',
-            estimatedCost: 'R$ 500,000',
-            resourceSource: 'Fonte de Recurso',
-            origin: 'PDM 2021-2024, Agenda de Prioridades SGM, Outros',
-            metaCode: 'ABC123',
-            metaText: 'Example Text',
-            escopo: ['asda asdas dasda dsasdasdadasdas'],
-            etapas: ['Etapa 1', 'sadas adsdas dasdasda asdddasd asddds aaaaas asdasd', 'Foo Barr'],
-            orgaosEnvolvidos: ['Órgão 1', 'Órgão 2', 'Órgão 3'],
-            documentosRelacionados: ['Doc 1', 'Doc 2', 'Doc 3'],
-            dataEntrada: '2023-04-20'
-        };
-
-        return res.render('users', { ...data });
-        //        const templatesDir = join(__dirname, '..', 'templates');
-
-        //        ejs.renderFile(join(templatesDir, 'users.ejs'), { users })
-        //            .then(renderedHtml => {
-        //                res.json({ html: renderedHtml });
-        //            })
-        //            .catch(error => {
-        //                res.status(500).json({ error: 'An error occurred while rendering the template.' });
-        //            });
-    }
+    // exemplo de como usar o ejs via server-side
+    //    @Get()
+    //    @IsPublic()
+    //    async getUsers(@Res() res: Response) {
+    //        const data = {
+    //            projectName: 'Example Project',
+    //            projectStatus: 'Em Elaboração',
+    //            projectDescription: 'A sample project for demonstration',
+    //            orgaoResponsavel: ['Org 1', 'Org 2'],
+    //            projectResponsible: 'John Doe',
+    //            startDate: '2023-04-20',
+    //            endDate: '2023-08-30',
+    //            estimatedCost: 'R$ 500,000',
+    //            resourceSource: 'Fonte de Recurso',
+    //            origin: 'PDM 2021-2024, Agenda de Prioridades SGM, Outros',
+    //            metaCode: 'ABC123',
+    //            metaText: 'Example Text',
+    //            escopo: ['asda asdas dasda dsasdasdadasdas'],
+    //            etapas: ['Etapa 1', 'sadas adsdas dasdasda asdddasd asddds aaaaas asdasd', 'Foo Barr'],
+    //            orgaosEnvolvidos: ['Órgão 1', 'Órgão 2', 'Órgão 3'],
+    //            documentosRelacionados: ['Doc 1', 'Doc 2', 'Doc 3'],
+    //            dataEntrada: '2023-04-20'
+    //        };
+    //
+    //        return res.render('users', { ...data });
+    //        //        const templatesDir = join(__dirname, '..', 'templates');
+    //
+    //        //        ejs.renderFile(join(templatesDir, 'users.ejs'), { users })
+    //        //            .then(renderedHtml => {
+    //        //                res.json({ html: renderedHtml });
+    //        //            })
+    //        //            .catch(error => {
+    //        //                res.status(500).json({ error: 'An error occurred while rendering the template.' });
+    //        //            });
+    //    }
 }
