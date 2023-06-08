76d6625be0d040d18da7f3faca919521d1f8d004
lucasansei <lucas.ansei@eokoe.com>
2023-06-08 18:15:34 -0300

    Disparando a trigger apos soft delete

--- backend/prisma/manual-copy/triggers-crono-etapa.sql
+++ backend/prisma/manual-copy/triggers-crono-etapa.sql
@@ -291,7 +291,7 @@ DECLARE
 BEGIN
     SET session_replication_role = replica;
 
-    IF (NEW.percentual_execucao <> OLD.percentual_execucao OR NEW.peso <> OLD.peso) OR TG_OP = 'INSERT' THEN
+    IF (NEW.percentual_execucao <> OLD.percentual_execucao OR NEW.peso <> OLD.peso OR NEW.removido_em <> OLD.removido_em) OR TG_OP = 'INSERT' THEN
         parent_id := NEW.etapa_pai_id;
         WHILE parent_id IS NOT NULL LOOP
             PERFORM calculate_percentual_execucao_for_id(parent_id);
