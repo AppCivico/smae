9512cd57889cd4407f007b801fff8dae4bf67a71
lucasansei <lucas.ansei@eokoe.com>
2023-06-08 18:11:20 -0300

    Disparando recalc quando for INSERT

--- backend/prisma/manual-copy/triggers-crono-etapa.sql
+++ backend/prisma/manual-copy/triggers-crono-etapa.sql
@@ -291,7 +291,7 @@ DECLARE
 BEGIN
     SET session_replication_role = replica;
 
-    IF (NEW.percentual_execucao <> OLD.percentual_execucao OR NEW.peso <> OLD.peso) THEN
+    IF (NEW.percentual_execucao <> OLD.percentual_execucao OR NEW.peso <> OLD.peso) OR TG_OP = 'INSERT' THEN
         parent_id := NEW.etapa_pai_id;
         WHILE parent_id IS NOT NULL LOOP
             PERFORM calculate_percentual_execucao_for_id(parent_id);
