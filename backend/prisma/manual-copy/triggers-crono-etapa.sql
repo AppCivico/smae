5db56097e1f6cfadfae216fb6a7469f3fb6e038b
lucasansei <lucas.ansei@eokoe.com>
2023-06-08 17:54:31 -0300

    Melhorando trigger de pct de exec

--- backend/prisma/manual-copy/triggers-crono-etapa.sql
+++ backend/prisma/manual-copy/triggers-crono-etapa.sql
@@ -270,13 +270,13 @@ BEGIN
         END LOOP;
     END IF;
 
-    IF total_peso > 0 AND is_cronograma = false THEN
+    IF is_cronograma = false THEN
         UPDATE etapa
-        SET percentual_execucao = total_percentual_execucao_peso / total_peso
+        SET percentual_execucao = total_percentual_execucao_peso / NULLIF(total_peso, 0)
         WHERE id = p_id;
-    ELSIF total_peso > 0 AND is_cronograma = true THEN 
+    ELSIF is_cronograma = true THEN 
         UPDATE cronograma
-        SET percentual_execucao = total_percentual_execucao_peso / total_peso
+        SET percentual_execucao = total_percentual_execucao_peso / NULLIF(total_peso, 0)
         WHERE id = p_id;
     END IF;
 
@@ -292,8 +292,6 @@ BEGIN
     SET session_replication_role = replica;
 
     IF (NEW.percentual_execucao <> OLD.percentual_execucao OR NEW.peso <> OLD.peso) THEN
-        PERFORM calculate_percentual_execucao_for_id(NEW.id::INTEGER);
-
         parent_id := NEW.etapa_pai_id;
         WHILE parent_id IS NOT NULL LOOP
             PERFORM calculate_percentual_execucao_for_id(parent_id);
