d30bd90f06523b32d85974f5a3fcd8a743c91db9
lucasansei <lucas.ansei@eokoe.com>
2023-06-11 16:51:40 -0300

    Ajuste no comportamento de ordem para cronograma etapa

--- backend/src/cronograma/cronograma.module.ts
+++ backend/src/cronograma/cronograma.module.ts
@@ -3,9 +3,10 @@ import { EtapaService } from '../etapa/etapa.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { CronogramaController } from './cronograma.controller';
 import { CronogramaService } from './cronograma.service';
+import { CronogramaEtapaModule } from 'src/cronograma-etapas/cronograma-etapas.module';
 
 @Module({
-    imports: [PrismaModule],
+    imports: [PrismaModule, CronogramaEtapaModule],
     controllers: [CronogramaController],
     providers: [CronogramaService, EtapaService],
     exports: [CronogramaService, EtapaService],
