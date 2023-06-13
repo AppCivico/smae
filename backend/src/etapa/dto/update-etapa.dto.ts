82b47fcb493c97084ae57eedee36d784616ee9de
Renato CRON <renato.cron@gmail.com>
2023-06-13 13:23:26 -0300

    rem un dep

--- backend/src/etapa/dto/update-etapa.dto.ts
+++ backend/src/etapa/dto/update-etapa.dto.ts
@@ -1,5 +1,4 @@
 import { OmitType, PartialType } from '@nestjs/swagger';
 import { CreateEtapaDto } from './create-etapa.dto';
-import { IsNumber, IsOptional, Max, Min, ValidateIf } from 'class-validator';
 
 export class UpdateEtapaDto extends PartialType(OmitType(CreateEtapaDto, ['etapa_pai_id', 'ordem'])) {}
