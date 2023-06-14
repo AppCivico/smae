f41ced3f5b1fbc8cd08dde5004b095d4f216d924
Sobral <robson@oficiodesign.com>
2023-06-14 14:53:41 -0300

    Formatar código

--- frontend/src/stores/ods.store.js
+++ frontend/src/stores/ods.store.js
@@ -1,68 +1,67 @@
-import { defineStore } from 'pinia';
 import { requestS } from '@/helpers';
+import { defineStore } from 'pinia';
+
 const baseUrl = `${import.meta.env.VITE_API_URL}`;
 
 export const useODSStore = defineStore({
-    id: 'ODS',
-    state: () => ({
-        ODS: {},
-        tempODS: {},
-    }),
-    actions: {
-        clear (){
-            this.ODS = {};
-            this.tempODS = {};
-        },
-        async getAll() {
-            this.ODS = { loading: true };
-            try {
-                let r = await requestS.get(`${baseUrl}/ods`);    
-                this.ODS = r.linhas;
-            } catch (error) {
-                this.ODS = { error };
-            }
-        },
-        async getById(id) {
-            this.tempODS = { loading: true };
-            try {
-                if(!this.ODS.length){
-                    await this.getAll();
-                }
-                this.tempODS = this.ODS.find((u)=>u.id == id);
-                if(!this.tempODS) throw 'ODS não encontrada';
-            } catch (error) {
-                this.tempODS = { error };
-            }
-        },
-        async insert(params) {
-            if(await requestS.post(`${baseUrl}/ods`, params)) return true;
-            return false;
-        },
-        async update(id, params) {
-            var m = {
-                numero: params.numero,
-                titulo: params.titulo,
-                descricao: params.descricao
-            };
-            if(await requestS.patch(`${baseUrl}/ods/${id}`, m)) return true;
-            return false;
-        },
-        async delete(id) {
-            if(await requestS.delete(`${baseUrl}/ods/${id}`)) return true;
-            return false;
-        },
-        async filterODS(f){
-            this.tempODS = { loading: true };
-            try {
-                if(!this.ODS.length){
-                    await this.getAll();
-                }
-                this.tempODS = f ? this.ODS.filter((u)=>{
-                    return f.textualSearch ? (u.descricao+u.titulo+u.numero).toLowerCase().includes(f.textualSearch.toLowerCase()) : 1;
-                }) : this.ODS;
-            } catch (error) {
-                this.tempODS = { error };
-            }
-        },
-    }
+  id: 'ODS',
+  state: () => ({
+    ODS: {},
+    tempODS: {},
+  }),
+  actions: {
+    clear() {
+      this.ODS = {};
+      this.tempODS = {};
+    },
+    async getAll() {
+      this.ODS = { loading: true };
+      try {
+        const r = await requestS.get(`${baseUrl}/ods`);
+        this.ODS = r.linhas;
+      } catch (error) {
+        this.ODS = { error };
+      }
+    },
+    async getById(id) {
+      this.tempODS = { loading: true };
+      try {
+        if (!this.ODS.length) {
+          await this.getAll();
+        }
+        this.tempODS = this.ODS.find((u) => u.id == id);
+        if (!this.tempODS) throw 'ODS não encontrada';
+      } catch (error) {
+        this.tempODS = { error };
+      }
+    },
+    async insert(params) {
+      if (await requestS.post(`${baseUrl}/ods`, params)) return true;
+      return false;
+    },
+    async update(id, params) {
+      const m = {
+        numero: params.numero,
+        titulo: params.titulo,
+        descricao: params.descricao,
+      };
+      if (await requestS.patch(`${baseUrl}/ods/${id}`, m)) return true;
+      return false;
+    },
+    async delete(id) {
+      if (await requestS.delete(`${baseUrl}/ods/${id}`)) return true;
+      return false;
+    },
+    async filterODS(f) {
+      this.tempODS = { loading: true };
+      try {
+        if (!this.ODS.length) {
+          await this.getAll();
+        }
+        this.tempODS = f ? this.ODS.filter((u) => (f.textualSearch ? (u.descricao + u.titulo + u.numero).toLowerCase().includes(f.textualSearch.toLowerCase()) : 1)) : this.ODS;
+      } catch (error) {
+        this.tempODS = { error };
+      }
+    },
+  },
 });
--- frontend/src/stores/temas.store.js
+++ frontend/src/stores/temas.store.js
@@ -1,100 +1,97 @@
-import { defineStore } from 'pinia';
 import { requestS } from '@/helpers';
 import { usePdMStore } from '@/stores';
+import { defineStore } from 'pinia';
+
 const baseUrl = `${import.meta.env.VITE_API_URL}`;
 
 export const useTemasStore = defineStore({
-    id: 'Temas',
-    state: () => ({
-        Temas: {},
-        tempTemas: {},
-    }),
-    actions: {
-        clear (){
-            this.Temas = {};
-            this.tempTemas = {};
-        },
-        async getAll() {
-            try {
-                if(this.Temas.loading) return;
-                this.Temas = { loading: true };
-                let r = await requestS.get(`${baseUrl}/tema`);    
-                if(r.linhas.length){
-                    const PdMStore = usePdMStore();
-                    if(!PdMStore.PdM.length)await PdMStore.getAll();
-                    this.Temas = r.linhas.map(x=>{
-                        x.pdm = PdMStore.PdM.find(z=>z.id==x.pdm_id);
-                        return x;
-                    });
-                }else{
-                    this.Temas = r.linhas;
-                }
-            } catch (error) {
-                this.Temas = { error };
-            }
-        },
-        async getAllSimple() {
-            this.Temas = { loading: true };
-            try {
-                let r = await requestS.get(`${baseUrl}/tema`);    
-                this.Temas = r.linhas;
-            } catch (error) {
-                this.Temas = { error };
-            }
-        },
-        async getById(id) {
-            this.tempTemas = { loading: true };
-            try {
-                if(!this.Temas.length){
-                    await this.getAllSimple();
-                }
-                this.tempTemas = this.Temas.length?this.Temas.find((u)=>u.id == id):{};
-                if(this.Temas&&!this.tempTemas) throw 'Tipo de documento não encontrado';
-            } catch (error) {
-                this.tempTemas = { error };
-            }
-        },
-        async insert(params) {
-            if(await requestS.post(`${baseUrl}/tema`, params)) return true;
-            return false;
-        },
-        async update(id, params) {
-            var m = {
-                pdm_id: params.pdm_id,
-                descricao: params.descricao,
-            };
-            if(await requestS.patch(`${baseUrl}/tema/${id}`, m)) return true;
-            return false;
-        },
-        async delete(id) {
-            if(await requestS.delete(`${baseUrl}/tema/${id}`)) return true;
-            return false;
-        },
-        async filterTemas(f){
-            this.tempTemas = { loading: true };
-            try {
-                if(!this.Temas.length){
-                    await this.getAll();
-                }
-                this.tempTemas = f ? this.Temas.filter((u)=>{
-                    return f.textualSearch ? (u.descricao+u.titulo+u.codigo+u.extensoes).toLowerCase().includes(f.textualSearch.toLowerCase()) : 1;
-                }) : this.Temas;
-            } catch (error) {
-                this.tempTemas = { error };
-            }
-        },
-        async filterByPdm(pdm_id){
-            this.tempTemas = { loading: true };
-            try {
-                if(!this.Temas.length){
-                    await this.getAllSimple();
-                }
-                this.tempTemas = this.Temas.filter((u)=>{
-                    return u.pdm_id == pdm_id;
-                });
-            } catch (error) {
-                this.tempTemas = { error };
-            }
+  id: 'Temas',
+  state: () => ({
+    Temas: {},
+    tempTemas: {},
+  }),
+  actions: {
+    clear() {
+      this.Temas = {};
+      this.tempTemas = {};
+    },
+    async getAll() {
+      try {
+        if (this.Temas.loading) return;
+        this.Temas = { loading: true };
+        const r = await requestS.get(`${baseUrl}/tema`);
+        if (r.linhas.length) {
+          const PdMStore = usePdMStore();
+          if (!PdMStore.PdM.length) await PdMStore.getAll();
+          this.Temas = r.linhas.map((x) => {
+            x.pdm = PdMStore.PdM.find((z) => z.id == x.pdm_id);
+            return x;
+          });
+        } else {
+          this.Temas = r.linhas;
         }
-    }
+      } catch (error) {
+        this.Temas = { error };
+      }
+    },
+    async getAllSimple() {
+      this.Temas = { loading: true };
+      try {
+        const r = await requestS.get(`${baseUrl}/tema`);
+        this.Temas = r.linhas;
+      } catch (error) {
+        this.Temas = { error };
+      }
+    },
+    async getById(id) {
+      this.tempTemas = { loading: true };
+      try {
+        if (!this.Temas.length) {
+          await this.getAllSimple();
+        }
+        this.tempTemas = this.Temas.length ? this.Temas.find((u) => u.id == id) : {};
+        if (this.Temas && !this.tempTemas) throw 'Tipo de documento não encontrado';
+      } catch (error) {
+        this.tempTemas = { error };
+      }
+    },
+    async insert(params) {
+      if (await requestS.post(`${baseUrl}/tema`, params)) return true;
+      return false;
+    },
+    async update(id, params) {
+      const m = {
+        pdm_id: params.pdm_id,
+        descricao: params.descricao,
+      };
+      if (await requestS.patch(`${baseUrl}/tema/${id}`, m)) return true;
+      return false;
+    },
+    async delete(id) {
+      if (await requestS.delete(`${baseUrl}/tema/${id}`)) return true;
+      return false;
+    },
+    async filterTemas(f) {
+      this.tempTemas = { loading: true };
+      try {
+        if (!this.Temas.length) {
+          await this.getAll();
+        }
+        this.tempTemas = f ? this.Temas.filter((u) => (f.textualSearch ? (u.descricao + u.titulo + u.codigo + u.extensoes).toLowerCase().includes(f.textualSearch.toLowerCase()) : 1)) : this.Temas;
+      } catch (error) {
+        this.tempTemas = { error };
+      }
+    },
+    async filterByPdm(pdm_id) {
+      this.tempTemas = { loading: true };
+      try {
+        if (!this.Temas.length) {
+          await this.getAllSimple();
+        }
+        this.tempTemas = this.Temas.filter((u) => u.pdm_id == pdm_id);
+      } catch (error) {
+        this.tempTemas = { error };
+      }
+    },
+  },
 });
