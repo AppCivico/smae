f3b829e54875c2ad08c8a7b781d19e1245cd8e02
Sobral <robson@oficiodesign.com>
2023-06-14 16:31:34 -0300

    Formatar código

--- frontend/src/views/Administracao.vue
+++ frontend/src/views/Administracao.vue
@@ -1,15 +1,15 @@
 <script setup>
 import { storeToRefs } from 'pinia';
-import { Dashboard} from '@/components';
 import { useAuthStore } from '@/stores';
+import { Dashboard } from '@/components';
 const authStore = useAuthStore();
 const { user } = storeToRefs(authStore);
 </script>
 
 <template>
-    <Dashboard>
-        <div class="flex spacebetween center">
-            <h1>Administração</h1>
-        </div>
-    </Dashboard>
+  <Dashboard>
+    <div class="flex spacebetween center">
+      <h1>Administração</h1>
+    </div>
+  </Dashboard>
 </template>
