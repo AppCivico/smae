bd32c7e4be263c54ec3fb126e1c81caa63789188
Renato CRON <renato.cron@gmail.com>
2023-06-15 10:05:41 -0300

    Ue

--- backend/src/meta/meta.module.ts
+++ backend/src/meta/meta.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { CronogramaEtapaModule } from 'src/cronograma-etapas/cronograma-etapas.module';
+import { CronogramaEtapaService } from 'src/cronograma-etapas/cronograma-etapas.service';
+import { UploadModule } from 'src/upload/upload.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { MetaController } from './meta.controller';
 import { MetaService } from './meta.service';
-import { CronogramaEtapaService } from 'src/cronograma-etapas/cronograma-etapas.service';
-import { CronogramaEtapaModule } from 'src/cronograma-etapas/cronograma-etapas.module';
 
 @Module({
     imports: [PrismaModule, CronogramaEtapaModule, UploadModule],
