798bbffc05f25135ec2a3fceba99a151374f135e
Sobral <robson@oficiodesign.com>
2023-06-15 17:08:09 -0300

    Exibir parâmetros na lista de relatórios de previsão de custo por PdM

--- frontend/src/views/relatorios/RelatoriosDePrevisaoDeCustoPdM.vue
+++ frontend/src/views/relatorios/RelatoriosDePrevisaoDeCustoPdM.vue
@@ -4,13 +4,36 @@ import BotãoParaCarregarMais from '@/components/relatorios/BotaoParaCarregarMai
 import { useAuthStore } from '@/stores/auth.store';
 import { useRelatoriosStore } from '@/stores/relatorios.store.ts';
 import { storeToRefs } from 'pinia';
+import { ref } from 'vue';
+import { relatórioDePrevisãoDeCustoPdM as schema } from '@/consts/formSchemas';
+import { prepararEtiquetas, prepararTags, prepararPdm } from './helpers/preparadorDeColunaParametros';
 
 const relatóriosStore = useRelatoriosStore();
 const { temPermissãoPara } = storeToRefs(useAuthStore());
 const fonte = 'PrevisaoCusto';
+const etiquetasParaValoresDeParâmetros = ref({
+  iniciativa_id: {},
+  atividade_id: {},
+  pdm_id: {},
+  meta_id: {},
+  tags: {},
+});
 
-relatóriosStore.$reset();
-relatóriosStore.getAll({ fonte });
+const etiquetasParaParâmetros = {
+  ...prepararEtiquetas(schema),
+  // TODO: vai dar um trampo enorme pegar esse pessoal. Vamos desabilitá-los por enquanto
+  atividade_id: undefined,
+  iniciativa_id: undefined,
+  meta_id: undefined,
+};
+
+async function iniciar() {
+  relatóriosStore.$reset();
+  relatóriosStore.getAll({ fonte });
+  etiquetasParaValoresDeParâmetros.value.tags = await prepararTags();
+  etiquetasParaValoresDeParâmetros.value.pdm_id = await prepararPdm();
+}
+iniciar();
 </script>
 <template>
   <div class="flex spacebetween center mb2">
@@ -25,7 +48,11 @@ relatóriosStore.getAll({ fonte });
     </router-link>
   </div>
 
-  <TabelaBásica class="mb1" />
+  <TabelaBásica
+    class="mb1"
+    :etiquetas-para-valores-de-parâmetros="etiquetasParaValoresDeParâmetros"
+    :etiquetas-para-parâmetros="etiquetasParaParâmetros"
+  />
 
   <BotãoParaCarregarMais :fonte="fonte" />
 </template>
