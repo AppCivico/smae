23ab87c8b4e00bd1e668ec678f3fe23f7a73dc8b
Renato Cron <renato.cron@gmail.com>
2023-07-20 23:35:01 -0300

    Tentando refazer operações do banco que falharam

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,8 +1,8 @@
 import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient, UnwrapTuple } from '@prisma/client';
+import { RetryPromise } from '../common/retryPromise';
 
-@Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+class PrismaServiceBase extends PrismaClient implements OnModuleInit {
     constructor() {
         super({
             log: [
@@ -41,5 +41,39 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
         this.$on('beforeExit', async () => {
             await app.close();
         });
+    }
+}
+
+@Injectable()
+export class PrismaService extends PrismaServiceBase {
+    constructor() {
+        super();
+    }
+
+    async $transaction<P extends Prisma.PrismaPromise<any>[]>(arg: [...P], options?: { isolationLevel?: Prisma.TransactionIsolationLevel }): Promise<UnwrapTuple<P>>;
+    async $transaction<R>(fn: (prisma: Omit<this, "$connect" | "$disconnect" | "$on" | "$transaction" | "$use">) => Promise<R>, options?: { maxWait?: number, timeout?: number, isolationLevel?: Prisma.TransactionIsolationLevel }): Promise<R>;
+
+    async $transaction(arg: any, options?: any): Promise<any> {
+        if (Array.isArray(arg)) {
+            const transactionPromiseFn = async () => {
+                return super.$transaction(arg, options);
+            };
+
+            return RetryPromise(transactionPromiseFn, 10, 10000, 2000);
+        } else if (typeof arg === 'function') {
+
+            if (!options) options = {};
+
+            if (options.timeout === undefined) options.timeout = 60 * 1000;
+            if (options.maxWait === undefined) options.maxWait = 45 * 1000;
+
+            const transactionPromiseFn = async () => {
+                return super.$transaction(arg, options);
+            };
+
+            return RetryPromise(transactionPromiseFn, 10, 10000, 2000);
+        }
+
+        throw new Error("Invalid arguments passed to $transaction");
     }
 }
