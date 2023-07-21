f66f3019cf1e611bbdb13af42f3e978feb4eae1a
Renato Cron <renato.cron@gmail.com>
2023-07-20 22:28:18 -0300

    Add HEALTH_CHECK_TOKEN

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpException, Query } from '@nestjs/common';
 import { IsPublic } from './auth/decorators/is-public.decorator';
+import percentile from 'percentile';
+import { PrismaService } from './prisma/prisma.service';
 
 //import { Res } from '@nestjs/common';
 //import { Response } from 'express';
@@ -9,10 +11,48 @@ import { IsPublic } from './auth/decorators/is-public.decorator';
 
 @Controller()
 export class AppController {
+    constructor(private readonly prisma: PrismaService) { }
+
     @IsPublic()
     @Get('/ping')
     async ping() {
         return 'pong';
+    }
+
+    @Get('/performance-check')
+    async performanceCheck(@Query() query: Record<string, string>) {
+        if (query.token !== process.env.HEALTH_CHECK_TOKEN)
+            throw new HttpException('mismatched HEALTH_CHECK_TOKEN', 401)
+
+        let delayTime = query.delayTime ? Number(query.delayTime) : 0;
+
+        return await this.recordPerformance(delayTime);
+    }
+
+    async recordPerformance(delayTime: number) {
+        let executionTimes = [];
+        let start = Date.now();
+
+        while (Date.now() - start < 10000) {
+            const queryStart = Date.now();
+            await this.prisma.$transaction(async (prismaTx) => {
+                await prismaTx.$queryRaw`select 1`;
+            });
+            executionTimes.push(Date.now() - queryStart);
+
+            if (delayTime) {
+                await new Promise(resolve => setTimeout(resolve, delayTime));
+            }
+        }
+
+        executionTimes.sort((a, b) => a - b);
+
+        let min = executionTimes[0];
+        let max = executionTimes[executionTimes.length - 1];
+        let p50 = percentile(50, executionTimes);
+        let p99 = percentile(99, executionTimes);
+
+        return { min, max, p50, p99 };
     }
 
     // exemplo de como usar o ejs via server-side
