c5a3263fd35ddb119c85337427a55f83716583bd
Renato Cron <renato.cron@gmail.com>
2023-07-25 14:31:15 -0300

    Nova formatacao de nota_empenho no banco de dados

---
+++ backend/prisma/migrations/20230725170944_patch_nota_empenho/migration.sql
@@ -0,0 +1,89 @@
+begin;
+
+
+update orcamento_realizado me
+set nota_empenho = new_nota_empenho
+from (
+    select
+       id,
+       nota_empenho,
+       cast(split_part(nota_empenho, '/', 1) as integer)::text
+       || '/'
+       || split_part(nota_empenho, '/', 2) as new_nota_empenho
+    from
+       orcamento_realizado
+    where
+       nota_empenho is not null
+       and nota_empenho <> cast(split_part(nota_empenho, '/', 1) as integer)::text
+       || '/'
+       || split_part(nota_empenho, '/', 2)
+) x
+where me.nota_empenho != new_nota_empenho
+and me.id = x.id;
+
+
+update dotacao_processo_nota me
+set dotacao_processo_nota = new_nota_empenho
+from (
+    select
+       id,
+       dotacao_processo_nota,
+       cast(split_part(dotacao_processo_nota, '/', 1) as integer)::text
+       || '/'
+       || split_part(dotacao_processo_nota, '/', 2) as new_nota_empenho
+    from
+       dotacao_processo_nota
+    where
+       dotacao_processo_nota is not null
+       and dotacao_processo_nota <> cast(split_part(dotacao_processo_nota, '/', 1) as integer)::text
+       || '/'
+       || split_part(dotacao_processo_nota, '/', 2)
+) x
+where me.dotacao_processo_nota != new_nota_empenho
+and me.id = x.id;
+
+
+update pdm_dotacao_processo_nota me
+set dotacao_processo_nota = new_nota_empenho
+from (
+    select
+       id,
+       dotacao_processo_nota,
+       cast(split_part(dotacao_processo_nota, '/', 1) as integer)::text
+       || '/'
+       || split_part(dotacao_processo_nota, '/', 2) as new_nota_empenho
+    from
+       pdm_dotacao_processo_nota
+    where
+       dotacao_processo_nota is not null
+       and dotacao_processo_nota <> cast(split_part(dotacao_processo_nota, '/', 1) as integer)::text
+       || '/'
+       || split_part(dotacao_processo_nota, '/', 2)
+) x
+where me.dotacao_processo_nota != new_nota_empenho
+and me.id = x.id;
+
+
+update portfolio_dotacao_processo_nota me
+set dotacao_processo_nota = new_nota_empenho
+from (
+    select
+       id,
+       dotacao_processo_nota,
+       cast(split_part(dotacao_processo_nota, '/', 1) as integer)::text
+       || '/'
+       || split_part(dotacao_processo_nota, '/', 2) as new_nota_empenho
+    from
+       portfolio_dotacao_processo_nota
+    where
+       dotacao_processo_nota is not null
+       and dotacao_processo_nota <> cast(split_part(dotacao_processo_nota, '/', 1) as integer)::text
+       || '/'
+       || split_part(dotacao_processo_nota, '/', 2)
+) x
+where me.dotacao_processo_nota != new_nota_empenho
+and me.id = x.id;
+
+
+
+commit;
