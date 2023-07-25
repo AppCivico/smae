7b3db17fd18c2b8a0e02532b4079185c6c31a050
Renato Cron <renato.cron@gmail.com>
2023-07-25 11:00:48 -0300

    Truncate dotacao apos 9 partes

--- backend-orcamento/core/dao/api_client/sof_client.py
+++ backend-orcamento/core/dao/api_client/sof_client.py
@@ -22,7 +22,8 @@ class SofClient:
     def __build_headers(self):
 
         return {"Authorization" : f"Bearer {self.auth_token}",
-                "Accept": "application/json"}
+                "Accept": "application/json",
+                "User-Agent": "smae, version 0.1"}
 
     def __authorize_get(self):
 
@@ -30,7 +31,13 @@ class SofClient:
         headers = self.__build_headers()
         get = partial(client.get, headers=headers)
 
-        return get
+        def debug_get(*args, **kwargs):
+            print(f"Request made with headers: {headers}")
+            print(f"URL: {self.base_url}")
+            response = get(*args, **kwargs)
+            size = len(json.dumps(response))  # Get the size of response (serialized)
+            print(f"Response size: {size}")
+            print(f"Full response: {response}")
+            return response
 
-
-
+        return debug_get
