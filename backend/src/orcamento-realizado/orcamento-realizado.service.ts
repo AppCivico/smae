2ff79f4a9f383720ede20dd3c2348ad9812da8e2
Renato CRON <renato.cron@gmail.com>
2023-07-27 16:34:11 -0300

    Fix bug no cruzamento da importacao, adicionar filtro de atividade|iniciativa na hora da busca do processo|dotacao|nota

--- backend/src/orcamento-realizado/orcamento-realizado.service.ts
+++ backend/src/orcamento-realizado/orcamento-realizado.service.ts
@@ -15,22 +15,14 @@ type PartialOrcamentoRealizadoDto = {
     ano_referencia: number;
 };
 
-const fk_nota = (row: {
-    dotacao: string,
-    dotacao_processo: string,
-    dotacao_processo_nota: string,
-}): string => {
+const fk_nota = (row: { dotacao: string; dotacao_processo: string; dotacao_processo_nota: string }): string => {
     return row.dotacao + '_' + row.dotacao_processo + '_' + row.dotacao_processo_nota;
 };
 
 @Injectable()
 export class OrcamentoRealizadoService {
     liberarValoresMaioresQueSof: boolean;
-    constructor(
-        private readonly prisma: PrismaService,
-        private readonly orcamentoPlanejado: OrcamentoPlanejadoService,
-        private readonly dotacaoService: DotacaoService
-    ) {
+    constructor(private readonly prisma: PrismaService, private readonly orcamentoPlanejado: OrcamentoPlanejadoService, private readonly dotacaoService: DotacaoService) {
         // deixar desligado a verificação
         this.liberarValoresMaioresQueSof = true;
     }
@@ -159,28 +151,11 @@ export class OrcamentoRealizadoService {
 
                 const orcRealizado = await prismaTxn.orcamentoRealizado.findUniqueOrThrow({ where: { id: +id } });
                 if (orcRealizado.nota_empenho) {
-                    await this.atualizaNotaEmpenho(
-                        meta.pdm_id,
-                        prismaTxn,
-                        orcRealizado.dotacao,
-                        orcRealizado.processo,
-                        orcRealizado.nota_empenho,
-                    );
+                    await this.atualizaNotaEmpenho(meta.pdm_id, prismaTxn, orcRealizado.dotacao, orcRealizado.processo, orcRealizado.nota_empenho);
                 } else if (orcRealizado.processo) {
-                    await this.atualizaProcesso(
-                        meta.pdm_id,
-                        prismaTxn,
-                        { ano_referencia: orcRealizado.ano_referencia },
-                        orcRealizado.dotacao,
-                        orcRealizado.processo,
-                    );
+                    await this.atualizaProcesso(meta.pdm_id, prismaTxn, { ano_referencia: orcRealizado.ano_referencia }, orcRealizado.dotacao, orcRealizado.processo);
                 } else if (orcRealizado.dotacao) {
-                    await this.atualizaDotacao(
-                        meta.pdm_id,
-                        prismaTxn,
-                        { ano_referencia: orcRealizado.ano_referencia },
-                        orcRealizado.dotacao,
-                    );
+                    await this.atualizaDotacao(meta.pdm_id, prismaTxn, { ano_referencia: orcRealizado.ano_referencia }, orcRealizado.dotacao);
                 } else {
                     throw new HttpException('Erro interno: nota, processo ou dotação está null', 500);
                 }
@@ -232,12 +207,7 @@ export class OrcamentoRealizadoService {
         return updated;
     }
 
-    private async atualizaDotacao(
-        pdm_id: number,
-        prismaTxn: Prisma.TransactionClient,
-        dto: PartialOrcamentoRealizadoDto,
-        dotacao: string,
-    ) {
+    private async atualizaDotacao(pdm_id: number, prismaTxn: Prisma.TransactionClient, dto: PartialOrcamentoRealizadoDto, dotacao: string) {
         console.log({
             func: 'atualizaDotacao',
             dotacao,
@@ -265,26 +235,23 @@ export class OrcamentoRealizadoService {
                 ano_referencia: dto.ano_referencia,
                 dotacao: dotacao,
                 pdm_id: pdm_id,
-            }
+            },
         });
 
-        if (novo_valor && this.liberarValoresMaioresQueSof == false &&
-            novo_valor.soma_valor_empenho.greaterThan(dotacaoTx.empenho_liquido)) {
+        if (novo_valor && this.liberarValoresMaioresQueSof == false && novo_valor.soma_valor_empenho.greaterThan(dotacaoTx.empenho_liquido)) {
             throw new HttpException(
-                `Novo valor de empenho no SMAE (${novo_valor.soma_valor_empenho.toFixed(2)}) seria maior do que o valor de empenho para a Dotação-Processo (${dotacaoTx.empenho_liquido.toFixed(
+                `Novo valor de empenho no SMAE (${novo_valor.soma_valor_empenho.toFixed(
                     2,
-                )}).` + FRASE_FIM,
+                )}) seria maior do que o valor de empenho para a Dotação-Processo (${dotacaoTx.empenho_liquido.toFixed(2)}).` + FRASE_FIM,
                 400,
             );
         }
 
-        if (novo_valor && this.liberarValoresMaioresQueSof == false &&
-            novo_valor.soma_valor_liquidado.greaterThan(dotacaoTx.valor_liquidado)
-        ) {
+        if (novo_valor && this.liberarValoresMaioresQueSof == false && novo_valor.soma_valor_liquidado.greaterThan(dotacaoTx.valor_liquidado)) {
             throw new HttpException(
-                `Novo valor de liquidado no SMAE (${novo_valor.soma_valor_liquidado.toFixed(2)}) seria maior do que o valor liquidado para a Dotação-Processo (${dotacaoTx.valor_liquidado.toFixed(
+                `Novo valor de liquidado no SMAE (${novo_valor.soma_valor_liquidado.toFixed(
                     2,
-                )}).` + FRASE_FIM,
+                )}) seria maior do que o valor liquidado para a Dotação-Processo (${dotacaoTx.valor_liquidado.toFixed(2)}).` + FRASE_FIM,
                 400,
             );
         }
@@ -292,14 +259,7 @@ export class OrcamentoRealizadoService {
         return mes_utilizado;
     }
 
-    private async atualizaProcesso(
-        pdm_id: number,
-        prismaTxn: Prisma.TransactionClient,
-        dto: PartialOrcamentoRealizadoDto,
-        dotacao: string,
-        processo: string,
-    ) {
-
+    private async atualizaProcesso(pdm_id: number, prismaTxn: Prisma.TransactionClient, dto: PartialOrcamentoRealizadoDto, dotacao: string, processo: string) {
         const processoTx = await prismaTxn.dotacaoProcesso.findUnique({
             where: {
                 ano_referencia_dotacao_dotacao_processo: {
@@ -308,7 +268,7 @@ export class OrcamentoRealizadoService {
                     dotacao_processo: processo,
                 },
             },
-            select: { empenho_liquido: true, valor_liquidado: true, id: true, mes_utilizado: true }
+            select: { empenho_liquido: true, valor_liquidado: true, id: true, mes_utilizado: true },
         });
         if (!processoTx) throw new HttpException('Operação não pode ser realizada no momento. Nota-Empenho deixou de existir durante a atualização.', 400);
         const mes_utilizado = processoTx.mes_utilizado;
@@ -324,26 +284,23 @@ export class OrcamentoRealizadoService {
                 dotacao: dotacao,
                 dotacao_processo: processo,
                 pdm_id: pdm_id,
-            }
+            },
         });
 
-        if (novo_valor && this.liberarValoresMaioresQueSof == false &&
-            novo_valor.soma_valor_empenho.greaterThan(processoTx.empenho_liquido)) {
+        if (novo_valor && this.liberarValoresMaioresQueSof == false && novo_valor.soma_valor_empenho.greaterThan(processoTx.empenho_liquido)) {
             throw new HttpException(
-                `Novo valor de empenho no SMAE (${novo_valor.soma_valor_empenho.toFixed(2)}) seria maior do que o valor de empenho para a Dotação-Processo (${processoTx.empenho_liquido.toFixed(
+                `Novo valor de empenho no SMAE (${novo_valor.soma_valor_empenho.toFixed(
                     2,
-                )}).` + FRASE_FIM,
+                )}) seria maior do que o valor de empenho para a Dotação-Processo (${processoTx.empenho_liquido.toFixed(2)}).` + FRASE_FIM,
                 400,
             );
         }
 
-        if (novo_valor && this.liberarValoresMaioresQueSof == false &&
-            novo_valor.soma_valor_liquidado.greaterThan(processoTx.valor_liquidado)
-        ) {
+        if (novo_valor && this.liberarValoresMaioresQueSof == false && novo_valor.soma_valor_liquidado.greaterThan(processoTx.valor_liquidado)) {
             throw new HttpException(
-                `Novo valor de liquidado no SMAE (${novo_valor.soma_valor_liquidado.toFixed(2)}) seria maior do que o valor liquidado para a Dotação-Processo (${processoTx.valor_liquidado.toFixed(
+                `Novo valor de liquidado no SMAE (${novo_valor.soma_valor_liquidado.toFixed(
                     2,
-                )}).` + FRASE_FIM,
+                )}) seria maior do que o valor liquidado para a Dotação-Processo (${processoTx.valor_liquidado.toFixed(2)}).` + FRASE_FIM,
                 400,
             );
         }
@@ -352,16 +309,10 @@ export class OrcamentoRealizadoService {
     }
 
     private getAnoNota(nota_empenho: string): number {
-        return +(nota_empenho.split('/')[1]);
+        return +nota_empenho.split('/')[1];
     }
 
-    private async atualizaNotaEmpenho(
-        pdm_id: number,
-        prismaTxn: Prisma.TransactionClient,
-        dotacao: string,
-        processo: string | null,
-        nota_empenho: string,
-    ) {
+    private async atualizaNotaEmpenho(pdm_id: number, prismaTxn: Prisma.TransactionClient, dotacao: string, processo: string | null, nota_empenho: string) {
         const notaEmpenhoTx = await prismaTxn.dotacaoProcessoNota.findUnique({
             where: {
                 ano_referencia_dotacao_dotacao_processo_dotacao_processo_nota: {
@@ -376,7 +327,7 @@ export class OrcamentoRealizadoService {
                     dotacao_processo_nota: nota_empenho,
                 },
             },
-            select: { empenho_liquido: true, valor_liquidado: true, id: true, mes_utilizado: true }
+            select: { empenho_liquido: true, valor_liquidado: true, id: true, mes_utilizado: true },
         });
         if (!notaEmpenhoTx) throw new HttpException('Operação não pode ser realizada no momento. Nota-Empenho deixou de existir durante a atualização.', 400);
         const mes_utilizado = notaEmpenhoTx.mes_utilizado;
@@ -393,26 +344,23 @@ export class OrcamentoRealizadoService {
                 dotacao_processo: processo!,
                 dotacao_processo_nota: nota_empenho,
                 pdm_id: pdm_id,
-            }
+            },
         });
 
-        if (novo_valor && this.liberarValoresMaioresQueSof == false &&
-            novo_valor.soma_valor_empenho.greaterThan(notaEmpenhoTx.empenho_liquido)) {
+        if (novo_valor && this.liberarValoresMaioresQueSof == false && novo_valor.soma_valor_empenho.greaterThan(notaEmpenhoTx.empenho_liquido)) {
             throw new HttpException(
-                `Novo valor de empenho no SMAE (${novo_valor.soma_valor_empenho.toFixed(2)}) seria maior do que o valor de empenho para a Nota-Empenho (${notaEmpenhoTx.empenho_liquido.toFixed(
+                `Novo valor de empenho no SMAE (${novo_valor.soma_valor_empenho.toFixed(
                     2,
-                )}).` + FRASE_FIM,
+                )}) seria maior do que o valor de empenho para a Nota-Empenho (${notaEmpenhoTx.empenho_liquido.toFixed(2)}).` + FRASE_FIM,
                 400,
             );
         }
 
-        if (novo_valor && this.liberarValoresMaioresQueSof == false &&
-            novo_valor.soma_valor_liquidado.greaterThan(notaEmpenhoTx.valor_liquidado)
-        ) {
+        if (novo_valor && this.liberarValoresMaioresQueSof == false && novo_valor.soma_valor_liquidado.greaterThan(notaEmpenhoTx.valor_liquidado)) {
             throw new HttpException(
-                `Novo valor de liquidado no SMAE (${novo_valor.soma_valor_liquidado.toFixed(2)}) seria maior do que o valor liquidado para a Nota-Empenho (${notaEmpenhoTx.valor_liquidado.toFixed(
+                `Novo valor de liquidado no SMAE (${novo_valor.soma_valor_liquidado.toFixed(
                     2,
-                )}).` + FRASE_FIM,
+                )}) seria maior do que o valor liquidado para a Nota-Empenho (${notaEmpenhoTx.valor_liquidado.toFixed(2)}).` + FRASE_FIM,
                 400,
             );
         }
@@ -436,11 +384,9 @@ export class OrcamentoRealizadoService {
         if (nota_empenho) {
             if (!processo) throw new HttpException('Necessário enviar Processo ao enviar Nota Empenho', 400);
 
-            if (!dto.nota_ano && nota_empenho.includes('/'))
-                dto.nota_ano = nota_empenho.split('/')[1];
+            if (!dto.nota_ano && nota_empenho.includes('/')) dto.nota_ano = nota_empenho.split('/')[1];
 
-            if (!dto.nota_ano)
-                throw new HttpException('Faltando enviar o Ano Referencia da Nota de Empenho', 400);
+            if (!dto.nota_ano) throw new HttpException('Faltando enviar o Ano Referencia da Nota de Empenho', 400);
 
             const notaDb = await this.prisma.dotacaoProcessoNota.findUnique({
                 where: {
@@ -499,11 +445,13 @@ export class OrcamentoRealizadoService {
         const queryRows = await this.prisma.orcamentoRealizado.findMany({
             where: {
                 removido_em: null,
-                dotacao: filters?.dotacao,
-                AND: [{ meta_id: filters?.meta_id }, { meta_id: filterIdIn ? { in: filterIdIn } : undefined }],
-                processo: filters?.processo,
-                nota_empenho: filters?.nota_empenho,
+                dotacao: filters.dotacao,
+                AND: [{ meta_id: filters.meta_id }, { meta_id: filterIdIn ? { in: filterIdIn } : undefined }],
+                processo: filters.processo,
+                nota_empenho: filters.nota_empenho,
                 ano_referencia: filters.ano_referencia, // obrigatório para que o 'join' com a dotação seja feito sem complicações
+                iniciativa_id: filters.iniciativa_id,
+                atividade_id: filters.atividade_id,
             },
             select: {
                 criador: { select: { nome_exibicao: true } },
@@ -560,7 +508,7 @@ export class OrcamentoRealizadoService {
                 dotacao: true,
                 dotacao_processo: true,
                 dotacao_processo_nota: true,
-            }
+            },
         });
         const notasInfoSomaRef: Record<string, (typeof notasSomaInfo)[0]> = {};
         for (const nota of notasSomaInfo) {
@@ -569,7 +517,7 @@ export class OrcamentoRealizadoService {
         const notasInfo = await this.prisma.dotacaoProcessoNota.findMany({
             where: {
                 OR: Object.keys(notaEncontradasSemAno).map((nota_prefixo: string) => {
-                    return { dotacao_processo_nota: { startsWith: nota_prefixo } }
+                    return { dotacao_processo_nota: { startsWith: nota_prefixo } };
                 }),
                 ano_referencia: { in: Object.keys(anoNotas).map(ano => +ano) },
             },
@@ -598,7 +546,7 @@ export class OrcamentoRealizadoService {
                 soma_valor_liquidado: true,
                 dotacao_processo: true,
                 dotacao: true,
-            }
+            },
         });
         const processoInfoSomaRef: Record<string, (typeof processoSomaInfo)[0]> = {};
         for (const processo of processoSomaInfo) {
@@ -664,18 +612,24 @@ export class OrcamentoRealizadoService {
             let valor_liquidado: string | null = null;
 
             if (orcaRealizado.nota_empenho) {
-                const notaInfo = notasInfoRef[fk_nota({
-                    dotacao: orcaRealizado.dotacao,
-                    dotacao_processo: orcaRealizado.processo!,
-                    dotacao_processo_nota: orcaRealizado.nota_empenho
-                })];
-                const notaSomaInfo = notasInfoSomaRef[fk_nota({
-                    dotacao: orcaRealizado.dotacao,
-                    dotacao_processo: orcaRealizado.processo!,
-                    dotacao_processo_nota: orcaRealizado.nota_empenho
-                })];
+                const notaInfo =
+                    notasInfoRef[
+                        fk_nota({
+                            dotacao: orcaRealizado.dotacao,
+                            dotacao_processo: orcaRealizado.processo!,
+                            dotacao_processo_nota: orcaRealizado.nota_empenho,
+                        })
+                    ];
+                const notaSomaInfo =
+                    notasInfoSomaRef[
+                        fk_nota({
+                            dotacao: orcaRealizado.dotacao,
+                            dotacao_processo: orcaRealizado.processo!,
+                            dotacao_processo_nota: orcaRealizado.nota_empenho,
+                        })
+                    ];
 
-                smae_soma_valor_empenho = notaSomaInfo?.soma_valor_empenho.toFixed(2) ?? '0.00'
+                smae_soma_valor_empenho = notaSomaInfo?.soma_valor_empenho.toFixed(2) ?? '0.00';
                 smae_soma_valor_liquidado = notaSomaInfo?.soma_valor_liquidado.toFixed(2) ?? '0.00';
                 if (notaInfo) {
                     empenho_liquido = notaInfo.empenho_liquido.toFixed(2);
@@ -708,7 +662,7 @@ export class OrcamentoRealizadoService {
 
             const orc_config = await this.prisma.pdmOrcamentoConfig.findFirst({
                 where: { pdm_id: meta.pdm_id, ano_referencia: filters.ano_referencia },
-                select: { execucao_disponivel_meses: true }
+                select: { execucao_disponivel_meses: true },
             });
 
             rows.push({
