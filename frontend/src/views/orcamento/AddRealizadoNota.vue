422049884b10e5fa8f237617b81c686529ffaa3d
Sobral <robson@oficiodesign.com>
2023-08-01 16:59:40 -0300

    Solicitar nova validação após editar Nota de Empenho

--- frontend/src/views/orcamento/AddRealizadoNota.vue
+++ frontend/src/views/orcamento/AddRealizadoNota.vue
@@ -382,11 +382,19 @@ async function validarDota(evt) {
           name="itens"
         />
 
+        <div
+          v-show="respostasof.nota_empenho !== `${values.nota_empenho}/${dotaAno}`"
+          class="error-msg"
+        >
+          Validação pendente
+        </div>
+
         <div class="flex spacebetween center mb2">
           <hr class="mr2 f1">
           <button
             class="btn big"
-            :disabled="isSubmitting"
+            :disabled="isSubmitting
+              || respostasof.nota_empenho !== `${values.nota_empenho}/${dotaAno}`"
           >
             Salvar
           </button>
