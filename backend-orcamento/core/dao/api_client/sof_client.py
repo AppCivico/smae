1ff6f3bd480e69441bc1dfae9798e172229b9db5
Renato Cron <renato.cron@gmail.com>
2023-08-14 15:51:48 -0300

    Sync SOF api

--- backend-orcamento/core/dao/api_client/sof_client.py
+++ backend-orcamento/core/dao/api_client/sof_client.py
@@ -1,13 +1,12 @@
 from functools import partial
 from .abstract_client import Client
-import json
+from config import SOF_API_HOST, SOF_API_VERSION
 
 
 class SofClient:
 
-    host = 'gatewayapi.prodam.sp.gov.br:443'
-    base_path = '/financas/orcamento/sof/'
-    version = 'v3.0.1'
+    host = SOF_API_HOST
+    version = SOF_API_VERSION
 
     def __init__(self, auth_token:str)->None:
 
@@ -18,13 +17,12 @@ class SofClient:
 
     def __build_base_url(self):
 
-        return f'https://{self.host}{self.base_path}{self.version}/'
+        return f'https://{self.host}/{self.version}/'
 
     def __build_headers(self):
 
         return {"Authorization" : f"Bearer {self.auth_token}",
-                "Accept": "application/json",
-                "User-Agent": "smae, version 0.1"}
+                "Accept": "application/json"}
 
     def __authorize_get(self):
 
@@ -32,13 +30,7 @@ class SofClient:
         headers = self.__build_headers()
         get = partial(client.get, headers=headers)
 
-        def debug_get(*args, **kwargs):
-            print(f"Request made with headers: {headers}")
-            print(f"URL: {self.base_url}")
-            response = get(*args, **kwargs)
-            size = len(json.dumps(response))  # Get the size of response (serialized)
-            print(f"Response size: {size}")
-            print(f"Full response: {response}")
-            return response
+        return get
 
-        return debug_get
+
+
