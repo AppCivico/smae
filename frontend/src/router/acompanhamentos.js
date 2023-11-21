84540f57d466725f5206de3923c99db9212830ab
sobral <sobral@oficiodesign.com>
2023-11-21 13:35:08 -0300

    Alterar título de tela de acompanhamento

--- frontend/src/router/acompanhamentos.js
+++ frontend/src/router/acompanhamentos.js
@@ -24,7 +24,7 @@ export default {
       path: '',
       component: AcompanhamentosLista,
       meta: {
-        título: 'Reuniões de acompanhamento',
+        título: 'Acompanhamento de projeto',
         títuloParaMenu: 'Acompanhamento do projeto',
 
         rotasParaMigalhasDePão: [
