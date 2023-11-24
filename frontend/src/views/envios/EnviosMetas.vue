9b0a434a784cf9e1258596505474b997c95c8d05
sobral <sobral@oficiodesign.com>
2023-11-24 08:27:53 -0300

    Prevenir herança de atributos para compontentes com múltiplas raízes

--- frontend/src/views/envios/EnviosMetas.vue
+++ frontend/src/views/envios/EnviosMetas.vue
@@ -18,6 +18,12 @@ if (!PdMStore.PdM.length) {
   });
 }
 </script>
+<script>
+// use normal <script> to declare options
+export default {
+  inheritAttrs: false,
+};
+</script>
 <template>
   <div class="flex spacebetween center mb2">
     <h1>
