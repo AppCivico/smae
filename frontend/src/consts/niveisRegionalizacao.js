374b6281635a750840d9050a4417847aa6095b48
sobral <sobral@oficiodesign.com>
2023-11-27 17:52:56 -0300

    Adicionar nível de regionalização ao portfólio

---
+++ frontend/src/consts/niveisRegionalizacao.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    id: 1,
+    nome: 'Município',
+  },
+  {
+    id: 2,
+    nome: 'Região',
+  },
+  {
+    id: 3,
+    nome: 'Subprefeitura',
+  },
+  {
+    id: 4,
+    nome: 'Distrito',
+  },
+];
