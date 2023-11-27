4d79805619b1e13f21e8086e19da2ba79cd8e12e
sobral <sobral@oficiodesign.com>
2023-11-27 15:26:43 -0300

    Tentar carga assíncrona do `formula_parser.js`.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,19 +3,22 @@ import { fileURLToPath, URL } from 'node:url';
 import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
 
-const htmlPlugin = () => ({
-  name: 'html-transform',
-  transformIndexHtml(html) {
-    return html.replace(
-      /__FORMULA_JS_SCRIPT__/,
-      `<script type="text/javascript" src="${process.env.FORMULA_JS_URL || '/api/public/js/formula_parser.js'}"></script>`,
-    );
-  },
-});
+// const htmlPlugin = () => ({
+//   name: 'html-transform',
+//   transformIndexHtml(html) {
+//     return html.replace(
+//       /__FORMULA_JS_SCRIPT__/,
+//       `<script type="text/javascript" src="${process.env.FORMULA_JS_URL || '/api/public/js/formula_parser.js'}"></script>`,
+//     );
+//   },
+// });
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [htmlPlugin(), vue()],
+  plugins: [
+    // htmlPlugin(),
+    vue(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
