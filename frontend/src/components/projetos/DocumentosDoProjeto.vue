056802d4da75f92c539dfa1887c62db7ab561fe4
sobral <sobral@oficiodesign.com>
2023-12-04 22:45:46 -0300

    Permitir ordenação de arquivos nos diretórios

--- frontend/src/components/projetos/DocumentosDoProjeto.vue
+++ frontend/src/components/projetos/DocumentosDoProjeto.vue
@@ -16,6 +16,19 @@ const {
   erro,
 } = storeToRefs(projetosStore);
 
+const ordenadoPor = ref('descricao');
+const ordem = ref('crescente');
+
+const arquivosOrdenados = computed(() => [...arquivos.value]
+  .sort((a, b) => {
+    if (a[ordenadoPor.value] || b[ordenadoPor.value]) {
+      return ordem.value === 'crescente'
+        ? (a[ordenadoPor.value] || '').localeCompare((b[ordenadoPor.value] || ''))
+        : (b[ordenadoPor.value] || '').localeCompare((a[ordenadoPor.value] || ''));
+    }
+    return 0;
+  }));
+
 const árvoreDeDiretórios = computed(() => createDataTree(
   diretóriosConsolidados.value
     .reduce((acc, cur) => acc.concat(
@@ -41,7 +54,9 @@ const arquivosAgrupadosPorCaminho = computed(() => listaFiltradaPorTermoDeBusca.
       ? [cur]
       : acc[caminho].concat([cur]);
     return acc;
-  }, {}) || {});
+  }, {})
+
+  || {});
 
 function excluirArquivo(id) {
   alertStore.confirmAction('Deseja remover o arquivo?', () => {
@@ -57,10 +72,39 @@ function iniciar() {
 iniciar();
 </script>
 <template>
-  <div class="flex center mb2 spacebetween">
+  <div class="flex flexwrap mb2 spacebetween">
+    <div class="f1 mr1">
+      <label class="label tc300">Ordenar por</label>
+      <select
+        v-model="ordenadoPor"
+        class="inputtext"
+      >
+        <option value="descricao">
+          Descrição
+        </option>
+        <option value="data">
+          Data
+        </option>
+      </select>
+    </div>
+    <div class="f1 mr1">
+      <label class="label tc300">Ordem</label>
+      <select
+        v-model="ordem"
+        class="inputtext"
+      >
+        <option value="crescente">
+          crescente
+        </option>
+        <option value="decrescente">
+          decrescente
+        </option>
+      </select>
+    </div>
     <LocalFilter
       v-model="listaFiltradaPorTermoDeBusca"
-      :lista="arquivos"
+      class="f2"
+      :lista="arquivosOrdenados"
     />
   </div>
 
