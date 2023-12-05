b21ab7a43108d35835cf418a2a38da4f2c16d92a
Renato Cron <renato.cron@gmail.com>
2023-12-05 14:52:40 -0300

    Adicionando titulo: dto.titulo + regiao.descricao

--- backend/src/regiao/dto/detalhe-regiao.dto.ts
+++ backend/src/regiao/dto/detalhe-regiao.dto.ts
@@ -2,6 +2,7 @@ export class DetalheRegiaoDto {
     id: number;
     descricao: string;
     shapefile: string | null;
+    pdm_codigo_sufixo: string | null;
     codigo: number | null;
     nivel: number;
     parente_id: number | null;
