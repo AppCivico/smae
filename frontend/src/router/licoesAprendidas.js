d9c67e267939f5a33dbc9939d53277f769525745
lucasansei <lucas.ansei@eokoe.com>
2023-12-07 15:13:29 -0300

    Adicionando resumo para Lições Aprendidas

--- frontend/src/router/licoesAprendidas.js
+++ frontend/src/router/licoesAprendidas.js
@@ -3,6 +3,14 @@ import liçõesAprendidasCriarEditar from '@/views/licoesAprendidas/LicoesAprend
 import liçõesAprendidasItem from '@/views/licoesAprendidas/LicoesAprendidasItem.vue';
 import liçõesAprendidasLista from '@/views/licoesAprendidas/LicoesAprendidasLista.vue';
 import liçõesAprendidasRaiz from '@/views/licoesAprendidas/LicoesAprendidasRaiz.vue';
+import { useProjetosStore } from '@/stores/projetos.store.ts';
+import LoadingComponent from '@/components/LoadingComponent.vue';
+import { defineAsyncComponent } from 'vue';
+
+const licoesAprendidasResumo = defineAsyncComponent({
+  loader: () => import('@/views/licoesAprendidas/LicoesAprendidasResumo.vue'),
+  loadingComponent: LoadingComponent,
+});
 
 export default {
   path: 'licoes-aprendidas',
@@ -85,6 +93,31 @@ export default {
               'liçõesAprendidasListar',
               'liçõesAprendidasEditar',
             ],
+          },
+        },
+
+        {
+          path: 'resumo',
+          name: 'liçõesAprendidasResumo',
+          component: licoesAprendidasResumo,
+          props: ({ params }) => ({
+            ...params,
+            projetoId: Number.parseInt(params.projetoId, 10) || undefined,
+            licaoAprendidaId: Number.parseInt(params.licaoAprendidaId, 10) || undefined,
+          }),
+          meta: {
+            título: () => {
+              let título = useLiçõesAprendidasStore()?.emFoco?.data_registro
+                ? `Acompanhamento ${dateToField(useLiçõesAprendidasStore()?.emFoco?.data_registro)}`
+                : 'Resumo de licão aprendida';
+
+              if (useProjetosStore()?.emFoco?.nome) {
+                título = `${título} do projeto ${useProjetosStore()?.emFoco?.nome}`;
+              }
+
+              return título;
+            },
+            títuloParaMenu: 'Resumo',
           },
         },
       ],
---
+++ frontend/src/views/licoesAprendidas/LicoesAprendidasResumo.vue
@@ -0,0 +1,120 @@
+<script setup>
+import { liçãoAprendida as schema } from '@/consts/formSchemas';
+import dateToField from '@/helpers/dateToField';
+import { useAcompanhamentosStore } from '@/stores/acompanhamentos.store.ts';
+import { useLiçõesAprendidasStore } from '@/stores/licoesAprendidas.store';
+import { useRiscosStore } from '@/stores/riscos.store.ts';
+import { storeToRefs } from 'pinia';
+
+const licoesAprendidasStore = useLiçõesAprendidasStore();
+const riscosStore = useRiscosStore();
+
+const {
+  chamadasPendentes,
+  emFoco,
+  erro,
+} = storeToRefs(licoesAprendidasStore);
+</script>
+<template>
+  <div class="flex spacebetween center mb2">
+    <h1>
+      {{ typeof route?.meta?.título === 'function'
+        ? route.meta.título()
+        : route?.meta?.título || 'Resumo de lição aprendida' }}
+    </h1>
+
+    <hr class="ml2 f1">
+
+    <router-link
+      v-if="emFoco?.id"
+      :to="{
+        name: 'liçõesAprendidasEditar', params: {
+          licaoAprendidaId: emFoco.id,
+          projetoId: Number.parseInt($route.params.projetoId, 10) || undefined,
+        }
+      }"
+      class="btn big ml2"
+    >
+      Editar
+    </router-link>
+  </div>
+
+  <div
+    v-if="emFoco"
+    class="boards"
+  >
+    <div class="flex g2 mb1">
+
+      <div class="f2 mb1">
+        <dt class="t12 uc w700 mb05 tamarelo">
+          {{ schema.fields.responsavel.spec.label }}
+        </dt>
+        <dd class="t13">
+          {{ emFoco?.responsavel || '-' }}
+        </dd>
+      </div>
+    </div>
+
+    <div class="flex g2">
+      <div class="f1 mb1">
+        <dt class="t12 uc w700 mb05 tamarelo">
+          {{ schema.fields.contexto.spec.label }}
+        </dt>
+        <dd class="t13">
+          {{ emFoco?.contexto || '-' }}
+        </dd>
+      </div>
+    </div>
+
+    <div class="flex g2">
+      <div class="f1 mb1">
+        <dt class="t12 uc w700 mb05 tamarelo">
+          {{ schema.fields.descricao.spec.label }}
+        </dt>
+        <dd class="t13">
+          {{ emFoco?.descricao || '-' }}
+        </dd>
+      </div>
+    </div>
+
+    <div class="flex g2">
+      <div class="f1 mb1">
+        <dt class="t12 uc w700 mb05 tamarelo">
+          {{ schema.fields.observacao.spec.label }}
+        </dt>
+        <dd class="t13">
+          {{ emFoco?.observacao || '-' }}
+        </dd>
+      </div>
+    </div>
+
+    <div class="flex g2">
+      <div class="f1 mb1">
+        <dt class="t12 uc w700 mb05 tamarelo">
+          {{ schema.fields.resultado.spec.label }}
+        </dt>
+        <dd class="t13">
+          {{ emFoco?.resultado || '-' }}
+        </dd>
+      </div>
+    </div>
+
+
+  </div>
+
+  <div
+    v-if="chamadasPendentes?.emFoco"
+    class="spinner"
+  >
+    Carregando
+  </div>
+
+  <div
+    v-if="erro"
+    class="error p1"
+  >
+    <div class="error-msg">
+      {{ erro }}
+    </div>
+  </div>
+</template>
