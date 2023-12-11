bb22cdacaf454a96c12d6aa87eba88f7639713db
Renato Cron <renato.cron@gmail.com>
2023-12-11 13:17:58 -0300

    Mais um log do report inteiro

--- backend/src/reports/pp-status/pp-status.service.ts
+++ backend/src/reports/pp-status/pp-status.service.ts
@@ -122,8 +122,6 @@ export class PPStatusService implements ReportableService {
             };
         });
 
-        console.log(projetoStatusOut);
-
         return {
             linhas: projetoStatusOut,
         };
