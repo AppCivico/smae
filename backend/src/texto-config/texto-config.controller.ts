b5ffa44afc2b96a2a8082b3c63f1f5dc374c8a24
Renato Cron <renato.cron@gmail.com>
2023-12-18 12:04:04 -0300

    Adicionando endpoint @Patch(/texto-tos) apenas para @Roles(SMAE.superadmin)

--- backend/src/texto-config/texto-config.controller.ts
+++ backend/src/texto-config/texto-config.controller.ts
@@ -1,17 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Get, Patch } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { IsPublic } from '../auth/decorators/is-public.decorator';
 import { PrismaService } from '../prisma/prisma.service';
 import { textoTosDto } from './entities/texto-config.entity';
 import { TextoConfigService } from './texto-config.service';
+import { UpdateTextoConfigDto } from './dto/update-texto-config.dto';
+import { Roles } from '../auth/decorators/roles.decorator';
 
 @Controller('texto-config')
 export class TextoConfigController {
-    constructor(
-        // TODO implementar o patch
-        private readonly textoConfigService: TextoConfigService,
-        private readonly prisma: PrismaService
-    ) {}
+    constructor(private readonly textoConfigService: TextoConfigService, private readonly prisma: PrismaService) {}
 
     @ApiTags('Público')
     @IsPublic()
@@ -22,5 +20,14 @@ export class TextoConfigController {
             bemvindo_email: textoConfig.bemvindo_email,
             tos: textoConfig.tos,
         };
+    }
+
+    @ApiTags('default')
+    @Patch('/texto-tos')
+    @ApiBearerAuth('access-token')
+    @Roles('SMAE.superadmin')
+    async patchTextoTos(@Body() dto: UpdateTextoConfigDto): Promise<string> {
+        await this.textoConfigService.update(dto);
+        return '';
     }
 }
--- backend/src/texto-config/texto-config.service.ts
+++ backend/src/texto-config/texto-config.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { UpdateTextoConfigDto } from './dto/update-texto-config.dto';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class TextoConfigService {
-    update(id: number, updateTextoConfigDto: UpdateTextoConfigDto) {
-        return `This action updates a #${id} textoConfig`;
+    constructor(private readonly prisma: PrismaService) {}
+
+    async update(dto: UpdateTextoConfigDto): Promise<void> {
+        await this.prisma.textoConfig.update({
+            where: { id: 1 },
+            data: {
+                ...dto,
+            },
+        });
     }
 }
