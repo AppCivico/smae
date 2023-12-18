a0d4571a91e4e40c4f63687116a2d6b0ba150148
Renato Cron <renato.cron@gmail.com>
2023-12-18 12:04:04 -0300

    Adicionando endpoint @Patch(/texto-tos) apenas para @Roles(SMAE.superadmin)

--- backend/src/texto-config/dto/update-texto-config.dto.ts
+++ backend/src/texto-config/dto/update-texto-config.dto.ts
@@ -1,1 +1,9 @@
-export class UpdateTextoConfigDto {}
+import { IsString } from 'class-validator';
+
+export class UpdateTextoConfigDto {
+    @IsString()
+    bemvindo_email: string;
+
+    @IsString()
+    tos: string;
+}
