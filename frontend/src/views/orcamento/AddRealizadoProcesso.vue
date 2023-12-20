6d6e47bc5b686694e6945103d35de946b192a7e0
sobral <sobral@oficiodesign.com>
2023-12-20 09:15:35 -0300

    Reduzir dependências cíclicas

--- frontend/src/views/orcamento/AddRealizadoProcesso.vue
+++ frontend/src/views/orcamento/AddRealizadoProcesso.vue
@@ -3,9 +3,11 @@ import CheckClose from '@/components/CheckClose.vue';
 import ItensRealizado from '@/components/orcamento/ItensRealizado.vue';
 import patterns from '@/consts/patterns';
 import formatProcesso from '@/helpers/formatProcesso';
-import {
-  useAlertStore, useAtividadesStore, useIniciativasStore, useMetasStore, useOrcamentosStore,
-} from '@/stores';
+import { useAlertStore } from '@/stores/alert.store';
+import { useAtividadesStore } from '@/stores/atividades.store';
+import { useIniciativasStore } from '@/stores/iniciativas.store';
+import { useMetasStore } from '@/stores/metas.store';
+import { useOrcamentosStore } from '@/stores/orcamentos.store';
 import { useDotaçãoStore } from '@/stores/dotacao.store.ts';
 import { useProjetosStore } from '@/stores/projetos.store.ts';
 import { storeToRefs } from 'pinia';
