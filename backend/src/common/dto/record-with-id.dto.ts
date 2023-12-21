cca511f63367ca3a81d4e0770094d6247aac1d09
Renato CRON <renato.cron@gmail.com>
2023-12-21 18:41:12 -0300

    Adicionando validacoes no BatchRecordWithId/RecordWithId

--- backend/src/common/dto/record-with-id.dto.ts
+++ backend/src/common/dto/record-with-id.dto.ts
@@ -1,7 +1,16 @@
+import { Type } from 'class-transformer';
+import { IsArray, IsInt, Max, Min, ValidateNested } from 'class-validator';
+
 export class RecordWithId {
+    @IsInt()
+    @Min(0)
+    @Max(2 ** 32)
     id: number;
 }
 
 export class BatchRecordWithId {
+    @IsArray()
+    @Type(() => RecordWithId)
+    @ValidateNested({ each: true })
     ids: RecordWithId[];
 }
