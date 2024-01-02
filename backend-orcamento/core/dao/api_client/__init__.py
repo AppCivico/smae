6506f50542111384e66c3e7350e595fa04557eb1
Renato Cron <renato.cron@gmail.com>
2024-01-02 18:48:34 -0300

    Add endpoint SOF orcado_dotacao (sync)

--- backend-orcamento/core/dao/api_client/__init__.py
+++ backend-orcamento/core/dao/api_client/__init__.py
@@ -1,3 +1,4 @@
 from .client_empenhos import Client as ClientEmpenhos
 from .client_dotacoes import Client as ClientDotacoes
 from .client_despesas import Client as ClientDespesas
+from .client_despesas_dotacao import Client as ClientDespesasDotacao
