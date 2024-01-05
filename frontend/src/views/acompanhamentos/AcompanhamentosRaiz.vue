c13f6491f5d14ff0576f80978e60d6b37781c0f8
sobral <sobral@oficiodesign.com>
2024-01-05 17:41:06 -0300

    Remover riscos da lista de acompanhamento

--- frontend/src/views/acompanhamentos/AcompanhamentosRaiz.vue
+++ frontend/src/views/acompanhamentos/AcompanhamentosRaiz.vue
@@ -1,13 +1,7 @@
 <script setup>
-import { useRiscosStore } from '@/stores/riscos.store.ts';
 import { useTiposDeAcompanhamentoStore } from '@/stores/tiposDeAcompanhamento.store.ts';
 
-const riscosStore = useRiscosStore();
 const tiposDeAcompanhamentoStore = useTiposDeAcompanhamentoStore();
-
-if (!riscosStore?.lista?.length) {
-  riscosStore?.buscarTudo();
-}
 
 if (!tiposDeAcompanhamentoStore?.lista?.length) {
   tiposDeAcompanhamentoStore?.buscarTudo();
