0cff203f33442a682e2fe308e290b70e03fc0912
sobral <sobral@oficiodesign.com>
2024-01-22 17:30:20 -0300

    Criar componentes de metas para panorama

---
+++ frontend/src/components/panorama/MetaAtrasada.vue
@@ -0,0 +1,203 @@
+<script setup>
+import dateToDate from '@/helpers/dateToDate';
+
+const dateToMonth = (date) => dateToDate(date, { dateStyle: undefined, month: 'short' });
+const dateToYear = (date) => dateToDate(date, { dateStyle: undefined, year: 'numeric' });
+
+</script>
+<template>
+  <article
+    class="meta bgc50 p1 br6"
+  >
+    <h2 class="meta__título w900 t14 spacebetween uc br8">
+      Meta 10 - Manter a fila por vaga em creche zerada
+    </h2>
+    <div class="meta__meta-dados flex flexwrap spacebetween g1">
+      <ul class="meta__lista-de-anos flex flexwrap start">
+        <li class="meta__ano flex f1 g1">
+          <span class="meta__ano-valor tamarelo flex center tc">
+            {{ dateToYear('2023/01/01') }}
+          </span>
+          <ul class="meta__lista-de-eventos">
+            <li class="meta__item_do_ano br999 pl05 pr05 ib mr05 mb05">
+              <span class="meta__mês">
+                {{ dateToMonth('2024/04/01') }}
+              </span>
+              <span class="meta__numero-de-variaveis">
+                12
+              </span>
+              <span class="meta__orcamento">$</span>
+            </li>
+            <li class="meta__item_do_ano br999 pl05 pr05 ib mr05 mb05">
+              <span class="meta__mês">
+                {{ dateToMonth('2024/04/01') }}
+              </span>
+              <span class="meta__numero-de-variaveis">
+                12
+              </span>
+              <span class="meta__orcamento">$</span>
+            </li>
+            <li class="meta__item_do_ano br999 pl05 pr05 ib mr05 mb05">
+              <span class="meta__mês">
+                {{ dateToMonth('2024/04/01') }}
+              </span>
+              <span class="meta__numero-de-variaveis">
+                12
+              </span>
+              <span class="meta__orcamento">$</span>
+            </li>
+            <li class="meta__item_do_ano br999 pl05 pr05 ib mr05 mb05">
+              <span class="meta__mês">
+                {{ dateToMonth('2024/04/01') }}
+              </span>
+              <span class="meta__numero-de-variaveis">
+                12
+              </span>
+              <span class="meta__orcamento">$</span>
+            </li>
+            <li class="meta__item_do_ano br999 pl05 pr05 ib mr05 mb05">
+              <span class="meta__mês">
+                {{ dateToMonth('2024/04/01') }}
+              </span>
+              <span class="meta__numero-de-variaveis">
+                12
+              </span>
+              <span class="meta__orcamento">$</span>
+            </li>
+            <li class="meta__item_do_ano br999 pl05 pr05 ib mr05 mb05">
+              <span class="meta__mês">
+                {{ dateToMonth('2024/04/01') }}
+              </span>
+              <span class="meta__numero-de-variaveis">
+                12
+              </span>
+              <span class="meta__orcamento">$</span>
+            </li>
+            <li class="meta__item_do_ano br999 pl05 pr05 ib mr05 mb05">
+              <span class="meta__mês">
+                {{ dateToMonth('2024/04/01') }}
+              </span>
+              <span class="meta__numero-de-variaveis">
+                12
+              </span>
+              <span class="meta__orcamento">$</span>
+            </li>
+            <li class="meta__item_do_ano br999 pl05 pr05 ib mr05 mb05">
+              <span class="meta__mês">
+                {{ dateToMonth('2024/04/01') }}
+              </span>
+              <span class="meta__numero-de-variaveis">
+                12
+              </span>
+              <span class="meta__orcamento">$</span>
+            </li>
+            <li class="meta__item_do_ano br999 pl05 pr05 ib mr05 mb05">
+              <span class="meta__mês">
+                {{ dateToMonth('2024/04/01') }}
+              </span>
+              <span class="meta__numero-de-variaveis">
+                12
+              </span>
+              <span class="meta__orcamento">$</span>
+            </li>
+            <li class="meta__item_do_ano br999 pl05 pr05 ib mr05 mb05">
+              <span class="meta__mês">
+                {{ dateToMonth('2024/04/01') }}
+              </span>
+              <span class="meta__numero-de-variaveis">
+                12
+              </span>
+              <span class="meta__orcamento">$</span>
+            </li>
+            <li class="meta__item_do_ano br999 pl05 pr05 ib mr05 mb05">
+              <span class="meta__mês">
+                {{ dateToMonth('2024/04/01') }}
+              </span>
+              <span class="meta__numero-de-variaveis">
+                12
+              </span>
+              <span class="meta__orcamento">$</span>
+            </li>
+            <li class="meta__item_do_ano br999 pl05 pr05 ib mr05 mb05">
+              <span class="meta__mês">
+                {{ dateToMonth('2024/04/01') }}
+              </span>
+              <span class="meta__numero-de-variaveis">
+                12
+              </span>
+              <span class="meta__orcamento">$</span>
+            </li>
+          </ul>
+        </li>
+        <li class="meta__ano flex f1 g1">
+          <span class="meta__ano-valor tamarelo flex center tc">
+            {{ dateToYear('2022/01/01') }}
+          </span>
+          <ul class="meta__lista-de-eventos">
+            <li class="meta__item_do_ano br999 pl05 pr05 ib mr05 mb05">
+              <span class="meta__mês">
+                {{ dateToMonth('2024/04/01') }}
+              </span>
+              <span class="meta__numero-de-variaveis">
+                12
+              </span>
+              <span class="meta__orcamento">$</span>
+            </li>
+            <li class="meta__item_do_ano br999 pl05 pr05 ib mr05 mb05">
+              <span class="meta__mês">
+                {{ dateToMonth('2024/04/01') }}
+              </span>
+              <span class="meta__numero-de-variaveis">
+                12
+              </span>
+              <span class="meta__orcamento">$</span>
+            </li>
+            <li class="meta__item_do_ano br999 pl05 pr05 ib mr05 mb05">
+              <span class="meta__mês">
+                {{ dateToMonth('2024/04/01') }}
+              </span>
+              <span class="meta__numero-de-variaveis">
+                12
+              </span>
+              <span class="meta__orcamento">$</span>
+            </li>
+            <li class="meta__item_do_ano br999 pl05 pr05 ib mr05 mb05">
+              <span class="meta__mês">
+                {{ dateToMonth('2024/04/01') }}
+              </span>
+              <span class="meta__numero-de-variaveis">
+                12
+              </span>
+              <span class="meta__orcamento">$</span>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </article>
+</template>
+<style lang="less">
+.meta__lista-de-eventos {
+  position: relative;
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: -0.5rem;
+
+    border-left: 1px solid;
+  }
+}
+
+.meta__item_do_ano {
+  background: @cinza-claro-azulado;
+
+  span + span {
+    &::before {
+      content: ' | ';
+    }
+  }
+}
+</style>
---
+++ frontend/src/components/panorama/MetaNormal.vue
@@ -0,0 +1,219 @@
+<script setup>
+defineProps({
+  meta: {
+    type: Object,
+    required: true,
+  },
+  perfil: {
+    type: String,
+    default: 'PdM',
+    validator(valor) {
+      return typeof valor === 'string'
+        && [
+          'admin_cp',
+          'ponto_focal',
+          'tecnico_cp',
+        ].indexOf(valor) > -1;
+    },
+  },
+});
+</script>
+<template>
+  <article
+    class="meta bgc50 p1 br6"
+  >
+    <h2 class="meta__título w900 t14 spacebetween uc br8">
+      {{ meta.titulo }}
+    </h2>
+    <div class="meta__meta-dados flex flexwrap spacebetween tc g1">
+      <ul class="meta__icones-lista flex g1">
+        <li
+          v-if="meta.variaveis?.conferidas < meta.variaveis?.total && perfil !== 'ponto_focal'"
+          class="meta__icones-item tipinfo"
+        >
+          <svg
+            class="meta__icone fracasso"
+            width="24"
+            height="24"
+          ><use xlink:href="#i_clock" /></svg><div>Conferência</div>
+        </li>
+        <li
+          v-if="meta.variaveis?.aguardando_complementacao"
+          class="meta__icones-item tipinfo"
+        >
+          <svg
+            class="meta__icone fracasso"
+            width="24"
+            height="24"
+            color="#EE3B2B"
+          ><use xlink:href="#i_alert" /></svg><div>Complementação</div>
+        </li>
+        <li
+          v-if="meta.cronograma?.total"
+          class="meta__icones-item tipinfo"
+        >
+          <svg
+            class="meta__icone"
+            :class="meta.cronograma?.preenchido < meta.cronograma?.total
+              ? 'fracasso'
+              : 'sucesso'"
+            width="24"
+            height="24"
+          ><use xlink:href="#i_calendar" /></svg><div>Cronograma</div>
+        </li>
+        <li
+          v-if="meta.orcamento?.total"
+          class="meta__icones-item tipinfo"
+        >
+          <svg
+            class="meta__icone"
+            :class="meta.orcamento?.preenchido < meta.orcamento?.total
+              ? 'fracasso'
+              : 'sucesso'"
+            width="24"
+            height="24"
+          ><use xlink:href="#i_$" /></svg><div>Orçamento</div>
+        </li>
+        <li
+          v-if="meta.analise_qualitativa_enviada !== null"
+          class="meta__icones-item tipinfo"
+        >
+          <svg
+            class="meta__icone"
+            :class="meta.analise_qualitativa_enviada
+              ? 'sucesso'
+              : 'fracasso'"
+            width="24"
+            height="24"
+          ><use xlink:href="#i_iniciativa" /></svg><div>Qualificação</div>
+        </li>
+        <li
+          v-if="meta.risco_enviado !== null"
+          class="meta__icones-item tipinfo"
+        >
+          <svg
+            class="meta__icone"
+            :class="meta.risco_enviado
+              ? 'sucesso'
+              : 'fracasso'"
+            width="24"
+            height="24"
+          ><use xlink:href="#i_binoculars" /></svg><div>Análise de Risco</div>
+        </li>
+        <li
+          v-if="meta.fechamento_enviado !== null"
+          class="meta__icones-item tipinfo"
+        >
+          <svg
+            class="meta__icone"
+            :class="meta.fechamento_enviado
+              ? 'sucesso'
+              : 'fracasso'"
+            width="24"
+            height="24"
+          ><use xlink:href="#i_check" /></svg><div>Fechamento</div>
+        </li>
+      </ul>
+
+      <dl
+        v-if="meta.variaveis?.total"
+        class="meta__lista-de-variáveis flex"
+      >
+        <div class="meta__variável pl1 pr1">
+          <dt>{{ meta.variaveis.total }}</dt>
+          <dd>variáveis</dd>
+        </div>
+        <div
+          v-if="meta.variaveis.aguardando_complementacao"
+          class="meta__variável pl1 pr1"
+        >
+          <dt class="fracasso">
+            {{ meta.variaveis.aguardando_complementacao }}
+          </dt>
+          <dd>
+            aguardando complementação
+          </dd>
+        </div>
+        <div
+          v-if="meta.variaveis.aguardando_cp"
+          class="meta__variável pl1 pr1"
+        >
+          <dt class="alerta">
+            {{ meta.variaveis.aguardando_cp }}
+          </dt>
+          <dd>
+            aguardando CP
+          </dd>
+        </div>
+        <div
+          v-if="meta.variaveis.conferidas"
+          class="meta__variável pl1 pr1"
+        >
+          <dt
+            :class="meta.variaveis.conferidas < meta.variaveis.total
+              ? 'fracasso'
+              : 'sucesso'"
+          >
+            {{ meta.variaveis.conferidas }}
+          </dt>
+          <dd>conferidas</dd>
+        </div>
+        <div
+          v-if="meta.variaveis.enviadas"
+          class="meta__variável pl1 pr1"
+        >
+          <dt
+            :class="meta.variaveis.enviadas < meta.variaveis.total
+              ? 'fracasso'
+              : 'sucesso'"
+          >
+            {{ meta.variaveis.enviadas }}
+          </dt>
+          <dd>enviadas</dd>
+        </div>
+        <div
+          v-if="meta.variaveis.preenchidas"
+          class="meta__variável pl1 pr1"
+        >
+          <dt
+            :class="meta.variaveis.preenchidas < meta.variaveis.total
+              ? 'fracasso'
+              : 'sucesso'"
+          >
+            {{ meta.variaveis.preenchidas }}
+          </dt>
+          <dd>preenchidas</dd>
+        </div>
+      </dl>
+    </div>
+  </article>
+</template>
+<style lang="less">
+.meta__icones-lista {}
+
+.meta__icones-item {}
+
+.meta__icone {}
+
+.meta__lista-de-variáveis {}
+
+.meta__variável {
+  border-left: 1px solid;
+
+  &:first-child {
+    border-left: 0;
+  }
+}
+
+.sucesso {
+  color: @verde;
+}
+
+.fracasso {
+  color: @vermelho;
+}
+
+.alerta {
+  color: @amarelo;
+}
+</style>
