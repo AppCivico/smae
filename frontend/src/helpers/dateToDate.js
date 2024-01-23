9613a672da6a92ad2d55586fd097359d725b3a25
sobral <sobral@oficiodesign.com>
2024-01-23 11:12:56 -0300

    Aprimorar conversor de datas

--- frontend/src/helpers/dateToDate.js
+++ frontend/src/helpers/dateToDate.js
@@ -1,6 +1,14 @@
-export default function dateToDate(d, options = {}) {
+function dateToDate(d, options = {}) {
   const dd = d ? new Date(d) : false;
   if (!dd) return d;
   const dx = (dd) ? dd.toLocaleString('pt-BR', { dateStyle: 'short', timeZone: 'UTC', ...options }) : '';
   return dx || '';
 }
+
+export const dateToYear = (date, format = 'numeric') => dateToDate(date, { dateStyle: undefined, year: format });
+
+export const dateToMonth = (date, format = 'short') => dateToDate(date, { dateStyle: undefined, month: format });
+
+export const dateToDay = (date, format = 'numeric') => dateToDate(date, { dateStyle: undefined, day: format });
+
+export default dateToDate;
