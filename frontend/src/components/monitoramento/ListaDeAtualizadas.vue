2033c0703182e94f7359fdcb47210e2d0fd45673
sobral <sobral@oficiodesign.com>
2024-01-28 14:56:13 -0300

    Reorganizar listas de monitoramento

---
+++ frontend/src/components/monitoramento/ListaDeAtualizadas.vue
@@ -0,0 +1,71 @@
+<script setup>
+import { computed } from 'vue';
+import { storeToRefs } from 'pinia';
+import { usePanoramaStore } from '@/stores/panorama.store.ts';
+import truncate from '@/helpers/truncate';
+
+const panoramaStore = usePanoramaStore();
+const {
+  perfil,
+  variáveisPorId,
+} = storeToRefs(panoramaStore);
+
+//  atualizadas
+//    ponto focal:
+//      - todas as conferidas
+//    para os outros:
+//      - todas as enviadas
+const listaDeAtualizadas = computed(() => {
+  if (!perfil.value) return [];
+
+  const aUsar = perfil.value === 'ponto_focal'
+    ? 'conferidas'
+    : 'enviadas';
+  return panoramaStore.listaDeAtualizadas.map((x) => ({
+    id: x.id,
+    código: x.codigo,
+    título: x.titulo,
+    variáveis: x.variaveis?.[aUsar]?.map((y) => ({
+      id: y,
+      código: variáveisPorId.value[y]?.codigo || '',
+      título: variáveisPorId.value[y]?.titulo || '',
+      aguardaComplementação: x.variaveis.aguardando_complementacao.includes(y),
+      aguardaConferência: x.variaveis.conferidas.includes(y),
+      aguardaEnvio: x.variaveis.enviadas.includes(y),
+      aguardaPreenchimento: x.variaveis.preenchidas.includes(y),
+    })),
+  }));
+});
+</script>
+<template>
+  <ul>
+    <li
+      v-for="meta in listaDeAtualizadas"
+      :key="meta.id"
+    >
+      {{ meta.código }} - {{ meta.título }}
+      <ul
+        v-if="meta.variáveis.length"
+      >
+        <li
+          v-for="variável in meta.variáveis"
+          :key="variável.id"
+          :title="variável.título?.length > 36
+            ? variável.título
+            : undefined"
+          class="ml2"
+        >
+          {{ variável.código || variável.id }} - {{
+            truncate(variável.título, 36) }}
+
+          <small>
+            (<code>aguardaComplementação:&nbsp;{{ variável.aguardaComplementação }}</code>)
+            (<code>aguardaConferência:&nbsp;{{ variável.aguardaConferência }}</code>)
+            (<code>aguardaEnvio:&nbsp;{{ variável.aguardaEnvio }}</code>)
+            (<code>aguardaPreenchimento:&nbsp;{{ variável.aguardaPreenchimento }}</code>)
+          </small>
+        </li>
+      </ul>
+    </li>
+  </ul>
+</template>
--- frontend/src/views/monitoramento/MonitoramentosRaiz.vue
+++ frontend/src/views/monitoramento/MonitoramentosRaiz.vue
@@ -3,8 +3,9 @@ import EnvelopeDeAbas from '@/components/EnvelopeDeAbas.vue';
 import FeedbackEmptyList from '@/components/FeedbackEmptyList.vue';
 import LegendaDeVariáveis from '@/components/monitoramento/LegendaDeVariaveis.vue';
 import LegendaDeTarefas from '@/components/monitoramento/LegendaDeTarefas.vue';
-import ListaComVariáveisAtrasadas from '@/components/monitoramento/ListaComVariaveisAtrasadas.vue';
-import truncate from '@/helpers/truncate';
+import ListaDeAtrasadas from '@/components/monitoramento/ListaDeAtrasadas.vue';
+import ListaDeAtualizadas from '@/components/monitoramento/ListaDeAtualizadas.vue';
+import ListaDePendentes from '@/components/monitoramento/ListaDePendentes.vue';
 import { Dashboard } from '@/components';
 import { useRoute, useRouter } from 'vue-router';
 import { useAuthStore } from '@/stores/auth.store';
@@ -13,7 +14,7 @@ import { usePdMStore } from '@/stores/pdm.store';
 import { storeToRefs } from 'pinia';
 import dateToField from '@/helpers/dateToField';
 import dateToTitle from '@/helpers/dateToTitle';
-import { computed, ref, watch } from 'vue';
+import { computed, watch } from 'vue';
 
 const dadosExtrasDeAbas = {
   TabelaDeVariaveis: {
@@ -37,9 +38,8 @@ const panoramaStore = usePanoramaStore();
 const {
   chamadasPendentes,
   listaDeAtrasadasComDetalhes,
-  perfil,
-  tarefasPorId,
-  variáveisPorId,
+  listaDeAtualizadas,
+  listaDePendentes,
   erro,
 } = storeToRefs(panoramaStore);
 const PdMStore = usePdMStore();
@@ -49,72 +49,9 @@ const authStore = useAuthStore();
 const route = useRoute();
 const router = useRouter();
 
-const idDoItemAberto = ref(0);
-
 const faseCorrente = computed(() => (Array.isArray(activePdm.value?.ciclo_fisico_ativo?.fases)
   ? activePdm.value.ciclo_fisico_ativo.fases.find((x) => x.fase_corrente)
   : null));
-
-//  pendentes:
-//    para o ponto focal:
-//      - total - enviadas + aguardando complementação
-//    para os outros:
-//      - total - conferidas + aguardando complementação
-//  atualizadas
-//    ponto focal:
-//      - todas as conferidas
-//    para os outros:
-//      - todas as enviadas
-
-const listaDePendentes = computed(() => {
-  if (!perfil.value) return [];
-
-  const aRemover = perfil.value === 'ponto_focal'
-    ? 'enviadas'
-    : 'conferidas';
-  return panoramaStore.listaDePendentes.map((x) => ({
-    id: x.id,
-    código: x.codigo,
-    título: x.titulo,
-    variáveis: x.variaveis?.total?.reduce((acc, cur) => {
-      const manter = x.variaveis.aguardando_complementacao.includes(cur);
-      const remover = !x.variaveis[aRemover].includes(cur);
-      return (manter || remover)
-        ? acc.concat([{
-          id: cur,
-          código: variáveisPorId.value[cur]?.codigo || '',
-          título: variáveisPorId.value[cur]?.titulo || '',
-          aguardaComplementação: manter,
-          aguardaConferência: x.variaveis.conferidas.includes(cur),
-          aguardaEnvio: x.variaveis.enviadas.includes(cur),
-          aguardaPreenchimento: x.variaveis.preenchidas.includes(cur),
-        }])
-        : acc;
-    }, []),
-  }));
-});
-
-const listaDeAtualizadas = computed(() => {
-  if (!perfil.value) return [];
-
-  const aUsar = perfil.value === 'ponto_focal'
-    ? 'conferidas'
-    : 'enviadas';
-  return panoramaStore.listaDeAtualizadas.map((x) => ({
-    id: x.id,
-    código: x.codigo,
-    título: x.titulo,
-    variáveis: x.variaveis?.[aUsar]?.map((y) => ({
-      id: y,
-      código: variáveisPorId.value[y]?.codigo || '',
-      título: variáveisPorId.value[y]?.titulo || '',
-      aguardaComplementação: x.variaveis.aguardando_complementacao.includes(y),
-      aguardaConferência: x.variaveis.conferidas.includes(y),
-      aguardaEnvio: x.variaveis.enviadas.includes(y),
-      aguardaPreenchimento: x.variaveis.preenchidas.includes(y),
-    })),
-  }));
-});
 
 async function iniciar() {
   if (!route.query.exibir) {
@@ -232,107 +169,7 @@ watch([
               mensagem="Você não possui pendências!"
             />
 
-            <ul
-              v-else
-              class="uc w700"
-            >
-              <li
-                v-for="meta in listaDePendentes"
-                :key="meta.id"
-              >
-                <span class="block mb1 bgc50 br6 p1 flex start">
-                  <button
-                    class="like-a__text addlink ib f0"
-                    :arial-label="idDoItemAberto === meta.id
-                      ? `fechar variáveis da meta ${meta.código}`
-                      : `abrir variáveis da meta ${meta.código}`"
-                    :title="idDoItemAberto === meta.id
-                      ? `fechar variáveis da meta ${meta.código}`
-                      : `abrir variáveis da meta ${meta.código}`"
-                    type="button"
-                    :disabled="!meta.variáveis.length"
-                    @click="idDoItemAberto = idDoItemAberto !== meta.id
-                      ? meta.id
-                      : 0"
-                  >
-                    <svg
-                      width="13"
-                      height="13"
-                    ><use
-                      :xlink:href="idDoItemAberto === meta.id ? '#i_down' : '#i_right'"
-                    /></svg>
-                  </button>
-                  {{ meta.código }} - {{ meta.título }}
-                </span>
-                <Transition
-                  v-if="meta.variáveis.length"
-                  name="fade"
-                >
-                  <ul
-                    v-if="idDoItemAberto === meta.id"
-                    class="pl2"
-                  >
-                    <li
-                      v-for="variável in meta.variáveis"
-                      :key="variável.id"
-                      class="mb1 bgc50 br6 p1 flex start"
-                    >
-                      <span
-                        v-if="variável.aguardaPreenchimento"
-                        class="tipinfo ib mr1 f0"
-                      >
-                        <svg
-                          width="20"
-                          height="20"
-                          color="#f2890d"
-                        ><use xlink:href="#i_alert" /></svg>
-                        <div>Aguarda preenchimento</div>
-                      </span>
-                      <span
-                        v-else-if="variável.aguardaComplementação"
-                        class="tipinfo ib mr1 f0"
-                      >
-                        <svg
-                          width="20"
-                          height="20"
-                          color="#ee3b2b"
-                        ><use xlink:href="#i_alert" /></svg>
-                        <div>Aguarda coleta</div>
-                      </span>
-                      <span
-                        v-else-if="variável.aguardaConferência"
-                        class="tipinfo ib mr1 f0"
-                      >
-                        <svg
-                          width="20"
-                          height="20"
-                          color="#4c626d"
-                        ><use xlink:href="#i_alert" /></svg>
-                        <div>Aguarda complementação</div>
-                      </span>
-                      <span
-                        v-else-if="variável.aguardaEnvio"
-                        class="tipinfo ib mr1 f0"
-                      >
-                        <svg
-                          width="20"
-                          height="20"
-                          color="#f2890d"
-                        ><use xlink:href="#i_circle" /></svg>
-                        <div>Aguarda envio</div>
-                      </span>
-                      {{ variável.código || variável.id }} - {{ variável.título }}
-                      <small v-ScrollLockDebug>
-                        (<code>aguardaComplementação:&nbsp;{{ variável.aguardaComplementação }}</code>)
-                        (<code>aguardaConferência:&nbsp;{{ variável.aguardaConferência }}</code>)
-                        (<code>aguardaEnvio:&nbsp;{{ variável.aguardaEnvio }}</code>)
-                        (<code>aguardaPreenchimento:&nbsp;{{ variável.aguardaPreenchimento }}</code>)
-                      </small>
-                    </li>
-                  </ul>
-                </Transition>
-              </li>
-            </ul>
+            <ListaDePendentes v-else />
           </template>
         </template>
 
@@ -347,36 +184,7 @@ watch([
               mensagem="Complete pendências para visualizar-las aqui."
             />
 
-            <ul v-else>
-              <li
-                v-for="meta in listaDeAtualizadas"
-                :key="meta.id"
-              >
-                {{ meta.código }} - {{ meta.título }}
-                <ul
-                  v-if="meta.variáveis.length"
-                >
-                  <li
-                    v-for="variável in meta.variáveis"
-                    :key="variável.id"
-                    :title="variável.título?.length > 36
-                      ? variável.título
-                      : undefined"
-                    class="ml2"
-                  >
-                    {{ variável.código || variável.id }} - {{
-                      truncate(variável.título, 36) }}
-
-                    <small>
-                      (<code>aguardaComplementação:&nbsp;{{ variável.aguardaComplementação }}</code>)
-                      (<code>aguardaConferência:&nbsp;{{ variável.aguardaConferência }}</code>)
-                      (<code>aguardaEnvio:&nbsp;{{ variável.aguardaEnvio }}</code>)
-                      (<code>aguardaPreenchimento:&nbsp;{{ variável.aguardaPreenchimento }}</code>)
-                    </small>
-                  </li>
-                </ul>
-              </li>
-            </ul>
+            <ListaDeAtualizadas v-else />
           </template>
         </template>
 
@@ -391,10 +199,7 @@ watch([
               mensagem="Você não possui atrasos!"
             />
 
-            <ListaComVariáveisAtrasadas
-              v-else
-              :lista="listaDeAtrasadasComDetalhes"
-            />
+            <ListaDeAtrasadas v-else />
 
             <textarea
               readonly
