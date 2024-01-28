6a755891bdf873471664d4b51a0637f18dccbe3a
sobral <sobral@oficiodesign.com>
2024-01-28 16:05:15 -0300

    Reorganizar rotas de monitoramento

---
+++ frontend/src/views/monitoramento/MonitoramentoPorVariaveis.vue
@@ -0,0 +1,135 @@
+<script setup>
+import EnvelopeDeAbas from '@/components/EnvelopeDeAbas.vue';
+import FeedbackEmptyList from '@/components/FeedbackEmptyList.vue';
+import LegendaDeVariáveis from '@/components/monitoramento/LegendaDeVariaveis.vue';
+import ListaDeAtrasadas from '@/components/monitoramento/ListaDeAtrasadas.vue';
+import ListaDeAtualizadas from '@/components/monitoramento/ListaDeAtualizadas.vue';
+import ListaDePendentes from '@/components/monitoramento/ListaDePendentes.vue';
+import { usePanoramaStore } from '@/stores/panorama.store.ts';
+import { usePdMStore } from '@/stores/pdm.store';
+import { storeToRefs } from 'pinia';
+import { useRoute, useRouter } from 'vue-router';
+import { watch } from 'vue';
+
+const route = useRoute();
+const router = useRouter();
+
+const statusesVálidos = ['pendentes', 'atualizadas', 'atrasadas'];
+
+const panoramaStore = usePanoramaStore();
+const {
+  chamadasPendentes,
+  listaDeAtrasadasComDetalhes,
+  listaDeAtualizadas,
+  listaDePendentes,
+} = storeToRefs(panoramaStore);
+
+const PdMStore = usePdMStore();
+const { activePdm } = storeToRefs(PdMStore);
+
+const dadosExtrasDeAbas = {
+  TabelaDeVariaveis: {
+    id: 'variaveis',
+    etiqueta: 'Variáveis',
+  },
+  TabelaDeVariaveisCompostas: {
+    id: 'variaveis-compostas',
+    etiqueta: 'Variáveis Compostas',
+  },
+  TabelaDeVariaveisCompostasEmUso: {
+    id: 'variaveis-compostas-em-uso',
+    etiqueta: 'Variáveis compostas em uso',
+    aberta: true,
+  },
+};
+
+async function iniciar() {
+  if (statusesVálidos.indexOf(route.query.status) === -1) {
+    router.replace({
+      query: {
+        ...route.query,
+        status: statusesVálidos[0],
+      },
+    });
+  }
+
+  if (!activePdm.value.id) {
+    await PdMStore.getActive();
+  }
+  panoramaStore.buscarTudo(activePdm.value.id, route.query.status, {
+    retornar_detalhes: true,
+  });
+}
+
+watch(() => route.query.status, () => {
+  iniciar();
+}, { immediate: true });
+</script>
+<template>
+  <LegendaDeVariáveis class="legenda legenda--variáveis" />
+  <EnvelopeDeAbas
+    :meta-dados-por-id="dadosExtrasDeAbas"
+    nome-da-chave-de-abas="status"
+    class="mb1 f3"
+    alinhamento="esquerda"
+  >
+    <template #pendentes="{ estáAberta }">
+      <template v-if="estáAberta">
+        <LoadingComponent v-if="chamadasPendentes.lista" />
+
+        <FeedbackEmptyList
+          v-else-if="!listaDePendentes.length"
+          título="Bom trabalho!"
+          tipo="positivo"
+          mensagem="Você não possui pendências!"
+        />
+
+        <ListaDePendentes v-else />
+      </template>
+    </template>
+
+    <template #atualizadas="{ estáAberta }">
+      <template v-if="estáAberta">
+        <LoadingComponent v-if="chamadasPendentes.lista" />
+
+        <FeedbackEmptyList
+          v-else-if="!listaDeAtualizadas.length"
+          tipo="negativo"
+          título="Você ainda não possui atividades atualizadas!"
+          mensagem="Complete pendências para visualizar-las aqui."
+        />
+
+        <ListaDeAtualizadas v-else />
+      </template>
+    </template>
+
+    <template #atrasadas="{ estáAberta }">
+      <template v-if="estáAberta">
+        <LoadingComponent v-if="chamadasPendentes.lista" />
+
+        <FeedbackEmptyList
+          v-else-if="!listaDeAtrasadasComDetalhes.length"
+          título="Bom trabalho!"
+          tipo="positivo"
+          mensagem="Você não possui atrasos!"
+        />
+
+        <ListaDeAtrasadas v-else />
+      </template>
+    </template>
+  </EnvelopeDeAbas>
+</template>
+<style lang="less" scoped>
+.legenda {
+  margin-right: 0;
+  margin-left: auto;
+}
+
+.legenda h2 {
+  text-align: center;
+}
+
+.legenda--variáveis {
+  max-width: 20em;
+}
+</style>
--- frontend/src/views/monitoramento/MonitoramentosRaiz.vue
+++ frontend/src/views/monitoramento/MonitoramentosRaiz.vue
@@ -1,11 +1,4 @@
 <script setup>
-import EnvelopeDeAbas from '@/components/EnvelopeDeAbas.vue';
-import FeedbackEmptyList from '@/components/FeedbackEmptyList.vue';
-import LegendaDeVariáveis from '@/components/monitoramento/LegendaDeVariaveis.vue';
-import LegendaDeTarefas from '@/components/monitoramento/LegendaDeTarefas.vue';
-import ListaDeAtrasadas from '@/components/monitoramento/ListaDeAtrasadas.vue';
-import ListaDeAtualizadas from '@/components/monitoramento/ListaDeAtualizadas.vue';
-import ListaDePendentes from '@/components/monitoramento/ListaDePendentes.vue';
 import { Dashboard } from '@/components';
 import { useRoute, useRouter } from 'vue-router';
 import { useAuthStore } from '@/stores/auth.store';
@@ -14,32 +7,10 @@ import { usePdMStore } from '@/stores/pdm.store';
 import { storeToRefs } from 'pinia';
 import dateToField from '@/helpers/dateToField';
 import dateToTitle from '@/helpers/dateToTitle';
-import { computed, watch } from 'vue';
-
-const dadosExtrasDeAbas = {
-  TabelaDeVariaveis: {
-    id: 'variaveis',
-    etiqueta: 'Variáveis',
-  },
-  TabelaDeVariaveisCompostas: {
-    id: 'variaveis-compostas',
-    etiqueta: 'Variáveis Compostas',
-  },
-  TabelaDeVariaveisCompostasEmUso: {
-    id: 'variaveis-compostas-em-uso',
-    etiqueta: 'Variáveis compostas em uso',
-    aberta: true,
-  },
-};
-const exibiçãoPadrão = 'variaveis';
-const statusesVálidos = ['pendentes', 'atualizadas', 'atrasadas'];
+import { computed } from 'vue';
 
 const panoramaStore = usePanoramaStore();
 const {
-  chamadasPendentes,
-  listaDeAtrasadasComDetalhes,
-  listaDeAtualizadas,
-  listaDePendentes,
   erro,
 } = storeToRefs(panoramaStore);
 const PdMStore = usePdMStore();
@@ -54,31 +25,9 @@ const faseCorrente = computed(() => (Array.isArray(activePdm.value?.ciclo_fisico
   : null));
 
 async function iniciar() {
-  if (!route.query.exibir) {
-    router.replace({
-      query: {
-        ...route.query,
-        exibir: exibiçãoPadrão,
-      },
-    });
-  }
-
-  if (statusesVálidos.indexOf(route.query.status) === -1) {
-    router.replace({
-      query: {
-        ...route.query,
-        status: statusesVálidos[0],
-      },
-    });
-  }
-
   if (!activePdm.value.id) {
     await PdMStore.getActive();
   }
-
-  panoramaStore.buscarTudo(activePdm.value.id, route.query.status, {
-    retornar_detalhes: true,
-  });
 }
 
 if (!authStore.temPermissãoPara(['PDM.admin_cp', 'PDM.tecnico_cp'])) {
@@ -89,12 +38,7 @@ if (!authStore.temPermissãoPara(['PDM.admin_cp', 'PDM.tecnico_cp'])) {
   });
 }
 
-watch([
-  () => route.query.status,
-  () => route.query.exibir,
-], () => {
-  iniciar();
-}, { immediate: true });
+iniciar();
 </script>
 <template>
   <Dashboard>
@@ -125,23 +69,22 @@ watch([
           class="flex g1 flexwrap"
         >
           <router-link
-            :to="{ query: { ...$route.query, exibir: 'variaveis' } }"
-            class="btn bgnone outline tcprimary"
-            :class="{
-              tcamarelo: $route.query.exibir === 'variaveis'
+            :to="{
+              name: 'monitoramentoPorVariáveis',
+              query: $route.query,
             }"
-            :aria-current="$route.query.exibir === 'variaveis' ? 'page' : undefined"
+            class="btn bgnone outline tcprimary"
+            exact-active-class="tcamarelo"
           >
             por Variável
           </router-link>
 
           <router-link
-            :to="{ query: { ...$route.query, exibir: 'tarefas' } }"
-            class="btn bgnone outline tcprimary"
-            :class="{
-              tcamarelo: $route.query.exibir === 'tarefas'
+            :to="{
+              name: 'monitoramentoPorTarefas',
             }"
-            :aria-current="$route.query.exibir === 'tarefas' ? 'page' : undefined"
+            class="btn bgnone outline tcprimary"
+            exact-active-class="tcamarelo"
           >
             por Cronograma
           </router-link>
@@ -149,94 +92,10 @@ watch([
       </div>
     </header>
 
-    <template v-if="$route.query.exibir === 'variaveis'">
-      <LegendaDeVariáveis class="legenda legenda--variáveis" />
-
-      <EnvelopeDeAbas
-        :meta-dados-por-id="dadosExtrasDeAbas"
-        nome-da-chave-de-abas="status"
-        class="mb1 f3"
-        alinhamento="esquerda"
-      >
-        <template #pendentes="{ estáAberta }">
-          <template v-if="estáAberta">
-            <LoadingComponent v-if="chamadasPendentes.lista" />
-
-            <FeedbackEmptyList
-              v-else-if="!listaDePendentes.length"
-              título="Bom trabalho!"
-              tipo="positivo"
-              mensagem="Você não possui pendências!"
-            />
-
-            <ListaDePendentes v-else />
-          </template>
-        </template>
-
-        <template #atualizadas="{ estáAberta }">
-          <template v-if="estáAberta">
-            <LoadingComponent v-if="chamadasPendentes.lista" />
-
-            <FeedbackEmptyList
-              v-else-if="!listaDeAtualizadas.length"
-              tipo="negativo"
-              título="Você ainda não possui atividades atualizadas!"
-              mensagem="Complete pendências para visualizar-las aqui."
-            />
-
-            <ListaDeAtualizadas v-else />
-          </template>
-        </template>
-
-        <template #atrasadas="{ estáAberta }">
-          <template v-if="estáAberta">
-            <LoadingComponent v-if="chamadasPendentes.lista" />
-
-            <FeedbackEmptyList
-              v-else-if="!listaDeAtrasadasComDetalhes.length"
-              título="Bom trabalho!"
-              tipo="positivo"
-              mensagem="Você não possui atrasos!"
-            />
-
-            <ListaDeAtrasadas v-else />
-
-            <textarea
-              readonly
-              cols="30"
-              rows="10"
-            >listaDeAtrasadasComDetalhes:
-    {{ listaDeAtrasadasComDetalhes }}
-    </textarea>
-          </template>
-        </template>
-      </EnvelopeDeAbas>
-    </template>
-
-    <template v-if="$route.query.exibir === 'tarefas'">
-      <LegendaDeTarefas class="legenda legenda--tarefas" />
-    </template>
+    <RouterView />
 
     <ErrorComponent v-if="erro">
       {{ erro }}
     </ErrorComponent>
   </Dashboard>
 </template>
-<style lang="less" scoped>
-.legenda {
-  margin-right: 0;
-  margin-left: auto;
-}
-
-.legenda h2 {
-  text-align: center;
-}
-
-.legenda--variáveis {
-  max-width: 20em;
-}
-
-.legenda--tarefas {
-  max-width: 40em;
-}
-</style>
