294ba74917237ce589441cf4809e8590a9a5fe25
sobral <sobral@oficiodesign.com>
2024-01-30 10:57:13 -0300

    Exibir lista de metas e tarefas no monitoramento

--- frontend/src/views/monitoramento/MonitoramentoPorTarefas.vue
+++ frontend/src/views/monitoramento/MonitoramentoPorTarefas.vue
@@ -1,8 +1,31 @@
 <script setup>
 import LegendaDeTarefas from '@/components/monitoramento/LegendaDeTarefas.vue';
+import { usePanoramaStore } from '@/stores/panorama.store.ts';
+import ListaDeCronogramas from '@/components/monitoramento/ListaDeCronogramas.vue';
+import { usePdMStore } from '@/stores/pdm.store';
+import { storeToRefs } from 'pinia';
+
+const panoramaStore = usePanoramaStore();
+const {
+  listaDePendentes,
+} = storeToRefs(panoramaStore);
+
+const PdMStore = usePdMStore();
+const { activePdm } = storeToRefs(PdMStore);
+
+async function iniciar() {
+  if (!activePdm.value.id) {
+    await PdMStore.getActive();
+  }
+
+  panoramaStore.buscarTudo(activePdm.value.id, 'pendentes', {
+    retornar_detalhes: true,
+  });
+}
+iniciar();
 </script>
 <template>
-  <div>
-    <LegendaDeTarefas class="legenda legenda--tarefas mb2" />
-  </div>
+  <LegendaDeTarefas class="legenda legenda--tarefas mb2" />
+
+  <ListaDeCronogramas />
 </template>
