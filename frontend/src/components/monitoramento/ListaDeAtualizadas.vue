2c7cad6332432d0ae169500d0ede6390d21dfb78
sobral <sobral@oficiodesign.com>
2024-01-30 15:55:19 -0300

    Atualizar listagem de atualizadas no monitoramento

--- frontend/src/components/monitoramento/ListaDeAtualizadas.vue
+++ frontend/src/components/monitoramento/ListaDeAtualizadas.vue
@@ -1,14 +1,15 @@
 <script setup>
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import { storeToRefs } from 'pinia';
 import { usePanoramaStore } from '@/stores/panorama.store.ts';
-import truncate from '@/helpers/truncate';
 
 const panoramaStore = usePanoramaStore();
 const {
   perfil,
   variáveisPorId,
 } = storeToRefs(panoramaStore);
+
+const idsDosItensAbertos = ref([]);
 
 //  atualizadas
 //    ponto focal:
@@ -21,6 +22,7 @@ const listaDeAtualizadas = computed(() => {
   const aUsar = perfil.value === 'ponto_focal'
     ? 'conferidas'
     : 'enviadas';
+
   return panoramaStore.listaDeAtualizadas.map((x) => ({
     id: x.id,
     código: x.codigo,
@@ -29,47 +31,57 @@ const listaDeAtualizadas = computed(() => {
       id: y,
       código: variáveisPorId.value[y]?.codigo || '',
       título: variáveisPorId.value[y]?.titulo || '',
-      aguardaComplementação: x.variaveis.aguardando_complementacao.includes(y),
-      aguardaConferência: x.variaveis.conferidas.includes(y),
-      aguardaEnvio: x.variaveis.enviadas.includes(y),
-      aguardaPreenchimento: x.variaveis.preenchidas.includes(y),
     }))
       .sort((a, b) => a.código.localeCompare(b.código)),
   }));
 });
 </script>
 <template>
-  <ul>
+  <ul class="uc w700">
     <li
       v-for="meta in listaDeAtualizadas"
       :key="meta.id"
     >
-      {{ meta.código }} - {{ meta.título }}
-      <small v-ScrollLockDebug>
-        (<code>meta.atualizado_em:&nbsp;{{ meta.atualizado_em }}</code>)
-      </small>
-      <ul
-        v-if="meta.variáveis.length"
+      <input
+        :id="`atualizada--${meta.id}`"
+        v-model="idsDosItensAbertos"
+        type="checkbox"
+        :value="meta.id"
+        :arial-label="idsDosItensAbertos.includes(meta.id)
+          ? `fechar variáveis da meta ${meta.código}`
+          : `abrir variáveis da meta ${meta.código}`"
+        :title="idsDosItensAbertos.includes(meta.id)
+          ? `fechar variáveis da meta ${meta.código}`
+          : `abrir variáveis da meta ${meta.código}`"
+        :disabled="!meta.variáveis.length"
+        class="accordion-opener"
       >
-        <li
-          v-for="variável in meta.variáveis"
-          :key="variável.id"
-          :title="variável.título?.length > 36
-            ? variável.título
-            : undefined"
-          class="ml2"
+      <label
+        :for="`atualizada--${meta.id}`"
+        class="block mb1 bgc50 br6 p1 g1 flex center"
+      >
+        {{ meta.código }} - {{ meta.título }}
+        <small v-ScrollLockDebug>
+          (<code>meta.atualizado_em:&nbsp;{{ meta.atualizado_em }}</code>)
+        </small>
+      </label>
+      <Transition
+        v-if="meta.variáveis.length"
+        name="fade"
+      >
+        <ul
+          v-if="idsDosItensAbertos.includes(meta.id)"
+          class="pl2"
         >
-          {{ variável.código || variável.id }} - {{
-            truncate(variável.título, 36) }}
-
-          <small>
-            (<code>aguardaComplementação:&nbsp;{{ variável.aguardaComplementação }}</code>)
-            (<code>aguardaConferência:&nbsp;{{ variável.aguardaConferência }}</code>)
-            (<code>aguardaEnvio:&nbsp;{{ variável.aguardaEnvio }}</code>)
-            (<code>aguardaPreenchimento:&nbsp;{{ variável.aguardaPreenchimento }}</code>)
-          </small>
-        </li>
-      </ul>
+          <li
+            v-for="variável in meta.variáveis"
+            :key="variável.id"
+            class="mb1 bgc50 br6 p1 flex start"
+          >
+            {{ variável.código || variável.id }} - {{ variável.título }}
+          </li>
+        </ul>
+      </Transition>
     </li>
   </ul>
 </template>
