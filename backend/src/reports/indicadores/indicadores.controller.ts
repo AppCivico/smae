a957b566accf1d16806bd192df4469111a67cc3d
Renato Cron <renato.cron@gmail.com>
2024-02-02 14:36:03 -0300

    Adicionando report de indicadores com streaming

--- backend/src/reports/indicadores/indicadores.controller.ts
+++ backend/src/reports/indicadores/indicadores.controller.ts
@@ -1,8 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { ApiBearerAuth, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
+import { Body, Controller, Post, Res } from '@nestjs/common';
+import {
+    ApiBearerAuth,
+    ApiExtraModels,
+    ApiOkResponse,
+    ApiProduces,
+    ApiTags,
+    ApiUnauthorizedResponse,
+    refs,
+} from '@nestjs/swagger';
+import { Response as ExpressResponse } from 'express';
 import { Roles } from '../../auth/decorators/roles.decorator';
+import { JsonStringifyTransformStream } from '../../auth/pipe/JsonStringifyTransformStream.pipe';
 import { CreateRelIndicadorDto } from './dto/create-indicadore.dto';
-import { ListIndicadoresDto } from './entities/indicadores.entity';
+import { ListIndicadoresDto, RelIndicadoresDto, RelIndicadoresVariaveisDto } from './entities/indicadores.entity';
 import { IndicadoresService } from './indicadores.service';
 
 @ApiTags('Relatórios - API')
@@ -16,5 +26,62 @@ export class IndicadoresController {
     @Roles('Reports.executar')
     async create(@Body() dto: CreateRelIndicadorDto): Promise<ListIndicadoresDto> {
         return await this.indicadoresService.create(dto);
+    }
+
+    @Post('/stream-linhas')
+    @ApiBearerAuth('access-token')
+    @ApiUnauthorizedResponse()
+    @Roles('Reports.executar')
+    @ApiOkResponse({
+        content: {
+            'application/jsonlines+json': {
+                schema: {
+                    oneOf: refs(RelIndicadoresDto),
+                },
+            },
+        },
+    })
+    @ApiExtraModels(RelIndicadoresDto)
+    streamLinhas(@Body() dto: CreateRelIndicadorDto, @Res() res: ExpressResponse) {
+        res.setHeader('Content-Type', 'application/jsonlines+json');
+        res.writeHead(200);
+        res.flushHeaders();
+
+        const stream = this.indicadoresService.streamLinhas(dto).on('error', this.processError(res));
+        const transformStream = JsonStringifyTransformStream();
+
+        stream.pipe(transformStream).pipe(res);
+    }
+
+    @Post('/stream-regioes')
+    @ApiBearerAuth('access-token')
+    @ApiUnauthorizedResponse()
+    @Roles('Reports.executar')
+    @ApiOkResponse({
+        content: {
+            'application/jsonlines+json': {
+                schema: { oneOf: refs(RelIndicadoresVariaveisDto) },
+            },
+        },
+    })
+    @ApiExtraModels(RelIndicadoresVariaveisDto)
+    @ApiProduces('application/jsonlines+json')
+    streamRegioes(@Body() dto: CreateRelIndicadorDto, @Res() res: ExpressResponse) {
+        res.setHeader('Content-Type', 'application/jsonlines+json');
+        res.writeHead(200);
+        res.flushHeaders();
+
+        const stream = this.indicadoresService.streamRegioes(dto).on('error', this.processError(res));
+        const transformStream = JsonStringifyTransformStream();
+
+        stream.pipe(transformStream).pipe(res);
+    }
+
+    private processError(res: ExpressResponse<any, Record<string, any>>): (err: Error) => void {
+        return (err) => {
+            console.log(err);
+            res.write(JSON.stringify({ error: 'error occurred during stream processing' }));
+            res.end();
+        };
     }
 }
