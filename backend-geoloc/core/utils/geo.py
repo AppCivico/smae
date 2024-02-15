83e69dcee8071e25fb07a0f7e38c744668ca9546
Renato CRON <renato.cron@gmail.com>
2024-02-15 12:14:20 -0300

    Inverter de novo

--- backend-geoloc/core/utils/geo.py
+++ backend-geoloc/core/utils/geo.py
@@ -41,24 +41,24 @@ def geojson_dict_to_geodf(geojson:dict, crs_data=True)->gpd.GeoDataFrame:
 
 
 def extract_points_from_feature(geojson_feature:dict)->Tuple[float, float]:
-        
+
         geom = geojson_feature['geometry']
         geom_type = geom['type']
         if geom_type!='Point':
             raise ValueError(f'Geometria deve ser ponto. Geometria: {geom_type}')
-        
+
         x, y = geom['coordinates']
 
         #nunca entendi o por que, mas precisa inverter
         return y, x
-    
+
 def convert_points_to_sirgas(geojson_feature:dict)->Tuple[float, float]:
 
     x, y = extract_points_from_feature(geojson_feature)
     x, y = point_from_wgs_to_sirgas(x, y)
 
-
-    return x, y
+    # inverter de volta
+    return y, x
 
 
 def geojson_crs_param(epsg_num:int)->None:
@@ -81,7 +81,7 @@ def geojson_envelop(feature_list:List[dict], epsg_num:int=None)->dict:
 
     if epsg_num:
         geojson['crs'] = geojson_crs_param(epsg_num)
-        
+
 
     return geojson
 
