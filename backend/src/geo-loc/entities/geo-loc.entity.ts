818fdad0ba2d484855832a8540a4d0c4e996df8c
Renato CRON <renato.cron@gmail.com>
2024-02-15 13:52:48 -0300

    Retornando descricao

--- backend/src/geo-loc/entities/geo-loc.entity.ts
+++ backend/src/geo-loc/entities/geo-loc.entity.ts
@@ -19,6 +19,7 @@ export class GeoLocCamadaSimplesDto {
     id: number;
     titulo: string;
     codigo: string;
+    descricao: string
 }
 
 export class GeoLocCamadaFullDto extends GeoLocCamadaSimplesDto {
