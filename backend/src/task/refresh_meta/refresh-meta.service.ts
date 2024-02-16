58ad3fbc06768cdbe4b99d2c4a900c3c32394b66
Renato CRON <renato.cron@gmail.com>
2024-02-15 21:23:43 -0300

    Retries em todos os erros no RefreshMetaService

--- backend/src/task/refresh_meta/refresh-meta.service.ts
+++ backend/src/task/refresh_meta/refresh-meta.service.ts
@@ -3,6 +3,7 @@ import { RetryPromise } from '../../common/retryPromise';
 import { PrismaService } from '../../prisma/prisma.service';
 import { TaskableService } from '../entities/task.entity';
 import { CreateRefreshMetaDto } from './dto/create-refresh-mv.dto';
+import { sleepFor } from '../../common/sleepFor';
 
 @Injectable()
 export class RefreshMetaService implements TaskableService {
@@ -13,19 +14,31 @@ export class RefreshMetaService implements TaskableService {
         const before = Date.now();
 
         this.logger.verbose(`Refreshing meta ${inputParams.meta_id}...`);
-
-        await RetryPromise(
-            () =>
-                this.prisma.$queryRaw`
+        let tries = 0;
+        do {
+            try {
+                tries++;
+                await RetryPromise(
+                    () =>
+                        this.prisma.$queryRaw`
                     select atualiza_meta_status_consolidado(
                       ${inputParams.meta_id}::int,
                       (select id from ciclo_fisico where ativo)
                     );
                 `,
-            10,
-            100,
-            20
-        );
+                    10,
+                    100,
+                    20
+                );
+                // sai fora do loop no sucesso
+                tries = Number.MAX_SAFE_INTEGER;
+            } catch (error) {
+                this.logger.error(error);
+                this.logger.error('Erro desconhecido, tentando novamente em 1s');
+                await sleepFor(1000);
+                if (tries >= 5) throw error;
+            }
+        } while (tries < 5); // just in case, pra n ter loop infinito
 
         const took = Date.now() - before;
         return {
