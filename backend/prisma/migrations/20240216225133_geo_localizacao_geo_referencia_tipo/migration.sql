579c46ddf33f468a5f2af59aa67b661cdd488f5e
Renato CRON <renato.cron@gmail.com>
2024-02-16 20:37:05 -0300

    Rename table

--- backend/prisma/migrations/20240216225133_geo_localizacao_geo_referencia_tipo/migration.sql
+++ backend/prisma/migrations/20240216225133_geo_localizacao_geo_referencia_tipo/migration.sql
@@ -1,5 +1,5 @@
-
-delete from geo_localizacao_regiao;
+alter table geo_endereco_regiao rename to geo_localizacao_camada;
+delete from geo_localizacao_camada;
 delete from geo_localizacao;
 
 ALTER TABLE "geo_localizacao" ADD COLUMN     "tipo" "GeoReferenciaTipo" NOT NULL;
