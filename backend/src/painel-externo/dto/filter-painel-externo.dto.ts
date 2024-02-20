39a4bb70cec6d24e8ee5edc71e4a89c6ce5a398f
Renato Cron <renato.cron@gmail.com>
2024-02-20 15:36:58 -0300

    Add @Transform((a: TransformFnParams) => (a.value ===  ? undefined : +a.value))

--- backend/src/painel-externo/dto/filter-painel-externo.dto.ts
+++ backend/src/painel-externo/dto/filter-painel-externo.dto.ts
@@ -1,7 +1,9 @@
+import { Transform, TransformFnParams } from 'class-transformer';
 import { IsInt, IsOptional } from 'class-validator';
 
 export class FilterPainelExternoDto {
     @IsOptional()
     @IsInt()
+    @Transform((a: TransformFnParams) => (a.value === '' ? undefined : +a.value))
     id?: number;
 }
