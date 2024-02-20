9bb2ff2c05fc9e44a29da87e5bb2a1c6122ee623
Renato Cron <renato.cron@gmail.com>
2024-02-19 21:12:45 -0300

    src/auth/transforms/number-array.error.ts ~31.51% otimize

--- backend/src/auth/transforms/number-array.error.ts
+++ backend/src/auth/transforms/number-array.error.ts
@@ -1,9 +1,21 @@
 import { TransformFnParams } from 'class-transformer';
 
 export function NumberArrayTransform(a: TransformFnParams): number[] | undefined {
-    if (Array.isArray(a.value))
-        return (a.value as string[])
-            .map((i) => (i === '' ? undefined : +i))
-            .filter((i) => typeof i !== undefined) as number[];
-    return a.value === '' ? undefined : [+a.value];
+    if (Array.isArray(a.value)) {
+        const result: number[] = [];
+
+        for (const currentValue of a.value) {
+            if (currentValue === '') continue;
+
+            const parsedValue = +currentValue;
+            if (!isNaN(parsedValue)) result.push(parsedValue);
+        }
+
+        return result.length > 0 ? result : undefined;
+    } else if (a.value !== '') {
+        const parsedValue = +a.value;
+        return isNaN(parsedValue) ? undefined : [parsedValue];
+    }
+
+    return undefined;
 }
