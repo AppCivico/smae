39172689a86785fdc9a8d1f813137c38c47c8d92
lucasansei <lucas.ansei@eokoe.com>
2024-02-21 12:03:04 -0300

    ajuste em verificação de endereçoo

--- backend/prisma/manual-copy/triggers-crono-etapa.sql
+++ backend/prisma/manual-copy/triggers-crono-etapa.sql
@@ -309,25 +309,31 @@ FOR EACH ROW
 EXECUTE FUNCTION calculate_percentual_execucao_trigger();
 
 CREATE OR REPLACE FUNCTION assert_geoloc_rule(e_id INTEGER, c_id INTEGER)
-RETURNS VOID AS $$
+RETURNS record
+AS $$
+DECLARE
+    rec record;
 BEGIN
-    IF EXISTS(
-        SELECT
-            1
-        FROM cronograma_etapa ce1 
-        JOIN etapa e1 ON ce1.etapa_id = e1.id
-        LEFT JOIN etapa e2 ON e2.id = e1.etapa_pai_id AND e2.removido_em IS NULL AND e2.endereco_obrigatorio = true
-        LEFT JOIN etapa e3 ON e3.id = e2.etapa_pai_id AND e3.removido_em IS NULL AND e3.endereco_obrigatorio = true
-        WHERE
-            ce1.etapa_id = e_id AND ce1.cronograma_id = c_id AND
-            (
-                NOT EXISTS ( SELECT 1 FROM geo_localizacao_referencia WHERE removido_em IS NULL AND etapa_id = e1.id ) OR
-                NOT EXISTS ( SELECT 1 FROM geo_localizacao_referencia WHERE removido_em IS NULL AND etapa_id = e2.id ) OR 
-                NOT EXISTS ( SELECT 1 FROM geo_localizacao_referencia WHERE removido_em IS NULL AND etapa_id = e3.id )
+    SELECT
+        CASE WHEN
+            EXISTS (
+                SELECT 1 FROM etapa WHERE etapa.id = e2.id AND etapa.endereco_obrigatorio = true AND NOT EXISTS (SELECT 1 FROM geo_localizacao_referencia WHERE removido_em IS NULL AND etapa_id = e2.id)
             )
-    ) THEN
-        RAISE EXCEPTION 'Endereço é obrigatório';
-    END IF;
+        THEN e2.titulo ELSE NULL END as e2_titulo,
+        CASE WHEN
+            EXISTS (
+                SELECT 1 FROM etapa WHERE etapa.id = e3.id AND etapa.endereco_obrigatorio = true AND NOT EXISTS (SELECT 1 FROM geo_localizacao_referencia WHERE removido_em IS NULL AND etapa_id = e3.id)
+            )
+        THEN e3.titulo ELSE NULL END as e3_titulo
+    INTO rec
+    FROM cronograma_etapa ce1 
+    JOIN etapa e1 ON ce1.etapa_id = e1.id
+    LEFT JOIN etapa e2 ON e2.id = e1.etapa_pai_id AND e2.removido_em IS NULL
+    LEFT JOIN etapa e3 ON e3.id = e2.etapa_pai_id AND e3.removido_em IS NULL
+    WHERE
+        ce1.etapa_id = e_id AND ce1.cronograma_id = c_id;
+
+    RETURN rec;
 END;
 $$ LANGUAGE plpgsql;
 
