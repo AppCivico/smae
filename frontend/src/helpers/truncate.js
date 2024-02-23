9837c6bf6258a8879c7d1855fdfee002f1c476b3
Giovana <dev08@linvix.com.br>
2024-02-22 23:25:12 -0300

    feat: aprimorando truncate

--- frontend/src/helpers/truncate.js
+++ frontend/src/helpers/truncate.js
@@ -1,4 +1,10 @@
-export default ((str, maxLen, suffix = '...', separator = ' ') => (!str || str.length <= maxLen
-  ? str
-  : str.substr(0, str.lastIndexOf(separator, maxLen)) + suffix)
-);
+export default ((str, maxLen, suffix = '...', separator = ' ') => {
+  switch (true) {
+    case (!str || str.length <= maxLen):
+      return str;
+    case str.lastIndexOf(separator, maxLen) === -1:
+      return str.substr(0, maxLen) + suffix;
+    default:
+      return str.substr(0, str.lastIndexOf(separator, maxLen)) + suffix;
+  }
+});
