2f1cfd0a870f30d80f206e38ed82221ae9812905
lucasansei <lucas.ansei@eokoe.com>
2024-02-23 15:30:36 -0300

    Adicionando CRUD de partido

---
+++ backend/src/partido/entities/partido.entity.ts
@@ -0,0 +1,42 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class PartidoOneDto {
+    @ApiProperty({ description: 'ID do Partido' })
+    id: number;
+    
+    @ApiProperty({ description: 'Sigla do partido' })
+    sigla: string;
+    
+    @ApiProperty({ description: 'Nome do partido' })
+    nome: string;
+
+    @ApiProperty({ description: 'Número do partido' })
+    numero: number;
+
+    @ApiProperty({ description: 'Observações sobre partido' })
+    observacao: string | null;
+
+    @ApiProperty({ description: 'Data de fundacao do partido' })
+    fundacao: Date | null;
+
+    @ApiProperty({ description: 'Data de encerramento do partido' })
+    encerramento: Date | null;
+}
+
+export class PartidoDto {
+    @ApiProperty({ description: 'ID do Partido' })
+    id: number;
+    
+    @ApiProperty({ description: 'Sigla do partido' })
+    sigla: string;
+    
+    @ApiProperty({ description: 'Nome do partido' })
+    nome: string;
+
+    @ApiProperty({ description: 'Número do partido' })
+    numero: number;
+}
+
+export class ListPartidoDto {
+    linhas: PartidoDto[]
+}
