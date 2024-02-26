e02a2bcb13c87dc9718341853bea6581b1952231
Giovana <dev08@linvix.com.br>
2024-02-26 07:50:30 -0300

    wip: adicionando novas telas de partidos

---
+++ frontend/src/views/partidos/PartidosCriarEditar.vue
@@ -0,0 +1,149 @@
+<script setup>
+import { partido as schema } from '@/consts/formSchemas';
+import { useAlertStore } from '@/stores/alert.store';
+import { usePartidosStore } from '@/stores/partidos.store';
+import { storeToRefs } from 'pinia';
+import { ErrorMessage, Field, Form } from 'vee-validate';
+import { useRoute, useRouter } from 'vue-router';
+
+const router = useRouter();
+const route = useRoute();
+const props = defineProps({
+  painelId: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const alertStore = useAlertStore();
+const partidosStore = usePartidosStore();
+const { chamadasPendentes, erro, itemParaEdição } = storeToRefs(partidosStore);
+
+async function onSubmit(values) {
+  values.numero = parseInt(values.numero);
+
+  try {
+    let r;
+    const msg = props.painelId
+      ? 'Dados salvos com sucesso!'
+      : 'Item adicionado com sucesso!';
+
+    if (props.painelId) {
+      r = await partidosStore.salvarItem(values, props.painelId);
+    } else {
+      r = await partidosStore.salvarItem(values);
+    }
+    if (r) {
+      alertStore.success(msg);
+      partidosStore.$reset();
+      router.push({ name: 'partidosListar' });
+    }
+  } catch (error) {
+    alertStore.error(error);
+  }
+}
+
+if (props.painelId) {
+  partidosStore.buscarItem(props.painelId);
+}
+
+</script>
+
+<template>
+  <div class="flex spacebetween center mb2">
+    <h1>{{ route?.meta?.título || 'Partido' }}</h1>
+    <hr class="ml2 f1">
+    <CheckClose />
+  </div>
+  <Form
+    v-slot="{ errors, isSubmitting, }"
+    :validation-schema="schema"
+    :initial-values="itemParaEdição"
+    @submit="onSubmit"
+  >
+    <div class="flex g2 mb1">
+      <div class="f1">
+        <LabelFromYup
+          name="nome"
+          :schema="schema"
+        />
+        <Field
+          name="nome"
+          type="text"
+          class="inputtext light mb1"
+        />
+        <ErrorMessage
+          class="error-msg mb1"
+          name="nome"
+        />
+      </div>
+    </div>
+
+    <div class="flex g2 mb1">
+      <div class="f1">
+        <LabelFromYup
+          name="sigla"
+          :schema="schema"
+        />
+        <Field
+          name="sigla"
+          type="text"
+          class="inputtext light mb1"
+        />
+        <ErrorMessage
+          class="error-msg mb1"
+          name="sigla"
+        />
+      </div>
+    </div>
+
+    <div class="flex g2 mb1">
+      <div class="f1">
+        <LabelFromYup
+          name="numero"
+          :schema="schema"
+        />
+        <Field
+          v-model.number="numero"
+          name="numero"
+          type="number"
+          class="inputtext light mb1"
+        />
+        <ErrorMessage
+          class="error-msg mb1"
+          name="numero"
+        />
+      </div>
+    </div>
+
+    <FormErrorsList :errors="errors" />
+
+    <div class="flex spacebetween center mb2">
+      <hr class="mr2 f1">
+      <button
+        class="btn big"
+        :disabled="isSubmitting || Object.keys(errors)?.length"
+        :title="Object.keys(errors)?.length
+          ? `Erros de preenchimento: ${Object.keys(errors)?.length}`
+          : null"
+      >
+        Salvar
+      </button>
+      <hr class="ml2 f1">
+    </div>
+  </Form>
+
+  <span
+    v-if="chamadasPendentes?.emFoco"
+    class="spinner"
+  >Carregando</span>
+
+  <div
+    v-if="erro"
+    class="error p1"
+  >
+    <div class="error-msg">
+      {{ erro }}
+    </div>
+  </div>
+</template>
