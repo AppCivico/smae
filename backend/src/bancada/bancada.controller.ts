3aa40fb69a611b090cc2988dbdc6765acbf48acb
lucasansei <lucas.ansei@eokoe.com>
2024-02-26 10:10:01 -0300

    Adicionando CRUD de bancadas

---
+++ backend/src/bancada/bancada.controller.ts
@@ -0,0 +1,62 @@
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post } from '@nestjs/common';
+import { ApiBearerAuth, ApiNoContentResponse, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
+import { CurrentUser } from '../auth/decorators/current-user.decorator';
+// import { Roles } from '../auth/decorators/roles.decorator';
+import { PessoaFromJwt } from '../auth/models/PessoaFromJwt';
+import { FindOneParams } from '../common/decorators/find-params';
+import { RecordWithId } from '../common/dto/record-with-id.dto';
+import { BancadaService } from './bancada.service';
+import { CreateBancadaDto } from './dto/create-bancada.dto';
+import { BancadaOneDto, ListBancadaDto } from './entities/bancada.entity';
+import { UpdateBancadaDto } from './dto/update-bancada.dto';
+
+@ApiTags('Bancada')
+@Controller('bancada')
+export class BancadaController {
+    constructor(private readonly bancadaService: BancadaService) {}
+
+    @Post()
+    @ApiBearerAuth('access-token')
+    @ApiUnauthorizedResponse()
+    // @Roles('CadastroOrgao.inserir')
+    async create(@Body() dto: CreateBancadaDto, @CurrentUser() user: PessoaFromJwt): Promise<RecordWithId> {
+        return await this.bancadaService.create(dto, user);
+    }
+
+    @ApiBearerAuth('access-token')
+    @Get()
+    async findAll(): Promise<ListBancadaDto> {
+        return { linhas: await this.bancadaService.findAll() };
+    }
+
+    @Get(':id')
+    @ApiBearerAuth('access-token')
+    @ApiUnauthorizedResponse()
+    // @Roles('Projeto.administrar_portfolios', 'Projeto.administrador_no_orgao', ...PROJETO_READONLY_ROLES)
+    async findOne(@Param() params: FindOneParams, @CurrentUser() user: PessoaFromJwt): Promise<BancadaOneDto> {
+        return await this.bancadaService.findOne(params.id, user);
+    }
+
+    @Patch(':id')
+    @ApiBearerAuth('access-token')
+    @ApiUnauthorizedResponse()
+    // @Roles('CadastroOrgao.editar')
+    async update(
+        @Param() params: FindOneParams,
+        @Body() dto: UpdateBancadaDto,
+        @CurrentUser() user: PessoaFromJwt
+    ): Promise<RecordWithId> {
+        return await this.bancadaService.update(+params.id, dto, user);
+    }
+
+    @Delete(':id')
+    @ApiBearerAuth('access-token')
+    @ApiUnauthorizedResponse()
+    // @Roles('CadastroOrgao.remover')
+    @ApiNoContentResponse()
+    @HttpCode(HttpStatus.ACCEPTED)
+    async remove(@Param() params: FindOneParams, @CurrentUser() user: PessoaFromJwt) {
+        await this.bancadaService.remove(+params.id, user);
+        return '';
+    }
+}
---
+++ backend/src/bancada/dto/create-bancada.dto.ts
@@ -0,0 +1,19 @@
+import { IsNumber, IsOptional, IsString, MaxLength } from "class-validator";
+
+export class CreateBancadaDto {
+    @IsNumber()
+    numero: number;
+
+    @IsString({ message: '$property| sigla: Precisa ser alfanumérico' })
+    @MaxLength(20, { message: '$property| sigla: Máximo 20 caracteres' })
+    sigla: string;
+    
+    @IsString({ message: '$property| nome: Precisa ser alfanumérico' })
+    @MaxLength(250, { message: '$property| nome: Máximo 250 caracteres' })
+    nome: string;
+
+    @IsOptional()
+    @IsString({ message: '$property| descricao: Precisa ser alfanumérico' })
+    @MaxLength(250, { message: '$property| descricao: Máximo 250 caracteres' })
+    descricao?: string;
+}
