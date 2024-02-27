5e292ecfcab2b538521d4e0c4b90063fcdacb515
lucasansei <lucas.ansei@eokoe.com>
2024-02-27 14:38:49 -0300

    Movendo biografia para parlamentar

--- backend/src/parlamentar/dto/create-parlamentar.dto.ts
+++ backend/src/parlamentar/dto/create-parlamentar.dto.ts
@@ -9,7 +9,7 @@ export class CreateParlamentarDto {
 
     @IsString({ message: '$property| Nome popular: Precisa ser alfanumérico' })
     @MaxLength(250, { message: '$property| Nome popular: Máximo 250 caracteres' })
-    nome_popular: string | null;
+    nome_popular?: string;
     
     /**
     * @example YYYY-MM-DD
@@ -17,25 +17,34 @@ export class CreateParlamentarDto {
     @IsOnlyDate()
     @Type(() => Date)
     @ValidateIf((object, value) => value !== null)
-    nascimento: Date | null;
+    nascimento?: Date;
 
     @IsOptional()
     @IsString()
     @MaxLength(10, { message: '$property| Nome popular: Máximo 250 caracteres' })
-    telefone: string | null;
+    telefone?: string;
 
     @IsOptional()
     @IsString()
     @MaxLength(250, { message: '$property| Nome popular: Máximo 250 caracteres' })
-    email: string | null;
+    email?: string;
     
     @IsOptional()
     @IsString()
     @MaxLength(250, { message: '$property| Nome popular: Máximo 250 caracteres' })
-    atuacao: string | null;
+    atuacao?: string;
+
+    @IsOptional()
+    @IsString()
+    @MaxLength(250, { message: '$property| Nome popular: Máximo 250 caracteres' })
+    biografia?: string | null;
 
     @IsOptional()
     @IsBoolean()
     em_atividade: boolean
+
+}
+
+export class CreateMandatoDto {
 
 }
