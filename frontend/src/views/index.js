11f03fbc061266fbe795ec308d54aa1d249724a3
sobral <sobral@oficiodesign.com>
2024-02-28 14:10:30 -0300

    Remover título duplicado na raiz da administração

--- frontend/src/views/index.js
+++ frontend/src/views/index.js
@@ -1,2 +1,2 @@
 export { default as Home } from './HomeScreen.vue';
-export { default as Administracao } from './Administracao.vue';
+export { default as Administracao } from './AdministracaoRaiz.vue';
