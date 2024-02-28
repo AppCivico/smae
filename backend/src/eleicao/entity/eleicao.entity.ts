2db6f449f002c9af0b97d12b9ce674880a9f6f47
lucasansei <lucas.ansei@eokoe.com>
2024-02-28 10:52:33 -0300

    Adicionando detalhamento de enum para eleição

--- backend/src/eleicao/entity/eleicao.entity.ts
+++ backend/src/eleicao/entity/eleicao.entity.ts
@@ -1,8 +1,12 @@
+import { ApiProperty } from "@nestjs/swagger"
 import { EleicaoTipo } from "@prisma/client"
+import { IsEnum } from "class-validator"
 
 export class EleicaoDto {
     id: number
     ano: number
+    @ApiProperty({ enum: EleicaoTipo, enumName: 'EleicaoTipo' })
+    @IsEnum(EleicaoTipo)
     tipo: EleicaoTipo
     atual_para_mandatos: boolean
 }
