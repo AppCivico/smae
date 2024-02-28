9a490101069aad40f6c454c25ffec8d1c4daf3e7
sobral <sobral@oficiodesign.com>
2024-02-28 14:02:40 -0300

    Mover todas as páginas adminstrativas para dentro de `/admistracao`

--- frontend/src/views/ods/ListItems.vue
+++ frontend/src/views/ods/ListItems.vue
@@ -1,5 +1,4 @@
 <script setup>
-import { Dashboard } from '@/components';
 import { useAuthStore, useODSStore } from '@/stores';
 import { storeToRefs } from 'pinia';
 import { reactive, ref } from 'vue';
@@ -23,85 +22,83 @@ function filterItems() {
 }
 </script>
 <template>
-  <Dashboard>
-    <div class="flex spacebetween center mb2">
-      <h1>Categorias</h1>
-      <hr class="ml2 f1">
-      <router-link
-        v-if="perm?.CadastroOds?.inserir"
-        to="/categorias/nova"
-        class="btn big ml2"
+  <div class="flex spacebetween center mb2">
+    <h1>Categorias</h1>
+    <hr class="ml2 f1">
+    <router-link
+      v-if="perm?.CadastroOds?.inserir"
+      to="/categorias/nova"
+      class="btn big ml2"
+    >
+      Nova categoria
+    </router-link>
+  </div>
+  <div class="flex center mb2">
+    <div class="f2 search">
+      <input
+        v-model="filters.textualSearch"
+        placeholder="Buscar"
+        type="text"
+        class="inputtext"
+        @input="filterItems"
       >
-        Nova categoria
-      </router-link>
     </div>
-    <div class="flex center mb2">
-      <div class="f2 search">
-        <input
-          v-model="filters.textualSearch"
-          placeholder="Buscar"
-          type="text"
-          class="inputtext"
-          @input="filterItems"
-        >
-      </div>
-    </div>
+  </div>
 
-    <table class="tablemain">
-      <thead>
-        <tr>
-          <th style="width: 10%">
-            Número
-          </th>
-          <th style="width: 35%">
-            Título
-          </th>
-          <th style="width: 45%">
-            Descrição
-          </th>
-          <th style="width: 10%" />
-        </tr>
-      </thead>
-      <tbody>
-        <template v-if="itemsFiltered.length">
-          <tr
-            v-for="item in itemsFiltered"
-            :key="item.id"
-          >
-            <td>{{ item.numero }}</td>
-            <td>{{ item.titulo }}</td>
-            <td>{{ item.descricao }}</td>
-            <td style="white-space: nowrap; text-align: right;">
-              <template v-if="perm?.CadastroOds?.editar">
-                <router-link
-                  :to="`/categorias/editar/${item.id}`"
-                  class="tprimary"
-                >
-                  <svg
-                    width="20"
-                    height="20"
-                  ><use xlink:href="#i_edit" /></svg>
-                </router-link>
-              </template>
-            </td>
-          </tr>
-        </template>
-        <tr v-else-if="itemsFiltered.loading">
-          <td colspan="54">
-            Carregando
+  <table class="tablemain">
+    <thead>
+      <tr>
+        <th style="width: 10%">
+          Número
+        </th>
+        <th style="width: 35%">
+          Título
+        </th>
+        <th style="width: 45%">
+          Descrição
+        </th>
+        <th style="width: 10%" />
+      </tr>
+    </thead>
+    <tbody>
+      <template v-if="itemsFiltered.length">
+        <tr
+          v-for="item in itemsFiltered"
+          :key="item.id"
+        >
+          <td>{{ item.numero }}</td>
+          <td>{{ item.titulo }}</td>
+          <td>{{ item.descricao }}</td>
+          <td style="white-space: nowrap; text-align: right;">
+            <template v-if="perm?.CadastroOds?.editar">
+              <router-link
+                :to="`/categorias/editar/${item.id}`"
+                class="tprimary"
+              >
+                <svg
+                  width="20"
+                  height="20"
+                ><use xlink:href="#i_edit" /></svg>
+              </router-link>
+            </template>
           </td>
         </tr>
-        <tr v-else-if="itemsFiltered.error">
-          <td colspan="54">
-            Error: {{ itemsFiltered.error }}
-          </td>
-        </tr>
-        <tr v-else>
-          <td colspan="54">
-            Nenhum resultado encontrado.
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </Dashboard>
+      </template>
+      <tr v-else-if="itemsFiltered.loading">
+        <td colspan="54">
+          Carregando
+        </td>
+      </tr>
+      <tr v-else-if="itemsFiltered.error">
+        <td colspan="54">
+          Error: {{ itemsFiltered.error }}
+        </td>
+      </tr>
+      <tr v-else>
+        <td colspan="54">
+          Nenhum resultado encontrado.
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </template>
--- frontend/src/views/paineis/ListGrupos.vue
+++ frontend/src/views/paineis/ListGrupos.vue
@@ -1,5 +1,4 @@
 <script setup>
-import { Dashboard } from '@/components';
 import { useAuthStore, usePaineisGruposStore } from '@/stores';
 import { storeToRefs } from 'pinia';
 import { ref } from 'vue';
@@ -26,164 +25,162 @@ function toggleAccordeon(t) {
 }
 </script>
 <template>
-  <Dashboard>
-    <div class="flex spacebetween center mb2">
-      <h1>Grupos de paineis</h1>
-      <hr class="ml2 f1">
-      <router-link
-        v-if="perm?.CadastroGrupoPaineis?.inserir"
-        to="/paineis-grupos/novo"
-        class="btn big ml2"
+  <div class="flex spacebetween center mb2">
+    <h1>Grupos de paineis</h1>
+    <hr class="ml2 f1">
+    <router-link
+      v-if="perm?.CadastroGrupoPaineis?.inserir"
+      to="/paineis-grupos/novo"
+      class="btn big ml2"
+    >
+      Novo Grupo
+    </router-link>
+  </div>
+  <div class="flex center mb2">
+    <div class="f2 search">
+      <input
+        v-model="filters.textualSearch"
+        placeholder="Buscar"
+        type="text"
+        class="inputtext"
+        @input="filterItems"
       >
-        Novo Grupo
-      </router-link>
     </div>
-    <div class="flex center mb2">
-      <div class="f2 search">
-        <input
-          v-model="filters.textualSearch"
-          placeholder="Buscar"
-          type="text"
-          class="inputtext"
-          @input="filterItems"
-        >
-      </div>
-    </div>
+  </div>
 
-    <table class="tablemain fix">
-      <thead>
-        <tr>
-          <th style="width: 55%">
-            Grupo
-          </th>
-          <th style="width: 15%">
-            Painéis
-          </th>
-          <th style="width: 15%">
-            Usuários
-          </th>
-          <th style="width: 15%">
-            Status
-          </th>
-          <th style="width: 50px" />
+  <table class="tablemain fix">
+    <thead>
+      <tr>
+        <th style="width: 55%">
+          Grupo
+        </th>
+        <th style="width: 15%">
+          Painéis
+        </th>
+        <th style="width: 15%">
+          Usuários
+        </th>
+        <th style="width: 15%">
+          Status
+        </th>
+        <th style="width: 50px" />
+      </tr>
+    </thead>
+    <template v-if="itemsFiltered.length">
+      <template
+        v-for="p in itemsFiltered"
+        :key="p.id"
+      >
+        <tr
+          class="tzaccordeon"
+          @click="toggleAccordeon"
+        >
+          <td>
+            <div class="flex">
+              <svg
+                class="arrow"
+                width="13"
+                height="8"
+              ><use xlink:href="#i_down" /></svg><span>{{ p.nome }}</span>
+            </div>
+          </td>
+          <td>{{ p.paineis.length }}</td>
+          <td>{{ p.pessoas.length }}</td>
+          <td>{{ p.ativo ? "Ativo" : "Inativo" }}</td>
+          <td style="white-space: nowrap; text-align: right">
+            <router-link
+              v-if="perm?.CadastroGrupoPaineis?.editar"
+              :to="`/paineis-grupos/${p.id}`"
+              class="tprimary"
+            >
+              <svg
+                width="20"
+                height="20"
+              ><use xlink:href="#i_edit" /></svg>
+            </router-link>
+          </td>
         </tr>
-      </thead>
-      <template v-if="itemsFiltered.length">
-        <template
-          v-for="p in itemsFiltered"
-          :key="p.id"
-        >
-          <tr
-            class="tzaccordeon"
-            @click="toggleAccordeon"
-          >
-            <td>
-              <div class="flex">
-                <svg
-                  class="arrow"
-                  width="13"
-                  height="8"
-                ><use xlink:href="#i_down" /></svg><span>{{ p.nome }}</span>
-              </div>
-            </td>
-            <td>{{ p.paineis.length }}</td>
-            <td>{{ p.pessoas.length }}</td>
-            <td>{{ p.ativo?"Ativo":"Inativo" }}</td>
-            <td style="white-space: nowrap; text-align: right">
-              <router-link
-                v-if="perm?.CadastroGrupoPaineis?.editar"
-                :to="`/paineis-grupos/${p.id}`"
-                class="tprimary"
-              >
-                <svg
-                  width="20"
-                  height="20"
-                ><use xlink:href="#i_edit" /></svg>
-              </router-link>
-            </td>
-          </tr>
-          <tz>
-            <td colspan="66">
-              <table
-                v-if="p.paineis.length"
-                class="tablemain fix mb2"
-              >
-                <thead>
-                  <tr>
-                    <th style="width: 65%">
-                      Painéis relacionados
-                    </th>
-                    <th style="width: 25%">
-                      Status
-                    </th>
-                    <th style="width: 10%" />
-                  </tr>
-                </thead>
-                <tr v-for="pp in p.paineis">
-                  <td>{{ pp.nome }}</td>
-                  <td>{{ pp.ativo?"Ativo":"Inativo" }}</td>
-                  <td style="white-space: nowrap; text-align: right">
-                    <router-link
-                      v-if="perm?.CadastroPainel?.editar"
-                      :to="`/paineis/${pp.id}`"
-                      class="tprimary"
-                    >
-                      <svg
-                        width="20"
-                        height="20"
-                      ><use xlink:href="#i_edit" /></svg>
-                    </router-link>
-                  </td>
+        <tz>
+          <td colspan="66">
+            <table
+              v-if="p.paineis.length"
+              class="tablemain fix mb2"
+            >
+              <thead>
+                <tr>
+                  <th style="width: 65%">
+                    Painéis relacionados
+                  </th>
+                  <th style="width: 25%">
+                    Status
+                  </th>
+                  <th style="width: 10%" />
                 </tr>
-              </table>
+              </thead>
+              <tr v-for="pp in p.paineis">
+                <td>{{ pp.nome }}</td>
+                <td>{{ pp.ativo ? "Ativo" : "Inativo" }}</td>
+                <td style="white-space: nowrap; text-align: right">
+                  <router-link
+                    v-if="perm?.CadastroPainel?.editar"
+                    :to="`/paineis/${pp.id}`"
+                    class="tprimary"
+                  >
+                    <svg
+                      width="20"
+                      height="20"
+                    ><use xlink:href="#i_edit" /></svg>
+                  </router-link>
+                </td>
+              </tr>
+            </table>
 
-              <table
-                v-if="p.paineis.length"
-                class="tablemain fix mb2"
-              >
-                <thead>
-                  <tr>
-                    <th style="width: 90%">
-                      Usuários relacionados
-                    </th>
-                    <th style="width: 10%" />
-                  </tr>
-                </thead>
-                <tr v-for="pp in p.pessoas">
-                  <td>{{ pp.nome_exibicao }}</td>
-                  <td style="white-space: nowrap; text-align: right">
-                    <router-link
-                      v-if="perm?.CadastroPessoa?.editar"
-                      :to="`/usuarios/editar/${pp.id}`"
-                      class="tprimary"
-                    >
-                      <svg
-                        width="20"
-                        height="20"
-                      ><use xlink:href="#i_edit" /></svg>
-                    </router-link>
-                  </td>
+            <table
+              v-if="p.paineis.length"
+              class="tablemain fix mb2"
+            >
+              <thead>
+                <tr>
+                  <th style="width: 90%">
+                    Usuários relacionados
+                  </th>
+                  <th style="width: 10%" />
                 </tr>
-              </table>
-            </td>
-          </tz>
-        </template>
+              </thead>
+              <tr v-for="pp in p.pessoas">
+                <td>{{ pp.nome_exibicao }}</td>
+                <td style="white-space: nowrap; text-align: right">
+                  <router-link
+                    v-if="perm?.CadastroPessoa?.editar"
+                    :to="`/usuarios/editar/${pp.id}`"
+                    class="tprimary"
+                  >
+                    <svg
+                      width="20"
+                      height="20"
+                    ><use xlink:href="#i_edit" /></svg>
+                  </router-link>
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tz>
       </template>
-      <tr v-else-if="itemsFiltered.loading">
-        <td colspan="54">
-          Carregando
-        </td>
-      </tr>
-      <tr v-else-if="itemsFiltered.error">
-        <td colspan="54">
-          Error: {{ itemsFiltered.error }}
-        </td>
-      </tr>
-      <tr v-else>
-        <td colspan="54">
-          Nenhum resultado encontrado.
-        </td>
-      </tr>
-    </table>
-  </Dashboard>
+    </template>
+    <tr v-else-if="itemsFiltered.loading">
+      <td colspan="54">
+        Carregando
+      </td>
+    </tr>
+    <tr v-else-if="itemsFiltered.error">
+      <td colspan="54">
+        Error: {{ itemsFiltered.error }}
+      </td>
+    </tr>
+    <tr v-else>
+      <td colspan="54">
+        Nenhum resultado encontrado.
+      </td>
+    </tr>
+  </table>
 </template>
