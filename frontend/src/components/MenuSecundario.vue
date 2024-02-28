f0333e7a922b9daa3ef7de64f52d4f43775a8d5b
sobral <sobral@oficiodesign.com>
2024-02-28 20:28:49 -0300

    feat: Renovar menus

--- frontend/src/components/MenuSecundario.vue
+++ frontend/src/components/MenuSecundario.vue
@@ -1,14 +1,18 @@
 <script setup>
 import { useAuthStore } from '@/stores/auth.store';
 import { storeToRefs } from 'pinia';
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
+import TransitionExpand from './TransitionExpand.vue';
 
 const authStore = useAuthStore();
 const { temPermissãoPara } = storeToRefs(authStore);
 
 const route = useRoute();
 const router = useRouter();
+
+const itensFechados = ref([]);
+const menuAberto = ref(true);
 
 const limparRotas = (listaDeRotas) => (Array.isArray(listaDeRotas)
   ? listaDeRotas
@@ -69,12 +73,37 @@ const rotasParaMigalhasDePão = computed(() => {
     ? limparRotas(listaDeRotas)
     : [];
 });
+
+function alternarItens(índice) {
+  const posiçãoDoÍndice = itensFechados.value.indexOf(índice);
+
+  if (posiçãoDoÍndice > -1) {
+    itensFechados.value.splice(posiçãoDoÍndice, 1);
+  } else {
+    itensFechados.value.push(índice);
+  }
+}
 </script>
 <template>
   <div
     v-if="rotasParaMenu.length || rotasParaMigalhasDePão.length"
-    id="submenu"
+    class="menu-secundário"
+    :class="{ aberto: menuAberto }"
   >
+    <button
+      type="button"
+      class="menu-secundário__botão-de-alternância tipinfo bottom"
+      @click="menuAberto = !menuAberto"
+    >
+      <svg
+        width="32"
+        height="32"
+      ><use xlink:href="#i_right-double" /></svg>
+      <div>
+        Expandir menu secundário
+      </div>
+    </button>
+
     <div
       v-if="rotasParaMigalhasDePão.length"
       class="breadcrumbmenu"
@@ -92,30 +121,157 @@ const rotasParaMigalhasDePão = computed(() => {
       </router-link>
     </div>
 
-    <div
-      v-for="item, i in rotasParaMenu"
+    <nav
+      v-for="(item, i) in rotasParaMenu"
       :key="i"
-      class="subpadding"
+      class="menu-secundário__grupos-de-rotas"
     >
-      <h2 v-if="item.títuloParaGrupoDeLinksNoMenu">
+      <h2
+        v-if="item.títuloParaGrupoDeLinksNoMenu"
+        class="menu-secundário__sub-título"
+      >
         {{ item.títuloParaGrupoDeLinksNoMenu }}
-      </h2>
 
-      <div class="links-container">
-        <router-link
-          v-for="rota, k in item.rotas"
-          :key="k"
-          :to="rota.href"
+        <button
+          type="button"
+          :value="i"
+          aria-hidden="true"
+          class="menu-secundário__botão-de-alternância-de-grupo"
+          :class="{
+            'menu-secundário__botão-de-alternância-de-grupo--aberto': itensFechados.includes(i)
+          }"
+          @click="alternarItens(i)"
         >
-          {{ rota.meta?.títuloParaMenu
-            || (typeof rota.meta?.título === 'function'
-              ? rota.meta.título()
-              : rota.meta?.título
-              || rota.name
-            )
-          }}
-        </router-link>
-      </div>
-    </div>
+          <svg
+            width="13"
+            height="8"
+          ><use xlink:href="#i_down" /></svg>
+        </button>
+      </h2>
+      <TransitionExpand>
+        <ul
+          v-show="!itensFechados.includes(i)"
+          class="menu-secundário__lista-de-links"
+        >
+          <li
+            v-for="rota, k in item.rotas"
+            :key="k"
+            class="menu-secundário__item-de-lista"
+          >
+            <router-link
+              :to="rota.href"
+              class="menu-secundário__link"
+            >
+              {{ rota.meta?.títuloParaMenu
+                || (typeof rota.meta?.título === 'function'
+                  ? rota.meta.título()
+                  : rota.meta?.título
+                )
+                || rota.name
+              }}
+            </router-link>
+          </li>
+        </ul>
+      </TransitionExpand>
+    </nav>
   </div>
 </template>
+<style lang="less" scoped>
+.menu-secundário {
+  position: fixed;
+  left: 70px;
+  top: 0;
+  bottom: 0;
+  /* 224 px */
+  max-width: 14rem;
+  font-weight: 700;
+  z-index: 80;
+  background: @branco;
+
+  .transition();
+  .bs(0 0 40px 20px fadeOut(black, 93%));
+
+  + #dashboard {
+    margin-left: 350px;
+  }
+}
+
+.menu-secundário__botão-de-alternância,
+.menu-secundário__botão-de-alternância-de-grupo {
+  border: 0;
+  padding: 0;
+}
+
+.menu-secundário__botão-de-alternância {
+  background-color: @branco;
+  position: absolute;
+  border-radius: 100%;
+  top: 1rem;
+  left: 0;
+  z-index: 1000;
+
+  .bs();
+  .transition();
+  transform: translateX(-50%);
+  left: 100%;
+
+  svg {
+    .transition();
+
+    .menu-secundário.aberto & {
+      transform: rotate(180deg);
+    }
+  }
+}
+
+.menu-secundário__botão-de-alternância-de-grupo {
+  background: none;
+}
+
+.menu-secundário__botão-de-alternância-de-grupo svg {
+  transform: rotate(-180deg);
+}
+
+.menu-secundário__botão-de-alternância-de-grupo--aberto svg {
+  transform: rotate(0);
+}
+
+.menu-secundário__grupos-de-rotas {
+  overflow: hidden;
+  width: 0;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  .transition();
+
+  .menu-secundário.aberto & {
+    width: 100%;
+  }
+
+  > * {
+    margin-left: 1rem;
+    margin-right: 1rem;
+
+    @media (min-width: @larguras-de-tela__estreita) {
+      margin-right: 1.5rem;
+      margin-left: 1.5rem;
+    }
+  }
+}
+
+.menu-secundário__sub-título {
+  color: @amarelo;
+  font-size: 1rem;
+  display: flex;
+  margin-top: 0;
+  margin-bottom: 1rem;
+  gap: 1rem;
+}
+
+.menu-secundário__link {
+  font-size: 1.142857rem;
+  display: block;
+  padding: 1rem 0;
+
+  border-bottom: 1px solid @c100;
+}
+</style>
