b08eee45cd61344093ef8791e7116bffc622c8f9
Renato Cron <renato.cron@gmail.com>
2024-02-28 17:27:23 -0300

    Refactor findAll de permissoes do portfolio

--- backend/src/pp/portfolio/portfolio.controller.ts
+++ backend/src/pp/portfolio/portfolio.controller.ts
@@ -5,12 +5,17 @@ import { Roles } from '../../auth/decorators/roles.decorator';
 import { PessoaFromJwt } from '../../auth/models/PessoaFromJwt';
 import { FindOneParams } from '../../common/decorators/find-params';
 import { RecordWithId } from '../../common/dto/record-with-id.dto';
+import { PROJETO_READONLY_ROLES } from '../projeto/projeto.controller';
 import { CreatePortfolioDto } from './dto/create-portfolio.dto';
 import { UpdatePortfolioDto } from './dto/update-portfolio.dto';
 import { ListPortfolioDto, PortfolioOneDto } from './entities/portfolio.entity';
 import { PortfolioService } from './portfolio.service';
-import { PROJETO_READONLY_ROLES } from '../projeto/projeto.controller';
+import { ListaDePrivilegios } from '../../common/ListaDePrivilegios';
 
+export const PORT_ROLES: ListaDePrivilegios[] = [
+    'Projeto.administrar_portfolios',
+    'Projeto.administrar_portfolios_no_orgao',
+];
 @ApiTags('Portfólio')
 @Controller('portfolio')
 export class PortfolioController {
@@ -19,7 +24,7 @@ export class PortfolioController {
     @Post()
     @ApiBearerAuth('access-token')
     @ApiUnauthorizedResponse()
-    @Roles('Projeto.administrar_portfolios')
+    @Roles(...PORT_ROLES)
     async create(
         @Body() createPortfolioDto: CreatePortfolioDto,
         @CurrentUser() user: PessoaFromJwt
@@ -30,22 +35,27 @@ export class PortfolioController {
     @Get()
     @ApiBearerAuth('access-token')
     @ApiUnauthorizedResponse()
-    @Roles(
-        'Projeto.administrar_portfolios',
-        'Projeto.administrador',
-        'Projeto.administrador_no_orgao',
-        ...PROJETO_READONLY_ROLES
-    )
+    @Roles(...PORT_ROLES)
     async findAll(@CurrentUser() user: PessoaFromJwt): Promise<ListPortfolioDto> {
         return {
-            linhas: await this.portfolioService.findAll(user),
+            linhas: await this.portfolioService.findAll(user, false),
+        };
+    }
+
+    @Get('para-projetos')
+    @ApiBearerAuth('access-token')
+    @ApiUnauthorizedResponse()
+    @Roles(...PORT_ROLES, 'Projeto.administrador', 'Projeto.administrador_no_orgao', ...PROJETO_READONLY_ROLES)
+    async findAllParaProjetos(@CurrentUser() user: PessoaFromJwt): Promise<ListPortfolioDto> {
+        return {
+            linhas: await this.portfolioService.findAll(user, true),
         };
     }
 
     @Get(':id')
     @ApiBearerAuth('access-token')
     @ApiUnauthorizedResponse()
-    @Roles('Projeto.administrar_portfolios', 'Projeto.administrador_no_orgao', ...PROJETO_READONLY_ROLES)
+    @Roles(...PORT_ROLES, 'Projeto.administrador', 'Projeto.administrador_no_orgao', ...PROJETO_READONLY_ROLES)
     async findOne(@Param() params: FindOneParams, @CurrentUser() user: PessoaFromJwt): Promise<PortfolioOneDto> {
         return await this.portfolioService.findOne(params.id, user);
     }
@@ -53,7 +63,7 @@ export class PortfolioController {
     @Patch(':id')
     @ApiBearerAuth('access-token')
     @ApiUnauthorizedResponse()
-    @Roles('Projeto.administrar_portfolios')
+    @Roles(...PORT_ROLES)
     async update(
         @Param() params: FindOneParams,
         @Body() updatePortfolioDto: UpdatePortfolioDto,
@@ -65,7 +75,7 @@ export class PortfolioController {
     @Delete(':id')
     @ApiBearerAuth('access-token')
     @ApiUnauthorizedResponse()
-    @Roles('Projeto.administrar_portfolios')
+    @Roles(...PORT_ROLES)
     @ApiNoContentResponse()
     @HttpCode(HttpStatus.ACCEPTED)
     async remove(@Param() params: FindOneParams, @CurrentUser() user: PessoaFromJwt) {
