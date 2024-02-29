79c0044a1c47c51c3666b70152073f5f4462c2c5
lucasansei <lucas.ansei@eokoe.com>
2024-02-29 13:54:15 -0300

    Endpoints de delete e update de mandato

--- backend/src/parlamentar/dto/update-parlamentar.dto.ts
+++ backend/src/parlamentar/dto/update-parlamentar.dto.ts
@@ -1,8 +1,10 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateAssessorDto, CreateMandatoRepresentatividadeDto, CreateParlamentarDto } from './create-parlamentar.dto';
+import { OmitType, PartialType } from '@nestjs/swagger';
+import { CreateAssessorDto, CreateMandatoDto, CreateMandatoRepresentatividadeDto, CreateParlamentarDto } from './create-parlamentar.dto';
 
 export class UpdateParlamentarDto extends PartialType(CreateParlamentarDto) {}
 
 export class UpdateAssessorDto extends PartialType(CreateAssessorDto) {}
+
+export class UpdateMandatoDto extends PartialType(OmitType(CreateMandatoDto, ['eleicao_id'])) {}
 
 export class UpdateRepresentatividadeDto extends PartialType(CreateMandatoRepresentatividadeDto) {}
