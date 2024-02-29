cb73a6e12b195c1fcd26e5cc15f0fc0c9fa61cb4
lucasansei <lucas.ansei@eokoe.com>
2024-02-29 13:37:58 -0300

    Adicionando CRUD de deps de mandato

--- backend/src/parlamentar/dto/update-parlamentar.dto.ts
+++ backend/src/parlamentar/dto/update-parlamentar.dto.ts
@@ -1,6 +1,8 @@
 import { PartialType } from '@nestjs/swagger';
-import { CreateAssessorDto, CreateParlamentarDto } from './create-parlamentar.dto';
+import { CreateAssessorDto, CreateMandatoRepresentatividadeDto, CreateParlamentarDto } from './create-parlamentar.dto';
 
 export class UpdateParlamentarDto extends PartialType(CreateParlamentarDto) {}
 
 export class UpdateAssessorDto extends PartialType(CreateAssessorDto) {}
+
+export class UpdateRepresentatividadeDto extends PartialType(CreateMandatoRepresentatividadeDto) {}
