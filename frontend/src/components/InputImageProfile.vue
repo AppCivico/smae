0112e34a765a8f593c31b93087a19056dc8b3b8e
Giovana <f_gi@live.com>
2024-02-29 21:30:01 -0300

    feat: finalizando componente de envio de imagem para editar e criar parlamentar

--- frontend/src/components/InputImageProfile.vue
+++ frontend/src/components/InputImageProfile.vue
@@ -1,28 +1,21 @@
-<script setup>
-
-// defineProps(['img']);
-const emit = defineEmits(['uploadImageFile']);
-// emit('uploadImageFile');
-
-</script>
-
 <template>
   <div class="input-image-profile">
     <label class="input-image-profile__container">
-      <img
-        src="@/assets/logo.svg"
-        class="input-image-profile__image"
-      >
+      <div class="input-image-profile__image-container">
+        <img
+          v-if="imgSrc"
+          :src="imgSrc"
+          class="input-image-profile__image"
+        >
+      </div>
       <input
         id="shapefile"
         type="file"
         accept=".jpg,.png,.jpeg"
-        :onchange="uploadImageFile"
         class="input-image-profile__input"
+        @change="uploadImageFile"
       >
-      <p
-        class="addlink input-image-profile__label"
-      >
+      <p class="addlink input-image-profile__label">
         <svg
           width="20"
           height="20"
@@ -31,30 +24,59 @@ const emit = defineEmits(['uploadImageFile']);
     </label>
   </div>
 </template>
-<style scoped lang="less">
 
+<script setup>
+import { defineProps, ref } from 'vue';
+
+const props = defineProps({
+  modelValue: {
+    type: String,
+    default: '',
+  },
+});
+
+const imgSrc = ref(props.modelValue);
+
+const emit = defineEmits(['update:modelValue']);
+
+const uploadImageFile = (event) => {
+  const [file] = event.target.files;
+  const fileUrl = URL.createObjectURL(file);
+  imgSrc.value = fileUrl;
+  emit('update:modelValue', file);
+};
+</script>
+
+<style scoped lang="less">
 .input-image-profile{
     display: flex;
 
     &__container {
-        display: flex;
-        flex-direction: column;
+      display: flex;
+      flex-direction: column;
     }
 
     &__input{
-        display:none;
+      display:none;
+    }
+
+    &__image-container{
+      width: 205px;
+      height: 205px;
+      background-color: #D9D9D9;
+      border-radius: 15%;
+      position: relative;
+      overflow: hidden;
     }
 
     &__image{
-        width: 205px;
-    height: 205px;
-    background-color: #D9D9D9;
-    border-radius: 15%;
+      position: absolute;
+      size: 100%;
     }
 
     &__label{
-        max-width: 140px;
-        margin: 15px auto;
+      max-width: 140px;
+      margin: 15px auto;
     }
 }
 </style>
