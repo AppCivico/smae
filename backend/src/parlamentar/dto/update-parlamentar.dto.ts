36fb0fe0fe427ffca234d6dfb0d65ff4d4ca1009
lucasansei <lucas.ansei@eokoe.com>
2024-03-04 13:55:12 -0300

    Ajustes em parlamentar

--- backend/src/parlamentar/dto/update-parlamentar.dto.ts
+++ backend/src/parlamentar/dto/update-parlamentar.dto.ts
@@ -1,10 +1,10 @@
 import { OmitType, PartialType } from '@nestjs/swagger';
-import { CreateAssessorDto, CreateMandatoDto, CreateMandatoRepresentatividadeDto, CreateParlamentarDto } from './create-parlamentar.dto';
+import { CreateEquipeDto, CreateMandatoDto, CreateMandatoRepresentatividadeDto, CreateParlamentarDto } from './create-parlamentar.dto';
 
 export class UpdateParlamentarDto extends PartialType(CreateParlamentarDto) {}
 
-export class UpdateAssessorDto extends PartialType(CreateAssessorDto) {}
+export class UpdateEquipeDto extends PartialType(CreateEquipeDto) {}
 
 export class UpdateMandatoDto extends PartialType(OmitType(CreateMandatoDto, ['eleicao_id'])) {}
 
-export class UpdateRepresentatividadeDto extends PartialType(CreateMandatoRepresentatividadeDto) {}
+export class UpdateRepresentatividadeDto extends PartialType(OmitType(CreateMandatoRepresentatividadeDto, ['mandato_id', 'municipio_tipo', 'nivel', 'regiao_id'])) {}
