dcea3ea199aa9a659c1f6a85d01b2f64a14d5542
Renato Cron <renato.cron@gmail.com>
2024-03-05 08:19:23 -0300

    Remove not used deps on backend-orcamento

--- utils/sync-sof.sh
+++ utils/sync-sof.sh
@@ -4,3 +4,4 @@ rsync -av $HOME/projetos/appcivico/middleware_orcamento_smae/* $HOME/projetos/ap
 rsync -av $HOME/projetos/appcivico/middleware_orcamento_smae/.gitignore $HOME/projetos/appcivico/smae/backend-orcamento/
 rsync -av $HOME/projetos/appcivico/middleware_orcamento_smae/.env.* $HOME/projetos/appcivico/smae/backend-orcamento/
 
+cp backend-orcamento-requirements.txt $HOME/projetos/appcivico/smae/backend-orcamento/
