79240dae04ba60d8f5e234b32e2663e0b78f23fc
Renato Cron <renato.cron@gmail.com>
2024-03-04 21:02:27 -0300

    Add cadastro de nível e novos campos do orgão

--- backend/src/orgao/dto/list-orgao.dto.ts
+++ backend/src/orgao/dto/list-orgao.dto.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Orgao } from '../entities/orgao.entity';
+import { OrgaoCompletoDto } from '../entities/orgao.entity';
 
 export class ListOrgaoDto {
     @ApiProperty({ description: 'Lista de Órgão' })
-    linhas: Orgao[];
+    linhas: OrgaoCompletoDto[];
 }
