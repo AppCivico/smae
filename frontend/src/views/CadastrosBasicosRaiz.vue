5c11e668706b0c643e4b4ec825b3f32111541525
sobral <sobral@oficiodesign.com>
2024-03-05 22:02:49 -0300

    Organizar cadastros básicos no menu

---
+++ frontend/src/views/CadastrosBasicosRaiz.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { Dashboard } from '@/components';
+</script>
+
+<template>
+  <Dashboard>
+    <MigalhasDeMetas class="mb1" />
+    <TítuloDePágina>
+      Cadastros básicos
+    </TítuloDePágina>
+  </Dashboard>
+</template>
