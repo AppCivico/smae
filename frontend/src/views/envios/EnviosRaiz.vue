1160784cdc72ddf7452e70b92465dbabc16991b3
Sobral <robson@oficiodesign.com>
2024-03-06 16:52:47 -0300

    Redirecionar tela de envio de arquivos

--- frontend/src/views/envios/EnviosRaiz.vue
+++ frontend/src/views/envios/EnviosRaiz.vue
@@ -1,5 +1,32 @@
 <script setup>
 import { Dashboard } from '@/components';
+import { useAuthStore } from '@/stores/auth.store';
+import { storeToRefs } from 'pinia';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const authStore = useAuthStore();
+const {
+  sistemaEscolhido,
+} = storeToRefs(authStore);
+
+switch (sistemaEscolhido.value) {
+  case 'PDM':
+    router.replace({
+      name: 'EnviosOrçamentosMetas',
+    });
+    break;
+
+  case 'Projetos':
+    router.replace({
+      name: 'EnviosOrçamentosProjetos',
+    });
+    break;
+
+  default:
+    break;
+}
 </script>
 <template>
   <Dashboard>
