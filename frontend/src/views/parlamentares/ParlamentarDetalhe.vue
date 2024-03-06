500ce3d42fec29bdb7cb6cfc841a80c1336ee9b5
Giovana <f_gi@live.com>
2024-03-06 16:08:13 -0300

    feat: (wip) adicionando listagem de assessores

--- frontend/src/views/parlamentares/ParlamentarDetalhe.vue
+++ frontend/src/views/parlamentares/ParlamentarDetalhe.vue
@@ -1,10 +1,13 @@
 <template>
+  -  {{ emFoco }} -
+  <!-- TODO: pegar <dt></dt> do  schema-->
   <h1>Parlamentar</h1>
   <div>
     <div class="flex g2 mb1 flexwrap">
       <img
+
         class="carometro__img"
-        :src="emFoco.foto"
+        src="/home/gio/Downloads/image.png"
       >
       <div>
         <dl
@@ -42,7 +45,9 @@
             {{ new Date(emFoco.nascimento).toLocaleDateString('pt-BR', { month: '2-digit', day: '2-digit' }) }}
           </dd>
         </dl>
-        <!-- <dl class="f1 mb1">
+
+        <!-- TODO: ver como acessar o emFoco.telefone -->
+        <!-- <dl class="f1 mb1" v-if="emFoco.telefone">
           <dt class="t12 uc w700 mb05 ">
             Telefone:
           </dt>
@@ -50,52 +55,66 @@
             {{ emFoco.telefone }}
           </dd>
         </dl> -->
+
+        <dl class="f1 mb1">
+          <dt class="t12 uc w700 mb05 ">
+            Atuação
+          </dt>
+          <dd
+            v-if="emFoco"
+            class="t13"
+          >
+            {{ emFoco.atuacao }}
+          </dd>
+        </dl>
       </div>
     </div>
 
     <div class="flex spacebetween center mb2">
-      <hr class="ml2 f1">
-      <h4>Assessores</h4>
+      <h1>Biografia</h1>
       <hr class="ml2 f1">
     </div>
-    <!-- iterar emFoco.assessores ou  equipe -->
+    <div v-if="emFoco">
+      <!-- {{ emFoco.biografia }} -->
+      TEXTOTEXTOTEXTOTEXTO TEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTO TEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTO TEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTO
+    </div>
     <div class="flex spacebetween center mb2">
-      <dl
-        class="f1 mb1"
-      >
-        <dt class="t12 uc w700 mb05 ">
-          Nome
-        </dt>
-        <dd
-          v-if="emFoco"
-          class="t13"
-        >
-          {{ emFoco.nome }}
-        </dd>
-      </dl>
-      <dl class="f1 mb1">
-        <dt class="t12 uc w700 mb05 ">
-          Telefone
-        </dt>
-        <dd
-          v-if="emFoco"
-          class="t13"
-        >
-          {{ emFoco.nome_popular }}
-        </dd>
-      </dl>
-      <dl class="f1 mb1">
-        <dt class="t12 uc w700 mb05 ">
-          E-mail
-        </dt>
-        <dd
-          v-if="emFoco"
-          class="t13"
-        >
-          {{ new Date(emFoco.nascimento).toLocaleDateString('pt-BR', { month: '2-digit', day: '2-digit' }) }}
-        </dd>
-      </dl>
+      <h1>Assessores</h1>
+      <hr class="ml2 f1">
     </div>
+
+    <table class="tablemain">
+      <colgroup>
+        <col>
+        <col>
+        <col>
+      </colgroup>
+      <thead>
+        <tr>
+          <th>Nome</th>
+          <th>Telefone</th>
+          <th>E-mail</th>
+        </tr>
+      </thead>
+      <tbody>
+        <template v-if="emFoco">
+          <tr
+            v-for="item in emFoco.equipe"
+            v-if="item.tipo === 'Assessor'"
+            :key="item.id"
+          >
+            <td>{{ item.nome }}</td>
+            <td>{{ item.telefone }}</td>
+            <td>{{ item.email }}</td>
+          </tr>
+        </template>
+        <tr v-else>
+          <td colspan="3">
+            Nenhum resultado encontrado.
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
