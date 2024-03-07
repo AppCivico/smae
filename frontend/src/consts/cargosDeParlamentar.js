d2b9497813862d327f6daa41e082c2914eb1b266
Sobral <robson@oficiodesign.com>
2024-03-07 20:59:00 -0300

    Listar cargos de parlamentar

---
+++ frontend/src/consts/cargosDeParlamentar.js
@@ -0,0 +1,17 @@
+export default {
+  DeputadoEstadual: {
+    valor: 'DeputadoEstadual',
+    nome: 'Deputado Estadual',
+    tipo: 'Estadual',
+  },
+  DeputadoFederal: {
+    valor: 'DeputadoFederal',
+    nome: 'Deputado Federal',
+    tipo: 'Estadual',
+  },
+  Vereador: {
+    valor: 'Vereador',
+    nome: 'Vereador',
+    tipo: 'Municipal',
+  },
+};
