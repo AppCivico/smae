4c86e63bde5dcd0c048dc09d49944d6a395a3f6e
Renato Cron <renato.cron@gmail.com>
2024-03-08 16:06:17 -0300

    Add v1 do transfer/copia de responsabilidades

--- backend/prisma/manual-copy/views.permissoes.sql
+++ backend/prisma/manual-copy/views.permissoes.sql
@@ -99,3 +99,19 @@ WHERE px.codigo in ('SMAE.espectador_de_painel_externo' )
 AND p.desativado = false
 and pf.orgao_id is not null
 group by 1, 2;
+
+CREATE OR REPLACE VIEW view_pessoa_coordenador_responsavel_cp AS
+SELECT
+    p.id as pessoa_id,
+    pf.orgao_id
+FROM
+    pessoa p
+    join pessoa_fisica pf on pf.id = p.pessoa_fisica_id
+    join pessoa_perfil pp on pp.pessoa_id = p.id
+    join perfil_acesso pa on pp.perfil_acesso_id = pa.id
+    join perfil_privilegio priv on priv.perfil_acesso_id = pa.id
+    join privilegio px on px.id = priv.privilegio_id
+WHERE px.codigo in ('PDM.coordenador_responsavel_cp' )
+AND p.desativado = false
+and pf.orgao_id is not null
+group by 1, 2;
