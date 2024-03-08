51c44b47d081c60fa8b02831af36ed1b89a7c26f
Sobral <robson@oficiodesign.com>
2024-03-08 17:56:03 -0300

    Adicionar suplência ao mandato

---
+++ frontend/src/consts/niveisDeSuplencia.js
@@ -0,0 +1,10 @@
+export default {
+  PrimeiroSuplente: {
+    valor: 'PrimeiroSuplente',
+    nome: 'Primeiro Suplente',
+  },
+  SegundoSuplente: {
+    valor: 'SegundoSuplente',
+    nome: 'Segundo Suplente',
+  },
+};
