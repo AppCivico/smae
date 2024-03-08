1fb3ea4663f86b395b14941a9d0ab0790c03e8eb
lucasansei <lucas.ansei@eokoe.com>
2024-03-08 13:36:50 -0300

    Não permitir edição de comparecimento

--- backend/src/parlamentar/dto/update-parlamentar.dto.ts
+++ backend/src/parlamentar/dto/update-parlamentar.dto.ts
@@ -1,10 +1,10 @@
 import { OmitType, PartialType } from '@nestjs/swagger';
 import { CreateEquipeDto, CreateMandatoDto, CreateMandatoRepresentatividadeDto, CreateParlamentarDto } from './create-parlamentar.dto';
 
-export class UpdateParlamentarDto extends PartialType(CreateParlamentarDto) {}
+export class UpdateParlamentarDto extends PartialType(CreateParlamentarDto) { }
 
-export class UpdateEquipeDto extends PartialType(CreateEquipeDto) {}
+export class UpdateEquipeDto extends PartialType(CreateEquipeDto) { }
 
-export class UpdateMandatoDto extends PartialType(OmitType(CreateMandatoDto, ['eleicao_id'])) {}
+export class UpdateMandatoDto extends PartialType(OmitType(CreateMandatoDto, ['eleicao_id'])) { }
 
-export class UpdateRepresentatividadeDto extends PartialType(OmitType(CreateMandatoRepresentatividadeDto, ['mandato_id', 'municipio_tipo', 'nivel', 'regiao_id'])) {}
+export class UpdateRepresentatividadeDto extends PartialType(OmitType(CreateMandatoRepresentatividadeDto, ['mandato_id', 'municipio_tipo', 'nivel', 'regiao_id', 'numero_comparecimento'])) { }
