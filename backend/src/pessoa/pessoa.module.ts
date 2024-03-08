ded5e0e3bb599a88e31fca563b33058374673d1e
Renato Cron <renato.cron@gmail.com>
2024-03-08 16:06:17 -0300

    Add v1 do transfer/copia de responsabilidades

--- backend/src/pessoa/pessoa.module.ts
+++ backend/src/pessoa/pessoa.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
 import { PessoaController } from './pessoa.controller';
 import { PessoaService } from './pessoa.service';
+import { PessoaResponsabilidadesMetaService } from './pessoa.responsabilidades.metas.service';
 
 @Module({
     imports: [PrismaModule],
     controllers: [PessoaController],
-    providers: [PessoaService],
+    providers: [PessoaService,PessoaResponsabilidadesMetaService],
     exports: [PessoaService],
 })
 export class PessoaModule {}
