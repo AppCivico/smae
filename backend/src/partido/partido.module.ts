cdfd8ed07cc67a1b3023fb457db6515587ca525c
lucasansei <lucas.ansei@eokoe.com>
2024-03-09 11:18:30 -0300

    adição de partido em bancada

--- backend/src/partido/partido.module.ts
+++ backend/src/partido/partido.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { PartidoController } from './partido.controller';
 import { PartidoService } from './partido.service';
-import { BancadaModule } from 'src/bancada/bancada.module';
 
 @Module({
-    imports: [PrismaModule, BancadaModule],
+    imports: [PrismaModule],
     controllers: [PartidoController],
     providers: [PartidoService],
     exports: [PartidoService],
 })
-export class PartidoModule { }
+export class PartidoModule {}
