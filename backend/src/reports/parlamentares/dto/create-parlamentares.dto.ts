a8e3675073517708a98a3f0f9e4e008725b99ef4
lucasansei <lucas.ansei@eokoe.com>
2024-03-11 15:27:31 -0300

    Add transform para param de create de report

--- backend/src/reports/parlamentares/dto/create-parlamentares.dto.ts
+++ backend/src/reports/parlamentares/dto/create-parlamentares.dto.ts
@@ -1,10 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { ParlamentarCargo } from '@prisma/client';
+import { Transform } from 'class-transformer';
 import { IsEnum, IsNumber, IsOptional } from 'class-validator';
+import { PositiveNumberTransform } from 'src/auth/transforms/number.transform';
 
 export class CreateRelParlamentaresDto {
     @IsOptional()
     @IsNumber()
+    @Transform(PositiveNumberTransform)
     partido_id?: number;
 
     @IsOptional()
@@ -14,5 +17,6 @@ export class CreateRelParlamentaresDto {
 
     @IsOptional()
     @IsNumber()
+    @Transform(PositiveNumberTransform)
     eleicao_id?: number;
 }
