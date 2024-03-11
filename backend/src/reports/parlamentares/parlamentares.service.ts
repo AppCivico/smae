30f950fd60ba7d84704ce61ecf5aac557e1fab44
lucasansei <lucas.ansei@eokoe.com>
2024-03-11 15:49:11 -0300

    adicionando logs

--- backend/src/reports/parlamentares/parlamentares.service.ts
+++ backend/src/reports/parlamentares/parlamentares.service.ts
@@ -20,6 +20,8 @@ export class ParlamentaresService implements ReportableService {
     ) {}
 
     async create(dto: CreateRelParlamentaresDto): Promise<ParlamentaresRelatorioDto> {
+        console.log(dto);
+        console.log('=================');
         if (dto.cargo === null) dto.cargo = undefined;
         if (dto.partido_id === null) dto.cargo = undefined;
         if (dto.eleicao_id === null) dto.eleicao_id = undefined;
