73eb96df390454152541ba5a47895e2560d8df6a
Sobral <robson@oficiodesign.com>
2024-03-13 10:57:53 -0300

    Filtrar objetos por múltiplos termos

--- frontend/src/helpers/filtrarObjetos.js
+++ frontend/src/helpers/filtrarObjetos.js
@@ -1,23 +1,25 @@
-function filtrar(item, termo = '') {
-  switch (true) {
-    case termo === '':
-      return true;
+function filtrar(item, termos = '') {
+  return termos.split(' ').every((termo) => {
+    switch (true) {
+      case termo === '':
+        return true;
 
-    case typeof item === 'number':
-      return String(item).indexOf(termo) > -1;
+      case typeof item === 'number':
+        return String(item).indexOf(termo) > -1;
 
-    case typeof item === 'string':
-      return item.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase().indexOf(termo.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase()) > -1;
+      case typeof item === 'string':
+        return item.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase().indexOf(termo.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase()) > -1;
 
-    case Array.isArray(item):
-      return item.some((y) => filtrar(y, termo));
+      case Array.isArray(item):
+        return item.some((y) => filtrar(y, termo));
 
-    case item !== null:
-      return Object.values(item).some((z) => filtrar(z, termo));
+      case item !== null:
+        return Object.values(item).some((z) => filtrar(z, termo));
 
-    default:
-      return false;
-  }
+      default:
+        return false;
+    }
+  });
 }
 
 export default ((lista, termo) => (!termo.trim()
