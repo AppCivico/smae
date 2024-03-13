6a6f88d8e3d84af1baf9846e479cafcf9b9855c4
Renato Cron <renato.cron@gmail.com>
2024-03-13 11:24:39 -0300

    Fix prisma/migrations/20240313005805_resp/migration.sql

--- backend/prisma/migrations/20240313005805_resp/migration.sql
+++ backend/prisma/migrations/20240313005805_resp/migration.sql
@@ -1,49 +1,55 @@
 /*
- Warnings:
+  Warnings:
 
- - A unique constraint covering the columns `[pessoa_id,atividade_id]` on the table `atividade_responsavel` will be added. If there are existing duplicate values, this will fail.
- - A unique constraint covering the columns `[pessoa_id,iniciativa_id]` on the table `iniciativa_responsavel` will be added. If there are existing duplicate values, this will fail.
- - A unique constraint covering the columns `[pessoa_id,meta_id]` on the table `meta_responsavel` will be added. If there are existing duplicate values, this will fail.
- - A unique constraint covering the columns `[pessoa_id,variavel_id]` on the table `variavel_responsavel` will be added. If there are existing duplicate values, this will fail.
- */
+  - A unique constraint covering the columns `[pessoa_id,atividade_id,coordenador_responsavel_cp]` on the table `atividade_responsavel` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[pessoa_id,iniciativa_id,coordenador_responsavel_cp]` on the table `iniciativa_responsavel` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[pessoa_id,meta_id,coordenador_responsavel_cp]` on the table `meta_responsavel` will be added. If there are existing duplicate values, this will fail.
+
+*/
+
 DELETE FROM "atividade_responsavel"
-WHERE (pessoa_id, atividade_id, id)
+WHERE (pessoa_id, atividade_id, coordenador_responsavel_cp, id)
     NOT IN (
         SELECT
             pessoa_id,
             atividade_id,
-            max(id) AS max_id
+            coordenador_responsavel_cp max(id) AS max_id
         FROM
             "atividade_responsavel"
         GROUP BY
-            pessoa_id,
-            atividade_id);
+            1,
+            2,
+            3);
 
 DELETE FROM "iniciativa_responsavel"
-WHERE (pessoa_id, iniciativa_id, id)
+WHERE (pessoa_id, iniciativa_id, coordenador_responsavel_cp, id)
     NOT IN (
         SELECT
             pessoa_id,
             iniciativa_id,
+            coordenador_responsavel_cp,
             max(id) AS max_id
         FROM
             "iniciativa_responsavel"
         GROUP BY
-            pessoa_id,
-            iniciativa_id);
+            1,
+            2,
+            3);
 
 DELETE FROM "meta_responsavel"
-WHERE (pessoa_id, meta_id, id)
+WHERE (pessoa_id, meta_id, coordenador_responsavel_cp, id)
     NOT IN (
         SELECT
             pessoa_id,
             meta_id,
+            coordenador_responsavel_cp,
             max(id) AS max_id
         FROM
             "meta_responsavel"
         GROUP BY
-            pessoa_id,
-            meta_id);
+            1,
+            2,
+            3);
 
 DELETE FROM "variavel_responsavel"
 WHERE (pessoa_id, variavel_id, id)
@@ -58,15 +64,12 @@ WHERE (pessoa_id, variavel_id, id)
             pessoa_id,
             variavel_id);
 
--- CreateIndex
-CREATE UNIQUE INDEX "atividade_responsavel_pessoa_id_atividade_id_key" ON "atividade_responsavel"("pessoa_id", "atividade_id");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "iniciativa_responsavel_pessoa_id_iniciativa_id_key" ON "iniciativa_responsavel"("pessoa_id", "iniciativa_id");
+CREATE UNIQUE INDEX "atividade_responsavel_pessoa_id_atividade_id_coordenador_re_key" ON "atividade_responsavel"("pessoa_id", "atividade_id", "coordenador_responsavel_cp");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "meta_responsavel_pessoa_id_meta_id_key" ON "meta_responsavel"("pessoa_id", "meta_id");
+CREATE UNIQUE INDEX "iniciativa_responsavel_pessoa_id_iniciativa_id_coordenador__key" ON "iniciativa_responsavel"("pessoa_id", "iniciativa_id", "coordenador_responsavel_cp");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "variavel_responsavel_pessoa_id_variavel_id_key" ON "variavel_responsavel"("pessoa_id", "variavel_id");
-
+CREATE UNIQUE INDEX "meta_responsavel_pessoa_id_meta_id_coordenador_responsavel__key" ON "meta_responsavel"("pessoa_id", "meta_id", "coordenador_responsavel_cp");
