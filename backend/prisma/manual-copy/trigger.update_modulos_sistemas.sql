efc3ef52c8638b6f7aaaab9a33a8a77b9062dc24
Renato Cron <renato.cron@gmail.com>
2024-03-13 07:32:55 -0300

    Fix triggers privilegio_trigger_update

--- backend/prisma/manual-copy/trigger.update_modulos_sistemas.sql
+++ backend/prisma/manual-copy/trigger.update_modulos_sistemas.sql
@@ -17,6 +17,32 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+CREATE OR REPLACE FUNCTION update_modulos_sistemas_priv_updated()
+RETURNS TRIGGER AS $$
+BEGIN
+    WITH updated_modulos AS (
+        SELECT
+            pp.perfil_acesso_id,
+            ARRAY_AGG(DISTINCT pm.modulo_sistema) AS new_modulos_sistemas_array
+        FROM Perfil_Privilegio pp
+        JOIN Privilegio p ON p.id = pp.privilegio_id
+        JOIN Privilegio_Modulo pm ON pm.id = p.modulo_id
+        WHERE pp.privilegio_id = NEW.id
+        GROUP BY
+            pp.perfil_acesso_id
+    )
+    UPDATE Perfil_Acesso pa
+    SET modulos_sistemas = um.new_modulos_sistemas_array
+    FROM updated_modulos um
+    WHERE pa.id = um.perfil_acesso_id
+    AND pa.modulos_sistemas IS DISTINCT FROM um.new_modulos_sistemas_array;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+
 CREATE TRIGGER perfil_privilegio_trigger
 AFTER INSERT OR UPDATE
 ON Perfil_Privilegio
@@ -30,19 +56,4 @@ FOR EACH ROW
 WHEN (
     (OLD.modulo_id IS DISTINCT FROM NEW.modulo_id)
 )
-EXECUTE FUNCTION update_modulos_sistemas();
-
-CREATE TRIGGER privilegio_modulo_trigger_update
-AFTER UPDATE
-ON privilegio_modulo
-FOR EACH ROW
-WHEN (
-    (OLD.modulo_sistema IS DISTINCT FROM NEW.modulo_sistema)
-)
-EXECUTE FUNCTION update_modulos_sistemas();
-
-CREATE TRIGGER privilegio_modulo_trigger_insert
-AFTER INSERT
-ON privilegio_modulo
-FOR EACH ROW
-EXECUTE FUNCTION update_modulos_sistemas();
+EXECUTE FUNCTION update_modulos_sistemas_priv_updated();
