a5f8bc1decb05e099a080345cdcb826e0231db21
Sobral <robson@oficiodesign.com>
2024-03-15 15:11:00 -0300

    fix: erros dos paineis externos vazando

--- frontend/src/views/gruposDePaineisExternos/GruposDePaineisExternosRaiz.vue
+++ frontend/src/views/gruposDePaineisExternos/GruposDePaineisExternosRaiz.vue
@@ -1,3 +1,9 @@
+<script setup>
+import { useGruposPaineisExternos } from '@/stores/grupospaineisExternos.store.ts';
+import { onUnmounted } from 'vue';
+
+onUnmounted(() => { useGruposPaineisExternos().$reset(); });
+</script>
 <template>
   <router-view />
 </template>
