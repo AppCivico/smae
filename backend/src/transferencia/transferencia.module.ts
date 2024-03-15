0cf8112e80376b8422fff1dc0d059f47729824d7
lucasansei <lucas.ansei@eokoe.com>
2024-03-15 11:29:34 -0300

    Adicionando module

--- backend/src/transferencia/transferencia.module.ts
+++ backend/src/transferencia/transferencia.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { TransferenciaService } from './transferencia.service';
 import { TransferenciaTipoController } from './transferencia-tipo.controller';
+import { TransferenciaController } from './transferencia.controller';
 
 @Module({
     imports: [PrismaModule],
-    controllers: [TransferenciaTipoController],
+    controllers: [TransferenciaController, TransferenciaTipoController],
     providers: [TransferenciaService],
     exports: [TransferenciaService],
 })
