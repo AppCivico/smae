311b7561811a6fe437928fbc329c52ebde06fa5c
lucasansei <lucas.ansei@eokoe.com>
2024-03-15 11:25:22 -0300

    Adicionando POST de transferencia

---
+++ backend/src/transferencia/dto/create-transferencia.dto.ts
@@ -0,0 +1,145 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { TransferenciaInterface, TransferenciaTipoEsfera } from '@prisma/client';
+import { Type } from 'class-transformer';
+import {
+    IsBoolean,
+    IsEnum,
+    IsInt,
+    IsNumber,
+    IsOptional,
+    IsString,
+    Max,
+    MaxLength,
+    Min,
+    MinLength,
+    ValidateIf,
+} from 'class-validator';
+import { IsOnlyDate } from 'src/common/decorators/IsDateOnly';
+
+export class CreateTransferenciaDto {
+    @IsNumber()
+    tipo_id: number;
+
+    @IsNumber()
+    orgao_concedente_id: number;
+
+    @IsNumber()
+    secretaria_concedente_id: number;
+
+    @IsOptional()
+    @IsNumber()
+    partido_id?: number;
+
+    @IsOptional()
+    @IsNumber()
+    parlamentar_id: number;
+
+    @IsString()
+    @MinLength(1)
+    @MaxLength(1024)
+    objeto: string;
+
+    @IsBoolean()
+    critico: boolean;
+
+    @ApiProperty({ enum: TransferenciaInterface, enumName: 'TransferenciaInterface' })
+    @IsEnum(TransferenciaInterface, {
+        message: '$property| Precisa ser um dos seguintes valores: ' + Object.values(TransferenciaInterface).join(', '),
+    })
+    interface: TransferenciaInterface;
+
+    @ApiProperty({ enum: TransferenciaTipoEsfera, enumName: 'TransferenciaTipoEsfera' })
+    @IsEnum(TransferenciaTipoEsfera, {
+        message:
+            '$property| Precisa ser um dos seguintes valores: ' + Object.values(TransferenciaTipoEsfera).join(', '),
+    })
+    esfera: TransferenciaTipoEsfera;
+
+    @IsString()
+    @MinLength(1)
+    @MaxLength(250)
+    identificador: string;
+
+    @IsBoolean()
+    clausula_suspensiva: boolean;
+
+    /**
+     * clausula_suspensiva_vencimento
+     * @example YYYY-MM-DD
+     */
+    @IsOptional()
+    @IsOnlyDate()
+    @Type(() => Date)
+    @ValidateIf((object, value) => value !== null)
+    clausula_suspensiva_vencimento?: Date | null;
+
+    /**
+     * ano: ano para pesquisa
+     * @example "2022"
+     */
+    @IsInt({ message: '$property| ano precisa ser positivo' })
+    @Min(1889)
+    @Max(2050)
+    @Type(() => Number)
+    ano: number;
+
+    @IsOptional()
+    @IsString()
+    @MinLength(1)
+    @MaxLength(250)
+    emenda?: string;
+
+    @IsOptional()
+    @IsString()
+    @MinLength(1)
+    @MaxLength(250)
+    demanda?: string;
+
+    @IsOptional()
+    @IsString()
+    @MinLength(1)
+    @MaxLength(250)
+    programa?: string;
+
+    @IsOptional()
+    @IsString()
+    @MinLength(1)
+    @MaxLength(250)
+    normativa?: string;
+
+    @IsOptional()
+    @IsString()
+    @MinLength(1)
+    @MaxLength(1024)
+    observacoes?: string;
+
+    @IsOptional()
+    @IsString()
+    @MinLength(1)
+    @MaxLength(250)
+    detalhamento?: string;
+
+    @IsOptional()
+    @IsString()
+    @MinLength(1)
+    @MaxLength(250)
+    nome_programa?: string;
+
+    @IsOptional()
+    @IsString()
+    @MinLength(1)
+    @MaxLength(250)
+    agencia_aceite?: string;
+
+    @IsOptional()
+    @IsString()
+    @MinLength(1)
+    @MaxLength(250)
+    emenda_unitaria?: string;
+
+    @IsOptional()
+    @IsString()
+    @MinLength(1)
+    @MaxLength(250)
+    numero_identificacao?: string;
+}
