cca14988572cdc53a7422cc56f707db4fa1ab238
Sobral <robson@oficiodesign.com>
2024-03-20 23:02:03 -0300

    Formatar código

--- frontend/src/views/tarefas/TarefasLista.vue
+++ frontend/src/views/tarefas/TarefasLista.vue
@@ -1,14 +1,14 @@
 <script setup>
-import LegendaEstimadoVsEfetivo from "@/components/LegendaEstimadoVsEfetivo.vue";
-import LinhaDeCronograma from "@/components/projetos/LinhaDeCronograma.vue";
-import { emailTransferencia as schema } from "@/consts/formSchemas";
-import { useTarefasStore } from "@/stores/tarefas.store.ts";
-import { storeToRefs } from "pinia";
-import { computed, ref } from "vue";
-import CabecalhoResumo from "@/components/tarefas/CabecalhoResumo.vue";
-import { useRoute } from "vue-router";
+import LegendaEstimadoVsEfetivo from '@/components/LegendaEstimadoVsEfetivo.vue';
+import LinhaDeCronograma from '@/components/projetos/LinhaDeCronograma.vue';
+import CabecalhoResumo from '@/components/tarefas/CabecalhoResumo.vue';
+import { emailTransferencia as schema } from '@/consts/formSchemas';
+import { useTarefasStore } from '@/stores/tarefas.store.ts';
+import { storeToRefs } from 'pinia';
+import { computed, ref } from 'vue';
+import { useRoute } from 'vue-router';
 
-import { Field, Form } from "vee-validate";
+import { Field, Form } from 'vee-validate';
 
 const route = useRoute();
 const tarefasStore = useTarefasStore();
@@ -16,42 +16,42 @@ const { árvoreDeTarefas, chamadasPendentes, erro } = storeToRefs(tarefasStore);
 
 const projetoEmFoco = computed(() => tarefasStore?.extra?.projeto);
 const apenasLeitura = computed(
-  () => !!projetoEmFoco.value?.permissoes?.apenas_leitura
+  () => !!projetoEmFoco.value?.permissoes?.apenas_leitura,
 );
 
 // eslint-disable-next-line max-len
 const nívelMáximoPermitido = computed(
-  () => tarefasStore?.extra?.portfolio?.nivel_maximo_tarefa || 0
+  () => tarefasStore?.extra?.portfolio?.nivel_maximo_tarefa || 0,
 );
 
 const nívelMáximoVisível = ref(0);
 
-const newEmail = ref("");
-let localEmails = ref([
-  "joao@hotmail.com",
-  "maria@gmail.com",
-  "carlos@yahoo.com",
+const newEmail = ref('');
+const localEmails = ref([
+  'joao@hotmail.com',
+  'maria@gmail.com',
+  'carlos@yahoo.com',
 ]);
 
-const periodicidades = ["Semanal", "Quinzenal", "Mensal"];
+const periodicidades = ['Semanal', 'Quinzenal', 'Mensal'];
 
 function removeEmail(index, event) {
-  console.log("entrou no removeEmail");
+  console.log('entrou no removeEmail');
   event.preventDefault();
   localEmails.value.splice(index, 1);
-  console.log("localEmails: ", localEmails.value);
+  console.log('localEmails: ', localEmails.value);
 }
 
 function addNewEmail() {
   const emails = newEmail.value
     .split(/[;, ]+/)
-    .filter((email) => email.trim() !== "");
+    .filter((email) => email.trim() !== '');
   emails.forEach((email) => {
-    if (email !== "" && !localEmails.value.includes(email)) {
+    if (email !== '' && !localEmails.value.includes(email)) {
       localEmails.value.push(email);
     }
   });
-  newEmail.value = "";
+  newEmail.value = '';
 }
 
 function onSubmit() {}
@@ -59,9 +59,9 @@ let prefixo = null;
 
 async function iniciar() {
   if (route.params?.projetoId) {
-    prefixo = "projeto";
+    prefixo = 'projeto';
   } else if (route.params?.transferenciaId) {
-    prefixo = "transferencia";
+    prefixo = 'transferencia';
   }
 
   tarefasStore.$reset();
@@ -115,9 +115,15 @@ export default {
   <!--  v-if="route.meta.entidadeMãe === 'transferencia'" -->
   <div class="mb4 disparo-email">
     <div class="">
-      <Form :validation-schema="schema" @submit="onSubmit">
+      <Form
+        :validation-schema="schema"
+        @submit="onSubmit"
+      >
         <div class="mb2">
-          <LabelFromYup name="disparo_email" :schema="schema" />
+          <LabelFromYup
+            name="disparo_email"
+            :schema="schema"
+          />
           <Field
             name="disparo_email"
             type="checkbox"
@@ -127,16 +133,18 @@ export default {
         </div>
         <div class="flex mb2 flexwrap g2">
           <div class="f1">
-            <LabelFromYup name="periodicidade" :schema="schema" />
+            <LabelFromYup
+              name="periodicidade"
+              :schema="schema"
+            />
             <Field
-              v-model.number="eleiçãoEscolhida"
               name="periodicidade"
               as="select"
               class="inputtext light mb1"
             >
               <option
                 v-for="periodicidade in periodicidades"
-                :key="id"
+                :key="periodicidade"
                 :value="periodicidade"
               >
                 {{ periodicidade }}
@@ -144,7 +152,10 @@ export default {
             </Field>
           </div>
           <div class="f1">
-            <LabelFromYup name="data" :schema="schema" />
+            <LabelFromYup
+              name="data"
+              :schema="schema"
+            />
             <Field
               name="data"
               type="date"
@@ -156,26 +167,38 @@ export default {
         </div>
         <div class="mb2">
           <div class="f1">
-            <LabelFromYup name="com_copia" :schema="schema" />
-            <Field
+            <LabelFromYup
               name="com_copia"
+              :schema="schema"
+            />
+            <Field
               v-model="newEmail"
+              name="com_copia"
               type="email"
               class="inputtext light mb1"
               maxlength="250"
               placeholder="email@dominio.com"
               @blur="addNewEmail()"
             />
-            <ul v-if="localEmails" class="flex flexwrap">
-              <li v-for="(email, index) in localEmails" :key="index">
+            <ul
+              v-if="localEmails"
+              class="flex flexwrap"
+            >
+              <li
+                v-for="(email, index) in localEmails"
+                :key="index"
+              >
                 <button
-                  @click="removeEmail(index, $event)"
                   type="button"
                   class="tagsmall"
                   tabindex="1"
+                  @click="removeEmail(index, $event)"
                 >
                   {{ email }}
-                  <svg width="12" height="12"><use xlink:href="#i_x" /></svg>
+                  <svg
+                    width="12"
+                    height="12"
+                  ><use xlink:href="#i_x" /></svg>
                 </button>
               </li>
             </ul>
@@ -189,7 +212,10 @@ export default {
           type="button"
           @click="salvarDisparo"
         >
-          <svg width="20" height="20">
+          <svg
+            width="20"
+            height="20"
+          >
             <use xlink:href="#i_+" />
           </svg>
           Salvar disparo de e-mail
@@ -210,7 +236,7 @@ export default {
           min="1"
           :max="nívelMáximoPermitido"
           class="f1"
-        />
+        >
         <output class="f1 ml1">
           {{ nívelMáximoVisível }}
         </output>
@@ -220,26 +246,29 @@ export default {
 
   <LegendaEstimadoVsEfetivo />
 
-  <table v-if="árvoreDeTarefas.length" class="tabela-de-etapas">
+  <table
+    v-if="árvoreDeTarefas.length"
+    class="tabela-de-etapas"
+  >
     <colgroup>
-      <col class="genealogia" />
-      <col />
-      <col />
-      <col />
+      <col class="genealogia">
+      <col>
+      <col>
+      <col>
 
-      <col class="col--data" />
-      <col class="col--data" />
-      <col class="col--data" />
-      <col class="col--data" />
+      <col class="col--data">
+      <col class="col--data">
+      <col class="col--data">
+      <col class="col--data">
 
-      <col />
-      <col />
-      <col />
+      <col>
+      <col>
+      <col>
 
       <template v-if="!apenasLeitura">
-        <col class="col--botão-de-ação" />
-        <col class="col--botão-de-ação" />
-        <col class="col--botão-de-ação" />
+        <col class="col--botão-de-ação">
+        <col class="col--botão-de-ação">
+        <col class="col--botão-de-ação">
       </template>
     </colgroup>
 
@@ -249,9 +278,21 @@ export default {
         <th />
         <th />
         <th />
-        <th colspan="2" class="dado-estimado">Planejado</th>
-        <th colspan="2" class="dado-efetivo">Execução</th>
-        <th colspan="2">Custo <small>(R$)</small></th>
+        <th
+          colspan="2"
+          class="dado-estimado"
+        >
+          Planejado
+        </th>
+        <th
+          colspan="2"
+          class="dado-efetivo"
+        >
+          Execução
+        </th>
+        <th colspan="2">
+          Custo <small>(R$)</small>
+        </th>
         <th />
         <template v-if="!apenasLeitura">
           <th />
@@ -262,15 +303,33 @@ export default {
       <tr class="pl3 center mb05 tc300 w700 t12 uc">
         <th />
         <th />
-        <th class="cell--number">% conclusão</th>
-        <th class="cell--number">Duração</th>
-        <th class="cell--data">Início</th>
-        <th class="cell--data">Término</th>
-        <th class="cell--data">Início</th>
-        <th class="cell--data">Término</th>
-        <th class="cell--number dado-estimado">Planejado</th>
-        <th class="cell--number dado-efetivo">Real</th>
-        <th class="cell--number">Atraso</th>
+        <th class="cell--number">
+          % conclusão
+        </th>
+        <th class="cell--number">
+          Duração
+        </th>
+        <th class="cell--data">
+          Início
+        </th>
+        <th class="cell--data">
+          Término
+        </th>
+        <th class="cell--data">
+          Início
+        </th>
+        <th class="cell--data">
+          Término
+        </th>
+        <th class="cell--number dado-estimado">
+          Planejado
+        </th>
+        <th class="cell--number dado-efetivo">
+          Real
+        </th>
+        <th class="cell--number">
+          Atraso
+        </th>
         <template v-if="!apenasLeitura">
           <th />
           <th />
@@ -293,9 +352,15 @@ export default {
     </tbody>
   </table>
 
-  <span v-if="chamadasPendentes?.lista" class="spinner">Carregando</span>
+  <span
+    v-if="chamadasPendentes?.lista"
+    class="spinner"
+  >Carregando</span>
 
-  <div v-if="erro" class="error p1">
+  <div
+    v-if="erro"
+    class="error p1"
+  >
     <div class="error-msg">
       {{ erro }}
     </div>
