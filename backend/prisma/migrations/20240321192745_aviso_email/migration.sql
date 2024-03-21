eda40d5ed922860b241f28b15914fa292bbdd53a
Renato Cron <renato.cron@gmail.com>
2024-03-21 16:27:52 -0300

    Add migration aviso email

---
+++ backend/prisma/migrations/20240321192745_aviso_email/migration.sql
@@ -0,0 +1,51 @@
+-- CreateEnum
+CREATE TYPE "TipoAviso" AS ENUM ('CronogramaTerminoPlanejado');
+
+-- CreateEnum
+CREATE TYPE "AvisoPeriodo" AS ENUM ('Dias', 'Semanas', 'Meses', 'Anos');
+
+-- CreateTable
+CREATE TABLE "aviso_email" (
+    "id" SERIAL NOT NULL,
+    "tipo" "TipoAviso" NOT NULL,
+    "tarefa_cronograma_id" INTEGER,
+    "tarefa_id" INTEGER,
+    "com_copia" TEXT[],
+    "numero" INTEGER NOT NULL,
+    "numero_periodo" "AvisoPeriodo" NOT NULL,
+    "recorrencia_dias" INTEGER NOT NULL DEFAULT 0,
+    "ativo" BOOLEAN NOT NULL,
+    "criado_por" INTEGER,
+    "criado_em" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "atualizado_por" INTEGER,
+    "atualizado_em" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "removido_por" INTEGER,
+    "removido_em" TIMESTAMPTZ(6),
+
+    CONSTRAINT "aviso_email_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "aviso_email_disparos" (
+    "id" SERIAL NOT NULL,
+    "para" TEXT NOT NULL,
+    "com_copia" TEXT[],
+    "criado_por" INTEGER,
+    "criado_em" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "aviso_email_id" INTEGER NOT NULL,
+    "emaildb_queue_id" UUID NOT NULL,
+
+    CONSTRAINT "aviso_email_disparos_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "aviso_email" ADD CONSTRAINT "aviso_email_tarefa_cronograma_id_fkey" FOREIGN KEY ("tarefa_cronograma_id") REFERENCES "tarefa_cronograma"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "aviso_email" ADD CONSTRAINT "aviso_email_tarefa_id_fkey" FOREIGN KEY ("tarefa_id") REFERENCES "tarefa"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "aviso_email_disparos" ADD CONSTRAINT "aviso_email_disparos_id_fkey" FOREIGN KEY ("id") REFERENCES "aviso_email"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "aviso_email_disparos" ADD CONSTRAINT "aviso_email_disparos_emaildb_queue_id_fkey" FOREIGN KEY ("emaildb_queue_id") REFERENCES "emaildb_queue"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
