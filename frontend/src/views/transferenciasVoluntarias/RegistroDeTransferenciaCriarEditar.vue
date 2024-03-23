5d670ecb616551db06dd130a116bd196c236e297
Sobral <robson@oficiodesign.com>
2024-03-22 23:12:39 -0300

    Formatar código

--- frontend/src/views/transferenciasVoluntarias/RegistroDeTransferenciaCriarEditar.vue
+++ frontend/src/views/transferenciasVoluntarias/RegistroDeTransferenciaCriarEditar.vue
@@ -2,17 +2,14 @@
 import { registroDeTransferencia as schema } from '@/consts/formSchemas';
 import { useAlertStore } from '@/stores/alert.store';
 import { useTransferenciasVoluntariasStore } from '@/stores/transferenciasVoluntarias.store';
-
 import { storeToRefs } from 'pinia';
-import { ErrorMessage, Field, Form} from 'vee-validate';
-import { useRoute, useRouter } from 'vue-router';
+import { ErrorMessage, Field, Form } from 'vee-validate';
+import { useRouter } from 'vue-router';
 
 const transferenciasVoluntarias = useTransferenciasVoluntariasStore();
-const { chamadasPendentes, erro, lista} = storeToRefs(transferenciasVoluntarias);
-
+const { chamadasPendentes, erro } = storeToRefs(transferenciasVoluntarias);
 
 const router = useRouter();
-const route = useRoute();
 const props = defineProps({
   transferenciaId: {
     type: Number,
@@ -57,21 +54,42 @@ if (props.transferenciaId) {
   </div>
 
   <div class="flex spacebetween center mb1">
-    <h3 class="title">Recurso Financeiro</h3>
+    <h3 class="title">
+      Recurso Financeiro
+    </h3>
     <hr class="ml2 f1">
   </div>
 
-  <Form v-slot="{ errors, isSubmitting }" :validation-schema="schema" :initial-values="itemParaEdição"
-    @submit="onSubmit">
+  <Form
+    v-slot="{ errors, isSubmitting }"
+    :validation-schema="schema"
+    :initial-values="itemParaEdição"
+    @submit="onSubmit"
+  >
     <div class="flex g2 mb1">
       <div class="f1">
-        <LabelFromYup name="valor" :schema="schema" />
-        <Field name="valor" type="text" class="inputtext light mb1" />
-        <ErrorMessage class="error-msg mb1" name="valor" />
+        <LabelFromYup
+          name="valor"
+          :schema="schema"
+        />
+        <Field
+          name="valor"
+          type="text"
+          class="inputtext light mb1"
+        />
+        <ErrorMessage
+          class="error-msg mb1"
+          name="valor"
+        />
       </div>
       <div class="f1">
         <label class="label">Empenho <span class="tvermelho">*</span></label>
-        <Field name="empenho" as="select" class="inputtext light mb1" :class="{ 'error': errors.empenho }">
+        <Field
+          name="empenho"
+          as="select"
+          class="inputtext light mb1"
+          :class="{ 'error': errors.empenho }"
+        >
           <option value="">
             Selecionar
           </option>
@@ -89,79 +107,196 @@ if (props.transferenciaId) {
     </div>
     <div class="flex g2 mb1">
       <div class="f1">
-        <LabelFromYup name="valor_contrapartida" :schema="schema" />
-        <Field name="valor_contrapartida" type="text" class="inputtext light mb1" placeholder="R$ 000.000.000.000,00"/>
-        <ErrorMessage class="error-msg mb1" name="valor_contrapartida" />
+        <LabelFromYup
+          name="valor_contrapartida"
+          :schema="schema"
+        />
+        <Field
+          name="valor_contrapartida"
+          type="text"
+          class="inputtext light mb1"
+          placeholder="R$ 000.000.000.000,00"
+        />
+        <ErrorMessage
+          class="error-msg mb1"
+          name="valor_contrapartida"
+        />
       </div>
       <div class="f1">
-        <LabelFromYup name="dotacao" :schema="schema" />
-        <Field name="dotacao" type="text" class="inputtext light mb1" placeholder="16.24.12.306.3016.2.873.33903900.00"/>
-        <ErrorMessage class="error-msg mb1" name="dotacao" />
+        <LabelFromYup
+          name="dotacao"
+          :schema="schema"
+        />
+        <Field
+          name="dotacao"
+          type="text"
+          class="inputtext light mb1"
+          placeholder="16.24.12.306.3016.2.873.33903900.00"
+        />
+        <ErrorMessage
+          class="error-msg mb1"
+          name="dotacao"
+        />
       </div>
     </div>
     <div class="flex g2 mb1">
       <div class="f1">
-        <LabelFromYup name="valor_total" :schema="schema" />
-        <Field name="valor_total" type="text" class="inputtext light mb1" placeholder="R$ 000.000.000.000,00"/>
-        <ErrorMessage class="error-msg mb1" name="valor_total" />
+        <LabelFromYup
+          name="valor_total"
+          :schema="schema"
+        />
+        <Field
+          name="valor_total"
+          type="text"
+          class="inputtext light mb1"
+          placeholder="R$ 000.000.000.000,00"
+        />
+        <ErrorMessage
+          class="error-msg mb1"
+          name="valor_total"
+        />
       </div>
       <div class="f1">
-        <LabelFromYup name="ordenador_despesa" :schema="schema" />
-        <Field name="ordenador_despesa" type="text" class="inputtext light mb1" />
-        <ErrorMessage class="error-msg mb1" name="ordenador_despesa" />
+        <LabelFromYup
+          name="ordenador_despesa"
+          :schema="schema"
+        />
+        <Field
+          name="ordenador_despesa"
+          type="text"
+          class="inputtext light mb1"
+        />
+        <ErrorMessage
+          class="error-msg mb1"
+          name="ordenador_despesa"
+        />
       </div>
     </div>
 
     <div class="flex spacebetween center mb1">
-      <h3 class="title">Dados Bancários de Aceite</h3>
+      <h3 class="title">
+        Dados Bancários de Aceite
+      </h3>
       <hr class="ml2 f1">
     </div>
 
     <div class="flex g2 mb1">
       <div class="f1">
-        <LabelFromYup name="banco_aceite" :schema="schema" />
-        <Field name="banco_aceite" type="text" class="inputtext light mb1" />
-        <ErrorMessage class="error-msg mb1" name="banco_aceite" />
+        <LabelFromYup
+          name="banco_aceite"
+          :schema="schema"
+        />
+        <Field
+          name="banco_aceite"
+          type="text"
+          class="inputtext light mb1"
+        />
+        <ErrorMessage
+          class="error-msg mb1"
+          name="banco_aceite"
+        />
       </div>
       <div class="f1">
-        <LabelFromYup name="agencia_aceite" :schema="schema" />
-        <Field name="agencia_aceite" type="text" class="inputtext light mb1" />
-        <ErrorMessage class="error-msg mb1" name="agencia_aceite" />
+        <LabelFromYup
+          name="agencia_aceite"
+          :schema="schema"
+        />
+        <Field
+          name="agencia_aceite"
+          type="text"
+          class="inputtext light mb1"
+        />
+        <ErrorMessage
+          class="error-msg mb1"
+          name="agencia_aceite"
+        />
       </div>
     </div>
     <div class="f1 mb2">
-      <LabelFromYup name="conta_aceite" :schema="schema" />
-      <Field name="conta_aceite" type="text" class="inputtext light mb1" />
-      <ErrorMessage class="error-msg mb1" name="conta_aceite" />
+      <LabelFromYup
+        name="conta_aceite"
+        :schema="schema"
+      />
+      <Field
+        name="conta_aceite"
+        type="text"
+        class="inputtext light mb1"
+      />
+      <ErrorMessage
+        class="error-msg mb1"
+        name="conta_aceite"
+      />
     </div>
 
     <div class="flex spacebetween center mb1">
-      <h3 class="title">Dados Bancários Secretaria Fim</h3>
+      <h3 class="title">
+        Dados Bancários Secretaria Fim
+      </h3>
       <hr class="ml2 f1">
     </div>
 
     <div class="flex g2 mb1">
       <div class="f1">
-        <LabelFromYup name="banco_fim" :schema="schema" />
-        <Field name="banco_fim" type="text" class="inputtext light mb1" />
-        <ErrorMessage class="error-msg mb1" name="banco_fim" />
+        <LabelFromYup
+          name="banco_fim"
+          :schema="schema"
+        />
+        <Field
+          name="banco_fim"
+          type="text"
+          class="inputtext light mb1"
+        />
+        <ErrorMessage
+          class="error-msg mb1"
+          name="banco_fim"
+        />
       </div>
       <div class="f1">
-        <LabelFromYup name="agencia_fim" :schema="schema" />
-        <Field name="agencia_fim" type="text" class="inputtext light mb1" />
-        <ErrorMessage class="error-msg mb1" name="agencia_fim" />
+        <LabelFromYup
+          name="agencia_fim"
+          :schema="schema"
+        />
+        <Field
+          name="agencia_fim"
+          type="text"
+          class="inputtext light mb1"
+        />
+        <ErrorMessage
+          class="error-msg mb1"
+          name="agencia_fim"
+        />
       </div>
     </div>
-    <div  class="g2 mb1">
+    <div class="g2 mb1">
       <div class="f1 mb2">
-        <LabelFromYup name="conta_fim" :schema="schema" />
-        <Field name="conta_fim" type="text" class="inputtext light mb1" />
-        <ErrorMessage class="error-msg mb1" name="conta_fim" />
+        <LabelFromYup
+          name="conta_fim"
+          :schema="schema"
+        />
+        <Field
+          name="conta_fim"
+          type="text"
+          class="inputtext light mb1"
+        />
+        <ErrorMessage
+          class="error-msg mb1"
+          name="conta_fim"
+        />
       </div>
       <div class="f1 mb3">
-        <LabelFromYup name="gestor_contrato" :schema="schema" />
-        <Field name="gestor_contrato" type="text" class="inputtext light mb1" />
-        <ErrorMessage class="error-msg mb1" name="gestor_contrato" />
+        <LabelFromYup
+          name="gestor_contrato"
+          :schema="schema"
+        />
+        <Field
+          name="gestor_contrato"
+          type="text"
+          class="inputtext light mb1"
+        />
+        <ErrorMessage
+          class="error-msg mb1"
+          name="gestor_contrato"
+        />
       </div>
     </div>
 
@@ -169,21 +304,30 @@ if (props.transferenciaId) {
 
     <div class="flex spacebetween center mb2">
       <hr class="mr2 f1">
-      <button class="btn big" :disabled="isSubmitting || Object.keys(errors)?.length" :title="Object.keys(errors)?.length
-      ? `Erros de preenchimento: ${Object.keys(errors)?.length}`
-      : null">
+      <button
+        class="btn big"
+        :disabled="isSubmitting || Object.keys(errors)?.length"
+        :title="Object.keys(errors)?.length
+          ? `Erros de preenchimento: ${Object.keys(errors)?.length}`
+          : null"
+      >
         Salvar
       </button>
       <hr class="ml2 f1">
     </div>
   </Form>
 
-  <span v-if="chamadasPendentes?.emFoco" class="spinner">Carregando</span>
+  <span
+    v-if="chamadasPendentes?.emFoco"
+    class="spinner"
+  >Carregando</span>
 
-  <div v-if="erro" class="error p1">
+  <div
+    v-if="erro"
+    class="error p1"
+  >
     <div class="error-msg">
       {{ erro }}
     </div>
   </div>
 </template>
-
