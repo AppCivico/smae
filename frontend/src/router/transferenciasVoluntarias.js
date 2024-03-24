fa567f7c84f7b7a969a937c4d090ce5b624ed0d3
Giovana <f_gi@live.com>
2024-03-23 22:50:02 -0300

    feat: organizando rotas das tarefas das transferencias

--- frontend/src/router/transferenciasVoluntarias.js
+++ frontend/src/router/transferenciasVoluntarias.js
@@ -6,8 +6,15 @@ import TransferenciasVoluntariasRaiz from '@/views/transferenciasVoluntarias/Tra
 import TransferenciasVoluntariasDetalhes from '@/views/transferenciasVoluntarias/TransferenciasVoluntariasDetalhes.vue';
 import TransferenciasVoluntariasDocumentos from '@/views/transferenciasVoluntarias/TransferenciasVoluntariasDocumentos.vue';
 import TransferenciasVoluntariasEnviarArquivo from '@/views/transferenciasVoluntarias/TransferenciasVoluntariasEnviarArquivo.vue'
-import transferenciasVoluntariasTarefas from './transferenciasVoluntarias.tarefas';
+import TarefasLista from '@/views/tarefas/TarefasLista.vue';
 import DialogWrapper from '@/views/DialogWrapper.vue'
+
+import { useTarefasStore } from '@/stores/tarefas.store.ts';
+import TarefasCriarEditar from '@/views/tarefas/TarefasCriarEditar.vue';
+import TarefasItem from '@/views/tarefas/TarefasItem.vue';
+import TarefasProgresso from '@/views/tarefas/TarefasProgresso.vue';
+import TarefasRaiz from '@/views/tarefas/TarefasRaiz.vue';
+import EmailModal from '@/components/EmailModal.vue'
 
 const rotasParaMenuSecundário = [
   {
@@ -22,6 +29,7 @@ const rotasParaMenuSecundário = [
       'RegistroDeTransferenciaEditar',
       'TransferenciaDistribuicaoDeRecursosEditar',
       'TransferenciasVoluntariasDocumentos',
+      'TransferenciaCronograma',
     ],
   },
 ];
@@ -155,6 +163,114 @@ export default {
         },
       ],
     },
-    transferenciasVoluntariasTarefas,
+    {
+      path: ':transferenciaId/tarefas',
+      component: TarefasRaiz,
+    
+      props: true,
+    
+      meta: {
+        títuloParaMenu: 'Tarefas',
+      },
+      children: [
+        {
+          name: 'TransferenciaCronograma',
+          path: '',
+          component: TarefasLista,
+          meta: {
+            título: 'Cronograma de transferência voluntária',
+            títuloParaMenu: 'Cronograma',
+            rotasParaMenuSecundário,
+            rotasParaMigalhasDePão: [
+              'TransferenciasVoluntariasListar',
+              'TransferenciasVoluntariasResumo',
+              'TransferenciasVoluntariasTarefasListar',
+            ],
+          },
+          props: true,
+    
+          children: [
+            {
+              path: 'disparo-email',
+              component: DialogWrapper,
+              meta: {
+                rotaDeEscape: 'TransferenciasVoluntariasTarefasListar',
+                título: 'Novo disparo de e-mail para cronograma',
+                títuloParaMenu: 'Novo e-mail',
+              },
+              children: [
+                {
+                  path: '',
+                  name: 'transferenciaEmailModal',
+                  component: EmailModal,
+                },
+              ],
+            },
+          ],
+        },
+    
+        {
+          name: 'TransferenciasVoluntariasTarefasCriar',
+          path: 'nova',
+          component: TarefasCriarEditar,
+          meta: {
+            título: 'Nova tarefa',
+            títuloParaMenu: 'Nova tarefa',
+            rotaDeEscape: 'TransferenciasVoluntariasTarefasListar',
+          },
+        },
+    
+        {
+          path: ':tarefaId',
+          component: TarefasItem,
+          props: true,
+    
+          children: [
+            {
+              path: '',
+              name: 'TransferenciasVoluntariasTarefasEditar',
+              component: TarefasCriarEditar,
+              props: true,
+    
+              meta: {
+                título: () => useTarefasStore()?.emFoco?.tarefa || 'Editar tarefa',
+                títuloParaMenu: 'Editar tarefa',
+    
+                rotaDeEscape: 'TransferenciasVoluntariasTarefasListar',
+    
+                rotasParaMigalhasDePão: [
+                  'TransferenciasVoluntariasListar',
+                  'TransferenciasVoluntariasResumo',
+                  'TransferenciasVoluntariasTarefasListar',
+                  'tarefasEditar',
+                ],
+              },
+            },
+    
+            {
+              path: 'progresso',
+              name: 'TransferenciasVoluntariasTarefasProgresso',
+              component: TarefasProgresso,
+              props: true,
+              meta: {
+                título: () => (useTarefasStore()?.emFoco?.tarefa
+                  ? `Registro de progresso de ${useTarefasStore().emFoco.tarefa}`
+                  : 'Registrar progresso'),
+                títuloParaMenu: 'Progresso',
+    
+                rotaDeEscape: 'TransferenciasVoluntariasTarefasListar',
+    
+                rotasParaMigalhasDePão: [
+                  'TransferenciasVoluntariasListar',
+                  'TransferenciasVoluntariasResumo',
+                  'TransferenciasVoluntariasTarefasListar',
+                  'TransferenciasVoluntariasTarefasProgresso',
+                ],
+              },
+            },
+          ],
+        },
+      ],
+    }
   ],
 }
