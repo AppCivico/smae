541a6fe9d9244b371cf4721ba53dba5636fb7701
lucasansei <lucas.ansei@eokoe.com>
2024-03-25 11:24:38 -0300

    Transform param de filtro

--- backend/src/distribuicao-recurso/dto/filter-distribuicao-recurso.dto.ts
+++ backend/src/distribuicao-recurso/dto/filter-distribuicao-recurso.dto.ts
@@ -1,7 +1,9 @@
+import { Type } from 'class-transformer';
 import { IsOptional, IsNumber } from 'class-validator';
 
 export class FilterDistribuicaoRecursoDto {
     @IsOptional()
     @IsNumber()
+    @Type(() => Number)
     transferencia_id?: number;
 }
