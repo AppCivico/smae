9cc672c264547f47c3b59b232e71df99828ccf3e
Sobral <robson@oficiodesign.com>
2024-03-26 10:19:49 -0300

    fix: Tipos de listas sendo convertidos em objetos

--- frontend/src/helpers/nulificadorTotal.ts
+++ frontend/src/helpers/nulificadorTotal.ts
@@ -2,14 +2,21 @@ interface AnyObject {
   [key: string]: any;
 }
 
-const nulificadorTotal = (obj: AnyObject): AnyObject => Object.keys(obj)
-  .reduce((acc: AnyObject, key: string) => {
-    if (typeof obj[key] === 'object' && obj[key] !== null) {
-      acc[key] = nulificadorTotal(obj[key]);
-    } else {
-      acc[key] = obj[key] === '' ? null : obj[key];
-    }
-    return acc;
-  }, {});
+const nulificadorTotal = (obj: any): any => {
+  if (Array.isArray(obj)) {
+    return obj.map((item: any) => nulificadorTotal(item));
+  } if (typeof obj === 'object' && obj !== null) {
+    return Object.keys(obj)
+      .reduce((acc: AnyObject, key: string) => {
+        if (typeof obj[key] === 'object' && obj[key] !== null) {
+          acc[key] = nulificadorTotal(obj[key]);
+        } else {
+          acc[key] = obj[key] === '' ? null : obj[key];
+        }
+        return acc;
+      }, {});
+  }
+  return obj === '' ? null : obj;
+};
 
 export default nulificadorTotal;
