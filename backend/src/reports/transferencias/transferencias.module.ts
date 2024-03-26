63048c21cfe15511574fe0159548bcbe33f4374c
lucasansei <lucas.ansei@eokoe.com>
2024-03-26 08:57:03 -0300

    Adicionando dados de cronograma em report

--- backend/src/reports/transferencias/transferencias.module.ts
+++ backend/src/reports/transferencias/transferencias.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { TransferenciasController } from './transferencias.controller';
 import { TransferenciasService } from './transferencias.service';
+import { TarefaModule } from 'src/pp/tarefa/tarefa.module';
 
 @Module({
-    imports: [PrismaModule],
+    imports: [PrismaModule, forwardRef(() => TarefaModule)],
     controllers: [TransferenciasController],
     providers: [TransferenciasService],
     exports: [TransferenciasService],
