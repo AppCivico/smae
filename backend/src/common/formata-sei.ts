942d107352e8b0f5c4a006b1207cb546d7a8c150
lucasansei <lucas.ansei@eokoe.com>
2024-03-26 14:37:04 -0300

    Ajuste em formatação do sei

--- backend/src/common/formata-sei.ts
+++ backend/src/common/formata-sei.ts
@@ -5,12 +5,12 @@ export function formataSEI(input: string): string {
 
     const part1 = numero.slice(0, 4);
     const part2 = numero.slice(4, 8);
-    const part3 = numero.slice(8, 14);
+    const part3 = numero.slice(8, 15);
 
     let seiFormatado = `${part1}.${part2}/${part3}`;
 
-    if (numero.length > 13) {
-        const part4 = numero.slice(14, 15);
+    if (numero.length > 15) {
+        const part4 = numero.slice(15, 16);
         seiFormatado += `-${part4}`;
     }
 
