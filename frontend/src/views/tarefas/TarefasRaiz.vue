2d963712e242944c5eea87d26f1d97cae264ff64
Sobral <robson@oficiodesign.com>
2024-04-01 11:42:53 -0300

    perf: Limpar tarefas baixadas na saída da rota

--- frontend/src/views/tarefas/TarefasRaiz.vue
+++ frontend/src/views/tarefas/TarefasRaiz.vue
@@ -1,4 +1,10 @@
 <script setup>
+import { useTarefasStore } from '@/stores/tarefas.store.ts';
+import { onUnmounted } from 'vue';
+
+const tarefasStore = useTarefasStore();
+
+onUnmounted(() => { tarefasStore.$reset(); });
 </script>
 <template>
   <router-view />
