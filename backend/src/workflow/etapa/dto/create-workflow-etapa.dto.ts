e614654bd28899ac1e35bbdfc67cc8df660fc8ff
lucasansei <lucas.ansei@eokoe.com>
2024-04-05 16:39:47 -0300

    Arquivo que falta

--- backend/src/workflow/etapa/dto/create-workflow-etapa.dto.ts
+++ backend/src/workflow/etapa/dto/create-workflow-etapa.dto.ts
@@ -4,5 +4,6 @@ export class CreateWorkflowEtapaDto {
     @IsString()
     @MaxLength(250)
     @MinLength(1)
+    // Modificando de "etapa_fluxo" para "descrição" para facilitar implementação do front-end.
     descricao: string;
 }
