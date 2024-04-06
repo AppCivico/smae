d3bb9847fd3bcb4147a6d4346a6d92c6c30ca024
Giovana <f_gi@live.com>
2024-04-06 10:04:11 -0300

    feat: adicionando telas do crud de situação do workflow

---
+++ frontend/src/views/situacao/SituacaoCriarEditar.vue
@@ -0,0 +1,112 @@
+<script setup>
+import { situacao as schema } from "@/consts/formSchemas";
+import { useAlertStore } from "@/stores/alert.store";
+import { useSituacaoStore } from "@/stores/situacao.store.js";
+import { storeToRefs } from "pinia";
+import { ErrorMessage, Field, Form } from "vee-validate";
+import { computed } from "vue";
+import { useRoute, useRouter } from "vue-router";
+import tiposSituacao from "@/consts/tiposSituacao";
+
+const alertStore = useAlertStore();
+const situacaoStore = useSituacaoStore();
+const router = useRouter();
+const route = useRoute();
+const { chamadasPendentes, erro, tiposPorId } = storeToRefs(situacaoStore);
+
+const props = defineProps({
+  situacaoId: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const itemParaEdição = computed(
+  () => tiposPorId.value[props.situacaoId] || null 
+);
+async function onSubmit(_, { controlledValues: carga }) {
+  try {
+    const msg = props.situacaoId
+      ? "Dados salvos com sucesso!"
+      : "Item adicionado com sucesso!";
+
+    const resposta = await situacaoStore.salvarItem(carga, props.situacaoId);
+
+    if (resposta) {
+      alertStore.success(msg);
+      situacaoStore.$reset();
+      situacaoStore.buscarTudo();
+      router.push({ name: "situacaoListar" });
+    }
+  } catch (error) {
+    alertStore.error(error);
+  }
+}
+
+</script>
+<template>
+  <div class="flex spacebetween center mb2">
+    <h1>
+      <h1>{{ route?.meta?.título || "Nova situação" }}</h1>
+    </h1>
+    <hr class="ml2 f1" />
+    <CheckClose />
+  </div>
+  <Form
+    v-slot="{ errors, isSubmitting }"
+    :initial-values="itemParaEdição"
+    :disabled="chamadasPendentes.emFoco"
+    :validation-schema="schema"
+    @submit="onSubmit"
+  >
+    <div class="flex g2 mb1">
+      <div class="f2 mb1">
+        <LabelFromYup name="situacao" :schema="schema" />
+        <Field
+          id="situacao"
+          name="situacao"
+          type="text"
+          maxlength="250"
+          class="inputtext light mb1"
+        />
+        <ErrorMessage name="situacao" class="error-msg" />
+      </div>
+      <div class="f1">
+        <LabelFromYup name="tipo_situacao" :schema="schema" />
+        <Field
+          name="tipo_situacao"
+          as="select"
+          class="inputtext light mb1"
+          :class="{ error: errors.tipo, loading: chamadasPendentes.emFoco }"
+        >
+          <option value="">Selecionar</option>
+          <option v-for="tipo, key in tiposSituacao" :key="key" :value="tipo.value">
+            {{ tipo.label }}
+          </option>
+        </Field>
+        <ErrorMessage class="error-msg" name="tipo_situacao" />
+      </div>
+    </div>
+
+    <FormErrorsList :errors="errors" />
+
+    <div class="flex spacebetween center mb2">
+      <hr class="mr2 f1" />
+      <button
+        class="btn big"
+        :disabled="isSubmitting || Object.keys(errors)?.length"
+      >
+        Salvar
+      </button>
+      <hr class="ml2 f1" />
+    </div>
+  </Form>
+
+  <div v-if="chamadasPendentes?.emFoco" class="spinner">Carregando</div>
+
+  <div v-if="erro" class="error p1">
+    <div class="error-msg">
+      {{ erro }}
+    </div>
+  </div>
+</template>
---
+++ frontend/src/views/situacao/SituacaoLista.vue
@@ -0,0 +1,106 @@
+<script setup>
+import { useAlertStore } from '@/stores/alert.store';
+import { useSituacaoStore } from '@/stores/situacao.store.js';
+import { storeToRefs } from 'pinia';
+
+const situacaoStore = useSituacaoStore();
+const { lista, chamadasPendentes, erro} = storeToRefs(situacaoStore);
+
+const alertStore = useAlertStore();
+
+async function excluirSituacao(id) {
+  alertStore.confirmAction('Deseja mesmo remover esse item?', async () => {
+    if (await situacaoStore.excluirItem(id)) {
+      situacaoStore.buscarTudo();
+      alertStore.success('Item removido.');
+    }
+  }, 'Remover');
+}
+
+function ordenarListaAlfabeticamente() {
+  lista.value.sort((a, b) => a.situacao.localeCompare(b.situacao));
+}
+
+situacaoStore.buscarTudo().then(ordenarListaAlfabeticamente);
+</script>
+<template>
+  <div class="flex spacebetween center mb2">
+    <h1>{{ $route.meta.título }}</h1>
+    <hr class="ml2 f1">
+    <router-link
+      :to="{ name: 'situacaoCriar' }"
+      class="btn big ml2"
+    >
+      Nova situação
+    </router-link>
+  </div>
+
+  <table class="tablemain">
+    <col>
+    <col class="col--botão-de-ação">
+    <col class="col--botão-de-ação">
+    <thead>
+      <tr>
+        <th>
+          Situação
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="item in lista"
+        :key="item.id"
+      >
+        <td>{{ item.situacao }}</td>
+        <td>
+          <button
+            class="like-a__text"
+            arial-label="excluir"
+            title="excluir"
+            @click="excluirSituacao(item.id)"
+          >
+            <svg
+              width="20"
+              height="20"
+            ><use xlink:href="#i_remove" /></svg>
+          </button>
+        </td>
+        <td>
+          <router-link
+            :to="{ name: 'situacaoEditar', params: { situacaoId: item.id } }"
+            class="tprimary"
+          >
+            <svg
+              width="20"
+              height="20"
+            ><use xlink:href="#i_edit" /></svg>
+          </router-link>
+        </td>
+      </tr>
+      <tr v-if="chamadasPendentes.lista">
+        <td colspan="3">
+          Carregando
+        </td>
+      </tr>
+      <tr v-else-if="erro">
+        <td colspan="3">
+          Erro: {{ erro }}
+        </td>
+      </tr>
+      <tr v-else-if="!lista.length">
+        <td colspan="3">
+          Nenhum resultado encontrado.
+        </td>
+      </tr>
+    </tbody>
+  </table>
+
+  <div
+    v-if="erro"
+    class="error p1"
+  >
+    <div class="error-msg">
+      {{ erro }}
+    </div>
+  </div>
+</template>
