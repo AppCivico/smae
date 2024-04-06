2e7d773b65a2de1cbf95cd38deb949e18f9e1fb5
Giovana <f_gi@live.com>
2024-04-06 08:17:18 -0300

    feat: adicionando variavel de tipos da situação

---
+++ frontend/src/consts/tiposSituacao.js
@@ -0,0 +1,9 @@
+export default [
+  { label: "Não iniciado", value: "NaoIniciado" },
+  { label: "Em andamento", value: "EmAndamento" },
+  { label: "Suspenso", value: "Suspenso" },
+  { label: "Cancelado", value: "Cancelado" },
+  { label: "Concluído (com sucesso)", value: "ConcluidoComSucesso" },
+  { label: "Encerrado (sem sucesso)", value: "EncerradoSemSucesso" },
+  { label: "Terminal (situação que não tem saída)", value: "Terminal" },
+];
