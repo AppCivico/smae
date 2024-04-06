220e36a7944d3f6caf753f3ea041db569b37a52e
Giovana <f_gi@live.com>
2024-04-06 10:04:11 -0300

    feat: adicionando telas do crud de situação do workflow

---
+++ frontend/src/views/situacao/SituacaoRaiz.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { useSituacaoStore } from '@/stores/situacao.store.js';
+import { onUnmounted} from 'vue';
+const situacaoStore = useSituacaoStore();
+
+situacaoStore.buscarTudo();
+onUnmounted(() => { situacaoStore.$reset(); });
+</script>
+<template>
+  <router-view />
+</template>
