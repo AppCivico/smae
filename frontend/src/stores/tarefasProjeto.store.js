7addc1ece8a3a5c6618d3318b64441325e121863
ingredbarraqui <54070315+ingredbarraqui@users.noreply.github.com>
2024-04-06 17:44:53 -0300

    Crud: Workflow tarefas

---
+++ frontend/src/stores/tarefasProjeto.store.js
@@ -0,0 +1,62 @@
+import { defineStore } from 'pinia';
+
+const baseUrl = `${import.meta.env.VITE_API_URL}`;
+
+export const useTarefasProjetosStore = defineStore('tarefasProjetos', {
+  state: () => ({
+    lista: [],
+    chamadasPendentes: {
+      lista: false,
+    },
+    erro: null,
+  }),
+  actions: {
+    async buscarTudo(params = {}) {
+      this.chamadasPendentes.lista = true;
+      this.erro = null;
+
+      try {
+        const { linhas } = await this.requestS.get(`${baseUrl}/workflow-tarefa`, params);
+        this.lista = linhas;
+      } catch (erro) {
+        this.erro = erro;
+      }
+      this.chamadasPendentes.lista = false;
+    },
+
+    async excluirItem(id) {
+      this.chamadasPendentes.lista = true;
+      this.erro = null;
+
+      try {
+        await this.requestS.delete(`${baseUrl}/workflow-tarefa/${id}`);
+        this.chamadasPendentes.lista = false;
+        return true;
+      } catch (erro) {
+        this.erro = erro;
+        this.chamadasPendentes.lista = false;
+        return false;
+      }
+    },
+
+    async salvarItem(params = {}, id = 0) {
+      this.chamadasPendentes.emFoco = true;
+      this.erro = null;
+
+      try {
+        if (id) {
+          await this.requestS.patch(`${baseUrl}/workflow-tarefa/${id}`, params);
+        } else {
+          await this.requestS.post(`${baseUrl}/workflow-tarefa`, params);
+        }
+
+        this.chamadasPendentes.emFoco = false;
+        return true;
+      } catch (erro) {
+        this.erro = erro;
+        this.chamadasPendentes.emFoco = false;
+        return false;
+      }
+    },
+  },
+});
--- frontend/src/views/fasesProjeto/FasesRaiz.vue
+++ frontend/src/views/fasesProjeto/FasesRaiz.vue
@@ -1,3 +1,11 @@
+<script setup>
+import { useFasesProjetosStore } from '@/stores/fasesProjeto.store.js';
+import { onUnmounted} from 'vue';
+const fasesProjetosStore = useFasesProjetosStore();
+
+fasesProjetosStore.buscarTudo();
+onUnmounted(() => { fasesProjetosStore.$reset(); });
+</script>
 <template>
   <router-view />
 </template>
