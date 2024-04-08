96f9d0829fe2fe1445f896812331f1d245c9b01d
ingredbarraqui <54070315+ingredbarraqui@users.noreply.github.com>
2024-04-08 17:51:04 -0300

    Crud: Cadastro de Fluxo

---
+++ frontend/src/stores/fluxosProjeto.store.js
@@ -0,0 +1,77 @@
+import { defineStore } from 'pinia';
+
+const baseUrl = `${import.meta.env.VITE_API_URL}`;
+
+export const useFluxosProjetosStore = defineStore('fluxosProjetos', {
+  state: () => ({
+    lista: [],
+    chamadasPendentes: {
+      lista: false,
+    },
+    erro: null,
+  }),
+  actions: {
+    async buscarItem(id = 0, params = {}) {
+      this.chamadasPendentes.emFoco = true;
+      this.erro = null;
+
+      try {
+        const resposta = await this.requestS.get(`${baseUrl}/workflow/${id}`, params);
+        this.emFoco = {
+          ...resposta,
+        };
+      } catch (erro) {
+        this.erro = erro;
+      }
+      this.chamadasPendentes.emFoco = false;
+    },
+
+    async buscarTudo(params = {}) {
+      this.chamadasPendentes.lista = true;
+      this.erro = null;
+
+      try {
+        const { linhas } = await this.requestS.get(`${baseUrl}/workflow`, params);
+        this.lista = linhas;
+      } catch (erro) {
+        this.erro = erro;
+      }
+      this.chamadasPendentes.lista = false;
+    },
+
+    async excluirItem(id) {
+      this.chamadasPendentes.lista = true;
+      this.erro = null;
+
+      try {
+        await this.requestS.delete(`${baseUrl}/workflow/${id}`);
+        this.chamadasPendentes.lista = false;
+        return true;
+      } catch (erro) {
+        this.erro = erro;
+        this.chamadasPendentes.lista = false;
+        return false;
+      }
+    },
+
+    async salvarItem(params = {}, id = 0) {
+      this.chamadasPendentes.emFoco = true;
+      this.erro = null;
+
+      try {
+        if (id) {
+          await this.requestS.patch(`${baseUrl}/workflow/${id}`, params);
+        } else {
+          await this.requestS.post(`${baseUrl}/workflow`, params);
+        }
+
+        this.chamadasPendentes.emFoco = false;
+        return true;
+      } catch (erro) {
+        this.erro = erro;
+        this.chamadasPendentes.emFoco = false;
+        return false;
+      }
+    },
+  },
+});
