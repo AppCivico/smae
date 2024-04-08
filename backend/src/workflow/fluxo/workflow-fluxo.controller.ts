52941c718d4174fbdd736c7950e1888919cc28e5
lucasansei <lucas.ansei@eokoe.com>
2024-04-08 08:18:32 -0300

    Adicionando fluxo de workflow

---
+++ backend/src/workflow/fluxo/workflow-fluxo.controller.ts
@@ -0,0 +1,67 @@
+import { Controller, Post, Body, Patch, Param, Get, Delete, HttpCode, HttpStatus, Query } from '@nestjs/common';
+import { ApiTags, ApiBearerAuth, ApiUnauthorizedResponse, ApiNoContentResponse } from '@nestjs/swagger';
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
+import { PessoaFromJwt } from 'src/auth/models/PessoaFromJwt';
+import { RecordWithId } from 'src/common/dto/record-with-id.dto';
+import { FindOneParams } from 'src/common/decorators/find-params';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { WorkflowFluxoService } from './workflow-fluxo.service';
+import { CreateWorkflowFluxoDto } from './dto/create-workflow-fluxo.dto';
+import { ListWorkflowFluxoDto } from './entities/workflow-fluxo.entity';
+import { UpdateWorkflowFluxoDto } from './dto/update-workflow-fluxo.dto';
+import { FilterWorkflowFluxoDto } from './dto/filter-workflow-fluxo.dto';
+
+@ApiTags('Workflow - Configuração')
+@Controller('workflow-fluxo')
+export class WorkflowFluxoController {
+    constructor(private readonly workflowFluxoService: WorkflowFluxoService) {}
+
+    @Post('')
+    @ApiBearerAuth('access-token')
+    @Roles('CadastroWorkflows.inserir')
+    @ApiUnauthorizedResponse()
+    async create(@Body() dto: CreateWorkflowFluxoDto, @CurrentUser() user: PessoaFromJwt): Promise<RecordWithId> {
+        return await this.workflowFluxoService.create(dto, user);
+    }
+
+    @ApiBearerAuth('access-token')
+    @Roles('CadastroWorkflows.listar')
+    @Get()
+    async findAll(
+        @Query() filters: FilterWorkflowFluxoDto,
+        @CurrentUser() user: PessoaFromJwt
+    ): Promise<ListWorkflowFluxoDto> {
+        return { linhas: await this.workflowFluxoService.findAll(filters, user) };
+    }
+
+    // @Get(':id')
+    // @ApiBearerAuth('access-token')
+    // @Roles('CadastroWorkflows.listar')
+    // @ApiUnauthorizedResponse()
+    // async findOne(@Param() params: FindOneParams, @CurrentUser() user: PessoaFromJwt): Promise<TransferenciaDetailDto> {
+    //     return await this.workflowFluxoService.f(params.id, user);
+    // }
+
+    @Patch(':id')
+    @ApiBearerAuth('access-token')
+    @Roles('CadastroWorkflows.editar')
+    @ApiUnauthorizedResponse()
+    async update(
+        @Param() params: FindOneParams,
+        @Body() dto: UpdateWorkflowFluxoDto,
+        @CurrentUser() user: PessoaFromJwt
+    ): Promise<RecordWithId> {
+        return await this.workflowFluxoService.update(+params.id, dto, user);
+    }
+
+    @Delete(':id')
+    @ApiBearerAuth('access-token')
+    @ApiUnauthorizedResponse()
+    @Roles('CadastroWorkflows.remover')
+    @ApiNoContentResponse()
+    @HttpCode(HttpStatus.ACCEPTED)
+    async remove(@Param() params: FindOneParams, @CurrentUser() user: PessoaFromJwt) {
+        await this.workflowFluxoService.remove(+params.id, user);
+        return '';
+    }
+}
---
+++ backend/src/workflow/fluxo/workflow-fluxo.service.ts
@@ -0,0 +1,221 @@
+import { HttpException, Injectable, NotFoundException } from '@nestjs/common';
+import { RecordWithId } from 'src/common/dto/record-with-id.dto';
+import { Prisma } from '@prisma/client';
+import { CreateWorkflowFluxoDto } from './dto/create-workflow-fluxo.dto';
+import { UpdateWorkflowFluxoDto } from './dto/update-workflow-fluxo.dto';
+import { WorkflowFluxoDto } from './entities/workflow-fluxo.entity';
+import { PessoaFromJwt } from 'src/auth/models/PessoaFromJwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { FilterWorkflowFluxoDto } from './dto/filter-workflow-fluxo.dto';
+
+@Injectable()
+export class WorkflowFluxoService {
+    constructor(private readonly prisma: PrismaService) {}
+
+    async create(dto: CreateWorkflowFluxoDto, user: PessoaFromJwt): Promise<RecordWithId> {
+        const created = await this.prisma.$transaction(
+            async (prismaTxn: Prisma.TransactionClient): Promise<RecordWithId> => {
+                const saidaJaExiste = await prismaTxn.fluxo.count({
+                    where: {
+                        workflow_id: dto.workflow_id,
+                        fluxo_etapa_de_id: dto.workflow_etapa_de_id,
+                        removido_em: null,
+                    },
+                });
+                if (saidaJaExiste)
+                    throw new HttpException('workflow_etapa_de_id| Etapa de saída já em uso para este Workflow.', 400);
+
+                const destinoJaExiste = await prismaTxn.fluxo.count({
+                    where: {
+                        workflow_id: dto.workflow_id,
+                        fluxo_etapa_para_id: dto.workflow_etapa_para_id,
+                        removido_em: null,
+                    },
+                });
+                if (destinoJaExiste)
+                    throw new HttpException(
+                        'workflow_etapa_para_id| Etapa de saída já em uso para este Workflow.',
+                        400
+                    );
+
+                // Tratando ordem
+                let ordem: number;
+                if (dto.ordem != undefined) {
+                    const ordemEmUso = await prismaTxn.fluxo.count({
+                        where: {
+                            workflow_id: dto.workflow_id,
+                            ordem: dto.ordem,
+                            removido_em: null,
+                        },
+                    });
+                    if (ordemEmUso) throw new HttpException('ordem| Ordem já em uso para este Workflow.', 400);
+
+                    ordem = dto.ordem;
+                } else {
+                    const ultimaOrdem = await prismaTxn.fluxo.findFirst({
+                        where: {
+                            workflow_id: dto.workflow_id,
+                            removido_em: null,
+                        },
+                        select: { ordem: true },
+                    });
+
+                    ordem = ultimaOrdem?.ordem ?? 1;
+                }
+
+                // TODO: tratar fluxo circular
+
+                const workflowFluxo = await prismaTxn.fluxo.create({
+                    data: {
+                        workflow_id: dto.workflow_id,
+                        fluxo_etapa_de_id: dto.workflow_etapa_de_id,
+                        fluxo_etapa_para_id: dto.workflow_etapa_para_id,
+                        ordem: ordem,
+                        criado_por: user.id,
+                        criado_em: new Date(Date.now()),
+                    },
+                    select: { id: true },
+                });
+
+                return workflowFluxo;
+            }
+        );
+
+        return created;
+    }
+
+    async update(id: number, dto: UpdateWorkflowFluxoDto, user: PessoaFromJwt): Promise<RecordWithId> {
+        const updated = await this.prisma.$transaction(
+            async (prismaTxn: Prisma.TransactionClient): Promise<RecordWithId> => {
+                const self = await prismaTxn.fluxo.findFirst({
+                    where: {
+                        id,
+                        removido_em: null,
+                    },
+                    select: {
+                        workflow_id: true,
+                        fluxo_etapa_de_id: true,
+                        fluxo_etapa_para_id: true,
+                        ordem: true,
+                    },
+                });
+                if (!self) throw new NotFoundException('Fluxo não encontrado');
+
+                if (dto.workflow_etapa_de_id != undefined && dto.workflow_etapa_de_id != self.fluxo_etapa_de_id) {
+                    const saidaJaExiste = await prismaTxn.fluxo.count({
+                        where: {
+                            workflow_id: self.workflow_id,
+                            fluxo_etapa_de_id: dto.workflow_etapa_de_id,
+                            removido_em: null,
+                        },
+                    });
+                    if (saidaJaExiste)
+                        throw new HttpException(
+                            'workflow_etapa_de_id| Etapa de saída já em uso para este Workflow.',
+                            400
+                        );
+                }
+
+                if (dto.workflow_etapa_para_id != undefined && dto.workflow_etapa_para_id != self.fluxo_etapa_para_id) {
+                    const destinoJaExiste = await prismaTxn.fluxo.count({
+                        where: {
+                            workflow_id: self.workflow_id,
+                            fluxo_etapa_para_id: dto.workflow_etapa_para_id,
+                            removido_em: null,
+                        },
+                    });
+                    if (destinoJaExiste)
+                        throw new HttpException(
+                            'workflow_etapa_para_id| Etapa de saída já em uso para este Workflow.',
+                            400
+                        );
+                }
+
+                // Tratando ordem
+                let ordem: number | undefined;
+                if (dto.ordem != undefined && dto.ordem != self.ordem) {
+                    const ordemEmUso = await prismaTxn.fluxo.count({
+                        where: {
+                            workflow_id: self.workflow_id,
+                            ordem: dto.ordem,
+                            removido_em: null,
+                        },
+                    });
+                    if (ordemEmUso) throw new HttpException('ordem| Ordem já em uso para este Workflow.', 400);
+
+                    ordem = dto.ordem;
+                }
+
+                const workflowFluxo = await prismaTxn.fluxo.update({
+                    where: { id },
+                    data: {
+                        fluxo_etapa_de_id: dto.workflow_etapa_de_id,
+                        fluxo_etapa_para_id: dto.workflow_etapa_para_id,
+                        ordem: ordem,
+                        atualizado_por: user.id,
+                        atualizado_em: new Date(Date.now()),
+                    },
+                    select: { id: true },
+                });
+
+                return workflowFluxo;
+            }
+        );
+
+        return updated;
+    }
+
+    async findAll(filters: FilterWorkflowFluxoDto, user: PessoaFromJwt): Promise<WorkflowFluxoDto[]> {
+        const rows = await this.prisma.fluxo.findMany({
+            where: {
+                workflow_id: filters.workflow_id,
+                removido_em: null,
+            },
+            orderBy: [{ ordem: 'asc' }],
+            select: {
+                id: true,
+                workflow_id: true,
+                ordem: true,
+
+                fluxo_etapa_de: {
+                    select: {
+                        id: true,
+                        etapa_fluxo: true,
+                    },
+                },
+
+                fluxo_etapa_para: {
+                    select: {
+                        id: true,
+                        etapa_fluxo: true,
+                    },
+                },
+            },
+        });
+
+        return rows.map((r) => {
+            return {
+                ...r,
+
+                workflow_etapa_de: {
+                    id: r.fluxo_etapa_de.id,
+                    descricao: r.fluxo_etapa_de.etapa_fluxo,
+                },
+                workflow_etapa_para: {
+                    id: r.fluxo_etapa_para.id,
+                    descricao: r.fluxo_etapa_para.etapa_fluxo,
+                },
+            };
+        });
+    }
+
+    async remove(id: number, user: PessoaFromJwt) {
+        await this.prisma.fluxo.update({
+            where: { id },
+            data: {
+                removido_por: user.id,
+                removido_em: new Date(Date.now()),
+            },
+        });
+    }
+}
