695a8bde5aba5cc82299292673dff01c128cec2c
ingredbarraqui <54070315+ingredbarraqui@users.noreply.github.com>
2024-04-09 17:47:46 -0300

    feat: Store de Etapa do fluxo

---
+++ frontend/src/stores/fluxosFasesProjeto.store.js
@@ -0,0 +1,85 @@
+import { defineStore } from 'pinia';
+const baseUrl = `${import.meta.env.VITE_API_URL}`;
+
+export const useFluxosFasesProjetosStore = defineStore('fluxosFasesProjetos', {
+  state: () => ({
+    lista: [],
+    emFoco: null,
+    chamadasPendentes: {
+      lista: false,
+      emFoco: false,
+    },
+    erro: null,
+  }),
+  actions: {
+    async buscarItem(id = 0, params = {}) {
+      this.chamadasPendentes.emFoco = true;
+      this.erro = null;
+
+      try {
+        const resposta = await this.requestS.get(`${baseUrl}/workflow-fluxo-fase/${id}`, params);
+        this.emFoco = {
+          ...resposta,
+        };
+      } catch (erro) {
+        this.erro = erro;
+      }
+      this.chamadasPendentes.emFoco = false;
+    },
+
+    async buscarTudo(params = {}) {
+      this.chamadasPendentes.lista = true;
+      this.erro = null;
+
+      try {
+        const { linhas } = await this.requestS.get(`${baseUrl}/workflow-fluxo-fase`, params);
+        this.lista = linhas;
+      } catch (erro) {
+        this.erro = erro;
+      }
+      this.chamadasPendentes.lista = false;
+    },
+
+    async excluirItem(id) {
+      this.chamadasPendentes.lista = true;
+      this.erro = null;
+
+      try {
+        await this.requestS.delete(`${baseUrl}/workflow-fluxo-fase/${id}`);
+        this.chamadasPendentes.lista = false;
+        return true;
+      } catch (erro) {
+        this.erro = erro;
+        this.chamadasPendentes.lista = false;
+        return false;
+      }
+    },
+
+    async salvarItem(params = {}, id = 0) {
+      this.chamadasPendentes.emFoco = true;
+      this.erro = null;
+
+      try {
+        if (id) {
+          await this.requestS.patch(`${baseUrl}/workflow-fluxo-fase/${id}`, params);
+        } else {
+          await this.requestS.post(`${baseUrl}/workflow-fluxo-fase`, params);
+        }
+
+        this.chamadasPendentes.emFoco = false;
+        return true;
+      } catch (erro) {
+        this.erro = erro;
+        this.chamadasPendentes.emFoco = false;
+        return false;
+      }
+    },
+  },
+  getters: {
+    itemParaEdição({ emFoco }) {
+      return {
+        ...emFoco,
+      };
+    },
+  },
+});
