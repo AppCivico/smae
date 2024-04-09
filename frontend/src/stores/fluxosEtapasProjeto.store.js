b2f4605f6e5f8cf6d59f2ace528bcb4a26a951c4
ingredbarraqui <54070315+ingredbarraqui@users.noreply.github.com>
2024-04-09 19:13:36 -0300

    feat: Store de Fases do fluxo

---
+++ frontend/src/stores/fluxosEtapasProjeto.store.js
@@ -0,0 +1,78 @@
+import { defineStore } from 'pinia';
+const baseUrl = `${import.meta.env.VITE_API_URL}`;
+
+export const useFluxosEtapasProjetosStore = defineStore('fluxosEtapasProjetos', {
+  state: () => ({
+    lista: [],
+    chamadasPendentes: {
+      lista: false,
+    },
+    erro: null,
+  }),
+  actions: {
+    async buscarItem(id = 0, params = {}) {
+      this.chamadasPendentes.emFoco = true;
+      this.erro = null;
+
+      try {
+        const resposta = await this.requestS.get(`${baseUrl}/workflow-fluxo/${id}`, params);
+        this.emFoco = {
+          ...resposta,
+        };
+      } catch (erro) {
+        this.erro = erro;
+      }
+      this.chamadasPendentes.emFoco = false;
+    },
+
+    async buscarTudo(params = {}) {
+      this.chamadasPendentes.lista = true;
+      this.erro = null;
+
+      try {
+        const { linhas } = await this.requestS.get(`${baseUrl}/workflow-fluxo`, params);
+        this.lista = linhas;
+      } catch (erro) {
+        this.erro = erro;
+      }
+      this.chamadasPendentes.lista = false;
+    },
+
+    async excluirItem(id) {
+      this.chamadasPendentes.lista = true;
+      this.erro = null;
+
+      try {
+        await this.requestS.delete(`${baseUrl}/workflow-fluxo/${id}`);
+        this.chamadasPendentes.lista = false;
+        return true;
+      } catch (erro) {
+        this.erro = erro;
+        this.chamadasPendentes.lista = false;
+        return false;
+      }
+    },
+
+    async salvarItem(params = {}, id = 0) {
+      this.chamadasPendentes.emFoco = true;
+      this.erro = null;
+
+      try {
+        if (id) {
+          await this.requestS.patch(`${baseUrl}/workflow-fluxo/${id}`, params);
+        } else {
+          await this.requestS.post(`${baseUrl}/workflow-fluxo`, params);
+        }
+
+        this.chamadasPendentes.emFoco = false;
+        return true;
+      } catch (erro) {
+        this.erro = erro;
+        this.chamadasPendentes.emFoco = false;
+        return false;
+      }
+    },
+  },
+});
+
+
