6eb7426759076d1b3afe2a61914e285481866cb4
lucasansei <lucas.ansei@eokoe.com>
2024-04-11 15:58:28 -0300

    Fix: transformer para int

--- backend/src/workflow/andamento/dto/filter-andamento.dto.ts
+++ backend/src/workflow/andamento/dto/filter-andamento.dto.ts
@@ -1,6 +1,8 @@
+import { Type } from 'class-transformer';
 import { IsInt } from 'class-validator';
 
 export class FilterWorkflowAndamentoDto {
     @IsInt()
+    @Type(() => Number)
     transferencia_id: number;
 }
