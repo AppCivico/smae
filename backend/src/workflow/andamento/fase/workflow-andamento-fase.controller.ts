ed046cdd3ee2823aff3a9582f9ab072650f65554
lucasansei <lucas.ansei@eokoe.com>
2024-04-11 09:12:46 -0300

    Adicionando andamento de fase e conclusão

---
+++ backend/src/workflow/andamento/fase/workflow-andamento-fase.controller.ts
@@ -0,0 +1,33 @@
+import { Controller, Body, Patch } from '@nestjs/common';
+import { ApiTags, ApiBearerAuth, ApiUnauthorizedResponse } from '@nestjs/swagger';
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
+import { PessoaFromJwt } from 'src/auth/models/PessoaFromJwt';
+import { RecordWithId } from 'src/common/dto/record-with-id.dto';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { UpdateWorkflowAndamentoFaseDto, WorkflowFinalizarFaseDto } from './dto/patch-workflow-andamento-fase.dto';
+import { WorkflowAndamentoFaseService } from './workflow-andamento-fase.service';
+
+@ApiTags('Workflow - Andamento')
+@Controller('workflow-andamento-fase')
+export class WorkflowAndamentoTarefasController {
+    constructor(private readonly workflowAndamentoFaseService: WorkflowAndamentoFaseService) {}
+
+    @Patch('')
+    @ApiBearerAuth('access-token')
+    @Roles('CadastroWorkflows.inserir')
+    @ApiUnauthorizedResponse()
+    async update(
+        @Body() dto: UpdateWorkflowAndamentoFaseDto,
+        @CurrentUser() user: PessoaFromJwt
+    ): Promise<RecordWithId> {
+        return await this.workflowAndamentoFaseService.update(dto, user);
+    }
+
+    @Patch('finalizar')
+    @ApiBearerAuth('access-token')
+    @Roles('CadastroWorkflows.inserir')
+    @ApiUnauthorizedResponse()
+    async create(@Body() dto: WorkflowFinalizarFaseDto, @CurrentUser() user: PessoaFromJwt): Promise<RecordWithId> {
+        return await this.workflowAndamentoFaseService.finalizarFase(dto, user);
+    }
+}
