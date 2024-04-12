e4797a1792626d3ae81d7e526ab205e1975c5a33
Sobral <robson@oficiodesign.com>
2024-04-12 20:40:18 -0300

    feat: Avançar etapas e iniciar fases do Workflow

--- frontend/src/stores/workflow.andamento.store.ts
+++ frontend/src/stores/workflow.andamento.store.ts
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { WorkflowAndamentoDto } from '@/../../backend/src/workflow/andamento/entities/workflow-andamento.entity';
+import { WorkflowAndamentoDto, WorkflowAndamentoFasesDto, WorkflowAndamentoFluxoDto } from '@/../../backend/src/workflow/andamento/entities/workflow-andamento.entity';
 import { defineStore } from 'pinia';
 
 const baseUrl = `${import.meta.env.VITE_API_URL}`;
@@ -62,11 +62,32 @@ export const useWorkflowAndamentoStore = defineStore('workflowAndamento', {
         return false;
       }
     },
+
+    async iniciarFase(faseId: Number, transferênciaId: Number): Promise<boolean> {
+      this.chamadasPendentes.fase = true;
+      await new Promise((resolve) => { setTimeout(resolve, 5000); });
+
+      try {
+        const resposta = await this.requestS.post(`${baseUrl}/workflow-andamento-fase/iniciar`, {
+          transferencia_id: transferênciaId || Number(this.route.params.transferenciaId),
+          fase_id: faseId,
+        });
+
+        this.chamadasPendentes.fase = false;
+        this.erro = null;
+        return !!resposta;
+      } catch (erro) {
+        this.erro = erro;
+        this.chamadasPendentes.fase = false;
+        return false;
+      }
+    },
+
     async encerrarFase(params = {}): Promise<boolean> {
       this.chamadasPendentes.fase = true;
 
       try {
-        const resposta = await this.requestS.patch(`${baseUrl}/workflow-andamento-fase/finalizar`, {
+        const resposta = await this.requestS.post(`${baseUrl}/workflow-andamento-fase/finalizar`, {
           transferencia_id: Number(this.route.params.transferenciaId) || undefined,
           ...params,
         });
@@ -80,23 +101,45 @@ export const useWorkflowAndamentoStore = defineStore('workflowAndamento', {
         return false;
       }
     },
-    async editarTarefas(params = {}): Promise<boolean> {
-      this.chamadasPendentes.tarefas = true;
+
+    async avançarEtapa(transferênciaId: Number): Promise<boolean> {
+      this.chamadasPendentes.fase = true;
+      await new Promise((resolve) => { setTimeout(resolve, 5000); });
 
       try {
-        const resposta = await this.requestS.patch(`${baseUrl}/workflow-andamento-tarefas`, {
-          transferencia_id: Number(this.route.params.transferenciaId) || undefined,
-          ...params,
+        const resposta = await this.requestS.post(`${baseUrl}/workflow-andamento/iniciar-prox-etapa`, {
+          transferencia_id: transferênciaId || Number(this.route.params.transferenciaId),
         });
 
-        this.chamadasPendentes.tarefas = false;
+        this.chamadasPendentes.fase = false;
         this.erro = null;
         return !!resposta;
       } catch (erro) {
         this.erro = erro;
-        this.chamadasPendentes.tarefas = false;
+        this.chamadasPendentes.fase = false;
         return false;
       }
+    },
+  },
+
+  getters: {
+    etapaCorrente: ({ workflow }): WorkflowAndamentoFluxoDto | null => workflow?.fluxo?.[0] || null,
+
+    inícioDeFasePermitido() {
+      return this.etapaCorrente?.fases?.some((x: WorkflowAndamentoFasesDto) => x.andamento === null)
+      || false;
+    },
+
+    avançoDeEtapaPermitido() {
+      // eslint-disable-next-line max-len
+      return !this.etapaCorrente?.fases?.some((x: WorkflowAndamentoFasesDto) => !x.andamento?.concluida)
+      || false;
+    },
+
+    idDaPróximaFasePendente() {
+      // eslint-disable-next-line max-len
+      return this.etapaCorrente?.fases?.find((x: WorkflowAndamentoFasesDto) => x.andamento === null)?.fase?.id
+       || 0;
     },
   },
 });
