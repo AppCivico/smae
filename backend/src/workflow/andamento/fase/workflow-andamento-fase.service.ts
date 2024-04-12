28007ac2a2affaef64c57123e1027d5e9d5aadb3
lucasansei <lucas.ansei@eokoe.com>
2024-04-12 08:58:13 -0300

    Ajuste em DTO

--- backend/src/workflow/andamento/fase/workflow-andamento-fase.service.ts
+++ backend/src/workflow/andamento/fase/workflow-andamento-fase.service.ts
@@ -162,85 +162,87 @@ export class WorkflowAndamentoFaseService {
 
         const operations = [];
         const idsAtualizados: RecordWithId[] = [];
-        for (const tarefa of dto.tarefas) {
-            // Verificando se esta tarefa está de fato na configuração do Workflow.
-            const tarefaWorkfloConfig = await prismaTxn.fluxoTarefa.findFirst({
-                where: {
-                    removido_em: null,
-                    workflow_tarefa_id: tarefa.id,
+        if (dto.tarefas != undefined) {
+            for (const tarefa of dto.tarefas) {
+                // Verificando se esta tarefa está de fato na configuração do Workflow.
+                const tarefaWorkfloConfig = await prismaTxn.fluxoTarefa.findFirst({
+                    where: {
+                        removido_em: null,
+                        workflow_tarefa_id: tarefa.id,
 
-                    fluxo_fase: {
-                        id: dto.fase_id,
-                        fluxo: {
-                            workflow_id: transferenciaAndamento.transferencia.workflow_id,
+                        fluxo_fase: {
+                            id: dto.fase_id,
+                            fluxo: {
+                                workflow_id: transferenciaAndamento.transferencia.workflow_id,
+                            },
                         },
                     },
-                },
-                select: {
-                    responsabilidade: true,
-                },
-            });
-            if (!tarefaWorkfloConfig) throw new Error('Tarefa não existe na configuração do Workflow.');
+                    select: {
+                        responsabilidade: true,
+                    },
+                });
+                if (!tarefaWorkfloConfig) throw new Error('Tarefa não existe na configuração do Workflow.');
 
-            // Verificando necessidade de preencher órgão responsável.
-            if (
-                tarefaWorkfloConfig.responsabilidade == WorkflowResponsabilidade.Propria &&
-                tarefa.orgao_responsavel_id != undefined
-            )
-                throw new HttpException(
-                    `orgao_responsavel_id| Órgão não deve ser enviado para tarefa ${tarefa.id}, pois é de responsabilidade própria.`,
-                    400
-                );
+                // Verificando necessidade de preencher órgão responsável.
+                if (
+                    tarefaWorkfloConfig.responsabilidade == WorkflowResponsabilidade.Propria &&
+                    tarefa.orgao_responsavel_id != undefined
+                )
+                    throw new HttpException(
+                        `orgao_responsavel_id| Órgão não deve ser enviado para tarefa ${tarefa.id}, pois é de responsabilidade própria.`,
+                        400
+                    );
 
-            const transferenciaAndamentoTarefaRow = await prismaTxn.transferenciaAndamentoTarefa.findFirst({
-                where: {
-                    transferencia_andamento_id: transferenciaAndamento.id,
-                    workflow_tarefa_fluxo_id: tarefa.id,
-                    removido_em: null,
-                },
-                select: {
-                    id: true,
-                    orgao_responsavel_id: true,
-                    feito: true,
-                },
-            });
-            if (!transferenciaAndamentoTarefaRow)
-                throw new Error(
-                    'Não foi encontrado registro de andamento para a tarefa. Fase anterior não foi fechada ou está em fase Terminal.'
-                );
+                const transferenciaAndamentoTarefaRow = await prismaTxn.transferenciaAndamentoTarefa.findFirst({
+                    where: {
+                        transferencia_andamento_id: transferenciaAndamento.id,
+                        workflow_tarefa_fluxo_id: tarefa.id,
+                        removido_em: null,
+                    },
+                    select: {
+                        id: true,
+                        orgao_responsavel_id: true,
+                        feito: true,
+                    },
+                });
+                if (!transferenciaAndamentoTarefaRow)
+                    throw new Error(
+                        'Não foi encontrado registro de andamento para a tarefa. Fase anterior não foi fechada ou está em fase Terminal.'
+                    );
 
-            if (
-                tarefaWorkfloConfig.responsabilidade == WorkflowResponsabilidade.OutroOrgao &&
-                (!tarefa.orgao_responsavel_id || !transferenciaAndamentoTarefaRow.orgao_responsavel_id)
-            )
-                throw new HttpException(
-                    `orgao_responsavel_id| Órgão deve ser enviado para tarefa ${tarefa.id}, pois é de responsabilidade de outro órgão.`,
-                    400
-                );
+                if (
+                    tarefaWorkfloConfig.responsabilidade == WorkflowResponsabilidade.OutroOrgao &&
+                    (!tarefa.orgao_responsavel_id || !transferenciaAndamentoTarefaRow.orgao_responsavel_id)
+                )
+                    throw new HttpException(
+                        `orgao_responsavel_id| Órgão deve ser enviado para tarefa ${tarefa.id}, pois é de responsabilidade de outro órgão.`,
+                        400
+                    );
 
-            if (
-                transferenciaAndamentoTarefaRow.feito != tarefa.concluida ||
-                transferenciaAndamentoTarefaRow.orgao_responsavel_id != tarefa.orgao_responsavel_id
-            ) {
-                operations.push(
-                    prismaTxn.transferenciaAndamentoTarefa.update({
-                        where: {
-                            id: transferenciaAndamentoTarefaRow.id,
-                        },
-                        data: {
-                            feito: tarefa.concluida,
-                            orgao_responsavel_id: tarefa.orgao_responsavel_id,
-                            atualizado_por: user.id,
-                            atualizado_em: new Date(Date.now()),
-                        },
-                    })
-                );
+                if (
+                    transferenciaAndamentoTarefaRow.feito != tarefa.concluida ||
+                    transferenciaAndamentoTarefaRow.orgao_responsavel_id != tarefa.orgao_responsavel_id
+                ) {
+                    operations.push(
+                        prismaTxn.transferenciaAndamentoTarefa.update({
+                            where: {
+                                id: transferenciaAndamentoTarefaRow.id,
+                            },
+                            data: {
+                                feito: tarefa.concluida,
+                                orgao_responsavel_id: tarefa.orgao_responsavel_id,
+                                atualizado_por: user.id,
+                                atualizado_em: new Date(Date.now()),
+                            },
+                        })
+                    );
 
-                idsAtualizados.push({ id: transferenciaAndamentoTarefaRow.id });
+                    idsAtualizados.push({ id: transferenciaAndamentoTarefaRow.id });
+                }
             }
-        }
 
-        await Promise.all(operations);
+            await Promise.all(operations);
+        }
 
         return idsAtualizados;
     }
