0ec2266a12c984ec0851f24d12a5384875b991be
Giovana <f_gi@live.com>
2024-04-12 23:17:21 -0300

    feat: adicionando função de salvar nota

--- frontend/src/stores/blocoNotas.store.js
+++ frontend/src/stores/blocoNotas.store.js
@@ -28,6 +28,26 @@ export const useBlocoDeNotasStore = defineStore("blocoDeNotasStore", {
       }
       this.chamadasPendentes.lista = false;
     },
+    
+    async salvarItem(params = {}, id = 0) {
+      this.chamadasPendentes.emFoco = true;
+      this.erro = null;
+
+      try {
+        if (params.id) {
+          await this.requestS.patch(`${baseUrl}/nota/${params.id}`, params);
+        } else {
+          await this.requestS.post(`${baseUrl}/nota`, params);
+        }
+
+        this.chamadasPendentes.emFoco = false;
+        return true;
+      } catch (erro) {
+        this.erro = erro;
+        this.chamadasPendentes.emFoco = false;
+        return false;
+      }
+    },
   },
 
   getters: {
