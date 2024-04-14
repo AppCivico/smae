37972ff0a14d7577bc2401993dc27b53f4dc7e1a
Giovana <f_gi@live.com>
2024-04-14 17:44:23 -0300

    feat: adicionando chamada do store dos órgãos

--- frontend/src/components/notas/ModalNotas.vue
+++ frontend/src/components/notas/ModalNotas.vue
@@ -114,6 +114,7 @@ watch(() => props.blocosToken, () => {
 }, { immediate: true });
 
 tipoStore.buscarTudo();
+ÓrgãosStore.getAll();
 
 // deveria funcionar
 watch(itemParaEdição, (novosValores) => {
