566c03eaae48b70f7295e550e299ac51d167b71b
Renato Cron <renato.cron@gmail.com>
2024-04-16 08:27:22 -0300

    garantir a exclusividade do meta_id

--- backend/prisma/manual-copy/mv_variavel_pdm.sql
+++ backend/prisma/manual-copy/mv_variavel_pdm.sql
@@ -130,7 +130,7 @@ BEGIN
   IF TG_OP = 'UPDATE' AND (OLD.removido_em IS DISTINCT FROM NEW.removido_em) THEN
     REFRESH MATERIALIZED VIEW mv_variavel_pdm;
 
-    v_meta_id := (SELECT meta_id FROM mv_variavel_pdm WHERE indicador_id = NEW.id);
+    v_meta_id := (SELECT DISTINCT meta_id FROM mv_variavel_pdm WHERE indicador_id = NEW.id);
     CALL add_refresh_meta_task(v_meta_id);
 
   END IF;
