4b5c89c70cdb92aee92adf664678fe74198a97df
ingredbarraqui <54070315+ingredbarraqui@users.noreply.github.com>
2024-04-22 11:04:35 -0300

    Fix: trocar termo "contra-medida" por "contramedida"

--- frontend/src/views/planosDeAcao/PlanosDeAcaoCriarEditar.vue
+++ frontend/src/views/planosDeAcao/PlanosDeAcaoCriarEditar.vue
@@ -1,8 +1,8 @@
 <script setup>
 import MaskedFloatInput from '@/components/MaskedFloatInput.vue';
 import { planoDeAção as schema } from '@/consts/formSchemas';
-import { useAlertStore } from '@/stores/alert.store';
 import truncate from '@/helpers/truncate';
+import { useAlertStore } from '@/stores/alert.store';
 import { useOrgansStore } from '@/stores/organs.store';
 import { usePlanosDeAçãoStore } from '@/stores/planosDeAcao.store.ts';
 import { useProjetosStore } from '@/stores/projetos.store.ts';
@@ -108,7 +108,7 @@ iniciar();
 
   <Form
     v-if="!planoId || emFoco"
-    v-slot="{ errors, isSubmitting, setFieldValue, values }"
+    v-slot="{ errors, isSubmitting, values }"
     :disabled="chamadasPendentes.emFoco"
     :initial-values="itemParaEdição"
     :validation-schema="schema"
