5e8272b5f7e7c005be62b3cec91c774741c38557
ingredbarraqui <54070315+ingredbarraqui@users.noreply.github.com>
2024-04-24 13:36:42 -0300

    Feat: filtros em Quadro de atividades

--- frontend/src/views/PanoramaTransferencias.vue
+++ frontend/src/views/PanoramaTransferencias.vue
@@ -1,14 +1,78 @@
 <script setup>
-import { onUnmounted } from 'vue';
-import { storeToRefs } from 'pinia';
 import { Dashboard } from '@/components';
+import esferasDeTransferencia from '@/consts/esferasDeTransferencia';
+import truncate from '@/helpers/truncate';
+import { useOrgansStore } from '@/stores/organs.store';
 import { usePanoramaTransferenciasStore } from '@/stores/panoramaTransferencias.store';
+import { usePartidosStore } from '@/stores/partidos.store';
+import { storeToRefs } from 'pinia';
+import { onUnmounted, ref, watch } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
 
 const panoramaTransferenciasStore = usePanoramaTransferenciasStore();
+const partidoStore = usePartidosStore();
+const OrgaosStore = useOrgansStore();
+
+const route = useRoute();
+const router = useRouter();
+
 const { chamadasPendentes, erro, lista } = storeToRefs(
   panoramaTransferenciasStore,
 );
+const { lista: partidoComoLista } = storeToRefs(partidoStore);
+const { órgãosComoLista } = storeToRefs(OrgaosStore);
+
+// não finalizado
+const esfera = ref(route.query.esfera
+  ? Object.keys(esferasDeTransferencia)
+    .find((x) => x.toLowerCase() === route.query.esfera.toLocaleLowerCase())
+  : undefined);
+const palavraChave = ref(route.query.palavra_chave);
+const partido = ref(route.query.partido_ids);
+const orgao = ref(route.query.orgaos_ids);
+
+function atualizarUrl() {
+  router.push({
+    query: {
+      ...route.query,
+      partido_ids: partido.value || undefined,
+      orgaos_ids: orgao.value || undefined,
+      esfera: esfera.value || undefined,
+      palavra_chave: palavraChave.value || undefined,
+    },
+  });
+}
+
+watch([
+  () => route.query.esfera,
+  () => route.query.partido_ids,
+  () => route.query.orgaos_ids,
+  () => route.query.palavra_chave,
+], () => {
+  const {
+    palavra_chave: palavraChaveParaBusca,
+    partido_ids: partidoFiltro,
+    orgaos_ids: orgaoFiltro,
+  } = route.query;
+  if (typeof palavraChaveParaBusca === 'string') {
+    // eslint-disable-next-line no-const-assign
+    palavraChaveParaBusca = palavraChaveParaBusca.trim();
+  }
+  panoramaTransferenciasStore.$reset();
+  panoramaTransferenciasStore.buscarTudo({
+    esfera: route.query.esfera
+      ? Object.keys(esferasDeTransferencia)
+        .find((x) => x.toLowerCase() === route.query.esfera.toLocaleLowerCase())
+      : undefined,
+    partido_ids: partidoFiltro,
+    palavra_chave: palavraChaveParaBusca,
+    orgaos_ids: orgaoFiltro,
+  });
+}, { immediate: true });
+
+OrgaosStore.getAll();
 panoramaTransferenciasStore.buscarTudo();
+partidoStore.buscarTudo();
 
 onUnmounted(() => {
   panoramaTransferenciasStore.$reset();
@@ -20,6 +84,117 @@ onUnmounted(() => {
       <h1>Quadro de atividades</h1>
       <hr class="ml2 f1">
     </div>
+    <form
+      class="flex flexwrap bottom mb2 g1"
+      @submit.prevent="atualizarUrl"
+    >
+      <div class="f1">
+        <label
+          for="esfera"
+          class="label tc300"
+        >Esfera</label>
+        <select
+          id="esfera"
+          v-model.trim="esfera"
+          class="inputtext mb1"
+          name="esfera"
+        >
+          <option value="" />
+          <option
+            v-for="item in Object.values(esferasDeTransferencia)"
+            :key="item.valor"
+            :value="item.valor"
+          >
+            {{ item.nome }}
+          </option>
+        </select>
+      </div>
+
+      <div class="f1">
+        <label
+          for="partido"
+          class="label tc300"
+        >Partido</label>
+        <select
+          id="partido"
+          v-model.trim="partido"
+          class="inputtext mb1"
+          name="partido"
+        >
+          <option value="" />
+          <option
+            v-for="item in partidoComoLista"
+            :key="item"
+            :value="item.id"
+          >
+            {{ item.nome }} - {{ item.sigla }}
+          </option>
+        </select>
+      </div>
+
+      <!-- <div class="f1">
+        <label
+          for="situacao"
+          class="label tc300"
+        >Situação</label>
+        <select
+          id="situacao"
+          v-model.trim="situacao"
+          class="inputtext mb1"
+          name="situacao"
+        >
+          <option value="" />
+          <option
+            v-for="item in lista"
+            :key="item.id"
+            :value="item.id"
+          >
+            {{ item.situacao }}
+          </option>
+        </select>
+      </div> -->
+
+      <div class="f1">
+        <label
+          for="orgao"
+          class="label tc300"
+        >orgão</label>
+        <select
+          id="orgao"
+          v-model.trim="orgao"
+          class="inputtext mb1"
+          name="orgao"
+        >
+          <option value="" />
+          <option
+            v-for="item in órgãosComoLista"
+            :key="item"
+            :value="item.id"
+            :title="item.descricao?.length > 36 ? item.descricao : null"
+          >
+            {{ item.sigla }} - {{ truncate(item.descricao, 36) }}
+          </option>
+        </select>
+      </div>
+
+      <div class="f1">
+        <label
+          for="palavra_chave"
+          class="label tc300"
+        >Palavra-chave</label>
+        <input
+          id="palavra_chave"
+          v-model.trim="palavraChave"
+          class="inputtext"
+          name="palavra_chave"
+          type="text"
+        >
+        <small class="tc200 t13">pode-se usar <strong>e</strong> ou <strong>ou</strong></small>
+      </div>
+      <button class="btn outline bgnone tcprimary mtauto mb1">
+        Pesquisar
+      </button>
+    </form>
     <div>
       <table class="tablemain mb1">
         <col>
