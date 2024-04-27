921be01354eb473f9d2503a809da573a46e8080e
Giovana <f_gi@live.com>
2024-04-27 20:28:16 -0300

    feat: adicionando tela de detalhe da nota

---
+++ frontend/src/components/notas/NotaDetalhe.vue
@@ -0,0 +1,86 @@
+<script setup>
+import { useBlocoDeNotasStore } from '@/stores/blocoNotas.store';
+import { storeToRefs } from 'pinia';
+
+const blocoStore = useBlocoDeNotasStore();
+const { emFoco } = storeToRefs(blocoStore);
+const props = defineProps({
+  notaId: {
+    type: String,
+    default: '',
+  },
+});
+if (props.notaId) {
+  blocoStore.buscarItem(props.notaId);
+}
+</script>
+<template>
+  <div class="flex spacebetween center mb2">
+    <h1>Nota</h1>
+    <hr class="ml2 f1">
+    <router-link
+      v-if="emFoco?.id_jwt && emFoco?.pode_editar"
+      :to="{ name: 'notasEditar', params: { notaId: emFoco.id_jwt } }"
+      class="btn big ml2"
+    >
+      Editar
+    </router-link>
+  </div>
+  <div
+    class="mb1"
+    v-html="emFoco?.nota"
+  />
+  <div class="flex spacebetween center">
+    <h3>Detalhes</h3>
+    <hr class="ml2 f1">
+  </div>
+  <dl class="flex mb2 flexwrap g2">
+    <div class="f1">
+      <dt>Data</dt>
+      <dd>{{ new Date(emFoco?.data_nota).toLocaleDateString("pt-BR") }}</dd>
+    </div>
+    <div class="f1">
+      <dt>Status</dt>
+      <dd>{{ emFoco?.status }}</dd>
+    </div>
+    <div class="f1">
+      <dt>Órgão responsável</dt>
+      <dd>{{ emFoco?.orgao_responsavel?.sigla }}</dd>
+    </div>
+    <div class="f1">
+      <dt>Data de ordenacao</dt>
+      <dd>
+        {{
+          emFoco?.data_ordenacao
+            ? new Date(emFoco?.data_ordenacao).toLocaleDateString("pt-BR")
+            : " - "
+        }}
+      </dd>
+    </div>
+    <div class="f1">
+      <dt>Pessoa responsável</dt>
+      <dd>{{ emFoco?.pessoa_responsavel?.nome_exibicao }}</dd>
+    </div>
+    <div class="f1">
+      <dt>Endereçamentos</dt>
+      <dd>{{ emFoco?.enderecamentos.length ? emFoco?.enderecamentos : " - " }}</dd>
+    </div>
+    <div class="f1">
+      <dt>Rever em</dt>
+      <dd>{{ emFoco?.rever_em ? emFoco.rever_em : " - " }}</dd>
+    </div>
+  </dl>
+</template>
+<style scoped>
+h3 {
+  font-weight: 600;
+}
+dl div {
+  min-width: 300px;
+}
+
+dl div dt {
+  color: #607a9f;
+  font-weight: 600;
+}
+</style>
