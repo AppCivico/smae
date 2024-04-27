430e27fbe538f9dd74a40fb7a40e3dd3ef43bcd7
Giovana <f_gi@live.com>
2024-04-27 15:33:53 -0300

    fix: refatorando listagem de notas

--- frontend/src/components/notas/NotasLista.vue
+++ frontend/src/components/notas/NotasLista.vue
@@ -1,20 +1,14 @@
 <script setup>
-import SmallModal from '@/components/SmallModal.vue';
-import { nota as schema } from '@/consts/formSchemas';
-import truncate from '@/helpers/truncate';
 import { useAlertStore } from '@/stores/alert.store';
 import { useBlocoDeNotasStore } from '@/stores/blocoNotas.store';
-import { useOrgansStore } from '@/stores/organs.store';
 import { useTipoDeNotasStore } from '@/stores/tipoNotas.store';
 import { useTransferenciasVoluntariasStore } from '@/stores/transferenciasVoluntarias.store';
-import { useUsersStore } from '@/stores/users.store';
 import { storeToRefs } from 'pinia';
-import {
-  ErrorMessage, Field, FieldArray, useForm,
-} from 'vee-validate';
-import { computed, ref, watch } from 'vue';
 
-const alertStore = useAlertStore();
+import {
+  computed, ref, watch, onMounted,
+} from 'vue';
+
 const status = {
   Programado: {
     value: 'Programado',
@@ -40,30 +34,12 @@ const props = defineProps({
     default: 0,
   },
 });
+
 const TransferenciasVoluntarias = useTransferenciasVoluntariasStore();
 const { emFoco: transferênciaEmFoco } = storeToRefs(TransferenciasVoluntarias);
 
-const ÓrgãosStore = useOrgansStore();
-const { organs, órgãosComoLista } = storeToRefs(ÓrgãosStore);
-
-const UserStore = useUsersStore();
-const { pessoasSimplificadas, pessoasSimplificadasPorÓrgão } = storeToRefs(UserStore);
-
 const blocoStore = useBlocoDeNotasStore();
-const {
-  lista: listaNotas,
-  erro,
-  chamadasPendentes,
-  emFoco,
-  itemParaEdição,
-} = storeToRefs(blocoStore);
-
-const {
-  errors, handleSubmit, isSubmitting, resetForm, setFieldValue, values,
-} = useForm({
-  initialValues: itemParaEdição,
-  validationSchema: schema,
-});
+const { lista: listaNotas, erro, chamadasPendentes } = storeToRefs(blocoStore);
 
 const tipoStore = useTipoDeNotasStore();
 const { lista: listaTipo } = storeToRefs(tipoStore);
@@ -71,29 +47,18 @@ const { lista: listaTipo } = storeToRefs(tipoStore);
 const statusSelecionado = ref('');
 // eslint-disable-next-line prefer-const
 let exibeForm = ref(false);
-const tipoNotaId = ref(null);
 
-const blocosToken = computed(
-  () => transferênciaEmFoco?.value?.bloco_nota_token,
-);
+const blocosToken = ref([]);
 
-const camposPermitidos = computed(() => ({
-  email: listaTipo.value
-    .find((tipo) => tipo.id === tipoNotaId.value && tipo.permite_email),
-  revisao: listaTipo.value
-    .find((tipo) => tipo.id === tipoNotaId.value && tipo.permite_revisao),
-  enderecamento: listaTipo.value
-    .find((tipo) => tipo.id === tipoNotaId.value && tipo.permite_enderecamento),
-}));
-
-const podeEditarDisponivel = computed(() => listaNotas.value.some((item) => item.pode_editar));
-
-async function iniciar() {
-  if (props.transferenciaId !== transferênciaEmFoco?.value?.id) {
-    await TransferenciasVoluntarias.buscarItem(props.transferenciaId);
+onMounted(async () => {
+  await TransferenciasVoluntarias.buscarItem(props.transferenciaId);
+  if (props.transferenciaId && transferênciaEmFoco.value) {
+    blocosToken.value = transferênciaEmFoco.value.bloco_nota_token;
+    tipoStore.buscarTudo();
+    blocoStore.buscarTudo(blocosToken?.value);
   }
-  blocoStore.buscarTudo(blocosToken.value);
-}
+});
+const podeEditarDisponivel = computed(() => listaNotas.value.some((item) => item.pode_editar));
 
 async function excluirNota(id) {
   useAlertStore().confirmAction(
@@ -113,317 +78,23 @@ function editarNota(id) {
   exibeForm.value = true;
   blocoStore.buscarItem(id);
 }
-
-function fecharModal() {
-  emFoco.value = null;
-  exibeForm.value = false;
-}
-
-const onSubmit = handleSubmit.withControlled(async (valoresControlados) => {
-  const valoresAuxiliares = {
-    ...values,
-    bloco_token: blocosToken.value,
-    id: itemParaEdição.value.id_jwt ? itemParaEdição.value.id_jwt : undefined,
-  };
-  try {
-    const msg = 'Dados salvos com sucesso!';
-    const resposta = await blocoStore.salvarItem(valoresAuxiliares);
-    if (resposta) {
-      alertStore.success(msg);
-      blocoStore.$reset();
-      blocoStore.buscarTudo(blocosToken.value);
-      resetForm({ values: itemParaEdição.value });
-      exibeForm.value = false;
-    }
-  } catch (error) {
-    alertStore.error(error);
-  }
-});
-
 watch(statusSelecionado, (novoValor) => {
   blocoStore.buscarTudo(blocosToken.value, { status: novoValor });
 });
 
-ÓrgãosStore.getAll();
-tipoStore.buscarTudo();
-UserStore.buscarPessoasSimplificadas();
-iniciar();
 </script>
 
 <template>
   <div class="flex spacebetween center mb2">
-    <h2>Notas</h2>
+    <h1>{{ $route?.meta?.título || "Notas" }}</h1>
     <hr class="ml2 f1">
-    <button
-      class="btn ml2"
-      @click="exibeForm = true"
+    <router-link
+      :to="{ name: 'notasCriar' }"
+      class="btn big ml1"
     >
       Nova nota
-    </button>
+    </router-link>
   </div>
-  <SmallModal v-if="exibeForm">
-    <div class="flex spacebetween center mb2">
-      <h2 v-if="itemParaEdição.id_jwt">
-        Editar nota
-      </h2>
-      <h2 v-else>
-        Nova nota
-      </h2>
-      <hr class="ml2 f1">
-      <CheckClose
-        :apenas-emitir="true"
-        @close="fecharModal()"
-      />
-    </div>
-    <div class="mb4">
-      <form @submit.prevent="onSubmit">
-        <div class="flex mb2 flexwrap g2">
-          <div class="f1">
-            <LabelFromYup
-              name="status"
-              :schema="schema"
-            />
-            <Field
-              name="status"
-              as="select"
-              class="inputtext light mb1"
-            >
-              <option value>
-                Selecionar
-              </option>
-              <option
-                v-for="(item, key) in Object.values(status)"
-                :key="key"
-                :value="item.value"
-              >
-                {{ item.text }}
-              </option>
-            </Field>
-          </div>
-          <div class="f1">
-            <LabelFromYup
-              name="data_nota"
-              :schema="schema"
-            />
-            <Field
-              name="data_nota"
-              type="date"
-              class="inputtext light"
-              placeholder="dd/mm/aaaa"
-            />
-          </div>
-          <div class="f1">
-            <LabelFromYup
-              name="tipo_nota_id"
-              :schema="schema"
-            />
-            <Field
-              v-model="tipoNotaId"
-              name="tipo_nota_id"
-              as="select"
-              class="inputtext light mb1"
-            >
-              <option value>
-                Selecionar
-              </option>
-              <option
-                v-for="(tipo, key) in listaTipo"
-                :key="key"
-                :value="tipo.id"
-              >
-                {{ tipo.codigo }}
-              </option>
-            </Field>
-          </div>
-        </div>
-        <div class="flex mb1">
-          <div
-            v-if="camposPermitidos.email"
-            class="f1"
-          >
-            <LabelFromYup
-              name="dispara_email"
-              :schema="schema"
-            />
-            <Field
-              name="dispara_email"
-              type="checkbox"
-              class="inputcheckbox"
-              :value="true"
-              :unchecked-value="false"
-            />
-          </div>
-          <Field
-            v-else
-            name="dispara_email"
-            type="hidden"
-          />
-          <div
-            v-if="camposPermitidos.revisao"
-            class="f1"
-          >
-            <LabelFromYup
-              name="rever_em"
-              :schema="schema"
-            />
-            <Field
-              name="rever_em"
-              type="date"
-              class="inputtext light"
-              placeholder="dd/mm/aaaa"
-            />
-          </div>
-        </div>
-
-        <div
-          v-if="camposPermitidos.enderecamento"
-          class="mb2"
-        >
-          <LabelFromYup
-            class="label mt2 mb1"
-            as="legend"
-            name="enderecamentos"
-            :schema="schema"
-          />
-
-          <FieldArray
-            v-slot="{ fields, push, remove }"
-            name="enderecamentos"
-          >
-            <div
-              v-for="(field, idx) in fields"
-              :key="field.key"
-              class="flex g2 mb1"
-            >
-              <div class="f1">
-                <LabelFromYup
-                  name="orgao_enderecado_id"
-                  :schema="schema.fields.enderecamentos.innerType"
-                />
-                <Field
-                  :name="`enderecamentos[${idx}].orgao_enderecado_id`"
-                  as="select"
-                  class="inputtext light mb1"
-                  :class="{
-                    error: errors[`enderecamentos[${idx}].orgao_enderecado_id`],
-                    loading: organs?.loading
-                  }"
-                  :disabled="!órgãosComoLista?.length"
-                  @change="setFieldValue(`enderecamentos[${idx}].pessoa_enderecado_id`, null)"
-                >
-                  <option value="" />
-                  <option
-                    v-for="item in órgãosComoLista"
-                    :key="item"
-                    :value="item.id"
-                    :title="item.descricao?.length > 36 ? item.descricao : null"
-                  >
-                    {{ item.sigla }} - {{ truncate(item.descricao, 36) }}
-                  </option>
-                </Field>
-                <ErrorMessage
-                  class="error-msg mb2"
-                  :name="`enderecamentos[${idx}].orgao_enderecado_id`"
-                />
-              </div>
-
-              <div class="f1">
-                <LabelFromYup
-                  name="pessoa_enderecado_id"
-                  :schema="schema.fields.enderecamentos.innerType"
-                />
-                <Field
-                  :name="`enderecamentos[${idx}].pessoa_enderecado_id`"
-                  as="select"
-                  class="inputtext light mb1"
-                  :class="{
-                    error: errors[`enderecamentos[${idx}].pessoa_enderecado_id`],
-                    loading: pessoasSimplificadas?.loading
-                  }"
-                  :disabled="
-                    !pessoasSimplificadasPorÓrgão[values.enderecamentos?.[idx]?.orgao_enderecado_id]?.length
-                  "
-                >
-                  <option value="" />
-                  <option
-                    v-for="item in pessoasSimplificadasPorÓrgão[values.enderecamentos?.[idx]?.orgao_enderecado_id]"
-                    :key="item"
-                    :value="item.id"
-                  >
-                    {{ item.nome_exibicao }}
-                  </option>
-                </Field>
-                <ErrorMessage
-                  class="error-msg mb2"
-                  :name="`enderecamentos[${idx}].pessoa_enderecado_id`"
-                />
-              </div>
-
-              <button
-                class="like-a__text addlink"
-                arial-label="excluir"
-                title="excluir"
-                @click="remove(idx)"
-              >
-                <svg
-                  width="20"
-                  height="20"
-                ><use xlink:href="#i_remove" /></svg>
-              </button>
-            </div>
-
-            <button
-              class="like-a__text addlink"
-              type="button"
-              @click="push({
-                orgao_enderecado_id: null,
-                pessoa_enderecado_id: null,
-              })"
-            >
-              <svg
-                width="20"
-                height="20"
-              ><use xlink:href="#i_+" /></svg>
-              Adicionar endereçamento
-            </button>
-          </FieldArray>
-        </div>
-
-        <div class="mb2">
-          <LabelFromYup
-            name="nota"
-            :schema="schema"
-          />
-          <Field
-            name="nota"
-            type="textarea"
-            class="inputtext light mb1"
-            as="textarea"
-            rows="10"
-          />
-        </div>
-        <FormErrorsList
-          :errors="errors"
-          class="mb1"
-        />
-        <div class="flex spacebetween center mb2">
-          <hr class="mr2 f1">
-          <button
-            class="btn big"
-            :disabled="isSubmitting || Object.keys(errors)?.length"
-            :title="
-              Object.keys(errors)?.length
-                ? `Erros de preenchimento: ${Object.keys(errors)?.length}`
-                : null
-            "
-          >
-            Salvar
-          </button>
-          <hr class="ml2 f1">
-        </div>
-      </form>
-    </div>
-  </SmallModal>
 
   <div class="mb1">
     <label class="label">Filtrar por status</label>
@@ -443,7 +114,6 @@ iniciar();
       </option>
     </select>
   </div>
-
   <table class="tablemain mb1 tbody-zebra">
     <col>
     <col>
@@ -488,7 +158,7 @@ iniciar();
         <td>
           {{
             item.rever_em
-              ? new Date( item.rever_em).toLocaleDateString("pt-BR")
+              ? new Date(item.rever_em).toLocaleDateString("pt-BR")
               : " - "
           }}
         </td>
