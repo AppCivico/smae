cbdd3c117319f7c1dbece3e727cd509975f119ba
Giovana <f_gi@live.com>
2024-05-02 20:19:12 -0300

    refactor: reestruturando text editor

--- frontend/src/components/TextEditor.vue
+++ frontend/src/components/TextEditor.vue
@@ -1,52 +1,94 @@
 <script setup>
-  import { ref } from 'vue';
-  import { useEditor, EditorContent } from '@tiptap/vue-3'
-  import StarterKit from '@tiptap/starter-kit'
-  
-  const props = defineProps(['class','modelValue']);
-  const emit = defineEmits(['update:modelValue']);
-  const editor = useEditor({
-    content: props.modelValue,
+import {
+  ref, watch, onMounted, onBeforeUnmount, defineOptions,
+} from 'vue';
+import StarterKit from '@tiptap/starter-kit';
+import { Editor, EditorContent } from '@tiptap/vue-3';
+
+defineOptions({ inheritAttrs: false });
+
+const props = defineProps({
+  modelValue: {
+    type: String,
+    default: '',
+  },
+});
+const emit = defineEmits(['update:modelValue']);
+const editor = ref(null);
+
+onMounted(() => {
+  editor.value = new Editor({
     extensions: [
       StarterKit,
     ],
-    onUpdate: ({editor}) => {
-      emit('update:modelValue', editor.getHTML())
-    },
-    onCreate: ({editor}) =>{
-      editor.commands.setContent(props.modelValue);
+    content: props.modelValue,
+    onUpdate: () => {
+      emit('update:modelValue', editor.value.getHTML());
     },
   });
+});
+
+onBeforeUnmount(() => {
+  editor.value.destroy();
+});
+
+watch(() => props.modelValue, (value) => {
+  const isSame = editor.value.getHTML() === value;
+  if (!isSame) {
+    editor.value.commands.setContent(value, false);
+  }
+});
 </script>
 <template>
-  <div class="menueditor" v-if="editor">
-    <a @click="editor.chain().focus().toggleBold().run()" 
-      :disabled="!editor.can().chain().focus().toggleBold().run()" 
+  <div
+    v-if="editor"
+    class="menueditor"
+  >
+    <a
+      :disabled="!editor.can().chain().focus().toggleBold().run()"
       class="editorbt w700"
-      :class="{ 'is-active': editor.isActive('bold') }">N</a>
-    <a @click="editor.chain().focus().toggleItalic().run()" 
-      :disabled="!editor.can().chain().focus().toggleItalic().run()" 
+      :class="{ 'is-active': editor.isActive('bold') }"
+      @click="editor.chain().focus().toggleBold().run()"
+    >N</a>
+    <a
+      :disabled="!editor.can().chain().focus().toggleItalic().run()"
       class="editorbt i"
-      :class="{ 'is-active': editor.isActive('italic') }">It</a>
-    <a @click="editor.chain().focus().toggleStrike().run()" 
-      :disabled="!editor.can().chain().focus().toggleStrike().run()" 
-      class="editorbt c" 
-      :class="{ 'is-active': editor.isActive('strike') }">S</a>
+      :class="{ 'is-active': editor.isActive('italic') }"
+      @click="editor.chain().focus().toggleItalic().run()"
+    >It</a>
+    <a
+      :disabled="!editor.can().chain().focus().toggleStrike().run()"
+      class="editorbt c"
+      :class="{ 'is-active': editor.isActive('strike') }"
+      @click="editor.chain().focus().toggleStrike().run()"
+    >S</a>
 
-    <a @click="editor.chain().focus().toggleBulletList().run()" 
+    <a
       class="editorbt"
-      :class="{ 'is-active': editor.isActive('bulletList') }">&bull;</a>
-    <a @click="editor.chain().focus().toggleOrderedList().run()" 
+      :class="{ 'is-active': editor.isActive('bulletList') }"
+      @click="editor.chain().focus().toggleBulletList().run()"
+    >&bull;</a>
+    <a
       class="editorbt"
-      :class="{ 'is-active': editor.isActive('orderedList') }">1.</a>
-    <a @click="editor.chain().focus().setHardBreak().run()" 
-      class="editorbt c" >&crarr;</a>
+      :class="{ 'is-active': editor.isActive('orderedList') }"
+      @click="editor.chain().focus().toggleOrderedList().run()"
+    >1.</a>
+    <a
+      class="editorbt c"
+      @click="editor.chain().focus().setHardBreak().run()"
+    >&crarr;</a>
 
-    <a class="editorbt" @click="editor.chain().focus().unsetAllMarks().clearNodes().run()">
+    <a
+      class="editorbt"
+      @click="editor.chain().focus().unsetAllMarks().clearNodes().run()"
+    >
       Limpar formatação
     </a>
   </div>
-  <div class="contentStyle" :class="class">
+  <div
+    class="contentStyle"
+    v-bind="$attrs"
+  >
     <editor-content :editor="editor" />
   </div>
 </template>
