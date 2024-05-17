a3ecbaa55d826787909e253daf80223a65e32aed
Renato Cron <renato.cron@gmail.com>
2024-05-17 17:45:25 -0300

    Melhorando tratamento do escaping da exception do banco

--- backend/src/prisma-error-unknown.filter.ts
+++ backend/src/prisma-error-unknown.filter.ts
@@ -1,17 +1,21 @@
 import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import { Response } from 'express';
+import { AllExceptionsFilter } from './any-error.filter';
+import { HttpAdapterHost } from '@nestjs/core';
 
 // Tratando erro de dentro de trigger: eg função f_tgr_atualiza_variavel_na_troca_da_etapa
 @Catch(Prisma.PrismaClientUnknownRequestError)
 export class PrismaErrorFilterUnknown implements ExceptionFilter {
+    constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
+
     catch(exception: Prisma.PrismaClientUnknownRequestError, host: ArgumentsHost) {
         const ctx = host.switchToHttp();
         const response = ctx.getResponse<Response>();
 
         const jsonMatch = exception.message.match(/__HTTP__ (.+?) __EOF__/);
         if (jsonMatch && jsonMatch[1]) {
-            const jsonString = jsonMatch[1].replace(/\\"/g, '"');
+            const jsonString = jsonMatch[1].replace(/\\"/g, '"').replace(/\\\\/g, '\\');
 
             console.log('Detected and extracted JSON from a trigger:', jsonString);
 
@@ -20,11 +24,14 @@ export class PrismaErrorFilterUnknown implements ExceptionFilter {
                 console.log('Extracted JSON:', jsonObject);
 
                 response.status(jsonObject?.code ?? 400).json(jsonObject);
+                return;
             } catch (error) {
                 console.error('Failed to parse JSON:', error);
             }
         }
 
-        throw exception;
+        console.error('redirecting to AllExceptionsFilter');
+        const tmp = new AllExceptionsFilter(this.httpAdapterHost);
+        tmp.catch(exception, host);
     }
 }
