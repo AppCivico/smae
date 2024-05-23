75c45489a10d96e3c49ed7028b9666f28c74987c
lucasansei <lucas.ansei@eokoe.com>
2024-05-23 10:19:56 -0300

    Movendo legenda para esquerda

--- backend/src/casa-civil/dash/dto/transferencia.dto.ts
+++ backend/src/casa-civil/dash/dto/transferencia.dto.ts
@@ -192,7 +192,7 @@ export class ChartLegendDto {
     layout?: string;
     align?: string;
     verticalAlign?: string;
-    top: number;
+    left: number;
     orient: string;
     textStyle?: {
         overflow: string;
