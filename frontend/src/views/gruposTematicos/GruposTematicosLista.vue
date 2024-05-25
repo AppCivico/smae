5a936f8d2c23552bbc7e7e30b0d4bcb1cb97ef79
Giovana <f_gi@live.com>
2024-05-25 11:22:54 -0300

    wip: adicionando conteudo a listagem, criação e edição de grupos temáticos

--- frontend/src/views/gruposTematicos/GruposTematicosLista.vue
+++ frontend/src/views/gruposTematicos/GruposTematicosLista.vue
@@ -1,13 +1,102 @@
 <template>
-  <div>
-    <h1>Grupos temáticos</h1>
+  <div class="flex spacebetween center mb2">
+    <h1>{{ route?.meta?.título || "Grupos temáticos" }}</h1>
+    <hr class="ml2 f1">
+    <router-link
+      :to="{ name: 'grupoTematicoCriar' }"
+      class="btn big ml1"
+    >
+      Novo grupo
+    </router-link>
   </div>
+  <table class="tablemain">
+    <col>
+    <col class="col--botão-de-ação">
+    <col class="col--botão-de-ação">
+    <thead>
+      <tr>
+        <th>Nome</th>
+        <th />
+        <th />
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="item in lista"
+        :key="item.id"
+      >
+        <td>{{ item.nome }}</td>
+        <td>
+          <router-link
+            :to="{ name: 'grupoTematicoEditar', params: { grupoTematicoId: item.id } }"
+            class="tprimary"
+          >
+            <svg
+              width="20"
+              height="20"
+            ><use xlink:href="#i_edit" /></svg>
+          </router-link>
+        </td>
+        <td>
+          <button
+            class="like-a__text"
+            arial-label="excluir"
+            title="excluir"
+            @click="excluirGrupoTematico(item.id)"
+          >
+            <svg
+              width="20"
+              height="20"
+            ><use xlink:href="#i_remove" /></svg>
+          </button>
+        </td>
+      </tr>
+      <tr v-if="chamadasPendentes.lista">
+        <td colspan="3">
+          Carregando
+        </td>
+      </tr>
+      <tr v-else-if="erro">
+        <td colspan="3">
+          Erro: {{ erro }}
+        </td>
+      </tr>
+      <tr v-else-if="!lista.length">
+        <td colspan="3">
+          Nenhum resultado encontrado.
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script setup>
+import { useAlertStore } from '@/stores/alert.store';
+import { storeToRefs } from 'pinia';
+import { useRoute } from 'vue-router';
+import { useGruposTematicosStore } from '@/stores/gruposTematicos.store';
 
+const route = useRoute();
+const alertStore = useAlertStore();
+const gruposTematicosStore = useGruposTematicosStore();
+const { lista, chamadasPendentes, erro } = storeToRefs(gruposTematicosStore);
+
+async function excluirGrupoTematico(id) {
+  alertStore.confirmAction(
+    'Deseja mesmo remover esse item?',
+    async () => {
+      if (await gruposTematicosStore.excluirItem(id)) {
+        gruposTematicosStore.$reset();
+        gruposTematicosStore.buscarTudo();
+        alertStore.success('Grupo removido.');
+      }
+    },
+    'Remover',
+  );
+}
+
+gruposTematicosStore.$reset();
+gruposTematicosStore.buscarTudo();
 </script>
 
-<style>
-
-</style>
+<style></style>
