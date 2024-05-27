7e5cb31df68b7fec9546aef3a09b205187e688e1
Sobral <robson@oficiodesign.com>
2024-05-27 12:00:34 -0300

    feat: Adicionar barra de chamadas pendentes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,6 +2,7 @@
 import { Alert, EditModal, SideBar } from '@/components';
 import { useAuthStore } from '@/stores/auth.store';
 import { provide } from 'vue';
+import BarraDePendência from './components/BarraDeChamadaPendente.vue';
 
 const gblLimiteDeSeleçãoSimultânea = Number.parseInt(
   import.meta.env.VITE_LIMITE_SELECAO,
@@ -17,6 +18,7 @@ if (authStore.estouAutenticada) {
 }
 </script>
 <template>
+  <BarraDePendência />
   <!-- vamos avançar até essa chave ser desnecessária para o sistema todo -->
   <router-view v-if="$route.meta.rotaPrescindeDeChave ?? gblHabilitarBeta" />
   <router-view
