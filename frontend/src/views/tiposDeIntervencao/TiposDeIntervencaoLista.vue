86ae410e23e4406f34f97d3cf45e0ee90b1d398b
Giovana <f_gi@live.com>
2024-05-28 10:11:42 -0300

    feat: adicionando telas do crud de tipos de intervenção

---
+++ frontend/src/views/tiposDeIntervencao/TiposDeIntervencaoLista.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="flex spacebetween center mb2">
+    <h1>{{ route?.meta?.título || "Tipos de intervenção" }}</h1>
+    <hr class="ml2 f1">
+    <router-link
+      :to="{ name: 'tiposDeIntervencaoCriar' }"
+      class="btn big ml1"
+    >
+      Novo tipo
+    </router-link>
+  </div>
+  <table class="tablemain">
+    <col>
+    <col class="col--botão-de-ação">
+    <col class="col--botão-de-ação">
+    <thead>
+      <tr>
+        <th>Tipo</th>
+        <th />
+        <th />
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="item in lista"
+        :key="item.id"
+      >
+        <td>{{ item.nome }}</td>
+        <td>
+          <router-link
+            :to="{ name: 'tiposDeIntervencaoEditar', params: { tipoDeIntervencaoId: item.id } }"
+            class="tprimary"
+          >
+            <svg
+              width="20"
+              height="20"
+            ><use xlink:href="#i_edit" /></svg>
+          </router-link>
+        </td>
+        <td>
+          <button
+            class="like-a__text"
+            arial-label="excluir"
+            title="excluir"
+            @click="excluirGrupoTematico(item.id)"
+          >
+            <svg
+              width="20"
+              height="20"
+            ><use xlink:href="#i_remove" /></svg>
+          </button>
+        </td>
+      </tr>
+      <tr v-if="chamadasPendentes.lista">
+        <td colspan="3">
+          Carregando
+        </td>
+      </tr>
+      <tr v-else-if="erro">
+        <td colspan="3">
+          Erro: {{ erro }}
+        </td>
+      </tr>
+      <tr v-else-if="!lista.length">
+        <td colspan="3">
+          Nenhum resultado encontrado.
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script setup>
+import { useAlertStore } from '@/stores/alert.store';
+import { storeToRefs } from 'pinia';
+import { useRoute } from 'vue-router';
+import { useTiposDeIntervencaoStore } from '@/stores/tiposDeIntervencao.store';
+
+const route = useRoute();
+const alertStore = useAlertStore();
+const tiposDeIntervencaoStore = useTiposDeIntervencaoStore();
+const { lista, chamadasPendentes, erro } = storeToRefs(tiposDeIntervencaoStore);
+
+async function excluirGrupoTematico(id) {
+  alertStore.confirmAction(
+    'Deseja mesmo remover esse item?',
+    async () => {
+      if (await tiposDeIntervencaoStore.excluirItem(id)) {
+        tiposDeIntervencaoStore.$reset();
+        tiposDeIntervencaoStore.buscarTudo();
+        alertStore.success('Grupo removido.');
+      }
+    },
+    'Remover',
+  );
+}
+
+tiposDeIntervencaoStore.$reset();
+tiposDeIntervencaoStore.buscarTudo();
+</script>
+
+<style></style>
