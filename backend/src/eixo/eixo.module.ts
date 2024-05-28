a4453a880642cd9e8419faee2a8e4bc16be075c0
lucasansei <lucas.ansei@eokoe.com>
2024-05-28 09:29:37 -0300

    Adicionando macrotema para plano setorial

--- backend/src/eixo/eixo.module.ts
+++ backend/src/eixo/eixo.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
-import { EixoController, EixoController2 } from './eixo.controller';
+import { EixoController, EixoController2, PlanoSetorialEixoController } from './eixo.controller';
 import { EixoService } from './eixo.service';
 
 @Module({
     imports: [PrismaModule],
-    controllers: [EixoController, EixoController2],
+    controllers: [EixoController, EixoController2, PlanoSetorialEixoController],
     providers: [EixoService],
     exports: [EixoService],
 })
