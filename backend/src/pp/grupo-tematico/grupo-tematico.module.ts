9d7e1ddcee56549af789b365e40d8ee4c1b6f5ba
lucasansei <lucas.ansei@eokoe.com>
2024-06-04 15:30:37 -0300

    Removendo modulo de projeto de import

--- backend/src/pp/grupo-tematico/grupo-tematico.module.ts
+++ backend/src/pp/grupo-tematico/grupo-tematico.module.ts
@@ -1,11 +1,10 @@
-import { Module, forwardRef } from '@nestjs/common';
-import { ProjetoModule } from '../projeto/projeto.module';
+import { Module } from '@nestjs/common';
 import { GrupoTematicoController } from './grupo-tematico.controller';
 import { GrupoTematicoService } from './grupo-tematico.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
-    imports: [PrismaModule, forwardRef(() => ProjetoModule)],
+    imports: [PrismaModule],
     controllers: [GrupoTematicoController],
     providers: [GrupoTematicoService],
 })
