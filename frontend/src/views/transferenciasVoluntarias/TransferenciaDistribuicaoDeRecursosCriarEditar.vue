96964037cfa7ead128ed17738afea829beb272f8
Sobral <robson@oficiodesign.com>
2024-06-06 18:32:55 -0300

    feat: Permitir rolagem de tabela de distribuição de recursos

--- frontend/src/views/transferenciasVoluntarias/TransferenciaDistribuicaoDeRecursosCriarEditar.vue
+++ frontend/src/views/transferenciasVoluntarias/TransferenciaDistribuicaoDeRecursosCriarEditar.vue
@@ -173,94 +173,100 @@ const isSomaCorreta = computed(() => {
   </div>
 
   <div class="mb2">
-    <table class="tablemain mb1">
-      <col>
-      <col class="col--number">
-      <col class="col--data">
-      <col>
-      <col class="col--botão-de-ação">
-      <col class="col--botão-de-ação">
-      <thead>
-        <tr>
-          <th>
-            Gestor municipal
-          </th>
-          <th class="cell--number">
-            Valor total
-          </th>
-          <th class="cell--data">
-            Data de vigência
-          </th>
-          <th>
-            Nome
-          </th>
-          <th />
-          <th />
-        </tr>
-      </thead>
-      <tbody>
-        <tr
-          v-for="item in lista"
-          :key="item.id"
-        >
-          <td>{{ item.orgao_gestor.sigla }}</td>
-          <td class="cell--number">
-            {{
-              item.valor_total
-                ? dinheiro(item.valor_total )
-                : '-'
-            }}
-          </td>
-          <td class="cell--data">
-            {{ dateToField(item.vigencia) }}
-          </td>
-          <td>{{ item.nome || '-' }}</td>
-          <td>
-            <button
-              class="like-a__text"
-              arial-label="excluir"
-              title="excluir"
-              type="button"
-              @click="excluirDistribuição(item.id)"
-            >
-              <svg
-                width="20"
-                height="20"
-              ><use xlink:href="#i_remove" /></svg>
-            </button>
-          </td>
-          <td>
-            <button
-              class="like-a__text"
-              arial-label="editar"
-              title="editar"
-              type="button"
-              @click="editarDistribuicaoRecursos(item.id)"
-            >
-              <svg
-                width="20"
-                height="20"
-              ><use xlink:href="#i_edit" /></svg>
-            </button>
-          </td>
-        </tr>
-
-        <tr v-if="chamadasPendentes.lista">
-          <td
-            colspan="6"
-            class="loading"
+    <div
+      role="region"
+      aria-label="Distribuições de recursos já cadastradas"
+      tabindex="0"
+      class="mb1"
+    >
+      <table class="tablemain mb1">
+        <col>
+        <col class="col--number">
+        <col class="col--data">
+        <col>
+        <col class="col--botão-de-ação">
+        <col class="col--botão-de-ação">
+        <thead>
+          <tr>
+            <th>
+              Gestor municipal
+            </th>
+            <th class="cell--number">
+              Valor total
+            </th>
+            <th class="cell--data">
+              Data de vigência
+            </th>
+            <th>
+              Nome
+            </th>
+            <th />
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="item in lista"
+            :key="item.id"
           >
-            carregando
-          </td>
-        </tr>
+            <td>{{ item.orgao_gestor.sigla }}</td>
+            <td class="cell--number">
+              {{ item.valor_total
+                ? dinheiro(item.valor_total)
+                : '-' }}
+            </td>
+            <td class="cell--data">
+              {{ dateToField(item.vigencia) }}
+            </td>
+            <td>{{ item.nome || '-' }}</td>
+            <td>
+              <button
+                class="like-a__text"
+                arial-label="excluir"
+                title="excluir"
+                type="button"
+                @click="excluirDistribuição(item.id)"
+              >
+                <svg
+                  width="20"
+                  height="20"
+                ><use xlink:href="#i_remove" /></svg>
+              </button>
+            </td>
+            <td>
+              <button
+                class="like-a__text"
+                arial-label="editar"
+                title="editar"
+                type="button"
+                @click="editarDistribuicaoRecursos(item.id)"
+              >
+                <svg
+                  width="20"
+                  height="20"
+                ><use xlink:href="#i_edit" /></svg>
+              </button>
+            </td>
+          </tr>
 
-        <tr v-else-if="!lista.length">
-          <td colspan="6">
-            Nenhum Registro de Distribuição de Recursos encontrado.
-          </td>
-        </tr>
-      </tbody>
-    </table>
+          <tr v-if="chamadasPendentes.lista">
+            <td
+              colspan="6"
+              class="loading"
+            >
+              carregando
+            </td>
+          </tr>
+
+          <tr v-else-if="!lista.length">
+            <td colspan="6">
+              Nenhum Registro de Distribuição de Recursos encontrado.
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
     <button
       class="like-a__text addlink"
       @click="registrarNovaDistribuicaoRecursos"
