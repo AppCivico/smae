c7831b621d506f1a501c1b8f444e6fd02388eb34
lucasansei <lucas.ansei@eokoe.com>
2024-06-06 19:30:08 -0300

    Fix: verificando se atualmente é null

--- backend/src/distribuicao-recurso/distribuicao-recurso.service.ts
+++ backend/src/distribuicao-recurso/distribuicao-recurso.service.ts
@@ -376,7 +376,7 @@ export class DistribuicaoRecursoService {
                     throw new HttpException('nome| Nome igual ou semelhante já existe em outro registro ativo', 400);
             }
 
-            if (dto.vigencia && dto.vigencia != self.vigencia) {
+            if (dto.vigencia && self.vigencia != null && dto.vigencia != self.vigencia) {
                 if (!dto.justificativa_aditamento)
                     throw new HttpException('justificativa_aditamento| Deve ser enviada.', 400);
 
