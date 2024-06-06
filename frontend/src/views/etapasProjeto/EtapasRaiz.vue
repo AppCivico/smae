2034ced4c578eefb4163f4381c36214228573625
Sobral <robson@oficiodesign.com>
2024-06-06 11:27:00 -0300

    refactor: Exigir escolha de módulo ao listar etapas

--- frontend/src/views/etapasProjeto/EtapasRaiz.vue
+++ frontend/src/views/etapasProjeto/EtapasRaiz.vue
@@ -1,11 +1,22 @@
 <script setup>
-import { useEtapasProjetosStore } from '@/stores/etapasProjeto.store.js';
-import { onUnmounted} from 'vue';
+import { useEtapasProjetosStore } from '@/stores/etapasProjeto.store';
+import { onUnmounted } from 'vue';
+
 const etapasProjetosStore = useEtapasProjetosStore();
 
 etapasProjetosStore.buscarTudo();
-onUnmounted(() => { etapasProjetosStore.$reset(); });
+
+onUnmounted(() => {
+  etapasProjetosStore.$reset();
+});
 </script>
 <template>
+  <ErrorComponent
+    v-if="etapasProjetosStore.erro"
+    class="fb100 mb1"
+  >
+    {{ etapasProjetosStore.erro }}
+  </ErrorComponent>
+
   <router-view />
 </template>
