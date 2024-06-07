35bcca0eb96d5df43bbebb9d26bf0453f9b89c72
lucasansei <lucas.ansei@eokoe.com>
2024-06-07 10:30:25 -0300

    Revert

--- backend/src/task/importacao_parlamentar/parlamentar.service.ts
+++ backend/src/task/importacao_parlamentar/parlamentar.service.ts
@@ -12,7 +12,6 @@ export class ImportacaoParlamentarService implements TaskableService {
     private readonly logger = new Logger(ImportacaoParlamentarService.name);
     constructor(
         private readonly prisma: PrismaService,
-        private readonly storageService: StorageService,
         @Inject(forwardRef(() => UploadService)) private readonly uploadService: UploadService
     ) {}
 
@@ -22,7 +21,7 @@ export class ImportacaoParlamentarService implements TaskableService {
     async executeJob(inputParams: CreateImportacaoParlamentarDto, taskId: string): Promise<any> {
         this.logger.verbose(`Carregando importação parlamentar id ${inputParams.upload_token}`);
 
-        this.storageService.saveAsFile(inputParams.upload_token, '/tmp/import-parlamentar');
+        //this.storageService.saveAsFile(inputParams.upload_token, '/tmp/import-parlamentar');
 
         const db = await Database.create(this.getTmpFilePath('db-at-' + Date.now() + '.duckdb', taskId));
 
