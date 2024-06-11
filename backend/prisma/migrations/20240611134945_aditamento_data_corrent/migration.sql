092b4b701d838ad72e0cdedc6cdc01904d18f41e
lucasansei <lucas.ansei@eokoe.com>
2024-06-11 11:04:49 -0300

    Removendo null

--- backend/prisma/migrations/20240611134945_aditamento_data_corrent/migration.sql
+++ backend/prisma/migrations/20240611134945_aditamento_data_corrent/migration.sql
@@ -1,9 +1,4 @@
-/*
-  Warnings:
 
-  - Added the required column `data_vigencia_corrente` to the `distribuicao_recurso_aditamento` table without a default value. This is not possible if the table is not empty.
-
-*/
 -- AlterTable
 ALTER TABLE "distribuicao_recurso_aditamento" ADD COLUMN     "data_vigencia_corrente" DATE;
 
