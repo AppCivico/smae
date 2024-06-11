e08eb59cc700ad8772665d0b0879e624fc25b369
lucasansei <lucas.ansei@eokoe.com>
2024-06-11 10:59:39 -0300

    Fix: migration

--- backend/prisma/migrations/20240611134945_aditamento_data_corrent/migration.sql
+++ backend/prisma/migrations/20240611134945_aditamento_data_corrent/migration.sql
@@ -5,4 +5,8 @@
 
 */
 -- AlterTable
-ALTER TABLE "distribuicao_recurso_aditamento" ADD COLUMN     "data_vigencia_corrente" DATE NOT NULL;
+ALTER TABLE "distribuicao_recurso_aditamento" ADD COLUMN     "data_vigencia_corrente" DATE;
+
+UPDATE distribuicao_recurso_aditamento SET data_vigencia_corrente = NOW();
+
+ALTER TABLE distribuicao_recurso_aditamento ALTER COLUMN data_vigencia_corrente SET NOT NULL;
