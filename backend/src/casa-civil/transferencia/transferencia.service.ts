721b932893db5162ea37eee6842fe5585941ac04
lucasansei <lucas.ansei@eokoe.com>
2024-06-12 15:41:09 -0300

    Organizando pasta de tipo de transferência e separando service

--- backend/src/casa-civil/transferencia/transferencia.service.ts
+++ backend/src/casa-civil/transferencia/transferencia.service.ts
@@ -8,16 +8,13 @@ import { UploadService } from 'src/upload/upload.service';
 import { PessoaFromJwt } from '../../auth/models/PessoaFromJwt';
 import { BlocoNotaService } from '../../bloco-nota/bloco-nota/bloco-nota.service';
 import { PrismaService } from '../../prisma/prisma.service';
-import { CreateTransferenciaTipoDto } from './dto/create-transferencia-tipo.dto';
 import { CreateTransferenciaAnexoDto, CreateTransferenciaDto } from './dto/create-transferencia.dto';
 import { FilterTransferenciaDto } from './dto/filter-transferencia.dto';
-import { UpdateTransferenciaTipoDto } from './dto/update-transferencia-tipo.dto';
 import {
     CompletarTransferenciaDto,
     UpdateTransferenciaAnexoDto,
     UpdateTransferenciaDto,
 } from './dto/update-transferencia.dto';
-import { TransferenciaTipoDto } from './entities/transferencia-tipo.dto';
 import { TransferenciaAnexoDto, TransferenciaDetailDto, TransferenciaDto } from './entities/transferencia.dto';
 import { WorkflowService } from 'src/workflow/configuracao/workflow.service';
 import { TarefaService } from 'src/pp/tarefa/tarefa.service';
@@ -810,103 +807,6 @@ export class TransferenciaService {
 
     async removeTransferencia(id: number, user: PessoaFromJwt) {
         await this.prisma.transferencia.update({
-            where: { id },
-            data: {
-                removido_por: user.id,
-                removido_em: new Date(Date.now()),
-            },
-        });
-    }
-
-    async createTransferenciaTipo(dto: CreateTransferenciaTipoDto, user: PessoaFromJwt): Promise<RecordWithId> {
-        const created = await this.prisma.$transaction(
-            async (prismaTxn: Prisma.TransactionClient): Promise<RecordWithId> => {
-                const similarExists = await this.prisma.transferenciaTipo.count({
-                    where: {
-                        nome: { endsWith: dto.nome, mode: 'insensitive' },
-                        categoria: dto.categoria,
-                        esfera: dto.esfera,
-                        removido_em: null,
-                    },
-                });
-                if (similarExists > 0)
-                    throw new HttpException('nome| Nome igual ou semelhante já existe em outro registro ativo', 400);
-
-                const transferenciaTipo = await prismaTxn.transferenciaTipo.create({
-                    data: {
-                        nome: dto.nome,
-                        categoria: dto.categoria,
-                        esfera: dto.esfera,
-                        criado_por: user.id,
-                        criado_em: new Date(Date.now()),
-                    },
-                    select: { id: true },
-                });
-
-                return transferenciaTipo;
-            }
-        );
-
-        return created;
-    }
-
-    async findAllTransferenciaTipo(): Promise<TransferenciaTipoDto[]> {
-        const rows = await this.prisma.transferenciaTipo.findMany({
-            where: { removido_em: null },
-            orderBy: { nome: 'asc' },
-            select: {
-                id: true,
-                nome: true,
-                categoria: true,
-                esfera: true,
-            },
-        });
-
-        return rows;
-    }
-
-    async updateTransferenciaTipo(
-        id: number,
-        dto: UpdateTransferenciaTipoDto,
-        user: PessoaFromJwt
-    ): Promise<RecordWithId> {
-        const updated = await this.prisma.$transaction(
-            async (prismaTxn: Prisma.TransactionClient): Promise<RecordWithId> => {
-                const transferenciaTipo = await prismaTxn.transferenciaTipo.update({
-                    where: { id },
-                    data: {
-                        nome: dto.nome,
-                        categoria: dto.categoria,
-                        esfera: dto.esfera,
-                        atualizado_por: user.id,
-                        atualizado_em: new Date(Date.now()),
-                    },
-                    select: {
-                        nome: true,
-                        categoria: true,
-                        esfera: true,
-                    },
-                });
-
-                const similarExists = await this.prisma.transferenciaTipo.count({
-                    where: {
-                        nome: { endsWith: transferenciaTipo.nome, mode: 'insensitive' },
-                        categoria: transferenciaTipo.categoria,
-                        esfera: transferenciaTipo.esfera,
-                        removido_em: null,
-                    },
-                });
-                if (similarExists > 1) throw new HttpException('Já existe um registro com estes campos.', 400);
-
-                return { id };
-            }
-        );
-
-        return updated;
-    }
-
-    async removeTransferenciaTipo(id: number, user: PessoaFromJwt) {
-        await this.prisma.transferenciaTipo.update({
             where: { id },
             data: {
                 removido_por: user.id,
