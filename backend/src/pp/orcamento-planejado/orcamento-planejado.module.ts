873fdfed1e8d612c08cfa63f0d54d381399943ea
Renato Cron <renato.cron@gmail.com>
2024-06-12 06:15:08 -0300

    Orc Planejado

--- backend/src/pp/orcamento-planejado/orcamento-planejado.module.ts
+++ backend/src/pp/orcamento-planejado/orcamento-planejado.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { OrcamentoPlanejadoService } from './orcamento-planejado.service';
-import { OrcamentoPlanejadoController } from './orcamento-planejado.controller';
+import { OrcamentoPlanejadoController, OrcamentoPlanejadoMDOController } from './orcamento-planejado.controller';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { ProjetoModule } from '../projeto/projeto.module';
 import { DotacaoModule } from '../../dotacao/dotacao.module';
 
 @Module({
     imports: [PrismaModule, DotacaoModule, ProjetoModule],
-    controllers: [OrcamentoPlanejadoController],
+    controllers: [OrcamentoPlanejadoController, OrcamentoPlanejadoMDOController],
     providers: [OrcamentoPlanejadoService],
 })
 export class OrcamentoPlanejadoModule {}
