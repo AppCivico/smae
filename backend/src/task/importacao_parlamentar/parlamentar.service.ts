e7c39e638bae680425b1a1d7f0241ae9d6a8cd85
lucasansei <lucas.ansei@eokoe.com>
2024-06-14 03:08:36 -0300

    Ajuste em queries de import

--- backend/src/task/importacao_parlamentar/parlamentar.service.ts
+++ backend/src/task/importacao_parlamentar/parlamentar.service.ts
@@ -64,7 +64,12 @@ export class ImportacaoParlamentarService implements TaskableService {
             SELECT
                 dp.nome,
                 c.nome_urna nome_popular,
-                dp.email,
+                (
+                    CASE
+                        WHEN dp.email = '#NULO#' THEN null
+                        ELSE dp.email
+                    END
+                ),
                 (
                     CASE
                         WHEN c.cargo_que_concorre = 'deputado federal' THEN 'DeputadoFederal'
@@ -75,7 +80,7 @@ export class ImportacaoParlamentarService implements TaskableService {
                 ),
                 c.sigla_partido
             FROM importacao.dadospessoais dp
-            JOIN importacao.candidatura c ON dp.codigo_candidato_eleicao = c.codigo_candidato
+            JOIN importacao.candidatura c ON dp.codigo_candidato_eleicao = c.codigo_candidato AND dp.ano_eleicao = c.ano
             WHERE dp.cpf != '-4000000000'; 
         `);
 
@@ -104,7 +109,7 @@ export class ImportacaoParlamentarService implements TaskableService {
                 ),
                 c.codigo_candidato
             FROM importacao.dadospessoais dp
-            JOIN importacao.candidatura c ON dp.codigo_candidato_eleicao = c.codigo_candidato
+            JOIN importacao.candidatura c ON dp.codigo_candidato_eleicao = c.codigo_candidato AND dp.ano_eleicao = c.ano
             WHERE EXISTS (SELECT 1 FROM smae.parlamentar WHERE nome = dp.nome);
         `);
 
