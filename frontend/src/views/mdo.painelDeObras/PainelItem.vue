e6a9df40c8954c29cb58175290530e3480f3d5ab
Giovana <f_gi@live.com>
2024-06-15 13:29:32 -0300

    fix:(wip) adicionando componente faltante do painel de obras

---
+++ frontend/src/views/mdo.painelDeObras/PainelItem.vue
@@ -0,0 +1,72 @@
+<script setup>
+import { useDotaçãoStore } from '@/stores/dotacao.store.ts';
+import { usePortfolioStore } from '@/stores/portfolios.store.ts';
+import { useProjetosStore } from '@/stores/projetos.store.ts';
+import { useRegionsStore } from '@/stores/regions.store';
+import { useTarefasStore } from '@/stores/tarefas.store.ts';
+import { storeToRefs } from 'pinia';
+import { watch } from 'vue';
+
+const DotaçãoStore = useDotaçãoStore();
+const portfolioStore = usePortfolioStore();
+const projetosStore = useProjetosStore();
+const RegionsStore = useRegionsStore();
+const tarefasStore = useTarefasStore();
+
+const props = defineProps({
+  projetoId: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const { DotaçãoSegmentos } = storeToRefs(DotaçãoStore);
+const { emFoco } = storeToRefs(projetosStore);
+const { regions } = storeToRefs(RegionsStore);
+
+// eslint-disable-next-line padded-blocks
+async function iniciar() {
+  // eslint-disable-next-line padded-blocks
+  if (emFoco?.id !== props.projetoId) {
+    // Tarefas dependem do projeto, portanto precisam ser redefinidas na troca deste
+    tarefasStore.$reset();
+    projetosStore.$reset();
+
+    await projetosStore.buscarItem(props.projetoId);
+  }
+
+  const portfolioId = emFoco?.portfolio_id;
+
+  if (!Array.isArray(regions) || !regions.length) {
+    RegionsStore.getAll();
+  }
+
+  if (!portfolioStore?.portfoliosPorId[portfolioId]) {
+    portfolioStore.buscarTudo();
+  }
+}
+
+watch(emFoco, () => {
+  if (Array.isArray(emFoco?.value?.fonte_recursos)) {
+    emFoco.value.fonte_recursos.forEach((fonte) => {
+      if (fonte.fonte_recurso_ano) {
+        if (!Array.isArray(DotaçãoSegmentos?.value?.[fonte.fonte_recurso_ano]?.fonte_recursos)) {
+          DotaçãoStore.getDotaçãoSegmentos(fonte.fonte_recurso_ano);
+        }
+      }
+    });
+  }
+});
+
+iniciar();
+</script>
+<template>
+  <div
+    v-bind="$attrs"
+    class="t12 uc w700 tamarelo"
+  >
+    <small class="lc">Portfolio</small>
+    {{ portfolioStore.portfoliosPorId[projetosStore?.emFoco?.portfolio_id]?.titulo }}
+  </div>
+  <router-view />
+</template>
