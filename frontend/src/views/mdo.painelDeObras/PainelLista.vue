fc16ccc10af16dfbac11c4b2338787b336eb3790
Giovana <f_gi@live.com>
2024-06-15 11:08:43 -0300

    feat: adicionando estrutura de telas para crud de painél de obras

---
+++ frontend/src/views/mdo.painelDeObras/PainelLista.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+<template>
+  <div class="flex spacebetween center mb2">
+    <h1>{{ route?.meta?.título || 'Painéis de obras' }}</h1>
+    <hr class="ml2 f1">
+    <router-link
+      :to="{name: 'paineisCriar'}"
+      class="btn big ml1"
+    >
+      Novo painél
+    </router-link>
+  </div>
+</template>
