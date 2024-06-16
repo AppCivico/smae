b08d3483ae99ecf64fdc4f0d739f5f733e145cd9
Giovana <f_gi@live.com>
2024-06-16 15:42:40 -0300

    feat: adicionando gerenciador de estados básico para painel de obras

---
+++ frontend/src/stores/paineisDeObras.store.js
@@ -0,0 +1,88 @@
+import { defineStore } from 'pinia';
+
+const baseUrl = `${import.meta.env.VITE_API_URL}`;
+
+export const usePartidosStore = defineStore('partidosStore', {
+  state: () => ({
+    lista: [],
+    emFoco: null,
+
+    chamadasPendentes: {
+      lista: false,
+      emFoco: false,
+    },
+    erro: null,
+  }),
+  actions: {
+    async buscarItem(id = 0, params = {}) {
+      this.chamadasPendentes.emFoco = true;
+      this.erro = null;
+
+      try {
+        const resposta = await this.requestS.get(`${baseUrl}/painel-de-obras/${id}`, params);
+        this.emFoco = {
+          ...resposta,
+        };
+      } catch (erro) {
+        this.erro = erro;
+      }
+      this.chamadasPendentes.emFoco = false;
+    },
+
+    async buscarTudo(params = {}) {
+      this.chamadasPendentes.lista = true;
+      this.erro = null;
+
+      try {
+        const { linhas } = await this.requestS.get(`${baseUrl}/painel-de-obras`, params);
+        this.lista = linhas;
+      } catch (erro) {
+        this.erro = erro;
+      }
+      this.chamadasPendentes.lista = false;
+    },
+
+    async excluirItem(id) {
+      this.chamadasPendentes.lista = true;
+      this.erro = null;
+
+      try {
+        await this.requestS.delete(`${baseUrl}/painel-de-obras/${id}`);
+        this.chamadasPendentes.lista = false;
+        return true;
+      } catch (erro) {
+        this.erro = erro;
+        this.chamadasPendentes.lista = false;
+        return false;
+      }
+    },
+
+    async salvarItem(params = {}, id = 0) {
+      this.chamadasPendentes.emFoco = true;
+      this.erro = null;
+
+      try {
+        if (id) {
+          await this.requestS.patch(`${baseUrl}/painel-de-obras/${id}`, params);
+        } else {
+          await this.requestS.post(`${baseUrl}/painel-de-obras`, params);
+        }
+
+        this.chamadasPendentes.emFoco = false;
+        return true;
+      } catch (erro) {
+        this.erro = erro;
+        this.chamadasPendentes.emFoco = false;
+        return false;
+      }
+    },
+  },
+
+  getters: {
+    itemParaEdição({ emFoco }) {
+      return {
+        ...emFoco,
+      };
+    },
+  },
+});
