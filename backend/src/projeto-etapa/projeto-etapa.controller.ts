5e2a18230d611d55e6643824b572b1abc994a737
Renato Cron <renato.cron@gmail.com>
2024-06-18 16:59:24 -0300

    Add projeto-etapa-mdo

--- backend/src/projeto-etapa/projeto-etapa.controller.ts
+++ backend/src/projeto-etapa/projeto-etapa.controller.ts
@@ -9,10 +9,12 @@ import { CreateProjetoEtapaDto } from './dto/create-projeto-etapa.dto';
 import { ListProjetoEtapaDto } from './dto/list-projeto-etapa.dto';
 import { UpdateProjetoEtapaDto } from './dto/update-projeto-etapa.dto';
 import { ProjetoEtapaService } from './projeto-etapa.service';
+import { TipoProjeto } from '@prisma/client';
 
 @ApiTags('Projeto Etapa')
 @Controller('projeto-etapa')
 export class ProjetoEtapaController {
+    private tipo: TipoProjeto = 'PP';
     constructor(private readonly projetoEtapaService: ProjetoEtapaService) {}
 
     @Post()
@@ -22,13 +24,13 @@ export class ProjetoEtapaController {
         @Body() createTagDto: CreateProjetoEtapaDto,
         @CurrentUser() user: PessoaFromJwt
     ): Promise<RecordWithId> {
-        return await this.projetoEtapaService.create(createTagDto, user);
+        return await this.projetoEtapaService.create(this.tipo, createTagDto, user);
     }
 
     @ApiBearerAuth('access-token')
     @Get()
     async findAll(): Promise<ListProjetoEtapaDto> {
-        return { linhas: await this.projetoEtapaService.findAll() };
+        return { linhas: await this.projetoEtapaService.findAll(this.tipo) };
     }
 
     @Patch(':id')
@@ -39,7 +41,7 @@ export class ProjetoEtapaController {
         @Body() updateTagDto: UpdateProjetoEtapaDto,
         @CurrentUser() user: PessoaFromJwt
     ): Promise<RecordWithId> {
-        return await this.projetoEtapaService.update(+params.id, updateTagDto, user);
+        return await this.projetoEtapaService.update(this.tipo, +params.id, updateTagDto, user);
     }
 
     @Delete(':id')
@@ -48,7 +50,51 @@ export class ProjetoEtapaController {
     @ApiNoContentResponse()
     @HttpCode(HttpStatus.ACCEPTED)
     async remove(@Param() params: FindOneParams, @CurrentUser() user: PessoaFromJwt) {
-        await this.projetoEtapaService.remove(+params.id, user);
+        await this.projetoEtapaService.remove(this.tipo, +params.id, user);
+        return '';
+    }
+}
+
+@ApiTags('Projeto Etapa de Obras')
+@Controller('projeto-etapa-mdo')
+export class ProjetoEtapaMDOController {
+    private tipo: TipoProjeto = 'MDO';
+    constructor(private readonly projetoEtapaService: ProjetoEtapaService) {}
+
+    @Post()
+    @ApiBearerAuth('access-token')
+    @Roles(['CadastroProjetoEtapaMDO.inserir'])
+    async create(
+        @Body() createTagDto: CreateProjetoEtapaDto,
+        @CurrentUser() user: PessoaFromJwt
+    ): Promise<RecordWithId> {
+        return await this.projetoEtapaService.create(this.tipo, createTagDto, user);
+    }
+
+    @ApiBearerAuth('access-token')
+    @Get()
+    async findAll(): Promise<ListProjetoEtapaDto> {
+        return { linhas: await this.projetoEtapaService.findAll(this.tipo) };
+    }
+
+    @Patch(':id')
+    @ApiBearerAuth('access-token')
+    @Roles(['CadastroProjetoEtapaMDO.editar'])
+    async update(
+        @Param() params: FindOneParams,
+        @Body() updateTagDto: UpdateProjetoEtapaDto,
+        @CurrentUser() user: PessoaFromJwt
+    ): Promise<RecordWithId> {
+        return await this.projetoEtapaService.update(this.tipo, +params.id, updateTagDto, user);
+    }
+
+    @Delete(':id')
+    @ApiBearerAuth('access-token')
+    @Roles(['CadastroProjetoEtapaMDO.remover'])
+    @ApiNoContentResponse()
+    @HttpCode(HttpStatus.ACCEPTED)
+    async remove(@Param() params: FindOneParams, @CurrentUser() user: PessoaFromJwt) {
+        await this.projetoEtapaService.remove(this.tipo, +params.id, user);
         return '';
     }
 }
