aef3a256bc81d7e261b1add46a4faa6bb9479acc
Renato Cron <renato.cron@gmail.com>
2024-06-18 13:45:02 -0300

    Add ProjetoMDOProxyPdmMetasController

--- backend/src/pp/projeto/projeto.proxy-pdm-metas.controller.ts
+++ backend/src/pp/projeto/projeto.proxy-pdm-metas.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, ParseArrayPipe, Query } from '@nestjs/common';
-import { ApiBearerAuth, ApiOperation, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Roles } from '../../auth/decorators/roles.decorator';
 import { ListDadosMetaIniciativaAtividadesDto } from '../../meta/dto/create-meta.dto';
 import { MetaService } from '../../meta/meta.service';
@@ -35,12 +35,53 @@ export class ProjetoProxyPdmMetasController {
 
     @ApiBearerAuth('access-token')
     @Get('iniciativas-atividades')
-    @ApiUnauthorizedResponse({ description: 'Precisa: CadastroMeta.listar' })
     @Roles([
         'Projeto.administrador',
         'Projeto.administrador_no_orgao',
         'SMAE.gestor_de_projeto',
         'SMAE.colaborador_de_projeto',
+    ])
+    async buscaMetasIniciativaAtividades(
+        @Query('meta_ids', new ParseArrayPipe({ items: Number, separator: ',' })) ids: number[]
+    ): Promise<ListDadosMetaIniciativaAtividadesDto> {
+        return { linhas: await this.metaService.buscaMetasIniciativaAtividades(ids) };
+    }
+}
+
+@ApiTags('Projeto de Obras')
+@Controller('projeto-mdo/proxy')
+export class ProjetoMDOProxyPdmMetasController {
+    constructor(
+        private readonly svc: ProjetoProxyPdmMetasService,
+        private readonly metaService: MetaService
+    ) {}
+
+    @Get('pdm-e-metas')
+    @ApiBearerAuth('access-token')
+    @Roles(
+        [
+            'ProjetoMDO.administrador',
+            'ProjetoMDO.administrador_no_orgao',
+            'MDO.gestor_de_projeto',
+            'MDO.colaborador_de_projeto',
+        ],
+        'Consulta Metas e PDM do sistema'
+    )
+    @ApiOperation({
+        description:
+            'Como não há necessidade de puxar todo os dados da meta e do PDM, esse endpoint retorna um resumo de Meta+PDM',
+    })
+    async findAll(): Promise<ListProjetoProxyPdmMetaDto> {
+        return { linhas: await this.svc.findAll() };
+    }
+
+    @ApiBearerAuth('access-token')
+    @Get('iniciativas-atividades')
+    @Roles([
+        'ProjetoMDO.administrador',
+        'ProjetoMDO.administrador_no_orgao',
+        'MDO.gestor_de_projeto',
+        'MDO.colaborador_de_projeto',
     ])
     async buscaMetasIniciativaAtividades(
         @Query('meta_ids', new ParseArrayPipe({ items: Number, separator: ',' })) ids: number[]
