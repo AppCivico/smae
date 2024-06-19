551e99a45aab08a713ddd3e4a27a71b1f1f27010
Sobral <robson@oficiodesign.com>
2024-06-19 10:24:29 -0300

    fix: Mudança de portfólio indisponível quando havia só uma opção

--- frontend/src/views/projetos/ProjetosCriarEditar.vue
+++ frontend/src/views/projetos/ProjetosCriarEditar.vue
@@ -318,7 +318,7 @@ watch(emFoco, () => {
         >
           <template #append>
             <router-link
-              v-if="portfóliosDisponíveis.length > 1"
+              v-if="portfóliosDisponíveis.length"
               :to="{
                 name: 'projetosTrocarPortfolio'
               }"
