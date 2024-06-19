c5e0b7cd025886e35c94faf5cd0f0bc07c2cf780
Giovana <f_gi@live.com>
2024-06-19 20:13:48 -0300

    feat: adicionando telas do crud de macrotema

---
+++ frontend/src/views/ps.macrotemas/MacrotemasLista.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="flex spacebetween center mb2">
+    <TítuloDePágina />
+    <hr class="ml2 f1">
+    <router-link
+      :to="{ name: 'planosSetoriaisNovoMacrotema' }"
+      class="btn big ml1"
+    >
+      Novo {{ titulo }}
+    </router-link>
+  </div>
+  <table class="tablemain">
+    <col>
+    <col class="col--botão-de-ação">
+    <col class="col--botão-de-ação">
+    <thead>
+      <tr>
+        <th> {{ titulo }} </th>
+        <th />
+        <th />
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="item in lista"
+        :key="item.id"
+      >
+        <td>{{ item.descricao }}</td>
+        <td>
+          <router-link
+            :to="{ name: 'planosSetoriaisEditarMacrotema', params: { macrotemaId: item.id } }"
+            class="tprimary"
+          >
+            <svg
+              width="20"
+              height="20"
+            ><use xlink:href="#i_edit" /></svg>
+          </router-link>
+        </td>
+        <td>
+          <button
+            class="like-a__text"
+            arial-label="excluir"
+            title="excluir"
+            @click="excluirMacrotema(item.id, item.descricao)"
+          >
+            <svg
+              width="20"
+              height="20"
+            ><use xlink:href="#i_remove" /></svg>
+          </button>
+        </td>
+      </tr>
+      <tr v-if="chamadasPendentes.lista">
+        <td colspan="3">
+          Carregando
+        </td>
+      </tr>
+      <tr v-else-if="erro">
+        <td colspan="3">
+          Erro: {{ erro }}
+        </td>
+      </tr>
+      <tr v-else-if="!lista.length">
+        <td colspan="3">
+          Nenhum resultado encontrado.
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script setup>
+import { useAlertStore } from '@/stores/alert.store';
+import { storeToRefs } from 'pinia';
+import { useRoute } from 'vue-router';
+import { computed } from 'vue';
+
+import { useMacrotemasStore } from '@/stores/macrotemasPs.store';
+
+const route = useRoute();
+const titulo = typeof route?.meta?.título === 'function'
+  ? computed(() => route.meta.título())
+  : route?.meta?.título;
+const alertStore = useAlertStore();
+const macrotemasStore = useMacrotemasStore();
+const { lista, chamadasPendentes, erro } = storeToRefs(macrotemasStore);
+
+async function excluirMacrotema(id, descricao) {
+  alertStore.confirmAction(
+    `Deseja mesmo remover "${descricao}"?`,
+    async () => {
+      if (await macrotemasStore.excluirItem(id)) {
+        macrotemasStore.$reset();
+        macrotemasStore.buscarTudo();
+        alertStore.success(`"${descricao}" removido.`);
+      }
+    },
+    'Remover',
+  );
+}
+
+macrotemasStore.$reset();
+macrotemasStore.buscarTudo();
+</script>
+
+<style></style>
