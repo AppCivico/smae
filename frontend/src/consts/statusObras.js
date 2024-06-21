9cacd5054f0b2ab37af6fd21db2949e7267ed440
Sobral <robson@oficiodesign.com>
2024-06-21 10:06:18 -0300

    fix: Lista de status de obras incompleta

--- frontend/src/consts/statusObras.js
+++ frontend/src/consts/statusObras.js
@@ -1,4 +1,18 @@
-export default [
-  'Em andamento',
-  'Encerrada',
-];
+export default {
+  MDO_Concluida: {
+    valor: 'MDO_Concluida',
+    nome: 'Concluída',
+  },
+  MDO_EmAndamento: {
+    valor: 'MDO_EmAndamento',
+    nome: 'Em andamento',
+  },
+  MDO_NaoIniciada: {
+    valor: 'MDO_NaoIniciada',
+    nome: 'Não iniciada',
+  },
+  MDO_Paralisada: {
+    valor: 'MDO_Paralisada',
+    nome: 'Paralisada',
+  },
+};
