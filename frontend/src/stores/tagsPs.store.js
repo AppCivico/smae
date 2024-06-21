19a5e0e212d16594149d5bf75d98bf7eb6382410
Giovana <f_gi@live.com>
2024-06-21 19:20:22 -0300

    feat: adicionando gerenciador de estados de tag

---
+++ frontend/src/stores/tagsPs.store.js
@@ -0,0 +1,88 @@
+import { defineStore } from 'pinia';
+
+const baseUrl = `${import.meta.env.VITE_API_URL}`;
+
+export const useTagsPsStore = defineStore('tagsPsStore', {
+  state: () => ({
+    lista: [],
+    emFoco: null,
+
+    chamadasPendentes: {
+      lista: false,
+      emFoco: false,
+    },
+    erro: null,
+  }),
+  actions: {
+    async buscarItem(id = 0, params = {}) {
+      this.chamadasPendentes.emFoco = true;
+      this.erro = null;
+
+      try {
+        const resposta = await this.requestS.get(`${baseUrl}/tag/${id}`, params);
+        this.emFoco = {
+          ...resposta,
+        };
+      } catch (erro) {
+        this.erro = erro;
+      }
+      this.chamadasPendentes.emFoco = false;
+    },
+
+    async buscarTudo(params = {}) {
+      this.chamadasPendentes.lista = true;
+      this.erro = null;
+      console.log('entrou no buscar tudo de tags, params: ', params);
+      try {
+        const { linhas } = await this.requestS.get(`${baseUrl}/tag?pdm_id=${params}`);
+        this.lista = linhas;
+      } catch (erro) {
+        this.erro = erro;
+      }
+      this.chamadasPendentes.lista = false;
+    },
+
+    async excluirItem(id) {
+      this.chamadasPendentes.lista = true;
+      this.erro = null;
+
+      try {
+        await this.requestS.delete(`${baseUrl}/tag/${id}`);
+        this.chamadasPendentes.lista = false;
+        return true;
+      } catch (erro) {
+        this.erro = erro;
+        this.chamadasPendentes.lista = false;
+        return false;
+      }
+    },
+
+    async salvarItem(params = {}, id = 0) {
+      this.chamadasPendentes.emFoco = true;
+      this.erro = null;
+
+      try {
+        if (id) {
+          await this.requestS.patch(`${baseUrl}/tag/${id}`, params);
+        } else {
+          await this.requestS.post(`${baseUrl}/tag`, params);
+        }
+
+        this.chamadasPendentes.emFoco = false;
+        return true;
+      } catch (erro) {
+        this.erro = erro;
+        this.chamadasPendentes.emFoco = false;
+        return false;
+      }
+    },
+  },
+
+  getters: {
+    itemParaEdição({ emFoco }) {
+      return {
+        ...emFoco,
+      };
+    },
+  },
+});
