9454b89fd43067e99c76fa4c4f1e4669c404cf3d
Giovana <f_gi@live.com>
2024-06-21 09:06:24 -0300

    fix: corrigindo edit de macrotema, tema e subtema

--- frontend/src/views/ps.macrotemas/MacrotemasCriarEditar.vue
+++ frontend/src/views/ps.macrotemas/MacrotemasCriarEditar.vue
@@ -96,7 +96,7 @@ async function onSubmit(values) {
 
     const dataToSend = { ...values, pdm_id: Number(route.params.planoSetorialId) };
 
-    if (props.equipamentoId) {
+    if (props.macrotemaId) {
       response = await macrotemasStore.salvarItem(
         dataToSend,
         props.macrotemaId,
