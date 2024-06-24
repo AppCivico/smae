0db3fe1f8309d229a1e0cc4da4a09de2cc42ba06
Sobral <robson@oficiodesign.com>
2024-06-24 19:58:19 -0300

    feat: Limpar armazém de acompanhamentos na saída

--- frontend/src/views/acompanhamentos/AcompanhamentosRaiz.vue
+++ frontend/src/views/acompanhamentos/AcompanhamentosRaiz.vue
@@ -1,11 +1,19 @@
 <script setup>
+import { useAcompanhamentosStore } from '@/stores/acompanhamentos.store.ts';
 import { useTiposDeAcompanhamentoStore } from '@/stores/tiposDeAcompanhamento.store.ts';
+import { onUnmounted } from 'vue';
 
+const acompanhamentosStore = useAcompanhamentosStore();
 const tiposDeAcompanhamentoStore = useTiposDeAcompanhamentoStore();
 
-if (!tiposDeAcompanhamentoStore?.lista?.length) {
-  tiposDeAcompanhamentoStore?.buscarTudo();
+if (!tiposDeAcompanhamentoStore.lista.length) {
+  tiposDeAcompanhamentoStore.buscarTudo();
 }
+
+onUnmounted(() => {
+  acompanhamentosStore.$reset();
+  tiposDeAcompanhamentoStore.$reset();
+});
 </script>
 <template>
   <router-view />
