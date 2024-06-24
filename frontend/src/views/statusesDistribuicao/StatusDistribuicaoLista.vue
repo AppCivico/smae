aadea2d914aefb3df2a4985f9db4e0cd56687119
lucasansei <lucas.ansei@eokoe.com>
2024-06-24 02:42:31 -0300

    Cadastro base de status

---
+++ frontend/src/views/statusesDistribuicao/StatusDistribuicaoLista.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="flex spacebetween center mb2">
+    <TítuloDePágina />
+    <hr class="ml2 f1">
+    <router-link
+      :to="{ name: 'statusDistribuicaoCriar' }"
+      class="btn big ml1"
+    >
+      Novo {{ titulo }}
+    </router-link>
+  </div>
+  <table class="tablemain">
+    <col>
+    <col class="col--botão-de-ação">
+    <col class="col--botão-de-ação">
+    <thead>
+      <tr>
+        <th>
+          Nome
+        </th>
+        <th>
+          Tipo
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="item in lista"
+        :key="item.id"
+      >
+        <td>{{ item.nome }}</td>
+        <td>{{ item.tipo }}</td>
+        <td v-if="item.pode_editar">
+          <router-link
+            :to="{ name: 'statusDistribuicaoEditar', params: { statusDistribuicaoId: item.id } }"
+            class="tprimary"
+          >
+            <svg
+              width="20"
+              height="20"
+            ><use xlink:href="#i_edit" /></svg>
+          </router-link>
+        </td>
+        <td v-if="item.pode_editar">
+          <button
+            class="like-a__text"
+            arial-label="excluir"
+            title="excluir"
+            @click="excluirStatusDistribuicao(item.id, item.descricao)"
+          >
+            <svg
+              width="20"
+              height="20"
+            ><use xlink:href="#i_remove" /></svg>
+          </button>
+        </td>
+      </tr>
+      <tr v-if="chamadasPendentes.lista">
+        <td colspan="3">
+          Carregando
+        </td>
+      </tr>
+      <tr v-else-if="erro">
+        <td colspan="3">
+          Erro: {{ erro }}
+        </td>
+      </tr>
+      <tr v-else-if="!lista.length">
+        <td colspan="3">
+          Nenhum resultado encontrado.
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script setup>
+import { storeToRefs } from 'pinia';
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+import { useAlertStore } from '@/stores/alert.store';
+
+import { useStatusDistribuicaoStore } from '@/stores/statusDistribuicao.store';
+
+const route = useRoute();
+const titulo = typeof route?.meta?.título === 'function'
+  ? computed(() => route.meta.título())
+  : route?.meta?.título;
+const alertStore = useAlertStore();
+const statusDistribuicaoStore = useStatusDistribuicaoStore();
+const { lista, chamadasPendentes, erro } = storeToRefs(statusDistribuicaoStore);
+
+async function excluirTema(id, descricao) {
+  alertStore.confirmAction(
+    `Deseja mesmo remover "${descricao}"?`,
+    async () => {
+      if (await statusDistribuicaoStore.excluirItem(id)) {
+        statusDistribuicaoStore.$reset();
+        statusDistribuicaoStore.buscarTudo();
+        alertStore.success(`"${descricao}" removido.`);
+      }
+    },
+    'Remover',
+  );
+}
+
+statusDistribuicaoStore.$reset();
+statusDistribuicaoStore.buscarTudo({});
+</script>
+
+  <style></style>
