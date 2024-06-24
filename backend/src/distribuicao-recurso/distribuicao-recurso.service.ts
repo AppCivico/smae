dc7d0011641f00642e03121ee853a9353ab1d9bc
lucasansei <lucas.ansei@eokoe.com>
2024-06-24 16:18:14 -0300

    Fix: undefined

--- backend/src/distribuicao-recurso/distribuicao-recurso.service.ts
+++ backend/src/distribuicao-recurso/distribuicao-recurso.service.ts
@@ -762,10 +762,10 @@ export class DistribuicaoRecursoService {
                     },
                 });
 
-                let sumCusteio: number = +dto.custeio ?? 0;
-                let sumInvestimento: number = +dto.investimento ?? 0;
-                let sumContrapartida: number = +dto.valor_contrapartida ?? 0;
-                let sumTotal: number = +dto.valor_total ?? 0;
+                let sumCusteio: number = dto.custeio ?? 0;
+                let sumInvestimento: number = dto.investimento ?? 0;
+                let sumContrapartida: number = dto.valor_contrapartida ?? 0;
+                let sumTotal: number = dto.valor_total ?? 0;
 
                 for (const distRow of outrasDistribuicoes) {
                     sumCusteio += +distRow.custeio.toNumber();
