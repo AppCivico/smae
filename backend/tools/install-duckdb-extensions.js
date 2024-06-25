da35274458a27ff0531b7894addedaa3bb9dce1d
Renato Cron <renato.cron@gmail.com>
2024-06-25 13:37:20 -0300

    feat(Dockerfile): adiciona script para instalar extensões do DuckDB durante build do docker
    docs(README.md): adiciona instruções para adicionar novas extensões
    
    build(Dockerfile): copia e executa script de instalação de extensões do DuckDB

---
+++ backend/tools/install-duckdb-extensions.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const duckdb_async_1 = require("duckdb-async");
+const process_1 = require("process");
+async function bootstrap() {
+    const db = await duckdb_async_1.Database.create(':memory:');
+    for (const extension of ['https', 'postgres', 'sqlite', 'spatial']) {
+        console.log(`Installing ${extension} extension`);
+        const feedback = await db.all(`INSTALL ${extension}`);
+        if (feedback.length > 0) {
+            console.error(feedback);
+            (0, process_1.exit)(1);
+        }
+    }
+    await db.close();
+}
+bootstrap();
+//# sourceMappingURL=install-duckdb-extensions.js.map
