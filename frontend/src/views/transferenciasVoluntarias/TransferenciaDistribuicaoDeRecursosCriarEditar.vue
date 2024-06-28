fc7ee4f31db7e4111fe4cd56a6e4913a9d9478ec
Giovana <f_gi@live.com>
2024-06-28 19:30:16 -0300

    feat: ajustando exibição de status no histórico

--- frontend/src/views/transferenciasVoluntarias/TransferenciaDistribuicaoDeRecursosCriarEditar.vue
+++ frontend/src/views/transferenciasVoluntarias/TransferenciaDistribuicaoDeRecursosCriarEditar.vue
@@ -963,7 +963,7 @@ const isSomaCorreta = computed(() => {
           :key="item.id"
         >
           <td> {{ item.data_troca ? item.data_troca.split('T')[0].split('-').reverse().join('/') : '' }}</td>
-          <td>{{ item.status_base?.tipo }}</td>
+          <td>{{ item.status_base?.tipo || item.status_customizado?.tipo }}</td>
           <td>{{ item.orgao_responsavel?.sigla }}</td>
           <td>{{ item.nome_responsavel }}</td>
           <td>{{ item.motivo }}</td>
