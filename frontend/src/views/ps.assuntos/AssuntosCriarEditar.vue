ddf1bee0e037601ec5f9377bf509a3458a71b1a4
Giovana <f_gi@live.com>
2024-07-03 15:45:52 -0300

    feat: adicionando telas do crud de assunto

---
+++ frontend/src/views/ps.assuntos/AssuntosCriarEditar.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="flex spacebetween center mb2">
+    <h1> <span v-if="!assuntoId">Novo</span> Assunto</h1>
+    <hr class="ml2 f1">
+    <CheckClose />
+  </div>
+  <Form
+    v-slot="{ errors, isSubmitting }"
+    :validation-schema="schema"
+    :initial-values="itemParaEdição"
+    @submit="onSubmit"
+  >
+    <div class="flex g2 mb1">
+      <div class="f1">
+        <LabelFromYup
+          name="nome"
+          :schema="schema"
+        />
+        <Field
+          name="nome"
+          type="text"
+          min="3"
+          max="250"
+          class="inputtext light mb1"
+        />
+        <ErrorMessage
+          class="error-msg mb1"
+          name="nome"
+        />
+      </div>
+    </div>
+    <FormErrorsList :errors="errors" />
+
+    <div class="flex spacebetween center mb2">
+      <hr class="mr2 f1">
+      <button
+        class="btn big"
+        :disabled="isSubmitting || Object.keys(errors)?.length"
+        :title="
+          Object.keys(errors)?.length
+            ? `Erros de preenchimento: ${Object.keys(errors)?.length}`
+            : null
+        "
+      >
+        Salvar
+      </button>
+      <hr class="ml2 f1">
+    </div>
+  </Form>
+
+  <span
+    v-if="chamadasPendentes?.emFoco"
+    class="spinner"
+  >Carregando</span>
+
+  <div
+    v-if="erro"
+    class="error p1"
+  >
+    <div class="error-msg">
+      {{ erro }}
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { storeToRefs } from 'pinia';
+import { useRoute, useRouter } from 'vue-router';
+import { ErrorMessage, Field, Form } from 'vee-validate';
+import { useAssuntosStore } from '@/stores/assuntosPs.store';
+import { useAlertStore } from '@/stores/alert.store';
+import { assunto as schema } from '@/consts/formSchemas';
+
+const router = useRouter();
+const route = useRoute();
+const props = defineProps({
+  assuntoId: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const alertStore = useAlertStore();
+const assuntosStore = useAssuntosStore();
+const { chamadasPendentes, erro, itemParaEdição } = storeToRefs(assuntosStore);
+
+async function onSubmit(values) {
+  try {
+    let response;
+    const msg = props.assuntoId
+      ? 'Dados salvos com sucesso!'
+      : 'Item adicionado com sucesso!';
+
+    const dataToSend = { ...values };
+
+    if (route.params?.assuntoId) {
+      response = await assuntosStore.salvarItem(
+        dataToSend,
+        route.params.assuntoId,
+      );
+    } else {
+      response = await assuntosStore.salvarItem(dataToSend);
+    }
+    if (response) {
+      alertStore.success(msg);
+      assuntosStore.$reset();
+      router.push({ name: 'assuntosListar' });
+    }
+  } catch (error) {
+    alertStore.error(error);
+  }
+}
+
+assuntosStore.$reset();
+// não foi usada a prop.assuntoId pois estava vazando do edit na hora de criar uma nova
+if (route.params?.assuntoId) {
+  assuntosStore.buscarItem(route.params?.assuntoId);
+}
+</script>
+
+<style></style>
---
+++ frontend/src/views/ps.assuntos/AssuntosLista.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="flex spacebetween center mb2">
+    <TítuloDePágina />
+    <hr class="ml2 f1">
+    <router-link
+      :to="{ name: 'assuntosCriar' }"
+      class="btn big ml1"
+    >
+      Novo assunto
+    </router-link>
+  </div>
+
+  <table class="tablemain">
+    <col>
+    <col class="col--botão-de-ação">
+    <col class="col--botão-de-ação">
+    <thead>
+      <tr>
+        <th> Nome </th>
+        <th />
+        <th />
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="item in lista"
+        :key="item.id"
+      >
+        <td>{{ item.nome }}</td>
+        <td>
+          <router-link
+            :to="{ name: 'assuntosEditar', params: { assuntoId: item.id } }"
+            class="tprimary"
+          >
+            <svg
+              width="20"
+              height="20"
+            ><use xlink:href="#i_edit" /></svg>
+          </router-link>
+        </td>
+        <td>
+          <button
+            class="like-a__text"
+            arial-label="excluir"
+            title="excluir"
+            @click="excluirAssunto(item.id, item.nome)"
+          >
+            <svg
+              width="20"
+              height="20"
+            ><use xlink:href="#i_remove" /></svg>
+          </button>
+        </td>
+      </tr>
+      <tr v-if="chamadasPendentes.lista">
+        <td colspan="3">
+          Carregando
+        </td>
+      </tr>
+      <tr v-else-if="erro">
+        <td colspan="3">
+          Erro: {{ erro }}
+        </td>
+      </tr>
+      <tr v-else-if="!lista.length">
+        <td colspan="3">
+          Nenhum resultado encontrado.
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script setup>
+import { storeToRefs } from 'pinia';
+import { useRoute } from 'vue-router';
+import { useAlertStore } from '@/stores/alert.store';
+import { useAssuntosStore } from '@/stores/assuntosPs.store';
+
+const route = useRoute();
+
+const alertStore = useAlertStore();
+const assuntosStore = useAssuntosStore();
+const { lista, chamadasPendentes, erro } = storeToRefs(assuntosStore);
+
+async function excluirAssunto(id, descricao) {
+  alertStore.confirmAction(
+    `Deseja mesmo remover "${descricao}"?`,
+    async () => {
+      if (await assuntosStore.excluirItem(id)) {
+        assuntosStore.$reset();
+        assuntosStore.buscarTudo({ pdm_id: route.params.planoSetorialId });
+        alertStore.success(`"${descricao}" removido.`);
+      }
+    },
+    'Remover',
+  );
+}
+
+assuntosStore.$reset();
+assuntosStore.buscarTudo();
+</script>
+
+<style></style>
