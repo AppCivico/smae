03a5557f63c3be5a45b7fdba671c23b48a895a72
Renato Cron <renato.cron@gmail.com>
2024-07-05 19:01:23 -0300

    Add view_variavel_global

---
+++ backend/prisma/migrations/20240705214032_var/migration.sql
@@ -0,0 +1,61 @@
+-- AlterTable
+ALTER TABLE "variavel" ADD COLUMN     "atualizado_em" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "atualizado_por" INTEGER,
+ADD COLUMN     "criado_em" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "criado_por" INTEGER;
+
+ALTER TABLE "variavel"
+    ADD COLUMN "vetores_busca" tsvector GENERATED ALWAYS AS (
+        to_tsvector('simple',
+            COALESCE("titulo", '') || ' '
+        || COALESCE("descricao", '')
+        )
+    ) STORED;
+
+UPDATE variavel
+SET  criado_em = COALESCE(
+    (select i.criado_em
+    from indicador i
+    join mv_variavel_pdm mv on mv.indicador_id = i.id
+    where mv.variavel_id = variavel.id
+    limit 1 --, nao é necessário limitar ainda (pdm), mas é bom para garantir que só vai pegar da crash em prod
+    ),
+ criado_em);
+
+-- AddForeignKey
+ALTER TABLE "variavel" ADD CONSTRAINT "variavel_criado_por_fkey" FOREIGN KEY ("criado_por") REFERENCES "pessoa"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "variavel" ADD CONSTRAINT "variavel_atualizado_por_fkey" FOREIGN KEY ("atualizado_por") REFERENCES "pessoa"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+CREATE OR REPLACE VIEW view_variavel_global AS
+WITH PlanoSetorial AS (
+    select
+        variavel_id,
+        array_agg(distinct pdm_id) as pdm_ids
+    from mv_variavel_pdm
+    group by 1
+),
+AssuntoVariavel AS (
+    select
+        variavel_id,
+        array_agg(distinct av.assunto_variavel_id ) AS assunto_ids
+    from variavel_assunto_variavel AS av
+    group by 1
+)
+SELECT
+  v.id,
+  v.titulo,
+  v.codigo,
+  v.orgao_id,
+  v.orgao_proprietario_id,
+  coalesce( ps.pdm_ids , '{}'::int[]) as planos,
+  v.periodicidade,
+  v.inicio_medicao,
+  v.fim_medicao,
+  v.criado_em,
+  av.assunto_ids,
+  tipo
+FROM variavel AS v
+LEFT JOIN PlanoSetorial ps ON v.id = ps.variavel_id
+LEFT JOIN AssuntoVariavel av ON v.id = av.variavel_id
+WHERE v.removido_em IS NULL;
