afdde87a48af0b7987b39ca8bd243ebb26d924fc
Giovana <f_gi@live.com>
2024-07-05 12:56:20 -0300

    feat: adicionando gerenciador de estados de modalidade da contratação

---
+++ frontend/src/stores/modalidadeDeContratacao.store.js
@@ -0,0 +1,87 @@
+import { defineStore } from 'pinia';
+
+const baseUrl = `${import.meta.env.VITE_API_URL}`;
+
+export const useModalidadeDeContratacaoStore = defineStore('modalidadeDeContratacaoStore', {
+  state: () => ({
+    lista: [],
+    emFoco: null,
+
+    chamadasPendentes: {
+      lista: false,
+      emFoco: false,
+    },
+    erro: null,
+  }),
+  actions: {
+    async buscarItem(id = 0, params = {}) {
+      this.chamadasPendentes.emFoco = true;
+      this.erro = null;
+
+      try {
+        const resposta = await this.requestS.get(`${baseUrl}/modalidade-contratacao-mdo/${id}`, params);
+        this.emFoco = {
+          ...resposta,
+        };
+      } catch (erro) {
+        this.erro = erro;
+      }
+      this.chamadasPendentes.emFoco = false;
+    },
+
+    async buscarTudo(params = {}) {
+      this.chamadasPendentes.lista = true;
+      this.erro = null;
+
+      try {
+        const { linhas } = await this.requestS.get(`${baseUrl}/modalidade-contratacao-mdo`, params);
+        this.lista = linhas;
+      } catch (erro) {
+        this.erro = erro;
+      }
+      this.chamadasPendentes.lista = false;
+    },
+
+    async excluirItem(id) {
+      this.chamadasPendentes.lista = true;
+      this.erro = null;
+
+      try {
+        await this.requestS.delete(`${baseUrl}/modalidade-contratacao-mdo/${id}`);
+        this.chamadasPendentes.lista = false;
+        return true;
+      } catch (erro) {
+        this.erro = erro;
+        this.chamadasPendentes.lista = false;
+        return false;
+      }
+    },
+
+    async salvarItem(params = {}, id = 0) {
+      this.chamadasPendentes.emFoco = true;
+      this.erro = null;
+
+      try {
+        if (id) {
+          await this.requestS.patch(`${baseUrl}/modalidade-contratacao-mdo/${id}`, params);
+        } else {
+          await this.requestS.post(`${baseUrl}/modalidade-contratacao-mdo`, params);
+        }
+
+        this.chamadasPendentes.emFoco = false;
+        return true;
+      } catch (erro) {
+        this.erro = erro;
+        this.chamadasPendentes.emFoco = false;
+        return false;
+      }
+    },
+  },
+  getters: {
+    itemParaEdição({ emFoco }) {
+      return {
+        ...emFoco,
+      };
+    },
+  },
+});
