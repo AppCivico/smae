02c7913012d0490260c9b146eb80a58522a8cbb4
Thiago Borges <thiagoaborges@live.com>
2024-07-05 15:16:48 -0300

    fix: Calculo valor custeio/investimento

---
+++ frontend/src/stores/fontesPs.store.js
@@ -0,0 +1,88 @@
+import { defineStore } from 'pinia';
+
+const baseUrl = `${import.meta.env.VITE_API_URL}`;
+
+export const useFontesStore = defineStore('fontesStore', {
+  state: () => ({
+    lista: [],
+    emFoco: null,
+
+    chamadasPendentes: {
+      lista: false,
+      emFoco: false,
+    },
+    erro: null,
+  }),
+  actions: {
+    async buscarItem(id = 0, params = {}) {
+      this.chamadasPendentes.emFoco = true;
+      this.erro = null;
+
+      try {
+        const resposta = await this.requestS.get(`${baseUrl}/fonte-variavel/${id}`, params);
+        this.emFoco = {
+          ...resposta,
+        };
+      } catch (erro) {
+        this.erro = erro;
+      }
+      this.chamadasPendentes.emFoco = false;
+    },
+
+    async buscarTudo(params = {}) {
+      this.chamadasPendentes.lista = true;
+      this.erro = null;
+
+      try {
+        const { linhas } = await this.requestS.get(`${baseUrl}/fonte-variavel`, params);
+        this.lista = linhas;
+      } catch (erro) {
+        this.erro = erro;
+      }
+      this.chamadasPendentes.lista = false;
+    },
+
+    async excluirItem(id) {
+      this.chamadasPendentes.lista = true;
+      this.erro = null;
+
+      try {
+        await this.requestS.delete(`${baseUrl}/fonte-variavel/${id}`);
+        this.chamadasPendentes.lista = false;
+        return true;
+      } catch (erro) {
+        this.erro = erro;
+        this.chamadasPendentes.lista = false;
+        return false;
+      }
+    },
+
+    async salvarItem(params = {}, id = 0) {
+      this.chamadasPendentes.emFoco = true;
+      this.erro = null;
+
+      try {
+        if (id) {
+          await this.requestS.patch(`${baseUrl}/fonte-variavel/${id}`, params);
+        } else {
+          await this.requestS.post(`${baseUrl}/fonte-variavel`, params);
+        }
+
+        this.chamadasPendentes.emFoco = false;
+        return true;
+      } catch (erro) {
+        this.erro = erro;
+        this.chamadasPendentes.emFoco = false;
+        return false;
+      }
+    },
+  },
+
+  getters: {
+    itemParaEdição({ emFoco }) {
+      return {
+        ...emFoco,
+      };
+    },
+  },
+});
---
+++ frontend/src/views/ps.fontes/FontesLista.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="flex spacebetween center mb2">
+    <TítuloDePágina />
+    <hr class="ml2 f1">
+    <router-link
+      :to="{ name: 'fonteCriar' }"
+      class="btn big ml1"
+    >
+      Nova fonte
+    </router-link>
+  </div>
+
+  <table class="tablemain">
+    <col>
+    <col class="col--botão-de-ação">
+    <col class="col--botão-de-ação">
+    <thead>
+      <tr>
+        <th> Nome </th>
+        <th />
+        <th />
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="item in lista"
+        :key="item.id"
+      >
+        <td>{{ item.nome }}</td>
+        <td>
+          <router-link
+            :to="{ name: 'fonteEditar', params: { fonteId: item.id } }"
+            class="tprimary"
+          >
+            <svg
+              width="20"
+              height="20"
+            ><use xlink:href="#i_edit" /></svg>
+          </router-link>
+        </td>
+        <td>
+          <button
+            class="like-a__text"
+            arial-label="excluir"
+            title="excluir"
+            @click="excluirFonte(item.id, item.nome)"
+          >
+            <svg
+              width="20"
+              height="20"
+            ><use xlink:href="#i_remove" /></svg>
+          </button>
+        </td>
+      </tr>
+      <tr v-if="chamadasPendentes.lista">
+        <td colspan="3">
+          Carregando
+        </td>
+      </tr>
+      <tr v-else-if="erro">
+        <td colspan="3">
+          Erro: {{ erro }}
+        </td>
+      </tr>
+      <tr v-else-if="!lista.length">
+        <td colspan="3">
+          Nenhum resultado encontrado.
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script setup>
+import { storeToRefs } from 'pinia';
+import { useRoute } from 'vue-router';
+import { useAlertStore } from '@/stores/alert.store';
+import { useFontesStore } from '@/stores/fontesPs.store';
+
+const route = useRoute();
+
+const alertStore = useAlertStore();
+const fontesStore = useFontesStore();
+const { lista, chamadasPendentes, erro } = storeToRefs(fontesStore);
+
+async function excluirFonte(id, descricao) {
+  alertStore.confirmAction(
+    `Deseja mesmo remover "${descricao}"?`,
+    async () => {
+      if (await fontesStore.excluirItem(id)) {
+        fontesStore.$reset();
+        fontesStore.buscarTudo({ pdm_id: route.params.planoSetorialId });
+        alertStore.success(`"${descricao}" removido.`);
+      }
+    },
+    'Remover',
+  );
+}
+
+fontesStore.$reset();
+fontesStore.buscarTudo();
+</script>
+
+<style></style>
