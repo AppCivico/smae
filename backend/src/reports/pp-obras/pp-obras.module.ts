5d1ae066d747104d98759a7f2cbd1359e7c26293
lucasansei <lucas.ansei@eokoe.com>
2024-07-09 16:39:15 -0300

    Adicionando back-end de report de obras

---
+++ backend/src/reports/pp-obras/pp-obras.module.ts
@@ -0,0 +1,14 @@
+import { Module, forwardRef } from '@nestjs/common';
+import { ProjetoModule } from '../../pp/projeto/projeto.module';
+import { PrismaModule } from '../../prisma/prisma.module';
+import { PPObrasService } from './pp-obras.service';
+import { TarefaModule } from '../../pp/tarefa/tarefa.module';
+import { PPObrasController } from './pp-obras.controller';
+
+@Module({
+    imports: [PrismaModule, forwardRef(() => ProjetoModule), forwardRef(() => TarefaModule)],
+    controllers: [PPObrasController],
+    providers: [PPObrasService],
+    exports: [PPObrasService],
+})
+export class PPProjetosModule {}
