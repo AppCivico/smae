c2e41bd37a97496595d7812910f337e02a80c489
Renato CRON <renato.cron@gmail.com>
2024-07-16 01:34:21 -0300

    fix(sql): corrige a agregação de módulos no trigger update_modulos_sistemas para resolver erro escondido pela trigger no after: in an aggregate with DISTINCT, ORDER BY expressions must appear in argument list

--- backend/prisma/migrations/20240716020313_priv_arr/migration.sql
+++ backend/prisma/migrations/20240716020313_priv_arr/migration.sql
@@ -8,16 +8,20 @@ RETURNS TRIGGER AS $$
 DECLARE
     new_modulos_sistemas_array "ModuloSistema"[];
 BEGIN
-    SELECT ARRAY_AGG(DISTINCT unnest(pm.modulo_sistema) ORDER BY 1)
+    SELECT ARRAY_AGG(DISTINCT ms ORDER BY ms)
     INTO new_modulos_sistemas_array
-    FROM Privilegio p
-    JOIN Privilegio_Modulo pm ON p.modulo_id = pm.id
-    WHERE p.id IN (SELECT privilegio_id FROM Perfil_Privilegio WHERE perfil_acesso_id = NEW.perfil_acesso_id);
+    FROM (
+        SELECT unnest(pm.modulo_sistema) AS ms
+        FROM Privilegio p
+        JOIN Privilegio_Modulo pm ON p.modulo_id = pm.id
+        WHERE p.id IN (SELECT privilegio_id FROM Perfil_Privilegio WHERE perfil_acesso_id = NEW.perfil_acesso_id)
+    ) sub;
 
     UPDATE Perfil_Acesso pa
     SET modulos_sistemas = new_modulos_sistemas_array
     WHERE pa.id = NEW.perfil_acesso_id
     AND pa.modulos_sistemas IS DISTINCT FROM new_modulos_sistemas_array;
+
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
@@ -28,13 +32,13 @@ BEGIN
     WITH updated_modulos AS (
         SELECT
             pp.perfil_acesso_id,
-            ARRAY_AGG(DISTINCT unnest(pm.modulo_sistema) ORDER BY 1) AS new_modulos_sistemas_array
+            ARRAY_AGG(DISTINCT ms ORDER BY ms) AS new_modulos_sistemas_array
         FROM Perfil_Privilegio pp
         JOIN Privilegio p ON p.id = pp.privilegio_id
         JOIN Privilegio_Modulo pm ON pm.id = p.modulo_id
+        CROSS JOIN LATERAL unnest(pm.modulo_sistema) AS ms
         WHERE pp.privilegio_id = NEW.id
-        GROUP BY
-            pp.perfil_acesso_id
+        GROUP BY pp.perfil_acesso_id
     )
     UPDATE Perfil_Acesso pa
     SET modulos_sistemas = um.new_modulos_sistemas_array
@@ -45,4 +49,3 @@ BEGIN
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
-
