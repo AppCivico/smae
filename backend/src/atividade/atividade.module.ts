f56f71746cf1838d185d44427592f0213374e7db
Renato Cron <renato.cron@gmail.com>
2024-07-19 19:43:45 -0300

    refactor(atividade.module.ts): remove CronogramaEtapaService dos providers refactor(cronograma-etapas.module.ts): reorganiza imports para consistência refactor(iniciativa.module.ts): remove CronogramaEtapaService dos providers refactor(meta.module.ts): adiciona forwardRef para CronogramaEtapaModule refactor(meta.service.ts): adiciona forwardRef para CronogramaEtapaService

--- backend/src/atividade/atividade.module.ts
+++ backend/src/atividade/atividade.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { CronogramaEtapaService } from 'src/cronograma-etapas/cronograma-etapas.service';
 import { MetaModule } from '../meta/meta.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { VariavelModule } from '../variavel/variavel.module';
@@ -7,8 +6,8 @@ import { AtividadeController } from './atividade.controller';
 import { AtividadeService } from './atividade.service';
 
 @Module({
-    imports: [PrismaModule, MetaModule, VariavelModule, ],
+    imports: [PrismaModule, MetaModule, VariavelModule],
     controllers: [AtividadeController],
-    providers: [AtividadeService, CronogramaEtapaService],
+    providers: [AtividadeService],
 })
 export class AtividadeModule {}
--- backend/src/meta/meta.module.ts
+++ backend/src/meta/meta.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { CronogramaEtapaModule } from 'src/cronograma-etapas/cronograma-etapas.module';
 import { UploadModule } from 'src/upload/upload.module';
 import { GeoLocModule } from '../geo-loc/geo-loc.module';
@@ -7,7 +7,7 @@ import { MetaController, MetaSetorialController } from './meta.controller';
 import { MetaService } from './meta.service';
 
 @Module({
-    imports: [PrismaModule, CronogramaEtapaModule, UploadModule, GeoLocModule],
+    imports: [PrismaModule, forwardRef(() => CronogramaEtapaModule), UploadModule, GeoLocModule],
     controllers: [MetaController, MetaSetorialController],
     providers: [MetaService],
     exports: [MetaService],
