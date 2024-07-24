916b297d371fc3658df75c6dd106afcae9a912b7
Renato Cron <renato.cron@gmail.com>
2024-07-24 17:49:34 -0300

    Adicionando mais TODOs para validacao de orcamento em metas

--- backend/prisma/migrations/20240724063704_trigger_tsvector/migration.sql
+++ backend/prisma/migrations/20240724063704_trigger_tsvector/migration.sql
@@ -50,3 +50,20 @@ CREATE TRIGGER trigger_transferencia_parlamentar_update_tsvector_insert
 BEFORE INSERT ON transferencia_parlamentar
 FOR EACH ROW
 EXECUTE PROCEDURE f_transferencia_update_tsvector();
+
+CREATE OR REPLACE VIEW view_meta_responsavel_orcamento AS
+SELECT
+    mr.meta_id, mr.pessoa_id
+FROM
+    meta_responsavel mr
+    JOIN meta_orgao mo ON mo.orgao_id = mr.orgao_id AND mr.meta_id = mo.meta_id
+    join pessoa_perfil pp on pp.pessoa_id = mr.pessoa_id
+    join perfil_acesso pa on pp.perfil_acesso_id = pa.id
+    join perfil_privilegio priv on priv.perfil_acesso_id = pa.id
+    join privilegio p on p.id = priv.privilegio_id
+    join public.pessoa pessoa on pessoa.id = pp.pessoa_id AND pessoa.desativado = false
+WHERE mo.responsavel AND p.codigo in (
+    'CadastroMeta.orcamento', 'CadastroMeta.administrador_orcamento' ,
+    'CadastroMetaPS.orcamento', 'CadastroMetaPS.administrador_orcamento'
+)
+group by 1, 2;
