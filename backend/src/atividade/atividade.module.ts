cae64b31dc72bb2d43d984ce72987101bfc2a0d6
Renato Cron <renato.cron@gmail.com>
2024-07-23 22:56:23 -0300

    feat(atividade.module.ts): adiciona AtividadeSetorialController aos controllers do módulo de atividade

--- backend/src/atividade/atividade.module.ts
+++ backend/src/atividade/atividade.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { MetaModule } from '../meta/meta.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { VariavelModule } from '../variavel/variavel.module';
-import { AtividadeController } from './atividade.controller';
+import { AtividadeController, AtividadeSetorialController } from './atividade.controller';
 import { AtividadeService } from './atividade.service';
 
 @Module({
     imports: [PrismaModule, MetaModule, VariavelModule],
-    controllers: [AtividadeController],
+    controllers: [AtividadeController, AtividadeSetorialController],
     providers: [AtividadeService],
 })
 export class AtividadeModule {}
