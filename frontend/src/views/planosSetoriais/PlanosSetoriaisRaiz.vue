0a64ca454db79c85ef1cf5ec20e8ef354a97298b
Sobral <robson@oficiodesign.com>
2024-07-31 22:06:11 -0300

    test: dynamic store ID

--- frontend/src/views/planosSetoriais/PlanosSetoriaisRaiz.vue
+++ frontend/src/views/planosSetoriais/PlanosSetoriaisRaiz.vue
@@ -1,5 +1,15 @@
 <script setup>
 import Dashboard from '@/components/DashboardLayout.vue';
+import { useCounterStore } from '@/stores/counter.store';
+import { storeToRefs } from 'pinia';
+
+const a = useCounterStore('a');
+const b = useCounterStore('b');
+const counterStore = useCounterStore();
+
+const { count: defaultCounter } = storeToRefs(counterStore);
+const { count: countA } = storeToRefs(a);
+const { count: countB } = storeToRefs(b);
 
 </script>
 <template>
@@ -8,6 +18,35 @@ import Dashboard from '@/components/DashboardLayout.vue';
       v-if="!$route.meta.desabilitarMigalhasDePãoPadrão"
       class="mb1"
     />
+
+    <div class="flex g2">
+      <button
+        type="button"
+        class="f1"
+        @click="counterStore.increment()"
+      >
+        Increment defaultCounter
+      </button>
+      <button
+        type="button"
+        class="f1"
+        @click="a.increment()"
+      >
+        Increment A
+      </button>
+      <button
+        type="button"
+        class="f1"
+        @click="b.increment()"
+      >
+        Increment B
+      </button>
+    </div>
+
+    <pre>defaultCountB@PlanosSetoriaisRaiz.vue: {{ defaultCounter }}</pre>
+    <pre>countA@PlanosSetoriaisRaiz.vue: {{ countA }}</pre>
+    <pre>countB@PlanosSetoriaisRaiz.vue: {{ countB }}</pre>
+
     <router-view />
   </Dashboard>
 </template>
