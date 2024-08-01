5ca2fc16b40b4a9733f6fa3aabf6de66a34938ad
Renato Cron <renato.cron@gmail.com>
2024-08-01 10:49:27 -0300

    feat(func.ts): adiciona remoção de palavras comuns para melhorar a correspondência de strings fuzzy
    Essa mudança visa aprimorar a função de correspondência de strings, removendo palavras comuns que podem interferir na precisão da comparação.

--- utils/importacao-obras/func.ts
+++ utils/importacao-obras/func.ts
@@ -9,6 +9,19 @@ export function matchStringFuzzy(str1: string | undefined | null, str2: string |
             .normalize('NFD')
             .replace(/[\u0300-\u036f]/g, '');
 
+        // remove ' de '
+        tmp = tmp.replace(/ de /g, ' ');
+        // remove ' da '
+        tmp = tmp.replace(/ da /g, ' ');
+        // remove ' do '
+        tmp = tmp.replace(/ do /g, ' ');
+        // remove ' dos '
+        tmp = tmp.replace(/ dos /g, ' ');
+        // remove ' das '
+        tmp = tmp.replace(/ das /g, ' ');
+        // remove ' e '
+        tmp = tmp.replace(/ e /g, ' ');
+
         tmp = tmp.replace(/\s+/g, ' ');
         tmp = tmp.replace(/[^a-z0-9 ]/g, '');
         // remove left zero
