b8cc57ad53e6460ef4b8ad1f9457918ec430019b
Giovana <f_gi@live.com>
2024-08-05 13:18:08 -0300

    feat: edicionando rotas de contrato do projeto

---
+++ frontend/src/router/projetos.contratos.js
@@ -0,0 +1,90 @@
+import formatProcesso from '@/helpers/formatProcesso';
+import { useProcessosStore } from '@/stores/processos.store.ts';
+import ContratosCriarEditar from '@/views/mdo.contratos/ContratosCriarEditar.vue';
+
+export default {
+  path: 'contratos',
+  component: () => import('@/views/mdo.contratos/ContratosRaiz.vue'),
+
+  props: true,
+
+  children: [
+    {
+
+      name: 'contratosDoProjetoListar',
+      path: '',
+      component: () => import('@/views/mdo.contratos/ContratosLista.vue'),
+      meta: {
+        título: 'Contratos de projeto',
+        títuloParaMenu: 'Contratos',
+      },
+      props: true,
+    },
+
+    {
+      name: 'contratosDoProjetoCriar',
+      path: 'novo',
+      component: ContratosCriarEditar,
+      meta: {
+        título: 'Novo contrato',
+        títuloParaMenu: 'Novo contrato',
+
+        rotaDeEscape: 'contratosDoProjetoListar',
+
+        rotasParaMigalhasDePão: [
+          'contratosDoProjetoListar',
+          'contratosDoProjetoResumo',
+          'contratosDoProjetoListar',
+        ],
+      },
+    },
+
+    {
+      path: ':contratoId',
+      component: () => import('@/views/mdo.contratos/ContratosItem.vue'),
+      props: true,
+
+      children: [
+        {
+          path: '',
+          name: 'contratosDoProjetoEditar',
+          component: ContratosCriarEditar,
+          props: true,
+
+          meta: {
+            títuloParaMenu: 'Editar processo',
+
+            rotaDeEscape: 'contratosDoProjetoListar',
+
+            rotasParaMigalhasDePão: [
+              'contratosDoProjetoListar',
+              'contratosDoProjetoResumo',
+              'contratosDoProjetoListar',
+            ],
+          },
+        },
+
+        {
+          path: 'resumo',
+          name: 'contratosDoProjetoResumo',
+          component: () => import('@/views/mdo.contratos/ContratosResumo.vue'),
+          props: true,
+          meta: {
+            título: () => {
+              const daApi = useProcessosStore()?.emFoco?.processo_sei;
+
+              return daApi ? `Resumo do contrato ${formatProcesso(daApi)}` : 'Resumo de contrato';
+            },
+            títuloParaMenu: 'Resumo',
+
+            rotasParaMigalhasDePão: [
+              'contratosDoProjetoListar',
+              'contratosDoProjetoResumo',
+              'contratosDoProjetoListar',
+            ],
+          },
+        },
+      ],
+    },
+  ],
+};
