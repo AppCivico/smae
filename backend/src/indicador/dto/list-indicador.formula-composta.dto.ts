7dbd2129d58204490f4f593e6591233f1a7f033a
Renato Cron <renato.cron@gmail.com>
2024-08-12 17:17:33 -0300

    Fix para linhas

--- backend/src/indicador/dto/list-indicador.formula-composta.dto.ts
+++ backend/src/indicador/dto/list-indicador.formula-composta.dto.ts
@@ -1,5 +1,5 @@
 import { IndicadorFormulaCompostaDto } from '../entities/indicador.formula-composta.entity';
 
 export class ListIndicadorFormulaCompostaItemDto {
-    rows: IndicadorFormulaCompostaDto[];
+    linhas: IndicadorFormulaCompostaDto[];
 }
