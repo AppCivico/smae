8d572c8fc635379620e586cd87c6db86ba16f16e
Giovana <f_gi@live.com>
2024-08-12 08:18:13 -0300

    feat: adicionando estrutura de telas para crud de tipo de variavel categorica

---
+++ frontend/src/views/variaveisCategoricas/VariaveisCategoricasCriarEditar.vue
@@ -0,0 +1,152 @@
+<template>
+  <MigalhasDePão class="mb1" />
+  <div class="flex spacebetween center mb2">
+    <h1> <span v-if="!variavelId">Novo</span> Tipo de variavel categorica</h1>
+    <hr class="ml2 f1">
+    <CheckClose />
+  </div>
+  <Form
+    v-slot="{ errors, isSubmitting }"
+    :validation-schema="schema"
+    :initial-values="itemParaEdição"
+    @submit="onSubmit"
+  >
+    <div class="flex g2 mb1">
+      <div class="f1">
+        <LabelFromYup
+          name="tipo"
+          :schema="schema"
+        />
+        <Field
+          name="tipo"
+          type="text"
+          min="3"
+          max="250"
+          class="inputtext light mb1"
+        />
+        <ErrorMessage
+          class="error-msg mb1"
+          name="tipo"
+        />
+      </div>
+      <div class="f1">
+        <LabelFromYup
+          name="valores"
+          :schema="schema"
+        />
+        <Field
+          name="valores"
+          as="select"
+          min="3"
+          max="250"
+          class="inputtext light mb1"
+        >
+          <option :value="null">
+            Selecionar
+          </option>
+          <option
+            v-for="item, index in tipoDeVariaveisCategorigacas"
+            :key="index"
+            :value="item.valor"
+          >
+            {{ item.nome }}
+          </option>
+        </Field>
+
+        <ErrorMessage
+          class="error-msg mb1"
+          name="valores"
+        />
+      </div>
+    </div>
+    <FormErrorsList :errors="errors" />
+
+    <div class="flex spacebetween center mb2">
+      <hr class="mr2 f1">
+      <button
+        class="btn big"
+        :disabled="isSubmitting || Object.keys(errors)?.length"
+        :title="
+          Object.keys(errors)?.length
+            ? `Erros de preenchimento: ${Object.keys(errors)?.length}`
+            : null
+        "
+      >
+        Salvar
+      </button>
+      <hr class="ml2 f1">
+    </div>
+  </Form>
+
+  <span
+    v-if="chamadasPendentes?.emFoco"
+    class="spinner"
+  >Carregando</span>
+
+  <div
+    v-if="erro"
+    class="error p1"
+  >
+    <div class="error-msg">
+      {{ erro }}
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { storeToRefs } from 'pinia';
+import { useRoute, useRouter } from 'vue-router';
+import { ErrorMessage, Field, Form } from 'vee-validate';
+import tipoDeVariaveisCategorigacas from '@/consts/tipoDeVariaveisCategorigacas';
+import { useVariaveisCategoricasStore } from '@/stores/variaveisCategoricas.store';
+import { useAlertStore } from '@/stores/alert.store';
+import { variávelCategórica as schema } from '@/consts/formSchemas';
+
+const router = useRouter();
+const route = useRoute();
+const props = defineProps({
+  variavelId: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const alertStore = useAlertStore();
+const variaveisCategoricasStore = useVariaveisCategoricasStore();
+const { chamadasPendentes, erro, itemParaEdição } = storeToRefs(variaveisCategoricasStore);
+
+async function onSubmit(values) {
+  try {
+    let response;
+    const msg = props.variavelId
+      ? 'Dados salvos com sucesso!'
+      : 'Item adicionado com sucesso!';
+
+    const dataToSend = { ...values };
+
+    if (route.params?.variavelId) {
+      response = await variaveisCategoricasStore.salvarItem(
+        dataToSend,
+        route.params.variavelId,
+      );
+    } else {
+      response = await variaveisCategoricasStore.salvarItem(dataToSend);
+    }
+    if (response) {
+      alertStore.success(msg);
+      variaveisCategoricasStore.$reset();
+      router.push({ name: 'variaveisCategoricasListar' });
+    }
+  } catch (error) {
+    alertStore.error(error);
+  }
+}
+
+variaveisCategoricasStore.$reset();
+// não foi usada a prop.variavelId pois estava vazando do edit na hora de criar uma nova
+if (route.params?.variavelId) {
+  variaveisCategoricasStore.buscarItem(route.params?.variavelId);
+}
+</script>
+
+<style></style>
---
+++ frontend/src/views/variaveisCategoricas/VariaveisCategoricasLista.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="flex spacebetween center mb2">
+    <TítuloDePágina />
+    <hr class="ml2 f1">
+    <router-link
+      :to="{ name: 'variaveisCategoricasCriar' }"
+      class="btn big ml1"
+    >
+      Novo tipo de variável categórica
+    </router-link>
+  </div>
+
+  <table class="tablemain">
+    <col>
+    <col>
+    <col>
+    <col class="col--botão-de-ação">
+    <col class="col--botão-de-ação">
+    <thead>
+      <tr>
+        <th> Titulo </th>
+        <th> Tipo </th>
+        <th> Descrição </th>
+        <th />
+        <th />
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="item in lista"
+        :key="item.id"
+      >
+        <td>{{ item.titulo }}</td>
+        <td>{{ item.tipo }}</td>
+        <td>{{ item.descricao }}</td>
+        <td>
+          <router-link
+            :to="{ name: 'variaveisCategoricasCriar', params: { variavelId: item.id } }"
+            class="tprimary"
+          >
+            <svg
+              width="20"
+              height="20"
+            ><use xlink:href="#i_edit" /></svg>
+          </router-link>
+        </td>
+        <td>
+          <button
+            class="like-a__text"
+            arial-label="excluir"
+            title="excluir"
+            @click="excluirAssunto(item.id, item.titulo)"
+          >
+            <svg
+              width="20"
+              height="20"
+            ><use xlink:href="#i_remove" /></svg>
+          </button>
+        </td>
+      </tr>
+      <tr v-if="chamadasPendentes.lista">
+        <td colspan="3">
+          Carregando
+        </td>
+      </tr>
+      <tr v-else-if="erro">
+        <td colspan="3">
+          Erro: {{ erro }}
+        </td>
+      </tr>
+      <tr v-else-if="!lista.length">
+        <td colspan="3">
+          Nenhum resultado encontrado.
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script setup>
+import { storeToRefs } from 'pinia';
+import { useRoute } from 'vue-router';
+import { useAlertStore } from '@/stores/alert.store';
+import { useVariaveisCategoricasStore } from '@/stores/variaveisCategoricas.store';
+
+const route = useRoute();
+
+const alertStore = useAlertStore();
+const variaveisCategoricasStore = useVariaveisCategoricasStore();
+const { lista, chamadasPendentes, erro } = storeToRefs(variaveisCategoricasStore);
+
+async function excluirAssunto(id, descricao) {
+  alertStore.confirmAction(
+    `Deseja mesmo remover "${descricao}"?`,
+    async () => {
+      if (await variaveisCategoricasStore.excluirItem(id)) {
+        variaveisCategoricasStore.$reset();
+        variaveisCategoricasStore.buscarTudo({ pdm_id: route.params.planoSetorialId });
+        alertStore.success(`"${descricao}" removido.`);
+      }
+    },
+    'Remover',
+  );
+}
+
+variaveisCategoricasStore.$reset();
+variaveisCategoricasStore.buscarTudo();
+</script>
+
+<style></style>
