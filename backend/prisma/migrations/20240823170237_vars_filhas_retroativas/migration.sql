fccaf7fccf5a17d0442c4326993762be824e0726
lucasansei <lucas.ansei@eokoe.com>
2024-08-23 14:36:12 -0300

    Fix: migration

--- backend/prisma/migrations/20240823170237_vars_filhas_retroativas/migration.sql
+++ backend/prisma/migrations/20240823170237_vars_filhas_retroativas/migration.sql
@@ -1,7 +1,6 @@
 UPDATE variavel AS child
 SET variavel_mae_id = parent.id
 FROM variavel AS parent
-WHERE parent.tipo = 'Global' AND child.tipo = 'Global'
+WHERE parent.tipo = 'Global' AND child.tipo = 'Global' AND parent.id <> child.id
   AND child.codigo LIKE '%/%'
   AND parent.codigo = SPLIT_PART(child.codigo, '/', 1) || '/' || SPLIT_PART(parent.codigo, '/', 2);
-  
