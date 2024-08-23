1d32dfecfec9d115600e1e1fe79fd6f33e4a005f
Renato Cron <renato.cron@gmail.com>
2024-08-23 09:22:22 -0300

    Fix ultimo_periodo_valido pra anos

--- backend/prisma/manual-copy/periodicidade_intervalo.pgsql
+++ backend/prisma/manual-copy/periodicidade_intervalo.pgsql
@@ -31,9 +31,17 @@ RETURNS DATE
 AS $$
 DECLARE
     vUltimoPeriodo DATE;
+    vIntervalo INTERVAL;
 BEGIN
     vUltimoPeriodo := date_trunc('month', now() AT TIME ZONE 'America/Sao_Paulo') - (pAtrasoMeses || ' months')::interval;
-    vUltimoPeriodo := vUltimoPeriodo - ((EXTRACT(MONTH FROM vUltimoPeriodo)::integer - 1) % (EXTRACT(MONTH FROM periodicidade_intervalo(pPeriodicidade))::integer)) * '1 month'::interval;
+    vIntervalo := periodicidade_intervalo(pPeriodicidade);
+
+    IF EXTRACT(YEAR FROM vIntervalo) > 0 THEN
+        vUltimoPeriodo := date_trunc('year', vUltimoPeriodo);
+        vUltimoPeriodo := vUltimoPeriodo - ((EXTRACT(YEAR FROM vUltimoPeriodo)::integer % EXTRACT(YEAR FROM vIntervalo)::integer) * '1 year'::interval);
+    ELSE
+        vUltimoPeriodo := vUltimoPeriodo - ((EXTRACT(MONTH FROM vUltimoPeriodo)::integer - 1) % (EXTRACT(MONTH FROM vIntervalo)::integer)) * '1 month'::interval;
+    END IF;
 
     RETURN vUltimoPeriodo;
 END;
--- backend/prisma/migrations/20240823120228_lido/migration.sql
+++ backend/prisma/migrations/20240823120228_lido/migration.sql
@@ -1,2 +1,22 @@
 -- AlterTable
 ALTER TABLE "status_sei" ADD COLUMN     "usuarios_lidos" INTEGER[];
+CREATE OR REPLACE FUNCTION ultimo_periodo_valido(pPeriodicidade "Periodicidade", pAtrasoMeses INT)
+RETURNS DATE
+AS $$
+DECLARE
+    vUltimoPeriodo DATE;
+    vIntervalo INTERVAL;
+BEGIN
+    vUltimoPeriodo := date_trunc('month', now() AT TIME ZONE 'America/Sao_Paulo') - (pAtrasoMeses || ' months')::interval;
+    vIntervalo := periodicidade_intervalo(pPeriodicidade);
+
+    IF EXTRACT(YEAR FROM vIntervalo) > 0 THEN
+        vUltimoPeriodo := date_trunc('year', vUltimoPeriodo);
+        vUltimoPeriodo := vUltimoPeriodo - ((EXTRACT(YEAR FROM vUltimoPeriodo)::integer % EXTRACT(YEAR FROM vIntervalo)::integer) * '1 year'::interval);
+    ELSE
+        vUltimoPeriodo := vUltimoPeriodo - ((EXTRACT(MONTH FROM vUltimoPeriodo)::integer - 1) % (EXTRACT(MONTH FROM vIntervalo)::integer)) * '1 month'::interval;
+    END IF;
+
+    RETURN vUltimoPeriodo;
+END;
+$$ LANGUAGE plpgsql;
