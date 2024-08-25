cf981b48446561f03f9ad0833f6e8b48a2cb0e63
Giovana <f_gi@live.com>
2024-08-25 14:26:46 -0300

    feat: organizando alguns dados na tela de detalhe da tarefa

--- frontend/src/views/tarefas/TarefasDetalhe.vue
+++ frontend/src/views/tarefas/TarefasDetalhe.vue
@@ -3,29 +3,31 @@
     <h1 class="mt1 mb1">
       Resumo Atividade
     </h1>
-    <div class="flex spacebetween">
+    <div class="flex spacebetween maxw mb1">
       <dl>
-        <dt class="tc500 w700">
+        <dt class="tc500 w700 t16 mb025">
           Tarefa
         </dt>
         <dd> {{ emFoco?.tarefa }}</dd>
       </dl>
       <dl>
-        <dt class="tc500 w700">
+        <dt class="tc500 w700 t16 mb025">
           Marco do projeto?
         </dt>
         <dd> {{ emFoco?.eh_marco }}</dd>
       </dl>
     </div>
-    <div class="flex spacebetween">
+    <div class="flex spacebetween maxw mb1">
       <dl>
-        <dt class="tc500 w700">
+        <dt class="tc500 w700 t16 mb025">
           Tarefa-mãe
         </dt>
-        <dd> emFoco.tarefa_pai_id: {{ emFoco?.tarefa_pai_id }}</dd>
+        <dd>
+          <strong>Pego de onde?</strong>
+        </dd>
       </dl>
       <dl>
-        <dt class="tc500 w700">
+        <dt class="tc500 w700 t16 mb025">
           Ordem
         </dt>
         <dd>
@@ -33,7 +35,7 @@
         </dd>
       </dl>
       <dl>
-        <dt class="tc500 w700">
+        <dt class="tc500 w700 t16 mb025">
           Orgão responsável
         </dt>
         <dd>
@@ -41,23 +43,134 @@
         </dd>
       </dl>
     </div>
-    <dl>
-      <dt class="tc500 w700">
+    <dl class="mb1">
+      <dt class="tc500 w700 t16 mb025">
         Responsável pela atividade
       </dt>
       <dd> {{ emFoco?.projeto?.responsavel?.nome_exibicao }}</dd>
     </dl>
     <dl>
-      <dt class="tc500 w700">
+      <dt class="tc500 w700 t16 mb025 ">
         Descrição
       </dt>
       <dd> {{ emFoco?.descricao }}</dd>
     </dl>
-    <div>
+    <dl>
+      <div class="flex spacebetween center mt2 mb2">
+        <dt class="tc300 w700 t16">
+          Dependências
+        </dt>
+        <hr class="ml2 f1">
+      </div>
+      <dd>
+        <table class="tablemain wrap">
+          <thead>
+            <tr>
+              <td class="tc300 w700">
+                TAREFA RELACIONADA
+              </td>
+              <td class="tc300 w700">
+                TIPO DE RELAÇÃO
+              </td>
+              <td class="tc300 w700">
+                DIAS DE LATÊNCIA
+              </td>
+            </tr>
+          </thead>
+          <tbody>
+            <td>
+              <strong>Pego de onde?</strong>
+            </td>
+            <td>
+              <strong>Pego de onde?</strong>
+            </td>
+            <td>
+              <strong>Pego de onde?</strong>
+            </td>
+          </tbody>
+        </table>
+      </dd>
+      <hr class="mt3 mb3">
+    </dl>
+    <div class="flex spacebetween maxw">
+      <dl>
+        <dt class="tc500 w700 t16">
+          Previsão de início
+        </dt>
+        <dd> {{ emFoco?.inicio_planejado }}</dd>
+      </dl>
+      <dl>
+        <dt class="tc500 w700 t16">
+          Duração prevista
+        </dt>
+        <dd> {{ emFoco?.duracao_planejado }}</dd>
+      </dl>
+      <dl>
+        <dt class="tc500 w700 t16">
+          Previsão de término
+        </dt>
+        <dd> {{ emFoco?.termino_planejado }}</dd>
+      </dl>
+      <dl>
+        <dt class="tc500 w700 t16">
+          Atraso
+        </dt>
+        <dd> {{ emFoco?.atraso }}</dd>
+      </dl>
+      <dl>
+        <dt class="tc500 w700 t16">
+          Previsão de custo
+        </dt>
+        <dd> {{ emFoco?.custo_estimado }}</dd>
+      </dl>
+    </div>
+
+    <dl class="mt1 mb1">
+      <div class="flex spacebetween center">
+        <dt class="tc300 w700 t16">
+          Execução da atividade
+        </dt>
+        <hr class="ml2 f1">
+      </div>
+      <div class="flex spacebetween maxw">
+        <dl>
+          <dt class="tc500 w700 t16">
+            Data de início real
+          </dt>
+          <dd>{{ emFoco?.inicio_real }}</dd>
+        </dl>
+        <dl>
+          <dt class="tc500 w700 t16">
+            Duração real
+          </dt>
+          <dd>{{ emFoco?.duracao_real }}</dd>
+        </dl>
+        <dl>
+          <dt class="tc500 w700 t16">
+            Data de término real
+          </dt>
+          <dd>{{ emFoco?.termino_real }}</dd>
+        </dl>
+        <dl>
+          <dt class="tc500 w700 t16">
+            Custo real
+          </dt>
+          <dd>{{ emFoco?.custo_real }}</dd>
+        </dl>
+        <dl>
+          <dt class="tc500 w700 t16">
+            Percentual concluído
+          </dt>
+          <dd>{{ emFoco?.percentual_concluido }}%</dd>
+        </dl>
+      </div>
+    </dl>
+    <div class="mt4">
       emFoco: <pre>{{ emFoco }}</pre>
     </div>
   </div>
 </template>
+
 <script setup>
 import { storeToRefs } from 'pinia';
 import { useTarefasStore } from '@/stores/tarefas.store.ts';
@@ -73,5 +186,7 @@ const {
 </script>
 
 <style>
-
+.maxw{
+  max-width: 900px;
+}
 </style>
