90b005f383cdc3b394dc55f7f9937a1de33c107d
Giovana <f_gi@live.com>
2024-08-27 14:54:45 -0300

    fix: ajustando breadbrumb de detalhe do plano de ação do projeto

--- frontend/src/router/planosDeAcao.js
+++ frontend/src/router/planosDeAcao.js
@@ -142,7 +142,6 @@ export default {
               'projetosResumo',
               'riscosListar',
               'planosDeAçãoListar',
-              'detalhes',
             ],
           },
         },
