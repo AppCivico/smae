f910995dc0dca1f240294b8e68649d186a3b2027
Gustavo Soares <gustavo.fe.soares@gmail.com>
2024-08-28 14:19:26 -0300

    feat: criado arquivo de rotas

---
+++ frontend/src/views/transferenciasVoluntarias/sections/ComunicadosGerais/routes/comunicadosGerais.routes.js
@@ -0,0 +1,14 @@
+import ComuncadosGerais from '../index.vue';
+
+const comunicadosGeraisRoutes = [
+  {
+    name: 'ComunicadosGerais',
+    path: 'comunicados-gerais',
+    component: ComuncadosGerais,
+    meta: {
+      título: 'Comunicados Gerais',
+    },
+  },
+];
+
+export default comunicadosGeraisRoutes;
---
+++ frontend/src/views/transferenciasVoluntarias/sections/routes/index.routes.js
@@ -0,0 +1,5 @@
+import comunicadosGeraisRoutes from '../ComunicadosGerais/routes/comunicadosGerais.routes';
+
+const transferenciasVoluntariasRoutes = [...comunicadosGeraisRoutes];
+
+export default transferenciasVoluntariasRoutes;
