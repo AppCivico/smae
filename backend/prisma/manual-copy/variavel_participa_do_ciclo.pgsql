15ca96ca56b48fa85c854619feaf79956780c402
Renato Cron <renato.cron@gmail.com>
2024-08-30 13:46:56 -0300

    Fix edge case do variavel_participa_do_ciclo que usa o busca_periodos_variavel

--- backend/prisma/manual-copy/variavel_participa_do_ciclo.pgsql
+++ backend/prisma/manual-copy/variavel_participa_do_ciclo.pgsql
@@ -6,7 +6,15 @@ CREATE OR REPLACE FUNCTION variavel_participa_do_ciclo (pVariavelId int, dataCic
             SELECT
                 TRUE
             FROM
-            busca_periodos_variavel (pVariavelId) AS g (periodo, inicio, fim),
+            busca_periodos_variavel (pVariavelId, (
+                -- funcao apenas para PDM, se usar isso no PS vai dar errado
+                SELECT indicador_id
+                FROM indicador_variavel iv
+                WHERE iv.variavel_id = pVariavelId
+                AND iv.indicador_origem_id IS NULL
+                AND desativado = false
+                LIMIT 1
+            )) AS g (periodo, inicio, fim),
             generate_series(inicio, fim, periodo) p
         WHERE
             p.p = dataCiclo), FALSE);
--- backend/prisma/migrations/20240829193329_trigger_transf_parlamentar/migration.sql
+++ backend/prisma/migrations/20240829193329_trigger_transf_parlamentar/migration.sql
@@ -115,3 +115,28 @@ END;
 $$
 LANGUAGE plpgsql STABLE;
 
+CREATE OR REPLACE FUNCTION variavel_participa_do_ciclo (pVariavelId int, dataCiclo date)
+    RETURNS boolean
+    AS $$
+    SELECT
+        coalesce((
+            SELECT
+                TRUE
+            FROM
+            busca_periodos_variavel (pVariavelId, (
+                -- funcao apenas para PDM, se usar isso no PS vai dar errado
+                SELECT indicador_id
+                FROM indicador_variavel iv
+                WHERE iv.variavel_id = pVariavelId
+                AND iv.indicador_origem_id IS NULL
+                AND desativado = false
+                LIMIT 1
+            )) AS g (periodo, inicio, fim),
+            generate_series(inicio, fim, periodo) p
+        WHERE
+            p.p = dataCiclo), FALSE);
+
+$$
+LANGUAGE SQL COST 10000
+STABLE;
+
