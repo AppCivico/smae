d88e76f4c413732d169db4376a060e5be8712d84
Renato Cron <renato.cron@gmail.com>
2024-09-01 22:59:28 -0300

    feat(migration.sql): adiciona migração para criar índice único na tabela serie_variavel

---
+++ backend/prisma/migrations/20240902015559_idx/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[serie,variavel_id,data_valor]` on the table `serie_variavel` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- DropIndex
+DROP INDEX "idx_serie_variavel_variavel_id_data_valor";
+
+-- CreateIndex
+CREATE UNIQUE INDEX "idx_serie_variavel_variavel_id_data_valor" ON "serie_variavel"("serie", "variavel_id", "data_valor");
