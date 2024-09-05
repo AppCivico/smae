99bee24e6f0f7bb649cfb232a9f376c6e3278c04
Renato Cron <renato.cron@gmail.com>
2024-09-05 14:34:20 -0300

    Fix default for ativos

---
+++ backend/prisma/migrations/20240905173120_fix_defaults/migration.sql
@@ -0,0 +1,11 @@
+-- AlterTable
+ALTER TABLE "atividade" ALTER COLUMN "ativo" SET DEFAULT true;
+
+-- AlterTable
+ALTER TABLE "cronograma" ALTER COLUMN "ativo" SET DEFAULT true;
+
+-- AlterTable
+ALTER TABLE "iniciativa" ALTER COLUMN "ativo" SET DEFAULT true;
+
+-- AlterTable
+ALTER TABLE "meta" ALTER COLUMN "ativo" SET DEFAULT true;
