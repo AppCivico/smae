77eedd84ccfa39e58a1c0ac06c44d68bdebf04b6
Renato Cron <renato.cron@gmail.com>
2024-09-06 01:23:30 -0300

    ..

--- backend/src/transfere-gov-sync/transfere-gov-sync.service.ts
+++ backend/src/transfere-gov-sync/transfere-gov-sync.service.ts
@@ -212,7 +212,7 @@ export class TransfereGovSyncService {
                             numero: item.numero,
                             ano: item.ano,
                             link: item.link,
-                            publicado_em: item.publicado_em.toJSON(),
+                            publicado_em: item.publicado_em,
                         },
                         dispara_email: false,
                         tipo_nota_id: CONST_TIPO_NOTA_TRANSF_GOV,
