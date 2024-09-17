e0e4325909579ad23ee8106b268fae5e4b45aa5f
Giovana <f_gi@live.com>
2024-09-17 10:46:22 -0300

    feat: adicionando telas de oportunidades

---
+++ frontend/src/views/ps.oportunidades/OportunidadesLista.vue
@@ -0,0 +1,250 @@
+<template>
+  <div class="flex spacebetween center mb2">
+    <TítuloDePágina />
+    <hr class="ml2 f1">
+  </div>
+
+  <form
+    class="flex flexwrap bottom mb2 g1"
+    @submit.prevent="atualizarUrl"
+  >
+    <div class="f0">
+      <label
+        for="ano"
+        class="label tc300"
+      >Ano</label>
+      <input
+        id="ano"
+        v-model.number="ano"
+        inputmode="numeric"
+        class="inputtext mb1"
+        name="ano"
+        type="number"
+        min="2003"
+        max="9999"
+      >
+    </div>
+
+    <div class="f0">
+      <label
+        for="esfera"
+        class="label tc300"
+      >Modalidade</label>
+      <!-- <select
+        id="esfera"
+        v-model.trim="esfera"
+        class="inputtext mb1"
+        name="esfera"
+      >
+        <option value="" />
+        <option
+          v-for="item in Object.values(esferasDeTransferencia)"
+          :key="item.valor"
+          :value="item.valor"
+        >
+          {{ item.nome }}
+        </option>
+      </select> -->
+    </div>
+
+    <div class="f0">
+      <label
+        for="palavras_chave"
+        class="label tc300"
+      >Palavra-chave</label>
+      <input
+        id="palavras_chave"
+        v-model.trim="palavraChave"
+        class="inputtext"
+        name="palavras_chave"
+        type="text"
+      >
+    </div>
+    <button class="btn outline bgnone tcprimary mtauto mb1">
+      Filtrar
+    </button>
+  </form>
+  <!-- <pre>{{ lista }}</pre> -->
+
+  <table class="tablemain mb1">
+    <col>
+    <col>
+    <col>
+    <col>
+    <col>
+    <col>
+    <col>
+    <col>
+    <col>
+    <col>
+    <col class="col--botão-de-ação">
+    <thead>
+      <tr>
+        <th>
+          Órgão
+        </th>
+        <th>
+          Programa
+        </th>
+        <th>
+          Código do programa
+        </th>
+        <th>
+          Situação
+        </th>
+        <th>
+          Data de disponibilização
+        </th>
+        <th>
+          Início das propostas
+        </th>
+        <th>
+          Fim das propostas
+        </th>
+        <th>
+          Modalidade
+        </th>
+        <th>
+          Ação Orçamentária
+        </th>
+        <th>
+          Avaliação
+        </th>
+        <th />
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="item in lista"
+        :key="item.id"
+      >
+        <td>
+          {{ item.desc_orgao_sup_programa }}
+        </td>
+        <td>
+          {{ item.nome_programa }}
+        </td>
+        <td>
+          {{ item.cod_programa }}
+        </td>
+        <td>
+          {{ item.sit_programa }}
+        </td>
+        <td>
+          {{ item.data_disponibilizacao }}
+        </td>
+        <td>
+          {{ item.dt_ini_receb }}
+        </td>
+        <td>
+          {{ item.dt_fim_receb }}
+        </td>
+        <td>
+          {{ item.modalidade_programa }}
+        </td>
+        <td>
+          {{ item.acao_orcamentaria }}
+        </td>
+        <td>
+          {{ item.avaliacao }}
+        </td>
+        <td>
+          <button
+
+            class="like-a__text"
+            arial-label="excluir"
+            title="excluir"
+            @click="excluirTransferencia(item.id)"
+          >
+            <svg
+              width="20"
+              height="20"
+            >
+              <use xlink:href="#i_remove" />
+            </svg>
+          </button>
+        </td>
+        <td />
+      </tr>
+      <tr v-if="chamadasPendentes.lista">
+        <td colspan="11">
+          Carregando
+        </td>
+      </tr>
+      <tr v-else-if="erro">
+        <td colspan="11">
+          Erro: {{ erro }}
+        </td>
+      </tr>
+      <tr v-else-if="!lista.length">
+        <td colspan="11">
+          Nenhum resultado encontrado.
+        </td>
+      </tr>
+    </tbody>
+  </table>
+
+  <button
+    v-if="paginação.temMais && paginação.tokenDaPróximaPágina"
+    :disabled="chamadasPendentes.lista"
+    class="btn bgnone outline center"
+    @click="oportunidades.buscarTudo({
+      ...route.query,
+      token_proxima_pagina: paginação.tokenDaPróximaPágina
+    })"
+  >
+    carregar mais
+  </button>
+</template>
+
+<script setup>
+import { storeToRefs } from 'pinia';
+import { ref, watch } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+import { useAlertStore } from '@/stores/alert.store';
+import { useOportunidadesStore } from '@/stores/oportunidades.store';
+
+const route = useRoute();
+const router = useRouter();
+const oportunidades = useOportunidadesStore();
+// const transferenciasVoluntarias = useTransferenciasVoluntariasStore();
+
+const alertStore = useAlertStore();
+
+const ano = ref(route.query.ano);
+const palavras_chave = ref(route.query.palavras_chave);
+
+const {
+  lista, chamadasPendentes, erro, paginação,
+} = storeToRefs(oportunidades);
+
+function atualizarUrl() {
+  router.push({
+    query: {
+      ...route.query,
+      ano: ano.value || undefined,
+      palavras_chave: palavraChave.value || undefined,
+    },
+  });
+}
+
+watch([
+  () => route.query.ano,
+  () => route.query.palavras_chave,
+], () => {
+  console.log('entrou no watch');
+  let { ano: anoParaBusca, palavras_chave: palavraChaveParaBusca } = route.query;
+  if (typeof anoParaBusca === 'string') {
+    anoParaBusca = anoParaBusca.trim();
+  }
+  if (typeof palavraChaveParaBusca === 'string') {
+    palavraChaveParaBusca = palavraChaveParaBusca.trim();
+  }
+  oportunidades.$reset();
+  oportunidades.buscarTudo({
+    ano: anoParaBusca,
+    palavras_chave: palavraChaveParaBusca,
+  });
+}, { immediate: true });
+
+</script>
---
+++ frontend/src/views/ps.oportunidades/OportunidadesRaiz.vue
@@ -0,0 +1,14 @@
+<template>
+  <Dashboard>
+    <router-view />
+  </Dashboard>
+</template>
+<script setup>
+import { onUnmounted } from 'vue';
+import { Dashboard } from '@/components';
+import { useOportunidadesStore } from '@/stores/oportunidades.store';
+
+const oportunidades = useOportunidadesStore();
+
+onUnmounted(() => { oportunidades.$reset(); });
+</script>
