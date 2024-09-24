b5ad02fc85a6dbe1f53d86589987415f3ff8909d
Renato Cron <renato.cron@gmail.com>
2024-09-24 12:24:54 -0300

    fix(projeto-orcamento.controller.ts): corrige o tipo do DTO no método create fix(projeto-previsao-custo.controller.ts): corrige o tipo do DTO no método create refactor(projeto-orcamento.controller.ts): reorganiza importações para melhor legibilidade refactor(projeto-previsao-custo.controller.ts): reorganiza importações para melhor legibilidade

--- backend/src/reports/projeto-orcamento/projeto-orcamento.controller.ts
+++ backend/src/reports/projeto-orcamento/projeto-orcamento.controller.ts
@@ -1,9 +1,9 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { Roles } from 'src/auth/decorators/roles.decorator';
-import { OrcamentoService } from '../orcamento/orcamento.service';
-import { CreateRelProjetoOrcamentoDto } from './dto/create-projeto-orcamento.dto';
 import { ListOrcamentoExecutadoDto } from '../orcamento/entities/orcamento-executado.entity';
+import { OrcamentoService } from '../orcamento/orcamento.service';
+import { CreateRelObrasOrcamentoDto, CreateRelProjetoOrcamentoDto } from './dto/create-projeto-orcamento.dto';
 
 @ApiTags('Relatórios - API')
 @Controller('relatorio/projeto-orcamento')
@@ -26,7 +26,7 @@ export class ProjetoMDOOrcamentoController {
     @Post()
     @ApiBearerAuth('access-token')
     @Roles(['Reports.executar.MDO'])
-    async create(@Body() createPrevisaoCustDto: CreateRelProjetoOrcamentoDto): Promise<ListOrcamentoExecutadoDto> {
+    async create(@Body() createPrevisaoCustDto: CreateRelObrasOrcamentoDto): Promise<ListOrcamentoExecutadoDto> {
         return await this.orcamentoExecutadoService.asJSON(createPrevisaoCustDto);
     }
 }
--- backend/src/reports/projeto-previsao-custo/projeto-previsao-custo.controller.ts
+++ backend/src/reports/projeto-previsao-custo/projeto-previsao-custo.controller.ts
@@ -2,7 +2,10 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { Roles } from 'src/auth/decorators/roles.decorator';
 import { ListPrevisaoCustoDto } from '../previsao-custo/entities/previsao-custo.entity';
-import { CreateRelProjetoPrevisaoCustoDto } from './dto/create-projeto-previsao-custo.dto';
+import {
+    CreateRelObrasPrevisaoCustoDto,
+    CreateRelProjetoPrevisaoCustoDto,
+} from './dto/create-projeto-previsao-custo.dto';
 import { PrevisaoCustoService } from '../previsao-custo/previsao-custo.service';
 
 @ApiTags('Relatórios - API')
@@ -26,7 +29,7 @@ export class ProjetoMDOPrevisaoCustoController {
     @Post()
     @ApiBearerAuth('access-token')
     @Roles(['Reports.executar.MDO'])
-    async create(@Body() createPrevisaoCustDto: CreateRelProjetoPrevisaoCustoDto): Promise<ListPrevisaoCustoDto> {
+    async create(@Body() createPrevisaoCustDto: CreateRelObrasPrevisaoCustoDto): Promise<ListPrevisaoCustoDto> {
         return await this.previsaoCustoService.asJSON(createPrevisaoCustDto);
     }
 }
