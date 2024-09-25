1ebeb0b732d9653ad5f1899ae2d58cc2523f0cf8
Mario H. Nogueira <mario.nogueira@clueit.com.br>
2024-09-25 10:28:21 -0300

    Remocao da redundância das migrations

--- backend/prisma/manual-copy/S18.10TV002-Inclusao-classficacao.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE transferencia ADD classificacao_id int4 NULL;
-ALTER TABLE transferencia ADD CONSTRAINT transferencia_classificacao_id_fkey FOREIGN KEY (classificacao_id) REFERENCES classificacao(id) ON DELETE RESTRICT ON UPDATE RESTRICT;
