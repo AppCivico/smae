d49941fc2f933405d1369365c71cb76495acd201
Renato Cron <renato.cron@gmail.com>
2024-10-02 16:41:31 -0300

    WIP broken commit ALTER TYPE "FonteRelatorio" ADD VALUE PSMonitoramentoMensal;

--- backend/prisma/migrations/20240930131535_fonte_relatorio/migration.sql
+++ backend/prisma/migrations/20240930131535_fonte_relatorio/migration.sql
@@ -1,3 +1,4 @@
 ALTER TYPE "FonteRelatorio" ADD VALUE 'PSPrevisaoCusto';
 ALTER TYPE "FonteRelatorio" ADD VALUE 'PSOrcamento';
 ALTER TYPE "FonteRelatorio" ADD VALUE 'CasaCivilAtvPendentes';
+ALTER TYPE "FonteRelatorio" ADD VALUE 'PSMonitoramentoMensal';
