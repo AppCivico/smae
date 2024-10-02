0eaffc0383a4144e7946f038472ac96ef9f370dd
Renato Cron <renato.cron@gmail.com>
2024-10-02 05:45:28 -0300

    feat(pgsql-migrate): adiciona suporte para modo de observação com yargs e chokidar refactor(pgsql-migrate): reorganiza a lógica de processamento de arquivos .pgsql em uma função separada chore(package.json): adiciona dependências para yargs e chokidar

--- backend/bin/pgsql-migrate.ts
+++ backend/bin/pgsql-migrate.ts
@@ -1,12 +1,20 @@
+import { Logger } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import * as crypto from 'crypto';
 import * as fs from 'fs';
 import * as path from 'path';
-import * as crypto from 'crypto';
-import { PrismaClient } from '@prisma/client';
-import { Logger } from '@nestjs/common';
 import { Pool } from 'pg';
-
 const prisma = new PrismaClient();
 const logger = new Logger('PgsqlMigrate');
+
+let ignore_added = true;
+let yargs: any, chokidar: any;
+try {
+    yargs = require('yargs');
+    chokidar = require('chokidar');
+} catch (error) {
+    logger.warn('yargs and chokidar are not available. Watch mode will be disabled.');
+}
 
 type IFilenameHash = {
     fileName: string;
@@ -16,51 +24,68 @@ type IFilenameHash = {
 async function main() {
     const pgsqlDir = process.env.PGSQL_DIR || './pgsql';
 
+    const createTablesPromise = createMigrationTables();
     try {
-        // Check if directory exists
-        if (!fs.existsSync(pgsqlDir)) {
-            throw new Error(`Directory ${pgsqlDir} does not exist`);
-        }
-
-        const files = fs.readdirSync(pgsqlDir).filter((file) => file.endsWith('.pgsql'));
-
-        if (files.length === 0) {
-            logger.warn(`No .pgsql files found in ${pgsqlDir}`);
-            return;
-        }
-
-        const createTablesPromise = createMigrationTables();
-        const fileHashes: IFilenameHash[] = await Promise.all(
-            files.map(async (file) => {
-                const filePath = path.join(pgsqlDir, file);
-                return {
-                    fileName: file,
-                    hash: await calculateSHA256(filePath),
-                    content: await fs.promises.readFile(filePath, 'utf-8'),
-                };
-            })
-        );
-
+        await processPgsqlFiles(pgsqlDir);
         await createTablesPromise;
 
-        const dbEntries = await getExistingMigrations();
+        if (yargs && chokidar) {
+            const argv = await yargs.option('watch', { alias: 'w', description: 'Watch mode', type: 'boolean' }).help()
+                .argv;
 
-        const filesToUpdate = fileHashes.filter((file) => {
-            const dbEntry = dbEntries.find((entry) => entry.file_name === file.fileName);
-            return !dbEntry || dbEntry.hash !== file.hash;
-        });
-
-        if (filesToUpdate.length > 0) {
-            const feedback = await updateFunctions(filesToUpdate);
-            logger.log(`Updated ${feedback.ok} PostgreSQL function(s), ${feedback.error} errored.`);
+            if (argv.watch) {
+                logger.log(`Watching ${pgsqlDir} for changes...`);
+                const watcher = chokidar.watch(pgsqlDir, { ignored: /(^\|[\/\\])\../, persistent: true });
+                watcher
+                    .on('add', (path: string) => handleFileChange(path, 'added'))
+                    .on('change', (path: string) => handleFileChange(path, 'changed'))
+                    .on('unlink', (path: string) => logger.log(`File ${path} has been removed`));
+                setTimeout(() => {
+                    ignore_added = false;
+                }, 1000);
+            }
         } else {
-            logger.log('No PostgreSQL functions need updating.');
+            logger.log('Watch mode is not available in this environment.');
         }
     } catch (error) {
         logger.error('An error occurred during migration:', error.stack);
         process.exit(1);
     } finally {
         await prisma.$disconnect();
+    }
+}
+
+async function processPgsqlFiles(dir: string) {
+    const files = fs.readdirSync(dir).filter((file) => file.endsWith('.pgsql'));
+
+    if (files.length === 0) {
+        logger.warn(`No .pgsql files found in ${dir}`);
+        return;
+    }
+
+    const fileHashes: IFilenameHash[] = await Promise.all(
+        files.map(async (file) => {
+            const filePath = path.join(dir, file);
+            return {
+                fileName: file,
+                hash: await calculateSHA256(filePath),
+                content: await fs.promises.readFile(filePath, 'utf-8'),
+            };
+        })
+    );
+
+    const dbEntries = await getExistingMigrations();
+
+    const filesToUpdate = fileHashes.filter((file) => {
+        const dbEntry = dbEntries.find((entry) => entry.file_name === file.fileName);
+        return !dbEntry || dbEntry.hash !== file.hash;
+    });
+
+    if (filesToUpdate.length > 0) {
+        const feedback = await updateFunctions(filesToUpdate);
+        logger.log(`Updated ${feedback.ok} PostgreSQL function(s), ${feedback.error} errored.`);
+    } else {
+        logger.log('No PostgreSQL functions need updating.');
     }
 }
 
@@ -163,6 +188,13 @@ async function calculateSHA256(filePath: string): Promise<string> {
         stream.on('data', (chunk) => hash.update(chunk));
         stream.on('end', () => resolve(hash.digest('hex')));
     });
+}
+
+async function handleFileChange(filePath: string, changeType: 'added' | 'changed') {
+    if (path.extname(filePath) === '.pgsql' && ignore_added == false) {
+        logger.log(`File ${filePath} has been ${changeType}`);
+        await processPgsqlFiles(path.dirname(filePath));
+    }
 }
 
 main().catch((e) => {
