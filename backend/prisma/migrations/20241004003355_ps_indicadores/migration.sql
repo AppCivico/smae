6e9d9615e1d465f9347fd9e1ca84a1ddb4e5039e
Renato Cron <renato.cron@gmail.com>
2024-10-03 23:47:16 -0300

    Add ix_uniq_global_analise_no_ciclo

--- backend/prisma/migrations/20241004003355_ps_indicadores/migration.sql
+++ backend/prisma/migrations/20241004003355_ps_indicadores/migration.sql
@@ -1,2 +1,5 @@
 -- AlterEnum
 ALTER TYPE "FonteRelatorio" ADD VALUE 'PSIndicadores';
+
+create unique index ix_uniq_global_analise_no_ciclo on variavel_global_ciclo_analise(variavel_id, referencia_data, fase)
+    where removido_em is null and ultima_revisao = true;
