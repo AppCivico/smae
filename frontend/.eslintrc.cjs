9a2ce99dae4775fda57d1090c2eae310a4339ffa
Sobral <robson@oficiodesign.com>
2024-10-08 17:08:31 -0300

    fix: Erros de validação de caminhos no VSCode

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -32,7 +32,7 @@ module.exports = {
     'import/resolver': {
       alias: {
         map: [
-          ['@', './src'],
+          ['@', './frontend/src'],
         ],
         extensions: ['.js', '.ts', '.vue'],
       },
