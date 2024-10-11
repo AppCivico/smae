1abaf7202e739541f4c88ee63fd22056de0fc134
Giovana <f_gi@live.com>
2024-10-11 17:05:19 -0300

    refactor: adicionando rolagem da tabela

--- frontend/src/components/painelEstrategico/TabelaProjetos.vue
+++ frontend/src/components/painelEstrategico/TabelaProjetos.vue
@@ -11,56 +11,75 @@ const props = defineProps({
 
 const projetosOrganizados = computed(() => props.projetos);
 </script>
+
 <template>
   <CardEnvelope.Conteudo>
     <CardEnvelope.Titulo>
       Projetos
     </CardEnvelope.Titulo>
-    <table class="tabela-projetos mt1">
-      <thead>
-        <tr>
-          <th>Nome do Projeto</th>
-          <th>Secretaria</th>
-          <th>Meta</th>
-          <th>Status</th>
-          <th>Etapa Atual</th>
-          <th>Término Projetado</th>
-          <th>Riscos em Aberto</th>
-          <th>% de Atraso</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr
-          v-for="(projeto, index) in projetosOrganizados"
-          :key="index"
-        >
-          <td>{{ projeto.nome }}</td>
-          <td>{{ projeto.secretaria }}</td>
-          <td>{{ projeto.meta }}</td>
-          <td>{{ projeto.status }}</td>
-          <td>{{ projeto.etapaAtual }}</td>
-          <td>{{ projeto.terminoProjetado }}</td>
-          <td>{{ projeto.riscosEmAberto }}</td>
-          <td>{{ projeto.percentualAtraso }}</td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="tabela-wrapper">
+      <table class="tabela-projetos mt1">
+        <colgroup>
+          <col>
+          <col>
+          <col>
+          <col>
+          <col>
+          <col>
+          <col>
+          <col>
+        </colgroup>
+        <thead>
+          <tr>
+            <th>Nome do Projeto</th>
+            <th>Secretaria</th>
+            <th>Meta</th>
+            <th>Status</th>
+            <th>Etapa Atual</th>
+            <th>Término Projetado</th>
+            <th>Riscos em Aberto</th>
+            <th>% de Atraso</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="(projeto, index) in projetosOrganizados"
+            :key="index"
+          >
+            <td>{{ projeto.nome }}</td>
+            <td>{{ projeto.secretaria }}</td>
+            <td>{{ projeto.meta }}</td>
+            <td>{{ projeto.status }}</td>
+            <td>{{ projeto.etapaAtual }}</td>
+            <td>{{ projeto.terminoProjetado }}</td>
+            <td>{{ projeto.riscosEmAberto }}</td>
+            <td>{{ projeto.percentualAtraso }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </CardEnvelope.Conteudo>
 </template>
 
-<style scoped>
-.tabela-projetos {
-  width: 100%;
-  border-collapse: collapse;
-}
+  <style scoped>
+  .tabela-wrapper {
+    width: 100%;
+    overflow-x: auto;
+  }
 
-.tabela-projetos th, .tabela-projetos td {
-  border-bottom: 1px solid #ddd;
-  padding: 8px;
-  text-align: center;
-}
+  .tabela-projetos {
+    width: 100%;
+    border-collapse: collapse;
+    min-width: 1000px;
+  }
 
-.tabela-projetos th {
-  font-weight: bold;
-}
-</style>
+  .tabela-projetos th, .tabela-projetos td {
+    border-bottom: 1px solid #ddd;
+    padding: 8px;
+    text-align: center;
+  }
+
+  .tabela-projetos th {
+    font-weight: bold;
+  }
+  </style>
