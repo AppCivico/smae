aa481c781896f286384c99db64ec68b954d8397d
Renato Cron <renato.cron@gmail.com>
2024-10-11 17:56:09 -0300

    feat(prisma): adiciona função para garantir a unidade de medida "Sem unidade de medida" fix(unidade-medida.service.ts): exclui unidade de medida "Sem unidade de medida" das operações refactor(variavel.service.ts): atualiza campos categóricos para usar "Sem unidade de medida"

--- backend/src/common/consts.ts
+++ backend/src/common/consts.ts
@@ -2,5 +2,6 @@ export const CONST_BOT_USER_ID = -1;
 export const CONST_CRONO_VAR_CATEGORICA_ID = -1;
 export const CONST_TIPO_NOTA_DIST_RECURSO = -1;
 export const CONST_TIPO_NOTA_TRANSF_GOV = -2;
+export const CONST_VAR_SEM_UN_MEDIDA = -1;
 export const CONST_COD_NOTA_DIST_RECURSO = 'Aviso de distribuição de recurso';
 export const CONST_COD_NOTA_TRANSF_GOV = 'Transfere Gov';
