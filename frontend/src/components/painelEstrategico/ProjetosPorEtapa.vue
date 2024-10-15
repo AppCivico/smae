31d35d8998475ea3e74b0a51fb65e0a2fa05c839
eduruiz <contato@eduruiz.com.br>
2024-10-15 12:38:05 -0300

    feat: adiciona tooltip nos gráficos de projeto por etapa/status

--- frontend/src/components/painelEstrategico/ProjetosPorEtapa.vue
+++ frontend/src/components/painelEstrategico/ProjetosPorEtapa.vue
@@ -20,48 +20,27 @@ const props = defineProps({
 
 const etapas = computed(() => props.projetosPorEtapas.map((item) => item.etapa));
 const quantidades = computed(() => props.projetosPorEtapas.map((item) => item.quantidade));
+const total = computed(() => quantidades.value.reduce((acc, item) => acc + item, 0));
 
 const projetosPorEtapaChartOptions = computed(
   () => criaOpcoesDoGraficoDeProjetos(etapas.value, quantidades.value),
 );
 
+function calculaPorcentagem(valor: number) {
+  return Math.round((valor / total.value) * 100);
+}
+
 function formatTooltip(param: TooltipOptions) {
   return `
-    <div class="projeto-por-etapa-tooltip">
-      <h5 class="projeto-por-etapa-tooltip__valor">${param.data}</h5>
-      <h6 class="projeto-por-etapa-tooltip__label">${param.name}</h6>
+    <div class="projeto-tooltip">
+      <h5 class="projeto-tooltip__valor">${param.data}</h5>
+      <h6 class="projeto-tooltip__label">${param.name}</h6>
 
-      <span class="projeto-por-etapa-tooltip__porcentagem">
-        27%
+      <span class="projeto-tooltip__porcentagem">
+        ${calculaPorcentagem(param.data)}%
       </span>
     </div>
   `;
 }
 
 </script>
-
-<style>
-.projeto-por-etapa-tooltip__valor,
-.projeto-por-etapa-tooltip__label,
-.projeto-por-etapa-tooltip__porcentagem {
-  font-weight: 600;
-  margin: 0;
-  line-height: 1.2;
-}
-
-.projeto-por-etapa-tooltip__valor {
-  font-size: 15px;
-}
-
-.projeto-por-etapa-tooltip__label {
-  text-transform: uppercase;
-  font-size: 10px;
-  color: #7E858D;
-}
-
-.projeto-por-etapa-tooltip__porcentagem {
-  font-size: 30px;
-  font-weight: 600;
-  line-height: 39px;
-  }
-</style>
--- frontend/src/components/painelEstrategico/ProjetosPorStatus.vue
+++ frontend/src/components/painelEstrategico/ProjetosPorStatus.vue
@@ -2,13 +2,14 @@
   <GraficoDashboard
     class="pb3"
     :option="projetosPorStatusChartOptions"
+    :tooltip-template="formatTooltip"
   />
 </template>
 
-<script setup>
+<script lang="ts" setup>
 import { computed } from 'vue';
 import criaOpcoesDoGraficoDeProjetos from '@/helpers/criaOpcoesDoGraficoDeProjetos';
-import GraficoDashboard from '@/components/graficos/GraficoDashboard.vue';
+import GraficoDashboard, { TooltipOptions } from '@/components/graficos/GraficoDashboard.vue';
 
 const props = defineProps({
   projetosPorStatus: {
@@ -19,12 +20,26 @@ const props = defineProps({
 
 const status = computed(() => props.projetosPorStatus.map((item) => item.status));
 const quantidades = computed(() => props.projetosPorStatus.map((item) => item.quantidade));
+const total = computed(() => quantidades.value.reduce((acc, item) => acc + item, 0));
 
 const projetosPorStatusChartOptions = computed(
   () => criaOpcoesDoGraficoDeProjetos(status.value, quantidades.value),
 );
 
-</script>
+function calculaPorcentagem(valor: number) {
+  return Math.round((valor / total.value) * 100);
+}
 
-<style scoped>
-</style>
+function formatTooltip(param: TooltipOptions) {
+  return `
+    <div class="projeto-tooltip">
+      <h5 class="projeto-tooltip__valor">${param.data}</h5>
+      <h6 class="projeto-tooltip__label">${param.name}</h6>
+
+      <span class="projeto-tooltip__porcentagem">
+        ${calculaPorcentagem(param.data)}%
+      </span>
+    </div>
+  `;
+}
+</script>
