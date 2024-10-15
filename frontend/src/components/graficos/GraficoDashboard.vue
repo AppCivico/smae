bc52a913623417ef2fe26da0727b0bf2b5ba5c92
Gustavo Soares <gustavo.fe.soares@gmail.com>
2024-10-15 11:54:56 -0300

    chore: adicionando tooltip ao projeto por etapa

--- frontend/src/components/graficos/GraficoDashboard.vue
+++ frontend/src/components/graficos/GraficoDashboard.vue
@@ -69,7 +69,7 @@ const preparedOptions = computed(() => {
       // show: true,
       // alwaysShowContent: true,
       renderMode: 'html',
-      className: 'grafico-dashboard__tooltip-conteudo',
+      className: 'grafico-dashboard__tooltip',
       formatter: (params: any) => {
         const tooltipText = tooltipTemplate({
           data: params.data,
@@ -77,7 +77,11 @@ const preparedOptions = computed(() => {
           name: params.name,
         });
 
-        return tooltipText;
+        return `
+          <div style="color: ${params.color}" class="grafico-dashboard__tooltip-conteudo">
+            ${tooltipText}
+          </div>
+        `;
       },
     },
   };
@@ -91,12 +95,11 @@ const preparedOptions = computed(() => {
 </style>
 
 <style lang="less">
-.grafico-dashboard__tooltip-conteudo {
+.grafico-dashboard__tooltip {
   background-color: #E8E8E8 !important;
   border-radius: 10px !important;
   padding: 5px !important;
   border: none !important;
-  min-width: 150px;
 
   display: flex !important;
   flex-direction: column;
@@ -109,5 +112,9 @@ const preparedOptions = computed(() => {
     color: #232046;
     background: #232046;
   }
+}
+
+.grafico-dashboard__tooltip-conteudo {
+  padding: 5px 0 8px;
 }
 </style>
--- frontend/src/components/painelEstrategico/ProjetosPorEtapa.vue
+++ frontend/src/components/painelEstrategico/ProjetosPorEtapa.vue
@@ -2,13 +2,14 @@
   <GraficoDashboard
     class="pb3"
     :option="projetosPorEtapaChartOptions"
+    :tooltip-template="formatTooltip"
   />
 </template>
 
-<script setup>
+<script lang="ts" setup>
 import { defineProps, computed } from 'vue';
 import criaOpcoesDoGraficoDeProjetos from '@/helpers/criaOpcoesDoGraficoDeProjetos';
-import GraficoDashboard from '@/components/graficos/GraficoDashboard.vue';
+import GraficoDashboard, { TooltipOptions } from '@/components/graficos/GraficoDashboard.vue';
 
 const props = defineProps({
   projetosPorEtapas: {
@@ -24,4 +25,43 @@ const projetosPorEtapaChartOptions = computed(
   () => criaOpcoesDoGraficoDeProjetos(etapas.value, quantidades.value),
 );
 
+function formatTooltip(param: TooltipOptions) {
+  return `
+    <div class="projeto-por-etapa-tooltip">
+      <h5 class="projeto-por-etapa-tooltip__valor">${param.data}</h5>
+      <h6 class="projeto-por-etapa-tooltip__label">${param.name}</h6>
+
+      <span class="projeto-por-etapa-tooltip__porcentagem">
+        27%
+      </span>
+    </div>
+  `;
+}
+
 </script>
+
+<style>
+.projeto-por-etapa-tooltip__valor,
+.projeto-por-etapa-tooltip__label,
+.projeto-por-etapa-tooltip__porcentagem {
+  font-weight: 600;
+  margin: 0;
+  line-height: 1.2;
+}
+
+.projeto-por-etapa-tooltip__valor {
+  font-size: 15px;
+}
+
+.projeto-por-etapa-tooltip__label {
+  text-transform: uppercase;
+  font-size: 10px;
+  color: #7E858D;
+}
+
+.projeto-por-etapa-tooltip__porcentagem {
+  font-size: 30px;
+  font-weight: 600;
+  line-height: 39px;
+  }
+</style>
