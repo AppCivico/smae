0874084b4aa43ffc10ded10ad7fafe36fa3815e3
Renato Cron <renato.cron@gmail.com>
2024-10-17 15:23:45 -0300

    fix(auth.service.ts): remove o cast desnecessário de pessoa para Pessoa feat(auth.service.ts): adiciona verificação para pessoa_fisica nula em métodos de busca refactor(pessoa.entity.ts): altera tipo de orgao para OrgaoResumo e define pessoa_fisica como não opcional refactor(pessoa.service.ts): atualiza métodos para usar PessoaDto em vez de Pessoa e simplifica busca de pessoa

--- backend/src/pessoa/entities/pessoa.entity.ts
+++ backend/src/pessoa/entities/pessoa.entity.ts
@@ -1,5 +1,5 @@
 import { PessoaFisica } from '@prisma/client';
-import { OrgaoDto } from '../../orgao/entities/orgao.entity';
+import { OrgaoResumo } from '../../orgao/entities/orgao.entity';
 
 export class Pessoa {
     id?: number;
@@ -10,13 +10,14 @@ export class Pessoa {
 
     atualizado_em?: Date;
 
-    orgao?: OrgaoDto;
+    orgao?: OrgaoResumo;
 
     token_acesso_api?: string;
     session_id?: number;
     senha_bloqueada?: boolean;
+    senha_bloqueada_em: Date | null;
 
     desativado: boolean;
 
-    pessoa_fisica?: PessoaFisica | null;
+    pessoa_fisica: PessoaFisica | null;
 }
