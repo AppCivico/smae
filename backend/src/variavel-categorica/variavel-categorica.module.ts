e2ccd3c56006cafc720cf4a0224945728cf9250c
Renato Cron <renato.cron@gmail.com>
2024-10-17 17:51:02 -0300

    feat(variavel-categorica.module.ts): adiciona o módulo VariavelModule como dependência do módulo VariavelCategorica

--- backend/src/variavel-categorica/variavel-categorica.module.ts
+++ backend/src/variavel-categorica/variavel-categorica.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
 import { VariavelCategoricaController } from './variavel-categorica.controller';
 import { VariavelCategoricaService } from './variavel-categorica.service';
+import { VariavelModule } from '../variavel/variavel.module';
 
 @Module({
-    imports: [PrismaModule],
+    imports: [PrismaModule, VariavelModule],
     controllers: [VariavelCategoricaController],
     providers: [VariavelCategoricaService],
     exports: [VariavelCategoricaService],
