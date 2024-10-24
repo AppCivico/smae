91aedbbc1b24eb2e43e6e21f859c37b515425df4
Renato Cron <renato.cron@gmail.com>
2024-10-24 17:36:39 -0300

    feat(prisma): adiciona a flag pp_pe ao modelo feature_flag para controle do Painel Estratégico de Projetos feat(FeatureFlagDto): documenta a nova flag pp_pe e atualiza descrições existentes fix(feature-flag.service): ajusta o retorno padrão para incluir a flag pp_pe

--- backend/src/auth/models/FeatureFlagDto.ts
+++ backend/src/auth/models/FeatureFlagDto.ts
@@ -1,4 +1,10 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class FeatureFlagDto {
+    @ApiProperty({ description: 'Sempre true, usada no PDM, habilita home ter o panorama com stats de metas' })
     panorama: boolean;
+    @ApiProperty({ description: 'Sempre true, usada no PDM, menu "Monitoramento" remodelado, já não pode ser false' })
     mf_v2: boolean;
+    @ApiProperty({ description: 'Habilita dashboard interno de projetos' })
+    pp_pe: boolean;
 }
