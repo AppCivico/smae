c16d6fbe8319b6a24141acb790d2f6a760045654
Gustavo Soares <gustavo.fe.soares@gmail.com>
2024-10-30 12:02:55 -0300

    S18.4bPS016 - remover acoes ps (#48)
    Card-trello:
    https://trello.com/c/8TFQpotw/1745-s184bps016-remover-a%C3%A7%C3%B5es-de-variaveis-em-ps

--- frontend/src/components/metas/GruposDeSerie.vue
+++ frontend/src/components/metas/GruposDeSerie.vue
@@ -1,8 +1,10 @@
 <script setup>
-import SmallModal from '@/components/SmallModal.vue';
-import dateToDate, { dateToMonthYear } from '@/helpers/dateToDate';
-import requestS from '@/helpers/requestS.ts';
 import { computed, defineProps, ref } from 'vue';
+
+import SmallModal from '@/components/SmallModal.vue';
+
+import requestS from '@/helpers/requestS.ts';
+import dateToDate, { dateToMonthYear } from '@/helpers/dateToDate';
 
 const showModal = ref(false);
 const analise = ref(null);
@@ -82,7 +84,7 @@ function obterValorTabela(item, index) {
   }
 
   if (temVariavelCategorica.value) {
-    return props.g.dados_auxiliares.categoricas[valor] || valor;
+    return props.g.dados_auxiliares.categoricas?.[valor] || valor;
   }
 
   return valor;
