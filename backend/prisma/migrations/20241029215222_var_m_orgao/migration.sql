34552407b255ddd566f8c833b9ebcf5b15047bb8
Renato CRON <renato.cron@gmail.com>
2024-10-31 16:15:51 -0300

    fix

--- backend/prisma/migrations/20241029215222_var_m_orgao/migration.sql
+++ backend/prisma/migrations/20241029215222_var_m_orgao/migration.sql
@@ -19,5 +19,4 @@ update variavel set possui_variaveis_filhas = true where id in (
 
 -- update variavel_categorica_id based on parent if variavel_categorica_id is null
 update variavel set variavel_categorica_id = (select variavel_categorica_id from variavel where id = variavel_mae_id)
-where variavel_categorica_id is null and variavel_mae_id is not null
-and variavel_categorica_id != (select variavel_categorica_id from variavel where id = variavel_mae_id);
+where variavel_categorica_id is null and variavel_mae_id is not null;
