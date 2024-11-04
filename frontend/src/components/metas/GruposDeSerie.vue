076d220c918948427d98fb23d7957692010b4847
Sobral <robson@oficiodesign.com>
2024-11-04 20:11:37 -0300

    feat: Exibir histórico do ciclo para variáveis dos Planos Setoriais

--- frontend/src/components/metas/GruposDeSerie.vue
+++ frontend/src/components/metas/GruposDeSerie.vue
@@ -1,11 +1,16 @@
 <script setup>
 import SmallModal from '@/components/SmallModal.vue';
-import dateToDate, { dateToMonthYear } from '@/helpers/dateToDate';
 import requestS from '@/helpers/requestS.ts';
 import { computed, defineProps, ref } from 'vue';
+import { useRoute } from 'vue-router';
+import CicloFisicoPdM from './CicloFisicoPdM.vue';
+import CicloFisicoPS from './CicloFisicoPS.vue';
+
+const route = useRoute();
 
 const showModal = ref(false);
 const analise = ref(null);
+const periodo = ref('');
 const props = defineProps({
   g: {
     type: Object,
@@ -22,20 +27,9 @@ const props = defineProps({
 });
 const baseUrl = `${import.meta.env.VITE_API_URL}`;
 
-const mappedValues = computed(() => {
-  const mapping = {
-    Previsto: '',
-    Realizado: '',
-    PrevistoAcumulado: '',
-    RealizadoAcumulado: '',
-  };
-
-  analise.value.ordem_series.forEach((item, index) => {
-    mapping[item] = analise.value.series[index]?.valor_nominal || '';
-  });
-
-  return mapping;
-});
+const conteudoDoModal = computed(() => (route.meta.entidadeMãe === 'planoSetorial'
+  ? CicloFisicoPS
+  : CicloFisicoPdM));
 
 const temVariavelCategorica = computed(() => !!props.g.variavel?.variavel_categorica_id);
 
@@ -51,17 +45,30 @@ function toggleAccordeon(t) {
   t.target.closest('.tzaccordeon').classList.toggle('active');
 }
 
+// TO-DO: Usar usar query strings para abrir o diálogo
 function openAnalise() {
   showModal.value = true;
 }
 
 function hasModal(cicloFisico) {
-  return cicloFisico?.analise || cicloFisico?.tem_documentos;
+  return route.meta.entidadeMãe === 'planoSetorial'
+    ? cicloFisico?.contagem_qualitativa
+    : cicloFisico?.analise || cicloFisico?.tem_documentos;
 }
 
+// TO-DO: Mover toda a lógica para o componente correspondente e
 async function buscarAnalise(dataValor, variavelId) {
   try {
-    analise.value = await requestS.get(`${baseUrl}/mf/metas/variaveis/analise-qualitativa`, { data_valor: dataValor, variavel_id: variavelId });
+    analise.value = route.meta.entidadeMãe === 'planoSetorial'
+      ? await requestS.get(`${baseUrl}/variavel-analise-qualitativa`, {
+        consulta_historica: true,
+        data_referencia: dataValor,
+        variavel_id: variavelId,
+      })
+      : await requestS.get(`${baseUrl}/mf/metas/variaveis/analise-qualitativa`, {
+        data_valor: dataValor,
+        variavel_id: variavelId,
+      });
   } catch (erro) {
     console.log(erro);
   }
@@ -69,7 +76,19 @@ async function buscarAnalise(dataValor, variavelId) {
 
 function handleClick(obj) {
   if (hasModal(obj.ciclo_fisico)) {
-    buscarAnalise(obj.series[0].data_valor, props.g.variavel?.id);
+    periodo.value = obj.periodo;
+
+    if (route.meta.entidadeMãe === 'planoSetorial') {
+      buscarAnalise(
+        `${obj.periodo}-01`,
+        props.g.variavel?.variavel_mae_id || props.g.variavel?.id,
+      );
+    } else {
+      buscarAnalise(
+        obj.series[0].data_valor,
+        props.g.variavel?.id,
+      );
+    }
     openAnalise();
   }
 }
@@ -109,99 +128,11 @@ function obterValorTabela(item, index) {
         @close="showModal = false"
       />
     </div>
-    <div>
-      <h3>
-        {{ analise?.variavel?.titulo }}
-      </h3>
-
-      <table class="tablemain">
-        <col>
-        <col>
-        <col>
-        <col>
-        <col>
-        <thead>
-          <tr>
-            <th> Mês/ano </th>
-            <th> Previsto mensal </th>
-            <th> Realizado mensal </th>
-            <th> Previsto acumulado</th>
-            <th> Realizado acumulado </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-if="analise?.series?.length">
-            <td>{{ dateToMonthYear(analise?.series[0]?.data_valor) }}</td>
-            <td>{{ mappedValues['Previsto'] }}</td>
-            <td>{{ mappedValues['Realizado'] }}</td>
-            <td>{{ mappedValues['PrevistoAcumulado'] }}</td>
-            <td>{{ mappedValues['RealizadoAcumulado'] }}</td>
-          </tr>
-        </tbody>
-      </table>
-      <div class="mt2 mb2">
-        <h5 class="mb0 tc300">
-          ANÁLISE
-        </h5>
-        <hr class="mt05 mb1">
-        <p>
-          {{ analise?.analises[0]?.analise_qualitativa }}
-        </p>
-        <hr class="mt1 mb1">
-      </div>
-      <div
-        v-if="analise?.arquivos.length"
-        class="mt2 mb2"
-      >
-        <table class="tablemain">
-          <col>
-          <col>
-          <col>
-          <col>
-          <col>
-          <thead>
-            <tr>
-              <th>Documento comprobatório</th>
-              <th />
-              <th />
-              <th />
-              <th />
-            </tr>
-          </thead>
-          <tbody>
-            <tr
-              v-for="arquivo in analise.arquivos"
-              :key="arquivo.id"
-            >
-              <td class="flex center">
-                <svg
-                  width="20"
-                  height="20"
-                  class="mr1"
-                ><use xlink:href="#i_doc" /></svg>
-                {{ arquivo?.arquivo.nome_original }}
-              </td>
-              <td>{{ arquivo.arquivo.descricao }}</td>
-              <td>{{ arquivo.criador.nome_exibicao }}</td>
-              <td>{{ dateToDate(arquivo.criado_em) }}</td>
-              <td>
-                <SmaeLink
-                  v-if="arquivo?.arquivo.download_token"
-                  :to="baseUrl + '/download/' + arquivo?.arquivo.download_token"
-                  download
-                >
-                  <svg
-                    width="20"
-                    height="20"
-                    class="mr1"
-                  ><use xlink:href="#i_download" /></svg>
-                </SmaeLink>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
+    <component
+      :is="conteudoDoModal"
+      :analise="analise"
+      :periodo="periodo"
+    />
   </SmallModal>
 
   <template v-if="g?.linhas">
