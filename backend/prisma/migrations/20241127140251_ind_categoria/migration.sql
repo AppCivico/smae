ca860a2f82bf9b023adc213b9731683629950dc1
Renato Cron <renato.cron@gmail.com>
2024-11-27 11:16:40 -0300

    add update on prisma/migrations/20241127140251_ind_categoria/migration.sql

--- backend/prisma/migrations/20241127140251_ind_categoria/migration.sql
+++ backend/prisma/migrations/20241127140251_ind_categoria/migration.sql
@@ -3,3 +3,5 @@ CREATE TYPE "IndicadorTipo" AS ENUM ('Numerico', 'Categorica');
 
 -- AlterTable
 ALTER TABLE "indicador" ADD COLUMN     "indicador_tipo" "IndicadorTipo" NOT NULL DEFAULT 'Numerico';
+
+update indicador set indicador_tipo = 'Categoria' where variavel_categoria_id is not null;
