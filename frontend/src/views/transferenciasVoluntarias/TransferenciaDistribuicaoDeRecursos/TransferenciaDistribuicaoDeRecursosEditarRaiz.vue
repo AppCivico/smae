4643f8416bd80b50b297bf27126bb9f760f8d47b
Gustavo Soares <gustavo.fe.soares@gmail.com>
2024-11-27 15:49:20 -0300

    feat: carregando valor padrao para transferencia

--- frontend/src/views/transferenciasVoluntarias/TransferenciaDistribuicaoDeRecursos/TransferenciaDistribuicaoDeRecursosEditarRaiz.vue
+++ frontend/src/views/transferenciasVoluntarias/TransferenciaDistribuicaoDeRecursos/TransferenciaDistribuicaoDeRecursosEditarRaiz.vue
@@ -6,11 +6,15 @@
 import { onMounted } from 'vue';
 import { useRoute } from 'vue-router';
 import { useDistribuicaoRecursosStore } from '@/stores/transferenciasDistribuicaoRecursos.store';
+import { useTransferenciasVoluntariasStore } from '@/stores/transferenciasVoluntarias.store';
 
 const { params } = useRoute();
 const distribuicaoRecursosStore = useDistribuicaoRecursosStore();
+const transferenciasVoluntariasStore = useTransferenciasVoluntariasStore();
 
 onMounted(() => {
+  transferenciasVoluntariasStore.buscarItem(params.transferenciaId);
+
   if (params.recursoId) {
     distribuicaoRecursosStore.buscarItem(params.recursoId);
   }
