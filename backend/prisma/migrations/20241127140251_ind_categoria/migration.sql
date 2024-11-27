8be82a46eaa9863b550140411024568ba5fc8e7b
Renato Cron <renato.cron@gmail.com>
2024-11-27 11:17:56 -0300

    typo

--- backend/prisma/migrations/20241127140251_ind_categoria/migration.sql
+++ backend/prisma/migrations/20241127140251_ind_categoria/migration.sql
@@ -4,4 +4,4 @@ CREATE TYPE "IndicadorTipo" AS ENUM ('Numerico', 'Categorica');
 -- AlterTable
 ALTER TABLE "indicador" ADD COLUMN     "indicador_tipo" "IndicadorTipo" NOT NULL DEFAULT 'Numerico';
 
-update indicador set indicador_tipo = 'Categoria' where variavel_categoria_id is not null;
+update indicador set indicador_tipo = 'Categorica' where variavel_categoria_id is not null;
