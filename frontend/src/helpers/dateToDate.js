4252ded59fd7d3b035add92dd142336ef4a3df67
Sobral <robson@oficiodesign.com>
2024-12-06 17:42:29 -0300

    shame: Revertendo temporariamente as mudanças em data

--- frontend/src/helpers/dateToDate.js
+++ frontend/src/helpers/dateToDate.js
@@ -1,28 +1,10 @@
-export function hasTimeInDate(date) {
-  if (date instanceof Date && !Number.isNaN(date.getTime())) {
-    return date.getUTCHours() !== null;
-  }
-
-  if (typeof date === 'string') {
-    const hasDateAndTime = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(Z|([+-]\d{2}:\d{2}))?$/.test(date);
-    const isOnlyTime = /^\d{2}:\d{2}:\d{2}$/.test(date);
-
-    return hasDateAndTime && !isOnlyTime;
-  }
-
-  return false;
-}
-
 /**
  * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/DateTimeFormat#options
  */
 function dateToDate(d, options = {}) {
-  if (!d || typeof d === 'boolean' || typeof d === 'number') return 'Invalid Date';
-
-  const timeZone = hasTimeInDate(d) ? 'America/Sao_Paulo' : 'UTC';
   const dd = d ? new Date(d) : false;
   if (!dd) return d;
-  const dx = (dd) ? dd.toLocaleString('pt-BR', { dateStyle: 'short', timeZone, ...options }) : '';
+  const dx = (dd) ? dd.toLocaleString('pt-BR', { dateStyle: 'short', timeZone: 'UTC', ...options }) : '';
   return dx || '';
 }
 
@@ -33,7 +15,6 @@ export const dateToMonth = (date, format = 'numeric') => dateToDate(date, { date
 export const dateToDay = (date, format = 'numeric') => dateToDate(date, { dateStyle: undefined, day: format });
 
 export const dateToMonthYear = (date) => {
-  if (!date || typeof date === 'number') return 'Invalid Date';
   const month = dateToMonth(date, '2-digit');
   const year = dateToYear(date, 'numeric');
   return `${month}/${year}`;
@@ -42,10 +23,12 @@ export const dateToMonthYear = (date) => {
 export const localizarData = (date) => dateToDate(date, {
   dateStyle: 'short',
   timeStyle: undefined,
+  timeZone: 'UTC',
 });
 
 export const localizarDataHorario = (date) => dateToDate(date, {
   dateStyle: 'short',
+  timeZone: 'America/Sao_Paulo',
   timeStyle: 'short',
 });
 
@@ -53,6 +36,7 @@ export const dateToShortDate = (date) => dateToDate(date, {
   year: 'numeric',
   month: 'numeric',
   day: 'numeric',
+  timeZone: 'America/Sao_Paulo',
   dateStyle: undefined,
 });
 
