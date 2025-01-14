850a2db353cbf71b9b03a8da2115be997f217918
Renato Cron <renato.cron@gmail.com>
2025-01-14 07:54:40 -0300

    feat(dto): adiciona IdNomeExibicaoOrgaoDto para incluir órgão na exibição fix(projeto.service): atualiza colaboradores_no_orgao para usar IdNomeExibicaoOrgaoDto

--- backend/src/common/dto/IdNomeExibicao.dto.ts
+++ backend/src/common/dto/IdNomeExibicao.dto.ts
@@ -1,4 +1,12 @@
+import { IdSigla } from './IdSigla.dto';
+
 export class IdNomeExibicaoDto {
     id: number;
     nome_exibicao: string;
+}
+
+export class IdNomeExibicaoOrgaoDto {
+    id: number;
+    nome_exibicao: string;
+    orgao: IdSigla;
 }
