e3e64e2e7238933e4f0675daa05bbee5a6ec9ecf
Gustavo Soares <gustavo.fe.soares@gmail.com>
2025-01-20 16:21:29 -0300

    feat: rotas planosSetoriaisMacrotemas -> entidadeMae.planosSetoriaisMacrotemas

--- frontend/src/views/ps.macrotemas/MacrotemasCriarEditar.vue
+++ frontend/src/views/ps.macrotemas/MacrotemasCriarEditar.vue
@@ -61,13 +61,13 @@
   </div>
 </template>
 <script setup>
-import { macrotema as schema } from '@/consts/formSchemas';
-import { useAlertStore } from '@/stores/alert.store';
-import { useMacrotemasPsStore } from '@/stores/macrotemasPs.store';
 import { storeToRefs } from 'pinia';
 import { ErrorMessage, Field, Form } from 'vee-validate';
 import { computed, defineOptions } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
+import { useMacrotemasPsStore } from '@/stores/macrotemasPs.store';
+import { useAlertStore } from '@/stores/alert.store';
+import { macrotema as schema } from '@/consts/formSchemas';
 
 defineOptions({
   inheritAttrs: false,
@@ -110,7 +110,7 @@ async function onSubmit(values) {
     if (response) {
       alertStore.success(msg);
       macrotemasStore.$reset();
-      router.push({ name: 'planosSetoriaisMacrotemas' });
+      router.push({ name: `${route.meta.entidadeMãe}.planosSetoriaisMacrotemas` });
     }
   } catch (error) {
     alertStore.error(error);
