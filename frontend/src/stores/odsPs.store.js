291811e5ae62b3ac521e4d3b784680a510a0791d
Sobral <robson@oficiodesign.com>
2025-01-23 16:58:52 -0300

    fix: Caminhos errados na carga de ODS

--- frontend/src/stores/odsPs.store.js
+++ frontend/src/stores/odsPs.store.js
@@ -2,16 +2,6 @@ import { defineStore } from 'pinia';
 
 const baseUrl = `${import.meta.env.VITE_API_URL}`;
 
-function caminhoParaApi(rotaMeta) {
-  if (rotaMeta.entidadeMãe === 'pdm') {
-    return 'ods';
-  }
-  if (rotaMeta.entidadeMãe === 'planoSetorial') {
-    return 'plano-setorial-ods';
-  }
-  throw new Error('Você precisa estar em algum módulo para executar essa ação.');
-}
-
 export const useOdsStore = defineStore('odsStore', {
   state: () => ({
     lista: [],
@@ -29,7 +19,7 @@ export const useOdsStore = defineStore('odsStore', {
       this.erro = null;
 
       try {
-        const resposta = await this.requestS.get(`${baseUrl}/${caminhoParaApi(this.route.meta)}/${id}`, params);
+        const resposta = await this.requestS.get(`${baseUrl}/ods/${id}`, params);
         this.emFoco = {
           ...resposta,
         };
@@ -43,7 +33,7 @@ export const useOdsStore = defineStore('odsStore', {
       this.chamadasPendentes.lista = true;
       this.erro = null;
       try {
-        const { linhas } = await this.requestS.get(`${baseUrl}/${caminhoParaApi(this.route.meta)}`, params);
+        const { linhas } = await this.requestS.get(`${baseUrl}/ods`, params);
         this.lista = linhas;
       } catch (erro) {
         this.erro = erro;
@@ -56,7 +46,7 @@ export const useOdsStore = defineStore('odsStore', {
       this.erro = null;
 
       try {
-        await this.requestS.delete(`${baseUrl}/${caminhoParaApi(this.route.meta)}/${id}`);
+        await this.requestS.delete(`${baseUrl}/ods/${id}`);
         this.chamadasPendentes.lista = false;
         return true;
       } catch (erro) {
@@ -72,9 +62,9 @@ export const useOdsStore = defineStore('odsStore', {
 
       try {
         if (id) {
-          await this.requestS.patch(`${baseUrl}/${caminhoParaApi(this.route.meta)}/${id}`, params);
+          await this.requestS.patch(`${baseUrl}/ods/${id}`, params);
         } else {
-          await this.requestS.post(`${baseUrl}/${caminhoParaApi(this.route.meta)}`, params);
+          await this.requestS.post(`${baseUrl}/ods`, params);
         }
 
         this.chamadasPendentes.emFoco = false;
