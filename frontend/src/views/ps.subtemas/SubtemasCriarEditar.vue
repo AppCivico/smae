39f46a1bfdb4ee2fd9966ffa6ac52bd1ba7ee71c
Gustavo Soares <gustavo.fe.soares@gmail.com>
2025-01-23 14:41:02 -0300

    feat: ajustando subtemas

--- frontend/src/views/ps.subtemas/SubtemasCriarEditar.vue
+++ frontend/src/views/ps.subtemas/SubtemasCriarEditar.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="flex spacebetween center mb2">
-    <h1> <span v-if="!subtemaId">Novo</span> {{ titulo || "Subtema" }}</h1>
+    <TituloDaPagina />
+
     <hr class="ml2 f1">
+
     <CheckClose />
   </div>
   <Form
@@ -64,28 +66,19 @@
 <script setup>
 import { storeToRefs } from 'pinia';
 import { ErrorMessage, Field, Form } from 'vee-validate';
-import { computed, defineOptions } from 'vue';
+import { defineOptions } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 import { useSubtemasPsStore } from '@/stores/subtemasPs.store';
 import { useAlertStore } from '@/stores/alert.store';
 import { subtema as schema } from '@/consts/formSchemas';
+import TituloDaPagina from '@/components/TituloDaPagina.vue';
 
 defineOptions({
   inheritAttrs: false,
 });
 
-const router = useRouter();
 const route = useRoute();
-const props = defineProps({
-  subtemaId: {
-    type: Number,
-    default: 0,
-  },
-});
-
-const titulo = typeof route?.meta?.título === 'function'
-  ? computed(() => route.meta.título())
-  : route?.meta?.título;
+const router = useRouter();
 
 const alertStore = useAlertStore();
 const subtemasStore = useSubtemasPsStore();
@@ -94,7 +87,7 @@ const { chamadasPendentes, erro, itemParaEdicao } = storeToRefs(subtemasStore);
 async function onSubmit(values) {
   try {
     let response;
-    const msg = props.subtemaId
+    const msg = route.params?.subtemaId
       ? 'Dados salvos com sucesso!'
       : 'Item adicionado com sucesso!';
 
