0b00d009a84bb08f98f309e753727a6e84ac7c62
Sobral <robson@oficiodesign.com>
2025-01-29 09:55:42 -0300

    fix: Migalhas de pão de metas não atualizam

--- frontend/src/components/metas/MigalhasDeMetas.vue
+++ frontend/src/components/metas/MigalhasDeMetas.vue
@@ -2,67 +2,78 @@
 import rotasDosNiveisDeMetas from '@/consts/rotasDosNiveisDeMetas';
 import {
   useAtividadesStore,
-  useIniciativasStore, useMetasStore,
+  useIniciativasStore,
+  useMetasStore,
 } from '@/stores';
 import { storeToRefs } from 'pinia';
+import { computed, watchEffect } from 'vue';
 import { useRoute } from 'vue-router';
 
 const route = useRoute();
-// eslint-disable-next-line @typescript-eslint/naming-convention
-const { meta_id } = route.params;
-// eslint-disable-next-line @typescript-eslint/naming-convention
-const { iniciativa_id } = route.params;
-// eslint-disable-next-line @typescript-eslint/naming-convention
-const { atividade_id } = route.params;
 
 const MetasStore = useMetasStore();
 const { singleMeta, activePdm } = storeToRefs(MetasStore);
-// mantendo comportamento legado
-// eslint-disable-next-line eqeqeq
-if (meta_id && singleMeta.value.id != meta_id) MetasStore.getById(meta_id);
-if (meta_id && !activePdm.value.id) MetasStore.getPdM();
-
 const IniciativasStore = useIniciativasStore();
 const { singleIniciativa } = storeToRefs(IniciativasStore);
-if (
-  iniciativa_id
-  // mantendo comportamento legado
-  // eslint-disable-next-line eqeqeq
-  && singleIniciativa.value.id != iniciativa_id
-  && !singleIniciativa.value.loading
-) {
-  IniciativasStore.getByIdReal(iniciativa_id);
-}
 
 const AtividadesStore = useAtividadesStore();
 const { singleAtividade } = storeToRefs(AtividadesStore);
-if (
-  atividade_id
-  // mantendo comportamento legado
-  // eslint-disable-next-line eqeqeq
-  && singleAtividade.value.id != atividade_id
-  && !singleAtividade.value.loading
-) {
-  AtividadesStore.getByIdReal(atividade_id);
-}
 
 const groupBy = localStorage.getItem('groupBy') ?? 'todas';
+
+const metaId = computed(() => {
+  const id = Number(route.params.meta_id);
+  return Number.isNaN(id) ? undefined : id;
+});
+const iniciativaId = computed(() => {
+  const id = Number(route.params.iniciativa_id);
+  return Number.isNaN(id) ? undefined : id;
+});
+const atividadeId = computed(() => {
+  const id = Number(route.params.atividade_id);
+  return Number.isNaN(id) ? undefined : id;
+});
+
+watchEffect(() => {
+  if (metaId.value && singleMeta.value.id !== metaId.value) {
+    MetasStore.getById(metaId.value);
+  }
+
+  if (metaId.value && !activePdm.value?.id) {
+    MetasStore.getPdM();
+  }
+
+  if (
+    iniciativaId.value
+    && singleIniciativa.value.id !== iniciativaId.value
+    && !singleIniciativa.value.loading
+  ) {
+    IniciativasStore.getByIdReal(iniciativaId.value);
+  }
+  if (
+    atividadeId.value
+    && singleAtividade.value.id !== atividadeId.value
+    && !singleAtividade.value.loading
+  ) {
+    AtividadesStore.getByIdReal(atividadeId.value);
+  }
+});
 </script>
 <template>
   <nav class="migalhas-de-pão migalhas-de-pão--metas">
     <ul class="migalhas-de-pão__lista">
       <li
-        v-if="activePdm.id"
+        v-if="activePdm?.id"
         class="migalhas-de-pão__item"
       >
         <SmaeLink
           class="migalhas-de-pão__link"
           to="/metas"
         >
-          {{ activePdm.nome }}
+          {{ activePdm?.nome }}
         </SmaeLink>
       </li>
-      <template v-if="meta_id && activePdm.id">
+      <template v-if="metaId && activePdm?.id">
         <template
           v-for="item in Object.values(rotasDosNiveisDeMetas)"
           :key="item.nível"
@@ -84,11 +95,11 @@ const groupBy = localStorage.getItem('groupBy') ?? 'todas';
         </template>
       </template>
       <li
-        v-if="meta_id && singleMeta.id"
+        v-if="metaId && singleMeta.id"
         class="migalhas-de-pão__item"
       >
         <SmaeLink
-          :to="`/metas/${meta_id}`"
+          :to="`/metas/${metaId}`"
           class="migalhas-de-pão__link"
         >
           Meta {{ singleMeta?.codigo }} {{ singleMeta?.titulo }}
@@ -96,28 +107,28 @@ const groupBy = localStorage.getItem('groupBy') ?? 'todas';
       </li>
 
       <li
-        v-if="iniciativa_id && activePdm.possui_iniciativa && singleIniciativa.id"
+        v-if="iniciativaId && activePdm?.possui_iniciativa && singleIniciativa.id"
         class="migalhas-de-pão__item"
       >
         <SmaeLink
-          :to="`/metas/${meta_id}/iniciativas/${iniciativa_id}`"
+          :to="`/metas/${metaId}/iniciativas/${iniciativaId}`"
           class="migalhas-de-pão__link"
         >
-          {{ activePdm.rotulo_iniciativa }}
+          {{ activePdm?.rotulo_iniciativa }}
           {{ singleIniciativa?.codigo }}
           {{ singleIniciativa?.titulo }}
         </SmaeLink>
       </li>
 
       <li
-        v-if="atividade_id && activePdm.possui_atividade && singleAtividade.id"
+        v-if="atividadeId && activePdm?.possui_atividade && singleAtividade.id"
         class="migalhas-de-pão__item"
       >
         <SmaeLink
-          :to="`/metas/${meta_id}/iniciativas/${iniciativa_id}/atividades/${atividade_id}`"
+          :to="`/metas/${metaId}/iniciativas/${iniciativaId}/atividades/${atividadeId}`"
           class="migalhas-de-pão__link"
         >
-          {{ activePdm.rotulo_atividade }}
+          {{ activePdm?.rotulo_atividade }}
           {{ singleAtividade?.codigo }}
           {{ singleAtividade?.titulo }}
         </SmaeLink>
