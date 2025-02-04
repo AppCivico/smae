e9d370624eb91494cc081fbad9ba59892d26ee5b
Lucas Ansei <takushilucas@gmail.com>
2025-02-04 13:10:10 -0300

    Removendo orgao_id do omit

--- backend/src/grupo-panel-externo/dto/update-grupo-externo.dto.ts
+++ backend/src/grupo-panel-externo/dto/update-grupo-externo.dto.ts
@@ -1,4 +1,4 @@
 import { OmitType, PartialType } from '@nestjs/swagger';
 import { CreateGrupoPainelExternoDto } from './create-grupo-externo.dto';
 
-export class UpdateGrupoPainelExternoDto extends PartialType(OmitType(CreateGrupoPainelExternoDto, ['orgao_id'])) {}
+export class UpdateGrupoPainelExternoDto extends PartialType(OmitType(CreateGrupoPainelExternoDto, [])) {}
