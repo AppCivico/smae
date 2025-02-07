fb31c29bb9f7fdfc514530e412c1cd98fd6f7972
Gustavo Soares <gustavo.fe.soares@gmail.com>
2025-02-07 16:16:50 -0300

    feat: criando helper para obter parametro

---
+++ frontend/src/helpers/obterParametroNoObjeto.ts
@@ -0,0 +1,32 @@
+function obterParametroNoObjeto(caminho: string, objeto: any) {
+  if (!caminho.includes('.')) {
+    if (!objeto[caminho]) {
+      // eslint-disable-next-line no-console
+      console.warn(
+        `Item "${caminho}" não encontrado encontrado no objeto`,
+      );
+      return objeto;
+    }
+
+    return objeto[caminho];
+  }
+
+  const caminhoEmPassos = caminho.split('.');
+
+  const saida = caminhoEmPassos.reduce<any>((amount, itemCaminho) => {
+    if (!amount[itemCaminho]) {
+      // eslint-disable-next-line no-console
+      console.warn(
+        `Item "${itemCaminho}" não encontrado encontrado no caminho "${caminho}"`,
+      );
+
+      return amount;
+    }
+
+    return amount[itemCaminho];
+  }, objeto);
+
+  return saida;
+}
+
+export default obterParametroNoObjeto;
