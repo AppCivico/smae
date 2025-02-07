bf97415cf5e08a65a33090a04d61129b8f5410f9
Gustavo Soares <gustavo.fe.soares@gmail.com>
2025-02-07 16:17:11 -0300

    feat: WIP

--- frontend/src/components/DashboardLayout.vue
+++ frontend/src/components/DashboardLayout.vue
@@ -1,15 +1,16 @@
 <script setup>
-import MenuSecundario from '@/components/MenuSecundario.vue';
-import { Nav } from '@/components';
-import { useAuthStore } from '@/stores/auth.store';
-import { useEditModalStore } from '@/stores/editModal.store';
-import { storeToRefs } from 'pinia';
+import SmaeTable from '@/components/SmaeTable/SmaeTable.vue';
+// import { storeToRefs } from 'pinia';
+// import MenuSecundario from '@/components/MenuSecundario.vue';
+// import { Nav } from '@/components';
+// import { useAuthStore } from '@/stores/auth.store';
+// import { useEditModalStore } from '@/stores/editModal.store';
 
-const editModalStore = useEditModalStore();
-const { editModal } = storeToRefs(editModalStore);
-const props = defineProps(['submenu', 'parentPage']);
-const authStore = useAuthStore();
-const { user } = storeToRefs(authStore);
+// const editModalStore = useEditModalStore();
+// const { editModal } = storeToRefs(editModalStore);
+// const props = defineProps(['submenu', 'parentPage']);
+// const authStore = useAuthStore();
+// const { user } = storeToRefs(authStore);
 </script>
 <script>
 // use normal <script> to declare options
@@ -17,7 +18,7 @@ export default {
   inheritAttrs: false,
 };
 </script>
-<template>
+<!-- <template>
   <Nav :activate="props?.submenu?.__name" />
   <component
     :is="submenu"
@@ -35,5 +36,35 @@ export default {
     v-bind="$attrs"
   >
     <slot />
+  </main>
+</template> -->
+
+<template>
+  <main>
+    <SmaeTable
+      :dados="[
+        { nome: 'Gustavo', idade: 27, empresa: { id: 1, nome: 'FGV' }, estado: 'RS' },
+        { nome: 'Sobral', idade: 32, empresa: { id: 2, nome: 'APPCIVICO' }, estado: 'SP' },
+        { nome: 'Shalders', idade: 78, empresa: { id: 1, nome: 'FGV' }, estado: 'ES' },
+      ]"
+      :colunas="[
+        { chave: 'nome', label: 'Nome', cabecalho: true },
+        { chave: 'idade', label: 'Idade'},
+        { chave: 'empresa.nome', label: 'Empresa'},
+        { chave: 'empresa.campo-errado', label: 'Empresa CAMPO ERRADO'},
+        { chave: 'customizado', label: 'Campo customizado'},
+      ]"
+      :rota-editar="{ name: 'editarUsuários'}"
+      parametro-no-objeto="idade"
+    >
+      <template #celula:customizado="{ caminho, linha }">
+        campo: {{ caminho }}--{{ linha.nome }}-{{ linha.idade }}
+        <input type="text">
+      </template>
+
+      <template #footer>
+        LAYOUT FOOTER
+      </template>
+    </SmaeTable>
   </main>
 </template>
