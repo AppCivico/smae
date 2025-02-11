d3a6741c4481d7db0af1a75edb678a01f81634f8
Gustavo Soares <gustavo.fe.soares@gmail.com>
2025-02-11 17:25:01 -0300

    feat adicionando dados aos filtros

--- frontend/src/views/projetos/ProjetosLista/partials/ProjetosListaFiltro.vue
+++ frontend/src/views/projetos/ProjetosLista/partials/ProjetosListaFiltro.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, onMounted } from 'vue';
+import { storeToRefs } from 'pinia';
 import projectStatuses from '@/consts/projectStatuses';
 import { projetoFiltro as schema } from '@/consts/formSchemas';
 import FiltroParaPagina, { type Formulario } from '@/components/FiltroParaPagina.vue';
+import { useOrgansStore, usePortfolioStore } from '@/stores';
+import { useEtapasProjetosStore } from '@/stores/etapasProjeto.store';
 
 const ordenador = [
   'portfolio',
@@ -34,16 +37,39 @@ const mapaStatus = [
   label: projectStatuses[item] || item,
 }));
 
+const organsStore = useOrgansStore();
+const portfolioStore = usePortfolioStore();
+const etapasProjetosStore = useEtapasProjetosStore();
+
+const { organResponsibles } = storeToRefs(organsStore);
+const { lista: portfolioLista } = storeToRefs(portfolioStore);
+const { lista: etapasLista } = storeToRefs(etapasProjetosStore);
+
+const opcoesFormulario = computed(() => ({
+  orgaos: organResponsibles.value.map((item) => ({
+    id: item.id,
+    label: item.descricao,
+  })),
+  portfolio: portfolioLista.value.map((item) => ({
+    id: item.id,
+    label: item.titulo,
+  })),
+  etapas: etapasLista.value.map((item) => ({
+    id: item.id,
+    label: item.descricao,
+  })),
+}));
+
 const campos = computed<Formulario>(() => [
   {
     campos: {
-      portfolio: { tipo: 'select', opcoes: ['1', '2'] },
-      orgao_responsavel_id: { tipo: 'select', opcoes: ['1', '2'] },
+      portfolio: { tipo: 'select', opcoes: opcoesFormulario.value.portfolio },
+      orgao_responsavel_id: { tipo: 'select', opcoes: opcoesFormulario.value.orgaos },
       status: {
         tipo: 'select',
         opcoes: mapaStatus,
       },
-      etapa_id: { tipo: 'select', opcoes: ['1', '2'] },
+      etapa_id: { tipo: 'select', opcoes: opcoesFormulario.value.etapas },
       data_registro: { tipo: 'date' },
       revisado: { tipo: 'checkbox' },
     },
@@ -62,6 +88,12 @@ const valoresIniciais = computed(() => ({
   ordem_direcao: 'desc',
   ipp: 100,
 }));
+
+onMounted(() => {
+  organsStore.getAllOrganResponsibles();
+  portfolioStore.buscarTudo();
+  etapasProjetosStore.buscarTudo();
+});
 </script>
 
 <template>
