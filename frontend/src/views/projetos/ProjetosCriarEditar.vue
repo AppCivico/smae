afe42248ef3d3f23814957d1d83bf76e9846bb7b
Gustavo Soares <gustavo.fe.soares@gmail.com>
2025-02-14 16:43:21 -0300

    feat: adicionando breadcrumbs

--- frontend/src/views/projetos/ProjetosCriarEditar.vue
+++ frontend/src/views/projetos/ProjetosCriarEditar.vue
@@ -1,9 +1,17 @@
 <script setup>
+import { storeToRefs } from 'pinia';
+import {
+  ErrorMessage, Field, FieldArray, Form,
+  useIsFormDirty,
+} from 'vee-validate';
+import { computed, ref, watch } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
 import AutocompleteField from '@/components/AutocompleteField2.vue';
 import CampoDePessoasComBuscaPorOrgao from '@/components/CampoDePessoasComBuscaPorOrgao.vue';
 import CampoDePlanosMetasRelacionados from '@/components/CampoDePlanosMetasRelacionados.vue';
 import MapaCampo from '@/components/geo/MapaCampo.vue';
 import LabelFromYup from '@/components/LabelFromYup.vue';
+import TituloDaPagina from '@/components/TituloDaPagina.vue';
 import MaskedFloatInput from '@/components/MaskedFloatInput.vue';
 import MenuDeMudançaDeStatusDeProjeto from '@/components/projetos/MenuDeMudançaDeStatusDeProjeto.vue';
 import { projeto as schema } from '@/consts/formSchemas';
@@ -16,12 +24,6 @@ import { useObservadoresStore } from '@/stores/observadores.store.ts';
 import { useOrgansStore } from '@/stores/organs.store';
 import { usePortfolioStore } from '@/stores/portfolios.store.ts';
 import { useProjetosStore } from '@/stores/projetos.store.ts';
-import { storeToRefs } from 'pinia';
-import {
-  ErrorMessage, Field, FieldArray, Form,
-} from 'vee-validate';
-import { computed, ref, watch } from 'vue';
-import { useRoute, useRouter } from 'vue-router';
 
 const baseUrl = `${import.meta.env.VITE_API_URL}`;
 
@@ -59,6 +61,7 @@ const { DotaçãoSegmentos } = storeToRefs(DotaçãoStore);
 
 const router = useRouter();
 const route = useRoute();
+const formularioSujo = useIsFormDirty();
 
 const props = defineProps({
   projetoId: {
@@ -270,16 +273,15 @@ watch(emFoco, () => {
 
 <template>
   <header class="flex flexwrap spacebetween g1 center mb2">
-    <h1 class="mb0">
-      {{ emFoco?.nome || (projetoId ? 'Projeto' : 'Novo projeto') }}
-    </h1>
+    <TituloDaPagina />
 
     <hr class="f1">
 
     <MenuDeMudançaDeStatusDeProjeto
       v-if="projetoId"
     />
-    <CheckClose />
+
+    <CheckClose :formulario-sujo="formularioSujo" />
   </header>
 
   <Form
