97385142810a3b6cefe6190c0704218f2f72888a
Renato Cron <renato.cron@gmail.com>
2025-02-17 14:55:59 -0300

    feat(pessoa): adiciona serviço para recálculo de equipes
    Implementa o `PessoaUtilsService` para recalcular equipes de todas as
    pessoas ativas. Adiciona o endpoint `recalc-equipe` no `PessoaController`
    para acionar essa funcionalidade, acessível apenas para usuários com
    papel 'SMAE.superadmin'. Essa mudança visa melhorar a gestão de equipes,
    permitindo atualizações em massa e garantindo que as informações de
    equipes estejam sempre atualizadas e consistentes.

--- backend/src/pessoa/pessoa.module.ts
+++ backend/src/pessoa/pessoa.module.ts
@@ -5,11 +5,12 @@ import { PessoaService } from './pessoa.service';
 import { PessoaResponsabilidadesMetaService } from './pessoa.responsabilidades.metas.service';
 import { PessoaPrivilegioModule } from '../auth/pessoaPrivilegio.module';
 import { EquipeRespModule } from '../equipe-resp/equipe-resp.module';
+import { PessoaUtilsService } from './pessoa.utils.service';
 
 @Module({
     imports: [PrismaModule, PessoaPrivilegioModule, EquipeRespModule],
     controllers: [PessoaController],
-    providers: [PessoaService, PessoaResponsabilidadesMetaService],
+    providers: [PessoaService, PessoaResponsabilidadesMetaService, PessoaUtilsService],
     exports: [PessoaService],
 })
 export class PessoaModule {}
