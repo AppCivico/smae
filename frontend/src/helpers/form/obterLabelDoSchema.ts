d33763fac658131d150d0f3d0c8e1c9ce558abe0
Gustavo Soares <gustavo.fe.soares@gmail.com>
2025-02-25 18:41:39 -0300

    feat: adicionando label ao yup

--- frontend/src/helpers/form/obterLabelDoSchema.ts
+++
@@ -1,7 +0,0 @@
-import { ObjectSchema } from 'yup';
-
-function obterLabelDoSchema(shape: ObjectSchema<any>, field: string) {
-  return shape.fields[field].spec.label;
-}
-
-export default obterLabelDoSchema;
