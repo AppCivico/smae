0e7b18284b4293f25b9df1326c5d81bb5bdd1a64
Sobral <robson@oficiodesign.com>
2025-03-06 08:31:21 -0300

    fix: Carga de risco de ciclo

--- frontend/src/views/monitoramentoDeMetas/MonitoramentoDeMetasAnaliseDeRisco.vue
+++ frontend/src/views/monitoramentoDeMetas/MonitoramentoDeMetasAnaliseDeRisco.vue
@@ -11,12 +11,12 @@ const monitoramentoDeMetasStore = useMonitoramentoDeMetasStore(route.meta.entida
 const {
   chamadasPendentes,
   erros,
-  analiseEmFoco,
+  riscoEmFoco,
 } = storeToRefs(monitoramentoDeMetasStore);
 
 watchEffect(() => {
   monitoramentoDeMetasStore
-    .buscarAnaliseDoCiclo(route.params.planoSetorialId, route.params.cicloId, {
+    .buscarRiscoDoCiclo(route.params.planoSetorialId, route.params.cicloId, {
       meta_id: route.params.meta_id,
     });
 });
@@ -27,14 +27,14 @@ watchEffect(() => {
   Analise de risco
 
   <div class="debug flex flexwrap g2 mb1">
-    <pre class="fb100 mb0">chamadasPendentes.analiseEmFoco: {{ chamadasPendentes.analiseEmFoco }}</pre>
-    <pre class="fb100 mb0">erros.analiseEmFoco: {{ erros.analiseEmFoco }}</pre>
+    <pre class="fb100 mb0">chamadasPendentes.riscoEmFoco: {{ chamadasPendentes.riscoEmFoco }}</pre>
+    <pre class="fb100 mb0">erros.riscoEmFoco: {{ erros.riscoEmFoco }}</pre>
 
     <textarea
       class="f1"
       readonly
       cols="30"
       rows="30"
-    >analiseEmFoco: {{ analiseEmFoco }}</textarea>
+    >riscoEmFoco: {{ riscoEmFoco }}</textarea>
   </div>
 </template>
