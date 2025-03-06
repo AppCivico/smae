2d51c0b94ed758ae7c212f9451cc89ee60bed076
Sobral <robson@oficiodesign.com>
2025-03-06 11:20:47 -0300

    feat: Adicionar edição de Risco de Ciclo

--- frontend/src/views/monitoramentoDeMetas/MonitoramentoDeMetasAnaliseDeRisco.vue
+++ frontend/src/views/monitoramentoDeMetas/MonitoramentoDeMetasAnaliseDeRisco.vue
@@ -1,12 +1,22 @@
 <script setup>
+import TextEditor from '@/components/TextEditor.vue';
+import { useAlertStore } from '@/stores/alert.store';
 import { useMonitoramentoDeMetasStore } from '@/stores/monitoramentoDeMetas.store';
 import { storeToRefs } from 'pinia';
-import { computed, watchEffect } from 'vue';
-import { useRoute } from 'vue-router';
+import {
+  ErrorMessage,
+  Field,
+  useForm,
+  useIsFormDirty,
+} from 'vee-validate';
+import { computed, watch, watchEffect } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
 
 const route = useRoute();
+const router = useRouter();
 
 const monitoramentoDeMetasStore = useMonitoramentoDeMetasStore(route.meta.entidadeMãe);
+const alertStore = useAlertStore();
 
 const {
   chamadasPendentes,
@@ -21,6 +31,39 @@ const riscoEmFocoParaEdicao = computed(() => ({
   ponto_de_atencao: riscoEmFoco.value?.corrente.riscos[0].ponto_de_atencao,
 }));
 
+const {
+  errors, handleSubmit, isSubmitting, resetField, resetForm, setFieldValue, values,
+} = useForm({
+  initialValues: riscoEmFocoParaEdicao.value,
+});
+
+const formularioSujo = useIsFormDirty();
+
+const onSubmit = handleSubmit.withControlled(async (valoresControlados) => {
+  try {
+    if (await monitoramentoDeMetasStore.salvarRiscoDeCiclo(
+      route.params.planoSetorialId,
+      route.params.cicloId,
+      valoresControlados,
+    )) {
+      alertStore.success('Análise de risco atualizada!');
+      if (route.meta.rotaDeEscape) {
+        router.push({
+          name: route.meta.rotaDeEscape,
+          params: route.params,
+          query: route.query,
+        });
+      }
+    }
+  } catch (error) {
+    alertStore.error(error);
+  }
+});
+
+watch(riscoEmFocoParaEdicao, (novoValor) => {
+  resetForm({ values: novoValor });
+});
+
 watchEffect(() => {
   monitoramentoDeMetasStore
     .buscarRiscoDoCiclo(route.params.planoSetorialId, route.params.cicloId, {
@@ -33,21 +76,65 @@ watchEffect(() => {
 
   Analise de risco
 
-  <div class="debug flex flexwrap g2 mb1">
-    <pre class="fb100 mb0">chamadasPendentes.riscoEmFoco: {{ chamadasPendentes.riscoEmFoco }}</pre>
-    <pre class="fb100 mb0">erros.riscoEmFoco: {{ erros.riscoEmFoco }}</pre>
+  <CheckClose
+    :formulario-sujo="formularioSujo"
+  />
 
-    <textarea
-      class="f1"
-      readonly
-      cols="30"
-      rows="30"
-    >riscoEmFoco: {{ riscoEmFoco }}</textarea>
-    <textarea
-      class="f1"
-      readonly
-      cols="30"
-      rows="30"
-    >riscoEmFocoParaEdicao: {{ riscoEmFocoParaEdicao }}</textarea>
-  </div>
+  <form
+    :disabled="isSubmitting"
+    :aria-busy="isSubmitting"
+    @submit.prevent="onSubmit"
+  >
+    <Field
+      name="ciclo_fisico_id"
+      type="hidden"
+    />
+    <Field
+      name="meta_id"
+      type="hidden"
+    />
+
+    <Field
+      v-slot="{ field }"
+      name="detalhamento"
+    >
+      <TextEditor
+        v-bind="field"
+      />
+    </Field>
+    <ErrorMessage
+      class="error-msg"
+      name="detalhamento"
+    />
+
+    <Field
+      v-slot="{ field }"
+      name="ponto_de_atencao"
+    >
+      <TextEditor
+        v-bind="field"
+      />
+    </Field>
+    <ErrorMessage
+      class="error-msg"
+      name="ponto_de_atencao"
+    />
+
+    <FormErrorsList :errors="errors" />
+
+    <div class="flex spacebetween center mb2">
+      <hr class="mr2 f1">
+      <button
+        class="btn big"
+        type="submit"
+        :disabled="isSubmitting || Object.keys(errors)?.length"
+        :title="Object.keys(errors)?.length
+          ? `Erros de preenchimento: ${Object.keys(errors)?.length}`
+          : null"
+      >
+        Salvar
+      </button>
+      <hr class="ml2 f1">
+    </div>
+  </form>
 </template>
