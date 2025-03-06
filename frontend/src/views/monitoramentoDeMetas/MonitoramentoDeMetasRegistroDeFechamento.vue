21054b26e2de126e8c32936c97004100f7d1072d
Sobral <robson@oficiodesign.com>
2025-03-06 08:26:02 -0300

    feat: Carrregar dados de fechamento de ciclos

--- frontend/src/views/monitoramentoDeMetas/MonitoramentoDeMetasRegistroDeFechamento.vue
+++ frontend/src/views/monitoramentoDeMetas/MonitoramentoDeMetasRegistroDeFechamento.vue
@@ -1,4 +1,40 @@
-<script setup></script>
+<script setup>
+import { useMonitoramentoDeMetasStore } from '@/stores/monitoramentoDeMetas.store';
+import { storeToRefs } from 'pinia';
+import { watchEffect } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+const monitoramentoDeMetasStore = useMonitoramentoDeMetasStore(route.meta.entidadeMãe);
+
+const {
+  chamadasPendentes,
+  erros,
+  fechamentoEmFoco,
+} = storeToRefs(monitoramentoDeMetasStore);
+
+watchEffect(() => {
+  monitoramentoDeMetasStore
+    .buscarFechamentoDoCiclo(route.params.planoSetorialId, route.params.cicloId, {
+      meta_id: route.params.meta_id,
+    });
+});
+</script>
 <template>
+  <MigalhasDePao />
+
   Registro de fechamento
+
+  <div class="debug flex flexwrap g2 mb1">
+    <pre class="fb100 mb0">chamadasPendentes.fechamentoEmFoco: {{ chamadasPendentes.fechamentoEmFoco }}</pre>
+    <pre class="fb100 mb0">erros.fechamentoEmFoco: {{ erros.fechamentoEmFoco }}</pre>
+
+    <textarea
+      class="f1"
+      readonly
+      cols="30"
+      rows="30"
+    >fechamentoEmFoco: {{ fechamentoEmFoco }}</textarea>
+  </div>
 </template>
