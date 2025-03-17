5d34012d106a9dea98709e9e70166cba33844454
Renato Cron <renato.cron@gmail.com>
2025-03-17 15:12:51 -0300

    refactor(transforms): unifica lógica de transformação de números e melhora tratamento de erros
    Remove funções que retornavam undefined e unifica a lógica para retornar 0 em caso de erro de conversão, exceto quando o valor é vazio, onde agora lança uma exceção BadRequestException com a stack trace. Isso melhora a consistência no tratamento de números e fornece informações de depuração mais detalhadas em caso de erro.

--- backend/src/auth/transforms/number.transform.ts
+++ backend/src/auth/transforms/number.transform.ts
@@ -2,6 +2,28 @@ import { TransformFnParams } from 'class-transformer';
 import { MAX_DTO_SAFE_NUM, MIN_DTO_SAFE_NUM } from '../../common/dto/consts';
 import { BadRequestException } from '@nestjs/common';
 
+export function NumberTransform(a: TransformFnParams): number {
+    if (a.value === '') {
+        const stack = new Error().stack;
+        throw new BadRequestException('Número não pode ser vazio. Stack: ' + stack);
+    }
+
+    const n = +a.value;
+    if (!isNaN(n) && n <= MAX_DTO_SAFE_NUM && n >= MIN_DTO_SAFE_NUM) return n;
+    return 0;
+}
+
+export function PositiveNumberTransform(a: TransformFnParams): number {
+    if (a.value === '') {
+        const stack = new Error().stack;
+        throw new BadRequestException('Número não pode ser vazio. Stack: ' + stack);
+    }
+
+    const n = +a.value;
+    if (!isNaN(n) && n <= MAX_DTO_SAFE_NUM && n >= 0) return n;
+    return 0;
+}
+
 export function NumberTransformOrUndef(a: TransformFnParams): number | undefined {
     if (a.value === '') return undefined;
 
@@ -16,22 +38,4 @@ export function PositiveNumberTransformOrUndef(a: TransformFnParams): number | u
     const n = +a.value;
     if (!isNaN(n) && n <= MAX_DTO_SAFE_NUM && n >= 0) return n;
     return undefined;
-}
-
-export function NumberTransform(a: TransformFnParams): number {
-    if (a.value === '') {
-        throw new BadRequestException('Invalid number');
-    }
-
-    const n = +a.value;
-    if (!isNaN(n) && n <= MAX_DTO_SAFE_NUM && n >= MIN_DTO_SAFE_NUM) return n;
-    return 0;
-}
-
-export function PositiveNumberTransform(a: TransformFnParams): number {
-    if (a.value === '') return 0;
-
-    const n = +a.value;
-    if (!isNaN(n) && n <= MAX_DTO_SAFE_NUM && n >= 0) return n;
-    return 0;
 }
