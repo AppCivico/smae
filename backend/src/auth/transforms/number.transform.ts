ef030d16ba497b98cb93cd40c01c97fb98fc5170
Mateus Martinez rosa <41523047+mateusmrosa@users.noreply.github.com>
2025-03-17 15:16:15 -0300

    Edição de aditivos de contrato (#180)
    - Ajuste na verificação do update do tipo-aditivos-service.
    - Inserção da lib class-transformer.
    - Correção na verificação do update do contrato-aditivo-service.
    
    ---------
    
    Co-authored-by: Renato Cron <renato.cron@gmail.com>

--- backend/src/auth/transforms/number.transform.ts
+++ backend/src/auth/transforms/number.transform.ts
@@ -1,18 +1,41 @@
 import { TransformFnParams } from 'class-transformer';
 import { MAX_DTO_SAFE_NUM, MIN_DTO_SAFE_NUM } from '../../common/dto/consts';
+import { BadRequestException } from '@nestjs/common';
 
-export function NumberTransform(a: TransformFnParams): number | undefined {
-    const n = a.value === '' ? undefined : +a.value;
+export function NumberTransform(a: TransformFnParams): number {
+    if (a.value === '') {
+        const stack = new Error().stack;
+        throw new BadRequestException('Número não pode ser vazio. Stack: ' + stack);
+    }
 
-    // geralmente queremos números menores que 32 bits
-    if (n && !isNaN(n) && n <= MAX_DTO_SAFE_NUM && n >= MIN_DTO_SAFE_NUM) return n;
-    return n;
+    const n = +a.value;
+    if (!isNaN(n) && n <= MAX_DTO_SAFE_NUM && n >= MIN_DTO_SAFE_NUM) return n;
+    return 0;
 }
 
-export function PositiveNumberTransform(a: TransformFnParams): number | undefined {
-    const n = a.value === '' ? undefined : +a.value;
+export function PositiveNumberTransform(a: TransformFnParams): number {
+    if (a.value === '') {
+        const stack = new Error().stack;
+        throw new BadRequestException('Número não pode ser vazio. Stack: ' + stack);
+    }
 
-    // geralmente queremos números menores que 32 bits
-    if (n && !isNaN(n) && n <= MAX_DTO_SAFE_NUM && n >= 0) return n;
-    return n;
+    const n = +a.value;
+    if (!isNaN(n) && n <= MAX_DTO_SAFE_NUM && n >= 0) return n;
+    return 0;
+}
+
+export function NumberTransformOrUndef(a: TransformFnParams): number | undefined {
+    if (a.value === '') return undefined;
+
+    const n = +a.value;
+    if (!isNaN(n) && n <= MAX_DTO_SAFE_NUM && n >= MIN_DTO_SAFE_NUM) return n;
+    return undefined;
+}
+
+export function PositiveNumberTransformOrUndef(a: TransformFnParams): number | undefined {
+    if (a.value === '') return undefined;
+
+    const n = +a.value;
+    if (!isNaN(n) && n <= MAX_DTO_SAFE_NUM && n >= 0) return n;
+    return undefined;
 }
--- backend/src/tipo-aditivo/tipo-aditivo.service.ts
+++ backend/src/tipo-aditivo/tipo-aditivo.service.ts
@@ -53,7 +53,7 @@ export class ProjetoTipoAditivoService {
     async update(id: number, dto: UpdateTipoAditivoDto, user: PessoaFromJwt) {
         const self = await this.prisma.tipoAditivo.findFirstOrThrow({
             where: { id: id },
-            select: { id: true },
+            select: { id: true, habilita_valor: true, habilita_valor_data_termino: true },
         });
 
         if (dto.nome !== undefined) {
@@ -61,7 +61,6 @@ export class ProjetoTipoAditivoService {
                 where: {
                     nome: { equals: dto.nome, mode: 'insensitive' },
                     removido_em: null,
-
                     NOT: { id: self.id },
                 },
             });
@@ -69,14 +68,45 @@ export class ProjetoTipoAditivoService {
             if (similarExists > 0)
                 throw new HttpException('Nome igual ou semelhante já existe em outro registro ativo', 400);
         }
-        // verificar os contratos..
+
+        const contratosAditivos = await this.prisma.contratoAditivo.count({
+            where: {
+                tipo_aditivo_id: id,
+                removido_em: null,
+            },
+        });
+
+        if (contratosAditivos > 0) {
+            if (
+                (dto.habilita_valor !== undefined && dto.habilita_valor !== self.habilita_valor) ||
+                (dto.habilita_valor_data_termino !== undefined &&
+                    dto.habilita_valor_data_termino !== self.habilita_valor_data_termino)
+            ) {
+                throw new HttpException(
+                    'Existem contratos com aditivos desse tipo. Não é possível alterar os checkboxes.',
+                    400
+                );
+            }
+
+            if (dto.nome !== undefined) {
+                await this.prisma.tipoAditivo.update({
+                    where: { id: id },
+                    data: {
+                        atualizado_por: user.id,
+                        atualizado_em: new Date(Date.now()),
+                        nome: dto.nome, // Apenas o nome é atualizado
+                    },
+                });
+            }
+
+            return { id };
+        }
 
         await this.prisma.tipoAditivo.update({
             where: { id: id },
             data: {
                 atualizado_por: user.id,
                 atualizado_em: new Date(Date.now()),
-
                 habilita_valor: dto.habilita_valor,
                 habilita_valor_data_termino: dto.habilita_valor_data_termino,
                 nome: dto.nome,
@@ -87,6 +117,16 @@ export class ProjetoTipoAditivoService {
     }
 
     async remove(id: number, user: PessoaFromJwt) {
+        const contratosAditivos = await this.prisma.contratoAditivo.count({
+            where: {
+                tipo_aditivo_id: id,
+                removido_em: null,
+            },
+        });
+
+        if (contratosAditivos > 0)
+            throw new HttpException('Existem contratos com aditivos desse tipo. Não é possível remover.', 400);
+
         const created = await this.prisma.tipoAditivo.updateMany({
             where: { id: id },
             data: {
