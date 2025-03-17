be73f536be5e3d080783d697db273898444d37e8
Renato Cron <renato.cron@gmail.com>
2025-03-17 15:18:14 -0300

    fix(transforms): melhora mensagens de erro para NumberTransform e PositiveNumberTransform
    Atualiza as mensagens de erro para incluir o contexto do método
    específico, sugerindo verificar a mudança para as versões "OrUndef"
    quando o valor é vazio. Remove o retorno padrão de 0 e lança uma
    exceção para números inválidos, fornecendo uma pilha de erros para
    melhor rastreamento. Isso melhora a clareza e a depuração ao lidar
    com entradas inválidas.

--- backend/src/auth/transforms/number.transform.ts
+++ backend/src/auth/transforms/number.transform.ts
@@ -5,23 +5,32 @@ import { BadRequestException } from '@nestjs/common';
 export function NumberTransform(a: TransformFnParams): number {
     if (a.value === '') {
         const stack = new Error().stack;
-        throw new BadRequestException('Número não pode ser vazio. Stack: ' + stack);
+        throw new BadRequestException(
+            'Número não pode ser vazio em NumberTransform, verificar mudança para NumberTransformOrUndef. Stack: ' +
+                stack
+        );
     }
 
     const n = +a.value;
     if (!isNaN(n) && n <= MAX_DTO_SAFE_NUM && n >= MIN_DTO_SAFE_NUM) return n;
-    return 0;
+
+    const stack = new Error().stack;
+    throw new BadRequestException(`Número inválido ${n}. Stack: ` + stack);
 }
 
 export function PositiveNumberTransform(a: TransformFnParams): number {
     if (a.value === '') {
         const stack = new Error().stack;
-        throw new BadRequestException('Número não pode ser vazio. Stack: ' + stack);
+        throw new BadRequestException(
+            'Número não pode ser vazio em PositiveNumberTransform, verificar mudança para PositiveNumberTransformOrUndef. Stack: ' +
+                stack
+        );
     }
 
     const n = +a.value;
     if (!isNaN(n) && n <= MAX_DTO_SAFE_NUM && n >= 0) return n;
-    return 0;
+    const stack = new Error().stack;
+    throw new BadRequestException(`Número inválido ${n}. Stack: ` + stack);
 }
 
 export function NumberTransformOrUndef(a: TransformFnParams): number | undefined {
