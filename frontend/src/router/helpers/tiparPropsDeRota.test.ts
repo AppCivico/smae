40aaf609bef1343801d6b74a65116651903fd880
Sobral <robson@oficiodesign.com>
2025-03-19 14:33:37 -0300

    feat: Permitir sobrescrever a conversão de tipo de rota

--- frontend/src/router/helpers/tiparPropsDeRota.test.ts
+++ frontend/src/router/helpers/tiparPropsDeRota.test.ts
@@ -1,5 +1,6 @@
 import { describe, expect, it } from 'vitest';
 import type { RouteLocation } from 'vue-router';
+import type { Tipos } from './tiparPropsDeRota';
 import tiparPropsDeRota from './tiparPropsDeRota';
 
 const tests = [
@@ -31,10 +32,31 @@ const tests = [
     route: { params: { termo: 'undefined' } },
     expected: { termo: undefined },
   },
+  {
+    route: { params: { termo: 'undefined' } },
+    expected: { termo: 'undefined' },
+    tipos: { termo: 'string' },
+  },
+  {
+    route: { params: { valor: '1024' } },
+    expected: { valor: '1024' },
+    tipos: { valor: 'string' },
+  },
+  {
+    route: { params: { valor: 'true' } },
+    expected: { valor: NaN },
+    tipos: { valor: 'number' },
+  },
+  {
+    route: { params: { concluidas: 1 } },
+    expected: { concluidas: true },
+    tipos: { concluidas: 'boolean' },
+  },
 ];
 
 describe('tiparPropsDeRota', () => {
-  it.each(tests)('deve tipar $route como $expected', ({ route, expected }) => {
-    expect(tiparPropsDeRota(route as unknown as RouteLocation)).toStrictEqual(expected);
+  it.each(tests)('deve tipar $route como $expected', ({ route, expected, tipos }) => {
+    expect(tiparPropsDeRota(route as unknown as RouteLocation, tipos as unknown as Tipos))
+      .toStrictEqual(expected);
   });
 });
