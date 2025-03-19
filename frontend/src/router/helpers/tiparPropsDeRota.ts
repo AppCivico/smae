8e78f3b1483bd348e875e08cb50b58a123e8d1b1
Sobral <robson@oficiodesign.com>
2025-03-19 14:33:37 -0300

    feat: Permitir sobrescrever a conversão de tipo de rota

--- frontend/src/router/helpers/tiparPropsDeRota.ts
+++ frontend/src/router/helpers/tiparPropsDeRota.ts
@@ -1,13 +1,34 @@
 import decodificadorDePrimitivas from '@/helpers/decodificadorDePrimitivas';
 import type { RouteLocation } from 'vue-router';
 
-export default (route:RouteLocation) => {
+export type Tipos = {
+  [key: string]: 'string' | 'boolean' | 'number';
+};
+
+export default (route:RouteLocation, tipos:Tipos = {}) => {
   const props: Record<string, unknown> = {};
 
   Object.entries(route.params).forEach(([prop, value]) => {
-    props[prop] = typeof value === 'object'
-      ? value
-      : decodificadorDePrimitivas(value);
+    switch (tipos[prop]) {
+      case 'string':
+        props[prop] = String(value);
+        break;
+
+      case 'boolean':
+        props[prop] = ['true', '1', 'yes', 'on'].includes(String(value).toLowerCase());
+        break;
+
+      case 'number': {
+        props[prop] = Number.parseInt(String(value), 10);
+        break;
+      }
+
+      default:
+        props[prop] = typeof value === 'object'
+          ? value
+          : decodificadorDePrimitivas(value);
+        break;
+    }
   });
 
   return props;
