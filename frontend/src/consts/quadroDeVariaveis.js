f613f4c83ba6da02fd5cb508dbea2369ff89e7ba
eduruiz <contato@eduruiz.com.br>
2025-03-21 16:15:53 -0300

    feat: add novo gráfico de situação das variáveis no quadro de atividades

---
+++ frontend/src/consts/quadroDeVariaveis.js
@@ -0,0 +1,8 @@
+export default {
+  a_coletar_atrasadas: 'A coletar atrasadas',
+  a_coletar_prazo: 'A coletar no prazo',
+  coletadas_a_conferir: 'Coletadas a conferir',
+  conferidas_a_liberar: 'Conferidas a liberar',
+  liberadas: 'Liberadas',
+  total: 'Total',
+};
