9792dcab55c3a3f0d98c045e42116feadf1965ae
Gustavo Soares <gustavo.fe.soares@gmail.com>
2025-04-02 14:20:34 -0300

    feat: iniciando componentes de tela

---
+++ frontend/src/views/edicaoEmLote/EdicaoEmLoteNovo.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import FiltroParaRegistros from '@/components/alteracaoEmLotes.componentes/Novo/FiltroParaRegistros.vue';
+import TituloDaPagina from '@/components/TituloDaPagina.vue';
+
+</script>
+
+<template>
+  <div>
+    <header>
+      <TituloDaPagina />
+    </header>
+
+    <FiltroParaRegistros />
+  </div>
+</template>
