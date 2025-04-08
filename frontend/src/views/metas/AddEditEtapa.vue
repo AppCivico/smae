e9473a2331f274db667a5abdf72ef0d67f3bfaa6
Sobral <robson@oficiodesign.com>
2025-04-08 18:13:37 -0300

    fix: Prevenir erro de carga assíncrona

--- frontend/src/views/metas/AddEditEtapa.vue
+++ frontend/src/views/metas/AddEditEtapa.vue
@@ -93,6 +93,13 @@ const usersAvailable = ref([]);
 const responsaveis = ref({ participantes: [], busca: '' });
 
 const permissaoLiberada = computed(() => singleEtapa.value.etapa.pode_editar_realizado);
+const permissaoLiberada = computed(() => {
+  if (!singleEtapa.value?.etapa) {
+    return true;
+  }
+
+  return singleEtapa.value?.etapa?.pode_editar_realizado;
+});
 
 const geolocalizaçãoPorToken = computed(() => (
   singleEtapa.value?.loading
