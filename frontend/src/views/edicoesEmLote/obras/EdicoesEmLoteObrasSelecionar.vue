f7ee5b6869359a06a135c2fe22581783ad7af367
Gustavo Soares <gustavo.fe.soares@gmail.com>
2025-04-09 12:36:15 -0300

    feat: construindo tela selecao em lote

--- frontend/src/views/edicoesEmLote/obras/EdicoesEmLoteObrasSelecionar.vue
+++ frontend/src/views/edicoesEmLote/obras/EdicoesEmLoteObrasSelecionar.vue
@@ -1,5 +1,44 @@
 <script lang="ts" setup>
+import { watch } from 'vue';
+import { storeToRefs } from 'pinia';
+import { useRoute } from 'vue-router';
+import SmaeTable from '@/components/SmaeTable/SmaeTable.vue';
 import FiltroParaRegistros from '@/components/alteracaoEmLotes.componentes/Selecionar/FiltroParaRegistros.vue';
+import { useObrasStore } from '@/stores/obras.store';
+import { obras as obrasSchema } from '@/consts/formSchemas';
+import statusObras from '@/consts/statusObras';
+import { useEdicoesEmLoteStore } from '@/stores/edicoesEmLote.store';
+import MenuPaginacao from '@/components/MenuPaginacao.vue';
+import SmaeFieldsetSubmit from '@/components/SmaeFieldsetSubmit.vue';
+import ContadorItems from '@/components/alteracaoEmLotes.componentes/Selecionar/ContadorItems.vue';
+
+const route = useRoute();
+
+const obrasStore = useObrasStore();
+const edicoesEmLoteStore = useEdicoesEmLoteStore(route.meta.tipoDeAcoesEmLote as string);
+
+const { idsSelecionados } = storeToRefs(edicoesEmLoteStore);
+const { lista: listaDeObras, paginacao } = storeToRefs(obrasStore);
+
+function limparSelecionados() {
+  edicoesEmLoteStore.limparIdsSelecionados();
+}
+
+function handleSelecionarTodasObras() {
+  alert('Aguardando BE');
+}
+
+function handleFinalizarSelecao() {
+
+}
+
+watch(
+  () => route.query,
+  () => {
+    obrasStore.buscarTudo(route.query);
+  },
+  { immediate: true },
+);
 
 </script>
 
@@ -7,4 +46,86 @@ import FiltroParaRegistros from '@/components/alteracaoEmLotes.componentes/Selec
   <CabecalhoDePagina />
 
   <FiltroParaRegistros />
+
+  <section>
+    <ContadorItems />
+
+    <article class="mt1">
+      <MenuPaginacao v-bind="paginacao" />
+    </article>
+
+    <SmaeTable
+      v-selecionar-multiplas-opcoes
+      titulo-rolagem-horizontal="Tabela: Edição em Lote"
+      class="mt2"
+      rolagem-horizontal
+      :dados="listaDeObras"
+      :colunas="[
+        { chave: 'selecionado', ehDadoComputado: true },
+        { chave: 'orgao_origem.sigla', label: obrasSchema.fields['orgao_origem_id'].spec.label },
+        { chave: 'portfolio.titulo', label: obrasSchema.fields['portfolio_id'].spec.label },
+        { chave: 'nome', label: obrasSchema.fields['nome'].spec.label },
+        { chave: 'grupo_tematico.nome', label: obrasSchema.fields['grupo_tematico_id'].spec.label },
+        {
+          chave: 'tipo_intervencao.nome',
+          label: obrasSchema.fields['tipo_intervencao_id'].spec.label
+        },
+        { chave: 'equipamento.nome', label: obrasSchema.fields['equipamento_id'].spec.label },
+        { chave: 'regioes', label: obrasSchema.fields['regiao_ids'].spec.label },
+        { chave: 'status', label: obrasSchema.fields['status'].spec.label },
+      ]"
+      replicar-cabecalho
+    >
+      <template #cabecalho:selecionado>
+        <button
+          type="button"
+          @click="limparSelecionados"
+        >
+          Desmarcar todas
+        </button>
+      </template>
+
+      <template #celula:selecionado="{ linha }">
+        <div class="flex justifycenter">
+          <input
+            v-model="idsSelecionados"
+            type="checkbox"
+            name="selecionado"
+            :value="linha.id"
+          >
+        </div>
+      </template>
+
+      <template #celula:status="{ linha }">
+        {{ statusObras[linha.status]?.nome || linha.status }}
+      </template>
+    </SmaeTable>
+
+    <article class="mt2">
+      <MenuPaginacao v-bind="paginacao" />
+    </article>
+
+    <ContadorItems class="mt2" />
+
+    <SmaeFieldsetSubmit>
+      <button
+        class="btn big outline bgnone tcprimary"
+        @click="handleSelecionarTodasObras"
+      >
+        selecionar todas obras ({{ paginacao.totalRegistros }})
+      </button>
+
+      <SmaeLink
+        class="btn big"
+        :aria-disabled="idsSelecionados.length === 0"
+        :desabilitar="idsSelecionados.length === 0"
+        :to="{
+          name: 'edicoesEmLoteObrasNovoConstruir'
+        }"
+        @click="handleFinalizarSelecao"
+      >
+        finalizar seleção
+      </SmaeLink>
+    </SmaeFieldsetSubmit>
+  </section>
 </template>
