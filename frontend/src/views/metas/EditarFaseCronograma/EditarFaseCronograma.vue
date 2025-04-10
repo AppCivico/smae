a3c3c0e28c12b2d839f1cdd728a52f2b97963be8
Gustavo Soares <gustavo.fe.soares@gmail.com>
2025-04-10 10:11:05 -0300

    feat: ajustes recall

--- frontend/src/views/metas/EditarFaseCronograma/EditarFaseCronograma.vue
+++ frontend/src/views/metas/EditarFaseCronograma/EditarFaseCronograma.vue
@@ -85,7 +85,13 @@ const minLevel = ref(0);
 
 const usersAvailable = ref([]);
 
-const permissaoLiberada = computed(() => currentFase.value.pode_editar_realizado);
+const permissaoLiberada = computed(() => {
+  if (!currentFase.value?.id) {
+    return true;
+  }
+
+  return currentFase.value.pode_editar_realizado;
+});
 
 async function getRegionByParent(r_id, cur) {
   await RegionsStore.filterRegions({ id: r_id });
