e829fe0e2bee98f42a6f7c3aa5680b74fff94147
Gustavo Soares <gustavo.fe.soares@gmail.com>
2025-04-11 16:29:05 -0300

    feat: criando tela listagem e resumo

--- frontend/src/views/edicoesEmLote/EdicoesEmLoteLista.vue
+++ frontend/src/views/edicoesEmLote/EdicoesEmLoteLista.vue
@@ -1,14 +1,20 @@
 <script lang="ts" setup>
-import { useEdicoesEmLoteStore } from '@/stores/edicoesEmLote.store';
+import { onMounted } from 'vue';
 import { storeToRefs } from 'pinia';
 import { useRoute } from 'vue-router';
+import SmaeTable from '@/components/SmaeTable/SmaeTable.vue';
+import BarraDeProgresso from '@/components/BarraDeProgresso.vue';
+import { useEdicoesEmLoteStore } from '@/stores/edicoesEmLote.store';
+import dateToDate from '@/helpers/dateToDate';
 
 const route = useRoute();
 
 const edicoesEmLoteStore = useEdicoesEmLoteStore(route.meta.tipoDeAcoesEmLote as string);
 const { lista } = storeToRefs(edicoesEmLoteStore);
 
-edicoesEmLoteStore.buscarTudo({ tipo: route.meta.tipoDeAcoesEmLote as string });
+onMounted(() => {
+  edicoesEmLoteStore.buscarTudo({ tipo: route.meta.tipoDeAcoesEmLote as string });
+});
 </script>
 
 <template>
@@ -24,11 +30,62 @@ edicoesEmLoteStore.buscarTudo({ tipo: route.meta.tipoDeAcoesEmLote as string });
     </template>
   </CabecalhoDePagina>
 
-  <p class="debug">
-    Aqui, listamos as edições em lote concluídas
-  </p>
+  <section class="edicoes-em-lote-lista">
+    <SmaeTable
+      titulo-rolagem-horizontal="Tabela: Edição em Lote - Lista"
+      class="mt2"
+      rolagem-horizontal
+      :dados="lista"
+      :colunas="[
+        { chave: 'iniciou_em', label: 'Data do processamento', formatador: (v) => dateToDate(v) },
+        { chave: 'id', label: 'Item(s) modificado(s)' },
+        { chave: 'criador.nome_exibicao', label: 'Responsável pela solicitação' },
+        { chave: 'regitros_processados', label: 'registros processados', ehDadoComputado: true },
+        { chave: 'progresso', label: 'Progresso', ehDadoComputado: true },
+        { chave: 'acao', label: 'detalhamento', ehDadoComputado: true },
+      ]"
+    >
+      <template #celula:regitros_processados="{ linha }">
+        {{ linha.n_sucesso + linha.n_erro }} / {{ linha.n_total }}
+      </template>
 
-  <pre>
-    {{ lista }}
-  </pre>
+      <template #celula:progresso="{ linha }">
+        <BarraDeProgresso
+          :atual="linha.n_sucesso + linha.n_erro"
+          :total="linha.n_total"
+        />
+      </template>
+
+      <template #celula:acao="{ linha }">
+        <SmaeLink
+          class="btn small bgnone tcprimary edicoes-em-lote-lista__ler-mais"
+          :to="{
+            name: 'edicoesEmLoteObrasResumo',
+            params: { edicaoEmLoteId: linha.id },
+          }"
+        >
+          <span class="flex g05 center nowrap">
+            <svg
+              width="24"
+              height="24"
+            ><use xlink:href="#i_eye" /></svg>
+            Ler mais
+          </span>
+        </SmaeLink>
+      </template>
+    </SmaeTable>
+  </section>
 </template>
+
+<style lang="less" scoped>
+.edicoes-em-lote-lista {
+  :deep(.table-cell--progresso) {
+    width: 190px;
+  }
+}
+
+.edicoes-em-lote-lista__ler-mais svg {
+  width: initial;
+  height: initial;
+}
+</style>
