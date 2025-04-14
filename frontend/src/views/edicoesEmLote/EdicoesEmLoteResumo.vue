3163717d592cd22c813bb7ed88b901704ba061ef
Gustavo Soares <gustavo.fe.soares@gmail.com>
2025-04-14 12:07:30 -0300

    feat: adicionando lista de detalhes edicao em lote

--- frontend/src/views/edicoesEmLote/EdicoesEmLoteResumo.vue
+++ frontend/src/views/edicoesEmLote/EdicoesEmLoteResumo.vue
@@ -1,17 +1,36 @@
 <script lang="ts" setup>
-import { onMounted } from 'vue';
+import { computed, onMounted } from 'vue';
 import { storeToRefs } from 'pinia';
 import { useRoute } from 'vue-router';
 import SmaeTable from '@/components/SmaeTable/SmaeTable.vue';
 import { useEdicoesEmLoteStore } from '@/stores/edicoesEmLote.store';
+import dateToDate from '@/helpers/dateToDate';
 
 const route = useRoute();
 
 const edicoesEmLoteStore = useEdicoesEmLoteStore(route.meta.tipoDeAcoesEmLote as string);
 const { emFoco } = storeToRefs(edicoesEmLoteStore);
 
+const detalhesEdicao = computed(() => {
+  if (!emFoco.value) {
+    return [];
+  }
+
+  return [
+    { descricao: 'Iniciado em', valor: dateToDate(emFoco.value.iniciou_em) },
+    { descricao: 'terminado em', valor: dateToDate(emFoco.value.terminou_em) },
+    { descricao: 'Executado por', valor: emFoco.value.criador.nome_exibicao },
+    { descricao: 'Órgão', valor: emFoco.value.orgao?.sigla },
+    { descricao: 'Itens concluídos', valor: emFoco.value.n_sucesso },
+    { descricao: 'Itens ignorados', valor: emFoco.value.n_ignorado },
+    { descricao: 'Itens com erro', valor: emFoco.value.n_erro },
+    { descricao: 'Total itens', valor: emFoco.value.n_total },
+    { descricao: 'Tipo', valor: emFoco.value.tipo },
+  ];
+});
+
 onMounted(() => {
-  if (route.params.edicaoEmLoteId) {
+  if (!route.params.edicaoEmLoteId) {
     throw new Error('Parâmetro "edicaoEmLoteId" não informado');
   }
 
@@ -27,24 +46,35 @@ onMounted(() => {
       Edição
     </h3>
 
-    <h2 class="edicoes-em-lote-resumo__criador">
-      {{ emFoco?.criador.nome_exibicao }}
-    </h2>
+    <dl
+      v-if="emFoco"
+      class="edicoes-em-lote-resumo__detalhes"
+    >
+      <div
+        v-for="(itemDetalhe, detalheIndex) in detalhesEdicao"
+        :key="`detalhe-item--${detalheIndex}`"
+      >
+        <dt>{{ itemDetalhe.descricao }}</dt>
+        <dd>{{ itemDetalhe.valor }}</dd>
+      </div>
+    </dl>
 
     <SmaeTable
       titulo-rolagem-horizontal="Tabela: Edição em Lote - Resumo"
       class="mt2"
       rolagem-horizontal
-      :dados="emFoco?.operacao || []"
+      :dados="emFoco?.results_log?.falhas || []"
       :colunas="[
-        { chave: 'col', label: 'nome da obra' },
-        { chave: 'set', label: 'erros' },
+        { chave: 'id', label: 'nome da obra' },
+        { chave: 'erro', label: 'erros' },
       ]"
     />
   </section>
 </template>
 
 <style lang="less" scoped>
+@duas-colunas: 55rem;
+
 .edicoes-em-lote-resumo__titulo {
   font-weight: 400;
   font-size: 22px;
@@ -55,5 +85,40 @@ onMounted(() => {
   font-weight: 300;
   font-size: 20px;
   color: #152741;
+}
+
+.edicoes-em-lote-resumo__detalhes {
+  @media screen and (min-width: @duas-colunas) {
+    max-width: 50%;
+  }
+}
+
+.edicoes-em-lote-resumo__detalhes div {
+  width: 100%;
+  display: grid;
+  position: relative;
+  grid-template-columns: repeat(2, 1fr);
+  padding: .5rem;
+
+  &:before {
+    content: '';
+    position: absolute;
+    height: 1px;
+    width: 100%;
+    background-color: @c100;
+  }
+
+  &:first-of-type:before {
+    content: initial;
+  }
+
+  dt {
+    font-weight: 700;
+  }
+
+  dt:after {
+    content: ':'
+  }
+
 }
 </style>
