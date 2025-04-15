5f44b90e5a4fedfe73e8b41acaef3d68a2cba3bf
Sobral <robson@oficiodesign.com>
2025-04-15 09:49:30 -0300

    fix: Erro de carga de opções assíncronas no filtro para edição em lote

--- frontend/src/helpers/prepararParaSelect.ts
+++ frontend/src/helpers/prepararParaSelect.ts
@@ -8,10 +8,13 @@ function prepararParaSelect(
   mapa: MapaParaChave,
 ): MapaParaChave[] {
   let lista = [];
-  if (typeof valor === 'object') {
-    lista = Object.values(valor);
-  } else {
+
+  if (Array.isArray(valor)) {
     lista = valor;
+    // prevenindo erros com código legado que armazena estados de espera no
+    // lugar da listagem 🤬
+  } else if (typeof valor === 'object' && valor !== null && !valor.loading) {
+    lista = Object.values(valor);
   }
 
   return lista.map<MapaParaChave>((item) => ({
