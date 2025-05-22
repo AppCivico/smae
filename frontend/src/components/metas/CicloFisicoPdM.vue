7c869ca6e043451c29e705b82ba23745dd5713a5
Sobral <robson@oficiodesign.com>
2025-05-22 15:19:43 -0300

    fix: Marcação errada de tabela

--- frontend/src/components/metas/CicloFisicoPdM.vue
+++ frontend/src/components/metas/CicloFisicoPdM.vue
@@ -50,11 +50,13 @@
       class="mt2 mb2"
     >
       <table class="tablemain">
-        <col>
-        <col>
-        <col>
-        <col>
-        <col>
+        <colgroup>
+          <col>
+          <col>
+          <col>
+          <col>
+          <col>
+        </colgroup>
         <thead>
           <tr>
             <th>Documento comprobatório</th>
