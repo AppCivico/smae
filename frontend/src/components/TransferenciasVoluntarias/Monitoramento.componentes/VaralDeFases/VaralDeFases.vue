b10477687a4ab170b593df02aaa17de670ec7826
Gustavo Soares <gustavo.fe.soares@gmail.com>
2025-06-02 21:09:07 -0300

    feat: adicionando linhas de fases

--- frontend/src/components/TransferenciasVoluntarias/Monitoramento.componentes/VaralDeFases/VaralDeFases.vue
+++ frontend/src/components/TransferenciasVoluntarias/Monitoramento.componentes/VaralDeFases/VaralDeFases.vue
@@ -17,17 +17,78 @@ onMounted(() => {
   <section class="varal-de-fases mt2">
     <div
       v-if="etapaCorrente?.fases.length"
-      class="flex column g2"
+      class="varal-de-fases__lista"
     >
-      <VaralDeFaseItem
+      <div
         v-for="faseObjeto in etapaCorrente.fases"
         :key="`fase--${faseObjeto.id}`"
-        :titulo="faseObjeto.fase.fase"
-        :duracao="faseObjeto.duracao"
-        :situacao="faseObjeto.andamento?.situacao?.tipo_situacao"
-        :responsavel="faseObjeto.andamento?.orgao_responsavel.sigla"
-        :tarefas="faseObjeto.tarefas"
-      />
+        class="fase-item"
+      >
+        <div class="fase-item__contador">
+          <div class="fase-item__contador-container">
+            <div class="fase-item__contador-wrapper">
+              {{ `${faseObjeto.ordem}`.padStart(2, 0) }}
+            </div>
+          </div>
+        </div>
+
+        <VaralDeFaseItem
+          :titulo="faseObjeto.fase.fase"
+          :duracao="faseObjeto.duracao"
+          :situacao="faseObjeto.andamento?.situacao?.tipo_situacao"
+          :responsavel="faseObjeto.andamento?.orgao_responsavel.sigla"
+          :tarefas="faseObjeto.tarefas"
+        />
+      </div>
     </div>
   </section>
 </template>
+
+<style lang="less" scoped>
+.varal-de-fases__lista {
+  display: flex;
+  flex-direction: column;
+}
+
+.fase-item {
+  display: flex;
+  align-items: flex-start;
+  gap: 16px;
+  position: relative;
+  padding-bottom: 60px;
+
+  &:last-of-type {
+    padding-bottom: initial;
+  }
+}
+
+.fase-item__contador {
+  display: flex;
+  justify-content: center;
+
+  &::before {
+    content: '';
+    height: 100%;
+    position: absolute;
+    width: 1px;
+    background-color: #B8C0CC;
+    z-index: -1;
+  }
+}
+
+.fase-item__contador-container {
+  transform: translateY(-25%);
+  padding: 5px;
+  border: 1px solid #B8C0CC;
+  border-radius: 999px;
+  background-color: #fff;
+}
+
+.fase-item__contador-wrapper {
+  padding: .42rem;
+  background-color: #E0F2FF;
+  border-radius: 999px;
+  font-size: 1.14rem;
+  font-weight: 700;
+}
+</style>
