8f02a7acc8ae84d82c66b5464221cee1e2c80b15
Lucas Ansei <takushilucas@gmail.com>
2025-06-04 11:38:33 -0300

    fix(report-transferencias): ajuste em fonte de dado de nro sei e status

--- backend/src/reports/transferencias/transferencias.service.ts
+++ backend/src/reports/transferencias/transferencias.service.ts
@@ -178,9 +178,14 @@ export class TransferenciasService implements ReportableService {
                 dr.conclusao_suspensiva AS distribuicao_recurso_conclusao_suspensiva,
                 dr.custeio as distribuicao_recurso_custeio,
                 dr.investimento as distribuicao_recurso_investimento,
-                drs.processo_sei AS distribuicao_recurso_sei,
+                (
+                    SELECT
+                        string_agg(processo_sei, '|')
+                    FROM distribuicao_recurso_sei
+                    WHERE distribuicao_recurso_id = dr.id
+                ) AS distribuicao_recurso_sei,
                 drst.nome_responsavel AS distribuicao_recurso_status_nome_responsavel,
-                dsb.nome AS distribuicao_recurso_status_nome_base,
+                COALESCE(dsb.nome, ds.nome) AS distribuicao_recurso_status_nome_base,
                 o2.descricao AS distribuicao_recurso_orgao_gestor
             FROM transferencia t
             JOIN transferencia_tipo tt ON tt.id = t.tipo_id
@@ -189,8 +194,18 @@ export class TransferenciasService implements ReportableService {
             LEFT JOIN partido pa ON pa.id = tp.partido_id
             JOIN orgao o1 ON o1.id = t.orgao_concedente_id
             LEFT JOIN distribuicao_recurso dr ON dr.transferencia_id = t.id AND dr.removido_em IS NULL
-            LEFT JOIN distribuicao_recurso_status drst ON drst.distribuicao_id = dr.id AND drst.removido_em IS NULL
+            LEFT JOIN (
+                SELECT
+                    id,
+                    status_base_id,
+                    status_id,
+                    distribuicao_id
+                FROM distribuicao_recurso_status 
+                ORDER BY data_troca DESC, id DESC
+                LIMIT 1
+            ) drst ON drst.distribuicao_id = dr.id AND drst.removido_em IS NULL
             LEFT JOIN distribuicao_status_base dsb ON dsb.id = drst.status_base_id
+            LEFT JOIN distribuicao_status ds ON drst.status_id = ds.id AND ds.removido_em IS NULL
             LEFT JOIN distribuicao_recurso_sei drs ON drs.distribuicao_recurso_id = dr.id AND drs.removido_em IS NULL
             LEFT JOIN classificacao cl on t.classificacao_id = cl.id and tt.id = cl.transferencia_tipo_id
             JOIN orgao o2 ON dr.orgao_gestor_id = o2.id
