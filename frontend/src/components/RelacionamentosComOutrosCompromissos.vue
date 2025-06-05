8cd7acaf9a50c7274fa2a93bfd0444f04c39c416
Sobral <robson@oficiodesign.com>
2025-06-05 10:24:13 -0300

    fix: Marcação errada de tabela

--- frontend/src/components/RelacionamentosComOutrosCompromissos.vue
+++ frontend/src/components/RelacionamentosComOutrosCompromissos.vue
@@ -147,12 +147,14 @@
 
       <RolagemHorizontal aria-labelledby="titulo-outros-relacionamentos">
         <table class="tablemain">
-          <col>
-          <col>
-          <col>
-          <col>
-          <col>
-          <col>
+          <colgroup>
+            <col>
+            <col>
+            <col>
+            <col>
+            <col>
+            <col>
+          </colgroup>
           <thead>
             <tr>
               <th>
