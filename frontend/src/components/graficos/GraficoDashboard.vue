b2dccef8bbc041c8db450252cddcc916be66dabc
Sobral <robson@oficiodesign.com>
2025-06-09 14:00:06 -0300

    fix: Estilos diversos nos paineis flutuantes

--- frontend/src/components/graficos/GraficoDashboard.vue
+++ frontend/src/components/graficos/GraficoDashboard.vue
@@ -72,6 +72,13 @@ use([
 
 provide(THEME_KEY, 'light');
 
+const formatoPadraoDeEtiquetaDeEixo = {
+  fontSize: 14,
+  fontFamily: 'Roboto',
+  fontWeight: 600,
+  color: '#142133',
+};
+
 const slots = useSlots();
 
 const props = withDefaults(defineProps<{
@@ -82,6 +89,46 @@ const props = withDefaults(defineProps<{
   tooltipTemplate: undefined,
 });
 
+const definirPadroes = (opcoes: ECBasicOption) => ({
+  ...opcoes,
+  tooltip: {
+    confine: true,
+    className: 'painel-flutuante',
+    ...(typeof opcoes.tooltip === 'object' && opcoes.tooltip !== null ? opcoes.tooltip : {}),
+  },
+  xAxis: {
+    ...(opcoes.xAxis || {}),
+    axisLabel: {
+      ...formatoPadraoDeEtiquetaDeEixo,
+      hideOverlap: false,
+      ...(
+        typeof opcoes?.xAxis?.axisLabel === 'object' && opcoes?.xAxis?.axisLabel !== null
+          ? opcoes.xAxis.axisLabel
+          : {}
+      ),
+    },
+  },
+  yAxis: {
+    ...(opcoes.yAxis || {}),
+    nameTextStyle: {
+      align: 'right',
+      ...(
+        typeof opcoes?.yAxis?.nameTextStyle === 'object' && opcoes?.yAxis?.nameTextStyle !== null
+          ? opcoes.yAxis.nameTextStyle
+          : {}
+      ),
+    },
+    axisLabel: {
+      ...formatoPadraoDeEtiquetaDeEixo,
+      ...(
+        typeof opcoes?.yAxis?.axisLabel === 'object' && opcoes?.yAxis?.axisLabel !== null
+          ? opcoes.yAxis.axisLabel
+          : {}
+      ),
+    },
+  },
+});
+
 const el = ref(null);
 const elementoPainelFlutuante = ref<HTMLElement | null>(null);
 const conteudoPainelFlutuante = ref<VNode[] | null>(null);
@@ -89,17 +136,17 @@ const conteudoPainelFlutuante = ref<VNode[] | null>(null);
 const preparedOptions = computed((): ECBasicOption => {
   const { tooltipTemplate } = props;
   if (!tooltipTemplate && !slots['painel-flutuante']) {
-    return props.option;
+    return definirPadroes(props.option);
   }
 
   return {
-    ...props.option,
+    ...definirPadroes(props.option),
     tooltip: {
       trigger: 'item',
       renderMode: 'html',
       confine: true,
       className: 'painel-flutuante',
-      formatter: (params: any) => {
+      formatter: (params: unknown) => {
         switch (true) {
           case !!slots['painel-flutuante']:
             elementoPainelFlutuante.value?.setAttribute('hidden', '');
@@ -121,14 +168,14 @@ const preparedOptions = computed((): ECBasicOption => {
             });
 
             return `
-              <div style="color: ${params.color}" class="grafico-dashboard__tooltip-conteudo">
+              <div style="color: ${params.color}" class="painel-flutuante__conteudo">
                 ${tooltipText}
               </div>
             `;
           }
           default:
             return `
-              <div style="color: ${params.color}" class="grafico-dashboard__tooltip-conteudo">
+              <div style="color: ${params.color}" class="painel-flutuante__conteudo">
                 ${params.name}: ${params.data}
               </div>
             `;
@@ -143,35 +190,5 @@ const preparedOptions = computed((): ECBasicOption => {
 <style scoped>
 .chart {
   height: 400px;
-}
-</style>
-
-<style lang="less">
-.grafico-dashboard__tooltip {
-  background-color: #E8E8E8 !important;
-  border-radius: 10px !important;
-  padding: 5px !important;
-  border: none !important;
-
-  display: none;
-  flex-direction: column;
-
-  &:before,
-  &:after {
-    content: '';
-    margin: 0 auto;
-    width: 60%;
-    height: 1px;
-    color: #232046;
-    background: #232046;
-  }
-}
-
-.grafico-dashboard__tooltip:has(.grafico-dashboard__tooltip-conteudo) {
-  display: flex !important;
-}
-
-.grafico-dashboard__tooltip-conteudo {
-  padding: 5px 0 8px;
 }
 </style>
