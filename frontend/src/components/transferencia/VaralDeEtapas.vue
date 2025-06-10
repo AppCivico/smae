46ad310ec70347bac2e70f23646f23611cc060c2
eduruiz <contato@eduruiz.com.br>
2025-06-10 12:56:52 -0300

    feat: configurar workflow com histórico e ações de reabertura/deleção
    - Adiciona exibição e controle de histórico do workflow com modal (SmallModal);
    - Adiciona componente TextoComBotao para layout informativo com botão;
    - Permite reabrir fases e deletar workflows com confirmação via AlertStore;
    - Aplica verificação de permissão (CadastroWorkflows.editar) antes de mostrar botão;
    - Exibe detalhes das ações no histórico: Deleção, Troca de Tipo, Reabertura de Fase;
    - Melhora visual com uso de imagem no fundo para etapa selecionada;
    - Refatora lógica de etapa em foco e carregamento do workflow;
    - Usa cores do tema LESS (ex: `@amarelo`) para estilização.

--- frontend/src/components/transferencia/VaralDeEtapas.vue
+++ frontend/src/components/transferencia/VaralDeEtapas.vue
@@ -1,57 +1,326 @@
 <script setup>
+import { ref } from 'vue';
 import { storeToRefs } from 'pinia';
-import { useRoute } from 'vue-router';
+import { useAuthStore } from '@/stores/auth.store';
+import { useAlertStore } from '@/stores/alert.store';
 import { useWorkflowAndamentoStore } from '@/stores/workflow.andamento.store.ts';
+import { localizarDataHorario } from '@/helpers/dateToDate';
+import dateToField from '@/helpers/dateToField';
+import SmallModal from '@/components/SmallModal.vue';
+import TextoComBotao from '@/components/TextoComBotao.vue';
 
-const { params } = useRoute();
+const authStore = useAuthStore();
+const { temPermissãoPara } = storeToRefs(authStore);
+
+const alertStore = useAlertStore();
 
 const workflowAndamento = useWorkflowAndamentoStore();
-const { workflow, chamadasPendentes, etapaEmFoco } = storeToRefs(workflowAndamento);
+const {
+  workflow,
+  chamadasPendentes,
+  etapaEmFoco,
+  historico: historicoDoWorkflow,
+} = storeToRefs(workflowAndamento);
+
+const configurarWorkflow = ref(false);
 
 workflowAndamento.buscar();
+
+function abrirConfigurarWorkflow() {
+  workflowAndamento.buscarHistorico();
+  configurarWorkflow.value = true;
+}
+
+function rabrirFase() {
+  alertStore.confirmAction('Tem certeza?', async () => {
+    if (await workflowAndamento.reabrirFase()) {
+      await workflowAndamento.buscar();
+      alertStore.success('Fase reaberta!');
+    }
+  }, 'Reabrir');
+}
+
+function deletarWorkflow() {
+  alertStore.confirmAction('Tem certeza?', async () => {
+    if (await workflowAndamento.deletarWorklow()) {
+      await workflowAndamento.buscar();
+      alertStore.success('Workflow deletado!');
+      configurarWorkflow.value = false;
+    }
+  }, 'Deletar');
+}
+
+function formatarTexto(texto) {
+  if (!texto) {
+    return '';
+  }
+  return texto.replace(/([a-z])([A-Z])/g, '$1 $2');
+}
 </script>
 <template>
-  Etapa em foco: {{ etapaEmFoco?.id }}
-  <LoadingComponent
-    v-if="chamadasPendentes?.workflow"
-    class="horizontal"
-  />
-  <template
-    v-else
-  >
-    <p
-      v-if="!workflow?.fluxo?.length"
-    >
-      Nenhuma etapa encontrada.
-    </p>
-    <ol
+  <div>
+    <TextoComBotao>
+      <template #texto>
+        <p class="t20 mb0">
+          Estas são as <strong>etapas</strong>, você pode navegar entre elas.
+        </p>
+        <p class="t20 mb0">
+          Se for necessário <strong>reabrir a fase anterior</strong> ou
+          <strong>excluir o workflow</strong>, clique em <strong>"Configurar Workflow"</strong>
+        </p>
+      </template>
+      <template #botao>
+        <button
+          v-if="temPermissãoPara('CadastroWorkflows.editar')"
+          type="button"
+          class="btn bgnone outline tcprimary"
+          @click="abrirConfigurarWorkflow"
+        >
+          configurar workflow
+        </button>
+      </template>
+    </TextoComBotao>
+
+    Etapa em foco: {{ etapaEmFoco?.id }}
+
+    <LoadingComponent
+      v-if="chamadasPendentes?.workflow"
+      class="horizontal"
+    />
+    <template
       v-else
-      ref="listaDeEtapas"
-      class="flex pb1 varal-etapas"
     >
-      <li
-        v-for="item in workflow.fluxo"
-        :key="item.id"
-        class="tc varal-etapas__etapa"
-        :class="{
-          'varal-etapas__etapa--atual': item?.atual,
-          'varal-etapas__etapa--concluida': !!item?.andamento?.concluida,
-          'varal-etapas__etapa--selecionada': item.id === etapaEmFoco?.id
-        }"
+      <p
+        v-if="!workflow?.fluxo?.length"
       >
+        Nenhuma etapa encontrada.
+      </p>
+      <ol
+        v-else
+        ref="listaDeEtapas"
+        class="flex pb1 varal-etapas"
+      >
+        <li
+          v-for="item in workflow.fluxo"
+          :key="item.id"
+          class="tc varal-etapas__etapa"
+          :class="{
+            'varal-etapas__etapa--atual': item?.atual,
+            'varal-etapas__etapa--concluida': !!item?.andamento?.concluida,
+            'varal-etapas__etapa--selecionada': item.id === etapaEmFoco?.id
+          }"
+        >
+          <button
+            type="button"
+            class="w400 like-a__text varal-etapas__nome-da-etapa"
+            @click="workflowAndamento.setEtapaEmFoco(item.id)"
+          >
+            <span class="w400">Etapa {{ item.atual ? 'atual' : '' }}</span>
+            {{ item.workflow_etapa_de.descricao }}
+          </button>
+        </li>
+      </ol>
+    </template>
+    <SmallModal v-if="configurarWorkflow">
+      <div class="flex spacebetween center mb2">
+        <h2>
+          Configurar Workflow
+        </h2>
+        <hr class="ml2 f1">
+
+        <CheckClose
+          :apenas-modal="true"
+          :formulario-sujo="false"
+          @close="configurarWorkflow = false"
+        />
+      </div>
+      <div v-if="historicoDoWorkflow?.linhas?.length">
+        <div
+          v-for="(linha, index) in historicoDoWorkflow?.linhas"
+          :key="index"
+          class="mb2"
+        >
+          <div v-if="linha.acao==='DelecaoWorkflow'">
+            <strong class="tc600">
+              <span class="tamarelo mr1">DELEÇÃO WORKFLOW </span>
+              {{ linha.criador?.nome_exibicao }} - {{ localizarDataHorario(linha.criado_em) }}
+            </strong>
+          </div>
+          <div v-if="linha.acao==='TrocaTipo'">
+            <strong class="tc600">
+              <span class="tamarelo mr1">
+                TROCA TIPO
+              </span>
+              {{ linha.criador?.nome_exibicao }} - {{ localizarDataHorario(linha.criado_em) }}
+            </strong>
+            <div class="flex mt1">
+              <dl class="mr2">
+                <p class="tc500 w700 mb0">
+                  Informação anterior
+                </p>
+                <div class="flex mt05">
+                  <dt class="w700 mr1">
+                    Nome:
+                  </dt>
+                  <dd> {{ linha.tipo_antigo.nome }}</dd>
+                </div>
+                <div class="flex mt05">
+                  <dt class="w700 mr1">
+                    Esfera:
+                  </dt>
+                  <dd>{{ linha.tipo_antigo.esfera }} </dd>
+                </div>
+              </dl>
+              <dl>
+                <p class="tc500 w700 mb0">
+                  Informação nova
+                </p>
+                <div class="flex mt05">
+                  <dt class="w700 mr1">
+                    Nome:
+                  </dt>
+                  <dd> {{ linha.tipo_novo.nome }}</dd>
+                </div>
+                <div class="flex mt05">
+                  <dt class="w700 mr1">
+                    Esfera:
+                  </dt>
+                  <dd>{{ linha.tipo_novo.esfera }} </dd>
+                </div>
+              </dl>
+            </div>
+          </div>
+          <div v-if="linha.acao==='ReaberturaFaseWorkflow'">
+            <strong class="tc600 uc">
+              <span class="tamarelo mr1">
+                ABERTURA FASE:
+              </span>
+              {{ formatarTexto(linha.dados_extra?.faseReaberta?.fase) || ' - ' }}
+            </strong> <br>
+            <strong class="tc600">
+              {{ linha.criador?.nome_exibicao }} - {{ localizarDataHorario(linha.criado_em) }}
+            </strong>
+            <div class="flex mt1">
+              <dl class="mr2">
+                <p class="tc500 w700 mb0">
+                  Fase reaberta
+                </p>
+                <div class="flex mt05">
+                  <dt class="w700 mr1">
+                    Órgão responsável:
+                  </dt>
+                  <dd>{{ linha.dados_extra?.faseReaberta?.orgao_responsavel?.sigla || ' - ' }}</dd>
+                </div>
+                <div class="flex mt05">
+                  <dt class="w700 mr1">
+                    Pessoa responsável:
+                  </dt>
+                  <dd>
+                    {{
+                      linha.dados_extra?.faseReaberta?.pessoa_responsavel?.nome_exibicao || ' - '
+                    }}
+                  </dd>
+                </div>
+                <div class="flex mt05">
+                  <dt class="w700 mr1">
+                    Data de início:
+                  </dt>
+                  <dd>{{ dateToField(linha.dados_extra?.faseReaberta?.data_inicio) || ' - ' }} </dd>
+                </div>
+                <div class="flex mt05">
+                  <dt class="w700 mr1">
+                    Situação:
+                  </dt>
+                  <dd>
+                    {{ formatarTexto(
+                      linha.dados_extra?.faseReaberta?.situacao?.tipo_situacao
+                    ) || ' - ' }}
+                  </dd>
+                </div>
+              </dl>
+              <dl
+                v-if="linha.dados_extra?.faseIncompleta"
+                class="mr2"
+              >
+                <p class="tc500 w700 mb0">
+                  Fase incompleta
+                </p>
+                <div class="flex mt05">
+                  <dt class="w700 mr1">
+                    Órgão responsável:
+                  </dt>
+                  <dd>
+                    {{ linha.dados_extra?.faseIncompleta?.orgao_responsavel?.sigla || ' - ' }}
+                  </dd>
+                </div>
+                <div class="flex mt05">
+                  <dt class="w700 mr1">
+                    Pessoa responsável:
+                  </dt>
+                  <dd>
+                    {{
+                      linha.dados_extra?.faseIncompleta?.pessoa_responsavel?.nome_exibicao || ' - '
+                    }}
+                  </dd>
+                </div>
+                <div class="flex mt05">
+                  <dt class="w700 mr1">
+                    Data de início:
+                  </dt>
+                  <dd>
+                    {{ dateToField(linha.dados_extra?.faseIncompleta?.data_inicio) || ' - ' }}
+                  </dd>
+                </div>
+                <div class="flex mt05">
+                  <dt class="w700 mr1">
+                    Situação:
+                  </dt>
+                  <dd>
+                    {{ formatarTexto(
+                      linha.dados_extra?.faseIncompleta?.situacao?.tipo_situacao
+                    ) || ' - ' }}
+                  </dd>
+                </div>
+              </dl>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        v-else
+        class="mb1"
+      >
+        Este workflow ainda <strong>não</strong> possui histórico.
+      </div>
+      <div class="flex justifycenter">
         <button
           type="button"
-          class="w400 like-a__text varal-etapas__nome-da-etapa"
-          @click="workflowAndamento.setEtapaEmFoco(item.id)"
+          class="btn bgnone outline tvermelho mr1"
+          @click="deletarWorkflow()"
         >
-          <span class="w400">Etapa {{ item.atual ? 'atual' : '' }}</span>
-          {{ item.workflow_etapa_de.descricao }}
+          fechar e deletar workflow
         </button>
-      </li>
-    </ol>
-  </template>
+        <button
+          v-if="workflow?.pode_reabrir_fase"
+          type="button"
+          class="btn mr1"
+          @click="rabrirFase()"
+        >
+          reabrir fase
+        </button>
+        <button
+          type="button"
+          class="btn"
+          @click="configurarWorkflow = false"
+        >
+          fechar
+        </button>
+      </div>
+      <div />
+    </SmallModal>
+  </div>
 </template>
-<style scoped>
+<style scoped lang="less">
 .varal-etapas {
   display: flex;
   align-items: flex-start;
@@ -113,19 +382,19 @@ workflowAndamento.buscar();
 
 .varal-etapas__etapa--concluida {
   &::before {
-    background-color: #ffda00;
+    background-color: @amarelo;
   }
 }
 
 .varal-etapas__etapa--atual {
   &::before {
-    background-color: #ffda00;
+    background-color: @amarelo;
   }
 }
 
 .varal-etapas__etapa--selecionada {
   &::after {
-    background-color: #ffda00;
+    background-color: @amarelo;
   }
 }
 
@@ -150,27 +419,11 @@ workflowAndamento.buscar();
 
 .varal-etapas__etapa--selecionada .varal-etapas__nome-da-etapa span {
   &::before {
-    --step-yellow: #ffda00;
-    --border-thickness: 2px;
-    --radius: 16px;
-
-    /* socorro deus */
-    background:
-      linear-gradient(var(--step-yellow), var(--step-yellow))
-      no-repeat
-      0px 60% / 4px 30%,
-
-      linear-gradient(var(--step-yellow), var(--step-yellow))
-      no-repeat
-      right 27px / 5px calc(100% - 30px),
-
-      linear-gradient(var(--step-yellow), var(--step-yellow))
-      no-repeat
-      bottom 3px right / 60% 2px,
-
-      radial-gradient(circle, var(--step-yellow) 3px, transparent 3px)
-      no-repeat
-      bottom right 62% / 6px 8px
+    background-image: url('@{u}/icons/fase-ativa-bg.svg');
+    background-repeat: no-repeat;
+    // hardcoded em px porque tá complicado
+    background-size: 93px 118px;
+    background-position: 100% 23px;
   }
 }
 </style>
