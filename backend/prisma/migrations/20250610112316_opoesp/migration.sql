908480fc01f5fe0c19a87df2393ddba0c0f6c119
Renato Cron <renato.cron@gmail.com>
2025-06-10 14:04:22 -0300

    Dica de como inserir o TRANSFEREGOV_API_PREFIX/TRANSFEREGOV_API_ESPECIAIS_PREFIX

--- backend/prisma/migrations/20250610112316_opoesp/migration.sql
+++ backend/prisma/migrations/20250610112316_opoesp/migration.sql
@@ -9,3 +9,7 @@ ALTER COLUMN "data_disponibilizacao" DROP NOT NULL,
 ALTER COLUMN "dt_ini_receb" DROP NOT NULL,
 ALTER COLUMN "dt_fim_receb" DROP NOT NULL,
 ALTER COLUMN "cod_orgao_sup_programa" DROP NOT NULL;
+
+-- insert into smae_config (key ,value) values ('TRANSFEREGOV_API_PREFIX', 'http://smae_transferegov:80/comunicados/');
+-- insert into smae_config (key ,value) values ('TRANSFEREGOV_API_ESPECIAIS_PREFIX', 'http://smae_transferegov:80/especiais/');
+
