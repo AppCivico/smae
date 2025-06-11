766c5ec9d263f5b3562cdce47183411d88e138df
Gustavo Soares <gustavo.fe.soares@gmail.com>
2025-06-10 21:11:48 -0300

    feat: revisando task

--- frontend/src/consts/formSchemas.js
+++ frontend/src/consts/formSchemas.js
@@ -714,7 +714,7 @@ export const fase = object()
     inicio_real: string()
       .nullable()
       .matches(regEx['day/month/year'], 'Formato inválido')
-      .label('xInício real'),
+      .label('Início real'),
     ordem: string()
       .nullable()
       .label('Ordem'),
