48bd7a60c0b50f6e21f104c9d396a835cd830b1c
Renato Cron <renato.cron@gmail.com>
2025-07-11 16:52:18 -0300

    mv vendor

--- utils/sync-transfere-gov.sh
+++ utils/sync-transfere-gov.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rsync --exclude ".git" -av $HOME/projetos/appcivico/middleware_smae_comunicados_transferegov/ $HOME/projetos/appcivico/smae/vendor/backend-transfere-gov/ --delete
+rsync --exclude ".git" -av $HOME/projetos/appcivico/middleware_smae_comunicados_transferegov/ $HOME/projetos/appcivico/smae/backend-transfere-gov/vendor/backend-transfere-gov/ --delete
 
-rsync --exclude ".git" -av $HOME/projetos/appcivico/middleware_oportunidades_transferencias_especiais/ $HOME/projetos/appcivico/smae/vendor/backend-transfere-gov-especiais/ --delete
+rsync --exclude ".git" -av $HOME/projetos/appcivico/middleware_oportunidades_transferencias_especiais/ $HOME/projetos/appcivico/smae/backend-transfere-gov-especiais/vendor/backend-transfere-gov-especiais/ --delete
