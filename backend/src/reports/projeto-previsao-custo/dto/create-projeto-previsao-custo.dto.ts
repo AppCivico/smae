7c248127c0014a81f5131d446e86ddc4b5d14627
Renato Cron <renato.cron@gmail.com>
2025-07-15 17:38:01 -0300

    feat(dto): adiciona campo opcional projeto_id em CreateRelProjetoPrevisaoCustoDto
    Inclui o campo projeto_id como opcional para resolver problemas de validação que exigiam sua presença, mesmo quando não necessário. Isso melhora a flexibilidade do DTO ao permitir que o campo seja omitido quando não for relevante, enquanto ainda permite a transformação e exposição adequadas quando fornecido.

--- backend/src/reports/projeto-previsao-custo/dto/create-projeto-previsao-custo.dto.ts
+++ backend/src/reports/projeto-previsao-custo/dto/create-projeto-previsao-custo.dto.ts
@@ -1,6 +1,6 @@
 import { OmitType } from '@nestjs/swagger';
 import { Transform, Expose } from 'class-transformer';
-import { IsInt } from 'class-validator';
+import { IsInt, IsOptional } from 'class-validator';
 import { SuperCreateRelPrevisaoCustoDto } from 'src/reports/previsao-custo/dto/create-previsao-custo.dto';
 
 export class CreateRelProjetoPrevisaoCustoDto extends OmitType(SuperCreateRelPrevisaoCustoDto, [
@@ -10,6 +10,7 @@ export class CreateRelProjetoPrevisaoCustoDto extends OmitType(SuperCreateRelPre
     'pdm_id',
     'tags',
     'portfolio_id', // excluindo pra recriar com sem o decorator poluir
+    'projeto_id',
 ] as const) {
     /**
      * required nesse endpoint!
@@ -19,6 +20,16 @@ export class CreateRelProjetoPrevisaoCustoDto extends OmitType(SuperCreateRelPre
     @Transform(({ value }: any) => +value)
     @Expose()
     portfolio_id: number;
+
+    /**
+     * redeclarando pois estava reclamando ainda que era required
+     * @example ""
+     */
+    @IsInt()
+    @Transform(({ value }: any) => +value)
+    @IsOptional()
+    @Expose()
+    projeto_id?: number;
 }
 
 export class CreateRelObrasPrevisaoCustoDto extends CreateRelProjetoPrevisaoCustoDto {}
