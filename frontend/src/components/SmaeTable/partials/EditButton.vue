ab4a696b474c35245f2bf0ef58b9119396a34dd3
Gustavo Soares <gustavo.fe.soares@gmail.com>
2025-08-07 15:00:59 -0300

    feat: ajustando SmaeTable para atender melhor edicoes e slots

--- frontend/src/components/SmaeTable/partials/EditButton.vue
+++ frontend/src/components/SmaeTable/partials/EditButton.vue
@@ -10,13 +10,15 @@
 </template>
 
 <script lang="ts" setup>
-import obterPropriedadeNoObjeto from '@/helpers/objetos/obterPropriedadeNoObjeto';
 import { computed } from 'vue';
 import { RouteLocationRaw } from 'vue-router';
+import obterPropriedadeNoObjeto from '@/helpers/objetos/obterPropriedadeNoObjeto';
 import type { Linha } from '../tipagem';
 
+type FuncaoRotaEditar = (linha: Linha) => RouteLocationRaw;
+
 export type EditButtonProps = {
-  rotaEditar?: string | RouteLocationRaw
+  rotaEditar?: string | RouteLocationRaw | FuncaoRotaEditar
   parametroDaRotaEditar?: string
   parametroNoObjetoParaEditar?: string
 };
@@ -30,6 +32,10 @@ type Props = Omit<EditButtonProps, 'parametroDaRotaEditar' | 'parametroNoObjetoP
 const props = defineProps<Props>();
 
 const parametrosEditar = computed<RouteLocationRaw>(() => {
+  if (typeof props.rotaEditar === 'function') {
+    return props.rotaEditar(props.linha);
+  }
+
   const valorDoParametro = obterPropriedadeNoObjeto(props.parametroNoObjetoParaEditar, props.linha);
 
   if (typeof props.rotaEditar === 'string') {
