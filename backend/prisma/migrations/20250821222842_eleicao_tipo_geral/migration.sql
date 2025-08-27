ee4d2253bf0b453ca8d6c7e78bf0017f3b6d5d48
Renato Cron <renato.cron@gmail.com>
2025-08-27 14:26:48 -0300

    Trocar upgrade

--- backend/prisma/migrations/20250821222842_eleicao_tipo_geral/migration.sql
+++ backend/prisma/migrations/20250821222842_eleicao_tipo_geral/migration.sql
@@ -1,3 +1,9 @@
 -- AlterEnum
 ALTER TYPE "EleicaoTipo" ADD VALUE 'Geral';
-update eleicao set tipo ='Geral' where tipo='Estadual';
+INSERT INTO eleicao (id, tipo, ano, atual_para_mandatos, removido_em)
+SELECT id, 'Geral', ano, atual_para_mandatos, removido_em
+FROM eleicao
+WHERE tipo = 'Estadual'
+ON CONFLICT (tipo, ano) DO NOTHING;
+DELETE FROM eleicao
+WHERE tipo = 'Estadual';
