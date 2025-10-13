43cdfefd2d88070c30db16616ac06d3fae748168
Gustavo Soares <gustavo.fe.soares@gmail.com>
2025-10-13 16:42:00 -0300

    feat: montando card do mapa

--- frontend/src/components/BuscadorGeolocalizacao/BuscadorGeolocalizacaoMapa.vue
+++ frontend/src/components/BuscadorGeolocalizacao/BuscadorGeolocalizacaoMapa.vue
@@ -1,18 +1,42 @@
 <script lang="ts" setup>
 import { storeToRefs } from 'pinia';
+import { computed } from 'vue';
 import MapaExibir from '@/components/geo/MapaExibir.vue';
 import { useGeolocalizadorStore } from '@/stores/geolocalizador.store';
 
 const geolocalizadorStore = useGeolocalizadorStore();
 
-const { selecionado } = storeToRefs(geolocalizadorStore);
+type Props = {
+  camposComplementares?: string[]
+};
+
+const props = defineProps<Props>();
+
+const { selecionado, proximidadeFormatada } = storeToRefs(geolocalizadorStore);
+
+const localizacoes = computed(() => proximidadeFormatada.value.map((i) => {
+  const dados = i.localizacoes[0].geom_geojson;
+  if (!dados.properties) {
+    dados.properties = {};
+  }
+
+  dados.properties.camposComplementares = {};
+
+  props.camposComplementares?.forEach((campo) => {
+    if (i[campo] !== undefined) {
+      dados.properties.camposComplementares[campo] = i[campo];
+    }
+  });
+
+  return dados;
+}));
 
 </script>
 
 <template>
   <MapaExibir
     :key="selecionado"
-    :geo-json="selecionado?.enderecos ?? undefined"
+    :geo-json="localizacoes ?? undefined"
     :camadas="selecionado?.camadas ?? undefined"
     class="mb1"
     :opções-do-polígono="{
@@ -20,5 +44,12 @@ const { selecionado } = storeToRefs(geolocalizadorStore);
       opacity: 0.5,
     }"
     zoom="16"
-  />
+  >
+    <template #painel-flutuante="slotProps">
+      <slot
+        name="painel-flutuante"
+        v-bind="slotProps"
+      />
+    </template>
+  </MapaExibir>
 </template>
