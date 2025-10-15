d15f1d8a9843d38f95aa84b7a6256982e936f944
Lucas Ansei <takushilucas@gmail.com>
2025-10-15 15:55:25 -0300

    refactor(tipo-vinculo): removendo código duplicado

--- backend/src/casa-civil/tipo-vinculo/tipo-vinculo.service.ts
+++ backend/src/casa-civil/tipo-vinculo/tipo-vinculo.service.ts
@@ -30,16 +30,6 @@ export class TipoVinculoService {
             if (!self) throw new HttpException('Tipo de vínculo não encontrado', 404);
 
             // TODO?: verificar se está em uso.
-
-            await this.prisma.tipoVinculo.updateMany({
-                where: { id },
-                data: {
-                    nome: dto.nome,
-                    atualizado_por: user ? user.id : undefined,
-                    atualizado_em: new Date(Date.now()),
-                },
-            });
-        } else {
         }
 
         const created = await this.prisma.tipoVinculo.upsert({
