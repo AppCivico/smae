8227b6bd039181239e12afe31c218dc8b44a92b5
Lucas Ansei <takushilucas@gmail.com>
2025-10-17 13:07:40 -0300

    fix(vinculos): transform em params numerico de id

--- backend/src/casa-civil/vinculo/dto/filter-vinculo.dto.ts
+++ backend/src/casa-civil/vinculo/dto/filter-vinculo.dto.ts
@@ -1,25 +1,31 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { CampoVinculo } from '@prisma/client';
+import { Type } from 'class-transformer';
 import { IsEnum, IsNumber, IsOptional } from 'class-validator';
 
 export class FilterVinculoDto {
     @IsNumber()
     @IsOptional()
+    @Type(() => Number)
     meta_id?: number;
 
     @IsNumber()
     @IsOptional()
+    @Type(() => Number)
     projeto_id?: number;
 
     @IsNumber()
     @IsOptional()
+    @Type(() => Number)
     transferencia_id?: number;
 
     @IsNumber()
     @IsOptional()
+    @Type(() => Number)
     distribuicao_id?: number;
 
     @IsNumber()
+    @Type(() => Number)
     @IsOptional()
     tipo_vinculo_id?: number;
 
