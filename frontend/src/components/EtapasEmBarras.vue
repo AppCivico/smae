06de4b8a450bf30d88e52e58d9f9864d8847305a
Gustavo Soares <gustavo.fe.soares@gmail.com>
2025-10-27 15:19:56 -0300

    feat: ajustando BEM css

--- frontend/src/components/EtapasEmBarras.vue
+++ frontend/src/components/EtapasEmBarras.vue
@@ -17,48 +17,48 @@ const totalEtapas = computed(() => props.etapas.length);
 </script>
 
 <template>
-  <div class="etapas-container">
-    <div class="etapas-barras">
+  <div class="etapas">
+    <div class="etapas__barras">
       <div
         v-for="etapa in etapas"
         :key="etapa.id"
-        class="etapa-barra"
+        class="etapas__barra"
         :class="{
-          'etapa-concluida': etapa.concluido,
+          'etapas__barra--concluida': etapa.concluido,
         }"
       />
     </div>
-    <p class="etapas-texto t13">
+    <p class="etapas__texto t13">
       Etapas {{ etapasConcluidas }} / {{ totalEtapas }}
     </p>
   </div>
 </template>
 
 <style scoped lang="less">
-.etapas-container {
+.etapas {
   display: flex;
   align-items: center;
   gap: 16px;
 }
 
-.etapas-barras {
+.etapas__barras {
   display: flex;
   gap: 10px;
   flex: 1;
 }
 
-.etapa-barra {
+.etapas__barra {
   height: 8px;
   flex: 1;
   background-color: #E0E0E0;
   transition: background-color 0.3s ease;
-
-  &.etapa-concluida {
-    background-color: #607A9F;
-  }
 }
 
-.etapas-texto {
+.etapas__barra--concluida {
+  background-color: #607A9F;
+}
+
+.etapas__texto {
   white-space: nowrap;
   color: #152741;
   margin: 0;
