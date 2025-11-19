8fd2b5d8dd2f77421444383a89990e9bb6bafedb
Gustavo Soares <gustavo.fe.soares@gmail.com>
2025-11-18 22:39:24 -0300

    feat: trocando icone

--- frontend/src/views/edicoesEmLote/obras/EdicoesEmLoteObrasConstruir.vue
+++ frontend/src/views/edicoesEmLote/obras/EdicoesEmLoteObrasConstruir.vue
@@ -439,7 +439,7 @@ async function handlePropertyChange(event, idx) {
             <svg
               width="20"
               height="20"
-            ><use xlink:href="#i_remove" /></svg>
+            ><use xlink:href="#i_waste" /></svg>
           </button>
         </div>
 
