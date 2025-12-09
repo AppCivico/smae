fd2996a5c46975aaeb07848ca2181484e702d2de
Gustavo Soares <gustavo.fe.soares@gmail.com>
2025-12-09 14:16:12 -0300

    feat: iniciando formulario

---
+++ frontend/src/components/metas/SimpleIndicador/InformarPreviaIndicador/InformarNumerica.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { ListSeriesAgrupadas } from '@back/variavel/dto/list-variavel.dto';
+
+type Props = {
+  valores: ListSeriesAgrupadas;
+};
+
+defineProps<Props>();
+
+</script>
+
+<template>
+  numerica - {{ valores }}
+</template>
