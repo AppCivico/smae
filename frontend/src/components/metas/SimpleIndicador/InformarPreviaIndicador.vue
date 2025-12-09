590b7b2f1f653fd9c8e5dbe1fc84add338c6f556
Gustavo Soares <gustavo.fe.soares@gmail.com>
2025-12-09 14:14:58 -0300

    feat: criando modal informar valores

---
+++ frontend/src/components/metas/SimpleIndicador/InformarPreviaIndicador.vue
@@ -0,0 +1,89 @@
+<script lang="ts" setup>
+// eslint-disable-next-line import/extensions
+import { IndicadorDto } from '@back/indicador/entities/indicador.entity';
+// eslint-disable-next-line import/extensions
+import type { ListSeriesAgrupadas } from '@back/variavel/dto/list-variavel.dto';
+import { computed } from 'vue';
+
+import { dateToMonthYear } from '@/helpers/dateToDate';
+
+import InformarCategorica from './InformarPreviaIndicador/InformarCategorica.vue';
+import InformarCategoricaRegionalizavel from './InformarPreviaIndicador/InformarCategoricaRegionalizavel.vue';
+import InformarNumerica from './InformarPreviaIndicador/InformarNumerica.vue';
+
+type Props = {
+  indicador: IndicadorDto;
+  valores: ListSeriesAgrupadas;
+};
+
+const props = defineProps<Props>();
+
+const variavelTipo = computed(() => {
+  if (props.indicador.indicador_tipo === 'Numerico') {
+    return 'numerico';
+  }
+
+  if (props.indicador.regionalizavel) {
+    return 'categorica_regionalizavel';
+  }
+
+  return 'categorica';
+});
+
+const configuracaoPorTipo = computed(() => {
+  const config = {
+    numerico: {
+      titulo: 'Informar qualificação prévia',
+      componente: InformarNumerica,
+    },
+    categorica: {
+      titulo: 'Informar valor prévio',
+      componente: InformarCategorica,
+    },
+    categorica_regionalizavel: {
+      titulo: 'Informar valor prévio',
+      componente: InformarCategoricaRegionalizavel,
+    },
+  };
+
+  return config[variavelTipo.value];
+});
+
+</script>
+
+<template>
+  <div class="modal-preenchimento-manual">
+    <header class="modal-header">
+      <h2>{{ configuracaoPorTipo.titulo }}</h2>
+    </header>
+
+    <div class="flex g2 mb2">
+      <svg
+        width="28"
+        height="28"
+        viewBox="0 0 28 28"
+        color="#F2890D"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <use xlink:href="#i_indicador" />
+      </svg>
+      <div class="f1">
+        <div class="t18 w700">
+          {{ props.indicador.titulo }}
+        </div>
+        <div
+          v-if="$props.valores.ultima_previa_indicador?.data_valor"
+          class="t13 tc60"
+        >
+          Ciclo: {{ dateToMonthYear($props.valores.ultima_previa_indicador.data_valor) }}
+        </div>
+      </div>
+    </div>
+
+    <component
+      :is="configuracaoPorTipo.componente"
+      :indicador="props.indicador"
+      :valores="props.valores"
+    />
+  </div>
+</template>
