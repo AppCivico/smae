a029f2c1edaa8b642725b930c47960dd3738601a
Gustavo Soares <gustavo.fe.soares@gmail.com>
2025-12-10 10:24:04 -0300

    feat: adicionando variavel categorica regionalizada

--- frontend/src/components/metas/SimpleIndicador/InformarPreviaIndicador/InformarCategoricaRegionalizavel.vue
+++ frontend/src/components/metas/SimpleIndicador/InformarPreviaIndicador/InformarCategoricaRegionalizavel.vue
@@ -1,14 +1,78 @@
 <script lang="ts" setup>
 import { ListSeriesAgrupadas } from '@back/variavel/dto/list-variavel.dto';
+import { ErrorMessage, Field, useForm } from 'vee-validate';
+import { computed } from 'vue';
+
+import SmaeLabel from '@/components/camposDeFormulario/SmaeLabel.vue';
+import { ValorPrevioCategoricaRegionalizadaSchema as schema } from '@/consts/formSchemas/InformarPreviaIndicador.schema';
 
 type Props = {
   valores: ListSeriesAgrupadas;
 };
 
-defineProps<Props>();
+const props = defineProps<Props>();
 
+const { handleSubmit, errors } = useForm({
+  validationSchema: schema,
+  initialValues: {
+    qualificacao: '',
+  },
+});
+
+const opcoesCategoria = computed(() => {
+  const categoricas = props.valores?.dados_auxiliares?.categoricas;
+  if (!categoricas) return [];
+
+  return Object.entries(categoricas).map(([chave, valor]) => ({
+    value: chave,
+    label: valor,
+  }));
+});
+
+const onSubmit = handleSubmit.withControlled((valoresControlados) => {
+  console.log(valoresControlados);
+});
 </script>
 
 <template>
-  categorica regionalizavel - {{ valores }}
+  <div class="informar-categorica-regionalizavel flex">
+    <form
+      class="f1"
+      @submit="onSubmit"
+    >
+      <div>
+        <SmaeLabel name="qualificacao" />
+
+        <Field
+          name="qualificacao"
+          as="select"
+          class="inputtext light"
+          :class="{'error': errors.qualificacao}"
+        >
+          <option value="">
+            Selecione uma categoria
+          </option>
+
+          <option
+            v-for="opcao in opcoesCategoria"
+            :key="opcao.value"
+            :value="opcao.value"
+          >
+            {{ opcao.label }}
+          </option>
+        </Field>
+
+        <ErrorMessage name="qualificacao" />
+      </div>
+
+      <div class="flex justifycenter mt2">
+        <button
+          class="btn"
+          type="submit"
+        >
+          Salvar
+        </button>
+      </div>
+    </form>
+  </div>
 </template>
