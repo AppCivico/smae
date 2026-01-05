e610ac24f9890ae9c4f9cf38118b820483a76f47
Renato Cron <renato.cron@gmail.com>
2026-01-05 15:08:30 -0300

    oops, faltou atualizar o src/app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -74,7 +74,7 @@ import { DotacaoBuscaModule } from './dotacao-busca/dotacao-busca.module';
 import { TipoVinculoModule } from './casa-civil/tipo-vinculo/tipo-vinculo.module';
 import { VinculoModule } from './casa-civil/vinculo/vinculo.module';
 import { PortfolioTagModule } from './pp/portfolio-tag/portfolio-tag.module';
-import { TipoEncerramentoModule } from './projeto-tipo-encerramento/tipo-orgao.module';
+import { TipoEncerramentoModule } from './projeto-tipo-encerramento/tipo-encerramento.module';
 
 // Hacks pro JS
 /*
