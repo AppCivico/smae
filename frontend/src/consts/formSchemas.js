974c4249d0a84c0db27d6867e0c25f51e4c6c941
Gustavo Soares <gustavo.fe.soares@gmail.com>
2026-01-06 10:13:26 -0300

    feat: criando testes para validarCasasDecimais

--- frontend/src/consts/formSchemas.js
+++ frontend/src/consts/formSchemas.js
@@ -1,18 +1,6 @@
 /* eslint-disable no-template-curly-in-string */
 import { isAfter, isBefore } from 'date-fns';
-import {
-  addMethod,
-  array,
-  boolean,
-  date,
-  lazy,
-  mixed,
-  number,
-  object,
-  ref,
-  setLocale,
-  string,
-} from 'yup';
+import { lazy } from 'yup';
 
 import cargosDeParlamentar from '@/consts/cargosDeParlamentar';
 import categoriaDeTransferencia from '@/consts/categoriaDeTransferencia';
@@ -36,8 +24,6 @@ import tiposDeOrigens from '@/consts/tiposDeOrigens';
 import tiposNaEquipeDeParlamentar from '@/consts/tiposNaEquipeDeParlamentar';
 import tiposSituacaoSchema from '@/consts/tiposSituacaoSchema';
 import tiposStatusDistribuicao from '@/consts/tiposStatusDistribuicao';
-import fieldToDate from '@/helpers/fieldToDate';
-import haDuplicatasNaLista from '@/helpers/haDuplicatasNaLista';
 
 import {
   dataMax,
@@ -46,109 +32,17 @@ import {
   startYear,
 } from './formSchemas/config/datas';
 import i18n from './formSchemas/config/i18n';
-
-// eslint-disable-next-line @typescript-eslint/naming-convention
-addMethod(string, 'fieldUntilToday', function _(errorMessage = 'Valor de ${path} futuro') {
-  // eslint-disable-next-line @typescript-eslint/naming-convention
-  return this.test('teste', errorMessage, function __(value) {
-    const { path, createError } = this;
-
-    if (!value) return true;
-
-    try {
-      const cleanDate = fieldToDate(value).replace(/-/g, '');
-      const cleanNow = new Date()
-        .toISOString()
-        .substring(0, 10)
-        .replace(/-/g, '');
-
-      return Number(cleanDate) <= Number(cleanNow)
-        || createError({ path });
-    } catch (error) {
-      return createError({ path, message: 'Valor de ${path} inválido' });
-    }
-  });
-});
-
-// eslint-disable-next-line @typescript-eslint/naming-convention
-addMethod(string, 'nullableOuVazio', function _() {
-  return this
-    .nullable()
-    .transform((v) => (v === '' ? null : v));
-});
-
-addMethod(string, 'casasDecimais', function _(casasDecimais) {
-  return this
-    .test('casas-decimais', (value, { createError }) => {
-      if (!value) return true;
-
-      const valorNumerico = value.replace(',', '.');
-      const partesDecimais = valorNumerico.split('.')[1];
-      const numeroCasasDecimais = partesDecimais ? partesDecimais.length : 0;
-
-      if (numeroCasasDecimais > casasDecimais) {
-        return createError({
-          message: `O valor deve ter no máximo ${casasDecimais} casas decimais`,
-        });
-      }
-
-      return true;
-    });
-});
-
-// eslint-disable-next-line @typescript-eslint/naming-convention
-addMethod(date, 'nullableOuVazio', function _() {
-  return this
-    .nullable()
-    .transform((v) => {
-      try {
-        v.toISOString();
-        return v;
-      } catch (e) {
-        return null;
-      }
-    });
-});
-
-// eslint-disable-next-line @typescript-eslint/naming-convention
-addMethod(mixed, 'nullableOuVazio', function _() {
-  return this
-    .nullable()
-    .transform((v) => (v === '' || v === false ? null : v));
-});
-
-// eslint-disable-next-line @typescript-eslint/naming-convention
-addMethod(number, 'nullableOuVazio', function _() {
-  return this
-    .nullable()
-    .transform((v) => (v === '' || Number.isNaN(v) ? null : v));
-});
-
-/**
- * @link https://github.com/jquense/yup/issues/384#issuecomment-442958997
- */
-// eslint-disable-next-line @typescript-eslint/naming-convention
-addMethod(mixed, 'inArray', function _(arrayToCompare, message = '${path} não encontrado em ${arrayToCompare}') {
-  return this.test({
-    message,
-    name: 'inArray',
-    exclusive: true,
-    params: { arrayToCompare },
-    test(value) {
-      return (this.resolve(arrayToCompare) || []).includes(value);
-    },
-  });
-});
-
-addMethod(mixed, 'semDuplicatas', function semDuplicatas(message = '${path} não pode ter valores repetidos', params = {}) {
-  return this.test('semDuplicatas', message, function semDuplicatasTeste(value) {
-    const { path, createError } = this;
-
-    return haDuplicatasNaLista(value, params)
-      ? createError({ path, message })
-      : true;
-  });
-});
+import {
+  array,
+  boolean,
+  date,
+  mixed,
+  number,
+  object,
+  ref,
+  setLocale,
+  string,
+} from './formSchemas/initSchema';
 
 setLocale(i18n);
 
@@ -4438,7 +4332,7 @@ export const cicloAtualizacaoModalAdicionarSchema = (posicao, casasDecimais = 0)
     valor_realizado: string()
       .label('valor realizado')
       .required()
-      .casasDecimais(casasDecimais),
+      .validarCasasDecimais(casasDecimais),
     valor_realizado_acumulado: string().required()
       .label('valor realizado acumulado'),
   };
@@ -4458,7 +4352,7 @@ export const cicloAtualizacaoModalEditarSchema = (posicao, casasDecimais = 0) =>
         valor_realizado: string()
           .label('valor realizado')
           .required()
-          .casasDecimais(casasDecimais),
+          .validarCasasDecimais(casasDecimais),
         valor_realizado_acumulado: string()
           .label('valor realizado acumulado')
           .required(),
