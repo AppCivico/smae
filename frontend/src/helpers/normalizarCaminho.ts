c416f95b83ad6a21499b650e160e974a1dc3ab78
Sobral <robson@oficiodesign.com>
2026-01-12 14:25:01 -0300

    doc: Limpar anotação

--- frontend/src/helpers/normalizarCaminho.ts
+++ frontend/src/helpers/normalizarCaminho.ts
@@ -1,7 +1,6 @@
-// remove './' inicial e garante que o caminho não é vazio e garante barra final
+// remove './' inicial, garante que o caminho não é vazio e garante barra final
 export default (caminho = '') => (typeof caminho === 'string' && caminho.length
   ? caminho
-    // .replace(/\/$/, '')
     .replace(/^\.\//, '')
     .replace(/\/?$/, '/')
   : '/');
