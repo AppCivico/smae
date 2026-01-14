ad83fdae4f44a6a2c891371aff6093fff3a15b9e
Renato Cron <renato.cron@gmail.com>
2026-01-14 18:22:05 -0300

    feat(current-ip): melhora a extração e validação de endereços IP Adiciona validação para endereços IP usando funções do módulo 'net', garantindo que o IP retornado seja válido. Além disso, ao lidar com múltiplos IPs, agora é selecionado o último, que representa o cliente real, ignorando proxies. Isso aumenta a precisão e segurança na identificação do IP do cliente.

--- backend/src/common/decorators/current-ip.ts
+++ backend/src/common/decorators/current-ip.ts
@@ -1,6 +1,7 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import * as requestIp from 'request-ip';
 import { Request } from 'express';
+import { isIP, isIPv4, isIPv6 } from 'net';
 
 export const IpAddress = createParamDecorator((data: unknown, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest();
@@ -13,5 +14,15 @@ export const extractIpAddress = (request: Request): string => {
     const ip = requestIp.getClientIp(request);
     if (Array.isArray(ip)) return ip[0];
 
-    return requestIp.getClientIp(request) || '0.0.0.0';
+    let ipAddress = requestIp.getClientIp(request) || '0.0.0.0';
+    // if multiple ips, take the last one (the first one is the proxy)
+    if (ipAddress.includes(',')) {
+        ipAddress = ipAddress.split(',').pop()?.trim() || '0.0.0.0';
+    }
+
+    if (!isIP(ipAddress)) {
+        return '0.0.0.0';
+    }
+
+    return ipAddress;
 };
