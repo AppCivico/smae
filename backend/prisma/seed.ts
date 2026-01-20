8fa6961fa7c2fa9617449d8786bff4abbd1a3199
Renato Cron <renato.cron@gmail.com>
2026-01-20 09:51:29 -0300

    fix(seed.ts): atualiza a descrição do perfil de coordenador de equipe A descrição do perfil de coordenador de equipe foi simplificada para refletir melhor o papel e as responsabilidades do usuário, removendo detalhes desnecessários e focando na participação como responsável pela equipe.

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1319,7 +1319,7 @@ PerfilAcessoConfig.push(
 
     {
         nome: atualizarNomePerfil(CONST_PERFIL_COORDENADOR_EQUIPE, ['Coordenador de Grupo de Variáveis']),
-        descricao: 'Coordena equipes; possui privilégio de colaborador no mesmo órgão da equipe',
+        descricao: 'Participa como responsável por equipe',
         privilegios: ['SMAE.GrupoVariavel.colaborador'],
     },
     {
