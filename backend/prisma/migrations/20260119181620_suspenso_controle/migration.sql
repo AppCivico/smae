9c07512e6aa1d2d556a49a3aebd422c1e3901fe7
Renato Cron <renato.cron@gmail.com>
2026-01-21 13:16:22 -0300

    move pro lugar certo

--- backend/prisma/migrations/20260119181620_suspenso_controle/migration.sql
+++ backend/prisma/migrations/20260119181620_suspenso_controle/migration.sql
@@ -3,4 +3,5 @@ ALTER TABLE "variavel_suspensa_controle" ADD COLUMN     "removido_em" TIMESTAMPT
 ADD COLUMN     "removido_por" INTEGER;
 
 -- CreateIndex
-CREATE INDEX "variavel_suspensa_controle_variavel_id_removido_em_idx" ON "variavel_suspensa_controle"("variavel_id", "removido_em");
+CREATE UNIQUE INDEX "variavel_suspensa_controle_variavel_id_serie_ciclo_referenc_key" ON "variavel_suspensa_controle"("variavel_id", "serie", "ciclo_referencia")
+where removido_em IS NULL;
