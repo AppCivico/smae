409bf801bcfc249bddad5b3d20bd770aec9cf522
Gustavo Soares <gustavo.fe.soares@gmail.com>
2026-02-12 15:18:17 -0300

    feat: ajustando permissao

--- frontend/src/router/parlamentares.js
+++ frontend/src/router/parlamentares.js
@@ -10,7 +10,7 @@ export default {
   meta: {
     título: 'Parlamentares',
     rotaPrescindeDeChave: true,
-    limitarÀsPermissões: 'CadastroParlamentar.',
+    limitarÀsPermissões: 'CadastroParlamentar.inserir',
   },
   children: [
     {
