4964cfc4c1641f696271c36b26f532d01ce7bec7
Lucas Ansei <takushilucas@gmail.com>
2026-02-13 13:46:18 -0300

    fix: typo

--- backend/src/dotacao-busca/dto/dotacao-busca.dto.ts
+++ backend/src/dotacao-busca/dto/dotacao-busca.dto.ts
@@ -1,4 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { TipoPdm } from '@prisma/client';
 import { Transform, Type } from 'class-transformer';
 import { IsBoolean, IsInt, IsOptional, IsString, Max, Min } from 'class-validator';
 
