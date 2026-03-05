3093d73013daa9b552d5e82f21b3c79c1ced34c8
Gustavo Soares <gustavo.fe.soares@gmail.com>
2026-03-05 16:03:30 -0300

    feat: ajustando migalha de pao

--- frontend/src/router/obras.contratos.js
+++ frontend/src/router/obras.contratos.js
@@ -94,7 +94,6 @@ export default {
               'obrasListar',
               'obrasResumo',
               'contratosDaObraListar',
-              'contratosDaObraResumo',
             ],
           },
         },
