6cf0664a8f8d4f9d679368cc243470cefd2d75cd
Gustavo Soares <gustavo.fe.soares@gmail.com>
2026-03-11 08:36:13 -0300

    feat: ajustando lista de area tematica

--- frontend/src/views/areasTematicas/AreasTematicasLista.vue
+++ frontend/src/views/areasTematicas/AreasTematicasLista.vue
@@ -21,7 +21,11 @@ const listaOrdenada = computed(() => {
     return a.nome.localeCompare(b.nome);
   });
 
-  return listaClone;
+  return listaClone.map((item) => ({
+    ...item,
+    acoes_ativas: item.acoes?.filter((a) => a.ativo) ?? [],
+    acoes_inativas: item.acoes?.filter((a) => !a.ativo) ?? [],
+  }), {});
 });
 
 async function excluirArea(linha) {
@@ -59,10 +63,13 @@ onMounted(() => {
   <LoadingComponent v-if="areasTematicasStore.chamadasPendentes.lista" />
   <SmaeTable
     v-else
+    class="area-tematica__tabela"
     :dados="listaOrdenada"
     :colunas="[
       { chave: 'nome', label: 'Área' },
-      { chave: 'ativo', label: 'Ativo' },
+      { chave: 'ativo', label: 'Área ativa' },
+      { chave: 'acoes_ativas', label: 'ações ativas' },
+      { chave: 'acoes_inativas', label: 'ações inativas' },
     ]"
     :rota-editar="({ id }) => ({
       name: 'areasTematicas.editar',
@@ -74,5 +81,46 @@ onMounted(() => {
     <template #celula:ativo="{ linha }">
       {{ linha.ativo ? 'Sim' : 'Não' }}
     </template>
+
+    <template #celula:acoes_ativas="{ linha }">
+      <ul v-if="linha.acoes_ativas.length">
+        <li
+          v-for="acao in linha.acoes_ativas"
+          :key="acao.id"
+        >
+          {{ acao.nome }}
+        </li>
+      </ul>
+
+      <span v-else />
+    </template>
+
+    <template #celula:acoes_inativas="{ linha }">
+      <ul v-if="linha.acoes_inativas.length">
+        <li
+          v-for="acao in linha.acoes_inativas"
+          :key="acao.id"
+        >
+          {{ acao.nome }}
+        </li>
+      </ul>
+
+      <span v-else />
+    </template>
   </SmaeTable>
 </template>
+
+<style lang="less" scoped>
+.area-tematica__tabela {
+  :deep {
+    .table-cell--acoes_ativas,
+    .table-cell--acoes_inativas {
+      vertical-align: top;
+    }
+
+    ul, li {
+      list-style: disc;
+    }
+  }
+}
+</style>
