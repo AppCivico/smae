f9407aa5f019a72611668a5d5285ccfb5de273be
Lucas Ansei <takushilucas@gmail.com>
2026-03-13 14:55:31 -0300

    Revert "fix: query"
    This reverts commit 1188a654f80aa9c625f5e440b1915b719ba1dc28.

--- backend/src/reports/casa-civil-atividades-pendentes/casa-civil-atividades-pendentes.service.ts
+++ backend/src/reports/casa-civil-atividades-pendentes/casa-civil-atividades-pendentes.service.ts
@@ -30,7 +30,8 @@ export class CasaCivilAtividadesPendentesService implements ReportableService {
     `;
 
         if (params.tipo_id && params.tipo_id.length > 0)
-            whereConditions = Prisma.sql`${whereConditions} AND tt.id IN (${Prisma.join(params.tipo_id)})`;
+            whereConditions = Prisma.sql`${whereConditions} AND tt.id = ANY(${params.tipo_id}::int[])`;
+
         if (params.data_inicio) {
             const dataInicio = Date2YMD.toString(params.data_inicio);
             whereConditions = Prisma.sql`${whereConditions} AND tf.inicio_planejado >= ${dataInicio}::date`;
@@ -45,7 +46,8 @@ export class CasaCivilAtividadesPendentesService implements ReportableService {
             whereConditions = Prisma.sql`${whereConditions} AND t.esfera = ${params.esfera}::"TransferenciaTipoEsfera"`;
 
         if (params.orgao_id && params.orgao_id.length > 0)
-            whereConditions = Prisma.sql`${whereConditions} AND tf.orgao_id IN (${Prisma.join(params.orgao_id)})`;
+            whereConditions = Prisma.sql`${whereConditions} AND tf.orgao_id = ANY(${params.orgao_id}::int[])`;
+
         const linhas = await this.prisma.$queryRaw`
         SELECT
             t.identificador,
