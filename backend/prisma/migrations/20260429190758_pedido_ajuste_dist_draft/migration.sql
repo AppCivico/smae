d5fa7e7ea113d15b6969d6c06c476e6c970d70d3
Lucas Ansei <takushilucas@gmail.com>
2026-04-29 18:25:23 -0300

    fix: migration

--- backend/prisma/migrations/20260429190758_pedido_ajuste_dist_draft/migration.sql
+++ backend/prisma/migrations/20260429190758_pedido_ajuste_dist_draft/migration.sql
@@ -1,8 +1,1 @@
 ALTER TYPE "DistribuicaoSolicitacaoStatus" ADD VALUE 'EmRegistro' BEFORE 'Pendente';
-
--- Commit para que o novo valor do enum fique disponível (PG exige isso).
-COMMIT;
-BEGIN;
-
-ALTER TABLE "distribuicao_recurso_solicitacao_ajuste"
-ALTER COLUMN "status" SET DEFAULT 'EmRegistro'::"DistribuicaoSolicitacaoStatus";
