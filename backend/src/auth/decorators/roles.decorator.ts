7d915d5a0782454a7abcff92e921f35d8df26e24
Lucas Ansei <takushilucas@gmail.com>
2026-05-05 09:41:35 -0300

    fix: decorators

--- backend/src/auth/decorators/roles.decorator.ts
+++ backend/src/auth/decorators/roles.decorator.ts
@@ -7,11 +7,17 @@ export const RequerPriv = (...roles: ListaDePrivilegios[]) => SetMetadata(ROLES_
 
 export function Roles(roles: ListaDePrivilegios[], summary?: string): MethodDecorator & ClassDecorator {
     summary = summary ? `${summary} ` : '';
-    summary = `${summary}(Privilégios: ${Array.isArray(roles) ? roles.join(', ') : ''})`;
-    return applyDecorators(
-        ApiBearerAuth('access-token'),
-        ApiUnauthorizedResponse(),
-        ApiOperation({ summary: summary }),
-        RequerPriv(...roles)
-    );
+    const summaryText = `${summary}(Privilégios: ${Array.isArray(roles) ? roles.join(', ') : ''})`;
+
+    return (target: any, key?: string | symbol, descriptor?: PropertyDescriptor): any => {
+        if (descriptor !== undefined) {
+            return applyDecorators(
+                ApiBearerAuth('access-token'),
+                ApiUnauthorizedResponse(),
+                ApiOperation({ summary: summaryText }),
+                RequerPriv(...roles)
+            )(target, key!, descriptor);
+        }
+        return applyDecorators(ApiBearerAuth('access-token'), ApiUnauthorizedResponse(), RequerPriv(...roles))(target);
+    };
 }
