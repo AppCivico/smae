a151dacf701c0a2d8acc2ed1d11af38a05fc5b75
Sobral <robson@oficiodesign.com>
2026-06-29 19:08:30 -0300

    refactor: Aprimorar tipagem

--- frontend/src/helpers/nulificadorTotal.ts
+++ frontend/src/helpers/nulificadorTotal.ts
@@ -1,23 +1,27 @@
 interface QualquerObjeto {
-  [key: string]: any;
+  [key: string]: unknown;
 }
 
-const nulificadorTotal = (obj: any): any => {
+const nulificadorTotal = (obj: unknown): unknown => {
   if (Array.isArray(obj)) {
-    return obj.map((item: any) => nulificadorTotal(item));
-  } if (typeof obj === 'object' && obj !== null) {
+    return obj.map((item: unknown) => nulificadorTotal(item));
+  }
+
+  if (typeof obj === 'object' && obj !== null) {
     return Object.keys(obj)
       .reduce((acc: QualquerObjeto, key: string) => {
-        if (typeof obj[key] === 'object' && obj[key] !== null) {
-          acc[key] = nulificadorTotal(obj[key]);
-        } else if (typeof obj[key] === 'string') {
-          acc[key] = obj[key].trim() || null;
+        const value = (obj as QualquerObjeto)[key];
+        if (typeof value === 'object' && value !== null) {
+          acc[key] = nulificadorTotal(value);
+        } else if (typeof value === 'string') {
+          acc[key] = value.trim() || null;
         } else {
-          acc[key] = obj[key];
+          acc[key] = value;
         }
         return acc;
       }, {});
   }
+
   return typeof obj === 'string'
     ? (obj.trim() || null)
     : obj;
