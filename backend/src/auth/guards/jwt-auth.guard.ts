d32c02eb25c91604d535beb0a3c20fc9972ead71
Renato Cron <renato.cron@gmail.com>
2026-07-07 11:28:33 -0300

    fix(auth): substitui UnauthorizedException por ForbiddenException em guards A troca de UnauthorizedException por ForbiddenException nos guards de autenticação e roles melhora a precisão semântica das exceções lançadas, indicando que o usuário foi autenticado, mas não possui as permissões necessárias para acessar o recurso solicitado. Isso ajuda a diferenciar entre falhas de autenticação e autorização, fornecendo mensagens de erro mais claras e apropriadas.

--- backend/src/auth/guards/jwt-auth.guard.ts
+++ backend/src/auth/guards/jwt-auth.guard.ts
@@ -1,4 +1,10 @@
-import { BadRequestException, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+    BadRequestException,
+    ExecutionContext,
+    ForbiddenException,
+    Injectable,
+    UnauthorizedException,
+} from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 import { IS_PUBLIC_KEY } from '../decorators/is-public.decorator';
@@ -29,7 +35,7 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
         const canActivatePromise = canActivate as Promise<boolean>;
 
         return canActivatePromise.catch((error) => {
-            if (error instanceof BadRequestException) {
+            if (error instanceof BadRequestException || error instanceof ForbiddenException) {
                 throw error;
             } else if (error instanceof UnauthorizedError) {
                 throw new UnauthorizedException(error.message);
